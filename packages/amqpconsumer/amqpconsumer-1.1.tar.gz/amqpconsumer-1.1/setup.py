e42747290df067b1c0d3ba854719820a4cc8f0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package amqpconsumer/1.1/amqpconsumer-1.1.tar.gz

---
+++ packages/amqpconsumer/amqpconsumer-1.1.tar.gz/amqpconsumer-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='amqpconsumer',
+    version='1.1',
+    description='AMQP event listener',
+    long_description='Python module to facilitate listening for and acting on AMQP events.',
+    url='https://github.com/ByteInternet/amqpconsumer',
+    author='Byte B.V.',
+    author_email='tech@byte.nl',
+    license='3-clause BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Topic :: Communications',
+        'Topic :: Internet',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='amqp events consumer listener rabbitmq',
+    packages=['amqpconsumer'],
+    install_requires=['pika>=0.9.8']
+)
