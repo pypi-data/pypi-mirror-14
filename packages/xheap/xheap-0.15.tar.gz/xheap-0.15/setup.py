b81e4a90b19ce2b0f2aa1ac49b630226f96d7835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package xheap/0.15/xheap-0.15.tar.gz

---
+++ packages/xheap/xheap-0.15.tar.gz/xheap-0.15/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='xheap',
+    version='0.15',
+    description='Heap Implementation for Python',
+    author='Sven R. Kunze',
+    author_email='srkunze@mail.de',
+    url='https://github.com/srkunze/xheap',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+
+    py_modules=['xheap'],
+    install_requires=[],
+)
