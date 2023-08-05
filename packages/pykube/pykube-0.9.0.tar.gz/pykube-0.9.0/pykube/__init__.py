52789aa505d2527d4ce04f160a8eb1b11e2c26bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package pykube/0.9.0/pykube-0.9.0.tar.gz

---
+++ packages/pykube/pykube-0.9.0.tar.gz/pykube-0.9.0/pykube/__init__.py
@@ -0,0 +1,20 @@
+"""
+Python client for Kubernetes
+"""
+
+from .config import KubeConfig  # noqa
+from .exceptions import KubernetesError, PyKubeError, ObjectDoesNotExist  # noqa
+from .http import HTTPClient  # noqa
+from .objects import (  # noqa
+    Namespace,
+    Node,
+    Service,
+    Endpoint,
+    Secret,
+    ReplicationController,
+    Pod,
+    Job,
+    DaemonSet,
+    Deployment,
+)
+from .query import now, all_ as all, everything  # noqa
