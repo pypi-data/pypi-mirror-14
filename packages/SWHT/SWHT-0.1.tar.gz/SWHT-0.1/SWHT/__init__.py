40f37a8eeebd828d831cc5419f865e5b6b8e34a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package SWHT/0.1/SWHT-0.1.tar.gz

---
+++ packages/SWHT/SWHT-0.1.tar.gz/SWHT-0.1/SWHT/__init__.py
@@ -0,0 +1,6 @@
+"""
+A module interface to SWHT functions
+"""
+
+import ecef, fileio, ft, lofarConfig, swht, util, Ylm
+
---
+++ packages/SWHT/SWHT-0.1.tar.gz/SWHT-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import numpy as np
+import os, sys, glob
+
+__version__ = '0.1'
+
+setup(name = 'SWHT',
+    version = __version__,
+    description = 'Spherical Wave Harmonic Transform for radio astronomy visibility data',
+    long_description = "Spherical Wave Harmonic Transform for radio astronomy visibility data as described by T. Carozzi in 'Imaging on a Sphere with Interferometers: the Spherical Wave Harmonic Transform'",
+    author='Griffin Foster',
+    author_email='griffin.foster@gmail.com',
+    url='https://github.com/griffinfoster/SWHT',
+    platforms=['*nix'],
+    license='GPL',
+    requires = ['distutils','numpy','scipy','matplotlib','healpy','pyrap','ephem'],
+    provides = ['SWHT'],
+    packages = ['SWHT'],
+    include_dirs=[np.get_include()],
+    package_data={'SWHT': ['data/LOFAR/StaticMetaData/*.conf', 'data/LOFAR/*.txt']},
+    #scripts=glob.glob('scripts/*.py'),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+)
+
