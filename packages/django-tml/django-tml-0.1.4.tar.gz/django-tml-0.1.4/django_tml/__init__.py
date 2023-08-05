25839ad58c594cf59afdea0fa4310113ce55e7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:51 +0000

    Add package django-tml/0.1.4/django-tml-0.1.4.tar.gz

---
+++ packages/django-tml/django-tml-0.1.4.tar.gz/django-tml-0.1.4/django_tml/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+# encoding: UTF-8
+from django.utils import translation
+from .translator import Translation
+from django.conf import settings
+from tml.logger import get_logger
+
+
+__VERSION__ = '0.1.4'
+
+
+def tr(label, data = None, description = None, options = {}):
+    _, value, error = Translation.instance().context.tr(label, data, description, options)
+    if error:
+        get_logger().exception(error)
+    return value
+
+def activate(locale, tml_settings=None):
+    Translation.instance(tml_settings=tml_settings).activate(locale)
+
+def activate_source(source):
+    """ Use source block
+        Args:
+            source (string): source name
+    """
+    Translation.instance().activate_source(source)
+
+def deactivate_source():
+    Translation.instance().deactivate_source()
+
+def get_languages():
+    return Translation.instance().languages
+
+if settings.TML.pop('monkeypatch', False):
+    translation._trans = Translation.instance()
+
+
