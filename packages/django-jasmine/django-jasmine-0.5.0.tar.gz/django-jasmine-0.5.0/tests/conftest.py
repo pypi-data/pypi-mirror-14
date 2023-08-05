563b1a1d182860c76a30b4d979e1e5d779c2247f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:53 +0000

    Add package django-jasmine/0.5.0/django-jasmine-0.5.0.tar.gz

---
+++ packages/django-jasmine/django-jasmine-0.5.0.tar.gz/django-jasmine-0.5.0/tests/conftest.py
@@ -0,0 +1,26 @@
+"""Minimal settings needed for tests."""
+
+import os
+
+path_to_repo = os.path.dirname((os.path.dirname(os.path.abspath(__file__))))
+
+def pytest_configure():
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG=True,
+        DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3',
+                               'NAME': 'sqlite.db'}},
+        SECRET_KEY='Just a test key',
+        JASMINE_TEST_DIRECTORY= '%s/%s' % (path_to_repo, 'example_project/example_project/static/'),
+        STATIC_URL='/static/',
+        ROOT_URLCONF='tests.urls',
+        INSTALLED_APPS=(
+            'django_jasmine',
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
+        ),
+    )
+
+    import django
+    django.setup()
