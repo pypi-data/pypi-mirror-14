b11670050a7fbbe9a80c170d41221ef4b142e524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package ddbgraphite/0.0.7/ddbgraphite-0.0.7.tar.gz

---
+++ packages/ddbgraphite/ddbgraphite-0.0.7.tar.gz/ddbgraphite-0.0.7/ddbgraphite/listen.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import time
+import SocketServer
+from ddbpy.client import Send
+
+#DFE = ('127.0.0.1', 5555)
+DFE = ('192.168.99.100', 5555)
+
+BUCKET = 'graphite'
+DEBUG = True
+
+
+class TCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        try:
+            data = self.request.recv(1024).strip()
+            if data:
+                if DEBUG:
+                    print data.split()[0], data.split()[1]
+                with Send(DFE) as send:
+                    send.switch_streaming(BUCKET)
+                    ts = int(time.time())
+                    metric = data.split()[0]
+                    value = data.split()[1]
+                    send.send_payload(metric, ts, value)
+        except Exception, e:
+            print e
+
+
+def main():
+    HOST, PORT = "0.0.0.0", 2003
+    server = SocketServer.TCPServer((HOST, PORT), TCPHandler)
+    server.serve_forever()
