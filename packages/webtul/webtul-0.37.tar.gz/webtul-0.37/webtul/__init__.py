495874e243b949a61566459544c3bb1385f18946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package webtul/0.37/webtul-0.37.tar.gz

---
+++ packages/webtul/webtul-0.37.tar.gz/webtul-0.37/webtul/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" Webtul is short for WEB toolkit
+See modules of this package for more details.
+"""
+__author__ = 'Zagfai'
+__license__ = 'MIT@2014-01'
+
+import log
+import db
+import utils
+
+__all__ = ['log', 'db', 'utils', 'task', 'struct']
+__version__ = '0.37'
+
+#from os.path import dirname, abspath, join
+#folder = lambda x='': join(dirname(abspath(__file__)), x)
+
