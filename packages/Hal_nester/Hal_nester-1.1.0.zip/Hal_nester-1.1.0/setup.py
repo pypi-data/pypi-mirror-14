cfdc44b3d17fa6e72fe71839bb928ee309b8b08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:51 +0000

    Add package Hal_nester/1.1.0/Hal_nester-1.1.0.zip

---
+++ packages/Hal_nester/Hal_nester-1.1.0.zip/Hal_nester-1.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+		name           ='Hal_nester',
+		version        ='1.1.0',
+		py_modules     =['nester'],
+		author         ='kidd',
+		author_email   ='351084172@qq.com',
+		url            ='www.baidu.com',
+		description    ='Hello, world!',
+	)
