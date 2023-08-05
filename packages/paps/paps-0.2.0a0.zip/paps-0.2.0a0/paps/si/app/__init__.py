7601433a077e4b61b63dcbd4ef34f393cd4bb419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package paps/0.2.0a0/paps-0.2.0a0.zip

---
+++ packages/paps/paps-0.2.0a0.zip/paps-0.2.0a0/paps/si/app/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__ = "d01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2015-16, Florian JUNG"
+__license__ = "All rights reserved"
+__version__ = "0.1.0"
+__date__ = "2016-03-26"
+# Created: 2015-03-21 06:51
+"""
+Implementation of sensor interface with the audience participation protocol
+"""
+
+import logging
+
+
+logger = logging.getLogger(__name__)
