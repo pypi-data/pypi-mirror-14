2a9b3c06c1440c88a9cb17205261e4bb4963b312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package stacker-lang/0.2.2/stacker_lang-0.2.2-py2.py3-none-any.whl

---
+++ packages/stacker-lang/stacker_lang-0.2.2-py2.py3-none-any.whl/stacker/errors.py
@@ -0,0 +1,15 @@
+class StackerSyntaxError (Exception):
+    pass
+
+class StackerUndefinedVariable (Exception):
+    pass
+
+class StackerArgumentError (Exception):
+    pass
+
+class StackerTypeError (Exception):
+    pass
+
+class StackEatenUp (Exception):
+    pass
+
