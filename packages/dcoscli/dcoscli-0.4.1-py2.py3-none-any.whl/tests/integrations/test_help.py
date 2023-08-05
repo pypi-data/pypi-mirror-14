3acee95413e901dd64c32547a94afa505d5be439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:08 +0000

    Add package dcoscli/0.4.1/dcoscli-0.4.1-py2.py3-none-any.whl

---
+++ packages/dcoscli/dcoscli-0.4.1-py2.py3-none-any.whl/tests/integrations/test_help.py
@@ -0,0 +1,77 @@
+from .common import assert_command
+
+
+def test_help():
+    with open('tests/data/help/help.txt') as content:
+        assert_command(['dcos', 'help', '--help'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_info():
+    assert_command(['dcos', 'help', '--info'],
+                   stdout=b'Display command line usage information\n')
+
+
+def test_version():
+    assert_command(['dcos', 'help', '--version'],
+                   stdout=b'dcos-help version SNAPSHOT\n')
+
+
+def test_list():
+    stdout = """\
+Command line utility for the Mesosphere Datacenter Operating
+System (DCOS). The Mesosphere DCOS is a distributed operating
+system built around Apache Mesos. This utility provides tools
+for easy management of a DCOS installation.
+
+Available DCOS commands:
+
+\tconfig         \tGet and set DCOS CLI configuration properties
+\thelp           \tDisplay command line usage information
+\tmarathon       \tDeploy and manage applications on the DCOS
+\tnode           \tManage DCOS nodes
+\tpackage        \tInstall and manage DCOS packages
+\tservice        \tManage DCOS services
+\ttask           \tManage DCOS tasks
+
+Get detailed command description with 'dcos <command> --help'.
+""".encode('utf-8')
+
+    assert_command(['dcos', 'help'],
+                   stdout=stdout)
+
+
+def test_help_config():
+    with open('tests/data/help/config.txt') as content:
+        assert_command(['dcos', 'help', 'config'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_help_marathon():
+    with open('tests/data/help/marathon.txt') as content:
+        assert_command(['dcos', 'help', 'marathon'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_help_node():
+    with open('tests/data/help/node.txt') as content:
+        assert_command(['dcos', 'help', 'node'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_help_package():
+    with open('tests/data/help/package.txt') as content:
+        assert_command(['dcos', 'help', 'package'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_help_service():
+    with open('tests/data/help/service.txt') as content:
+        assert_command(['dcos', 'help', 'service'],
+                       stdout=content.read().encode('utf-8'))
+
+
+def test_help_task():
+    with open('tests/data/help/task.txt') as content:
+        assert_command(['dcos', 'help', 'task'],
+                       stdout=content.read().encode('utf-8'))
