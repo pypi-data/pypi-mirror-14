04fcf3406920e033e830062b667196cf173da1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package 2969nester/1.0.0/2969nester-1.0.0.zip

---
+++ packages/2969nester/2969nester-1.0.0.zip/2969nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='2969nester',
+    version     ='1.0.0',
+    py_modules  =['nester'],
+    author      ='gui',
+    author_email='296925885@qq.com',
+    url         ='none',
+    description ='a simple printer of nested list',
+    )
