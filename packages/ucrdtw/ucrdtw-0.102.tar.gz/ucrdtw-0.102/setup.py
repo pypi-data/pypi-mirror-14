b7c18949f69e67a44851d98a57ee8fb70a1a55f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:29 +0000

    Add package ucrdtw/0.102/ucrdtw-0.102.tar.gz

---
+++ packages/ucrdtw/ucrdtw-0.102.tar.gz/ucrdtw-0.102/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
+
+c_ext = Extension('_ucrdtw', ['src/_ucrdtw.c', 'src/ucrdtw.c','src/ucrdtw.h'])
+
+setup(
+    name='ucrdtw',
+    version='0.102',
+    url='https://pypi.python.org/pypi/pip',
+    maintainer='m3at',
+    maintainer_email='paul@alpacadb.com',
+    description='Fast Dynamic Time Warping, O(n log n) ',
+    long_description='Based on the following paper: http://www.cs.ucr.edu/~eamonn/SIGKDD_trillion.pdf',
+    license='MIT',
+    keywords='Dynamic Time Wraping',
+    install_requires=['numpy'],
+    ext_modules=[c_ext],
+    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+)
