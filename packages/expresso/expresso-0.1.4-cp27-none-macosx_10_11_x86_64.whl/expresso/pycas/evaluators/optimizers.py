c812dcc4c472519049d215b763b00a0eec7455af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package expresso/0.1.4/expresso-0.1.4-cp27-none-macosx_10_11_x86_64.whl

---
+++ packages/expresso/expresso-0.1.4-cp27-none-macosx_10_11_x86_64.whl/expresso/pycas/evaluators/optimizers.py
@@ -0,0 +1,42 @@
+
+
+
+import expresso.pycas as pc
+import rule_symbols as s
+from .logic_evaluator import is_numeric,is_atomic
+
+from .canonical_form import format_evaluator
+from .logic_evaluator import evaluator as logic_evaluator
+
+from .logic_evaluator import is_mpmath
+
+compile_evaluator = pc.RewriteEvaluator(recursive=False,split_binary=True)
+
+fold_accuracy = 20
+
+def fold(m):
+    try:
+        m[s.y] = pc.expresso.create_object(m[s.x].N(fold_accuracy,folding=True))
+    except Exception as e:
+        return False
+
+compile_evaluator.add_rule(s.x,s.y,fold)
+compile_evaluator.add_rule(s.x**2,s.x*s.x,condition=is_atomic(s.x))
+
+compile_evaluator.add_rule(s.x*abs(s.y),abs(s.x*s.y),condition=is_mpmath(s.x))
+compile_evaluator.add_rule(s.x*(s.y+s.z),(s.x*s.y+s.x*s.z),condition=is_mpmath(s.x))
+
+compiler_opt_evaluator = pc.MultiEvaluator(recursive = True, split_binary=True)
+compiler_opt_evaluator.add_evaluator(compile_evaluator)
+compiler_opt_evaluator.add_evaluator(logic_evaluator)
+
+def optimize_for_compilation(expr,cache = None):
+    opt = None
+    N = 10
+    # TODO: This should make no difference
+    while opt != expr and N>0:
+        N -= 1
+        opt = expr
+        expr = compiler_opt_evaluator(expr, cache = cache)
+    return format_evaluator(expr, cache = cache)
+
