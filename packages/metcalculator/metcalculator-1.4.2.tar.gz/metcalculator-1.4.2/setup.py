e8e9cc7f851401b514af7cdd0308dd7718e43589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package metcalculator/1.4.2/metcalculator-1.4.2.tar.gz

---
+++ packages/metcalculator/metcalculator-1.4.2.tar.gz/metcalculator-1.4.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='metcalculator',
+    version='1.4.2',
+    description='Calculate various meteorological parameters',
+    url='www.metpod.co.uk',
+    license='MIT',
+    author='Mark Baker',
+    author_email='mark2182@mac.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    entry_points={
+          'gui_scripts': [
+               'metcalculator = metcalculator.__main__:main'
+          ]
+    },
+)
