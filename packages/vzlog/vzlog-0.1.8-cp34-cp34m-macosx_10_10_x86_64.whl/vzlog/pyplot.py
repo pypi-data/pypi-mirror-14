04d87e65ec4091c6cf2e8a2956f04596f34ee0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package vzlog/0.1.8/vzlog-0.1.8-cp34-cp34m-macosx_10_10_x86_64.whl

---
+++ packages/vzlog/vzlog-0.1.8-cp34-cp34m-macosx_10_10_x86_64.whl/vzlog/pyplot.py
@@ -0,0 +1,9 @@
+from __future__ import division, print_function, absolute_import
+
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rc('font', size=8)
+matplotlib.rc('savefig', bbox='tight')
+
+from matplotlib.pyplot import *
+style.use('bmh')
