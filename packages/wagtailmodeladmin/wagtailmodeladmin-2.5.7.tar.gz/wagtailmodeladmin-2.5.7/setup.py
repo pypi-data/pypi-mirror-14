fd42023d654b1948be40200d18d8dcaa90e9d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:41 +0000

    Add package wagtailmodeladmin/2.5.7/wagtailmodeladmin-2.5.7.tar.gz

---
+++ packages/wagtailmodeladmin/wagtailmodeladmin-2.5.7.tar.gz/wagtailmodeladmin-2.5.7/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+from wagtailmodeladmin import __version__
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="wagtailmodeladmin",
+    version=__version__,
+    author="Andy Babic",
+    author_email="ababic@rkh.co.uk",
+    description="Customisable 'django-admin' style listing pages for Wagtail",
+    long_description=README,
+    packages=find_packages(),
+    license="MIT",
+    keywords="wagtail cms model utility",
+    download_url="https://github.com/rkhleics/wagtailmodeladmin/tarball/0.1",
+    url="https://github.com/rkhleics/wagtailmodeladmin",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Topic :: Internet :: WWW/HTTP',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    install_requires=[
+        "wagtail>=0.8.7",
+    ],
+)
