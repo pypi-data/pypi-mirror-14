e0e3c0424a9a0be732df075ec486db3c5052655f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package git-bin/0.3.0/git-bin-0.3.0.tar.gz

---
+++ packages/git-bin/git-bin-0.3.0.tar.gz/git-bin-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='git-bin',
+    version='0.3.0',
+    description='git extension to support binary files',
+    author='srubenst',
+    author_email='srubenst@cisco.com',
+    license='MIT',
+    keywords='binary git',
+    url='https://github.com/cisco-sas/git-bin',
+    download_url='https://github.com/cisco-sas/git-bin',
+    packages=["gitbin"],
+    install_requires=['sh', 'docopt'],
+    entry_points={
+        'console_scripts': [
+            'git-bin = gitbin.gitbin:main'
+        ]
+    },
+)
