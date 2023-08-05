a1943ce5a0b52a892c62d8bd0fe062f53e3f5605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:25 +0000

    Add package Myfirstload/1.1.0/myfirstload-1.3.0.tar.gz

---
+++ packages/Myfirstload/myfirstload-1.3.0.tar.gz/myfirstload-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name = 'myfirstload',
+	version = '1.3.0',
+	py_modules = ['myfirstload'],
+	author = 'Luiz Henrique Alves',
+	author_email = 'edge@jogargame.com.br',
+	url = 'http://www.jogargame.com.br',
+	description = 'A simple module printer of lists, with TAB, creating by guy brasilian',
+)
