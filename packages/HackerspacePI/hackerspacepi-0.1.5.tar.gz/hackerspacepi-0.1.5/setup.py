b30c57d83b6876f1f4b74a3d7ecc3ecbee1cbbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:18 +0000

    Add package HackerspacePI/0.1.5/hackerspacepi-0.1.5.tar.gz

---
+++ packages/HackerspacePI/hackerspacepi-0.1.5.tar.gz/hackerspacepi-0.1.5/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='hackerspacepi',
+    version='0.1.5',
+    description="SpaceAPI compliant service written in python3.5",
+    long_description="SpaceAPI compliant service written in python3.5",
+    author="David Francos Cuartero",
+    author_email='me@davidfrancos.net',
+    url='https://github.com/XayOn/HackerspacePI',
+    packages=[
+        'hackerspacepi',
+    ],
+    package_dir={'hackerspacepi':
+                 'hackerspacepi'},
+    entry_points={
+        'console_scripts':
+        ['hackerspacepi=hackerspacepi:server',
+        'hackerspacepi_client=hackerspacepi:client']
+    },
+
+    include_package_data=True,
+    install_requires=["aiohttp"],
+    license="BSD",
+    zip_safe=False,
+    keywords='hackerspacepi',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
