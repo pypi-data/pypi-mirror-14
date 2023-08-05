059af9991d7d26c7b8a03de41e35f71123a3de59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:57 +0000

    Add package dlcli/0.0.12/dlcli-0.0.12.tar.gz

---
+++ packages/dlcli/dlcli-0.0.12.tar.gz/dlcli-0.0.12/dlcli/api/links.py
@@ -0,0 +1,25 @@
+import logging
+import requests
+import utils
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class Links(object):
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.headers = {"Token": ctx.parent.parent.params['key']}
+
+    def get_links(self):
+        return requests.get(utils.build_api_url(self.ctx, 'links'), headers=self.headers).json()
+
+    def delete_link(self, link):
+        return requests.delete(utils.build_api_url(self.ctx, 'links') + '/' + link, headers=self.headers)
+
+    def export_link(self, link):
+        return requests.get(utils.build_api_url(self.ctx, 'links') + '/' + link, headers=self.headers).json()
+
+    def import_link(self, link_path):
+        link_json = json.loads(utils.read_file_content(link_path))
+        return requests.post(utils.build_api_url(self.ctx, 'links'), headers=self.headers, data=link_json)
