354730ee3902440651f2b7acdb61b4fa79e831e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package list_sweep/1.4.0/list_sweep-1.4.0.tar.gz

---
+++ packages/list_sweep/list_sweep-1.4.0.tar.gz/list_sweep-1.4.0/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+
+	name		= 'list_sweep',
+	version		= '1.4.0',
+	py_modules	= ['list_sweep'],
+	author		= 'Ronny C',
+	author_email	= 'n.s.gray@live.co.uk',
+	url		= 'none',	
+	description	= 'A simple function to print lists which may contain inner lists. Update now provides a secondary, optional arguement to print strings with a tab spacing relative to their list/inner list position. This secondary arguement must be activated by True/False. Default is False. Default is no indentation. Additional arguement added to enable list_sweep to write data to a file or by default display to screen with Standard Output. '
+)
