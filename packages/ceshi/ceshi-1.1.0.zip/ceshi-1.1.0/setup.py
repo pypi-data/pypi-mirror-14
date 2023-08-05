da5739f02bf2de5c6d38316c955c3a3d512493a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package ceshi/1.1.0/ceshi-1.1.0.zip

---
+++ packages/ceshi/ceshi-1.1.0.zip/ceshi-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='ceshi',
+	version='1.1.0',
+	py_modules=['ceshi'],
+	author='liyl',
+	author_email='liyl@amazon.com',
+	url='http://www.headfirstlabs.com',
+	descripyion='A simple printer of nested lists',
+	)
