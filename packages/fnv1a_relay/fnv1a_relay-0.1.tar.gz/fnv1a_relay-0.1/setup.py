86ece193cd29f55075bf768d9f072c2422c479cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package fnv1a_relay/0.1/fnv1a_relay-0.1.tar.gz

---
+++ packages/fnv1a_relay/fnv1a_relay-0.1.tar.gz/fnv1a_relay-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+description = '''
+    the fnv1a_32 function using in carbon-c-relay
+'''
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='fnv1a_relay',
+    version='0.1',
+    description=description,
+    long_description=readme(),
+    url='https://github.com/ZhangYet/fnv1a_relay',
+    author='dantezy',
+    author_email='zhengyet@gmail.com',
+    keywords='fnv1a carbon carbon-c-relay',
+    license='MIT',
+    package=['fnv1a_relay'],
+    install_requires=[
+        'cffi',
+    ],
+    zip_safe=False
+)
