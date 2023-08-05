69348774a334ef56fcdfc46c4cb406fb377b9da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:46 +0000

    Add package phantooom-qingcloud-cli/0.1/phantooom-qingcloud-cli-0.1.zip

---
+++ packages/phantooom-qingcloud-cli/phantooom-qingcloud-cli-0.1.zip/phantooom-qingcloud-cli-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import driver
+
+setup(
+  name = 'phantooom-qingcloud-cli',
+  version = 0.1,
+  packages = find_packages(),
+  install_requires = ['requests','PyYAML'],
+  entry_points = {
+    'console_scripts': [
+      'homework-qingcluod-cli = driver:main']
+  },
+
+  author = 'phantooom',
+  author_email = 'zouruixp@sina.com',
+  description = 'homework-qingcluod-cli',
+  license = 'MIT',
+  keywords = 'homework',
+  url = 'http://fullstack.love',
+  scripts=['bin/qingcloud'],
+)
