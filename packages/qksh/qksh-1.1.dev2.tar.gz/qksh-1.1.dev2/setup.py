6e961858d9d3f7a79f91bbe3caa6d60072c0da3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:47 +0000

    Add package qksh/1.1.dev2/qksh-1.1.dev2.tar.gz

---
+++ packages/qksh/qksh-1.1.dev2.tar.gz/qksh-1.1.dev2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# example: https://github.com/pypa/sampleproject
+from setuptools import setup
+
+setup(
+    name='qksh',
+    version='1.1.dev2',
+    description='A CLI utility working with F5 iHealth GUI'
+                'for uploading and downloading files/commands etc.',
+    url='https://github.com/ccorzlol/handy_qkview',
+    license='MIT',
+    author='Qiang He',
+    author_email='abc89d@gmail.com',
+    packages=['code'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Information Technology',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    install_requires=[
+        'args>=0.1.0',
+        'clint>=0.5.1',
+        'logging>=0.4.9.6',
+        'lockfile>=0.12.2',
+        'requests>=2.9.1',
+        'requests-toolbelt>=0.6.0',
+        'wheel>=0.29.0',
+        'python-daemon>=2.1.1'
+    ],
+
+    keywords='qkview ihealth',
+)
