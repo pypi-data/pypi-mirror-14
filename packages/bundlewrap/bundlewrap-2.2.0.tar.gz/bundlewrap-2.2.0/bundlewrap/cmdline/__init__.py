9276a63c19e65a2bed6c609451404e618d2e9b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package bundlewrap/2.2.0/bundlewrap-2.2.0.tar.gz

---
+++ packages/bundlewrap/bundlewrap-2.2.0.tar.gz/bundlewrap-2.2.0/bundlewrap/cmdline/__init__.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from os import environ, getcwd
+from sys import argv, exit
+
+from ..exceptions import NoSuchRepository
+from ..repo import Repository
+from ..utils.text import force_text, mark_for_translation as _, red
+from ..utils.ui import io
+from .parser import build_parser_bw
+
+
+def main(*args, **kwargs):
+    """
+    Entry point for the 'bw' command line utility.
+
+    The args and path parameters are used for integration tests.
+    """
+    if not args:
+        args = argv[1:]
+    path = kwargs.get('path', getcwd())
+
+    text_args = [force_text(arg) for arg in args]
+
+    parser_bw = build_parser_bw()
+    pargs = parser_bw.parse_args(args)
+    if not hasattr(pargs, 'func'):
+        parser_bw.print_help()
+        exit(2)
+
+    io.activate_as_parent(debug=pargs.debug)
+
+    environ.setdefault('BWADDHOSTKEYS', "1" if pargs.add_ssh_host_keys else "0")
+
+    if len(text_args) >= 1 and (
+        text_args[0] == "--version" or
+        (len(text_args) >= 2 and text_args[0] == "repo" and text_args[1] == "create") or
+        text_args[0] == "zen" or
+        "-h" in text_args or
+        "--help" in text_args
+    ):
+        # 'bw repo create' is a special case that only takes a path
+        repo = path
+    else:
+        try:
+            repo = Repository(path)
+        except NoSuchRepository:
+            io.stderr(_(
+                "{x} The current working directory "
+                "is not a BundleWrap repository."
+            ).format(x=red("!")))
+            exit(1)
+            return  # used during texting when exit() is mocked
+
+    # convert all string args into text
+    text_pargs = {key: force_text(value) for key, value in vars(pargs).items()}
+
+    output = pargs.func(repo, text_pargs)
+    if output is None:
+        output = ()
+
+    return_code = 0
+
+    for line in output:
+        if isinstance(line, int):
+            return_code = line
+            break
+        else:
+            io.stdout(line)
+
+    if return_code != 0:  # not raising SystemExit every time to ease testing
+        exit(return_code)
---
+++ packages/bundlewrap/bundlewrap-2.2.0.tar.gz/bundlewrap-2.2.0/bundlewrap/utils/ui.py
@@ -0,0 +1,182 @@
+from contextlib import contextmanager
+from datetime import datetime
+import fcntl
+from functools import wraps
+from multiprocessing import Lock, Manager
+import os
+from signal import signal, SIGPIPE, SIG_DFL
+import struct
+import sys
+import termios
+
+from . import STDERR_WRITER, STDOUT_WRITER
+from .text import ANSI_ESCAPE, inverse, mark_for_translation as _
+
+TTY = STDOUT_WRITER.isatty()
+
+
+try:
+    input_function = raw_input
+except NameError:  # Python 3
+    input_function = input
+
+# prevent BrokenPipeError when piping into `head`
+# http://stackoverflow.com/questions/14207708/ioerror-errno-32-broken-pipe-python
+signal(SIGPIPE, SIG_DFL)
+
+
+def add_debug_timestamp(f):
+    @wraps(f)
+    def wrapped(self, msg, **kwargs):
+        if self.debug_mode:
+            msg = datetime.now().strftime("[%Y-%m-%d %H:%M:%S.%f] ") + msg
+        return f(self, msg, **kwargs)
+    return wrapped
+
+
+def clear_formatting(f):
+    """
+    Makes sure formatting from cut-off lines can't bleed into next one
+    """
+    @wraps(f)
+    def wrapped(self, msg, **kwargs):
+        if TTY and os.environ.get("BWCOLORS", "1") != "0":
+            msg = "\033[0m" + msg
+        return f(self, msg, **kwargs)
+    return wrapped
+
+
+def term_width():
+    if not TTY:
+        return 0
+
+    fd = sys.stdout.fileno()
+    _, width = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, 'aaaa'))
+    return width
+
+
+def write_to_stream(stream, msg):
+    if TTY:
+        stream.write(msg)
+    else:
+        stream.write(ANSI_ESCAPE.sub("", msg))
+    stream.flush()
+
+
+class IOManager(object):
+    def __init__(self):
+        self.capture_mode = False
+        self.child_mode = False
+        self.parent_mode = False
+
+    def activate_as_child(self, lock, jobs, debug_mode, stdin):
+        self.parent_mode = False
+        self.child_mode = True
+        self.debug_mode = debug_mode
+        self.lock = lock
+        self.jobs = jobs
+        sys.stdin = stdin
+
+    def activate_as_parent(self, debug=False):
+        assert not self.child_mode
+        self.debug_mode = debug
+        self.jobs = Manager().list()
+        self.lock = Lock()
+        self.parent_mode = True
+
+    def ask(self, question, default, epilogue=None, get_input=input_function):
+        answers = _("[Y/n]") if default else _("[y/N]")
+        question = question + " " + answers + " "
+        with self.lock:
+            self._clear_last_job()
+            while True:
+                write_to_stream(STDOUT_WRITER, "\a" + question)
+
+                answer = get_input()
+                if answer.lower() in (_("y"), _("yes")) or (
+                    not answer and default
+                ):
+                    answer = True
+                    break
+                elif answer.lower() in (_("n"), _("no")) or (
+                    not answer and not default
+                ):
+                    answer = False
+                    break
+                write_to_stream(STDOUT_WRITER, _("Please answer with 'y(es)' or 'n(o)'.\n"))
+            if epilogue:
+                write_to_stream(STDOUT_WRITER, epilogue + "\n")
+            self._write_current_job()
+        return answer
+
+    @property
+    def child_parameters(self):
+        try:
+            new_stdin = os.fdopen(os.dup(sys.stdin.fileno()))
+        except ValueError:  # with pytest: redirected Stdin is pseudofile, has no fileno()
+            new_stdin = sys.stdin
+        return (
+            self.lock,
+            self.jobs,
+            self.debug_mode,
+            new_stdin,
+        )
+
+    @clear_formatting
+    @add_debug_timestamp
+    def debug(self, msg, append_newline=True):
+        if self.debug_mode:
+            with self.lock:
+                self._write(msg, append_newline=append_newline)
+
+    def job_add(self, msg):
+        with self.lock:
+            if TTY:
+                self._clear_last_job()
+                write_to_stream(STDOUT_WRITER, inverse("{} ".format(msg)[:term_width() - 1]))
+            self.jobs.append(msg)
+
+    def job_del(self, msg):
+        with self.lock:
+            self._clear_last_job()
+            self.jobs.remove(msg)
+            self._write_current_job()
+
+    @clear_formatting
+    @add_debug_timestamp
+    def stderr(self, msg, append_newline=True):
+        with self.lock:
+            self._write(msg, append_newline=append_newline, err=True)
+
+    @clear_formatting
+    @add_debug_timestamp
+    def stdout(self, msg, append_newline=True):
+        with self.lock:
+            self._write(msg, append_newline=append_newline)
+
+    @contextmanager
+    def job(self, job_text):
+        self.job_add(job_text)
+        try:
+            yield
+        finally:
+            self.job_del(job_text)
+
+    def _clear_last_job(self):
+        if self.jobs and TTY:
+            write_to_stream(STDOUT_WRITER, "\r\033[K")
+
+    def _write(self, msg, append_newline=True, err=False):
+        if self.jobs and TTY:
+            write_to_stream(STDOUT_WRITER, "\r\033[K")
+        if msg is not None:
+            if append_newline:
+                msg += "\n"
+            write_to_stream(STDERR_WRITER if err else STDOUT_WRITER, msg)
+        self._write_current_job()
+
+    def _write_current_job(self):
+        if self.jobs and TTY:
+            write_to_stream(STDOUT_WRITER, inverse("{} ".format(self.jobs[-1])[:term_width() - 1]))
+
+io = IOManager()
