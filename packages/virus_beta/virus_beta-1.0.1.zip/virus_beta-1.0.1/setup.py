9121b901c2bd4742c2506438891838b45df09df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package virus_beta/1.0.1/virus_beta-1.0.1.zip

---
+++ packages/virus_beta/virus_beta-1.0.1.zip/virus_beta-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name           = "virus_beta"  ,
+      version        = '1.0.1',
+      py_modules     = ['virus_beta'],
+      author         = "Arjun Narale",
+      url            = "narale.com",
+      author_email   = "arjunnarale183@gmail.com",
+      description    = "Sample virus function to print nested list",
+      )
