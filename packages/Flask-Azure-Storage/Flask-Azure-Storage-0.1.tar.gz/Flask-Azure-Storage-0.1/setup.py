78e7dab574bb801ee727aaadcb6209ac3f39f43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:47 +0000

    Add package Flask-Azure-Storage/0.1/Flask-Azure-Storage-0.1.tar.gz

---
+++ packages/Flask-Azure-Storage/Flask-Azure-Storage-0.1.tar.gz/Flask-Azure-Storage-0.1/setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-Azure-Storage
+-------------
+Flask extension that provides integration with Azure Storage.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Azure-Storage',
+    version='0.1',
+    url='https://github.com/alejoar/Flask-Azure-Storage',
+    license='MIT',
+    author='Alejo Arias',
+    author_email='alejoar@gmail.com',
+    description='Flask extension that provides integration with Azure Storage',
+    long_description=__doc__,
+    py_modules=['flask_azure_storage'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'azure-storage'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
