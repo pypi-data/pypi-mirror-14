a3ce02380fae14aa03a00c24cf78f619acfb4d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package sgf/0.1/sgf-0.1.tar.gz

---
+++ packages/sgf/sgf-0.1.tar.gz/sgf-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="sgf",
+    version="0.1",
+    description="Python library for reading and writing Smart Game Format",
+    license="MIT",
+    url="http://github.com/jtauber/sgf",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    py_modules=["sgf"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Utilities",
+    ],
+)
