7cecd7c28a6f17c5eb77a036ffe24d06b18f04e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package hotdog/1.3.15/hotdog-1.3.15.tar.gz

---
+++ packages/hotdog/hotdog-1.3.15.tar.gz/hotdog-1.3.15/hotdog/BaseDriver.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from hotdog.BaseElement import BaseElement
+
+
+class BaseWebDriver(WebDriver):
+
+    def find_element(self, by=By.ID, value=None, type=None):
+        element =  super().find_element(by, value)
+        if type:
+            element.__class__ = type
+        else:
+            element.__class__ = BaseElement
+
+        if hasattr(self, 'debug'):
+            element.debug = self.debug
+        return element
+
+    def find_elements(self, by=By.ID, value=None, type=None):
+        elements =  super().find_elements(by, value)
+        if type:
+             klass = type
+        else:
+            klass = BaseElement
+
+        for element in elements:
+            element.__class__ = klass
+
+            if hasattr(self, 'debug'):
+                element.debug = self.debug
+        return elements
+
+
+def get_driver():
+    global _driver_instance
+    if not _driver_instance:
+        _driver_instance = BaseWebDriver
+
+    return _driver_instance
