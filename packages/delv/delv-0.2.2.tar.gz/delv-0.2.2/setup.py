2d1b66c0a26e9db0ade9d0ebc36382dca05fe654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:39 +0000

    Add package delv/0.2.2/delv-0.2.2.tar.gz

---
+++ packages/delv/delv-0.2.2.tar.gz/delv-0.2.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Copyright 2015-2016 Bryce Schroeder, www.bryce.pw, bryce.schroeder@gmail.com
+# Wiki: http://www.ferazelhosting.net/wiki/delv
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# "Cythera" and "Delver" are trademarks of either Glenn Andreas or 
+# Ambrosia Software, Inc. 
+
+from distutils.core import setup
+import setuptools
+
+setup(name='delv',
+      version='0.2.2',
+      description='Module for making tools interoperable with Delver',
+      author='Bryce Schroeder',
+      author_email='bryce.schroeder@gmail.com',
+      license='GPL-3',
+      install_requires=['parsley'],
+      url='http://www.ferazelhosting.net/wiki/delv',
+	classifiers=["Development Status :: 3 - Alpha",
+                     "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                     "Topic :: Games/Entertainment :: Role-Playing"],
+      packages=['delv'],
+      package_data={'delv':['rdasm_include/*.rdasm', 'rdasm_include/Delver/*.rdasm']})
