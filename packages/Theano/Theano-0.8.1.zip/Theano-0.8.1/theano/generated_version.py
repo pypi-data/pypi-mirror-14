6f72cd28611cabb228bde2f5d01c0f21e2e77e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package Theano/0.8.1/Theano-0.8.1.zip

---
+++ packages/Theano/Theano-0.8.1.zip/Theano-0.8.1/theano/generated_version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.8.1'
+version = '0.8.1'
+git_revision = 'feeb079222e9e47e55ce0d35beb195f2344f4075'
+full_version = '0.8.1.dev-%(git_revision)s' % {
+    'git_revision': git_revision}
+release = True
+
+if not release:
+    version = full_version
