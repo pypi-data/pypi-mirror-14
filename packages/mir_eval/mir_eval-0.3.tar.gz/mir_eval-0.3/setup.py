6499ec6a37caa2e3bf3dcba6908bfdb5c5b1de4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package mir_eval/0.3/mir_eval-0.3.tar.gz

---
+++ packages/mir_eval/mir_eval-0.3.tar.gz/mir_eval-0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='mir_eval',
+    version='0.3',
+    description='Common metrics for common audio/music processing tasks.',
+    author='Colin Raffel',
+    author_email='craffel@gmail.com',
+    url='https://github.com/craffel/mir_eval',
+    packages=['mir_eval'],
+    long_description=long_description,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='audio music mir dsp',
+    license='MIT',
+    install_requires=[
+        'numpy >= 1.7.0',
+        'scipy >= 0.9.0',
+        'future',
+        'six'
+    ],
+)
