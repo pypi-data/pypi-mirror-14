91789eed8e9b7bfcacebd09c920586786626bd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package pygeoprocessing/0.3.0a14/pygeoprocessing-0.3.0a14.zip

---
+++ packages/pygeoprocessing/pygeoprocessing-0.3.0a14.zip/pygeoprocessing-0.3.0a14/pygeoprocessing/__init__.py
@@ -0,0 +1,22 @@
+"""__init__ module for pygeprocessing, imports all the geoprocessing functions
+    into the pygeoprocessing namespace"""
+
+import natcap.versioner
+__version__ = natcap.versioner.get_version('pygeoprocessing')
+
+import logging
+import types
+
+from geoprocessing import *
+
+# Expose test()
+from .tests import test
+
+__all__ = []
+for attrname in dir(geoprocessing):
+    if type(getattr(geoprocessing, attrname)) is types.FunctionType:
+        __all__.append(attrname)
+
+LOGGER = logging.getLogger('pygeoprocessing')
+LOGGER.setLevel(logging.DEBUG)
+
