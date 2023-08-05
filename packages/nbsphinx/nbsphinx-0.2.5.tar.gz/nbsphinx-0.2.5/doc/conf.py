ce911f8496a548f7844a6ba2147c1292c7c0c484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:19 +0000

    Add package nbsphinx/0.2.5/nbsphinx-0.2.5.tar.gz

---
+++ packages/nbsphinx/nbsphinx-0.2.5.tar.gz/nbsphinx-0.2.5/doc/conf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Use sphinx-quickstart to create your own conf.py file!
+# After that, you have to edit a few things.  See below.
+
+# Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# If True, the build process is continued even if an exception occurs:
+#nbsphinx_allow_errors = True
+
+# Default language for syntax highlighting (e.g. in Markdown cells)
+highlight_language = 'none'
+
+# -- The settings below this line are not specific to nbsphinx ------------
+
+master_doc = 'index'
+
+project = 'nbsphinx'
+author = 'Matthias Geier'
+copyright = '2016, ' + author
+
+# -- Get version information from Git -------------------------------------
+
+try:
+    from subprocess import check_output
+    release = check_output(['git', 'describe', '--tags', '--always'])
+    release = release.decode().strip()
+except Exception:
+    release = '<unknown>'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_title = project + ' version ' + release
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'preamble': r'\setcounter{tocdepth}{3}',
+}
+
+latex_documents = [
+    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
+]
+
+latex_show_urls = 'footnote'
