5ef823f71dfa85ca18a6725c52fc13b1c2473fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:50 +0000

    Add package openregister/0.7.0/openregister-0.7.0.tar.gz

---
+++ packages/openregister/openregister-0.7.0.tar.gz/openregister-0.7.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# see http://bugs.python.org/issue8876
+# this is just a quick hack so we can test build in vagrant
+import os
+if os.environ.get('USER','') == 'vagrant':
+  del os.link
+
+from setuptools import setup, find_packages
+
+setup(name='openregister',
+      version='0.7.0',
+      description='Openregister core package',
+      long_description='Python libraries for wrangling register data',
+      author='Openregister.org',
+      author_email='paul.downey@whatfettle.com',
+      url='https://github.com/openregister/openregister-python',
+      download_url='https://github.com/openregister/openregister-python',
+      packages=find_packages(exclude=['tests']),
+      zip_safe=False,
+      include_package_data=True,
+      license='MIT',
+      platforms='any',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.4',
+        ],
+      install_requires=[
+        'PyYAML>=3.11',
+        'pymongo>=2.7.2'
+      ]
+)
