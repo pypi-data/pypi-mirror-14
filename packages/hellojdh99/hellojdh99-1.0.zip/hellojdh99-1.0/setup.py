4776fba3cabf01730aae468ca2bcda42cd9d45d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package hellojdh99/1.0/hellojdh99-1.0.zip

---
+++ packages/hellojdh99/hellojdh99-1.0.zip/hellojdh99-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            =   'hellojdh99',
+        version         =   '1.0',
+        py_modules      =   ['hellojdh99'],
+        author          =   'jdh',
+        author_email    =   'jdh821@163.com',
+        url             =   'www.bazhangkeji.com',
+        description     =   'test module',
+    )
