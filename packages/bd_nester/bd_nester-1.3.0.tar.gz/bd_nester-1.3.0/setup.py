01d4baa45f8a9763d346a35fa6e09a4bb84166e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:17 +0000

    Add package bd_nester/1.3.0/bd_nester-1.3.0.tar.gz

---
+++ packages/bd_nester/bd_nester-1.3.0.tar.gz/bd_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = "bd_nester",
+    version         = "1.3.0",
+    py_modules      = ["bd_nester"],
+    author          = "darko bede",
+    author_email    = "darko@bede.hr",
+    url             = "http://www.bede.hr",
+    description     = "A simple printer of nested lists",
+    )
