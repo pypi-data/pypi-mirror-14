078c3d996a16c23a670b1591f780b0b731818b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package pg_monitor/1.5.8/pg_monitor-1.5.8.tar.gz

---
+++ packages/pg_monitor/pg_monitor-1.5.8.tar.gz/pg_monitor-1.5.8/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python 
+
+from setuptools import setup
+
+def readme():
+    with open('README.txt') as f:
+        return f.read()
+
+setup(name='pg_monitor',
+      version='1.5.8',
+      description='PostgreSQL monitoring checks',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking :: Monitoring',
+      ],
+      url='https://github.com/rugging24/pg_monitor',
+      author='Olakunle Olaniyi',
+      author_email='rugging24@gmail.com',
+      license='PostgreSQL License',
+      packages=['pg_monitor'],
+      install_requires=[
+          'psycopg2',
+      ],
+      scripts=['bin/pg_monitor'],
+      include_package_data=True,
+      zip_safe=False)
