c2da1b6ce52645b20b85aed48bc866b2d9c1c053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package vunit-hdl/0.63.0/vunit_hdl-0.63.0.tar.gz

---
+++ packages/vunit-hdl/vunit_hdl-0.63.0.tar.gz/vunit_hdl-0.63.0/vunit/simulator_interface.py
@@ -0,0 +1,100 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2015-2016, Lars Asplund lars.anders.asplund@gmail.com
+
+"""
+Generic simulator interface
+"""
+
+import sys
+import os
+
+
+class SimulatorInterface(object):
+    """
+    Generic simulator interface
+    """
+
+    name = None
+    supports_gui_flag = False
+    package_users_depend_on_bodies = False
+
+    @staticmethod
+    def add_arguments(parser):
+        """
+        Add command line arguments
+        """
+        pass
+
+    @staticmethod
+    def find_executable(executable):
+        """
+        Return a list of all executables found in PATH
+        """
+        path = os.environ['PATH']
+        paths = path.split(os.pathsep)
+        _, ext = os.path.splitext(executable)
+
+        if (sys.platform == 'win32' or os.name == 'os2') and (ext != '.exe'):
+            executable = executable + '.exe'
+
+        result = []
+        if isfile(executable):
+            result.append(executable)
+
+        for prefix in paths:
+            file_name = os.path.join(prefix, executable)
+            if isfile(file_name):
+                # the file exists, we have a shot at spawn working
+                result.append(file_name)
+        return result
+
+    @classmethod
+    def find_toolchain(cls, executables, constraints=None):
+        """
+        Find the first path prefix containing all executables
+        """
+        constraints = [] if constraints is None else constraints
+
+        if len(executables) == 0:
+            return None
+
+        all_paths = [[os.path.abspath(os.path.dirname(executables))
+                      for executables in cls.find_executable(name)]
+                     for name in executables]
+
+        for path0 in all_paths[0]:
+            if all([path0 in paths for paths in all_paths] +
+                   [constraint(path0) for constraint in constraints]):
+                return path0
+
+    def post_process(self, output_path):
+        """
+        Hook for simulator interface to perform post processing such as creating coverage reports
+        """
+        pass
+
+    def compile_project(self, project, vhdl_standard):
+        """
+        Compile the project
+        """
+        pass
+
+    def simulate(self, output_path,  # pylint: disable=too-many-arguments
+                 library_name, entity_name, architecture_name, config):
+        """
+        Simulate
+        """
+        pass
+
+
+def isfile(file_name):
+    """
+    Case insensitive os.path.isfile
+    """
+    if not os.path.isfile(file_name):
+        return False
+
+    return os.path.basename(file_name) in os.listdir(os.path.dirname(file_name))
