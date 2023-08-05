6235229a5e3b63a1a561f2bcc9eec9cb55a9180f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:48 +0000

    Add package proxybroker/0.1.3/proxybroker-0.1.3-py3-none-any.whl

---
+++ packages/proxybroker/proxybroker-0.1.3-py3-none-any.whl/proxybroker/errors.py
@@ -0,0 +1,45 @@
+class ProxyError(Exception):
+    pass
+
+
+class ProxyConnError(ProxyError):
+    pass
+
+
+class ProxyRecvError(ProxyError):
+    pass
+
+
+class ProxyTimeoutError(ProxyError):
+    pass
+
+
+class ProxyEmptyRecvError(ProxyError):
+    pass
+
+
+class ProxyTypeError(ValueError):
+    def __init__(self):
+        self.msg = ('Proxy <types> var can be a string '
+                    '(ex: "HTTP" or "HTTP,HTTPS") or any '
+                    'iterable type (ex: ["HTTP", "HTTPS"])')
+        self.args = (self.msg,)
+
+
+class BadStatusError(Exception):
+    pass
+
+
+
+# class ProxyAnonLvlError(ValueError):
+#     def __init__(self):
+#         self.msg = 'Proxy anonymity levels can be a string '
+#                    '(ex: "Transparent" or "Transparent,Anonymous") '
+#                    'or any iterable type (ex: ["Anonymous", "High"])'
+#         self.args = (self.msg,)
+
+# class ProxyISOCountryError(ValueError):
+#     def __init__(self):
+#         self.msg = 'ISO country can be a string (ex: "US" or "US,GB,DE") '
+#                    'or any iterable type (ex: ["US", "GB", "DE"])'
+#         self.args = (self.msg,)
