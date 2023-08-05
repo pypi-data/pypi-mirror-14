f15370cffed0294b4a8be4da21b45ff21e2169bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:42 +0000

    Add package ranges/0.1/ranges-0.1.tar.gz

---
+++ packages/ranges/ranges-0.1.tar.gz/ranges-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'ranges',
+    version = '0.1',
+    author = 'Marco Buttu',
+    author_email = "marco.buttu@gmail.com",
+    license = 'GPL',
+    url = 'https://pypi.python.org/pypi/ranges/',
+    scripts = ['ranges.py'],
+    platforms = 'all',
+    classifiers = [
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+   ],
+)
