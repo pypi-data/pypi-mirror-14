591486105a2cbe5f416dde629d1d7787acf4b89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package digimat.gapi/0.1.15/digimat.gapi-0.1.15.tar.gz

---
+++ packages/digimat.gapi/digimat.gapi-0.1.15.tar.gz/digimat.gapi-0.1.15/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digimat.gapi',
+    version='0.1.15',
+    description='Digimat Google API tools',
+    namespace_packages=['digimat'],
+    author='Frederic Hess',
+    author_email='fhess@splust.ch',
+    url='http://www.digimat.ch',
+    license='PSF',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'gspread',
+        'requests',
+        'strict_rfc3339',
+        'httplib2',
+        'google-api-python-client',
+        'setuptools'
+    ],
+    dependency_links=[
+        ''
+    ],
+    zip_safe=False)
