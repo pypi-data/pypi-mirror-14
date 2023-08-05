fd91ab9c6fbe15dfcd6a3265956c353663baa988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:58 +0000

    Add package sendgrid/2.0.0/sendgrid-2.0.0.tar.gz

---
+++ packages/sendgrid/sendgrid-2.0.0.tar.gz/sendgrid-2.0.0/sendgrid/client.py
@@ -0,0 +1,40 @@
+import python_http_client
+import json
+import os
+from .version import __version__
+
+class SendGridAPIClient(object):
+    """SendGrid API."""
+    def __init__(self, **opts):
+        """
+        Construct SendGrid v3 API object.
+
+        :params host: Base URL for the API call
+        :type host: string
+        
+        """
+        path =  '{0}/..'.format(os.path.abspath(os.path.dirname(__file__)))
+        python_http_client.Config(path)
+        self._apikey = os.environ.get('SENDGRID_API_KEY')
+        self.useragent = 'sendgrid/{0};python_v3'.format(__version__)
+        self.host = opts.get('host', 'https://api.sendgrid.com')
+        self.path = opts.get('path', os.path.abspath(os.path.dirname(__file__)))
+        self.version = __version__
+
+        headers = {
+                    "Authorization": 'Bearer {0}'.format(self._apikey),
+                    "Content-Type": "application/json",
+                    "User-agent": self.useragent
+                   }
+
+        self.client = python_http_client.Client(host=self.host, 
+                                                request_headers=headers,
+                                                version=3)
+
+    @property
+    def apikey(self):
+        return self._apikey
+
+    @apikey.setter
+    def apikey(self, value):
+        self._apikey = value
