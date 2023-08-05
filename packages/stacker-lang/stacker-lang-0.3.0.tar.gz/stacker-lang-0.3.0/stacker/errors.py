f4ddcd08487f39ac83d379e479d8663039a076a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package stacker-lang/0.3.0/stacker-lang-0.3.0.tar.gz

---
+++ packages/stacker-lang/stacker-lang-0.3.0.tar.gz/stacker-lang-0.3.0/stacker/errors.py
@@ -0,0 +1,18 @@
+class StackerUndefinedVariable (Exception):
+    pass
+
+class StackerArgumentError (Exception):
+    pass
+
+class StackerFileNotFound (Exception):
+    pass
+
+class StackerSyntaxError (Exception):
+    pass
+
+class StackerTypeError (Exception):
+    pass
+
+class StackEatenUp (Exception):
+    pass
+
