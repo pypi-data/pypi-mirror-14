c6ff24b4ebbdfbebed7503790493bc8e9496ffee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:16 +0000

    Add package syncer/1.0.0/syncer-1.0.0.tar.gz

---
+++ packages/syncer/syncer-1.0.0.tar.gz/syncer-1.0.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='syncer',
+    version='1.0.0',
+    description="Async to sync converter",
+    long_description=readme,
+    author="Hiroyuki Takagi",
+    author_email='miyako.dev@gmail.com',
+    url='https://github.com/miyakogi/syncer',
+    py_modules=['syncer'],
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    keywords='syncer',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='test_syncer',
+)
