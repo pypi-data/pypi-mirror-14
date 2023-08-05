b0a7799c4f4daec845a80ace70e44baf37b8ba7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package python-object-persistence/0.9.5/python-object-persistence-0.9.5.tar.gz

---
+++ packages/python-object-persistence/python-object-persistence-0.9.5.tar.gz/python-object-persistence-0.9.5/persistent/__init__.py
@@ -0,0 +1,6 @@
+from .persistent import Persistent
+from .errors import UniquenessError, NotFoundError
+from .database import get, connect, add_index, transaction
+from .query import Query, OrQuery
+
+import isodatetimehandler
