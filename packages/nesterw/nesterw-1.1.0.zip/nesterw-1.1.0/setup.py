7ec1eb5c88ee21ce150f7a4d2bf12f9cedfca55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:51 +0000

    Add package nesterw/1.1.0/nesterw-1.1.0.zip

---
+++ packages/nesterw/nesterw-1.1.0.zip/nesterw-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup       #从发布工具导入setup函数
+
+setup(
+    name        ='nesterw',
+    version     ='1.1.0',
+    py_modules  =['nesterw'],
+    author      ='JW',
+    author_email='jason_woung@163.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+    )
