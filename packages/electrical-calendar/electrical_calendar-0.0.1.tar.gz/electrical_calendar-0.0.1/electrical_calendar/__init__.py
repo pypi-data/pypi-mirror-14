e409c0bafbfe39453ffc65917be973ae06d1aab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:55 +0000

    Add package electrical-calendar/0.0.1/electrical_calendar-0.0.1.tar.gz

---
+++ packages/electrical-calendar/electrical_calendar-0.0.1.tar.gz/electrical_calendar-0.0.1/electrical_calendar/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'XaviTorello'
+
+import pkg_resources
+
+from electrical_calendar import *
+
+
+#: Module version, as defined in PEP-0396.
+#__version__ = pkg_resources.get_distribution(__package__).version
+__version__ = 0.3
