82be3f398ce1b1b8ce7f99fdd6187aacab10aa4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package ChatterLearning/1.0.1/ChatterLearning-1.0.1.zip

---
+++ packages/ChatterLearning/ChatterLearning-1.0.1.zip/ChatterLearning-1.0.1/chatter_learning/__init__.py
@@ -0,0 +1,4 @@
+from .chatter import Chatter
+__version__ = '1.0.1'
+__author__ = 'GCA'
+__email__ = 'gcaaa31928@gmail.com'
---
+++ packages/ChatterLearning/ChatterLearning-1.0.1.zip/ChatterLearning-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+version = __import__('chatter_learning').__version__
+author = __import__('chatter_learning').__author__
+author_email = __import__('chatter_learning').__email__
+setup(
+    name = 'ChatterLearning',
+    packages = [
+        'chatter_learning',
+        'chatter_learning.brains',
+        'chatter_learning.store_adapters',
+
+    ],
+    install_requires= ['pymongo', 'jieba'],
+    scripts = [],
+    version = version,
+    description = 'Automatic Chat',
+    author = author,
+    author_email = author_email,
+    url = 'https://github.com/gcaaa31928/ChatterLearningt',
+    download_url = 'https://github.com/gcaaa31928/ChatterLearning/releases/tag/v1.0',
+    keywords = ['ai', 'chat', 'bot'],
+    classifiers = [],
+)
