19b79cd4ac6bc30940d0f159177ee1da40c9d519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package nester_xu/1.1.0/nester_xu-1.1.0.tar.gz

---
+++ packages/nester_xu/nester_xu-1.1.0.tar.gz/nester_xu-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_xu',
+	version 	= '1.1.0',
+	py_modules 	= ['nester'],
+	author 		= 'tiger_xu',
+	author_email	= 'suiju1943@live.cn',
+	url		= '',
+	description	= 'A simple printer of nested lists',	
+)
+
