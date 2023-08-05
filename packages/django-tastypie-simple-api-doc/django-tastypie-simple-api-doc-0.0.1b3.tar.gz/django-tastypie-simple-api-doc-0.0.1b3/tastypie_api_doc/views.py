951288bc42ec1c36358130c244f48db3ad4a05ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package django-tastypie-simple-api-doc/0.0.1b3/django-tastypie-simple-api-doc-0.0.1b3.tar.gz

---
+++ packages/django-tastypie-simple-api-doc/django-tastypie-simple-api-doc-0.0.1b3.tar.gz/django-tastypie-simple-api-doc-0.0.1b3/tastypie_api_doc/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import json
+
+# Create your views here.
+import importlib
+from django.http import HttpResponse
+
+def build_doc(request):
+    try:
+        from project.settings import API_OBJECT_LOCATION
+        app, module, obj_name = API_OBJECT_LOCATION.split(".")
+        module = importlib.import_module(app + "." + module)
+        obj = getattr(module, obj_name)
+        api_json = obj.top_level(request)
+        api_json = json.loads(api_json.content)
+        # print api_json
+        return render_to_response('index.html', {'api': {'data': api_json, 'name': obj.api_name}}, context_instance=RequestContext(request))
+    except ImportError:
+        return HttpResponse("No donuts for you. You have to create API_OBJECT_LOCATION in settings.py")
