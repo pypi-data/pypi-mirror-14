592cb8b6f771ce2a21efa32077aeb388729de656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:32 +0000

    Add package optimizely/0.3/optimizely-0.3.tar.gz

---
+++ packages/optimizely/optimizely-0.3.tar.gz/optimizely-0.3/optimizely/__init__.py
@@ -0,0 +1,13 @@
+# Optimizely Python bindings
+# API docs at http://developers.optimizely.com/
+# Authors:
+# Andy Harris <andy.harris@optimizely.com>
+
+# Client
+from .client import *
+
+# Resources
+from .resource import *
+
+# Errors
+from .error import *
---
+++ packages/optimizely/optimizely-0.3.tar.gz/optimizely-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='optimizely',
+      version='0.3',
+      description='A Python interface to Optimizely\'s REST API.',
+      url='https://github.com/optimizely/optimizely-client-python',
+      author='Optimizely',
+      author_email = 'developers@optimizely.com',
+      packages=['optimizely'],
+      keywords= ['python-sdk', 'optimizely'],
+      download_url = 'https://github.com/optimizely/optimizely-client-python/tarball/0.3',
+      install_requires=[
+        'requests',
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'])
