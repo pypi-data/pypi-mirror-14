9beb8974b324b10d32fa854090ae0395dee42d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:13 +0000

    Add package reprophylo/1.2/reprophylo-1.2.tar.gz

---
+++ packages/reprophylo/reprophylo-1.2.tar.gz/reprophylo-1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='reprophylo',
+      version='1.2',
+      description='Reproducible phylogenetics pipeline',
+      author='Amir Szitenberg',
+      author_email='szitenberg@gmail.com',
+      url='http://hulluni-bioinformatics.github.io/ReproPhylo/',
+      license="CC0 1.0",
+      py_modules=['reprophylo','gb_syn','HTML','rpgit','css'],
+      )
