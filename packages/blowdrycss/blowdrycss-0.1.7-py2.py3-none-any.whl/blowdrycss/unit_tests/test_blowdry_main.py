8a0a022f38565b5f7d6e0941ac34202309e46d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package blowdrycss/0.1.7/blowdrycss-0.1.7-py2.py3-none-any.whl

---
+++ packages/blowdrycss/blowdrycss-0.1.7-py2.py3-none-any.whl/blowdrycss/unit_tests/test_blowdry_main.py
@@ -0,0 +1,33 @@
+from unittest import TestCase, main
+import sys
+from io import StringIO
+# custom
+import blowdrycss.blowdry as blowdry
+
+__author__ = 'chad nelson'
+__project__ = 'blowdrycss'
+
+
+class TestMain(TestCase):
+    def test_main(self):
+        substrings = [
+            '~~~ blowdrycss started ~~~', 'Project Files Found:',
+            'CSSBuilder Running...', '.css',
+        ]
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            blowdry.main()
+
+            output = out.getvalue()
+            for substring in substrings:
+                self.assertTrue(substring in output, msg=output + '\tsubstring: ' + substring)
+        finally:
+            sys.stdout = saved_stdout
+
+
+if __name__ == '__main__':
+    main()
+
