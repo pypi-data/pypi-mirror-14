a45d4204ec2b87d64c766c6cd2f648b4bed2df0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:01 +0000

    Add package python_constantcontact/0.1/python_constantcontact-0.1.tar.gz

---
+++ packages/python_constantcontact/python_constantcontact-0.1.tar.gz/python_constantcontact-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_constantcontact',
+    version='0.1',
+    description='Constant contact lib for python, with stuff for Django too',
+    author='Samuel Toriel',
+    author_email='samueltoriel@gmail.com',
+    url='http://github.com/riltsken/python-constantcontact/tree/master',
+    packages=find_packages(),
+    install_requires=['httplib2','feedparser'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
