7b1f9640c3365a1c849540db635a65f8bf118ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:37 +0000

    Add package SoftLayer/5.0.0/SoftLayer-5.0.0-py2.py3-none-any.whl

---
+++ packages/SoftLayer/SoftLayer-5.0.0-py2.py3-none-any.whl/SoftLayer/CLI/ticket/create.py
@@ -0,0 +1,29 @@
+"""Create a support ticket."""
+# :license: MIT, see LICENSE for more details.
+
+import click
+
+import SoftLayer
+from SoftLayer.CLI import environment
+from SoftLayer.CLI import ticket
+
+
+@click.command()
+@click.option('--title', required=True, help="The title of the ticket")
+@click.option('--subject-id',
+              required=True,
+              help="""The subject id to use for the ticket,
+ issue 'slcli ticket subjects' to get the list""")
+@click.option('--body', help="The ticket body")
+@environment.pass_env
+def cli(env, title, subject_id, body):
+    """Create a support ticket."""
+    mgr = SoftLayer.TicketManager(env.client)
+
+    if body is None:
+        body = click.edit('\n\n' + ticket.TEMPLATE_MSG)
+
+    created_ticket = mgr.create_ticket(title=title,
+                                       body=body,
+                                       subject=subject_id)
+    env.fout(ticket.get_ticket_results(mgr, created_ticket['id']))
