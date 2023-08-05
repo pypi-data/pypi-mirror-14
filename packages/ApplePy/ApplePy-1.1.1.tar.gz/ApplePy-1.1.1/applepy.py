6e1c3b6347b91bc4597f0de297bc76249c4085dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package ApplePy/1.1.1/ApplePy-1.1.1.tar.gz

---
+++ packages/ApplePy/ApplePy-1.1.1.tar.gz/ApplePy-1.1.1/applepy.py
@@ -0,0 +1,5 @@
+""" This is the "recipe.py" module, and it provides a list of os.system commands shortcude."""
+def clear ():
+	""" This function clears the screen """
+	import os
+	os.system('clear')
