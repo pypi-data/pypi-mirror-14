ac58b9b67462579d34233dae4282e38b39512e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package django-fhir/0.0.0.4/django-fhir-0.0.0.4.tar.gz

---
+++ packages/django-fhir/django-fhir-0.0.0.4.tar.gz/django-fhir-0.0.0.4/fhir/views/search.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from ..models import SupportedResourceType
+from collections import OrderedDict
+from django.http import HttpResponse
+import json
+from ..utils import kickout_400
+from .utils import check_access_interaction_and_resource_type
+from ..settings import FHIR_BACKEND
+
+def search(request, resource_type):
+    interaction_type = 'search'
+    #Check if this interaction type and resource type combo is allowed.
+    deny = check_access_interaction_and_resource_type(resource_type, interaction_type)
+    if deny:
+        #If not allowed, return a 4xx error.
+        return deny
+
+    """Search Interaction"""
+    # Example client use in curl:
+    # curl -X GET  http://127.0.0.1:8000/fhir/Practitioner?foo=bar
+    if request.method != 'GET':
+        msg = "HTTP method %s not supported at this URL." % (request.method)
+        return kickout_400(msg)
+
+    return FHIR_BACKEND.find(request, resource_type)
+
+
+    # Move to fhir_io_mongo (Plugable back-end)
+    od = OrderedDict()
+    od['request_method']= request.method
+    od['interaction_type'] = "search"
+    od['resource_type']    = resource_type
+    od['search_params'] = request.GET
+    od['note'] = "This is only a stub for future implementation"
+    
+    return HttpResponse(json.dumps(od, indent=4),
+                        content_type="application/json")
