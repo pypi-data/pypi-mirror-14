2ce53e55b5eba7503963d07d105752ee8942f651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package pytelemetrycli/1.1.0/pytelemetrycli-1.1.0-py3-none-any.whl

---
+++ packages/pytelemetrycli/pytelemetrycli-1.1.0-py3-none-any.whl/pytelemetrycli/runner.py
@@ -0,0 +1,131 @@
+import threading
+import time
+
+# Main class
+class Runner:
+    def __init__(self, transport, telemetry, plots, plotsLock, topics):
+
+        self.transport = transport
+        self.telemetryWrapper = telemetry
+        self.plots = plots
+        self.plotsLock = plotsLock
+        self.topics = topics
+
+        self.thread = None
+        self.running = threading.Event()
+        self.running.set()
+
+        self.connected = threading.Event()
+        self.connected.clear()
+
+        self.resetStats()
+
+    def connect(self,port,bauds):
+        # Create monitoring topics
+        self.topics.create("baudspeed",source="cli")
+        self.topics.create("baudspeed_avg",source="cli")
+        self.topics.create("rx_in_waiting",source="cli")
+        self.topics.create("rx_in_waiting_max",source="cli")
+        self.topics.create("rx_in_waiting_avg",source="cli")
+
+        # Connection options
+        options = dict()
+        options['port'] = port
+        options['baudrate'] = bauds
+
+        self.baudrate = bauds
+        self.transport.connect(options)
+
+        self._start_thread()
+        
+    def _start_thread(self):
+        self.connected.set()
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+    def disconnect(self):
+        self.connected.clear()
+        self.transport.disconnect()
+
+    def terminate(self):
+        self.running.clear()
+        if self.thread:
+            self.thread.join()
+        try:
+            self.transport.disconnect()
+        except:
+            pass # Already disconnected
+
+    def stats(self):
+        return {
+            "baudspeed" : self.baudspeed,
+            "baudspeed_avg" : self.baudspeed_avg,
+            "baudratio" : self.baudspeed / self.baudrate,
+            "baudratio_avg" : self.baudspeed_avg / self.baudrate
+        }
+
+    def resetStats(self):
+        self.baudrate = 1.0
+        self.baudspeed = 0.0
+        self.lasttime = time.time()
+        self.lastamount = 0.0
+        self.baudspeed_avg = 0.0
+
+    def update(self):
+        # Update protocol decoding
+        self.telemetryWrapper.update()
+
+        # Protect the self.plots data structure from
+        # being modified from the main thread
+        self.plotsLock.acquire()
+
+        # Poll each poll pipe to see if user closed them
+        plotToDelete = None
+        for p, i in zip(self.plots,range(len(self.plots))):
+            if p['ctrl'].poll():
+                if p['ctrl'].recv() == "closing":
+                    plotToDelete = i
+                    break
+
+        # Delete a plot if needed
+        if plotToDelete is not None:
+            self.plots[plotToDelete]['ctrl'].close()
+            topic = self.plots[plotToDelete]['topic']
+            self.topics.untransfer(topic)
+            self.plots.pop(plotToDelete)
+
+        self.plotsLock.release()
+
+    def computeStats(self):
+
+        current = time.time()
+        difft = current - self.lasttime
+
+        if difft > 0.05 :
+            self.lasttime = current
+
+            measures = self.transport.stats()
+            diff = measures['rx_bytes'] - self.lastamount
+            self.lastamount = measures['rx_bytes']
+
+            self.baudspeed = diff / difft
+
+            # Compute rolling average baud speed on about 1 second window
+            n = 20
+            self.baudspeed_avg = (self.baudspeed + n * self.baudspeed_avg) / (n + 1)
+
+            # Send cli system data to the topics so that they can be plotted.
+            self.topics.process("baudspeed",self.baudspeed)
+            self.topics.process("baudspeed_avg",self.baudspeed_avg)
+            self.topics.process("rx_in_waiting",measures['rx_in_waiting'])
+            self.topics.process("rx_in_waiting_max",measures['rx_in_waiting_max'])
+            self.topics.process("rx_in_waiting_avg",measures['rx_in_waiting_avg'])
+
+
+    def run(self):
+        while self.running.is_set():
+            if self.connected.is_set():
+                self.update()
+                self.computeStats()
+            else:
+                time.sleep(0.5)
---
+++ packages/pytelemetrycli/pytelemetrycli-1.1.0-py3-none-any.whl/pytelemetrycli/topics.py
@@ -0,0 +1,124 @@
+from sortedcontainers import SortedDict
+from logging import getLogger
+
+class Topic:
+    """
+A class to store and manage all data under a given topic
+    """
+    def __init__(self, name, source='remote'):
+        self.raw = []
+        self.indexes = SortedDict()
+        self.source = source
+        self.name = name
+
+    def has_indexed_data(self):
+        return len(self.indexes) > 0
+
+    def new_sample(self, sample, options):
+        if options:
+            self.indexes[options['index']] = sample
+        else:
+            self.raw.append(sample)
+
+class Topics:
+    """
+A class that manages a collection of `Topic`s.
+
+    """
+    def __init__(self):
+        self.logger = getLogger('topics')
+        self.logger.info('started session')
+        self.clear()
+
+    def clear(self):
+        self.logger.info('Cleared all topics and received data')
+        self.topic_list = SortedDict()
+        self.transfers = dict()
+
+    def create(self, topic, source='remote'):
+        # Create the topic if it doesn't exist already
+        if not topic in self.topic_list:
+            self.topic_list[topic] = Topic(topic,source=source)
+            self.logger.info('new:topic ' + topic)
+
+    def process(self, topic, payload, options=None):
+        # Create the topic if it doesn't exist already
+        self.create(topic)
+
+        # Add the new sample
+        self.topic_list[topic].new_sample(payload,options)
+
+        # logging
+        if options:
+            self.logger.debug('new sample | {0} [{1}] {2}'.format(topic, options['index'], payload))
+        else:
+            self.logger.debug('new sample | {0} {1}'.format(topic, payload))
+
+        # If there is an active transfer, transfer received data to the queue
+        if topic in self.transfers:
+            # If transfer requires indexed data, check there is an index
+            if self.transfers[topic]['type'] == 'indexed' and options is not None:
+                x = options['index']
+                self.transfers[topic]['queue'].put([x, payload])
+            # For linear data, provide sample id for x and payload for y
+            elif self.transfers[topic]['type'] == 'linear':
+                x = self.transfers[topic]['lastindex']
+                self.transfers[topic]['queue'].put([x, payload])
+                self.transfers[topic]['lastindex'] += 1
+
+    def ls(self,source='remote'):
+        if source is None:
+            return sorted([t.name for t in self.topic_list.keys()])
+        else:
+            return sorted([t.name for t in self.topic_list.values() if t.source == source])
+
+    def samples(self,topic,amount=1):
+        if not topic in self.topic_list:
+            return None
+
+        if amount == 0 or amount is None:
+            return self.topic_list[topic].raw
+
+        return self.topic_list[topic].raw[-amount:]
+
+    def count(self,topic):
+        if not topic in self.topic_list:
+            return 0
+
+        return len(self.topic_list[topic].raw)
+
+    def exists(self,topic):
+        return topic in self.topic_list
+
+    def transfer(self, topic, queue, transfer_type = "linear"):
+        # If the topic data is not already transfered to some queue
+        if not topic in self.transfers:
+            self.transfers[topic] = dict()
+            self.transfers[topic]['queue'] = queue
+            self.transfers[topic]['lastindex'] = 0
+            self.transfers[topic]['type'] = transfer_type
+
+            self.logger.info('start transfer | {0}'.format(topic))
+
+            # If there is already existing data under the topic
+            if topic in self.topic_list:
+                if transfer_type == 'indexed':
+                    for key, value in self.topic_list[topic].indexes.iteritems():
+                        queue.put([key, value])
+                elif transfer_type == 'linear':
+                    for item in self.topic_list[topic].raw:
+                        queue.put([self.transfers[topic]['lastindex'], item])
+                        self.transfers[topic]['lastindex'] += 1
+
+    def untransfer(self,topic):
+        # If the topic data is already transfered to some queue
+        if topic in self.transfers:
+            # Remove it from the transfer list
+            del self.transfers[topic]
+            self.logger.info('stop transfer | {0}'.format(topic))
+
+    def intransfer(self,topic):
+        return topic in self.transfers
+
+    def has_indexed_data(self,topic):
+        return self.topic_list[topic].has_indexed_data()
