964e518e3bd4fdcbb9e6204b71171bf64331a563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:56 +0000

    Add package homework-qingcloud-cli/0.2/homework-qingcloud-cli-0.2.zip

---
+++ packages/homework-qingcloud-cli/homework-qingcloud-cli-0.2.zip/homework-qingcloud-cli-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import driver
+
+setup(
+  name = 'homework-qingcloud-cli',
+  version = 0.2,
+  packages = find_packages('.'),
+  install_requires = ['requests','PyYAML>=3.1'],
+  entry_points = {
+    'console_scripts': [
+      'homework-qingcluod-cli = driver:main']
+  },
+
+  author = 'phantooom',
+  author_email = 'zouruixp@sina.com',
+  description = 'homework-qingcluod-cli',
+  license = 'MIT',
+  keywords = 'homework',
+  url = 'http://fullstack.love',
+  scripts=['bin/qingcloud'],
+  package_dir = {'homework-qingcluod-cli': 'action'},
+)
