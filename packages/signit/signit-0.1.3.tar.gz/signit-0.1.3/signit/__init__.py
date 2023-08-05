0f543c99dfbbb8e30b678abce8eff758ae3a4664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package signit/0.1.3/signit-0.1.3.tar.gz

---
+++ packages/signit/signit-0.1.3.tar.gz/signit-0.1.3/signit/__init__.py
@@ -0,0 +1,6 @@
+from .signature import generate_key
+from .signature import create_signature
+from .signature import parse_signature
+from .signature import verify_signature
+
+__version__ = '0.1.3'
