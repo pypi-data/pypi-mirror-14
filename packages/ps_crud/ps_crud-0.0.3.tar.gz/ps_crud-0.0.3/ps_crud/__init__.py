90290afc4940d0a32670b28ad3e1b37bea5330a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package ps_crud/0.0.3/ps_crud-0.0.3.tar.gz

---
+++ packages/ps_crud/ps_crud-0.0.3.tar.gz/ps_crud-0.0.3/ps_crud/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 uralbash <root@uralbash.ru>
+#
+# Distributed under terms of the MIT license.
+
+
+def includeme(config):
+    config.include('pyramid_jinja2')
+    config.add_jinja2_search_path("templates")
+    config.add_static_view('ps_crud_static', 'ps_crud:static')
