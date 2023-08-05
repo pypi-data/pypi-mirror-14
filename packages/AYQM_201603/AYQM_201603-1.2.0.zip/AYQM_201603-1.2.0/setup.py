0c950813cb54be9ffa3b9870e4d7ed3c6e7eb23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package AYQM_201603/1.2.0/AYQM_201603-1.2.0.zip

---
+++ packages/AYQM_201603/AYQM_201603-1.2.0.zip/AYQM_201603-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='AYQM_201603',
+    version='1.2.0',
+    py_modules=['print_L'],
+    author='hfpython',
+    author_email='ayqming@gmail.com',
+    url='http://www.163.com',
+    description='A simple printer ofnested list',
+    )
+
+
