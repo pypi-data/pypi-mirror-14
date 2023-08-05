b41ece069081dfe8a32295e366dfc03fe808acc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:59 +0000

    Add package django-easyfilters-ap/0.1.0/django-easyfilters-ap-0.1.0.tar.gz

---
+++ packages/django-easyfilters-ap/django-easyfilters-ap-0.1.0.tar.gz/django-easyfilters-ap-0.1.0/tests/test_project/settings.py
@@ -0,0 +1,63 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'tests.db',
+    },
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'django_easyfilters',
+    'test_app',
+    #'debug_toolbar',
+    #'django_extensions',
+]
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
+]
+try:
+    import debug_toolbar
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': lambda req: True,
+    }
+except ImportError:
+    pass
+
+try:
+    import django_extensions
+    INSTALLED_APPS.append('django_extensions')
+except ImportError:
+    pass
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+]
+
+ROOT_URLCONF = 'test_app.urls'
+
+DEBUG = True
+
+SITE_ID = 1
+
+STATIC_URL = '/static/'
+
+SECRET_KEY = 'x'
+
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
