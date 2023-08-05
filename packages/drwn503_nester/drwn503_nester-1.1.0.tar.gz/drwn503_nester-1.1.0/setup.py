151091f91178d7856b29a19e06abf23449e35199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package drwn503_nester/1.1.0/drwn503_nester-1.1.0.tar.gz

---
+++ packages/drwn503_nester/drwn503_nester-1.1.0.tar.gz/drwn503_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'drwn503_nester',
+	version 	= '1.1.0',
+	py_modules	= ['drwn503_nester'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://headfirstlabs.com',
+	description	= 'A simple printer of nested lists studied by darwinian503@gmail.com'
+)
