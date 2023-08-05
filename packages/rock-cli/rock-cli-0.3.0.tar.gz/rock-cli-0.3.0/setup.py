6ae30f0eec7f2558c55a2e4281c5887c8226ae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:50 +0000

    Add package rock-cli/0.3.0/rock-cli-0.3.0.tar.gz

---
+++ packages/rock-cli/rock-cli-0.3.0.tar.gz/rock-cli-0.3.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+import sys
+from setuptools import setup
+
+from rock_cli import __version__
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip3 freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(__version__))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name="rock-cli",
+    version=__version__,
+    author="Ale",
+    author_email="ale@songbee.net",
+    description="A Rocketbank CLI",
+    url="https://github.com/iamale/rock",
+    packages=["rock_cli"],
+    install_requires=[
+        "Click",
+        "yamlcfg",
+        "tabulate",
+        "arequests==0.1.0",
+    ],
+    entry_points="""
+        [console_scripts]
+        rock=rock_cli.cli:cli
+    """,
+)
