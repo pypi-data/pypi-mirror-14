5c016d3dbf12bfc7828a593e0412b9dc36e03531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:14 +0000

    Add package git-playback/0.1.1a0/git-playback-0.1.1a0.tar.gz

---
+++ packages/git-playback/git-playback-0.1.1a0.tar.gz/git-playback-0.1.1a0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='git-playback',
+    version='0.1.1a',
+    url='https://github.com/jianli/git-playback',
+    description='A git command to play back file history.',
+    packages=find_packages('.'),
+    entry_points={
+        'console_scripts': (
+            'git-playback = playback:playback',
+        ),
+    },
+    install_requires=[
+        'gitpython>=0.3.2.RC1',
+    ],
+)
