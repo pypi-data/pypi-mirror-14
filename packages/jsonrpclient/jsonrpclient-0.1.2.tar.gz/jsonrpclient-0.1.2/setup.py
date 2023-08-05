91e9149f355600af6f5bc24ed04d8fc336b7b66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:05 +0000

    Add package jsonrpclient/0.1.2/jsonrpclient-0.1.2.tar.gz

---
+++ packages/jsonrpclient/jsonrpclient-0.1.2.tar.gz/jsonrpclient-0.1.2/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+setup(
+    name='jsonrpclient',
+    version=open("version", "r").read().strip(),
+    packages=['jsonrpclient'],
+    url='https://github.com/jacexh/jsonrpclient',
+    license='MIT',
+    author='Jace Xu',
+    author_email='jace@xuh.me',
+    description='A JSON-RPC client in Python',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), 'r').read(),
+    install_requires=["requests>=2.5.0"])
