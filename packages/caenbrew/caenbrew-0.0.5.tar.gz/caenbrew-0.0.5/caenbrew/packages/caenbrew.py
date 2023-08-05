bc0f2d8bebed905618ae48b80ee30537e622167d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package caenbrew/0.0.5/caenbrew-0.0.5.tar.gz

---
+++ packages/caenbrew/caenbrew-0.0.5.tar.gz/caenbrew-0.0.5/caenbrew/packages/caenbrew.py
@@ -0,0 +1,28 @@
+from ..packaging import BasePackage, package
+
+
+@package
+class CaenbrewPackage(BasePackage):
+    """Caenbrew: the best CAEN package manager around."""
+
+    name = "caenbrew"
+
+    @property
+    def is_installed(self):
+        """If this code runs, then Caenbrew is installed."""
+        return True
+
+    def install(self):
+        """If the user tries to force a reinstall, instead try an upgrade."""
+        self._cmd("/usr/um/python-2.7/bin/pip",
+                  "install", "--upgrade", "caenbrew",
+                  title="Upgrading caenbrew")
+
+    def uninstall(self):
+        """Uninstall Caenbrew.
+
+        You can bet that some people will try this and be surprised when it
+        works ;)
+        """
+        self._cmd("/usr/um/python-2.7/bin/pip", "uninstall", "-y", "caenbrew",
+                  title="Uninstalling caenbrew")
