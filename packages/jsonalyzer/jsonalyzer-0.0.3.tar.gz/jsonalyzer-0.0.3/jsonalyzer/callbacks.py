98327d8971036a8ef3a375fe80b2da9351c576f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:47 +0000

    Add package jsonalyzer/0.0.3/jsonalyzer-0.0.3.tar.gz

---
+++ packages/jsonalyzer/jsonalyzer-0.0.3.tar.gz/jsonalyzer-0.0.3/jsonalyzer/callbacks.py
@@ -0,0 +1,99 @@
+import string
+import json
+
+def get_output_template(json_ds):
+  return {
+    'ds': json_ds,
+    'msg': '',
+    'status': 'OK'
+  }
+
+def no_op(json_ds, **kwargs):
+  output = get_output_template(json_ds)
+  output['msg'] = 'OK'
+  return output
+
+def check_has_key(json_ds, **kwargs):
+
+  output = get_output_template(json_ds)
+
+  if 'params' not in kwargs:
+    output['status'] = 'CRITICAL'
+    output['msg'] = "ERROR: Did not pass --params"
+    return output
+
+  if 'key' not in kwargs['params']:
+    output['status'] = 'CRITICAL'
+    output['msg'] = "ERROR: Did not set \"key\" in --params"
+    return output
+
+  target_key = kwargs['params']['key']
+
+  if target_key not in json_ds:
+    output['msg'] = 'ERROR: Key %s not found' % target_key
+    output['status'] = 'CRITICAL'
+    return output
+
+  output['msg'] = 'OK: Found key - %s = %s' % (target_key, json_ds[target_key])
+  return output
+
+def check_key_value(json_ds, **kwargs):
+
+  output = check_has_key(json_ds, **kwargs)
+
+  if output['status'] != 'OK':
+    return output
+
+  if 'value' not in kwargs['params']:
+    output['status'] = 'CRITICAL'
+    output['msg'] = "ERROR: Did not set \"value\" in --params"
+    return output
+
+  target_key = kwargs['params']['key']
+  target_value = kwargs['params']['value']
+
+  value = json_ds[target_key]
+  if value != target_value:
+    output['msg'] = 'ERROR: Key %s value == %s != %s' % (target_key, target_value,
+                                                         value)
+    output['status'] = 'CRITICAL'
+    return output
+
+  output['msg'] = 'OK: Found key - %s == %s' % (target_key, json_ds[target_key])
+  return output
+
+def check_key_value_limits(json_ds, **kwargs):
+  output = check_has_key(json_ds, **kwargs)
+
+  if output['status'] != 'OK':
+    return output
+
+  if 'warning' not in kwargs['params']:
+    output['status'] = 'CRITICAL'
+    output['msg'] = "ERROR: Did not set \"warning\" in --params"
+    return output
+
+  if 'critical' not in kwargs['params']:
+    output['status'] = 'CRITICAL'
+    output['msg'] = "ERROR: Did not set \"critical\" in --params"
+    return output
+
+  target_key = kwargs['params']['key']
+  value = float(json_ds[target_key])
+
+  warning = float(kwargs['params']['warning'])
+  critical = float(kwargs['params']['critical'])
+
+  if value >= critical:
+    output['status'] = 'CRITICAL'
+    output['msg'] = 'ERROR: %s = %.2f >= critical limit %.2f' % (target_key,
+                                                                 value, critical)
+    return output
+
+  if value >= warning:
+    output['status'] = 'WARNING'
+    output['msg'] = 'ERROR: %s = %.2f >= warning limit %.2f' % (target_key, 
+                                                                value, warning)
+
+  output['msg'] = 'OK: %s = %.2f < %.2f' % (target_key, value, warning)
+  return output
---
+++ packages/jsonalyzer/jsonalyzer-0.0.3.tar.gz/jsonalyzer-0.0.3/jsonalyzer/cmdline.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+""" json analyzer """
+
+# TODO
+# - release first cut pip
+# - decouple worker from output printer
+# - allow user to specify url directly
+# - add support for flattened keys
+# - add support to dump specific headers 
+# - add key ==, >=, <=, >, < value callback
+# - update README with examples and TODO
+# - add nports - revisit output format
+
+import os
+import json
+import click
+
+from jsonalyzer import defaults
+from jsonalyzer import utils
+
+def _load_callback(ctx, param, value):
+  """ Load jsonalzyer callback """
+  if value is None:
+    return value
+
+  module_src, callback_func = value.strip().split(':')
+
+  mod = None
+  try:
+    if os.path.exists(module_src):
+      mod = utils.load_module_frm_file(module_src)
+    else:
+      mod = utils.load_module_by_name(module_src)
+    return getattr(mod, callback_func)
+  except Exception as load_callback_exception:
+    raise click.BadParameter(str(load_callback_exception))
+
+def _load_json_frm_str(ctx, param, value):
+  """ Load json from string """
+  if value is None:
+    return value
+  try:
+    return utils.load_json_frm_str(value)
+  except Exception as exception:
+    raise click.BadParameter(str(exception))
+
+def validate_protocol(ctx, param, value):
+  """ Validate web command's argument - protocol """
+  if value not in defaults.VALID_PROTOCOLS:
+    raise click.BadParameter('%s: Valid values:' % defaults.VALID_PROTOCOLS)
+  return value
+
+def output_printer(output):
+  """ Common output printer """
+  color = 'green'
+  if 'color' in output:
+    color = output['color']
+  elif output['status'] != 'OK':
+    if output['status'] == 'WARNING':
+      color = 'yellow'
+    else:
+      color = 'red'
+
+  click.echo(click.style(output['msg'], fg=color))
+  print json.dumps(output['ds'], indent=2)
+  return True
+
+def common_worker(loader, **kwargs):
+  """ Common worker - load the json, run the callback on it """
+  try:
+    json_ds = loader(**kwargs)
+    if kwargs['flatten']:
+      json_ds = utils.flatten_ds(json_ds)
+  except Exception as exception:
+    click.echo(click.style('ERROR: Failed to load json. Dumping parameters', 
+                           fg='red'))
+    # stringify the functions otherwise as json can't encode them
+    for key in ['callback', 'output_printer']:
+      kwargs[key] = str(kwargs[key])
+    click.echo(json.dumps(kwargs, indent=2))
+    return 1
+
+  output = kwargs['callback'](json_ds, **kwargs)
+  kwargs['output_printer'](output)
+
+  if output['status'] == 'OK':
+    return 0
+
+  if output['status'] == 'WARNING':
+    return 1
+
+  return 2
+
+@click.group()
+@click.option('--flatten/--no-flatten', help='flatten ds',
+              default=False)
+@click.option('--verbose/--no-verbose', help='verbose mode',
+              default=False)
+@click.option('--callback',
+              help='callback to act upon json. filepath:func or module_name:func',
+              callback=_load_callback,
+              default=defaults.CALLBACK)
+@click.option('--params', help='stringified json to pass to callback',
+              callback=_load_json_frm_str,
+              default=None)
+@click.pass_context
+def jsonalyzer(ctx, **kwargs):
+  """ Top level command for jsonalyzer """
+  if ctx.obj is None:
+    ctx.obj = {}
+  for k in kwargs:
+    ctx.obj[k] = kwargs[k]
+  ctx.obj['output_printer'] = output_printer
+
+@jsonalyzer.command('web')
+@click.argument('PROTOCOL', callback=validate_protocol)
+@click.option('-H', '--host', help='host. Default: %s' % defaults.HOST,
+              default=defaults.HOST)
+@click.option('--uri', help='uri. Default: %s' % defaults.URI,
+              default=defaults.URI)
+@click.option('--port', help='url port',
+              type=click.IntRange(min=1, max=65535),
+              default=None)
+@click.option('--timeout', help='connection timeout. ' +
+              'Default: %d' % defaults.CONN_TIMEOUT,
+              default=defaults.CONN_TIMEOUT)
+@click.option('--username', help='username',
+              default=None)
+@click.option('--password', help='password',
+              default=defaults.URI)
+@click.option('--headers',
+              help='comma separated HTTP headers to dump in output',
+              default=None)
+@click.pass_context
+def load_from_web(ctx, **kwargs):
+  """ 
+  web http|https [options]
+  """
+  kwargs.update(ctx.obj)
+  ctx.exit(common_worker(utils.load_json_frm_url, **kwargs))
+  
+@jsonalyzer.command('file')
+@click.argument('FILE', type=click.Path(exists=True))
+@click.option('--callback',
+              help='callback to act upon json. filepath:func or module_name:func',
+              callback=_load_callback,
+              default=defaults.CALLBACK)
+@click.option('--params', help='stringified json to pass to callback',
+              callback=_load_json_frm_str,
+              default=None)
+@click.pass_context
+def load_from_file(ctx, **kwargs):
+  """ 
+  file filepath [options]
+  """
+  kwargs.update(ctx.obj)
+  ctx.exit(common_worker(utils.load_json_frm_file, **kwargs))
+
+if __name__ == '__main__':
+  jsonalyzer(obj={})
