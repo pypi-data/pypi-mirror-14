a002511a5ef9afbcd133b8f48ddd9c77abc36f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:33 +0000

    Add package cms7/0.1a8/cms7-0.1a8.tar.gz

---
+++ packages/cms7/cms7-0.1a8.tar.gz/cms7-0.1a8/cms7/util.py
@@ -0,0 +1,51 @@
+"""
+General utilities
+"""
+
+import re
+
+from urllib.parse import urlparse
+
+from .error import CMS7Error
+from .hyphenate import hyphenate_word
+
+_NOTHING = object()
+
+
+def meta_get_one(md, key, default=_NOTHING):
+    from .source import MarkdownSource
+    source = None
+    if isinstance(md, MarkdownSource):
+        source = md
+        md = md.meta
+    if key in md:
+        return md[key][0]
+    if default is not _NOTHING:
+        return default
+    if source:
+        raise CMS7Error('{!s} missing required metadata key: {!r}'.format(source.source, key))
+    else:
+        raise KeyError(key)
+
+
+def is_relative_url(url):
+    url = urlparse(url)
+    if url.scheme or url.netloc or url.path.startswith('/'):
+        return False
+    if url.path == '':
+        return False
+    return True
+
+
+WORD_RE = re.compile(r"[A-Za-z0-9'-]+")
+
+def _hyphenate(text):
+    le = 0
+    for match in WORD_RE.finditer(text):
+        yield text[le:match.start()]
+        le = match.end()
+        yield '\u00ad'.join(hyphenate_word(match.group()))
+    yield text[le:]
+
+def hyphenate(text):
+    return ''.join(_hyphenate(text))
