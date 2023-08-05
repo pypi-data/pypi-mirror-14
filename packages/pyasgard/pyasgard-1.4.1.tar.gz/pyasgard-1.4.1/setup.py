a17e51e08cbd3042a1d064ac3ed6ae7ef2be28a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package pyasgard/1.4.1/pyasgard-1.4.1.tar.gz

---
+++ packages/pyasgard/pyasgard-1.4.1.tar.gz/pyasgard-1.4.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+version = {}
+with open('pyasgard/version.py', 'rt') as version_file:
+    exec(version_file.read(), version)
+
+setup(name='pyasgard',
+      version=version['__version__'],
+      description='Python library for working with the Asgard api.',
+      long_description=open('README.rst').read(),
+      author='Sijis Aviles',
+      author_email='saviles@gogoair.com',
+      packages=find_packages(),
+      install_requires=['beautifulsoup4',
+                        'requests', ],
+      keywords="asgard api python netflixoss",
+      url='https://github.com/gogoit/pyasgard',
+      download_url='https://github.com/gogoit/pyasgard',
+      platforms=['OS Independent'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+      ], )
