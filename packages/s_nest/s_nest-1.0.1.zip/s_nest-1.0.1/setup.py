64c712c34110e6e46898f810f06c5ff5c13a50b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package s_nest/1.0.1/s_nest-1.0.1.zip

---
+++ packages/s_nest/s_nest-1.0.1.zip/s_nest-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 's_nest',
+    version = '1.0.1',
+    py_modules = ['s_nest'],
+    author = 's',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.hfpython@headfirstlabs.com',
+    description ='Test'
+)
