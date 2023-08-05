ab98a807c774306a324b84e64d7aab715d578d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:46 +0000

    Add package pypugly/0.2.0/pypugly-0.2.0.tar.gz

---
+++ packages/pypugly/pypugly-0.2.0.tar.gz/pypugly-0.2.0/pypugly/tag.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+import six
+
+
+def create_tag(name, args=None, klass=[], id_=''):
+    """
+
+    Shortcut to create the contents of an open XML tag.
+    :param str name:
+    :param dict args:
+    :param list(str) klass:
+    :param str id_:
+    :return str:
+    """
+
+    def is_quoted(text):
+        return isinstance(text, six.text_type) and not (
+            value.startswith("'") and value.endswith("'")
+        )
+
+    result = name
+
+    if args is None:
+        args = {}
+
+    # Add classes defined using dot syntax.
+    if klass:
+        lst = args.setdefault('class', [])
+        lst += klass
+
+    # Override "id" by the one defined using hash syntax.
+    if id_:
+        args['id'] = id_
+
+    # Format tag arguments in alphabetical order.
+    for i_name, i_value in sorted(args.items()):
+        # Name without value
+        if i_value is True:
+            result += ' {}'.format(i_name)
+            continue
+
+        # List values: join with spaces
+        if isinstance(i_value, list):
+            value = ' '.join(i_value)
+        else:
+            value = i_value
+
+        # Quote value
+        if is_quoted(value):
+            value = '"' + value + '"'
+        result += ' {}={}'.format(i_name, value)
+
+    return result
