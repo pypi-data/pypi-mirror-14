474b2ef1c97bc7d0be150d5f73fa57f2166d389c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package nester.g/1.0.0/nester.g-1.0.0.zip

---
+++ packages/nester.g/nester.g-1.0.0.zip/nester.g-1.0.0/nester.py
@@ -0,0 +1,13 @@
+"""Este é o modulo "nester.py", e fornece uma função chamada print_lol()
+que imprime listar que podem ou não incluir listas aninhadas"""
+def print_lol(the_list, level):
+    """Esta função requer um argumento posicional chamado "the_list), que é
+qualquer lista python (de possíveis listas aninhadas). Cada item de dados na
+lista fornecida é (recursivamente) impresso na tela em sua própria linha"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end ='')
+            print(each_item)
---
+++ packages/nester.g/nester.g-1.0.0.zip/nester.g-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester.g',
+        version         = '1.0.0',
+        py_modules      = ['nester'],
+        author          = 'germoglio',
+        author_email    = 'dgermoglio@gmail.com',
+        url             = 'www.dgermoglio.com',
+        description     = 'teste do livro headfirst',
+        )
