4f271a038f5ae1bc53ed80e67e65285cddb8c0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package vn.oanda/0.1/vn.oanda-0.1.zip

---
+++ packages/vn.oanda/vn.oanda-0.1.zip/vn.oanda-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'vn.oanda',
+	version = '0.1',
+	py_modules = ['vnoanda'],
+	author = 'vn.py project',
+	author_email = 'vn.py@foxmail.com',
+	url = 'http://github.com/vnpy/vnpy',
+	description = '')
