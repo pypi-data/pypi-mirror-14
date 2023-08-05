ab9ac52143f569832b7ed36b63eb70ce69a5becf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package nester_fatumepta/1.4.0/nester_fatumepta-1.4.0.tar.gz

---
+++ packages/nester_fatumepta/nester_fatumepta-1.4.0.tar.gz/nester_fatumepta-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nester_fatumepta',
+        version     = '1.4.0',
+        py_modules  = ['nester_fatumepta'],
+        author      = 'Michael',
+        author_email= 'fatumepta@yandex.ru',
+        url         = 'vk.com/fatumepta',
+        discription = 'a simple printer of nested lists',
+    )
