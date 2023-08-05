3d18528a08a3515f24844f69d58d575ea1efa996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package sentry/8.2.0/sentry-8.2.0.tar.gz

---
+++ packages/sentry/sentry-8.2.0.tar.gz/sentry-8.2.0/src/sentry/features/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+from .base import *  # NOQA
+from .handler import *  # NOQA
+from .manager import *  # NOQA
+
+
+default_manager = FeatureManager()
+default_manager.add('auth:register')
+default_manager.add('organizations:create')
+default_manager.add('organizations:sso', OrganizationFeature)
+default_manager.add('organizations:onboarding', OrganizationFeature)
+default_manager.add('projects:global-events', ProjectFeature)
+default_manager.add('projects:quotas', ProjectFeature)
+default_manager.add('projects:user-reports', ProjectFeature)
+default_manager.add('projects:plugins', ProjectPluginFeature)
+default_manager.add('projects:dsym', ProjectFeature)
+
+
+# expose public api
+add = default_manager.add
+get = default_manager.get
+has = default_manager.has
