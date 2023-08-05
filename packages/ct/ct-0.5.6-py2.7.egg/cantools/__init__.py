1a7e4815ca52f81ae76c385b35a7163c9dbadc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:17 +0000

    Add package ct/0.5.6/ct-0.5.6-py2.7.egg

---
+++ packages/ct/ct-0.5.6-py2.7.egg/cantools/__init__.py
@@ -0,0 +1,11 @@
+import util
+import config as cfgmod
+config = cfgmod.config
+from scripts import builder, deploy, init, pubsub, start
+
+ctstart = start.go
+ctdeploy = deploy.run
+ctpubsub = pubsub.get_addr_and_start
+ctinit = init.parse_and_make
+
+__version__ = "0.5.6"
