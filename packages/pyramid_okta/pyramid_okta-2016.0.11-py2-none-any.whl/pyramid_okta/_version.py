135aac309fc2e3e0f4b79e2a067a74f3ca8c5d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package pyramid_okta/2016.0.11/pyramid_okta-2016.0.11-py2-none-any.whl

---
+++ packages/pyramid_okta/pyramid_okta-2016.0.11-py2-none-any.whl/pyramid_okta/_version.py
@@ -0,0 +1,4 @@
+__major__ = 2016
+__minor__ = 0
+__revision__ = 11
+__hash__ = "ga9a788b"
---
+++ packages/pyramid_okta/pyramid_okta-2016.0.11-py2-none-any.whl/pyramid_okta/security/authorization.py
@@ -0,0 +1,8 @@
+__author__ = 'psuresh'
+from pyramid.authorization import ACLAuthorizationPolicy
+
+# Use the default ACL policy and define permissions as ACL's`
+
+
+class OktaAuthorizationPolicy(ACLAuthorizationPolicy):
+    pass
