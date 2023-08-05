73ae31d5bf9a4dcf03c3c5366cd53420cfc1bd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package django-tenant-schemas-ap/0.1.1/django_tenant_schemas_ap-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-tenant-schemas-ap/django_tenant_schemas_ap-0.1.1-py2.py3-none-any.whl/tenant_schemas/urlresolvers.py
@@ -0,0 +1,12 @@
+import warnings
+from django.conf import settings
+from django.core.urlresolvers import reverse as reverse_default
+from django.utils.functional import lazy
+from tenant_schemas.utils import clean_tenant_url
+
+
+def reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None):
+    url = reverse_default(viewname, urlconf, args, kwargs, current_app)
+    return clean_tenant_url(url)
+
+reverse_lazy = lazy(reverse, str)
