81761a6c1af088a2f09d1c7c6da3f49ae9bfb389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package armada/1.0/armada-1.0.zip

---
+++ packages/armada/armada-1.0.zip/armada-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='armada',
+    packages=['armada'],
+    version='1.0',
+    description='Configuration manager for ArmadaPlatform services.',
+    author='Ganymede',
+    author_email='cerebro@ganymede.eu',
+    url='https://github.com/armadaplatform/armada-py',
+    download_url='https://github.com/armadaplatform/armada-py/tarball/1.0',
+    keywords=['armada', 'hermes', 'config'],
+    classifiers=[],
+)
