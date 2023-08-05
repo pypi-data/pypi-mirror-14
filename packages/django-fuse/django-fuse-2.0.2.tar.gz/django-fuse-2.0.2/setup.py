c2bc26c0a6694fc0f57a4ce68212cd4603ca6cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package django-fuse/2.0.2/django-fuse-2.0.2.tar.gz

---
+++ packages/django-fuse/django-fuse-2.0.2.tar.gz/django-fuse-2.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-fuse',
+
+    url="https://chris-lamb.co.uk/projects/django-fuse",
+    version='2.0.2',
+    description="Abstractions for building FUSE filesystems using Django",
+
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+    license="BSD",
+
+    packages=find_packages(),
+
+    install_requires=(
+        'Django>=1.8',
+    ),
+)
