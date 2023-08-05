9ec1568446053e535344f0ead64881e6fd7eafd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package nester-wudashan/1.0.0/nester-wudashan-1.0.0.tar.gz

---
+++ packages/nester-wudashan/nester-wudashan-1.0.0.tar.gz/nester-wudashan-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='nester-wudashan',
+    version='1.0.0',
+    py_modules=['nester-wudashan'],
+    author='wudashan',
+    author_email='453685754@qq.com',
+    url='www.wudashan.site',
+    description='A simple printer of nested lists',
+)
