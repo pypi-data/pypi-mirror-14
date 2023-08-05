fec2a741c2a91f315be11c66af0b5be5f753ea37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:37 +0000

    Add package Mynested2703/1.2.5/Mynested2703-1.2.5.zip

---
+++ packages/Mynested2703/Mynested2703-1.2.5.zip/Mynested2703-1.2.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Mynested2703",
+    version="1.2.5",
+    py_modules=['nested'],
+    author="lenott",
+    author_email="lenott69@gmail.com",
+    description="A simple print function of nested lists"
+)
