0f1e678c89953aabfcd774f73e5ceea0bd87f633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package isatools/0.1.1/isatools-0.1.1.tar.gz

---
+++ packages/isatools/isatools-0.1.1.tar.gz/isatools-0.1.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='isatools',
+    version='0.1.1',
+    packages=['isatools', 'isatools.convert', 'isatools.io', 'isatools.model', 'isatools.sampledata',
+              'isatools.schemas', 'isatools.validate'],
+    description='ISA-API',
+    author='ISA Infrastructure Team',
+    author_email='isatools@googlegroups.com',
+    url='https://github.com/ISA-tools/isa-api',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        ],
+    install_requires=[
+        "numpy",
+        "biopy-isatab",
+        "jsonschema",
+        "pandas",
+        "networkx",
+        "behave",
+        "httpretty",
+        "sure",
+        "lxml",
+        "requests",
+        "coveralls"
+    ],
+    test_suite='tests'
+)
