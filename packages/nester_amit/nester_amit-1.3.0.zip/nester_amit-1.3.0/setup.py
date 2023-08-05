81475cab95c37ebd04119dc79dcb833543ca88c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package nester_amit/1.3.0/nester_amit-1.3.0.zip

---
+++ packages/nester_amit/nester_amit-1.3.0.zip/nester_amit-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_amit',
+    version='1.3.0',
+    py_modules=['nester_amit'],
+    author='amit',
+    author_email='amit.bit.it@gmail,com',
+    url='http://nikhattoo.com',
+    description='A badass printer of nested lists!',
+)
