178cb4caa5951e696e5bff0426721c777b3e4eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package sy_nester/1.0.0/sy_nester-1.0.0.zip

---
+++ packages/sy_nester/sy_nester-1.0.0.zip/sy_nester-1.0.0/sy_nester.py
@@ -0,0 +1,10 @@
+"""这是“nester.py模块”，提供了一个名为print_lol()的函数，这个函
+数作用是用来打印列表，其中有可能包含（也有可能不包含）嵌套列表"""
+def print_lol (the_list):
+        """将列表的中的每个数据项（包括嵌套列表的数据项）递归的输出
+        在屏幕上，每个数据各占一行"""
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item)
+                else:
+                        print(each_item)
