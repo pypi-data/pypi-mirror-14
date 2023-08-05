553feadc377fa9e0a05f6e0a53ec1c1b59e6649a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package anchorman/0.2.1/anchorman-0.2.1.tar.gz

---
+++ packages/anchorman/anchorman-0.2.1.tar.gz/anchorman-0.2.1/anchorman/logger.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+import inspect
+
+
+def log(msg):
+    """Create a flexible logger.
+    :param msg:
+    """
+    frm = inspect.stack()[1]
+    mod = inspect.getmodule(frm[0])
+    sys.stdout.write('[time] log: %s %s' % (mod.__name__, msg))
