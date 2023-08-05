02b753c1bb3634b0f2243cd2a1ab64339f02d269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package apicheck/1.0/apicheck-1.0.tar.gz

---
+++ packages/apicheck/apicheck-1.0.tar.gz/apicheck-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'apicheck',
+  packages = ['apicheck'],
+  version = '1.0',
+  description = 'A Python API testing tool for JSON based APIs',
+  author = 'Brad Kuykendall',
+  author_email = 'brad.kuykend@gmail.com',
+  url = 'https://github.com/kuykendb/APICheck',
+  download_url = 'https://github.com/kuykendb/APICheck/tarball/v1.0-beta.1', 
+  keywords = ['testing', 'api', 'web', 'json', 'test'],
+  classifiers = [],
+)
