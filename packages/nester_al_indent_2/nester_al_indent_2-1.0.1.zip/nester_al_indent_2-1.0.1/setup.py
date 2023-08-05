113b4c81a0cc996bceba388715081a81ff99f12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package nester_al_indent_2/1.0.1/nester_al_indent_2-1.0.1.zip

---
+++ packages/nester_al_indent_2/nester_al_indent_2-1.0.1.zip/nester_al_indent_2-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name = 'nester_al_indent_2',
+	version = '1.0.1',
+	py_modules = ['nester'],
+	author = 'alisowska',
+	author_email = 'anna-lisowska@outlook.com',
+	description = 'A simple printer of nested list with service of indentation'
+	)
