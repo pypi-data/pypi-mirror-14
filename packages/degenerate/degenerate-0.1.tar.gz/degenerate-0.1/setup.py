bd227f54dad306f88d6bb64f83b8c6608e2b32df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package degenerate/0.1/degenerate-0.1.tar.gz

---
+++ packages/degenerate/degenerate-0.1.tar.gz/degenerate-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='degenerate',
+      version='0.1',
+      description='Write only async code and use for both async and aync',
+      url='git@github.com:hayk91/degenerate.git',
+      license='MIT')
