7a7d788e0313db65ef4ca57e2bc2096d98bbfeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package SCHIdataExplore/1.4/SCHIdataExplore-1.4.tar.gz

---
+++ packages/SCHIdataExplore/SCHIdataExplore-1.4.tar.gz/SCHIdataExplore-1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'SCHIdataExplore',
+  packages = ['SCHIdataExplore'], # this must be the same as the name above
+  version = '1.4',
+  description = 'A tool for the exploration and summarization of new data sets.',
+  author = 'Alexander Titus',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/BioDataScience/SCHIdataExplore', # use the URL to the github repo
+  download_url = 'https://github.com/biodatascience/SCHIdataExplore/tarball/1.4', # I'll explain this in a second
+  keywords = ['data analysis', 'summarization', 'data discovery'], # arbitrary keywords
+  classifiers = [],
+)
