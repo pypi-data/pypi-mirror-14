73cb48d0826c59575295ad2bdfe13e3fc7247194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package fermipy/0.6.7/fermipy-0.6.7.tar.gz

---
+++ packages/fermipy/fermipy-0.6.7.tar.gz/fermipy-0.6.7/fermipy/defaults.py
@@ -0,0 +1,231 @@
+
+# Options for defining input data files
+data = {
+    'evfile': (None, 'Path to FT1 file or list of FT1 files.', str),
+    'scfile': (None, 'Path to FT2 (spacecraft) file.', str),
+    'ltcube': (None, 'Path to livetime cube.  If none a livetime cube will be generated with ``gtmktime``.', str),
+}
+
+# Options for data selection.
+selection = {
+    'emin': (None, 'Minimum Energy (MeV)', float),
+    'emax': (None, 'Maximum Energy (MeV)', float),
+    'logemin': (None, 'Minimum Energy (log10(MeV))', float),
+    'logemax': (None, 'Maximum Energy (log10(MeV))', float),
+    'tmin': (None, 'Minimum time (MET).', int),
+    'tmax': (None, 'Maximum time (MET).', int),
+    'zmax': (None, 'Maximum zenith angle.', float),
+    'evclass': (None, 'Event class selection.', int),
+    'evtype': (None, 'Event type selection.', int),
+    'convtype': (None, 'Conversion type selection.', int),
+    'target': (None, 'Choose an object on which to center the ROI.  '
+                     'This option takes precendence over ra/dec.', str),
+    'ra': (None, '', float),
+    'dec': (None, '', float),
+    'glat': (None, '', float),
+    'glon': (None, '', float),
+    'radius': (None, 'Radius of data selection.  If none this will be automatically set from the ROI size.', float),
+    'filter': (None, 'Filter string for ``gtmktime`` selection.', str),
+    'roicut': ('no', '', str)
+}
+
+# Options for ROI model.
+model = {
+    'src_radius':
+        (None, 'Set the maximum distance for inclusion of sources in the ROI '
+               'model.  Selects all sources within a circle of this radius '
+               'centered '
+               'on the ROI.  If none then no selection is applied.  This '
+               'selection '
+               'will be ORed with sources passing the cut on src_roiwidth.',
+         float),
+    'src_roiwidth':
+        (None, 'Select sources within a box of RxR centered on the ROI.  If '
+               'none then no cut is applied.', float),
+    'src_radius_roi':
+        (None,
+         'Half-width of the ROI selection.  This parameter can be used in '
+         'lieu of src_roiwidth.',
+         float),
+    'isodiff': (None, 'Set the isotropic template.', list),
+    'galdiff': (None, 'Set the galactic IEM mapcube.', list),
+    'limbdiff': (None, '', list),
+    'diffuse': (None, '', list),
+    'sources': (None, '', list),
+    'extdir': ('Extended_archive_v15', '', str),
+    'catalogs': (None, '', list),
+#    'min_ts': (None, '', float),
+#    'min_flux': (None, '', float),
+    'merge_sources' :
+        (True, 'Merge properties of sources that appear in multiple '
+         'source catalogs.  If merge_sources=false then subsequent sources with '
+         'the same name will be ignored.', bool),
+    'assoc_xmatch_columns' :
+        (['3FGL_Name'],'Choose a set of association columns on which to '
+         'cross-match catalogs.',list),
+#    'remove_duplicates': (False, 'Remove duplicate catalog sources.', bool),
+    'extract_diffuse': (
+        False, 'Extract a copy of all mapcube components centered on the ROI.',
+        bool)
+}
+
+# Options for configuring likelihood analysis
+gtlike = {
+    'irfs': (None, '', str),
+    'edisp': (True, 'Enable the correction for energy dispersion.', bool),
+    'edisp_disable': (None,
+                      'Provide a list of sources for which the edisp '
+                      'correction should be disabled.',
+                      list),
+#    'likelihood': ('binned', '', str),
+    'minbinsz': (0.05, 'Set the minimum bin size used for resampling diffuse maps.', float),
+    'rfactor': (2, '', int),
+    'convolve': (True, '', bool),
+    'resample': (True, '', bool),
+    'srcmap': (None, '', str),
+    'bexpmap': (None, '', str),
+}
+
+# Options for binning.
+binning = {
+    'projtype': ('WCS', 'Projection mode (WCS or HPX).', str),
+    'proj': ('AIT', 'Spatial projection for WCS mode.', str),
+    'coordsys': ('CEL', 'Coordinate system of the spatial projection (CEL or GAL).', str),
+    'npix':
+        (None,
+         'Number of pixels.  If none then this will be set from ``roiwidth`` '
+         'and ``binsz``.', int),
+    'roiwidth': (10.0,
+                 'Width of the ROI in degrees.  The number of pixels in each spatial dimension will be set from ``roiwidth`` / ``binsz`` (rounded up).',
+                 float),
+    'binsz': (0.1, 'Spatial bin size in degrees.', float),
+    'binsperdec': (8, 'Number of energy bins per decade.', float),
+    'enumbins': (
+        None,
+        'Number of energy bins.  If none this will be inferred from energy '
+        'range and ``binsperdec`` parameter.', int),
+    'hpx_ordering_scheme': ('RING', 'HEALPix Ordering Scheme', str),
+    'hpx_order': (10, 'Order of the map (int between 0 and 12, included)', int),
+    'hpx_ebin': (True, 'Include energy binning', bool)
+}
+
+# Options related to I/O and output file bookkeeping
+fileio = {
+    'outdir': (None, 'Path of the output directory.  If none this will default to the directory containing the configuration file.', str),
+    'scratchdir': ('/scratch', 'Path to the scratch directory.', str),
+    'workdir': (None, 'Override the working directory.', str),
+    'logfile': (None, 'Path to log file.  If None then log will be written to fermipy.log.', str),
+    'savefits': (True, 'Save intermediate FITS files.', bool),
+    'usescratch': (
+        False, 'Run analysis in a temporary directory under ``scratchdir``.', bool),
+}
+
+logging = {
+    'chatter': (3, 'Set the chatter parameter of the STs.', int),
+    'verbosity': (3, '', int)
+}
+
+# Options related to likelihood optimizer
+optimizer = {
+    'optimizer':
+        ('MINUIT', 'Set the optimization algorithm to use when maximizing the '
+                   'likelihood function.', str),
+    'tol': (1E-4, 'Set the optimizer tolerance.', float),
+    'retries': (3, 'Set the number of times to retry the fit.', int),
+    'min_fit_quality': (3, 'Set the minimum fit quality.', int),
+    'verbosity': (0, '', int)
+}
+
+# MC options
+mc = {
+    'seed' : (None, '', int)
+}
+
+# ROI Optimization
+roiopt = {
+    'npred_threshold': (1.0, '', float),
+    'npred_frac': (0.95, '', float),
+    'shape_ts_threshold': (100.0, '', float)
+}
+
+# Residual Maps
+residmap = {
+    'model': (None, '', dict),
+    'erange': (None, '', list),
+}
+
+# TS Map
+tsmap = {
+    'model': (None, '', dict),
+    'multithread': (False, '', bool),
+    'max_kernel_radius': (3.0, '', float),
+    'erange': (None, '', list),
+}
+
+# TS Cube
+tscube = {
+    'model': (None, '', dict),
+    'do_sed': (True, 'Compute the energy bin-by-bin fits', bool),
+    'nnorm': (10, 'Number of points in the likelihood v. normalization scan', int),
+    'norm_sigma': (5.0, 'Number of sigma to use for the scan range ', float),
+    'cov_scale_bb': (-1.0, 'Scale factor to apply to global fitting '
+                      'cov. matrix in broadband fits. ( < 0 -> no prior ) ', float),
+    'cov_scale': (-1.0, 'Scale factor to apply to broadband fitting cov. '
+                   'matrix in bin-by-bin fits ( < 0 -> fixed ) ', float),
+    'tol': (1E-3, 'Critetia for fit convergence (estimated vertical distance to min < tol )', float),
+    'max_iter': (30, 'Maximum number of iterations for the Newtons method fitter.', int),
+    'tol_type': (0, 'Absoulte (0) or relative (1) criteria for convergence.', int),
+    'remake_test_source': (False, 'If true, recomputes the test source image (otherwise just shifts it)', bool),
+    'st_scan_level': (0, 'Level to which to do ST-based fitting (for testing)', int),
+}
+
+# Options for Source Finder
+sourcefind = {
+    'model': (None, 'Set the source model dictionary.', dict),
+    'min_separation': (1.0, 'Set the minimum separation in deg for sources added in each iteration.', float),
+    'sqrt_ts_threshold': (5.0, 'Set the threshold on sqrt(TS).', float),
+    'max_iter': (3, 'Set the number of search iterations.', int),
+    'sources_per_iter': (3, '', int),
+    'tsmap_fitter': ('tsmap', '', str)
+}
+
+# Options for SED analysis
+sed = {
+    'bin_index': (2.0, '', float),
+    'use_local_index': (False, '', bool),
+    'fix_background': (True, 'Fix background parameters when fitting the '
+                       'source flux in each energy bin.', bool),
+    'ul_confidence': (0.95, 'Confidence level for upper limit calculation.',
+                      float)
+}
+
+# Options for extension analysis
+extension = {
+    'spatial_model': ('GaussianSource', 'Spatial model use for extension test.', str),
+    'width': (None, 'Parameter vector for scan over spatial extent.  If none then the parameter '
+              'vector will be set from ``width_min``, ``width_max``, and ``width_nstep``.', str),
+    'width_min': (0.01, '', float),
+    'width_max': (1.0, '', float),
+    'width_nstep': (21, '', int),
+    'save_templates': (False, '', bool),
+    'fix_background': (False, 'Fix any background parameters that are currently free in the model when '
+                       'performing the likelihood scan over extension.', bool),
+    'save_model_map': (False, '', bool),
+}
+
+# Options for localization analysis
+localize = {
+    'nstep': (5, '', int),
+    'dtheta_max': (0.3, '', float),
+    'update': (False, '', bool)
+}
+
+# Options for plotting
+plotting = {
+    'erange': (None, '', list),
+    'catalogs': (None, '', list),
+    'graticule_radii': (None, '', list),
+    'format': ('png', '', str),
+    'cmap': ('ds9_b', '', str),
+    
+}
---
+++ packages/fermipy/fermipy-0.6.7.tar.gz/fermipy-0.6.7/fermipy/sed_plotting.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+#
+
+# Description
+"""
+Utilities for plotting SEDs and Castro plots
+
+Many parts of this code are taken from dsphs/like/lnlfn.py by 
+  Matthew Wood <mdwood@slac.stanford.edu>
+  Alex Drlica-Wagner <kadrlica@slac.stanford.edu>
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+NORM_LABEL = [r'Flux Normalization [a.u.]',
+              r'E dN/dE [ph $cm^{-2} s^{-1}$]',
+              r'$E^2$ dN/dE [MeV $cm^{-2} s^{-1}]$',
+              r'$n_{\rm pred}$ [ph]',
+              r'dN/dE [ph $cm^{-2} s^{-1} MeV^{-1}$]',
+              r'E dN/dE [MeV $cm^{-2} s^{-1} MeV^{-1}$]']
+              
+
+def plotNLL_v_Flux(nll,nstep=25,xlims=None):
+    """ Plot the (negative) log-likelihood as a function of normalization
+
+    nll   : a LnLFN object
+    nstep : Number of steps to plot 
+    xlims : x-axis limits, if None, take tem from the nll object
+
+    returns fig,ax, which are matplotlib figure and axes objects
+    """
+    if xlims is None:
+        xmin = nll.interp.xmin
+        xmax = nll.interp.xmax
+    else:
+        xmin = xlims[0]
+        xmax = xlims[1]
+    y1 = nll.interp(xmin)
+    y2 = nll.interp(xmax)
+
+    ymin = min(y1,y2)
+    ymax = max(y1,y2,0.5)    
+
+    xvals = np.linspace(xmin,xmax,nstep)
+    yvals = nll.interp(xvals)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_xlim((xmin,xmax))
+    ax.set_ylim((ymin,ymax))
+    
+    ax.set_xlabel(NORM_LABEL[nll.fluxType])
+    ax.set_ylabel(r'$\Delta \log\mathcal{L}$')
+    ax.plot(xvals,yvals)
+    return fig,ax
+
+
+def plotCastro(castroData,ylims,nstep=25,zlims=None):
+    """ Make a color plot (castro plot) of the (negative) log-likelihood as a function of 
+    energy and flux normalization
+
+    castroData : A CastroData object, with the log-likelihood v. normalization for each energy bin
+    ylims      : y-axis limits
+    nstep      : Number of y-axis steps to plot for each energy bin
+    zlims      : z-axis limits
+
+    returns fig,ax,im which are matplotlib figure, axes and image objects
+    """
+    xmin = castroData.specData.log_ebins[0]
+    xmax = castroData.specData.log_ebins[-1]
+    ymin = ylims[0]
+    ymax = ylims[1]
+    if zlims is None:
+        zmin = -10.
+        zmax = 0.
+    else:
+        zmin = zlims[0]
+        zmax = zlims[1]
+  
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_yscale('log')
+    ax.set_xlim((xmin,xmax))
+    ax.set_ylim((ymin,ymax))
+    
+    ax.set_xlabel("log(Energy/GeV)")
+    ax.set_ylabel(NORM_LABEL[castroData.fluxType])
+
+    normVals = np.logspace(np.log10(ymin),np.log10(ymax),nstep)
+    ztmp = []
+    for i in range(castroData.specData.nE):
+        ztmp.append(castroData[i].interp(normVals))
+        pass
+    ztmp = np.asarray(ztmp).T
+    ztmp = np.where(ztmp<zmin,np.nan,ztmp)
+    im = ax.imshow(ztmp, extent=[xmin,xmax,ymin,ymax],
+                   origin='lower', aspect='auto',interpolation='nearest',
+                   vmin=zmin, vmax=zmax,cmap=matplotlib.cm.jet_r)
+    
+    limits = castroData.getLimits(0.05)
+    ebin_centers = (castroData.specData.ebins[0:-1]+castroData.specData.ebins[1:])/2.
+    ax.errorbar(ebin_centers,limits)
+
+    return fig,ax,im
+
+
+
+def plotSED(castroData,ylims,TS_thresh=4.0,errSigma=1.0,specVals=[]):
+    """ Make a color plot (castro plot) of the (negative) log-likelihood as a function of 
+    energy and flux normalization
+
+    castroData : A CastroData object, with the log-likelihood v. normalization for each energy bin
+    ylims      : y-axis limits
+    TS_thresh  : TS value above with to plot a point, rather than an upper limit
+    errSigma   : Number of sigma to use for error bars
+    specVals   : List of spectra to add to plot
+    returns fig,ax which are matplotlib figure and axes objects
+    """
+    xmin = castroData.specData.ebins[0]
+    xmax = castroData.specData.ebins[-1]
+    ymin = ylims[0]
+    ymax = ylims[1]
+  
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim((xmin,xmax))
+    ax.set_ylim((ymin,ymax))
+    
+    ax.set_xlabel("Energy [GeV]")
+    ax.set_ylabel(NORM_LABEL[castroData.fluxType])
+
+    ts_vals = castroData.ts_vals()
+    mles = castroData.mles()
+
+    has_point = ts_vals > TS_thresh
+    has_limit = ~has_point
+    ul_vals = castroData.getLimits(0.05)
+    
+    err_pos = castroData.getLimits(0.32) - mles
+    err_neg = mles - castroData.getLimits(0.32,upper=False) 
+
+    yerr_points = (err_neg[has_point],err_pos[has_point])
+    xerrs = ( castroData.specData.evals - castroData.specData.ebins[0:-1],
+              castroData.specData.ebins[1:] - castroData.specData.evals )
+
+    yerr_limits = (0.5*ul_vals[has_limit],np.zeros((has_limit.sum())))
+
+    ax.errorbar(castroData.specData.evals[has_point],mles[has_point],
+                yerr=yerr_points,fmt='o')
+
+    ax.errorbar(castroData.specData.evals[has_limit],ul_vals[has_limit], 
+                yerr=yerr_limits, lw=1, color='red', ls='none', zorder=1, uplims=True)
+
+    ax.errorbar(castroData.specData.evals[has_limit], ul_vals[has_limit], xerr=(xerrs[0][has_limit],xerrs[1][has_limit]),
+                lw=1.35, ls='none', color='red', zorder=2, capsize=0)
+
+    for spec in specVals:
+        ax.loglog(castro.specData.evals,spec)
+        pass
+
+    return fig,ax
+
+
+if __name__ == "__main__":
+
+    
+    from fermipy import sed
+    from fermipy import roi_model
+    import sys
+
+    if len(sys.argv) == 1:
+        flux_type = 0
+    elif sys.argv[1] == "norm":
+        flux_type = 0
+    elif sys.argv[1] == "flux":
+        flux_type = 1
+    elif sys.argv[1] == "eflux":
+        flux_type = 2
+    elif sys.argv[1] == "npred":
+        flux_type = 3
+    elif sys.argv[1] == "d_flux":
+        flux_type = 4
+    elif sys.argv[1] == "d_eflux":
+        flux_type = 5
+    else:
+        print "Didn't reconginize flux type %s, choose from norm | flux | eflux | npred"%sys.argv[1]
+
+    if flux_type == 0:
+        xlims = (0.,1.)
+        ylims = (1e-5,1e-1)
+    elif flux_type == 1:
+        xlims = (0.,1.)
+        ylims = (1e-13,1e-9)
+    elif flux_type == 2:
+        xlims = (0.,1.)
+        ylims = (1e-8,1e-4)
+    elif flux_type == 3:
+        xlims = (0.,1.)
+        ylims = (1e-1,1e3)
+    elif flux_type == 4:
+        xlims = (0.,1.)
+        ylims = (1e-18,1e-11)
+    elif flux_type == 5:
+        xlims = (0.,1.)
+        ylims = (1e-13,1e-9)
+
+        
+    tscube = sed.TSCube.create_from_fits("tscube_test.fits",flux_type)
+    peaks = tscube.find_and_refine_peaks(10.0,1.0,use_cumul=True)
+
+    max_ts = tscube.tsmap.counts.max()
+    (castro,test_dict) = tscube.test_spectra_of_peak(peaks[0])
+
+    nll = castro[2]
+    fig,ax = plotNLL_v_Flux(nll)
+
+    fig2,ax2,im2 = plotCastro(castro,ylims=ylims,nstep=100)
+        
+    spec_pl = test_dict["PowerLaw"]["Spectrum"]
+    spec_lp = test_dict["LogParabola"]["Spectrum"]
+    spec_pc = test_dict["PLExpCutoff"]["Spectrum"]
+
+    fig3,ax3 = plotSED(castro,ylims=ylims,TS_thresh=4.0,specVals=[spec_pl])        
+
+    result_pl = test_dict["PowerLaw"]["Result"]
+    result_lp = test_dict["LogParabola"]["Result"]
+    result_pc = test_dict["PLExpCutoff"]["Result"]
+    ts_pl = test_dict["PowerLaw"]["TS"]
+    ts_lp = test_dict["LogParabola"]["TS"]
+    ts_pc = test_dict["PLExpCutoff"]["TS"]
+
+    print "TS for PL index = 2:  %.1f"%max_ts
+    print "Cumulative TS:        %.1f"%castro.ts_vals().sum()
+    print "TS for PL index free: %.1f (Index = %.2f)"%(ts_pl,result_pl[1])
+    print "TS for LogParabola:   %.1f (Index = %.2f, Beta = %.2f)"%(ts_lp,result_lp[1],result_lp[2])
+    print "TS for PLExpCutoff:   %.1f (Index = %.2f, E_c = %.2f)"%(ts_pc,result_pc[1],result_pc[2])
+
+     
