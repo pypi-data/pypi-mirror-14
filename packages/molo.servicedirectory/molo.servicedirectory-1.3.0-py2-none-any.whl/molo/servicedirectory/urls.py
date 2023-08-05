c8cb6a069fae75881d02a68952197a7264bb6f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:40 +0000

    Add package molo.servicedirectory/1.3.0/molo.servicedirectory-1.3.0-py2-none-any.whl

---
+++ packages/molo.servicedirectory/molo.servicedirectory-1.3.0-py2-none-any.whl/molo/servicedirectory/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$',
+        views.HomeView.as_view(),
+        name='home'),
+
+    url(r'^location-search/$',
+        views.LocationSearchView.as_view(),
+        name='location-search'),
+
+    url(r'^location-results/$',
+        views.LocationResultsView.as_view(),
+        name='location-results'),
+
+    url(r'^service-results/$',
+        views.ServiceResultsView.as_view(),
+        name='service-results'),
+
+    url(r'^service/(?P<service_id>[0-9]+)/$',
+        views.ServiceDetailView.as_view(),
+        name='service-detail'),
+
+    url(r'^service/(?P<service_id>[0-9]+)/report/$',
+        views.ServiceReportIncorrectInformationView.as_view(),
+        name='service-report'),
+
+    url(r'^service/(?P<service_id>[0-9]+)/rate/$',
+        views.ServiceRateView.as_view(),
+        name='service-rate')
+]
