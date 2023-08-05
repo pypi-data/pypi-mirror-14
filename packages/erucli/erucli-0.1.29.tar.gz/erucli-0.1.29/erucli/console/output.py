7d5bd9c3210198b2c551ff22d30c4cf742a4e740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:04 +0000

    Add package erucli/0.1.29/erucli-0.1.29.tar.gz

---
+++ packages/erucli/erucli-0.1.29.tar.gz/erucli-0.1.29/erucli/console/output.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+import click
+from tabulate import tabulate
+
+from erucli.console.style import info
+
+
+def as_form(title, content):
+    empty = not bool(content)
+    if empty:
+        content = [['' for _ in title]]
+    header, contents = tabulate(content, headers=title).split('-\n', 1) # tricky
+    click.echo(info(header + '-'))
+    if not empty:
+        click.echo(contents)
