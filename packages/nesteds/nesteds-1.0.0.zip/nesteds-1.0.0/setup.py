2352f6a4e33c0ff8e67914598864bd5d39be39d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package nesteds/1.0.0/nesteds-1.0.0.zip

---
+++ packages/nesteds/nesteds-1.0.0.zip/nesteds-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nesteds',
+        version         = '1.0.0',
+        py_modules      = ['nesteds'],
+        author          = 'mSefa',
+        author_email    = 'hfpython@headfirstlabs.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'A simple printer of nested lists',
+
+    )
