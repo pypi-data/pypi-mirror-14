582af3830b54f31a6ef7f5c962975cb439167cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:40 +0000

    Add package nester.g/1.5.0/nester.g-1.5.0.zip

---
+++ packages/nester.g/nester.g-1.5.0.zip/nester.g-1.5.0/nester.py
@@ -0,0 +1,16 @@
+"""Este é o modulo "nester.py", e fornece uma função chamada print_lol()
+que imprime listar que podem ou não incluir listas aninhadas"""
+import sys
+
+def print_lol(the_list, indent=False ,level=0, fh=sys.stdout):
+    """Esta função requer um argumento posicional chamado "the_list), que é
+qualquer lista python (de possíveis listas aninhadas). Cada item de dados na
+lista fornecida é (recursivamente) impresso na tela em sua própria linha"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent ,level+1, fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end ='', file=fh)
+            print(each_item, file=fh)
