97dbcd0779f7d036c747c7fa5d5d8c61e7b42c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package dlcli/0.0.14/dlcli-0.0.14.tar.gz

---
+++ packages/dlcli/dlcli-0.0.14.tar.gz/dlcli-0.0.14/dlcli/cli/__init__.py
@@ -0,0 +1,8 @@
+from .cli import *  # this import needs to be first!
+from .backup import *
+from .pull import *
+from .push import *
+from .restore import *
+from .rm import *
+from .set import *
+from .show import *
