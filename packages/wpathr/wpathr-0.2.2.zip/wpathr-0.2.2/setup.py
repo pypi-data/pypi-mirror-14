6f05f3a84efc5f528801b1f0a41746254f3eecd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:02 +0000

    Add package wpathr/0.2.2/wpathr-0.2.2.zip

---
+++ packages/wpathr/wpathr-0.2.2.zip/wpathr-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='wpathr',
+      version='0.2.2',
+      description='Path optimization tool for Windows',
+      author='Ville Vainio',
+      author_email='vivainio@gmail.com',
+      url='https://github.com/vivainio/wpathr',
+      packages=['wpathr'],
+      install_requires=['pickleshare'],
+      entry_points = {
+        'console_scripts': [
+            'wpp = wpathr.wpathr:main'
+        ]
+      }
+     )
