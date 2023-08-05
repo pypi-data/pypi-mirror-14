6226d3d5188d703efa1ea6a3aea5203352602d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package iterminator/0.1.11/iterminator-0.1.11.tar.gz

---
+++ packages/iterminator/iterminator-0.1.11.tar.gz/iterminator-0.1.11/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='iterminator',
+    version=(open(os.path.join(os.path.dirname(__file__),
+                               'iterminator',
+                               'version.txt'))
+             .read().strip()),
+    author='Dan Davison',
+    author_email='dandavison7@gmail.com',
+    description="Command-line color scheme selector for iTerm2",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'iterminator = iterminator.iterminator:main',
+        ],
+    },
+)
