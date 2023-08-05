fa950b00deeb8ac86993868979f64d27a92bb4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package pyforex/0.0.1/pyforex-0.0.1.tar.gz

---
+++ packages/pyforex/pyforex-0.0.1.tar.gz/pyforex-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyforex',
+        version='0.0.1',
+        description='Python Currency Converter and Forex Engine',
+        url='https://github.com/rayattack/pyforex',
+        author='Raikishi',
+        author_email='r.ortserga@soothdata.com',
+        license='MIT',
+        packages=['pyforex'],
+        zip_safe=False)
