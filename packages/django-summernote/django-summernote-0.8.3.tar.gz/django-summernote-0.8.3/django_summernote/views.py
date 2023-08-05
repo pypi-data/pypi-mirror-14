618597db4f465e84c5c9d239f609a5d4021bee05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package django-summernote/0.8.3/django-summernote-0.8.3.tar.gz

---
+++ packages/django-summernote/django-summernote-0.8.3.tar.gz/django-summernote-0.8.3/django_summernote/views.py
@@ -0,0 +1,70 @@
+from django.http import (
+    HttpResponseBadRequest,
+    HttpResponseServerError,
+    HttpResponseForbidden,
+)
+from django.shortcuts import render
+from django_summernote.settings import summernote_config, get_attachment_model
+
+
+def editor(request, id):
+    return render(
+        request,
+        'django_summernote/widget_iframe_editor.html',
+        {
+            'id_src': id,
+            'id': id.replace('-', '_'),
+            'css': (
+                summernote_config['external_css'] +
+                summernote_config['internal_css'] +
+                summernote_config['internal_css_for_iframe']
+            ),
+            'js': (
+                summernote_config['external_js'] +
+                summernote_config['internal_js']
+            ),
+        }
+    )
+
+
+def upload_attachment(request):
+    if request.method != 'POST':
+        return HttpResponseBadRequest('Only POST method is allowed')
+
+    if summernote_config['attachment_require_authentication']:
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden('Only authenticated users are allowed')
+
+    if not request.FILES.getlist('files'):
+        return HttpResponseBadRequest('No files were requested')
+
+    try:
+        attachments = []
+
+        for file in request.FILES.getlist('files'):
+
+            # create instance of appropriate attachment class
+            klass = get_attachment_model()
+            attachment = klass()
+
+            attachment.file = file
+            attachment.name = file.name
+
+            if file.size > summernote_config['attachment_filesize_limit']:
+                return HttpResponseBadRequest(
+                    'File size exceeds the limit allowed and cannot be saved'
+                )
+
+            # remove unnecessary CSRF token, if found
+            request.POST.pop("csrfmiddlewaretoken", None)
+            kwargs = request.POST
+            # calling save method with attachment parameters as kwargs
+            attachment.save(**kwargs)
+
+            attachments.append(attachment)
+
+        return render(request, 'django_summernote/upload_attachment.json', {
+            'attachments': attachments,
+        })
+    except IOError:
+        return HttpResponseServerError('Failed to save attachment')
