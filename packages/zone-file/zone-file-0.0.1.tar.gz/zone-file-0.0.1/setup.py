eec0379ecb274c78644b08582f8082814cb60a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package zone-file/0.0.1/zone-file-0.0.1.tar.gz

---
+++ packages/zone-file/zone-file-0.0.1.tar.gz/zone-file-0.0.1/setup.py
@@ -0,0 +1,30 @@
+"""
+DNS Zone File
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zone-file',
+    version='0.0.1',
+    url='https://github.com/blockstack/dns-zone-file-py',
+    license='MIT',
+    author='Blockstack Developers',
+    author_email='hello@onename.com',
+    description=("Library for creating and parsing DNS zone files"),
+    keywords='dns zone file zonefile parse create',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
