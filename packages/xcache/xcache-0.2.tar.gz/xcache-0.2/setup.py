1c0da97cb2e8c16c24e51447c6afb0363d8a9a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package xcache/0.2/xcache-0.2.tar.gz

---
+++ packages/xcache/xcache-0.2.tar.gz/xcache-0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='xcache',
+    version='0.2',
+    description='clean caches when needed',
+    author='Sven R. Kunze',
+    author_email='srkunze@mail.de',
+    url='https://github.com/srkunze/xcache',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+
+    py_modules=['xcache'],
+    install_requires=[],
+)
