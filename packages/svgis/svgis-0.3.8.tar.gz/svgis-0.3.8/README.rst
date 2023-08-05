2708350b6a12dce0d286cffe0dc8432ae7be8a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package svgis/0.3.8/svgis-0.3.8.tar.gz

---
+++ packages/svgis/svgis-0.3.8.tar.gz/svgis-0.3.8/README.rst
@@ -0,0 +1,118 @@
+SVGIS
+-----
+
+|Build Status| |Coverage Status|
+
+Create SVG drawings from vector geodata files (SHP, geoJSON, etc).
+
+SVGIS is good for: creating small multiples, combining lots of datasets
+in a sensible projections, and drawing maps with basic styles based on
+classes in the source data. It's perfect for creating base maps for
+editing in a drawing program, and its CSS-based styling gives great
+flexibility for styling.
+
+::
+
+    svgis draw input.shp -o out.svg
+    svgis draw --project utm south_dakota.shp north_dakota.geojson -o dakota.svg
+    svgis draw --style my.css england.shp scotland.shp wales.shp -o great_britain.svg
+
+Documentation: http://pythonhosted.org/svgis
+
+Install
+-------
+
+Requires `fiona <http://pypi.python.org/pypi/fiona>`__, which in turn
+requires GDAL.
+
+Before installing, run the following on OS X: ``brew install gdal``. On
+Linux: ``sudo apt-get -qq install libgdal1-dev``.
+
+Then:
+
+::
+
+    pip install svgis
+
+An additional feature is available if you want to download more
+prerequisites:
+
+::
+
+    # Clip output shapes to the bounding box (smaller files)
+    brew install geos # os x
+    sudo yum/apt-get geos # linux
+    pip install svgis[clip]
+
+Command line tool
+-----------------
+
+The ``svgis`` command line tool includes several utilities. The most
+important is ``svgis draw``, which draws SVG maps based on input geodata
+layers.
+
+Additional commands: \* ``svgis bounds``: get the bounding box for a
+layer in a given projection \* ``svgis graticule``: create a graticule
+(grid) within a given bounds \* ``svgis project``: determine what
+projection ``svgis draw`` will (optionally) generate for given bounding
+box \* ``svgis scale``: change the scale of an existing SVG \*
+``svgis style``: add css styles to an existing SVG
+
+Read the `docs <http://pythonhosted.org/svgis/>`__ for complete
+information on these commands and their options.
+
+Examples
+~~~~~~~~
+
+Draw the outline of the contiguous United States, projected in Albers:
+
+::
+
+    curl -O http://www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_us_nation_20m.zip
+    unzip cb_2014_us_nation_20m.zip
+    svgis draw --crs EPSG:5070 --scale 1000 --bounds -124 20.5 -64 49 cb_2014_us_nation_20m.shp -o us.svg
+
+The next two examples use the `Natural
+Earth <http://naturalearthdata.com>`__ admin-0 data set.
+
+Draw upper income countries in green, low-income countries in blue:
+
+.. code:: css
+
+    /* style.css */
+    .income_grp_5_Low_income {
+        fill: blue;
+    }
+    .income_grp_3_Upper_middle_income {
+        fill: green
+    }
+    .ne_110m_lakes {
+        fill: #09d;
+        stroke: none;
+    }
+
+::
+
+    svgis draw --style style.css --class-fields income_grp ne_110m_admin_0_countries.shp ne_110m_lakes.shp -o out.svg
+
+Draw national boundaries and lakes in Europe using an `Albers
+projection <http://epsg.io/102013>`__, simplifying the output polygons,
+and draw Germany in purple.
+
+.. code:: bash
+
+    svgis draw \ 
+        --crs EPSG:102013 \ 
+        --scale 1000 \ 
+        --simplify 90 \ 
+        --style '.ne_110m_admin_0_countries { fill: tan } .Germany { fill: purple }' \
+        --style '.ne_110m_lakes { fill: #09d; stroke: none; }' \ 
+        --class-fields name \ 
+        --bounds -10 30 40 65 \ 
+        ne_110m_admin_0_countries.shp ne_110m_lakes.shp \ 
+        -o out.svg
+
+.. |Build Status| image:: http://img.shields.io/travis/fitnr/svgis/master.svg?style=flat
+   :target: https://travis-ci.org/fitnr/svgis
+.. |Coverage Status| image:: https://img.shields.io/coveralls/fitnr/svgis/master.svg?style=flat
+   :target: https://coveralls.io/r/fitnr/svgis?branch=master
---
+++ packages/svgis/svgis-0.3.8.tar.gz/svgis-0.3.8/tests/test_svgis.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+import unittest
+import re
+from xml.dom import minidom
+import six
+from svgis import svgis, errors
+
+
+class SvgisTestCase(unittest.TestCase):
+    properties = {
+        'apple': 'fruit',
+        'pear': 1,
+        'kale': 'leafy green',
+    }
+    classes = ('apple', 'potato')
+    file = 'tests/test_data/cb_2014_us_nation_20m.shp'
+
+    polygon = {
+        "properties": properties,
+        "geometry": {
+            "type": "Polygon",
+            "id": "Polygon",
+            "coordinates": [[(0, 0), (1, 0), (0, 1), (0, 0)]]
+        }
+    }
+
+    def setUp(self):
+        self.svgis_obj = svgis.SVGIS(self.file)
+
+    def assertSequenceAlmostEqual(self, a, b):
+        for z in zip(a, b):
+            self.assertAlmostEqual(*z)
+
+    def testSvgisError(self):
+        with self.assertRaises(errors.SvgisError):
+            raise errors.SvgisError('This is an error')
+
+    def testSvgisCreate(self):
+        self.assertEqual(self.svgis_obj.files, [self.file])
+        assert self.svgis_obj._projected_bounds == (None,) * 4
+        assert self.svgis_obj.out_crs is None
+        assert self.svgis_obj.style == svgis.STYLE
+
+        svgis_obj2 = svgis.SVGIS([self.file])
+        assert svgis_obj2.files == [self.file]
+
+        with self.assertRaises(ValueError):
+            svgis.SVGIS(12)
+
+    def testSvgisCompose(self):
+        composed = self.svgis_obj.compose()
+        assert isinstance(composed, six.string_types)
+
+    def testProperty(self):
+        result = svgis._property('apple', self.properties)
+        assert result == 'apple_fruit'
+
+        result = svgis._property('pear', self.properties)
+        assert result == 'pear_1'
+
+        result = svgis._property('apple', {'apple': u'früit'})
+        self.assertEqual(result, u'apple_früit')
+
+    def testGetClasses(self):
+        classes = svgis._get_classes(self.classes, self.properties, 'Name')
+        self.assertIn('apple', classes)
+        self.assertIn('Name', classes)
+
+    def testSvgisClassFields(self):
+        composed = self.svgis_obj.compose(class_fields=('NAME', 'GEOID'))
+
+        matchiter = re.finditer(r'class="(.+?)"', composed)
+        match = next(matchiter)
+
+        self.assertIsNotNone(match)
+        self.assertIn(u'AFFGEOID', match.groups()[0])
+        self.assertIn(u'GEOID', match.groups()[0])
+        self.assertIn(u'NAME', match.groups()[0])
+
+        match = next(matchiter)
+        self.assertIn('GEOID_US', match.groups()[0])
+        self.assertIn('cb_2014_us_nation_20m', match.groups()[0])
+
+    def testCreateClasses(self):
+        classes = svgis._construct_classes(self.classes, self.properties)
+        self.assertEqual(classes, u'apple_fruit potato')
+
+        classes = svgis._construct_classes(self.classes, {'apple': u'fruit'})
+        self.assertEqual(classes, u'apple_fruit potato')
+
+        classes = svgis._construct_classes(self.classes, {'apple': u'früit'})
+        self.assertEqual(classes, u'apple_früit potato')
+
+        classes = svgis._construct_classes(self.classes, {'apple': 1})
+        self.assertEqual(classes, u'apple_1 potato')
+
+    def testCreateClassesMissing(self):
+        classes = svgis._construct_classes(self.classes, {'apple': ''})
+        self.assertEqual(classes, 'apple_ potato')
+
+        classes = svgis._construct_classes(self.classes, {'apple': None})
+        self.assertEqual(classes, 'apple_None potato')
+
+    def testRepr(self):
+        expected = ("SVGIS(files=['{}'], "
+                    'out_crs=None)'.format(self.file))
+        self.assertEqual(str(self.svgis_obj), expected)
+
+    def testDrawGeometry(self):
+        feat = {
+            "geometry": {
+                'type': 'LineString',
+                'coordinates': [[-110.8, 35.3], [-110.9, 35.8], [-110.5, 35.1], [-110.8, 35.3]]
+            },
+            "properties": {
+                'foo': 'bar',
+                'cat': 'meow'
+            }
+        }
+        drawn = self.svgis_obj._feature(feat, [], classes=['foo'], id_field='cat')
+        assert isinstance(drawn, six.string_types)
+
+        self.assertIn('id="meow"', drawn)
+        self.assertIn('class="foo_bar"', drawn)
+
+    def testConstructClasses(self):
+        self.assertEqual(svgis._construct_classes('foo', {'foo': 'bar'}), 'foo_bar')
+        self.assertEqual(svgis._construct_classes(['foo'], {'foo': 'bar'}), 'foo_bar')
+
+        self.assertEqual(svgis._construct_classes(['foo'], {'foo': None}), 'foo_None')
+
+    def testDims(self):
+        bbox = 0, 0, 10, 10
+
+        self.svgis_obj.in_crs = {'init': 'epsg:4269'}
+        self.svgis_obj.out_crs = {'init': 'epsg:4269'}
+
+        a = self.svgis_obj._corners(1, bbox)
+        self.assertSequenceAlmostEqual(a, (0, 0, 10, 10))
+
+        b = self.svgis_obj._corners(0.5, bbox)
+        self.assertSequenceAlmostEqual(b, (0, 0, 5, 5))
+
+        self.svgis_obj.padding = 10
+        c = self.svgis_obj._corners(0.25, bbox)
+        self.assertSequenceAlmostEqual(c, (0, 0, 2.5, 2.5))
+
+        with self.assertRaises((TypeError, ValueError)):
+            self.svgis_obj._corners(0.5, (1, 2, 3))
+
+        d = self.svgis_obj._corners(0.5, None)
+        self.assertSequenceAlmostEqual(d, (0, 0, 0, 0))
+
+    def testSvgisComposeType(self):
+        a = self.svgis_obj.compose(inline_css=True)
+        b = self.svgis_obj.compose(inline_css=False)
+
+        try:
+            try:
+                self.assertIsInstance(a, unicode)
+            except AssertionError:
+                raise AssertionError(type(a))
+
+            try:
+                self.assertIsInstance(b, unicode)
+            except AssertionError:
+                raise AssertionError(type(b))
+
+        except NameError:
+            try:
+                self.assertIsInstance(a, str)
+            except AssertionError:
+                raise AssertionError(type(a))
+
+            try:
+                self.assertIsInstance(b, str)
+            except AssertionError:
+                raise AssertionError(type(b))
+
+        self.assertEqual(type(a), type(b))
+
+    def testMapFunc(self):
+        args = {
+            "scale": 1000,
+            "padding": 10,
+            "inline_css": True,
+            "clip": False,
+            'crs': 'EPSG:32117',
+        }
+        result = svgis.map([self.file], (-80, 40, -71, 45.1), **args)
+
+        self.assertIn(svgis.STYLE, result)
+
+        doc = minidom.parseString(result)
+
+        for poly in doc.getElementsByTagName('polygon'):
+            style = poly.getAttribute('style')
+            assert 'fill:none' in style
+            assert 'stroke-linejoin:round' in style
+
+    def testDrawWithClasses(self):
+        r0 = self.svgis_obj._feature(self.polygon, [], classes=['potato'], id_field=None)
+        assert 'class="potato"' in r0
+
+        r1 = self.svgis_obj._feature(self.polygon, [], classes=['kale'], id_field='apple')
+        assert 'kale_leafy_green' in r1
+        assert 'id="fruit"' in r1
+
+        r2 = self.svgis_obj._feature(self.polygon, [], id_field='pear', classes=['apple', 'pear', 'kale'])
+        self.assertIn('id="_1"', r2)
+        assert 'apple_fruit' in r2
+        assert 'pear_1' in r2
+        assert 'kale_leafy_green' in r2
+
+if __name__ == '__main__':
+    unittest.main()
