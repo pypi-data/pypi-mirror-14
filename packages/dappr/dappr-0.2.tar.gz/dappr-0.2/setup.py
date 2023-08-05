ef58afcb24f9f96b0595f9ae6916efc37b12612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package dappr/0.2/dappr-0.2.tar.gz

---
+++ packages/dappr/dappr-0.2.tar.gz/dappr-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='dappr',
+      version='0.2',
+      description='Django app for registration, with an approval step.',
+      url='https://github.com/millanp/dappr',
+      author='Millan Philipose',
+      author_email='millanmakes@gmail.com',
+      license='MIT',
+      packages=['dappr'],
+      install_requires=[
+          'Django>=1.8',
+      ],
+      zip_safe=False
+)
