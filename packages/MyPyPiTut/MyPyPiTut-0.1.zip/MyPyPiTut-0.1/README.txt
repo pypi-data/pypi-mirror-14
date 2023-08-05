237d0dad17a699f25109029a3c4507c23243e47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:23 +0000

    Add package MyPyPiTut/0.1/MyPyPiTut-0.1.zip

---
+++ packages/MyPyPiTut/MyPyPiTut-0.1.zip/MyPyPiTut-0.1/README.txt
@@ -0,0 +1,1 @@
+this is my first PyPi tut test.
---
+++ packages/MyPyPiTut/MyPyPiTut-0.1.zip/MyPyPiTut-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'MyPyPiTut',
+    package = ['pkgA', 'pkgB'],
+    scripts = ['runner'],
+    version = '0.1',
+    description = 'My first PyPi project',
+    author = 'shuontoku',
+    author_email = 'andychuyt@gmail.com',
+    keywords = ['Good Project'],
+    classifiers = [],
+)
