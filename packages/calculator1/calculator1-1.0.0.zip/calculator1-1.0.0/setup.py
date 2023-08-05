364a8a1129393b5c88081358406d3cc2365d139b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:32 +0000

    Add package calculator1/1.0.0/calculator1-1.0.0.zip

---
+++ packages/calculator1/calculator1-1.0.0.zip/calculator1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'calculator1',
+version = '1.0.0',
+py_modules = ['calculator1'],
+author = 'tamajit',
+author_email = 'tamojitguharoy@gmail.com',
+url = 'http://www.tamojitguharoy.com',
+description = 'A simplecalculatow',
+)
