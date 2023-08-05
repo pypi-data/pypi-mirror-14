aaf7b39e8bc2f6c3ad5572e3a2828543cd208897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package SQLAlchemy-Dao/1.2.4/SQLAlchemy-Dao-1.2.4.tar.gz

---
+++ packages/SQLAlchemy-Dao/SQLAlchemy-Dao-1.2.4.tar.gz/SQLAlchemy-Dao-1.2.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='SQLAlchemy-Dao',
+    version='1.2.4',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Simple wrapper for sqlalchemy.',
+    install_requires=[
+        'decorated',
+        'fixtures2',
+        'inflection',
+        'sqlalchemy',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    url='https://package-insights.appspot.com/packages/sqlalchemy-dao'
+)
