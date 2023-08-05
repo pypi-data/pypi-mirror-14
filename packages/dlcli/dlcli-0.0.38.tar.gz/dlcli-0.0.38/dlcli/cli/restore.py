5d71929d5514aa245f9c6af94546c9ee1a445e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package dlcli/0.0.38/dlcli-0.0.38.tar.gz

---
+++ packages/dlcli/dlcli-0.0.38.tar.gz/dlcli-0.0.38/dlcli/cli/restore.py
@@ -0,0 +1,30 @@
+from ..cli import *
+from ..api import *
+import click
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@cli.group('restore')
+@click.pass_context
+def restore(ctx):
+    """restores backups"""
+
+
+@click.command(short_help="Restore an Account")
+@click.argument('account')
+@click.pass_context
+def account(ctx, account):
+    utils.restore_account(ctx, account)
+
+
+@click.command(short_help="Restore an Organization")
+@click.argument('org')
+@click.pass_context
+def org(ctx, org):
+    utils.restore_org(ctx, org)
+
+
+restore.add_command(account)
+restore.add_command(org)
