f0a3dc9e562773e1561466903fe5d5c3f2712ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:42 +0000

    Add package staffjoy/0.13/staffjoy-0.13.tar.gz

---
+++ packages/staffjoy/staffjoy-0.13.tar.gz/staffjoy-0.13/staffjoy/config.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class DefaultConfig:
+    ENV = "prod"
+    LOG_LEVEL = logging.INFO
+    BASE = "https://www.staffjoy.com/api/v2/"
+
+
+class StageConfig(DefaultConfig):
+    ENV = "stage"
+    BASE = "https://stage.staffjoy.com/api/v2/"
+
+
+class DevelopmentConfig(DefaultConfig):
+    ENV = "dev"
+    LOG_LEVEL = logging.DEBUG
+    BASE = "http://dev.staffjoy.com/api/v2/"
+
+
+config_from_env = {  # Determined in main.py
+    "dev": DevelopmentConfig,
+    "stage": StageConfig,
+    "prod": DefaultConfig,
+}
