de3ef73e76ab28ea18989f9b0a9da9b5cbd1307c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package furi/0.6.3/furi-0.6.3.tar.gz

---
+++ packages/furi/furi-0.6.3.tar.gz/furi-0.6.3/furi/__init__.py
@@ -0,0 +1,15 @@
+""" fURI File access through URIs. """
+
+
+__author__  = "amancevice"
+__email__   = "smallweirdnum@gmail.com"
+__version__ = "0.6.3"
+
+
+from .furimap import chain
+from .utils import add_handler
+from .utils import download
+from .utils import exists
+from .utils import map
+from .utils import open
+from .utils import walk
