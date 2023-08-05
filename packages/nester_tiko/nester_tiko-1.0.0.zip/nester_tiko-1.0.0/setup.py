43535aaf82d4b57b4857af65fc80edba6431c95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package nester_tiko/1.0.0/nester_tiko-1.0.0.zip

---
+++ packages/nester_tiko/nester_tiko-1.0.0.zip/nester_tiko-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nester_tiko',
+        version     = '1.0.0',
+        py_modules  = ['nester'],
+        autor       = 'TBD',
+        autor_email = 'TBD',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested list',
+        )
