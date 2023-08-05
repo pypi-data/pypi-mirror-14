e839df5337688c4bd75cafd3520fee07e1fb6427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package whisker-serial-order/0.4.0/whisker_serial_order-0.4.0.tar.gz

---
+++ packages/whisker-serial-order/whisker_serial_order-0.4.0.tar.gz/whisker_serial_order-0.4.0/whisker_serial_order/version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# whisker_serial_order/version.py
+# Copyright (c) Rudolf Cardinal (rudolf@pobox.com).
+# See LICENSE for details.
+
+
+VERSION = '0.4.0'
+# Use semantic versioning: http://semver.org/
+
+MAX_VERSION_LENGTH = 50
