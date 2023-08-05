be6d0199f9a7fdf0c644fb4aba89010d01dd86a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package b2/0.4.6/b2-0.4.6.tar.gz

---
+++ packages/b2/b2-0.4.6.tar.gz/b2-0.4.6/b2/__init__.py
@@ -0,0 +1,9 @@
+######################################################################
+#
+# File: __init__.py
+#
+# Copyright 2016 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
