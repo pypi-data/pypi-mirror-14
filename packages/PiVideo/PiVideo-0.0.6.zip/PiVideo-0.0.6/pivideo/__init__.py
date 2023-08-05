be35bb379f7172168e39d35e14c6deaf33f9952e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:53 +0000

    Add package PiVideo/0.0.6/PiVideo-0.0.6.zip

---
+++ packages/PiVideo/PiVideo-0.0.6.zip/PiVideo-0.0.6/pivideo/__init__.py
@@ -0,0 +1,16 @@
+#
+# Python library for the Rasperry Pi PiCamera board
+# Copyright (c) 2016 Lintest Systems LLC
+#
+
+from __future__ import (
+    unicode_literals,
+    print_function,
+    division,
+    absolute_import,
+    )
+
+# Make Py2's str equivalent to Py3's
+str = type('')
+
+
