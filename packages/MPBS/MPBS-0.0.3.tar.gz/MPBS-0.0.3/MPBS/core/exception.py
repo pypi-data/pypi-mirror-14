c39b41c04b1e465bfa2bfb161e8087d254827971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:50 +0000

    Add package MPBS/0.0.3/MPBS-0.0.3.tar.gz

---
+++ packages/MPBS/MPBS-0.0.3.tar.gz/MPBS-0.0.3/MPBS/core/exception.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+'''
+Created on 2016年3月3日
+'''
+__author__ = 'chenjian'
+
+class MPBSException(Exception):
+    '''
+    classdocs
+    '''
+
+
+class UnimplementedException(MPBSException):
+    '''
+    '''
+    
+class MPBS404(MPBSException):
+    '''
+    '''        
