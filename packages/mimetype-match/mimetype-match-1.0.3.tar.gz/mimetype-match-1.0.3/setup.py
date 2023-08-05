ffd60784c8c867d255dbbe779e27ce8996020812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package mimetype-match/1.0.3/mimetype-match-1.0.3.tar.gz

---
+++ packages/mimetype-match/mimetype-match-1.0.3.tar.gz/mimetype-match-1.0.3/setup.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+version = '1.0.3'
+
+setup(
+    author='Tim Martin',
+    author_email='tim.martin@vertical-knowledge.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    description='A tool to make finding the appropriate mimetype to return easy',
+    entry_points={
+        'console_scripts': [
+            'mimetype-match = mimetype_match:cli'
+        ]
+    },
+    extras_require={
+        'dev': [
+            'sphinx',
+            'zest.releaser'
+        ]
+    },
+    install_requires=[
+        'six'
+    ],
+    keywords='HTTP python Accept Header HTTP_ACCEPT mimetype match Content-Type content type',
+    name='mimetype-match',
+    py_modules=['mimetype_match'],
+    tests_require=[
+        'mock',
+        'tox',
+        'unittest2'
+    ],
+    test_suite="tests",
+    url='https://github.com/timmartin19/mimetype-match',
+    version=version
+)
