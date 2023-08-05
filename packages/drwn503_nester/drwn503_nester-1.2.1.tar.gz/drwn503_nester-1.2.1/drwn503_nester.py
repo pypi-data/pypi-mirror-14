432b202f50d2e992f16ac46d08a7005af5fa5828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package drwn503_nester/1.2.1/drwn503_nester-1.2.1.tar.gz

---
+++ packages/drwn503_nester/drwn503_nester-1.2.1.tar.gz/drwn503_nester-1.2.1/drwn503_nester.py
@@ -0,0 +1,17 @@
+"""This is the "nester.py" module and it provides one function called print_lol()
+   wich prints lists that may or may not include nested lists."""
+
+
+def print_lol(the_list, level=0):
+	"""This function takes one positional argument called "the_list", which 
+	is any Python list (of - possibly - nested lists). Each data item in the 
+	provided list is (recursively) printed to the screen on it's own line.
+	A second argument called "level" is used to insert tabs when a nested
+	list is found."""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(each_item)
