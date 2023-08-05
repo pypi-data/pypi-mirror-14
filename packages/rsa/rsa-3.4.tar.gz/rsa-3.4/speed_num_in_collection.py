f9c4e09615371c4726a8f4dbd4019d64e7274570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package rsa/3.4/rsa-3.4.tar.gz

---
+++ packages/rsa/rsa-3.4.tar.gz/rsa-3.4/speed_num_in_collection.py
@@ -0,0 +1,24 @@
+"""
+"""
+
+import timeit
+
+setup = '''
+import random
+
+random.seed('slartibartfast')
+numbers = [random.randint(0, 10) for _ in range(100)]
+'''
+
+if __name__ == '__main__':
+    print('List time: ', timeit.timeit(
+        '[number in [2, 3, 5, 7] for number in numbers]',
+        setup=setup))
+
+    print('Set time: ', timeit.timeit(
+        '[number in {2, 3, 5, 7} for number in numbers]',
+        setup=setup))
+
+    print('Tuple time: ', timeit.timeit(
+        '[number in (2, 3, 5, 7) for number in numbers]',
+        setup=setup))
