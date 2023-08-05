cb03230c27ee5eec12a89b96b25cbd941bcd42cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package mownfish/3.1.3/mownfish-3.1.3.tar.gz

---
+++ packages/mownfish/mownfish-3.1.3.tar.gz/mownfish-3.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name="mownfish",
+    description="A Tornado Productional Skeleton",
+    author="Ethan-Zhang",
+    author_email = 'unpeeled_onion@outlook.com',
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    url = 'https://github.com/ethan-zhang/mownfish',
+    download_url = 'https://github.com/Ethan-Zhang/mownfish/tree/v3.1',
+    keywords = ['tornado'],
+    packages=find_packages(),
+    include_package_data=True,
+    version='3.1.3',
+    entry_points = {
+        'console_scripts': [
+            'fishing = mownfish.cmd.fishing:main',
+            ],
+        },
+    )
