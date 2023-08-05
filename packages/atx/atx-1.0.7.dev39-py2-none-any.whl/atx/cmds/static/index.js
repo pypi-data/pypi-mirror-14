2b73756a5739c6afaa1f75598c77ba653eb50eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:01 +0000

    Add package atx/1.0.7.dev39/atx-1.0.7.dev39-py2-none-any.whl

---
+++ packages/atx/atx-1.0.7.dev39-py2-none-any.whl/atx/cmds/static/index.js
@@ -0,0 +1,13 @@
+/* Javascript */
+$(function(){
+  var blocklyDiv = document.getElementById('blocklyDiv');
+  var workspace = Blockly.inject(blocklyDiv,
+    {toolbox: document.getElementById('toolbox')});
+
+  $('.fancybox').fancybox({
+  	
+  })
+})
+
+// var workspace = Blockly.inject('blocklyDiv',
+//       {toolbox: document.getElementById('toolbox')});
