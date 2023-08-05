bb6a5a73f1fa387533cff9556f9cc0426a98b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:19 +0000

    Add package mote/0.0.0/mote-0.0.0.tar.gz

---
+++ packages/mote/mote-0.0.0.tar.gz/mote-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, Extension
+
+setup(
+	name = 'mote',
+	version = '0.0.0',
+	author = "Philip Howard",
+	author_email = "phil@pimoroni.com",
+	url = "",
+	description = "",
+	long_description = "",
+	py_modules = ["mote"],
+	install_requires = [],
+)
