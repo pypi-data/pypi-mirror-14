ac2fbd9916f13463d5e7c66b825c070fd0e766c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package django-licensing/1.0.1/django-licensing-1.0.1.tar.gz

---
+++ packages/django-licensing/django-licensing-1.0.1.tar.gz/django-licensing-1.0.1/licensing/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class License(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    symbols = models.CharField(max_length=5)
+    url = models.URLField(unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return self.url
+
+class Licensed(models.Model):
+    license = models.ForeignKey(License)
+    class Meta:
+        abstract = True
+
+
