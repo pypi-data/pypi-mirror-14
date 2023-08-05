c4473c875d54915490c31e029d45c4feb90bac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:54 +0000

    Add package jiupin/1.0.0/jiupin-1.0.0.zip

---
+++ packages/jiupin/jiupin-1.0.0.zip/jiupin-1.0.0/jiupin/file_helper.py
@@ -0,0 +1,38 @@
+﻿# -*- coding: utf-8 -*-
+# boois flask 框架,作者:周骁鸣 boois@qq.com
+"""file_helper模块用来统一文件的读写操作"""
+import os
+
+
+def write(path, content, override=False):
+    """写入一个文件"""
+    if not override:
+        if os.path.exists(path):
+            return
+
+    # 检测目录是否存在,不存在则创建
+    pdir = os.path.dirname(path)
+    if not os.path.exists(pdir):
+        os.makedirs(pdir)
+
+    with open(path, "w+") as _file:
+        _file.write(content)
+        _file.close()
+
+
+def write_b(path, data):
+    """用二进制流方式写入一个文件"""
+    if data is None or data == "":
+        return
+    with open(path, "wb") as _file:
+        _file.write(data)
+        _file.flush()
+        _file.close()
+
+
+def read(path):
+    """读取一个文件"""
+    with open(path) as _file:
+        content = _file.read()
+        _file.close()
+        return content
---
+++ packages/jiupin/jiupin-1.0.0.zip/jiupin-1.0.0/jiupin/folder_helper.py
@@ -0,0 +1,57 @@
+# coding:utf-8
+'''
+文件夹帮助类
+'''
+import os
+import os.path
+import shutil
+
+
+def copytree(src, dst, symlinks=False):
+    '''
+    复制文件夹,若目标文件夹存在时不抛错误
+    :param src:
+    :param dst:
+    :param symlinks:
+    :return:
+    '''
+    names = os.listdir(src)
+    if not os.path.isdir(dst):
+        os.makedirs(dst)
+
+    errors = []
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks)
+            else:
+                if os.path.isdir(dstname):
+                    os.rmdir(dstname)
+                elif os.path.isfile(dstname):
+                    os.remove(dstname)
+                shutil.copy2(srcname, dstname)
+        except (IOError, os.error) as why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except OSError as err:
+            errors.extend(err.args[0])
+    try:
+        shutil.copystat(src, dst)
+    except WindowsError:
+        # can't copy file access times on Windows
+        pass
+    except OSError as why:
+        errors.extend((src, dst, str(why)))
+    if errors:
+        raise shutil.Error(errors)
+
+
+if __name__ == '__main__':
+    pass
+    # copytree('E:/book', 'E:/newbook')
