d16a492ac4b42461d7179c4223b253bac4245f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:21 +0000

    Add package sentry-phabricator/0.7.0/sentry-phabricator-0.7.0.tar.gz

---
+++ packages/sentry-phabricator/sentry-phabricator-0.7.0.tar.gz/sentry-phabricator-0.7.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+sentry-phabricator
+==================
+
+An extension for Sentry which integrates with Phabricator. Specifically, it allows you to easily create
+Maniphest tasks from events within Sentry.
+
+:copyright: (c) 2011 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+
+tests_require = [
+]
+
+install_requires = [
+    'sentry>=8',
+    'phabricator>=0.6.0,<1.0',
+]
+
+setup(
+    name='sentry-phabricator',
+    version='0.7.0',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://github.com/getsentry/sentry-phabricator',
+    description='A Sentry extension which integrates with Phabricator.',
+    long_description=__doc__,
+    license='BSD',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    entry_points={
+       'sentry.apps': [
+            'phabricator = sentry_phabricator',
+        ],
+       'sentry.plugins': [
+            'phabricator = sentry_phabricator.plugin:PhabricatorPlugin'
+        ],
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
