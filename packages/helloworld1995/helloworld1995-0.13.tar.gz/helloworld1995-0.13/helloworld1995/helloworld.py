0d883599903210498d8f390b7ab7cdd2b9b721ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package helloworld1995/0.13/helloworld1995-0.13.tar.gz

---
+++ packages/helloworld1995/helloworld1995-0.13.tar.gz/helloworld1995-0.13/helloworld1995/helloworld.py
@@ -0,0 +1,17 @@
+""" This module defines all necessary functions."""
+
+def hello_world():
+    """Returns hello world string.
+
+    :returns: str
+
+    """
+    return("Hello, world!")
+
+def print_hello_world():
+    """Prints the hello world string.
+
+    :returns: NoneType
+
+    """
+    print(hello_world())
