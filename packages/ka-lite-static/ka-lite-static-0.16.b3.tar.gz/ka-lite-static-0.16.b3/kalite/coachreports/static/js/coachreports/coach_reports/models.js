f481413d3d98592b4235adfc36a99dcda9fbbdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:16 +0000

    Add package ka-lite-static/0.16.b3/ka-lite-static-0.16.b3.tar.gz

---
+++ packages/ka-lite-static/ka-lite-static-0.16.b3.tar.gz/ka-lite-static-0.16.b3/kalite/coachreports/static/js/coachreports/coach_reports/models.js
@@ -0,0 +1,93 @@
+var Backbone = require("base/backbone");
+var _ = require("underscore");
+var setGetParamDict = require("utils/get_params").setGetParamDict;
+
+var StateModel = Backbone.Model.extend({
+
+    set: function(key, val, options) {
+        if (key === "facility" || key.facility) {
+            this.set({
+                group: undefined,
+                group_name: undefined
+            });
+        }
+        if (key === "facility" || key.facility || key === "group" || key.group) {
+            this.set({
+                topic_ids: undefined
+            });
+        }
+
+        Backbone.Model.prototype.set.call(this, key, val, options);
+    }
+});
+
+var FacilityModel = Backbone.Model.extend();
+
+var GroupModel = Backbone.Model.extend();
+
+var FacilityCollection = Backbone.Collection.extend({
+    url: window.FACILITY_RESOURCE_URL
+});
+
+var GroupCollection = Backbone.Collection.extend({
+    url: window.GROUP_RESOURCE_URL
+});
+
+var CoachReportModel = Backbone.Model.extend({
+    initialize: function(options) {
+        this.facility = options.facility;
+        this.group = options.group;
+        this.start_date = options.start_date;
+        this.end_date = options.end_date;
+        this.topic_ids = options.topic_ids;
+    },
+
+    url: function() {
+        return setGetParamDict(Urls.learner_logs(), {
+            facility_id: this.facility,
+            group_id: this.group,
+            start_date: this.start_date,
+            end_date: this.end_date,
+            topic_ids: this.topic_ids
+        });
+    }
+});
+
+var CoachReportAggregateModel = Backbone.Model.extend({
+    initialize: function(options) {
+        this.facility = options.facility;
+        this.group = options.group;
+        this.start_date = options.start_date;
+        this.end_date = options.end_date;
+        this.topic_ids = options.topic_ids;
+    },
+
+    url: function() {
+        // We want our date ranges to be inclusive, so send the day + 1
+        // to the server in order to do this.
+
+        var end_date;
+        if (this.end_date) {
+            end_date = new Date(this.end_date);
+            end_date.setDate(end_date.getDate() + 1);
+            end_date = end_date.getFullYear() + "/" + (end_date.getMonth() + 1) + "/" + end_date.getDate();
+        }
+        return setGetParamDict(Urls.aggregate_learner_logs(), {
+            facility_id: this.facility,
+            group_id: this.group,
+            start_date: this.start_date,
+            end_date: end_date,
+            topic_ids: this.topic_ids
+        });
+    }
+});
+
+module.exports = {
+    StateModel: StateModel,
+    FacilityModel: FacilityModel,
+    GroupModel: GroupModel,
+    FacilityCollection: FacilityCollection,
+    GroupCollection: GroupCollection,
+    CoachReportModel: CoachReportModel,
+    CoachReportAggregateModel: CoachReportAggregateModel
+};
