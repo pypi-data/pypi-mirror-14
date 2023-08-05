3d198800847a529eaec2b2ebe84698e5b9213d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:08 +0000

    Add package workabilly/0.1.2/workabilly-0.1.2.zip

---
+++ packages/workabilly/workabilly-0.1.2.zip/workabilly-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='workabilly',
+    packages=['workabilly', 'workabilly.base'],
+    version='0.1.2',
+    description='Tools to get a project set up',
+    author='weeska',
+    author_email='weeska@gmail.com',
+    url='https://github.com/weeska/workabilly',
+    download_url='https://github.com/weeska/workabilly/tarball/0.1.2',
+    keywords=['configuration', 'build'],
+    classifiers=[],
+)
