31149643372e22ae67167489532bc19a1bda6a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package neurosynth/0.3.5/neurosynth-0.3.5.tar.gz

---
+++ packages/neurosynth/neurosynth-0.3.5.tar.gz/neurosynth-0.3.5/neurosynth/base/lextab.py
@@ -0,0 +1,10 @@
+# lextab.py. This file automatically created by PLY (version 3.8). Don't edit!
+_tabversion   = '3.8'
+_lextokens    = {'FLOAT', 'OR', 'LT', 'WORD', 'LPAR', 'RT', 'ANDNOT', 'RPAR', 'AND'}
+_lexreflags   = 0
+_lexliterals  = ''
+_lexstateinfo = {'INITIAL': 'inclusive'}
+_lexstatere   = {'INITIAL': [('(?P<t_WORD>[a-zA-Z\\_\\-\\*]+)|(?P<t_FLOAT>[0-9\\.]+)|(?P<t_ANDNOT>\\&\\~)|(?P<t_AND>\\&)|(?P<t_LT>\\<)|(?P<t_LPAR>\\()|(?P<t_RPAR>\\))|(?P<t_RT>\\>)|(?P<t_OR>\\|)', [None, ('t_WORD', 'WORD'), ('t_FLOAT', 'FLOAT'), (None, 'ANDNOT'), (None, 'AND'), (None, 'LT'), (None, 'LPAR'), (None, 'RPAR'), (None, 'RT'), (None, 'OR')])]}
+_lexstateignore = {'INITIAL': ' \t'}
+_lexstateerrorf = {'INITIAL': 't_error'}
+_lexstateeoff = {}
