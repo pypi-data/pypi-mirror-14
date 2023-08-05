73b5a782e9b04b04c834b786dcf602bea45d35eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:00 +0000

    Add package func-timeout/3.0.0/func_timeout-3.0.0.tar.gz

---
+++ packages/func-timeout/func_timeout-3.0.0.tar.gz/func_timeout-3.0.0/README.md
@@ -0,0 +1,59 @@
+# func\_timeout
+Python module to support running any existing function with a given timeout.
+
+
+Package Includes
+----------------
+
+**func\_timeout**
+
+This is the function wherein you pass the timeout, the function you want to call, and any arguments, and it runs it for up to #timeout# seconds, and will return/raise anything the passed function would otherwise return or raise.
+
+	def func_timeout(timeout, func, args=(), kwargs=None):
+		'''
+			func_timeout - Runs the given function for up to #timeout# seconds.
+
+			Raises any exceptions #func# would raise, returns what #func# would return (unless timeout is exceeded), in which case it raises FunctionTimedOut
+
+			@param timeout <float> - Maximum number of seconds to run #func# before terminating
+			@param func <function> - The function to call
+			@param args    <tuple> - Any ordered arguments to pass to the function
+			@param kwargs  <dict/None> - Keyword arguments to pass to the function.
+
+			@raises - FunctionTimedOut if #timeout# is exceeded, otherwise anything #func# could raise will be raised
+
+			@return - The return value that #func# gives
+		'''
+
+**FunctionTimedOut**
+
+Exception raised if the function times out
+
+
+Example
+-------
+So, for esxample, if you have a function "doit('arg1', 'arg2')" that you want to limit to running for 5 seconds, with func\_timeout you can call it like this:
+
+
+	from func_timeout import func_timeout, FunctionTimedOut
+
+	...
+
+	try:
+
+		doitReturnValue = func_timeout(5, doit, args=('arg1', 'arg2'))
+
+	except FunctionTimedOut:
+		print ( "doit('arg1', 'arg2') could not complete within 5 seconds and was terminated.\n")
+	except Exception as e:
+		# Handle any exceptions that doit might raise here
+
+
+Support
+-------
+
+I've tested func\_timeout with python 2.7, 3.4, and 3.5. It should work on other versions as well.
+
+ChangeLog can be found at https://raw.githubusercontent.com/kata198/func_timeout/master/ChangeLog 
+
+Pydoc can be found at: http://htmlpreview.github.io/?https://github.com/kata198/func_timeout/blob/master/doc/func_timeout.html?vers=1
---
+++ packages/func-timeout/func_timeout-3.0.0.tar.gz/func_timeout-3.0.0/func_timeout/dafunc.py
@@ -0,0 +1,66 @@
+'''
+    Copyright (c) 2016 Tim Savannah All Rights Reserved.
+
+    Licensed under the Lesser GNU Public License Version 3, LGPLv3. You should have recieved a copy of this with the source distribution as
+    LICENSE, otherwise it is available at https://github.com/kata198/func_timeout/LICENSE
+'''
+import threading
+import time
+
+from .exceptions import FunctionTimedOut
+from .StoppableThread import StoppableThread
+
+def func_timeout(timeout, func, args=(), kwargs=None):
+    '''
+        func_timeout - Runs the given function for up to #timeout# seconds.
+
+        Raises any exceptions #func# would raise, returns what #func# would return (unless timeout is exceeded), in which case it raises FunctionTimedOut
+
+        @param timeout <float> - Maximum number of seconds to run #func# before terminating
+        @param func <function> - The function to call
+        @param args    <tuple> - Any ordered arguments to pass to the function
+        @param kwargs  <dict/None> - Keyword arguments to pass to the function.
+
+        @raises - FunctionTimedOut if #timeout# is exceeded, otherwise anything #func# could raise will be raised
+
+        @return - The return value that #func# gives
+    '''
+
+    if not kwargs:
+        kwargs = {}
+    if not args:
+        args = ()
+
+    ret = []
+    exception = []
+    isStopped = False
+
+    def funcwrap(args2, kwargs2):
+        try:
+            ret.append( func(*args2, **kwargs2) )
+        except Exception as e:
+            if isStopped is False:
+                # Don't capture stopping exception
+                exception.append(e)
+
+    thread = StoppableThread(target=funcwrap, args=(args, kwargs))
+    thread.daemon = True
+
+    thread.start()
+    thread.join(timeout)
+
+    stopException = None
+    if thread.isAlive():
+        isStopped = True
+        stopException = FunctionTimedOut 
+        thread._stopThread(stopException)
+        thread.join(.1)
+        raise FunctionTimedOut('Function %s (args=%s) (kwargs=%s) timed out after %f seconds.\n' %(func.__name__, str(args), str(kwargs), timeout))
+
+    if exception:
+        raise exception[0]
+
+    if ret:
+        return ret[0]
+
+
