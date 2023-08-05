9652c0a3c93d8324c52a379590d27aadd4f375fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package archimedes/0.1/archimedes-0.1.tar.gz

---
+++ packages/archimedes/archimedes-0.1.tar.gz/archimedes-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='archimedes',
+      version='0.1',
+      description='Test.',
+      url='http://github.com/adrianschneider94/archimedes',
+      author='Adrian Schneider',
+      author_email='post@adrian-schneider.de',
+      license='MIT',
+      packages=['archimedes'],
+      zip_safe=False)
