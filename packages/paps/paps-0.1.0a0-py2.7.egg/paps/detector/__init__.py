fdeb115f8c28397e615533ec27b856725c29708a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package paps/0.1.0a0/paps-0.1.0a0-py2.7.egg

---
+++ packages/paps/paps-0.1.0a0-py2.7.egg/paps/detector/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__ = "d01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2015-16, Florian JUNG"
+__license__ = "All rights reserved"
+__version__ = "0.1.0"
+__date__ = "2016-03-26"
+# Created: 2015-06-10 23:11
+""" GPIO detector """
+import logging
+
+from .gpio import GPIODetector
+
+__all__ = ['gpio']
+logger = logging.getLogger(__name__)
