7bc408489a237474e23a598282a93d35757d5585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package alchemyjsonschema/0.3.3/alchemyjsonschema-0.3.3.tar.gz

---
+++ packages/alchemyjsonschema/alchemyjsonschema-0.3.3.tar.gz/alchemyjsonschema-0.3.3/alchemyjsonschema/command.py
@@ -0,0 +1,134 @@
+# -*- coding:utf-8 -*-
+import inspect
+import sys
+import pkg_resources
+import json
+import argparse
+
+
+from . import (
+    SchemaFactory,
+    AlsoChildrenWalker,
+    OneModelOnlyWalker,
+    SingleModelWalker,
+    HandControlledWalkerFactory,
+    RelationDesicion,
+    ComfortableDesicion
+)
+
+
+def import_symbol(symbol):  # todo cache
+    return pkg_resources.EntryPoint.parse("x=%s" % symbol).load(False)
+
+
+def err(x):
+    sys.stderr.write(x)
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+
+
+def detect_walker(x):
+    if x == "structual":
+        return AlsoChildrenWalker
+    elif x == "noforeignkey":
+        return OneModelOnlyWalker
+    elif x == "foreignkey":
+        return SingleModelWalker
+    elif x == "control":
+        return HandControlledWalkerFactory
+    else:
+        raise Exception(x)
+
+
+def detect_decision(x):
+    if x == "default":
+        return RelationDesicion()
+    elif x == "comfortable":
+        return ComfortableDesicion()
+    else:
+        raise Exception(x)
+
+
+def write_output_file(schema, name, outdir):
+    schemaf = outdir + "/" + name
+    f = open(schemaf, 'w')
+    f.write(schema)
+    f.close()
+
+
+def handle_output(schema, name, outdir=None):
+    if outdir is None:
+        print(schema)
+    else:
+        write_output_file(schema, name, outdir)
+
+
+def is_alchemy_model(maybe_model):
+    return hasattr(maybe_model, "__table__") or hasattr(maybe_model, "__tablename__")
+
+
+def get_model_name_list(module):
+    if hasattr(module, "__all__"):
+        return module.__all__
+    else:
+        return [name for name, value in module.__dict__.items() if is_alchemy_model(value)]
+
+
+def run(walker, model=None, module=None, depth=None,
+        relation_decision=None, outdir=None, definition_name=None):
+    make_schema = SchemaFactory(walker, relation_decision=relation_decision)
+
+    if model is not None:
+        # default behavior
+        schema = make_schema(model, depth=depth)
+        handle_output(json.dumps(schema, indent=2, ensure_ascii=False),
+                      model, outdir=outdir)
+    elif module is not None:
+        # iterate over attributes of module looking for orm objects
+        definitions = {}
+        for name in get_model_name_list(module):
+            basemodel = getattr(module, name)
+            schema = make_schema(basemodel, depth=depth)
+            definitions[schema['title']] = schema
+            if definition_name is None:
+                handle_output(json.dumps(schema, indent=2, ensure_ascii=False),
+                              basemodel.__tablename__ + ".json", outdir=outdir)
+
+        # create a single definitions file, such as a swagger spec might use
+        if definition_name is not None:
+            outdir = outdir or "."
+            for schema in definitions.values():
+                schema.pop('definitions', None)
+            handle_output(json.dumps({"definitions": definitions}, indent=2, ensure_ascii=False),
+                          definition_name, outdir=outdir)
+    else:
+        print("Error: Target was neither a model nor a module.")
+
+
+def main(sys_args=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", help='the module or class to extract schemas from')
+    parser.add_argument("--walker", choices=["noforeignkey", "foreignkey", "structual", "control"], default="structual")
+    parser.add_argument("--decision", choices=["default", "comfortable"], default="default")
+    parser.add_argument("--depth", default=None, type=int)
+    parser.add_argument("--decision-relationship", default="")
+    parser.add_argument("--decision-foreignkey", default="")
+    parser.add_argument("--out-dir", default=None, help='Write output to files in this directory instead of printing.')
+    parser.add_argument("--definitions", default=None, help='Instead of individual files, output all schemas as a single definitions file.')
+    args = parser.parse_args(sys_args)
+    model = None
+    module = None
+    rawtarget = import_symbol(args.target)
+    if inspect.isclass(rawtarget):
+        model = rawtarget
+    else:
+        module = rawtarget
+    walker = detect_walker(args.walker)
+    if walker == HandControlledWalkerFactory:
+        decisions = {k.strip(): "relationship" for k in args.decision_relationship.split(" ")}
+        decisions.update({k.strip(): "foreignkey" for k in args.decision_foreignkey.split(" ")})
+        walker = walker(decisions)
+    return run(walker, model=model,
+               module=module, depth=args.depth,
+               relation_decision=detect_decision(args.decision),
+               outdir=args.out_dir, definition_name=args.definitions)
