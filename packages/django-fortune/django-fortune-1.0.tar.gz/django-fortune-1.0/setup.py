e6ab9fdd5e7b8e13daaf36312af2993c74354c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package django-fortune/1.0/django-fortune-1.0.tar.gz

---
+++ packages/django-fortune/django-fortune-1.0.tar.gz/django-fortune-1.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+import sys
+
+import pypandoc
+
+
+install_requires = ["django>=1.6"]
+
+if sys.version_info[0] == 2:
+    install_requires.append("pathlib2")
+
+try:
+    long_description = pypandoc.convert("README.md", "rst")
+    long_description = long_description.replace("\r", "")
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+    # pandoc is not installed, fallback to using raw contents
+    with io.open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(name="django-fortune",
+      version="1.0",
+      description="A Django template-tag for fortunes.",
+      long_description=long_description,
+      packages=find_packages(),
+      author="Robert Erb",
+      author_email="bob.erb@gmail.com",
+      license="GPL3",
+      keywords="django fortune",
+      url="https://github.com/rerb/django-fortune",
+      install_requires=install_requires,
+      test_suite='runtests.runtests',
+      classifiers=[
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Framework :: Django",
+          "Development Status :: 5 - Production/Stable"
+      ])
