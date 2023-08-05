f8de37ec5715c60898e11523ee5a9916a63dda50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:39 +0000

    Add package greentasks/1.0/greentasks-1.0.zip

---
+++ packages/greentasks/greentasks-1.0.zip/greentasks-1.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+import greentasks as pkg
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+VERSION = pkg.__version__
+
+setup(
+    name='greentasks',
+    version=VERSION,
+    license='BSD',
+    packages=[pkg.__name__],
+    include_package_data=True,
+    long_description=read('README.rst'),
+    install_requires=[
+        'gevent',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
