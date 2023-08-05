808af8d5f36ff3326b4d643d2fd1ca5f92228f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:37 +0000

    Add package arrayfire/3.3.20160320/arrayfire-3.3.20160320.tar.gz

---
+++ packages/arrayfire/arrayfire-3.3.20160320.tar.gz/arrayfire-3.3.20160320/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+#######################################################
+# Copyright (c) 2015, ArrayFire
+# All rights reserved.
+#
+# This file is distributed under 3-clause BSD license.
+# The complete license agreement can be obtained at:
+# http://arrayfire.com/licenses/BSD-3-Clause
+########################################################
+
+from setuptools import setup, find_packages
+
+## TODO:
+## 1) Look for af libraries during setup
+## 2) Include test suite
+
+setup(
+    author="Pavan Yalamanchili",
+    author_email="pavan@arrayfire.com",
+    name="arrayfire",
+    version="3.3.20160320",
+    description="Python bindings for ArrayFire",
+    license="BSD",
+    url="http://arrayfire.com",
+    packages=['arrayfire'],
+)
