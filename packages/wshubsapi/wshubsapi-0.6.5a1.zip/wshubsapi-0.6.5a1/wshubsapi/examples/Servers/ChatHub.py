b511a46c23b87b55278d14ada8cace44b28db106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package wshubsapi/0.6.5a1/wshubsapi-0.6.5a1.zip

---
+++ packages/wshubsapi/wshubsapi-0.6.5a1.zip/wshubsapi-0.6.5a1/wshubsapi/examples/Servers/ChatHub.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from wshubsapi.Hub import Hub
+
+
+class ChatHub(Hub):
+    def sendToAll(self, name, _sender, message="hello"):  # _sender is an automatically passed argument
+        ### we can call a sender's function in a intuitive way (the function has to be defined in the client):
+        # _sender.onMessage(name,message)
+        ### call function from all clients
+        # self.allClients.onMessage(name,message)
+        ### call function from all clients but the sender
+        otherClients = self._getClientsHolder().getOtherClients(_sender)
+        futures = otherClients.onMessage(name, message)
+        print futures[0].result()
+        ### or call function from a selection of clients
+        # self.getClients(lambda x:x.ID > 4).onMessage(name,message)
+        return len(otherClients)
+
+    @staticmethod
+    def staticFunc():
+        pass
+
+    @classmethod
+    def classMethod(cls):
+        pass
