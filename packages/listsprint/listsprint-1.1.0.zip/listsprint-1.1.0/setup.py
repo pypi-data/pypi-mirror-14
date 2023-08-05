8bb47a1bf972f28c1bf67de792336615f076d868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:22 +0000

    Add package listsprint/1.1.0/listsprint-1.1.0.zip

---
+++ packages/listsprint/listsprint-1.1.0.zip/listsprint-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = "listsprint",
+    version     = "1.1.0",
+    py_modules  = ["listsprint"],
+    author      = "eugurlubaylar",
+    author_email= "eugurlubaylar@hotmail.com",
+    url         = "http://www.eugurlubaylar.com",
+    description = "A simple printer of nested lists",
+    )
