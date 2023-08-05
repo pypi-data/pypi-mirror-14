e28771358f95117e7a4e6a484fd3aa4af486855e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package serverless-env/0.1/serverless-env-0.1.tar.gz

---
+++ packages/serverless-env/serverless-env-0.1.tar.gz/serverless-env-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def read_requirements(path):
+    requirements = []
+    validate = lambda req: req and not req.startswith('-')
+    with open(path, 'rb') as fd:
+        requirements = [req.strip() for req in fd.readlines() if validate(req)]
+    return requirements
+
+install_requires = read_requirements('requirements.txt')
+
+setup(
+    name='serverless-env',
+    version='0.1',
+    url='http://github.com/cscetbon/serverless-helpers-py',
+    license='BSD',
+    description='Helper functions for Serverless',
+    author='Cyril Scetbon',
+    author_email='cscetbon@xxxx.com',
+    packages=['serverless_env'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
