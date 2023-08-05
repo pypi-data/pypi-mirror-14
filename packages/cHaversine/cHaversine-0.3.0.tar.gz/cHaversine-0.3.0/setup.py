f344fd058fc275de702d26e05839863d42ca2367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:45 +0000

    Add package cHaversine/0.3.0/cHaversine-0.3.0.tar.gz

---
+++ packages/cHaversine/cHaversine-0.3.0.tar.gz/cHaversine-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import Extension
+
+setup(
+    name = 'cHaversine',
+    packages = ['cHaversine'],
+    version = '0.3.0',
+    description = 'Fast haversine calculation',
+    author = 'Eric Jiang',
+    author_email = 'eric@doublemap.com',
+    url = 'https://github.com/doublemap/cHaversine',
+    keywords = ['math', 'geo', 'cython'],
+    classifiers = ['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
+    ext_modules = [Extension("cHaversine", ["cHaversine/cHaversine.c"])]
+)
