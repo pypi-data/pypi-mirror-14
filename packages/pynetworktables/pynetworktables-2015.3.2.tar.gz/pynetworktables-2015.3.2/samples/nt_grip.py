ee420b5be375ddb3224539cbb8375642d38db774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package pynetworktables/2015.3.2/pynetworktables-2015.3.2.tar.gz

---
+++ packages/pynetworktables/pynetworktables-2015.3.2.tar.gz/pynetworktables-2015.3.2/samples/nt_grip.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# This is a NetworkTables client (eg, the DriverStation/coprocessor side).
+# You need to tell it the IP address of the NetworkTables server (the
+# robot or simulator).
+#
+# When running, this will continue incrementing the value 'dsTime', and the
+# value should be visible to other networktables clients and the robot.
+#
+
+import sys
+import time
+from networktables import NetworkTable, NumberArray
+
+# To see messages from networktables, you must setup logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+if len(sys.argv) != 2:
+    print("Error: specify an IP to connect to!")
+    exit(0)
+
+ip = sys.argv[1]
+
+NetworkTable.setIPAddress(ip)
+NetworkTable.setClientMode()
+NetworkTable.initialize()
+
+sd = NetworkTable.getTable("/GRIP/myContoursReport")
+na = NumberArray()
+#sna.append(1)
+
+#sd.putValue('centerX', na)
+
+i = 0
+
+while True:
+    try:
+        ret = sd.retrieveValue("centerX", na)
+        print(ret, na)
+    except KeyError:
+        print('centerX: N/A')
+
+    time.sleep(1)
+    i += 1
+
