a3cafc93cb54b8001a65da1f0c525f1277d267d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package mypackage234234234/0.1/mypackage234234234-0.1.tar.gz

---
+++ packages/mypackage234234234/mypackage234234234-0.1.tar.gz/mypackage234234234-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackage234234234',
+  packages = ['mypackage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Aaron Rosen',
+  author_email = 'aaronorosen@gmail.com',
+  url = 'https://github.com/aaronorosen/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/aaronorosen/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
