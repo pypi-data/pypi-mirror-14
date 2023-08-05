ef4ca9dc54d6a7fade04ee3b8ba05c9fb3ec3226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package spoppy/1.2.2/spoppy-1.2.2.tar.gz

---
+++ packages/spoppy/spoppy-1.2.2.tar.gz/spoppy-1.2.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+
+from setuptools import setup
+from spoppy import get_version
+
+if sys.version_info < (3, 3):
+    print('You need at least python 3.3')
+    sys.exit(1)
+
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='spoppy',
+    version=get_version(),
+    description='A lightweight spotify CLI',
+    long_description=long_description,
+    author='Sindri Guðmundsson',
+    author_email='sindrigudmundsson@gmail.com',
+    url='https://github.com/sindrig/spoppy',
+    licence='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License'
+    ],
+    keywords='spoppy spotify cli',
+    packages=['spoppy'],
+    test_suite='nose.collector',
+    install_requires=required,
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[
+        'scripts/spoppy'
+    ]
+)
