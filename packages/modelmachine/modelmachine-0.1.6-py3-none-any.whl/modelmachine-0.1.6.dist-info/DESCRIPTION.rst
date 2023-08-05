e9b886897e81bd9ea0340057cb12d557b29dee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package modelmachine/0.1.6/modelmachine-0.1.6-py3-none-any.whl

---
+++ packages/modelmachine/modelmachine-0.1.6-py3-none-any.whl/modelmachine-0.1.6.dist-info/DESCRIPTION.rst
@@ -0,0 +1,19 @@
+Read the doc: <https://github.com/vslutov/modelmachine>
+
+Home-page: https://github.com/vslutov/modelmachine
+Author: vslutov
+Author-email: vslutov@yandex.ru
+License: WTFPL
+Description: UNKNOWN
+Platform: any
+Classifier: Development Status :: 2 - Pre-Alpha
+Classifier: Environment :: Console
+Classifier: Intended Audience :: Education
+Classifier: Natural Language :: Russian
+Classifier: Natural Language :: English
+Classifier: Operating System :: Unix
+Classifier: Operating System :: Microsoft :: Windows
+Classifier: Programming Language :: Python :: 3 :: Only
+Classifier: Topic :: Education
+Classifier: Topic :: Utilities
+Classifier: Topic :: Scientific/Engineering
---
+++ packages/modelmachine/modelmachine-0.1.6-py3-none-any.whl/modelmachine/tests/test_ide.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Test case for IDE."""
+
+from unittest.mock import create_autospec
+
+from pytest import raises
+
+from modelmachine import ide
+from modelmachine.cpu import AbstractCPU
+
+def test_get_cpu():
+    """Test define cpu method."""
+    ide.CPU_LIST = {"abstract_cpu_test": create_autospec(AbstractCPU, True)}
+
+    with raises(ValueError):
+        ide.get_cpu(["not_found_cpu", "[config]", "[code]", "00 00", "[input]"], False)
+
+    with raises(ValueError):
+        ide.get_cpu(["[config]", "[code]", "00 00", "[input]"], False)
+
+    cpu = ide.get_cpu(["abstract_cpu_test", "[config]", "key=value",
+                       "[code]", "00 00", "99 00", "[input]"], False)
+    assert isinstance(cpu, AbstractCPU)
+
