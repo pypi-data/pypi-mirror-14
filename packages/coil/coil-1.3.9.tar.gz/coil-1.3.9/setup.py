74f348358e0a8d208e4107c2d15c915680632817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:10 +0000

    Add package coil/1.3.9/coil-1.3.9.tar.gz

---
+++ packages/coil/coil-1.3.9.tar.gz/coil-1.3.9/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import io
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fh:
+    dependencies = [l.strip() for l in fh]
+
+setup(name='coil',
+      version='1.3.9',
+      description='A user-friendly CMS frontend for Nikola.',
+      keywords='coil,nikola,cms',
+      author='Chris Warrick, Roberto Alsina, Henry Hirsch et al.',
+      author_email='chris@getnikola.com',
+      url='https://github.com/getnikola/coil',
+      license='MIT',
+      long_description=io.open('./README.rst', 'r', encoding='utf-8').read(),
+      platforms='any',
+      zip_safe=False,
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4'],
+      packages=['coil'],
+      install_requires=dependencies,
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'coil = coil.__main__:main',
+          ]
+      },
+      )
