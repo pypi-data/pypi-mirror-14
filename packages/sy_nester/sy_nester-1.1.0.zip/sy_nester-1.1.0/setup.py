0528d68747d22823f8dc5e7e2fa71ee11947f684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package sy_nester/1.1.0/sy_nester-1.1.0.zip

---
+++ packages/sy_nester/sy_nester-1.1.0.zip/sy_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'sy_nester',
+        version = '1.1.0',
+        py_modules = ['sy_nester'],
+        author = 'syang',
+        author_email = 'sangyang001@gmail.com',
+        url = 'http://www.renren.com/337884725',
+        description = 'A simple printer of nested lists'
+      )
