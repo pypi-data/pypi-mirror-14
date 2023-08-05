6889cdc826055cfbb5d4fe8ee2f938c8be68025e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package lsa-program/0.2.1/lsa-program-0.2.1.tar.gz

---
+++ packages/lsa-program/lsa-program-0.2.1.tar.gz/lsa-program-0.2.1/lsa/util.py
@@ -0,0 +1,91 @@
+import bisect
+import collections
+import functools
+import string
+
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+
+STOPWORDS = sorted(stopwords.words())
+
+STEMER = SnowballStemmer('spanish')
+
+PUNCTUATION = str.maketrans(dict.fromkeys(string.punctuation))
+
+
+def xml_to_text(func):
+    '''
+    Transforms a function that would return an XML element into a function
+    that returns the text content of the XML element as a string.
+    '''
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result is None:
+            return ''
+        return result.firstChild.nodeValue
+    return inner
+
+
+def gen_to_list(func):
+    '''
+    Transforms a function that would return a generator into a function that
+    returns a list of the generated values, ergo, do not use this decorator
+    with infinite generators.
+    '''
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        return list(func(*args, **kwargs))
+    return inner
+
+
+def isi_text_to_dic(text):
+    '''
+    This function takes in any ISI WOS plain text formatted string and turns it
+    into a dictionary where the keys are the two letter leading keys and the
+    values are a list of the strings under that key.
+    '''
+    fields = collections.defaultdict(list)
+    curr = ""
+    for line in text.split('\n'):
+        name = line[:2]
+        value = line[3:]
+        if not name.isspace():
+            curr = name
+        if not curr.isspace():
+            fields[curr].append(value)
+    return fields
+
+
+def is_stopword(word):
+    '''
+    Checks if a word is a stopword using the bisection method over a list of
+    known stopwords stored in a global variable.
+    '''
+    # TODO A hash-map is more suitable for this
+    # return word in STOPWORDS
+    _word = word.lower()
+    index = bisect.bisect_left(STOPWORDS, _word)
+    return STOPWORDS[index] == _word
+
+
+def normalize(word):
+    '''
+    Filters out the punctuation from a word and then applies a steemer, and
+    yields the steemed word.
+    '''
+    # TODO This obviously may take a word normalizer class, to control the
+    #      language and other normalization parameters
+    _word = word.translate(PUNCTUATION)
+    return STEMER.stem(_word)
+
+
+def to_list(obj):
+    '''
+    Transforms a non iterable object into a singleton list, or an iterable
+    into a list.
+    '''
+    if isinstance(obj, list) or isinstance(obj, tuple):
+        return list(obj)
+    return [obj]
---
+++ packages/lsa-program/lsa-program-0.2.1.tar.gz/lsa-program-0.2.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='lsa-program',
+    version='0.2.1',
+    author='Oscar David Arbeláez <@odarbelaeze>, German Augusto Osorio',
+    author_email='odarbelaeze@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'nltk>=3.1',
+        'pymongo>=3.2',
+        'numpy>=1.9.2',
+        'scipy>=0.16.0',
+        'marshmallow>=2.4.2',
+        'click>=6.2',
+        'bibtexparser>=0.6.2',
+    ],
+    tests_require=[
+        'cov-core>=1.15.0',
+        'coverage>=3.7.1',
+        'py>=1.4.29',
+        'pytest>=2.8.0',
+        'pytest-cov>=1.8.1',
+    ],
+    setup_requires=[
+        'pytest-runner>=2.7,<3dev',
+    ],
+    entry_points='''
+    [console_scripts]
+    lsapopulate=lsa.scripts.populate:lsapopulate
+    lsamodel=lsa.scripts.model:lsamodel
+    lsaquery=lsa.scripts.query:lsaquery
+    ''',
+    url='https://github.com/odarbelaeze/lsa-program',
+    download_url='https://github.com/odarbelaeze/lsa-program/targall/0.2.1',
+    keywords=['lsa', 'search', 'search engine', 'semantics', ],
+    description='A latent semantic search engine implementation',
+)
