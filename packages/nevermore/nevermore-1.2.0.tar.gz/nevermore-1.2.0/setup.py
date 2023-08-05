e8006952b1aaa253809b1e00709d21de320c849d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package nevermore/1.2.0/nevermore-1.2.0.tar.gz

---
+++ packages/nevermore/nevermore-1.2.0.tar.gz/nevermore-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name         = 'nevermore',
+	version      = '1.2.0',
+	py_modules   = ['nester'],
+	author       = 'summy',
+	author_email = '517692727@qq.com',
+	description  = 'A simple printer of nested lists',
+	)
