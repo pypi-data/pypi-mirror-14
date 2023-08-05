042cd96c8598788fbf12161620f1ffa39d8acdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package cheesepi/1.9.17/cheesepi-1.9.17.tar.gz

---
+++ packages/cheesepi/cheesepi-1.9.17.tar.gz/cheesepi-1.9.17/cheesepi/tasks/__init__.py
@@ -0,0 +1,21 @@
+
+from Task import Task
+from Dummy import Dummy
+from Upload import Upload
+from Upgradecode import Upgradecode
+from Status import Status
+from Ping import Ping
+from Httping import Httping
+from Traceroute import Traceroute
+from Dash import Dash
+from DNS import DNS
+from Throughput import Throughput
+from iPerf import iPerf
+from Wifi import Wifi
+
+try:
+	from Updatetasks import Updatetasks
+	from Beacon import Beacon
+except:
+	print "Warning: Problems importing Beacon/Updatetasks, python Twisted probably not installed."
+
