fa915ce158b6858af5582359a17adbed5c824df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package serenytics/0.3.3/serenytics-0.3.3.tar.gz

---
+++ packages/serenytics/serenytics-0.3.3.tar.gz/serenytics-0.3.3/tests/test_source.py
@@ -0,0 +1,71 @@
+import pytest
+import pandas as pd
+
+
+class TestDataSource(object):
+
+    @pytest.fixture(autouse=True)
+    def set_test_client(self, serenytics_client, push_data_source):
+        self._client = serenytics_client
+        self._data_source = push_data_source
+
+    def test_reload_data(self):
+        self._data_source.reload_data([
+            {'year': 2015, 'quarter': 'Q1', 'sales': 120},
+            {'year': 2015, 'quarter': 'Q2', 'sales': 80},
+            {'year': 2015, 'quarter': 'Q4', 'sales': 25},
+            {'year': 2014, 'quarter': 'Q2', 'sales': 85},
+        ])
+
+        data = self._data_source.get_data()
+        assert sorted(data.columns) == ['quarter', 'sales', 'year']
+        assert sorted(data.rows) == [[u'Q1', 120, 2015],
+                                     [u'Q2', 80, 2015],
+                                     [u'Q2', 85, 2014],
+                                     [u'Q4', 25, 2015]]
+
+    def test_reload_data_from_array(self):
+        columns = ['year', 'quarter', 'sales']
+        rows = [[2015, 'Q1', 120],
+                [2015, 'Q2', 80],
+                [2015, 'Q4', 25],
+                [2014, 'Q2', 85]]
+
+        self._data_source.reload_data_from_array(columns, rows)
+
+        data = self._data_source.get_data()
+        assert sorted(data.columns) == ['quarter', 'sales', 'year']
+        assert sorted(data.rows) == [[u'Q1', 120, 2015],
+                                     [u'Q2', 80, 2015],
+                                     [u'Q2', 85, 2014],
+                                     [u'Q4', 25, 2015]]
+
+    def test_reload_data_from_dataframe(self):
+        columns = ['year', 'quarter', 'sales']
+        rows = [[2015, 'Q1', 120],
+                [2015, 'Q2', 80],
+                [2015, 'Q4', 25],
+                [2014, 'Q2', 85]]
+
+        df = pd.DataFrame(rows, columns=columns)
+        self._data_source.reload_data_from_dataframe(df)
+
+        data = self._data_source.get_data()
+        assert sorted(data.columns) == ['quarter', 'sales', 'year']
+        assert sorted(data.rows) == [[u'Q1', 120, 2015],
+                                     [u'Q2', 80, 2015],
+                                     [u'Q2', 85, 2014],
+                                     [u'Q4', 25, 2015]]
+
+    def test_batch_push_data(self):
+        # clear the data source by reloading empty data
+        self._data_source.reload_data([])
+
+        self._data_source.batch_push_data([
+            {'year': 2015, 'quarter': 'Q2', 'sales': 80},
+            {'year': 2015, 'quarter': 'Q4', 'sales': 25},
+        ])
+
+        data = self._data_source.get_data()
+        assert sorted(data.rows) == [[u'Q2', 80, 2015],
+                                     [u'Q4', 25, 2015]]
