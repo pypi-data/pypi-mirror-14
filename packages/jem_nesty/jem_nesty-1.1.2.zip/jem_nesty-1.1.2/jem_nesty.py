45824aba14994dddd4d7ab761859d10c71578fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package jem_nesty/1.1.2/jem_nesty-1.1.2.zip

---
+++ packages/jem_nesty/jem_nesty-1.1.2.zip/jem_nesty-1.1.2/jem_nesty.py
@@ -0,0 +1,16 @@
+""" This Nesty module contains the print_lol function which lets you display
+the data in list and inner lists of the list"""
+
+def print_lol(arg1,level=0):
+        """This print_lol function takes a list in and checks for
+        lists with in lists and processes the inner list by calling the same
+        function recursively and if it is not an inner list then it simply
+        prints the data. The inner list is tab indented when the second argument
+        is provided"""
+        for each_item in arg1:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level)
+                else:
+                        for num in range(level):
+                                print('\t',end='')
+                        print(each_item)
