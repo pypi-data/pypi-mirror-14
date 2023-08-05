2472fc68670e210728195c3bf221af7a00fc12ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package django-mentions/0.1.10/django-mentions-0.1.10.tar.gz

---
+++ packages/django-mentions/django-mentions-0.1.10.tar.gz/django-mentions-0.1.10/mentions/views.py
@@ -0,0 +1,32 @@
+import json
+
+from django.http import HttpResponse
+
+
+from .registry import providers
+
+
+def autocomplete(request):
+    # to be haystack+solr friendly we must strip and join spaces
+    term = request.GET.get('term', '').strip()
+    term = ' '.join(term.split())
+
+    collection_name = request.GET.get('mentions', 'default')
+
+    ret = []
+    if term:
+        try:
+            collection = providers.get_collection(collection_name)
+        except KeyError:
+            collection = providers.get_collection('default')
+
+        for provider in collection:
+            objs = provider.search(request, term)
+            for obj in objs:
+                ret.append({
+                    'value': provider.get_title(obj),
+                    'image': provider.get_image(obj),
+                    'uid': '%s:%s' % (provider.name, obj.pk),
+                })
+
+    return HttpResponse(json.dumps(ret), mimetype='application/json')
