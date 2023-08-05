90ff7b86fcd0d7862c85b95572fa09e9b0add8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:48 +0000

    Add package toughcli/0.0.7/toughcli-0.0.7-py2-none-any.whl

---
+++ packages/toughcli/toughcli-0.0.7-py2-none-any.whl/toughcli-0.0.7.dist-info/DESCRIPTION.rst
@@ -0,0 +1,36 @@
+# toughcli
+
+toughcli 是一个基于toughradius以及相关软件的工具包。
+
+## 运行环境
+
+    - Linux/Mac OSX/BSD
+    - Python 2.7/PyPy
+    - Click
+
+## 安装
+
+    pip install toughcli
+
+## 使用说明
+
+    toughcli --help
+
+
+    Usage: toughcli [OPTIONS] COMMAND [ARGS]...
+
+    Options:
+      --version
+      --help     Show this message and exit.
+
+    Commands:
+      docker
+      mysql
+      radius
+      redis
+      upgrade
+
+
+
+
+
---
+++ packages/toughcli/toughcli-0.0.7-py2-none-any.whl/toughcli/service/docker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+import os
+
+def auto_install():
+    os.system("curl -sSL https://get.daocloud.io/docker | sh")
+    os.system("curl -L https://get.daocloud.io/docker/compose/releases/download"
+              "/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose")
+    os.system("ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose")
+    os.system("service docker start")
+
+
+
+
