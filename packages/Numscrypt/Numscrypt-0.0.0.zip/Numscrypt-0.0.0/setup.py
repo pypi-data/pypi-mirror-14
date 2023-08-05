3e2b8da91ede147cfb85a201343db65b15770f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package Numscrypt/0.0.0/Numscrypt-0.0.0.zip

---
+++ packages/Numscrypt/Numscrypt-0.0.0.zip/Numscrypt-0.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+def read (*paths):
+	with open (os.path.join (*paths), 'r') as aFile:
+		return aFile.read()
+
+setup (
+	name = 'Numscrypt',
+	version = '0.0.0',
+	description = 'Purely experimental, overly optimistic attempt to port a microscopic part of NumPy to Transcrypt using JS typed arrays',
+	long_description = (
+		read ('README.rst') + '\n\n' +
+		read ('Numscrypt/license_reference.txt')
+	),
+	keywords = ['transcrypt', 'numpy', 'browser'],
+	url = 'https://github.com/JdeH/Numscrypt',	
+	license = 'Apache 2.0',
+	author = 'Jacques de Hooge',
+	author_email = 'jacques.de.hooge@qquick.org',
+	packages = ['Numscrypt'],
+	include_package_data = True,
+	classifiers = [
+		'Development Status :: 1 - Planning',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: Apache Software License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.5',
+	],
+)
