16ce661044d3b466c77c80bc21e12bae3768148c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package ezzybot/1.0.2/ezzybot-1.0.2.tar.gz

---
+++ packages/ezzybot/ezzybot-1.0.2.tar.gz/ezzybot-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+#Don't actually use this ATM, this is only so I can keep track of install requires
+
+
+setup(name='ezzybot',
+      version='1.0.2',
+      description="Python IRC framework",
+      url='https://ezzybot.zzirc.xyz',
+      author='EzzyBot team',
+      author_email='me@lukej.me',
+      license='GNU',
+      packages=find_packages(),
+      install_requires=['thingdb'],
+      include_package_data=True,
+      zip_safe=False)
