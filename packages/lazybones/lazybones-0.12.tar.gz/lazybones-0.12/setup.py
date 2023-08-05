e13c54e15f2187d9387bc711480e3afee804ba18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package lazybones/0.12/lazybones-0.12.tar.gz

---
+++ packages/lazybones/lazybones-0.12.tar.gz/lazybones-0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lazybones',
+	version='0.12',
+	description='Tool for scaffolding projects in a variety of languages',
+	url='https://github.com/jacobmilligan/Lazybones.git',
+	download_url='https://github.com/jacobmilligan/Lazybones/tarball/0.1',
+	author='Jacob Milligan',
+	license='LICENSE',
+	packages=['lazybones'],
+	zip_safe=False)
