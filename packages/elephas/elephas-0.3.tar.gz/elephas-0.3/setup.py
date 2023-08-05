e21deec08723809832fff1ebfc2361b6734925bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package elephas/0.3/elephas-0.3.tar.gz

---
+++ packages/elephas/elephas-0.3.tar.gz/elephas-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='elephas',
+      version='0.3',
+      description='Deep learning on Spark with Keras',
+      url='http://github.com/maxpumperla/elephas',
+      download_url='https://github.com/maxpumperla/elephas/tarball/0.3',
+      author='Max Pumperla',
+      author_email='max.pumperla@googlemail.com',
+      install_requires=['keras', 'hyperas'],
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
