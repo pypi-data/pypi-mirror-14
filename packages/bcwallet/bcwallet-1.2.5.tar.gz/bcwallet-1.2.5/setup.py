cab47ba84093b20a045e3275c8eda9ddf3fec5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package bcwallet/1.2.5/bcwallet-1.2.5.tar.gz

---
+++ packages/bcwallet/bcwallet-1.2.5.tar.gz/bcwallet-1.2.5/setup.py
@@ -0,0 +1,24 @@
+# https://youtu.be/kNke39OZ2k0?t=65
+
+from setuptools import setup
+
+setup(
+        name='bcwallet',
+        version='1.2.5',
+        description='Simple BIP32 HD cryptocurrecy command line wallet',
+        author='Michael Flaxman',
+        author_email='mflaxman+blockcypher@gmail.com',
+        url='https://github.com/blockcypher/bcwallet/',
+        py_modules=['bcwallet'],
+        install_requires=[
+            'clint==0.4.1',
+            'blockcypher==1.0.69',
+            'bitmerchant==0.1.8',
+            'tzlocal==1.2',
+            ],
+        entry_points='''
+            [console_scripts]
+            bcwallet=bcwallet:invoke_cli
+        ''',
+        packages=['bcwallet'],
+        )
