68c20723b440fbd0106df3b8d1c2366be44f9303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package maxmao_nester/1.8.0/maxmao_nester-1.8.0.zip

---
+++ packages/maxmao_nester/maxmao_nester-1.8.0.zip/maxmao_nester-1.8.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          ='maxmao_nester',
+        version       ='1.8.0',
+        py_modules    =['maxmao_nester'],
+        author        ='maxmao',
+        author_email  ='277877044@qq.com',
+        url           ='http://www.headfirstlabs.com',
+        description   ='A simple printer of nested lists',
+    )
