83c5b90c331c16a2d58e20ee66c0f281126c81a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package restless-haystack/0.1.0/restless-haystack-0.1.0.tar.gz

---
+++ packages/restless-haystack/restless-haystack-0.1.0.tar.gz/restless-haystack-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='restless-haystack',
+    version='0.1.0',
+    packages=['restless_haystack'],
+    install_requires=['Django', 'django-haystack', 'restless'],
+    url='https://github.com/CraveFood/restless-haystack',
+    license='MIT',
+    author='Bruno Marques',
+    author_email='bruno@bmarques.net',
+    description='Integration of the Restless and Haystack libraries for Django',
+    long_description=open('README.md', 'r').read(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
