2e91796ce37ad503e88a4ecd3d739dc09cedbb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:44 +0000

    Add package dill_list/1.3.5/dill_list-1.3.5.tar.gz

---
+++ packages/dill_list/dill_list-1.3.5.tar.gz/dill_list-1.3.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'dill_list',
+	version = '1.3.5',
+	py_modules = ['dill_list'],
+	author = 'd1ll1nger',
+	author_email = 'hfpython@headfirstlabs.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
