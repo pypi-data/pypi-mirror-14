d395db3a6862f8e42fed68bc171633a7350e8146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:13 +0000

    Add package gazelib/1.2.3/gazelib-1.2.3.tar.gz

---
+++ packages/gazelib/gazelib-1.2.3.tar.gz/gazelib-1.2.3/gazelib/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Gazelib, a toolkit for gaze analysis.
+'''
+from . import package
+__version__ = package.version  # noqa
+
+from . import containers  # noqa
+from . import io  # noqa
+from . import legacy  # noqa
+# import gazelib.settings  # noqa
+from . import statistics  # noqa
+# import gazelib.validation  # noqa
