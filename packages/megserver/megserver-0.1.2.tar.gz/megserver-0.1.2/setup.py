225095c9fbe16699caa360512a3f2b601f855b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:29 +0000

    Add package megserver/0.1.2/megserver-0.1.2.tar.gz

---
+++ packages/megserver/megserver-0.1.2.tar.gz/megserver-0.1.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = "0.1.2"
+
+
+setup(
+    name="megserver",
+    author="Gregory Rehm",
+    version=__version__,
+    description="Server component for MEG",
+    packages=find_packages(exclude=["*.tests"]),
+    data_files=[('config', ['meg/config.default.yml'])],
+    install_requires=[
+        "configmaster>=2.3.6",
+        "Flask>=0.10.1",
+        "Flask-SQLAlchemy>=2.0",
+        "gunicorn",
+        "pgpdump>=1.5",
+        "psycopg2>=2.6.1",
+        "PyYAML>=3.11",
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "development=meg.development:main",
+        ]
+    }
+)
