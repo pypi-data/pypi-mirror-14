cb48d3facf175c80a043d2ba1ca05ea7aa983ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:05 +0000

    Add package companyparser/0.4/companyparser-0.4.tar.gz

---
+++ packages/companyparser/companyparser-0.4.tar.gz/companyparser-0.4/companyparser/__init__.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from builtins import zip
+from builtins import range
+from past.utils import old_div
+import os
+import re
+import warnings
+from collections import OrderedDict
+import string
+
+import pycrfsuite
+from metaphone import doublemetaphone
+import probableparsing
+
+# the labels available
+LABELS = [
+    'CorporationName',
+    'CorporationNameOrganization',
+    'CorporationNameAndCompany',
+    'CorporationNameBranchType',
+    'CorporationNameBranchIdentifier',
+    'CorporationCommitteeType',
+    'CorporationLegalType',
+    'ShortForm',
+    'ProxyFor',
+    'AKA',
+    ]
+
+PARENT_LABEL  = 'Name'                          # the XML tag for each labeled string
+GROUP_LABEL   = 'NameCollection'                # the XML tag for a group of strings
+NULL_LABEL    = 'Null'                          # the null XML tag
+MODEL_FILE    = 'learned_settings.crfsuite'     # filename for the crfsuite settings file
+
+VOWELS_Y = tuple('aeiouy')
+PREPOSITIONS = {'for', 'to', 'of', 'on'}
+
+
+try :
+    TAGGER = pycrfsuite.Tagger()
+    TAGGER.open(os.path.split(os.path.abspath(__file__))[0]+'/'+MODEL_FILE)
+except IOError :
+    TAGGER = None
+    warnings.warn('You must train the model (parserator train [traindata] [modulename]) to create the %s file before you can use the parse and tag methods' %MODEL_FILE)
+
+def parse(raw_string):
+    if not TAGGER:
+        raise IOError('\nMISSING MODEL FILE: %s\nYou must train the model before you can use the parse and tag methods\nTo train the model annd create the model file, run:\nparserator train [traindata] [modulename]' %MODEL_FILE)
+
+    tokens = tokenize(raw_string)
+    if not tokens :
+        return []
+
+    features = tokens2features(tokens)
+
+    tags = TAGGER.tag(features)
+    return list(zip(tokens, tags))
+
+def tag(raw_string) :
+    tagged = OrderedDict()
+
+    prev_label = None
+    proxy_label = False
+    aka_label = False
+
+    interrupting_tags = ('CorporationNameOrganization', 
+                         'CorporationNameBranchType',
+                         'CorporationNameBranchIdentifier',
+                         'ProxiedCorporationNameOrganization', 
+                         'OtherCorporationNameOrganization',
+                         'OtherCorporationNameBranchType',
+                         'OtherCorporationNameBranchIdentifier')
+
+    for token, label in parse(raw_string) :
+        original_label = label
+
+        if label == 'AKA' :
+            aka_label = True
+        elif label == 'ProxyFor' :
+            proxy_label = True
+
+        elif aka_label and label in tagged :
+            label = 'Other'+label
+        elif proxy_label and label in tagged :
+            label = 'Proxied'+label
+
+        if label not in tagged:
+            tagged[label] = [token]
+
+        elif label == prev_label or prev_label in interrupting_tags :
+            tagged[label].append(token)
+        elif label in interrupting_tags :
+            tagged[label].append(token)
+
+        else:
+            raise RepeatedLabelError(raw_string, parse(raw_string), label)
+
+        prev_label = label
+
+    for label in tagged :
+        component = ' '.join(tagged[label])
+        component = component.strip(' ,;')
+        tagged[label] = component
+
+    name_type = 'Corporation'
+
+    return tagged, name_type
+
+
+def tokenize(raw_string):
+    # this determines how any given string is split into its tokens
+    # handle any punctuation you want to split on, as well as any punctuation to capture
+
+    if isinstance(raw_string, bytes):
+        try:
+            raw_string = str(raw_string, encoding='utf-8')
+        except:
+            raw_string = str(raw_string)
+    
+    re_tokens = re.compile(r"""
+            \bc/o\b
+            |
+            [("']*\b[^\s\/,;#&()]+\b[.,;'")]* # ['a-b. cd,ef- '] -> ['a-b.', 'cd,', 'ef']
+            |
+            [#&]
+            """,
+            re.I | re.VERBOSE | re.UNICODE)
+
+    tokens = re_tokens.findall(raw_string)
+
+    if not tokens :
+        return []
+    return tokens
+
+
+#  _______________________
+# |3. CONFIGURE FEATURES! |
+# |_______________________| 
+#     (\__/) || 
+#     (•ㅅ•) || 
+#     / 　 づ
+def tokens2features(tokens):
+    
+    feature_sequence = [tokenFeatures(tokens[0])]
+    previous_features = feature_sequence[-1].copy()
+
+    seen_comma = False
+
+    for token in tokens[1:] :
+        token_features = tokenFeatures(token) 
+        if not seen_comma and previous_features['comma'] :
+            seen_comma = True
+        if seen_comma :
+            token_features['seen.comma'] = True
+
+        current_features = token_features.copy()
+
+        feature_sequence[-1]['next'] = current_features
+        token_features['previous'] = previous_features        
+            
+        feature_sequence.append(token_features)
+
+        previous_features = current_features
+
+    if len(feature_sequence) > 1 :
+        feature_sequence[0]['rawstring.start'] = True
+        feature_sequence[-1]['rawstring.end'] = True
+        feature_sequence[1]['previous']['rawstring.start'] = True
+        feature_sequence[-2]['next']['rawstring.end'] = True
+
+    else : 
+        feature_sequence[0]['singleton'] = True
+
+    return feature_sequence
+
+def tokenFeatures(token) :
+
+    if token in (u'&') :
+        token_clean = token_abbrev = token
+        
+    else :
+        token_clean = re.sub(r'(^[\W]*)|([^.\w]*$)', u'', token.lower())
+        token_abbrev = re.sub(r'\W', u'', token_clean)
+
+    metaphone = doublemetaphone(token_abbrev)
+
+    features = {'nopunc' : token_abbrev,
+                'abbrev' : token_clean.endswith('.'),
+                'comma'  : token.endswith(','), 
+                'hyphenated' : '-' in token_clean,
+                'contracted' : "'" in token_clean,
+                'bracketed' : bool(re.match(r'(["(\']\w+)|(\w+[")\'])', token) and not re.match(r'["(\']\w+[")\']', token)),
+                'fullbracketed' : bool(re.match(r'["(\']\w+[")\']', token)),
+                'length' : len(token_abbrev),
+                'initial' : len(token_abbrev) == 1 and token_abbrev.isalpha(),
+                'has.vowels'  : bool(set(token_abbrev[1:]) & set(VOWELS_Y)),
+                'just.letters' : token_abbrev.isalpha(),
+                'roman' : set('xvi').issuperset(token_abbrev),
+                'endswith.vowel' : token_abbrev.endswith(VOWELS_Y),
+                'digits' : digits(token_abbrev),
+                'metaphone1' : metaphone[0],
+                'metaphone2' : metaphone[-1],
+                'more.vowels' : vowelRatio(token_abbrev),
+                'prepositions' : token_abbrev in PREPOSITIONS,
+                'possessive' : token_clean.endswith("'s") 
+                }
+
+    reversed_token = token_abbrev[::-1]
+    for i in range(1, len(token_abbrev)) :
+        features['prefix_%s' % i] = token_abbrev[:i]
+        features['suffix_%s' % i] = reversed_token[:i][::-1]
+        if i > 4 :
+            break
+
+    for tri_gram in ngrams(token_abbrev, 3) :
+        features[tri_gram] = True
+
+    for four_gram in ngrams(token_abbrev, 4) :
+        features[four_gram] = True
+
+    return features
+
+
+# define any other methods for features. this is an example to get the casing of a token
+def vowelRatio(token) :
+    n_chars = len(token)
+    if n_chars > 1:
+        n_vowels = sum(token.count(c) for c in VOWELS_Y)
+        return old_div(n_vowels,float(n_chars))
+    else :
+        return False
+
+def digits(token) :
+    if token.isdigit() :
+        return 'all_digits' 
+    elif set(token) & set(string.digits) :
+        return 'some_digits' 
+    else :
+        return 'no_digits'
+
+def ngrams(word, n=2):
+    return (''.join(letters) for letters in zip(*[word[i:] for i in range(n)]))
+
+
+
+class RepeatedLabelError(probableparsing.RepeatedLabelError) :
+    REPO_URL = "https://github.com/datamade/probablepeople/issues/new"
+    DOCS_URL = ""
---
+++ packages/companyparser/companyparser-0.4.tar.gz/companyparser-0.4/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    version='0.4',
+    url='',
+    description='',
+    name='companyparser',
+    packages=['companyparser'],
+    package_data={'companyparser' : ['learned_settings.crfsuite']},
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    install_requires=['python-crfsuite>=0.7',
+                      'future',
+                      'probableparsing',
+                      'metafone'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+)
