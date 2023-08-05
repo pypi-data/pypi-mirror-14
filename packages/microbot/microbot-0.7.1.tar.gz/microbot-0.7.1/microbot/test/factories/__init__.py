34164e54aad0c55ef79782920585d827d1b83beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:09 +0000

    Add package microbot/0.7.1/microbot-0.7.1.tar.gz

---
+++ packages/microbot/microbot-0.7.1.tar.gz/microbot-0.7.1/microbot/test/factories/__init__.py
@@ -0,0 +1,7 @@
+from microbot.test.factories.user import UserFactory  # noqa
+from microbot.test.factories.bot import BotFactory  # noqa
+from microbot.test.factories.response import ResponseFactory  # noqa
+from microbot.test.factories.hook import HookFactory, RecipientFactory  # noqa
+from microbot.test.factories.telegram_lib import (UserLibFactory, ChatLibFactory,  # noqa
+                                                     MessageLibFactory, UpdateLibFactory)  # noqa
+from microbot.test.factories.handler import HandlerFactory, RequestFactory, UrlParamFactory, HeaderParamFactory  # noqa
