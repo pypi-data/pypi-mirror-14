ab1692a13b5ea7b47835b97598b1b2a1a74253c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package formasaurus/0.7.1/formasaurus-0.7.1-py2.py3-none-any.whl

---
+++ packages/formasaurus/formasaurus-0.7.1-py2.py3-none-any.whl/formasaurus/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+__version__ = '0.7.1'
+
+from .classifiers import (
+    extract_forms,
+    classify,
+    classify_proba,
+    FormFieldClassifier
+)
