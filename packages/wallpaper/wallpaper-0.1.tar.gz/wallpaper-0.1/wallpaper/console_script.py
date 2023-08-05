e909362a4ac70d99585b73b337e7d223b5f3d02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package wallpaper/0.1/wallpaper-0.1.tar.gz

---
+++ packages/wallpaper/wallpaper-0.1.tar.gz/wallpaper-0.1/wallpaper/console_script.py
@@ -0,0 +1,20 @@
+import argparse
+import time
+
+import wallpaper
+
+
+def create_wallpaper():
+    current_time = time.strftime('%Y-%m-%d-%H-%M-%S')
+
+    parser = argparse.ArgumentParser(prog='wallpaper')
+    parser.add_argument('--filename', default='wallpaper-%s.png' % current_time,
+                        help='Specify the filename.')
+
+    args = parser.parse_args()
+
+    w = wallpaper.Wallpaper(filename=args.filename)
+    w.paint()
+
+
+create_wallpaper()
