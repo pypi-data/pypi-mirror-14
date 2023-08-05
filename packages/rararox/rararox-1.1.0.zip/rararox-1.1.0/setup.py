75118d31e668d03f577c78cc196377809750fd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package rararox/1.1.0/rararox-1.1.0.zip

---
+++ packages/rararox/rararox-1.1.0.zip/rararox-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'rararox',
+		version = '1.1.0',
+		py_modules = ['rararox'],
+		author = 'RahulR',
+		author_mail = 'rahulrijhwani@live.com',
+		url = 'http://getgobble.com',
+		description = 'Simple prrogram',
+		)
