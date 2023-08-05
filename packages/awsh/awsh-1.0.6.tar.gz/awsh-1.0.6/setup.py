915c1598562b95b5801807715bc59273c1ecc518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package awsh/1.0.6/awsh-1.0.6.tar.gz

---
+++ packages/awsh/awsh-1.0.6.tar.gz/awsh-1.0.6/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from awsh.version import VERSION
+
+setup(
+    name='awsh',
+    packages=['awsh'],
+    version=VERSION,
+    description='SSH into your EC2 instances based on their configurations.',
+    author='Jean-Martin Archer',
+    author_email='pypi@jmartin.ca',
+    url='https://github.com/j-martin/awsh',
+    download_url='https://github.com/j-martin/awsh/archive/{}.tar.gz'.format(VERSION),
+    keywords=['tool', 'aws', 'ec2'],
+    classifiers=[],
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'awscli>=1.10.14'
+    ],
+    entry_points={
+        'console_scripts': ['awsh = awsh:main']
+    }
+)
