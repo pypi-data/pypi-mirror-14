c96077e0082345b38235407b9c72ff3ad28ebe26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:34 +0000

    Add package nester_gnoban/1.0.0/nester_gnoban-1.0.0.zip

---
+++ packages/nester_gnoban/nester_gnoban-1.0.0.zip/nester_gnoban-1.0.0/nester.py
@@ -0,0 +1,10 @@
+"""This is the nester.py comment"""
+
+def print_lol( the_list ):
+    """This is the print_lol comment"""
+    
+    for each_item in the_list:
+        if isinstance( each_item, list ):
+            print_lol( each_item )
+        else:
+            print( each_item )
---
+++ packages/nester_gnoban/nester_gnoban-1.0.0.zip/nester_gnoban-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester_gnoban',
+    version         = '1.0.0',
+    py_modules      = [ 'nester' ],
+    author          = 'hfpython',
+    author_email    = 'hfpython@headfirstlabs.com',
+    url             = 'http://www.headfirstlabs.com',
+    descripton      = 'A simple printer for nested lists'
+    )
