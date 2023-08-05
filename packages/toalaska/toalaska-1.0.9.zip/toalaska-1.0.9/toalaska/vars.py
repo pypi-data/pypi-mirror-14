cd2fd6c124b374ea8d88e448dd74dc6b84a569e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package toalaska/1.0.9/toalaska-1.0.9.zip

---
+++ packages/toalaska/toalaska-1.0.9.zip/toalaska-1.0.9/toalaska/vars.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+
+
+def to_lower(txt):
+    return re.sub("([A-Z])","_\g<1>",txt).lower().lstrip("_")
+def to_upper(txt):
+    return re.sub("(_[a-z])",lambda t:t.group(1).lstrip("_").upper(),txt).lstrip("_")
