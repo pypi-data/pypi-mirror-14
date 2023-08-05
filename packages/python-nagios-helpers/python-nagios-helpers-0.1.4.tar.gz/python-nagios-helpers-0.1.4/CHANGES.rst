cca45a2fcdf3fe1ed92b581c459db8e51e17a7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package python-nagios-helpers/0.1.4/python-nagios-helpers-0.1.4.tar.gz

---
+++ packages/python-nagios-helpers/python-nagios-helpers-0.1.4.tar.gz/python-nagios-helpers-0.1.4/CHANGES.rst
@@ -0,0 +1,20 @@
+0.1.4 (2016-03-08)
+------------------
+Better error management
+
+0.1.3 (2016-01-13)
+------------------
+Add docs
+
+0.1.1 (2016-01-08)
+------------------
+Add a launcher
+
+0.1.0 (2015-12-17)
+------------------
+Add some docs, tests
+Tune some functions
+
+0.0.7 (2015-11-19)
+------------------
+First working version
---
+++ packages/python-nagios-helpers/python-nagios-helpers-0.1.4.tar.gz/python-nagios-helpers-0.1.4/naghelp/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# python-nagios-helpers - Python Nagios plug-in and configuration environment
+# Copyright (C) 2015 Eric Lapouyade
+#
+
+__version__ = '0.1.4'
+__author__ = 'Eric Lapouyade'
+__copyright__ = 'Copyright 2015, python-nagios-helpers project'
+__credits__ = ['Eric Lapouyade']
+__license__ = 'LGPL'
+__maintainer__ = 'Eric Lapouyade'
+__status__ = 'Beta'
+
+
+from plugin import *
+from host import *
+from response import *
+from collect import *
+from perf import *
+
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
+def activate_debug():
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
