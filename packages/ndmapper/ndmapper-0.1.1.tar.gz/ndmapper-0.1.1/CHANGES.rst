3bab4f00eee4ae8da7dceb60ebdbbe0b27d6ebb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package ndmapper/0.1.1/ndmapper-0.1.1.tar.gz

---
+++ packages/ndmapper/ndmapper-0.1.1.tar.gz/ndmapper-0.1.1/CHANGES.rst
@@ -0,0 +1,25 @@
+NDMapper change log
+===================
+
+
+0.1.1 (2016-03-12)
+------------------
+
+- Clarify that processing library ``outputs`` parameters are now intended only
+  to specify filenames (rather than supporting shared data, at least for now)
+  and rename them accordingly, to ``out_names``.
+
+- Fixed a run_task failure when a DataFile modified in memory was passed as
+  the input to multiple IRAF steps, which raised an ``IOError`` second time,
+  due to sharing identically the same NDLater instances (& TabMapIO list) when
+  instatiating one DataFile from another, such that run_task ended up
+  modifying the NDMapIO instances of its inputs with a temporary filename.
+
+- Include missing ``add_bpm`` in ``cosmetics`` name space.
+
+
+0.1 (2016-03-01)
+----------------
+
+- PyPI test release.
+
---
+++ packages/ndmapper/ndmapper-0.1.1.tar.gz/ndmapper-0.1.1/ndmapper/lib/gemini.py
@@ -0,0 +1,48 @@
+# Copyright(c) 2015 Association of Universities for Research in Astronomy, Inc.
+# by James E.H. Turner.
+
+from ndmapper import config
+
+__all__ = ['CAL_DEPS', 'gemini_iraf_helper']
+
+
+# Default cal dependencies are defined by instrument mode, with just an empty
+# placeholder dict at this level:
+CAL_DEPS = {'target' : []}
+
+
+def gemini_iraf_helper():
+    """
+    Define a few common default values for Gemini IRAF tasks in one place
+    (just to avoid more repetition of static values than necessary) and
+    convert some Python defaults to be more appropriate for the IRAF tasks.
+
+    Returns
+    -------
+
+    dict
+        Common parameter values suitable for passing to Gemini IRAF tasks.
+
+    """
+
+    # Enable both VAR & DQ if either is specified, since most Gemini tasks
+    # can't control them individually:
+    if config['use_uncert'] or config['use_flags']:
+        vardq = True
+    else:
+        vardq = False
+
+    vals = {'gmosdata' : 'gmos$data/',
+            'key_airmass' : 'AIRMASS',
+            'key_biassec' : 'BIASSEC',
+            'key_datasec' : 'DATASEC',
+            'key_exptime' : 'EXPTIME',
+            'key_gain' : 'GAIN',
+            'key_ron' : 'RDNOISE',
+            'observatory' : 'default',
+            'vardq' : vardq,
+            'verbose' : True
+           }
+
+    return vals
+
