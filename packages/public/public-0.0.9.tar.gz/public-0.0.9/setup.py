0b91c28b6eb0f4d00e34ff0e80bff156b1b6f344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package public/0.0.9/public-0.0.9.tar.gz

---
+++ packages/public/public-0.0.9.tar.gz/public-0.0.9/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__all__ = []
+import os, sys
+
+dir = os.path.dirname(__file__)
+if not dir or dir==".": dir=os.getcwd()
+sys.path.append("%s/.setup" % dir)
+
+if __name__=="__main__":
+	# import .setup.py/__setup__.py
+	import __setup__
