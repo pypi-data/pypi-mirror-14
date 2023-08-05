a0f6f78b1fb13059c8dd638d46b347d256967cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:59 +0000

    Add package easybuild-framework/2.7.0/easybuild-framework-2.7.0.tar.gz

---
+++ packages/easybuild-framework/easybuild-framework-2.7.0.tar.gz/easybuild-framework-2.7.0/easybuild/__init__.py
@@ -0,0 +1,41 @@
+##
+# Copyright 2011-2016 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+Declares EasyBuild namespace, in an extendable way.
+
+@author: Jens Timmerman (Ghent University)
+@author: Kenneth Hoste (Ghent University)
+"""
+import os
+import pkg_resources
+import sys
+
+# check whether EasyBuild is being run from a directory that contains easybuild/__init__.py;
+# that doesn't work (fails with import errors), due to weirdness to Python packaging/setuptools/namespaces
+if __path__[0] == 'easybuild':
+    sys.stderr.write("ERROR: Running EasyBuild from %s does not work (Python packaging weirdness)...\n" % os.getcwd())
+    sys.exit(1)
+
+pkg_resources.declare_namespace(__name__)
