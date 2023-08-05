e919d5c038a57d25fc87504e8b8bf65f64b83796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:02 +0000

    Add package django-baseclasses/1.0.1/django-baseclasses-1.0.1.tar.gz

---
+++ packages/django-baseclasses/django-baseclasses-1.0.1.tar.gz/django-baseclasses-1.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from setuptools import setup, find_packages
+
+# avoid importing the module
+exec(open('baseclasses/_version.py').read())
+
+setup(
+    name='django-baseclasses',
+    version=__version__,
+    description='A set of reusable base classes and helpers for django',
+    long_description=open('README.md').read(),
+    author='Greg Brown',
+    author_email='greg@gregbrown.co.nz',
+    url='https://github.com/gregplaysguitar/django-baseclasses',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Django>=1.7'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Framework :: Django',
+    ],
+)
