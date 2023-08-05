3087caf3cf759cc05a6725b38eb2a2421f0f7c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:03 +0000

    Add package BigQuery-Python/1.6.0/BigQuery-Python-1.6.0.tar.gz

---
+++ packages/BigQuery-Python/BigQuery-Python-1.6.0.tar.gz/BigQuery-Python-1.6.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '1.6.0'
+
+setup_args = dict(
+    name='BigQuery-Python',
+    description='Simple Python client for interacting with Google BigQuery.',
+    url='https://github.com/tylertreat/BigQuery-Python',
+    version=VERSION,
+    license='Apache',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'google-api-python-client',
+        'httplib2',
+        'python-dateutil'
+    ],
+    author='Tyler Treat',
+    author_email='ttreat31@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
