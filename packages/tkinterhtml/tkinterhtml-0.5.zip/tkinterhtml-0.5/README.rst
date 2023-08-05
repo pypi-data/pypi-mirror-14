ee1c137862d7e610e8ce49616a444f7fb1aefc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package tkinterhtml/0.5/tkinterhtml-0.5.zip

---
+++ packages/tkinterhtml/tkinterhtml-0.5.zip/tkinterhtml-0.5/README.rst
@@ -0,0 +1,29 @@
+``tkinterhtml``
+==================
+This is Python wrapper for `Tkhtml3 <http://tkhtml.tcl.tk/>`_.
+
+It's tested with Python 2.7, 3.2, 3.3, 3.4, 3.5.
+
+Includes Python package ``tkinterhtml`` with class ``TkinterHtml`` and Tkhtml binaries for:
+
+* Windows (32 and 64-bit);
+* Mac OS X (64-bit, should work on Lion and later, possibly also on 64-bit Snow Leopard);
+* Linux (32 and 64-bit, tested on Ubuntu 12.04).
+
+The binaries should work both with Tk 8.5 and 8.6.
+
+Installation
+-------------
+``pip install tkinterhtml``
+
+Usage
+------
+See `tkinterhtml/__main__.py <https://bitbucket.org/aivarannamaa/tkinterhtml/src/master/tkinterhtml/__main__.py>`_.
+
+More about Tkhtml binaries
+--------------------------
+If you ``pip install tkinterhtml``, then besides Python code you also get pre-built Tkhtml binaries and scripts for all main platforms (see the folder *tkhtml* inside resulting package directory). You may delete unnecessary platform directories after installation.
+
+If you are not happy with those binaries, then you could try compiling Tkhtml yourself. The scripts in *tkinterhtml/tkhtml* explain how I compiled it. Alternatively, you could try the binaries from Active Tcl (but check their license about redistribution).
+
+If you don't want to keep the binaries inside package directory (maybe because you want to zip your packages), then you should copy the *Tkhtml* folder with *pcgIndex.tcl* and dll/so/dylib in it into your Tcl library directory. In Windows it's *<Python root>\tcl*.
---
+++ packages/tkinterhtml/tkinterhtml-0.5.zip/tkinterhtml-0.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+url = "https://bitbucket.org/aivarannamaa/tkinterhtml"
+
+setup(
+      name="tkinterhtml",
+      version=0.5,
+      description="Python wrapper for Tkhtml3 (http://tkhtml.tcl.tk/)",
+      long_description="See %s for more info" % url,
+      url=url,
+      license="MIT",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+      ],
+      keywords="tkinter Tkinter tkhtml Tkhtml Tk",
+      packages=["tkinterhtml"],
+      #include_package_data=True,
+      package_data={'tkinterhtml': ['tkhtml/Windows/32-bit/Tkhtml/*.tcl',
+                                    'tkhtml/Windows/32-bit/Tkhtml/*.dll',
+                                    'tkhtml/Windows/64-bit/Tkhtml/*.tcl',
+                                    'tkhtml/Windows/64-bit/Tkhtml/*.dll',
+                                    
+                                    'tkhtml/MacOSX/64-bit/Tkhtml/*.tcl',
+                                    'tkhtml/MacOSX/64-bit/Tkhtml/*.dylib',
+                                    
+                                    'tkhtml/Linux/32-bit/Tkhtml/*.tcl',
+                                    'tkhtml/Linux/32-bit/Tkhtml/*.so',
+                                    'tkhtml/Linux/64-bit/Tkhtml/*.tcl',
+                                    'tkhtml/Linux/64-bit/Tkhtml/*.so'
+                                    ]}
+)
