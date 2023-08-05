430b97ae02bd93c484ff08a1c89e5a526c3cc117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package spacy/0.100.6/spacy-0.100.6-cp34-none-win_amd64.whl

---
+++ packages/spacy/spacy-0.100.6-cp34-none-win_amd64.whl/spacy/tests/parser/test_space_attachment.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+import pytest
+import numpy
+from spacy.attrs import HEAD
+
+
+@pytest.mark.models
+def test_space_attachment(EN):
+    sentence = 'This is a test.\nTo ensure  spaces are attached well.'
+    doc = EN(sentence)
+
+    for sent in doc.sents:
+        if len(sent) == 1:
+            assert not sent[-1].is_space
+
+
+@pytest.mark.models
+def test_sentence_space(EN):
+    text = ('''I look forward to using Thingamajig.  I've been told it will '''
+            '''make my life easier...''')
+    doc = EN(text)
+    assert len(list(doc.sents)) == 2
+
