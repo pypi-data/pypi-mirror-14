a386b35ead102803e8fb7768314ebcab86234479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package python-ironic-inspector-client/1.4.0/python-ironic-inspector-client-1.4.0.tar.gz

---
+++ packages/python-ironic-inspector-client/python-ironic-inspector-client-1.4.0.tar.gz/python-ironic-inspector-client-1.4.0/ironic_inspector_client/test/functional.py
@@ -0,0 +1,174 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import eventlet
+eventlet.monkey_patch()
+
+import unittest
+
+from ironic_inspector.test import functional
+
+import ironic_inspector_client as client
+
+
+class TestV1PythonAPI(functional.Base):
+    def setUp(self):
+        super(TestV1PythonAPI, self).setUp()
+        self.client = client.ClientV1()
+
+    def test_introspect_get_status(self):
+        self.client.introspect(self.uuid)
+        eventlet.greenthread.sleep(functional.DEFAULT_SLEEP)
+        self.cli.node.set_power_state.assert_called_once_with(self.uuid,
+                                                              'reboot')
+
+        status = self.client.get_status(self.uuid)
+        self.assertEqual({'finished': False, 'error': None}, status)
+
+        res = self.call_continue(self.data)
+        self.assertEqual({'uuid': self.uuid}, res)
+        eventlet.greenthread.sleep(functional.DEFAULT_SLEEP)
+
+        self.assertCalledWithPatch(self.patch, self.cli.node.update)
+        self.cli.port.create.assert_called_once_with(
+            node_uuid=self.uuid, address='11:22:33:44:55:66')
+
+        status = self.client.get_status(self.uuid)
+        self.assertEqual({'finished': True, 'error': None}, status)
+
+    def test_setup_ipmi(self):
+        self.node.provision_state = 'enroll'
+        self.client.introspect(self.uuid, new_ipmi_username='admin',
+                               new_ipmi_password='pwd')
+        eventlet.greenthread.sleep(functional.DEFAULT_SLEEP)
+        self.assertFalse(self.cli.node.set_power_state.called)
+
+        res = self.call_continue(self.data)
+        self.assertEqual('admin', res['ipmi_username'])
+        self.assertEqual('pwd', res['ipmi_password'])
+        self.assertTrue(res['ipmi_setup_credentials'])
+
+    def test_api_versions(self):
+        minv, maxv = self.client.server_api_versions()
+        self.assertEqual((1, 0), minv)
+        self.assertGreaterEqual(maxv, (1, 0))
+        self.assertLess(maxv, (2, 0))
+
+    def test_client_init(self):
+        self.assertRaises(client.VersionNotSupported,
+                          client.ClientV1, api_version=(1, 999))
+        self.assertRaises(client.VersionNotSupported,
+                          client.ClientV1, api_version=2)
+
+        self.assertTrue(client.ClientV1(api_version=1).server_api_versions())
+        self.assertTrue(client.ClientV1(api_version='1.0')
+                        .server_api_versions())
+        self.assertTrue(client.ClientV1(api_version=(1, 0))
+                        .server_api_versions())
+
+        self.assertTrue(
+            client.ClientV1(inspector_url='http://127.0.0.1:5050')
+            .server_api_versions())
+        self.assertTrue(
+            client.ClientV1(inspector_url='http://127.0.0.1:5050/v1')
+            .server_api_versions())
+
+        self.assertTrue(client.ClientV1(auth_token='some token')
+                        .server_api_versions())
+
+    def test_rules_api(self):
+        res = self.client.rules.get_all()
+        self.assertEqual([], res)
+
+        rule = {'conditions': [],
+                'actions': [{'action': 'fail', 'message': 'boom'}],
+                'description': 'Cool actions',
+                'uuid': self.uuid}
+        res = self.client.rules.from_json(rule)
+        self.assertEqual(self.uuid, res['uuid'])
+        rule['links'] = res['links']
+        self.assertEqual(rule, res)
+
+        res = self.client.rules.get(self.uuid)
+        self.assertEqual(rule, res)
+
+        res = self.client.rules.get_all()
+        self.assertEqual(rule['links'], res[0].pop('links'))
+        self.assertEqual([{'uuid': self.uuid,
+                           'description': 'Cool actions'}],
+                         res)
+
+        self.client.rules.delete(self.uuid)
+        res = self.client.rules.get_all()
+        self.assertEqual([], res)
+
+        for _ in range(3):
+            res = self.client.rules.create(conditions=rule['conditions'],
+                                           actions=rule['actions'],
+                                           description=rule['description'])
+            self.assertTrue(res['uuid'])
+            for key in ('conditions', 'actions', 'description'):
+                self.assertEqual(rule[key], res[key])
+
+        res = self.client.rules.get_all()
+        self.assertEqual(3, len(res))
+
+        self.client.rules.delete_all()
+        res = self.client.rules.get_all()
+        self.assertEqual([], res)
+
+        self.assertRaises(client.ClientError, self.client.rules.get,
+                          self.uuid)
+        self.assertRaises(client.ClientError, self.client.rules.delete,
+                          self.uuid)
+
+
+class TestSimplePythonAPI(functional.Base):
+    def test_introspect_get_status(self):
+        client.introspect(self.uuid)
+        eventlet.greenthread.sleep(functional.DEFAULT_SLEEP)
+        self.cli.node.set_power_state.assert_called_once_with(self.uuid,
+                                                              'reboot')
+
+        status = client.get_status(self.uuid)
+        self.assertEqual({'finished': False, 'error': None}, status)
+
+        res = self.call_continue(self.data)
+        self.assertEqual({'uuid': self.uuid}, res)
+        eventlet.greenthread.sleep(functional.DEFAULT_SLEEP)
+
+        self.assertCalledWithPatch(self.patch, self.cli.node.update)
+        self.cli.port.create.assert_called_once_with(
+            node_uuid=self.uuid, address='11:22:33:44:55:66')
+
+        status = client.get_status(self.uuid)
+        self.assertEqual({'finished': True, 'error': None}, status)
+
+    def test_api_versions(self):
+        minv, maxv = client.server_api_versions()
+        self.assertEqual((1, 0), minv)
+        self.assertGreaterEqual(maxv, (1, 0))
+        self.assertLess(maxv, (2, 0))
+
+        self.assertRaises(client.VersionNotSupported,
+                          client.introspect, self.uuid, api_version=(1, 999))
+        self.assertRaises(client.VersionNotSupported,
+                          client.get_status, self.uuid, api_version=(1, 999))
+        # Error 404
+        self.assertRaises(client.ClientError,
+                          client.get_status, self.uuid, api_version=(1, 0))
+
+
+if __name__ == '__main__':
+    with functional.mocked_server():
+        unittest.main()
---
+++ packages/python-ironic-inspector-client/python-ironic-inspector-client-1.4.0.tar.gz/python-ironic-inspector-client-1.4.0/ironic_inspector_client/test/test_client.py
@@ -0,0 +1,100 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import types
+import unittest
+
+import mock
+from oslo_utils import uuidutils
+
+import ironic_inspector_client
+from ironic_inspector_client import client
+from ironic_inspector_client.common import http
+from ironic_inspector_client import v1
+
+
+class BaseTest(unittest.TestCase):
+    uuid = uuidutils.generate_uuid()
+    token = "token"
+
+
+@mock.patch.object(v1, 'ClientV1', autospec=True)
+class TestIntrospect(BaseTest):
+    def test(self, mock_v1):
+        client.introspect(self.uuid, base_url="http://host:port",
+                          auth_token=self.token)
+        mock_v1.assert_called_once_with(auth_token=self.token,
+                                        inspector_url="http://host:port",
+                                        api_version=client.DEFAULT_API_VERSION)
+        mock_v1.return_value.introspect.assert_called_once_with(
+            self.uuid, new_ipmi_username=None, new_ipmi_password=None)
+
+    def test_full_url(self, mock_v1):
+        client.introspect(self.uuid, base_url="http://host:port/v1/",
+                          auth_token=self.token)
+        mock_v1.assert_called_once_with(auth_token=self.token,
+                                        inspector_url="http://host:port/v1/",
+                                        api_version=client.DEFAULT_API_VERSION)
+        mock_v1.return_value.introspect.assert_called_once_with(
+            self.uuid, new_ipmi_username=None, new_ipmi_password=None)
+
+    def test_set_ipmi_credentials(self, mock_v1):
+        client.introspect(self.uuid, base_url="http://host:port",
+                          auth_token=self.token, new_ipmi_password='p',
+                          new_ipmi_username='u')
+        mock_v1.assert_called_once_with(auth_token=self.token,
+                                        inspector_url="http://host:port",
+                                        api_version=client.DEFAULT_API_VERSION)
+        mock_v1.return_value.introspect.assert_called_once_with(
+            self.uuid, new_ipmi_username='u', new_ipmi_password='p')
+
+
+@mock.patch.object(v1, 'ClientV1', autospec=True)
+class TestGetStatus(BaseTest):
+    def test(self, mock_v1):
+        mock_v1.return_value.get_status.return_value = 'json'
+
+        self.assertEqual('json',
+                         client.get_status(self.uuid, auth_token=self.token))
+
+        mock_v1.assert_called_once_with(auth_token=self.token,
+                                        inspector_url=None,
+                                        api_version=client.DEFAULT_API_VERSION)
+        mock_v1.return_value.get_status.assert_called_once_with(self.uuid)
+
+
+@mock.patch.object(http, 'BaseClient', autospec=True)
+class TestServerApiVersions(BaseTest):
+    def test(self, mock_cli):
+        mock_cli.return_value.server_api_versions.return_value = (1, 0), (1, 1)
+
+        minv, maxv = client.server_api_versions()
+
+        self.assertEqual((1, 0), minv)
+        self.assertEqual((1, 1), maxv)
+
+
+class TestExposedAPI(unittest.TestCase):
+    def test_only_client_all_exposed(self):
+        exposed = {x for x in dir(ironic_inspector_client)
+                   if not x.startswith('__') and
+                   not isinstance(getattr(ironic_inspector_client, x),
+                                  types.ModuleType)}
+        self.assertEqual(set(client.__all__) | {'ClientV1'}, exposed)
+
+    def test_client_exposes_everything(self):
+        actual = {x for x in dir(client)
+                  if not x.startswith('_') and x != 'LOG' and
+                  not isinstance(getattr(client, x),
+                                 types.ModuleType)}
+        self.assertEqual(actual, set(client.__all__))
