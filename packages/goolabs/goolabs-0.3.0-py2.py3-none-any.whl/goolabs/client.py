1ca87f88dfb167d52a969045583ceaed6faa9f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package goolabs/0.3.0/goolabs-0.3.0-py2.py3-none-any.whl

---
+++ packages/goolabs/goolabs-0.3.0-py2.py3-none-any.whl/goolabs/client.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    API Client for Goo labs API
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :author: tell-k <ffk2005@gmail.com>
+    :copyright: tell-k. All Rights Reserved.
+"""
+from __future__ import division, print_function, absolute_import  # NOQA
+
+import json
+import requests
+
+
+class GoolabsAPI(object):
+
+    BASE_API_URL = 'https://labs.goo.ne.jp/api/{0}'
+    API_NAMES = ['morph', 'similarity', 'hiragana',
+                 'entity', 'shortsum', 'keyword']
+
+    def __init__(self, app_id, **kwargs):
+        self._app_id = app_id
+        self._req_args = {'timeout': 30, 'headers': {}}
+        self._req_args.update(kwargs)
+        self._req_args['headers'].update({'content-type': 'application/json'})
+
+    def __getattr__(self, func):
+        if func not in self.API_NAMES:
+            raise AttributeError(
+                'Cannot access or call this attribute "{0}"'.format(func))
+
+        req_url = self.BASE_API_URL.format(func)
+
+        def inner_func(**kwargs):
+            payload = dict([(k, v) for k, v in kwargs.items() if v])
+            payload.update({'app_id': self._app_id})
+            self.response = requests.post(
+                req_url,
+                data=json.dumps(payload),
+                **self._req_args
+            )
+            self.response.raise_for_status()
+            return self.response.json()
+        return inner_func
---
+++ packages/goolabs/goolabs-0.3.0-py2.py3-none-any.whl/goolabs/commands.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+"""
+    Command line tools for Goo labs API
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :author: tell-k <ffk2005@gmail.com>
+    :copyright: tell-k. All Rights Reserved.
+"""
+from __future__ import division, print_function, absolute_import  # NOQA
+
+import json
+import locale
+
+import click
+import six
+
+import goolabs
+from goolabs import GoolabsAPI
+
+
+def text(s):
+    if isinstance(s, six.binary_type):
+        return s.decode(locale.getpreferredencoding())
+    return s
+
+
+def clean_app_id(app_id):
+    if not app_id:
+        raise click.UsageError('Missing option "--app-id" / "-a" '
+                               'or GOOLABS_APP_ID enviroment value.')
+    return app_id
+
+
+def clean_sentence(sentence, sentence_file):
+    if not sentence and not sentence_file:
+        raise click.UsageError('Missing sentence. You must set'
+                               ' SENTENCE argument or --file option.')
+    if not sentence and sentence_file:
+        sentence = text(sentence_file.read())
+    return sentence
+
+
+def clean_review(review, review_file):
+    if not review and not review_file:
+        raise click.UsageError('Missing review. You must set'
+                               ' REVIEW argument or --file option.')
+    if not review and review_file:
+        review = text(review_file.read())
+    return review.split('\n')
+
+
+def clean_body(body, body_file):
+    if not body and not body_file:
+        raise click.UsageError('Missing body. You must set'
+                               ' BODY argument or --file option.')
+    if not body and body_file:
+        body = text(body_file.read())
+    return body
+
+
+def clean_length(length):
+    if length is None:
+        return None
+    try:
+        return int(length)
+    except ValueError:
+        raise click.UsageError(
+            '--length is not Integer. You must choice length from 60/120/180.'
+        )
+
+
+def format_json(json_data):
+    return json.dumps(json_data, indent=2, ensure_ascii=False)
+
+
+@click.group()
+@click.pass_context
+@click.version_option(version=goolabs.__version__)
+def main(ctx):
+    """ Command line tools for Goo labs API(https://labs.goo.ne.jp/api/). """
+
+
+@main.command()
+@click.argument('sentence', required=False, type=text)
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--info-filter', '-i', 'info_filter',
+              type=text, help='form,pos,read')
+@click.option('--pos-filter', '-p', 'pos_filter',
+              type=text, help=u'名刺,動詞活用語尾,句点..etc')
+@click.option('--file', '-f', 'sentence_file', type=click.File('rb'))
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def morph(ctx, app_id, sentence_file, json_flag, **kwargs):
+    """ Morphological analysis for Japanese."""
+
+    app_id = clean_app_id(app_id)
+    kwargs['sentence'] = clean_sentence(kwargs['sentence'], sentence_file)
+
+    if kwargs['info_filter']:
+        kwargs['info_filter'] = kwargs['info_filter'].replace(',', '|')
+
+    if kwargs['pos_filter']:
+        kwargs['pos_filter'] = kwargs['pos_filter'].replace(',', '|')
+
+    api = GoolabsAPI(app_id)
+    ret = api.morph(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    for words in ret['word_list']:
+        for word in words:
+            click.echo(','.join(word))
+
+
+@main.command()
+@click.argument('query_pair', required=True, nargs=2, type=text)
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def similarity(ctx, app_id, json_flag, **kwargs):
+    """ Scoring the similarity of two words. """
+
+    app_id = clean_app_id(app_id)
+
+    api = GoolabsAPI(app_id)
+    ret = api.similarity(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    click.echo('{0:.16f}'.format(ret['score']))
+
+
+@main.command()
+@click.argument('sentence', required=False, type=text)
+@click.option('--output-type', '-o', 'output_type', default='hiragana',
+              type=click.Choice(['hiragana', 'katakana']))
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--file', '-f', 'sentence_file', type=click.File('rb'))
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def hiragana(ctx, app_id, sentence_file, json_flag, **kwargs):
+    """ Convert the Japanese to Hiragana or Katakana. """
+
+    app_id = clean_app_id(app_id)
+    kwargs['sentence'] = clean_sentence(kwargs['sentence'], sentence_file)
+
+    api = GoolabsAPI(app_id)
+    ret = api.hiragana(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    click.echo(ret['converted'])
+
+
+@main.command()
+@click.argument('sentence', required=False, type=text)
+@click.option('--class-filter', '-c', 'class_filter',
+              help='ART,ORG,PSN,LOC,DAT', type=text)
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--file', '-f', 'sentence_file', type=click.File('rb'))
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def entity(ctx, app_id, sentence_file, json_flag, **kwargs):
+    """ Extract unique representation from sentence. """
+
+    app_id = clean_app_id(app_id)
+    kwargs['sentence'] = clean_sentence(kwargs['sentence'], sentence_file)
+
+    if kwargs['class_filter']:
+        kwargs['class_filter'] = kwargs['class_filter'].replace(',', '|')
+
+    api = GoolabsAPI(app_id)
+    ret = api.entity(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    for ne in ret['ne_list']:
+        click.echo(','.join(ne))
+
+
+@main.command()
+@click.argument('review', required=False, type=text)
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--length', '-l', type=click.Choice(['60', '120', '180']))
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--file', '-f', 'review_file', type=click.File('rb'))
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def shortsum(ctx, app_id, review_file, json_flag, **kwargs):
+    """Summarize reviews into a short summary."""
+
+    app_id = clean_app_id(app_id)
+    kwargs['review_list'] = clean_review(kwargs.pop('review'), review_file)
+    kwargs['length'] = clean_length(kwargs['length'])
+
+    api = GoolabsAPI(app_id)
+    ret = api.shortsum(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    click.echo(ret['summary'])
+
+
+@main.command()
+@click.argument('title', required=True, type=text)
+@click.argument('body', required=False, type=text)
+@click.option('--app-id', '-a', 'app_id', envvar='GOOLABS_APP_ID', type=text)
+@click.option('--max_num', '-m', type=click.INT)
+@click.option('--forcus', '-fo', type=click.Choice(['ORG', 'PSN', 'LOC']))
+@click.option('--request-id', '-r', 'request_id', type=text)
+@click.option('--file', '-f', 'body_file', type=click.File('rb'))
+@click.option('--json/--no-json', '-j', 'json_flag', default=False)
+@click.pass_context
+def keyword(ctx, app_id, body_file, json_flag, **kwargs):
+    """Extract "keywords" from an input document. """
+
+    app_id = clean_app_id(app_id)
+    kwargs['body'] = clean_body(kwargs.pop('body'), body_file)
+
+    api = GoolabsAPI(app_id)
+    ret = api.keyword(**kwargs)
+
+    if json_flag:
+        click.echo(format_json(api.response.json()))
+        return
+
+    for k in ret['keywords']:
+        k = dict((key.encode('utf-8'), k[key]) for key in k.keys())
+        for keyword, score in six.iteritems(k):
+            click.echo(u'{0},{1}'.format(text(keyword), score))
