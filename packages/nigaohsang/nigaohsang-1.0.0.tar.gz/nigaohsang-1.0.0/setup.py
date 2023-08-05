c28026375aef260d1b68f3cac26b074f39458b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:39 +0000

    Add package nigaohsang/1.0.0/nigaohsang-1.0.0.tar.gz

---
+++ packages/nigaohsang/nigaohsang-1.0.0.tar.gz/nigaohsang-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = 'nigaohsang',
+      version = '1.0.0',
+      py_modules = ['nigaoshang'],
+      author = 'darker',
+      url = 'www.baidu.com',
+      description = 'A simple printer of nested lists',)
