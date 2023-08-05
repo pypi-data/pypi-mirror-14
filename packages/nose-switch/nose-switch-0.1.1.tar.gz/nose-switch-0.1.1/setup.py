a6e5911c2255ae9757ead89572654c070df87f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:45 +0000

    Add package nose-switch/0.1.1/nose-switch-0.1.1.tar.gz

---
+++ packages/nose-switch/nose-switch-0.1.1.tar.gz/nose-switch-0.1.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+VERSION = '0.1.1'
+
+setup(
+    name="nose-switch",
+    packages=['nose-switch'],
+    version=VERSION,
+    author="Bogdan Popa",
+    author_email="bogdititupopa@gmail.com",
+    license="GNU GPL",
+    description="Add special switches in code, based on options set when running tests.",
+    url="https://github.com/popas90/nose-switch",
+    download_url="https://github.com/popas90/nose-switch/tarball/0.1.0",
+    classifiers=[],
+)
