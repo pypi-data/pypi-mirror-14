952752592953fc73ec53ba2e83a51c0ccb7c6943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package Flask-pyoidc/0.1.0/Flask-pyoidc-0.1.0.tar.gz

---
+++ packages/Flask-pyoidc/Flask-pyoidc-0.1.0.tar.gz/Flask-pyoidc-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='Flask-pyoidc',
+        version='0.1.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        url='https://github.com/its-dirg/flask-pyoidc',
+        license='Apache 2.0',
+        author='Rebecka Gulliksson',
+        author_email='rebecka.gulliksson@umu.se',
+        description='Flask extension for OpenID Connect authentication.',
+        install_requires=[
+            'oic',
+            'Flask',
+            'requests'
+        ]
+)
