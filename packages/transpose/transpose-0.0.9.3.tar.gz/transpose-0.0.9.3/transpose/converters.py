18d6ccc159f78df30865953f77ffa8d702cb324b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:27 +0000

    Add package transpose/0.0.9.3/transpose-0.0.9.3.tar.gz

---
+++ packages/transpose/transpose-0.0.9.3.tar.gz/transpose-0.0.9.3/transpose/converters.py
@@ -0,0 +1,292 @@
+import json
+
+import yaml
+
+class Converter(object):
+  """
+  A converter turns a jsonschema `schema` into 
+  another format.
+  """
+
+  def __init__(self):
+    pass
+
+  def to(self, schema, **kw):
+    """
+    Default json-schema serializer.
+    """
+    return schema.body
+
+
+class JsonConverter(Converter):
+  """
+  Simple Json Converter
+  """
+  def to(self, schema, **kw):
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', True)
+    return json.dumps(schema.body, **kw)
+
+
+class YamlConverter(Converter):
+
+  def to(self, schema, **kw):
+    """
+    Dump JSON schema to Yaml.
+    """
+    return yaml.safe_dump(schema.body, default_flow_style=False)
+
+
+class AvroConverter(Converter):
+  """
+  Convert json schema dict to avsc format
+  https://avro.apache.org/docs/1.7.7/spec.html#schemas
+  
+  """
+  TYPE_MAP = {
+    'boolean': 'boolean',
+    'number': 'double',
+    'integer': 'long',
+    'string': 'string',
+    'date-time': 'string',
+    'null': 'null'
+  }
+
+  def to(self, schema, **kw):
+    """
+    Map json-schema to avsc
+    """
+    return self.to_avro(schema, **kw)
+
+  def to_avro(self, schema, **kw):
+    """
+    core function
+    """
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', False)
+    fields = self._map_properties_to_fields(
+      schema.body['properties'], **kw)
+    
+    return {
+      'name': "{}_rec".format(schema.body['title']),
+      'type': 'record',
+      'doc': '{} avro schema'.format(schema.body['title']),
+      'fields': fields
+    }
+
+  def _map_properties_to_fields(self, props, **kw):
+    """
+    Recursively map properties to a list of avsc fields.
+    """
+    fields = []
+    for name, prop in props.items():
+      types = prop.get('type', [])
+      if not isinstance(types, list):
+        types = [types]
+
+      # recurse on objects
+      if types[0] == 'object':
+        rec = self._init_obj_record(name)
+        rec['type']['fields'] = self._map_properties_to_fields(prop['properties'])
+      
+      # arrays
+      elif types[0] == 'array':
+        types = prop.get('items', {}).get('type', [])
+        
+        # array of objects
+        if 'object' in types:
+          rec = self._init_array_of_records(name)
+          fs = self._map_properties_to_fields(
+            prop['items']['properties'])
+          rec['type']['items']['fields'] = fs
+
+        # array of simple types:
+        else:
+          rec = self._init_simple_array(
+            name, self._map_types(types))
+
+      elif any([t in self.TYPE_MAP for t in types]):
+        rec = self._init_field(name, self._map_types(types))
+
+      fields.append(rec)
+    return fields
+
+  def _map_types(self, types):
+    """
+    Map simple types 
+    """
+    return [self.TYPE_MAP.get(t) for t in types]
+
+  def _init_field(self, name, types):
+    """
+    initialize an avsc field.
+    """
+    return {
+      "name": name,
+      "default": None,
+      "type": types
+    }
+
+  def _init_record(self, name):
+    """
+    initialize an avsc record
+    """
+    return { 
+        "type": "record", 
+        "name": "{}_rec".format(name),
+        "fields":[]
+      }
+
+  def _init_obj_record(self, name):
+    """
+    initialize an avsc record
+    """
+    return { 
+        "name": name,
+        "default": {},
+        "type": self._init_record(name)
+      }
+
+  def _init_simple_array(self, name, types):
+    """
+    initialize an avsc simple array
+    """
+    return {
+      "name": name,
+      "default": [],
+      "type": {
+        "type": "array",
+        "items": types
+      }
+    } 
+
+  def _init_array_of_records(self, name):
+    """
+    initialize an avsc array of records
+    """
+    return {
+      "name": name,
+      "default": [],
+      "type": {
+        "type": "array",
+        "items": self._init_record(name)
+      }
+    }
+
+class AvscConverter(AvroConverter):
+  """
+  simple json wrapper of avro converter
+  """
+
+  def to(self, schema, **kw):
+    """
+    Dump Avro Schema as Avsc
+    """
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', True)
+    return json.dumps(self.to_avro(schema, **kw), **kw)
+
+class ElasticSearchConverter(Converter):
+  
+  """
+  https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-types.html
+  """ 
+  
+  TYPE_MAP = {
+    'boolean': 'boolean',
+    'number': 'double',
+    'string': 'string',
+    'integer': 'integer',
+    'date-time': 'date'
+  }
+
+  def to(self, schema, **kw):
+    """
+    Format jsonschema as elasticsearch dictionary.
+    """
+    return self.to_elasticsearch(schema, **kw)
+
+  def to_elasticsearch(self, schema, **kw):
+    """
+    Map json schema to elasticsearch mapping schema.
+    """
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', False)
+    properties = self._map_properties(schema.body['properties'])
+    return {
+      schema.body.get('title'): {
+        'properties': properties
+      }
+    }
+
+  def _map_properties(self, properties):
+    """
+    map json scheam properties to elasticsearch properties
+    """
+    props = {}
+    for name, prop in properties.items():
+      types = prop.get('type')
+      if not isinstance(types, list):
+        types = [types]
+
+      type = [t for t in types if t!='null'][0]
+
+      # recurse on objects
+      if type == 'object':
+
+        rec = self._init_object(name)
+        rec[name].update(self._map_properties(prop['properties']))
+      
+      # nested arrays
+      elif type == 'array' and 'object' in prop['items'].get('type'):
+        rec = self._init_array_of_records(name)
+
+      else:
+        rec = self._init_field(name, type)
+
+      # set property
+      props.update(rec)
+    return props
+
+  def _init_field(self, name, type):
+    """
+    initialize an avsc field.
+    """
+    type = self.TYPE_MAP.get(type, 'string')
+    return {name: {"type": type}}
+
+  def _init_object(self, name):
+    """
+    initialize an es object type.
+    """
+    return {name: {}}
+
+  def _init_array_of_records(self, name):
+    """
+    initialize an avsc array of records
+    """
+    return {name: {"type": "nested"}}
+
+
+class ElasticSearchJsonConverter(ElasticSearchConverter):
+
+  def to(self, schema, **kw):
+    """
+    ElasticSearch json converter, 
+    """
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', True)
+    schema = self.to_elasticsearch(schema, **kw)
+    return json.dumps(schema, **kw)
+
+
+class ElasticSearchYamlConverter(ElasticSearchConverter):
+
+  def to(self, schema, **kw):
+    """
+    ElasticSearch yaml converter, 
+    """
+    kw.setdefault('indent', 2)
+    kw.setdefault('sort_keys', True)
+    schema = self.to_elasticsearch(schema, **kw)
+    return yaml.safe_dump(schema, default_flow_style=False)
---
+++ packages/transpose/transpose-0.0.9.3.tar.gz/transpose-0.0.9.3/transpose/schema.py
@@ -0,0 +1,228 @@
+import re
+import copy
+import hashlib
+import json
+import datetime
+from traceback import format_exc
+
+from jsonschema import validate
+from jsonschema import Draft4Validator
+
+# camel case => underscore
+_re_cml1 = re.compile('(.)([A-Z][a-z]+)')
+_re_cml2 = re.compile('([a-z0-9])([A-Z])')
+
+def no_camel(s):
+  # parse camel case
+  s1 = _re_cml1.sub(r'\1_\2', s)\
+    .strip()\
+    .replace('-', '_')\
+    .replace(' ', '_')
+  return  _re_cml2.sub( r'\1_\2', s1).lower()
+
+
+class SchemaError(Exception):
+  """
+  Raised when a schema is invalid.
+  """
+  pass
+
+
+class Schema:
+  """
+  An intelligently updating schema store.
+  Records are stored as JSON-SCHEMA and 
+  converted to other formats via 
+  standard customizable methods.
+  """
+  
+  # lookup of common python types to their
+  # jsonschema equivalent
+  TYPE_MAP = [
+    (bool, "boolean"),
+    (int, "integer"),
+    (long, "integer"),
+    (float, "number"),
+    (unicode, "string"),
+    (str, "string"),
+    (datetime.datetime, "date-time")
+  ]
+
+  BASE = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": '',
+    "description":  '',
+    "type": "object",
+    "properties": {}
+  }
+
+  BASE_OBJECT = {
+    "type": "object",
+    "properties": {}
+  }
+
+  def __init__(self, **kw):
+    """
+    initialize json schema
+    """
+    body = copy.copy(self.BASE)
+    self.body = self._update_nested_dict(body, kw.get('body', {}))
+    # include title and description
+    self.body['title'] = no_camel(kw.get('title', ''))
+    self.converters = kw.get('converters')
+    for k in ['description']:
+      self.body[k] = kw.get(k, '')
+    if not self.body['title']:
+      raise SchemaError('A schema requires a title')
+
+  @property
+  def md5(self):
+    """
+    md5 of schema object.
+    """
+    return hashlib.md5(self.to('json', indent=None)).hexdigest()
+
+  def to(self, format="json", **kw):
+    """
+    Convert this schema to another format.
+    """
+    if format not in self.converters:
+      raise ValueError("{} is not a valid export format. Choose from:\n{}".format(format, ", ".join(self.converters.keys())))
+    s = self.converters.get(format)
+    return s.to(self, **kw)
+
+  def check(self):
+    """
+    Check if our schema is valid.
+    """
+    try:
+      Draft4Validator.check_schema(self.body)
+    except Exception as e:
+      raise SchemaError(format_exc())
+    return True
+
+  def clear(self):
+    """
+    Clear our schema.
+    """
+    body = copy.copy(self.BASE)
+    self.body = body
+    return True
+
+  def update(self, obj, **kw):
+    """
+    Initialize json schema on an object.
+    """
+    try:
+      p = self._map_json_schema_properties(obj, **kw)
+      p2 = self._update_nested_dict(self.body['properties'], p, **kw)
+      self.body['properties'] = p2
+    except Exception as e:
+      raise SchemaError("{}: {}".format(self.body['title'], format_exc()))
+
+  def validate(self, obj):
+    """
+    Validate an object against this schema
+    """
+    try:
+      validate(obj, self.body)
+    except Exception as e:
+      raise SchemaError(format_exc())
+    return True
+
+  def _update_nested_dict(self, d, u, **kw):
+    """
+    Recursively update a nested dictionary.
+    Modified from: 
+    http://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+    """
+    kw.setdefault('overwrite', False)
+    for k, v in u.iteritems():
+      
+      if isinstance(v, dict):
+        r = self._update_nested_dict(d.get(k, {}), v, **kw)
+        d[k] = r
+      
+      elif isinstance(v, list):
+        if not k in d:
+          d[k] = []
+        vv = d.get(k, [])
+        if not isinstance(vv, list):
+          vv = [vv] 
+        d[k] = vv + v 
+        if not isinstance(v[0], dict):
+          d[k] = list(set(d[k]))
+
+      else:
+        # only add it if it doesn't already exist
+        if not kw.get('overwrite'):
+          if not d.get(k):
+            d[k] = u[k]
+        else:
+            d[k] = u[k]
+    return d
+
+  def _map_json_schema_properties(self, obj, **kw):
+    """
+    Recursively generate a json schema 
+    from a dictionary of python objects.
+    """
+    properties = {}
+    for k,v in obj.iteritems():
+      if not k in properties:
+        properties[k] = {}
+      
+      # recurse on dictionaries
+      if isinstance(v, dict):
+       
+        properties[k] = copy.copy(self.BASE_OBJECT)
+        p = self._map_json_schema_properties(v)
+        properties[k]['properties'] = p
+      
+      # generate arrays
+      elif isinstance(v, list):
+        properties[k] = {'type': 'array'}
+      
+        # .. of dicts
+        if isinstance(v[0], dict):
+          properties[k]['items'] = copy.copy(self.BASE_OBJECT)
+          for vv in v:
+            p = self._map_json_schema_properties(vv)
+            properties[k]['items']['properties'] = p
+        
+        # .. of simple types
+        else:
+          # simple items:
+          properties[k]['items'] = {
+            'type': self._map_json_schema_type(v[0], **kw)
+          }
+
+      # handle simple types.
+      else:
+        properties[k] = {
+          'type': self._map_json_schema_type(v, **kw)
+        }
+
+    return properties
+
+  def _map_json_schema_type(self, obj, **kw):
+    """
+    Map a simple python type to json schema type.
+    """
+    nullable = kw.setdefault('nullable', True)
+    m = kw.setdefault('default_type', ['string'])
+    for typ, mapping in self.TYPE_MAP:
+      if isinstance(obj, typ):
+        m = mapping 
+        break
+
+    # force lists for simple types
+    if not isinstance(m, list):
+      m = [m]
+
+    # make all simple types nullable
+    if nullable:
+      m += ["null"]
+
+    return m
+
