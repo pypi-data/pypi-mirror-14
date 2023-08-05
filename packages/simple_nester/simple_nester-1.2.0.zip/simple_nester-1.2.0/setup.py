b56330597b97e8e757c03d37f979d9d94967d742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:39 +0000

    Add package simple_nester/1.2.0/simple_nester-1.2.0.zip

---
+++ packages/simple_nester/simple_nester-1.2.0.zip/simple_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+        name            = 'simple_nester',
+        version         = '1.2.0',
+        py_modules      = ['simple_nester'],
+        author          = 'kkotari',
+        author_email    = 'kotarikirankumar@gmail.com',
+        url             = 'http://www.kkotari.com',
+        description     = 'A simple printer of nested lists',
+        
+    )
---
+++ packages/simple_nester/simple_nester-1.2.0.zip/simple_nester-1.2.0/simple_nester.py
@@ -0,0 +1,13 @@
+"""This is the 'simple_nester.py' module and it provides one function called print_lol()
+which prints lists that may or may not include nested lists."""
+def print_lol(the_list, level=0):
+    """This function takes one positional argument called 'the list', which is any
+        python list (of - possibly - nested lists). Each data item in the provided
+        list is (recursively) printed to the screen on it's own line.
+        A second argument called 'level' is used to insert tab-stops when a nested
+        list is encounterd."""
+    for each_flick in the_list:
+        if isinstance(each_flick, list):
+            print_lol(each_flick, level + 1)
+        else:
+            print('\t'*level + each_flick)
