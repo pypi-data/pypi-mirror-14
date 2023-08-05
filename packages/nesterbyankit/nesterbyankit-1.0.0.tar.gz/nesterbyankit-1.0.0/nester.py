28171466e4adea19c1654bef2908c73ccee1bf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:38 +0000

    Add package nesterbyankit/1.0.0/nesterbyankit-1.0.0.tar.gz

---
+++ packages/nesterbyankit/nesterbyankit-1.0.0.tar.gz/nesterbyankit-1.0.0/nester.py
@@ -0,0 +1,7 @@
+"""This module is used to print the nested list"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nesterbyankit/nesterbyankit-1.0.0.tar.gz/nesterbyankit-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            =   'nesterbyankit',
+    version         =   '1.0.0',
+    py_modules      =   ['nester'],
+    author          =   'Ankit Dixit',
+    auther_email    =   'abc@gmail.com',
+    url             =   'https://www.abc.com',
+    description     =   'A simple printer of nested list',
+)
