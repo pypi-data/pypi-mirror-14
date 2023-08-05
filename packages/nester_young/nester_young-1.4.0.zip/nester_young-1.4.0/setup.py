2a456fad56f6a9aff05169be125aeffa6cef2eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:04 +0000

    Add package nester_young/1.4.0/nester_young-1.4.0.zip

---
+++ packages/nester_young/nester_young-1.4.0.zip/nester_young-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        = 'nester_young',
+    version     = '1.4.0',
+    py_modules  = ['nester_young'],
+    author      = 'ypython',
+    author_email= 'liuyoung@hust.edu.cn',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'a simple printer of the nested lists',
+   )
