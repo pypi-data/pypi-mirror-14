fb769cb90c6f8226c4370b6edc46a295704863d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:04 +0000

    Add package ruskit/0.0.7/ruskit-0.0.7-py2.py3-none-any.whl

---
+++ packages/ruskit/ruskit-0.0.7-py2.py3-none-any.whl/ruskit/cmds/add.py
@@ -0,0 +1,54 @@
+import time
+
+from ruskit import cli
+from ..cluster import ClusterNode, Cluster
+from ..utils import echo
+
+
+# nodes = [
+#     {
+#         "addr": "127.0.0.1:7000",
+#         "role": "slave",
+#         "master": "master_name"
+#     }
+# ]
+def add_nodes(cluster, nodes):
+    for node in nodes:
+        cluster.add_node(node)
+
+    echo("Adding node", end='')
+    while not cluster.consistent():
+        echo('.', end='')
+        time.sleep(1)
+    echo()
+    cluster.wait()
+
+
+def format_nodes(nodes):
+    data = []
+    for node in nodes:
+        res = {}
+        if ',' in node:
+            n, m = node.split(',')
+            res["addr"] = n
+            res["role"] = "slave"
+            res["master"] = ClusterNode.from_uri(m).name
+        else:
+            res["addr"] = node
+            res["role"] = "master"
+        data.append(res)
+    return data
+
+
+@cli.command
+@cli.argument("cluster")
+@cli.argument("nodes", nargs='+')
+@cli.pass_ctx
+def add(ctx, args):
+    nodes = format_nodes(args.nodes)
+    cluster = Cluster.from_node(ClusterNode.from_uri(args.cluster))
+
+    if not cluster.healthy():
+        ctx.abort("Cluster not healthy.")
+
+    add_nodes(cluster, nodes)
