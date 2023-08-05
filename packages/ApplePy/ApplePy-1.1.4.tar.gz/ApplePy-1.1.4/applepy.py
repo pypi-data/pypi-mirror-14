609d5178225dacf61d5fe3fd5b09d9e42a828573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:40 +0000

    Add package ApplePy/1.1.4/ApplePy-1.1.4.tar.gz

---
+++ packages/ApplePy/ApplePy-1.1.4.tar.gz/ApplePy-1.1.4/applepy.py
@@ -0,0 +1,13 @@
+""" This is the "applepy.py" module, and it provides a list of os.system commands shortcude.
+	cls= This code was found on the stackoveflow thread below:
+	http://stackoverflow.com/questions/517970/how-to-clear-python-interpreter-console
+"""
+
+import os
+class Cls(object):
+    def __repr__(self):
+	""" This function clears the screen """
+        os.system('clear')
+        return ''
+"""" usage: type cls """
+cls = Cls()
