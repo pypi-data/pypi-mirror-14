25e08c007addbbb4bcdb0919f682f2414a6bb35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package svgis/0.3.9/svgis-0.3.9-py2-none-any.whl

---
+++ packages/svgis/svgis-0.3.9-py2-none-any.whl/svgis/projection.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+import os.path
+from fiona import transform
+import fiona.crs
+import fionautil.layer
+import pyproj
+import utm
+from . import convert
+
+'''
+Helpers for dealing with projections
+'''
+
+
+def tm_proj4(x0, y0, y1):
+    return ('+proj=lcc +lon_0={x0} +lat_1={y1} +lat_2={y0} +lat_0={y1}'
+            '+x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0'
+            '+units=m +no_defs').format(x0=x0, y0=y0, y1=y1)
+
+
+def utm_proj4(x, y):
+    '''Generate the proj4 string for a given (lon, lat) coordinate.'''
+    try:
+        _, _, zonenumber, zoneletter = utm.from_latlon(y, x)
+        return zonetoproj4(zonenumber, zoneletter)
+
+    except utm.error.OutOfRangeError as e:
+        raise ValueError(e)
+
+
+def zonetoproj4(zonenumber, zoneletter):
+    if zoneletter in 'ZYXWVUTSRQPN':
+        hemisphere = 'north'
+
+    elif zoneletter in 'MLKJHGFEDCBA':
+        hemisphere = 'south'
+
+    return '+proj=utm +zone={} +{} +datum=WGS84 +units=m +no_defs'.format(zonenumber, hemisphere)
+
+
+def generatecrs(minx, miny, maxx, maxy, proj_method=None):
+    '''Choose a projection, either the local UTM zone or
+    create a custom transverse mercator.
+
+    Returns:
+        proj4 string
+    '''
+    if proj_method == 'utm':
+        midx = (minx + maxx) / 2
+        midy = (miny + maxy) / 2
+
+        return utm_proj4(midx, midy)
+
+    else:
+        # Create a custom TM projection
+        x0 = (float(minx) + float(maxx)) // 2
+
+        return tm_proj4(x0, miny, maxy)
+
+
+def choosecrs(in_crs, bounds, proj_method=None):
+    '''Choose a projection. If the layer is projected, use that.
+    Otherwise, create use a passed projection or create a custom transverse mercator.
+
+    Args:
+        in_crs (dict): A fiona-type proj4 dictionary
+        bounds (tuple): (minx, miny, maxx, maxy)
+        proj_method (string): either 'utm' or 'local'
+
+    Returns:
+        fiona-type proj4 dict.
+    '''
+    if (proj_method is None or proj_method == 'file') and not pyproj.Proj(**in_crs).is_latlong():
+        # it's projected already, so noop.
+        return in_crs
+
+    else:
+        return fiona.crs.from_string(generatecrs(*bounds, proj_method=proj_method))
+
+
+def transform_bounds(in_crs, out_crs, bounds):
+    '''
+    Project a bounding box, taking care to not slice off the sides.
+    '''
+    if in_crs is None:
+        raise TypeError('Need input CRS, not None')
+
+    if out_crs is None:
+        raise TypeError('Need output CRS, not None')
+
+    ring = convert.bounds_to_ring(*bounds)
+
+    try:
+        xs, ys = list(zip(*ring))
+    except ValueError:
+        # file is likely empty
+        return 0.0, 0.0, 0.0, 0.0
+
+    xbounds, ybounds = transform.transform(in_crs, out_crs, xs, ys)
+
+    return min(xbounds), min(ybounds), max(xbounds), max(ybounds)
+
+
+def layer_bounds(layer, crs=None):
+    '''
+    Get the bounds of a layer, optionally transforming them into a given CRS (or local or utm).
+
+    Args:
+        layer (str): path to a geodata file.
+        crs (str): (optional) Any of the crs specs typically accepted
+                   by SVGIS (An EPSG code, a proj4 string, a file containing a proj4, 'local' or 'utm')
+
+    Returns:
+        tuple
+    '''
+    meta = fionautil.layer.meta_complete(layer)
+
+    if crs:
+        _, crs = pick(crs)
+        result = transform_bounds(meta['crs'], crs, meta['bounds'])
+    else:
+        result = meta['bounds']
+
+    return result
+
+
+def pick(project):
+    '''
+        Pick a projection or projection method to use.
+
+    Returns:
+        tuple: (projection method, crs)
+    '''
+    proj_method, out_crs = None, None
+
+    if project is None:
+        project = 'local'
+
+    if project.lower() in ('local', 'utm'):
+        proj_method = project.lower()
+
+    elif os.path.exists(project):
+        # Is a file
+        with open(project) as f:
+            out_crs = fiona.crs.from_string(f.read())
+
+    elif project[:5].lower() == 'epsg:':
+        # Is an epsg code
+        _, epsg = project.split(':')
+        out_crs = fiona.crs.from_epsg(int(epsg))
+
+    else:
+        # Assume it's a proj4 string.
+        # fiona.crs.from_string returns {} if it isn't.
+        out_crs = fiona.crs.from_string(project)
+
+    return proj_method, out_crs
---
+++ packages/svgis/svgis-0.3.9-py2-none-any.whl/svgis/svg.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+'''
+Create string versions of SVG elements.
+'''
+
+
+def _element(tag, contents=None, **kwargs):
+    '''
+    Draw an element, optionally wrapping contents.
+
+    Args:
+        tag (str): tag name
+        contents (str): contents to be wrapped in the tag
+        kwargs: to be transformed into attributes
+
+    Returns:
+        str (unicode in Python 2)
+    '''
+    attribs = toattribs(**kwargs)
+    if contents:
+        return u'<{0}{1}>{2}</{0}>'.format(tag, attribs, contents)
+    else:
+        return u'<{0}{1}/>'.format(tag, attribs)
+
+
+def _round(i, precision):
+    if precision is None:
+        return i
+    else:
+        return round(i, precision)
+
+
+def _fmtcoord(precision):
+    if precision is None:
+        return u'{0[0]},{0[1]}'
+    else:
+        return u'{{0[0]:.{0}f}},{{0[1]:.{0}f}}'.format(precision)
+
+
+def circle(point, precision=None, **kwargs):
+    '''
+    Create a svg circle element. Keyword arguments are mapped to attributes.
+
+    Args:
+        point (tuple): The center of the circle
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    return _element(u'circle', cx=_round(point[0], precision), cy=_round(point[1], precision), **kwargs)
+
+
+def text(string, start, precision=None, **kwargs):
+    '''
+    Create an svg text element.
+
+    Args:
+        string (str): text for element
+        start (tuple): starting coordinate
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    return _element(u'text', string, x=start[0], y=start[1], **kwargs)
+
+
+def rect(start, width, height, precision=None, **kwargs):
+    '''
+    Create an svg rect element.
+
+    Args:
+        start (tuple): starting coordinate
+        width (int): rect width
+        height (int): rect height
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    width = _round(width, precision)
+    height = _round(height, precision)
+
+    return _element(u'rect', x=start[0], y=start[1], width=width, height=height, **kwargs)
+
+
+def line(start, end, precision=None, **kwargs):
+    '''
+    Create an svg line element.
+
+    Args:
+        start (tuple): starting coordinate
+        end (tuple): ending coordinate
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    end = [_round(i, precision) for i in end]
+
+    return _element(u'line', x1=start[0], y1=start[1], x2=end[0], y2=end[1], **kwargs)
+
+
+def _isstr(x):
+    return isinstance(x, basestring)
+
+
+def path(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg path element as a string.
+
+    Args:
+        coordinates (Sequence): A sequence of coordinates and string instructions
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    coords = (i if _isstr(i) else fmt.format(i) for i in coordinates)
+    return _element(u'path', d=' '.join(coords), **kwargs)
+
+
+def polyline(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg polyline element
+
+    Args:
+        coordinates (Sequence): x, y coordinates
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    points = u' '.join(fmt.format(c) for c in coordinates)
+    return _element(u'polyline', points=points, **kwargs)
+
+
+def polygon(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg polygon element
+
+    Args:
+        coordinates (Sequence): x, y coordinates
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    points = u' '.join(fmt.format(c) for c in coordinates)
+    return _element(u'polygon', points=points, **kwargs)
+
+
+def toattribs(**kwargs):
+    attribs = u' '.join(u'{}="{}"'.format(k, v) for k, v in kwargs.items() if v is not None)
+
+    if len(attribs) > 0:
+        return ' ' + attribs
+    else:
+        return attribs
+
+
+def defstyle(style=None):
+    '''
+    Create a defs element that wraps a CSS style.
+
+    Args:
+        style (string): A CSS string.
+
+    Returns:
+        unicode
+    '''
+    if style:
+        return u'<defs><style type="text/css"><![CDATA[{}]]></style></defs>'.format(style)
+    else:
+        return u'<defs />'
+
+
+def group(members=None, **kwargs):
+    '''
+    Create a group with the given scale and translation.
+
+    Args:
+        members (Sequence): unicode SVG elements
+        kwargs (dict): elements of this dictionary will be converted to
+                        attributes of the group, i.e. key="value".
+
+    Returns:
+        unicode
+    '''
+    members = members or ''
+    return _element(u'g', u''.join(members), **kwargs)
+
+
+def drawing(size, members, precision=None, viewbox=None, style=None):
+    '''
+    Create an SVG element.
+
+    Args:
+        size (tuple): width, height
+        members (list): Strings to add to output.
+        viewbox (Sequence): Four coordinates that describe an SVG viewBox.
+        style (string): CSS string.
+    '''
+    kwargs = {
+        'width': size[0],
+        'height': size[1],
+        'baseProfile': 'full',
+        'version': '1.1',
+        'xmlns': 'http://www.w3.org/2000/svg',
+    }
+
+    if precision:
+        fmt = ('{:.{precision}f}',)
+    else:
+        fmt = ('{:f}',)
+
+    if viewbox:
+        kwargs['viewBox'] = (','.join(fmt * 4)).format(*viewbox, precision=precision)
+
+    contents = defstyle(style) + u''.join(members)
+    return _element(u'svg', contents, **kwargs)
