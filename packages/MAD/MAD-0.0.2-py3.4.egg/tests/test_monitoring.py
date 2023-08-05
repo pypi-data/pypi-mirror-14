57914d6989fbddb5a2f83b5e5a06e6fb785e7d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:37 +0000

    Add package MAD/0.0.2/MAD-0.0.2-py3.4.egg

---
+++ packages/MAD/MAD-0.0.2-py3.4.egg/tests/test_monitoring.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+#
+# This file is part of MAD.
+#
+# MAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MAD.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from io import StringIO
+from unittest import TestCase
+
+from mad.monitoring import CSVReportFactory, CSVReport
+from mad.parsing import Parser
+from mock import MagicMock
+
+from mad.datasource import InMemoryDataSource, Project, Mad, Settings
+
+
+class MonitoringTests(TestCase):
+
+    def test_loading(self):
+        Settings.new_identifier = MagicMock()
+        Settings.new_identifier = lambda: "1"
+
+        source = InMemoryDataSource(
+                {"test.mad": "service DB:"
+                             "  operation Select:"
+                             "      think 5"
+                             "client Browser:"
+                             "  every 10:"
+                             "      query DB/Select"})
+
+        mad = Mad(Parser(source), source)
+
+        simulation = mad.load(Project("test.mad", 25))
+        simulation.run_until(25)
+
+        data = source.read("test_1/DB.log").getvalue().split("\n")
+        self.assertEqual(4, len(data), data) # header line, + Monitoring at 10, 20 + newline
+
+
+class ReportFactoryTests(TestCase):
+
+    def test_report_unicity(self):
+        factory = CSVReportFactory(Project("test.mad", 25), InMemoryDataSource())
+
+        report1 = factory.report_for_service("my-service")
+        report2 = factory.report_for_service("my-service")
+
+        self.assertIsNotNone(report1)
+        self.assertIsNotNone(report2)
+        self.assertIs(report1, report2)
+
+
+class ReportTests(TestCase):
+
+    def test_report(self):
+        output = StringIO()
+        report = CSVReport(output,
+                           [    ("time", "%3d"),
+                                ("queue_length", "%3d")])
+
+        report(time=5, queue_length=3)
+        report(time=10, queue_length=6)
+
+        expected_csv = "time, queue length\n" \
+                       "  5,   3\n" \
+                       " 10,   6\n"
+
+        self.assertEqual(expected_csv, output.getvalue())
---
+++ packages/MAD/MAD-0.0.2-py3.4.egg/tests/test_simulation.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+#
+# This file is part of MAD.
+#
+# MAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MAD.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from unittest import TestCase
+
+from mad.simulation import WorkerPool, TaskPool
+
+
+class TaskPoolTests(TestCase):
+
+    def test_put_increases_size(self):
+        pool = TaskPool()
+        pool.put("request 1")
+
+        self.assertEqual(pool.size, 1)
+
+    def test_take_decreases_size(self):
+        pool = TaskPool()
+        pool.put("request 1")
+        pool.put("request 2")
+
+        pool.take()
+
+        self.assertEqual(pool.size, 1)
+
+    def test_taking_from_empty_pool_fails(self):
+        pool = TaskPool()
+        with self.assertRaises(ValueError):
+            pool.take()
+
+    def test_activate_increases_size(self):
+        pool = TaskPool()
+        pool.activate("request")
+        self.assertEqual(pool.size, 1)
+
+    def test_activated_requests_come_out_first(self):
+        pool = TaskPool()
+        pool.put("task 1")
+        pool.activate("task 2")
+        pool.put("task 3")
+
+        self.assertEqual(pool.take(), "task 2")
+
+
+class WorkerPoolTests(TestCase):
+
+    def test_all_busy(self):
+        pool = WorkerPool(["a", "b", "c"])
+        self.assertTrue(pool.are_available)
+        for i in range(3):
+            pool.acquire_one()
+        self.assertFalse(pool.are_available)
+
+    def test_acquiring_idle_worker(self):
+        pool = WorkerPool(["a", "b", "c"])
+        pool.acquire_one()
+        self.assertEqual(pool.idle_worker_count, 2)
+
+    def test_release_worker(self):
+        pool = WorkerPool(["a", "b", "c"])
+        pool.release("d")
+        self.assertEqual(pool.idle_worker_count, 4)
+
+    def test_acquire_is_not_permitted_on_empty_pools(self):
+        pool = WorkerPool(["w1"])
+        pool.acquire_one()
+        with self.assertRaises(ValueError):
+            pool.acquire_one()
+
+    def test_utilisation(self):
+        pool = WorkerPool(["w1", "w2"])
+        self.assertAlmostEqual(pool.utilisation, float(0))
+        pool.acquire_one()
+        self.assertAlmostEqual(pool.utilisation, float(50))
+        pool.acquire_one()
+        self.assertAlmostEqual(pool.utilisation, float(100))
+
+
+if __name__ == "__main__":
+    import unittest.main
+    unittest.main()
