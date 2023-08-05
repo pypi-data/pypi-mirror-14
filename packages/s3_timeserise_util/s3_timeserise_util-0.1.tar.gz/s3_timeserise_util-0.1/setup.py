551fa2daad68013ae37792c2e7b5254c92f62be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:27 +0000

    Add package s3_timeserise_util/0.1/s3_timeserise_util-0.1.tar.gz

---
+++ packages/s3_timeserise_util/s3_timeserise_util-0.1.tar.gz/s3_timeserise_util-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 's3_timeserise_util',
+  packages = ['s3_timeserise_util'], # this must be the same as the name above
+  version = '0.1',
+  description = 's3 timeserise utility',
+  author = 'Yuta Hinokuma',
+  author_email = 'yuta@abeja.asia',
+  url = 'https://github.com/higumachan/s3_timeserise_util', # use the URL to the github repo
+  download_url = 'https://github.com/higumachan/s3_timeseries_util/tarball/master', # I'll explain this in a second
+  keywords = ['s3', 'rx'], # arbitrary keywords
+  classifiers = [],
+)
