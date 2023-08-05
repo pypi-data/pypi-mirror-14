6f51e6a4008c7bcd820abcc418bf274b8d4495d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package MPBS/0.0.2.6/MPBS-0.0.2.6.tar.gz

---
+++ packages/MPBS/MPBS-0.0.2.6.tar.gz/MPBS-0.0.2.6/MPBS/examples/mpbs/client.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+'''
+Created on 2016年3月3日
+'''
+import zmq
+import time
+import json
+__author__ = 'chenjian'
+
+def main():
+    context   = zmq.Context()
+    publisher = context.socket(zmq.PUB)  # @UndefinedVariable
+    publisher.connect("tcp://localhost:5555")
+
+    while True:
+        # Write two messages, each with an envelope and content
+        publisher.send_multipart([b"/v1/", json.dumps({"cmdid": 0, 'timestamp': long(time.time() * 1000)}), b"We want to see this"])
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
