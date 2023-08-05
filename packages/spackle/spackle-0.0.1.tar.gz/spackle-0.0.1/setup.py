9da5c879b0e1140cd2051997f1d971394bb90041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:00 +0000

    Add package spackle/0.0.1/spackle-0.0.1.tar.gz

---
+++ packages/spackle/spackle-0.0.1.tar.gz/spackle-0.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='spackle',
+    version='0.0.1',
+    install_requires=['setuptools', 'coverage'],
+    author='Nicholas Serra',
+    author_email='nickserra@gmail.com',
+    license='MIT License',
+    url='https://github.com/nicholasserra/spackle/',
+    keywords=['spackle', 'coverage', 'testing'],
+    description='Help identify gaps in code coverage.',
+    long_description=open('README.md').read(),
+    download_url="https://github.com/nicholasserra/spackle/zipball/master",
+    packages=['spackle'],
+    entry_points={
+        'console_scripts': [
+            'spackle = spackle.script:main',
+    ]},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ]
+)
