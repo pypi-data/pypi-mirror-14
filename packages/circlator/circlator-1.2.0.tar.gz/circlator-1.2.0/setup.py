8d2bf343658fd40e3c04acfd507f4493c0a11a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package circlator/1.2.0/circlator-1.2.0.tar.gz

---
+++ packages/circlator/circlator-1.2.0.tar.gz/circlator-1.2.0/setup.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import sys
+import glob
+from setuptools import setup, find_packages
+
+
+setup(
+    name='circlator',
+    version='1.2.0',
+    description='circlator: a tool to circularise genome assemblies',
+    packages = find_packages(),
+    package_data={'circlator': ['data/*']},
+    author='Martin Hunt, Nishadi De Silva',
+    author_email='path-help@sanger.ac.uk',
+    url='https://github.com/sanger-pathogens/circlator',
+    scripts=glob.glob('scripts/*'),
+    test_suite='nose.collector',
+    tests_require=['nose >= 1.3'],
+    install_requires=[
+        'openpyxl',
+        'pyfastaq >= 3.10.0',
+        'pysam >= 0.8.1, <= 0.8.3',
+        'pymummer>=0.6.1',
+        'bio_assembly_refinement>=0.5.0',
+    ],
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
