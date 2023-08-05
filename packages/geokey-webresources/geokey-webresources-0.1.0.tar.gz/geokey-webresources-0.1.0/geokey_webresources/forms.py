bb3067f8c2c0cce8258d98cc9e68e1544388188b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package geokey-webresources/0.1.0/geokey-webresources-0.1.0.tar.gz

---
+++ packages/geokey-webresources/geokey-webresources-0.1.0.tar.gz/geokey-webresources-0.1.0/geokey_webresources/forms.py
@@ -0,0 +1,16 @@
+"""All forms for the extension."""
+
+from django.forms import ModelForm
+
+from .models import WebResource
+
+
+class WebResourceForm(ModelForm):
+    """Form for a single web resource."""
+
+    class Meta:
+        """Form meta."""
+
+        model = WebResource
+        fields = ('name', 'description', 'data_format', 'url', 'colour',
+                  'symbol')
