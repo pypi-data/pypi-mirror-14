7d94ca87ea77bc00696329e06e36a95538e1f024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:40 +0000

    Add package sentry/8.2.2/sentry-8.2.2-py27-none-any.whl

---
+++ packages/sentry/sentry-8.2.2-py27-none-any.whl/sentry/static/sentry/app/components/alerts.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Reflux from 'reflux';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+import AlertStore from '../stores/alertStore';
+import AlertMessage from './alertMessage';
+
+const Alerts = React.createClass({
+  mixins: [
+    PureRenderMixin,
+    Reflux.connect(AlertStore, 'alerts')
+  ],
+
+  getInitialState() {
+    return {
+      alerts: []
+    };
+  },
+
+  render() {
+    return (
+      <div {...this.props}>
+        {this.state.alerts.map(function(alert, key) {
+           return <AlertMessage id={alert.id} key={key} type={alert.type} message={alert.message} />;
+        })}
+      </div>
+    );
+  }
+});
+
+export default Alerts;
