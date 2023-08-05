b688afc608be3487f6f361f116fb55b6b7038f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:16 +0000

    Add package eti-page-cms/1.0.1/eti-page-cms-1.0.1.tar.gz

---
+++ packages/eti-page-cms/eti-page-cms-1.0.1.tar.gz/eti-page-cms-1.0.1/pages/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Django page CMS module."""
+VERSION = (1, 0, 1)
+__version__ = '.'.join(map(str, VERSION))
+__author__ = "Thomas Saunders"
+__contact__ = "tasaunders@gmail.com"
+__homepage__ = "http://packages.python.org/django-page-cms/"
+__docformat__ = "restructuredtext"
+__doc__ = 'A tree based Django CMS application'
+__license__ = 'BSD'
