6e9e0aff78a6f481e9287bf942f4a4137404357a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package stoq/1.11.0rc1.post1/stoq-1.11.0rc1.post1-py2-none-any.whl

---
+++ packages/stoq/stoq-1.11.0rc1.post1-py2-none-any.whl/stoq-1.11.0rc1.post1.data/data/share/stoq/sql/patch-05-32.sql
@@ -0,0 +1,36 @@
+-- Make product, service and storable use the same id as sellable
+
+-- TODO: We should provide a way for plugins to execute "pre-migration"
+-- actions, like this one. Without it this patch would fail for people
+-- using the magento plugin.
+DROP RULE IF EXISTS magento_on_update ON product;
+DROP RULE IF EXISTS magento_on_update ON service;
+DROP RULE IF EXISTS magento_on_update ON storable;
+DROP RULE IF EXISTS magento_on_update ON product_stock_item;
+DROP RULE IF EXISTS magento_on_insert ON product_stock_item;
+
+UPDATE parameter_data SET field_value = service.sellable_id::text
+  FROM service
+  WHERE parameter_data.field_name = 'DELIVERY_SERVICE' AND
+        parameter_data.field_value = service.id::text;
+
+UPDATE service SET id = sellable_id;
+ALTER TABLE service
+    DROP COLUMN sellable_id,
+    DROP CONSTRAINT IF EXISTS "service_sellable_id_fkey",
+    ADD CONSTRAINT "service_id_fkey" FOREIGN KEY (id) REFERENCES sellable(id) ON UPDATE CASCADE;
+
+UPDATE storable SET id = product.sellable_id FROM product
+    WHERE product.id = storable.product_id;
+ALTER TABLE storable
+    DROP COLUMN product_id,
+    DROP CONSTRAINT IF EXISTS "storable_product_id_fkey";
+
+UPDATE product SET id = sellable_id;
+ALTER TABLE product
+    DROP COLUMN sellable_id,
+    DROP CONSTRAINT IF EXISTS "product_sellable_id_fkey",
+    ADD CONSTRAINT "product_id_fkey" FOREIGN KEY (id) REFERENCES sellable(id) ON UPDATE CASCADE;
+
+ALTER TABLE storable
+    ADD CONSTRAINT "storable_id_fkey" FOREIGN KEY (id) REFERENCES product(id) ON UPDATE CASCADE;
