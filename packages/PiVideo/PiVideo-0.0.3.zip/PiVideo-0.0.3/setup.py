d9b4876a15c14ce4d677de16875dcb4c0e8f6bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:45 +0000

    Add package PiVideo/0.0.3/PiVideo-0.0.3.zip

---
+++ packages/PiVideo/PiVideo-0.0.3.zip/PiVideo-0.0.3/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="PiVideo",
+
+    # Version number (initial):
+    version="0.0.3",
+
+    # Application author details:
+    author="Lintest Systems LLC",
+    author_email="lintestsystems@gmail.com",
+
+    # Packages
+    packages=["pivideo"],
+
+    # Include additional files into the package
+    #include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/pivideo_v003/",
+
+    # License
+    license="LICENSE.txt",
+
+    description="Raspberry Pi PiVideo Utilities for PiCapture",
+    long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    # install_requires=[
+    #    "smbus-cffi",
+    # ],
+     
+    entry_points={
+       'console_scripts': [
+           'pivideo = pivideo_cmd:main',
+           'pivideo_cmd = pivideo_cmd:main',
+       ],
+    }
+)
