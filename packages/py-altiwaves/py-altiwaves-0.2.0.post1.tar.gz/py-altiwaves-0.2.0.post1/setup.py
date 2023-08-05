1ad19a9a5c1219deea38ccfb742f0dfca61cce75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:00 +0000

    Add package py-altiwaves/0.2.0.post1/py-altiwaves-0.2.0.post1.tar.gz

---
+++ packages/py-altiwaves/py-altiwaves-0.2.0.post1.tar.gz/py-altiwaves-0.2.0.post1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='py-altiwaves',
+    version='0.2.0.post1',
+    author='R. Dussurget',
+    author_email='renaud.dussurget@gmail.com',
+	packages=['kernel', 'external'],
+    scripts=['bin/test_detection.py','bin/test_spectral_analysis.py'],
+    url='https://code.google.com/p/py-altiwaves/',
+    license='LICENSE.TXT',
+    description='PyALTIWAVES: Python-based ALong-Track Inventory of WAVelet based EddieS',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy",
+        "py-altimetry",
+        "NetCDF4"
+    ],
+)
