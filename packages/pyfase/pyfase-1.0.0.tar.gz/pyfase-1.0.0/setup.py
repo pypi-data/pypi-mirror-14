c239f28a0a9e94d316e8b1386cbe996fa46acffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package pyfase/1.0.0/pyfase-1.0.0.tar.gz

---
+++ packages/pyfase/pyfase-1.0.0.tar.gz/pyfase-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyfase',
+    version='1.0.0',
+    url='https://github.com/jomorais/pyfase',
+    license='GPLv3',
+    author='Joaci Morais',
+    author_email='joaci.morais@gmail.com',
+    description='A Fast-Asynchronous-microService-Environment based on ZeroMQ.',
+    install_requires=['zmq']
+)
