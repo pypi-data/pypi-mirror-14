2b64ab26f193d2e9135384bf8f3bb6b8a870a2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package django-db-email-backend/0.2/django-db-email-backend-0.2.tar.gz

---
+++ packages/django-db-email-backend/django-db-email-backend-0.2.tar.gz/django-db-email-backend-0.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from distutils.core import setup
+
+import db_email_backend
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+long_description = open('README').read()
+
+setup_args = dict(
+    name='django-db-email-backend',
+    version=db_email_backend.__version__,
+    description='Django email backend for storing messages to a database.',
+    long_description=long_description,
+    author='Jeremy Satterfield',
+    author_email='jsatt@jsatt.com',
+    url='https://github.com/jsatt/django-db-email-backend',
+    license="MIT License",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+    ],
+    packages=['db_email_backend', 'db_email_backend.migrations'],
+    install_requires=[
+        "django>=1.4",
+        "pytz",
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
