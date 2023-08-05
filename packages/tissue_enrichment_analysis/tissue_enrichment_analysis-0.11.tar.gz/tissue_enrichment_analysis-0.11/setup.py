cdc918ec15759f35f660932d3d10e99926821e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package tissue_enrichment_analysis/0.11/tissue_enrichment_analysis-0.11.tar.gz

---
+++ packages/tissue_enrichment_analysis/tissue_enrichment_analysis-0.11.tar.gz/tissue_enrichment_analysis-0.11/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 11:27:39 2016
+
+@author: dangeles
+"""
+
+from distutils.core import setup
+setup(
+  name = 'tissue_enrichment_analysis',
+  packages = ['tissue_enrichment_analysis'], # this must be the same as the name above
+  version = '0.11',
+  description = 'This package contains all the software used to implement TEA in WormBase and remotely',
+  author = 'David Angeles-Albores',
+  author_email = 'dangeles@caltech.edu',
+  url = 'https://github.com/dangeles/TissueEnrichmentAnalysis', # use the URL to the github repo
+  download_url = 'https://github.com/dangeles/TissueEnrichmentAnalysis/tarball/0.11', # I'll explain this in a second
+  keywords = ['tissue enrichment', 'celegans', 'biology'], # arbitrary keywords
+  classifiers = [],
+)
+
