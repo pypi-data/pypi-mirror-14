697293e31ea12bacac5ad5fed322776a1b5c8e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package cloudflaredns-backup/1.7.4/cloudflaredns-backup-1.7.4.tar.gz

---
+++ packages/cloudflaredns-backup/cloudflaredns-backup-1.7.4.tar.gz/cloudflaredns-backup-1.7.4/setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+from os.path import join, dirname
+
+setup(
+    name='cloudflaredns-backup',
+    version="1.7.4",
+    packages=['cloudflaredns_backup'],
+    url='https://github.com/m-messiah/cloudflaredns-backup',
+    license='MIT',
+    author='m_messiah',
+    author_email='m.muzafarov@gmail.com',
+    description='CloudFlare DNS backup to BIND files',
+    long_description="\n".join([
+        open(join(dirname(__file__), 'README.rst')).read(),
+        open(join(dirname(__file__), 'HISTORY.rst')).read()
+    ]),    
+    scripts=['scripts/cf-backup'],
+    install_requires=['requests'],
+    keywords='cloudflare dns backup',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: System Administrators',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
