328a35bcbcd6bbd30601352e1b6d99a816d41ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package coala/0.4.2.dev20160309123931/coala-0.4.2.dev20160309123931-py3-none-any.whl

---
+++ packages/coala/coala-0.4.2.dev20160309123931-py3-none-any.whl/coalib/misc/Exceptions.py
@@ -0,0 +1,24 @@
+from pyprint.NullPrinter import NullPrinter
+
+from coalib.misc import Constants
+from coalib.output.printers.LogPrinter import LogPrinter
+
+
+def get_exitcode(exception, log_printer=None):
+    log_printer = log_printer or LogPrinter(NullPrinter())
+
+    if isinstance(exception, KeyboardInterrupt):  # Ctrl+C
+        print("Program terminated by user.")
+        exitcode = 130
+    elif isinstance(exception, EOFError):  # Ctrl+D
+        print("Found EOF. Exiting gracefully.")
+        exitcode = 0
+    elif isinstance(exception, SystemExit):
+        exitcode = exception.code
+    elif isinstance(exception, BaseException):
+        log_printer.log_exception(Constants.CRASH_MESSAGE, exception)
+        exitcode = 255
+    else:
+        exitcode = 0
+
+    return exitcode
