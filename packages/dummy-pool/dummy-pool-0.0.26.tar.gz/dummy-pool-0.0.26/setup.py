dfeec8bc880cb225a76b7ddaf54e66971b13a08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package dummy-pool/0.0.26/dummy-pool-0.0.26.tar.gz

---
+++ packages/dummy-pool/dummy-pool-0.0.26.tar.gz/dummy-pool-0.0.26/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dummy-pool',
+    version='0.0.26',
+    author='tonic',
+    zip_safe=False,
+    author_email='tonic@wolege.ca',
+    description='pool',
+    py_modules=['pool'],
+    packages=find_packages(),
+)
