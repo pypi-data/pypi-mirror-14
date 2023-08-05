7281f7fe1cefd8defc0a0a410c2e2f6ef2a5d225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:18 +0000

    Add package new_my_json/1.0.1/new_my_json-1.0.1.zip

---
+++ packages/new_my_json/new_my_json-1.0.1.zip/new_my_json-1.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 17:16:52 2016
+
+@author: Animesh Kumar Jha
+"""
+
+from setuptools import setup
+#from distutils.core import setup
+from codecs import open
+import os
+
+#here = path.abspath(path.dirname(__file__))
+os.path.dirname(os.path.realpath('__file__'))
+
+setup(
+    name='new_my_json',
+    version='1.0.1',
+    author='Animesh Kumar Jha',
+    author_email='animesh-kumar.jha@ucdconnect.ie',
+    packages=['new_my_json'],
+    #url='http://pypi.python.org/pypi/new_my_json',
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],    
+    description='convert all the data to json',
+    long_description=open('README.rst').read(),
+
+)
