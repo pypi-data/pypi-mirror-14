f2c4f898bf875a019bc0983d9a3906ca3f656121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package QuantDigger/0.300/quantdigger-0.300.tar.gz

---
+++ packages/QuantDigger/quantdigger-0.300.tar.gz/quantdigger-0.300/quantdigger/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+settings = {
+    'source': 'csv',
+    'data_path': './data',
+    'stock_commission': 3/10000,
+    'future_commission': 1/10000,
+    'tick_test': False,
+}
+
+__all__ = ['settings']
