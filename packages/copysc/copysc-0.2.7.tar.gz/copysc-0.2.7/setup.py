df6b9ecfd6bb36545fda14a12956a4d0ef214646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package copysc/0.2.7/copysc-0.2.7.tar.gz

---
+++ packages/copysc/copysc-0.2.7.tar.gz/copysc-0.2.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+
+
+
+setuptools.setup(
+    name="copysc",
+    version="0.2.7",
+    url="https://github.com/rv816/copysc",
+
+    author="Ryan Vass",
+    author_email="rvsax16@gmail.com",
+
+    description="Small utility that translates a shared screenshot url into a markdown-ready *.png url. Works with dropbox, screencloud, and others.",
+    long_description=open('README.rst', 'r').read(),
+    packages=setuptools.find_packages(),
+
+    install_requires=['pyperclip', 'bs4', 'requests'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
