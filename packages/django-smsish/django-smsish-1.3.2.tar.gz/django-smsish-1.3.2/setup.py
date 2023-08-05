8d24655f4022dd61d8f9179cfafc2e09a661b1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:37 +0000

    Add package django-smsish/1.3.2/django-smsish-1.3.2.tar.gz

---
+++ packages/django-smsish/django-smsish-1.3.2.tar.gz/django-smsish-1.3.2/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+	README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name='django-smsish',
+	version='1.3.2',
+	# packages=[
+	# 	'smsish',
+	# 	'smsish.mail',
+	# 	'smsish.sms',
+	# 	'smsish.sms.backends',
+	# ],
+	packages=find_packages(),
+	include_package_data=True,
+	license='MIT',  # example license
+	description='A simple Django app to send SMS messages using an API similar to that of django.core.mail.',
+	long_description=README,
+	url='https://github.com/RyanBalfanz/django-smsish',
+	author='Ryan Balfanz',
+	author_email='ryan@ryanbalfanz.net',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		# 'Development Status :: 5 - Production/Stable',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Framework :: Django :: 1.8',
+		'Framework :: Django :: 1.9',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Communications',
+		'Topic :: Communications :: Telephony',
+	],
+)
