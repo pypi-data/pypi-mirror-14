90dd282d439a632996d5db31e0a61c56ab43a441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package wiringpi2/2.32.3/wiringpi2-2.32.3.tar.gz

---
+++ packages/wiringpi2/wiringpi2-2.32.3.tar.gz/wiringpi2-2.32.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name = 'wiringpi2',
+    version = '2.32.3',
+    author = "Philip Howard",
+    author_email = "phil@gadgetoid.com",
+    url = 'https://github.com/WiringPi/WiringPi-Python/',
+    description = """A python interface to WiringPi 2.0 library which allows for
+    easily interfacing with the GPIO pins of the Raspberry Pi. Also supports
+    i2c and SPI""",
+    long_description=open('README.txt').read(),
+    py_modules=['wiringpi2'],
+    install_requires=['wiringpi>=2.23.1'],
+)
