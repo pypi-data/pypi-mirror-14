96eafaf230fd0e7f54a199b739a95e32d7cd86d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:22 +0000

    Add package dadongjun_nester/1.2.0/dadongjun_nester-1.2.0.tar.gz

---
+++ packages/dadongjun_nester/dadongjun_nester-1.2.0.tar.gz/dadongjun_nester-1.2.0/dadongjun_nester.py
@@ -0,0 +1,10 @@
+"""This is the dadongjun_nester.py module, and it provides one function called print_lol() which prints lists that may or may not include nested lists."""
+def print_lol(the_list, level=0):
+	"""This function takes a positional argument called the_list, which is any Python list 		(or possibly, nested lists). Each data in the provide list is (recurively) printed to 		the screen on its own line. A second argument called level is used to insert tab_stop 		when a nested list is encountered."""
+	for each_item in the_list:
+		if isinstance(each_item, list):	#each_list is list, invoke(call) print_lol()
+			print_lol(each_item, level+1)	#recursion
+		else:				#each_list is not a list, print()
+			for tab_stop in range(level):
+				print("\t", end='')
+			print(each_item)
