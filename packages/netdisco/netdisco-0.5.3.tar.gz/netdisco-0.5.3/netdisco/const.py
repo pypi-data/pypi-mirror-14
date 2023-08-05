012921bce4c50ad7d9f7be22ae7cb6bf0bcbec5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package netdisco/0.5.3/netdisco-0.5.3.tar.gz

---
+++ packages/netdisco/netdisco-0.5.3.tar.gz/netdisco-0.5.3/netdisco/const.py
@@ -0,0 +1,11 @@
+"""
+Constants of services that can be discovered.
+"""
+
+BELKIN_WEMO = "belkin_wemo"
+DLNA = "DLNA"
+GOOGLE_CAST = "google_cast"
+PHILIPS_HUE = "philips_hue"
+PMS = 'plex_mediaserver'
+NETGEAR_ROUTER = "netgear_router"
+SONOS = "sonos"
