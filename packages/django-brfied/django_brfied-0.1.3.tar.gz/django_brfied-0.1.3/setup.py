bb042da1ba7dcdd972b54d467341ad8f8426de40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package django-brfied/0.1.3/django_brfied-0.1.3.tar.gz

---
+++ packages/django-brfied/django_brfied-0.1.3.tar.gz/django_brfied-0.1.3/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='django_brfied',
+    packages=['django_brfied',
+              'python_brfied', ],
+    version='0.1.3',
+    description='Django Application specific brazilian fields types',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/kelsoncm/django_brfied', 
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.1.0',
+    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    classifiers=[]
+)
