c349f167ea01fb22218fad61d1164f8563fd1490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package django-auth-ldap-ng/1.7.5/django-auth-ldap-ng-1.7.5.tar.gz

---
+++ packages/django-auth-ldap-ng/django-auth-ldap-ng-1.7.5.tar.gz/django-auth-ldap-ng-1.7.5/django_auth_ldap/__init__.py
@@ -0,0 +1,2 @@
+version = (1, 2, 7)
+version_string = '.'.join(map(str, version))
---
+++ packages/django-auth-ldap-ng/django-auth-ldap-ng-1.7.5.tar.gz/django-auth-ldap-ng-1.7.5/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup
+
+PY3 = (sys.version_info[0] == 3)
+
+setup(
+    name="django-auth-ldap-ng",
+    version="1.7.5",
+    description="Django LDAP authentication backend",
+    long_description=open('README').read(),
+    url="https://github.com/DheerendraRathor/django-auth-ldap-ng",
+    author="Dheerendra Rathor",
+    author_email="dheeru.rathor14@gmail.com",
+    license="BSD",
+    packages=["django_auth_ldap"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["django", "ldap", "authentication", "auth"],
+    install_requires=[
+        "django",
+        "pyldap" if PY3 else "python-ldap >= 2.0",
+    ],
+    setup_requires=[
+        "setuptools >= 0.6c11",
+    ],
+    tests_require=[
+        "mockldap >= 0.2.6",
+    ]
+)
