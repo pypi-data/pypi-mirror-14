60e75775e1712f4f6bc0c17680d6f0cc06c228d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package agnostic/0.8/agnostic-0.8.tar.gz

---
+++ packages/agnostic/agnostic-0.8.tar.gz/agnostic-0.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='agnostic',
+    version='0.8', # Update docs/conf.py also!
+    author='Mark E. Haase',
+    author_email='mehaase@gmail.com',
+    description='Agnostic Database Migrations',
+    license='MIT',
+    keywords='database migrations',
+    url='https://github.com/TeamHG-Memex/agnostic',
+    packages=['agnostic'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'Click',
+    ],
+    extras_require = {
+        'mysql': ['PyMySQL'],
+        'postgres': ['pg8000'],
+    },
+    entry_points='''
+        [console_scripts]
+        agnostic=agnostic.cli:main
+    '''
+)
