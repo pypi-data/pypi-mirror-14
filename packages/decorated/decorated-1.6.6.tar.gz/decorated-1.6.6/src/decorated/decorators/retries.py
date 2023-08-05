dd6c17264c676a699d64f92ba4c007b04591ca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package decorated/1.6.6/decorated-1.6.6.tar.gz

---
+++ packages/decorated/decorated-1.6.6.tar.gz/decorated-1.6.6/src/decorated/decorators/retries.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from decorated.base.function import Function
+import time
+
+class Retries(Function):
+    def _init(self, times, delay=0):
+        if times < 0:
+            raise Exception('Times cannot be negative.')
+        self._times = times
+        self._delay = delay
+        
+    def _call(self, *args, **kw):
+        for i in range(self._times + 1):
+            try:
+                return super(Retries, self)._call(*args, **kw)
+            except Exception as e:
+                last_error = e
+                if i < self._times - 1:
+                    time.sleep(self._delay)
+        else:
+            raise
+        
