ca844dcbae6d6a788703b8f88f97188822be4b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package lucky_nester/1.0.0/lucky_nester-1.0.0.zip

---
+++ packages/lucky_nester/lucky_nester-1.0.0.zip/lucky_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'lucky_nester',
+    version = '1.0.0',
+    py_module = ['lucky_nester'],
+    author	= 'lucky_joe',
+    author_email = 'joyang508@gmail.com',
+    url	= 'http://www.beamconvergence.com',
+    description = 'A simple printer of nested lists',
+    )
