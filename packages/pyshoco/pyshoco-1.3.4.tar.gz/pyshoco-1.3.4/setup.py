d169fed728a212061c580df63224678dbe077db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:00 +0000

    Add package pyshoco/1.3.4/pyshoco-1.3.4.tar.gz

---
+++ packages/pyshoco/pyshoco-1.3.4.tar.gz/pyshoco-1.3.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+import os
+
+if os.name == 'posix':
+    cflags = ['-std=c99']
+else:
+    cflags = None
+
+pyshoco = Extension('pyshoco', sources = ['pyshoco.c', 'shoco.c'], include_dirs = ['.'], extra_compile_args = cflags)
+
+setup(name = 'pyshoco', version = '1.3.4', description = 'Shoco Python wrapper module', ext_modules = [pyshoco], author = 'R. Rowe', author_email = 'mega.gamer05@gmail.com', url = 'https://pypi.python.org/pypi/pyshoco/')
