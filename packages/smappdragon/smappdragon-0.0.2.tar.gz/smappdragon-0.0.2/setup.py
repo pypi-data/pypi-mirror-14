af9101a1636277828e2bf87094e9112e521a356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package smappdragon/0.0.2/smappdragon-0.0.2.tar.gz

---
+++ packages/smappdragon/smappdragon-0.0.2.tar.gz/smappdragon-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup
+
+# if python version is above 3 then throw a message and exit
+if not sys.version_info.major < 3:
+    print "Hello python pirate! smappdragon is only for python versions below version 3.0!"
+    print "You appear to have python version:"
+    print "{}".format(sys.version_info.major)
+    sys.exit(1)
+
+setup(name='smappdragon',
+	packages=['smappdragon'],
+	version='0.0.2',
+	description='smappdragon is a set of tools for working with twitter data',
+	author='yvan',
+	author_email='yns207@nyu.edu',
+	url='https://github.com/SMAPPNYU/smappdragon',
+	keywords='twitter parse smappdragon',
+	license='MIT',
+	install_requires=[
+	  'pymongo>=3.2.1'
+	]
+)
