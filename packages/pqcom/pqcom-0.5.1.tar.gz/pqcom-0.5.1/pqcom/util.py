e0eb353533a680b4b3febccdb3f7f700315ef235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:46 +0000

    Add package pqcom/0.5.1/pqcom-0.5.1.tar.gz

---
+++ packages/pqcom/pqcom-0.5.1.tar.gz/pqcom-0.5.1/pqcom/util.py
@@ -0,0 +1,16 @@
+
+import sys
+import os
+import pkg_resources
+
+VERSION = 0.5.1
+
+script_path = os.path.dirname(sys.argv[0])
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', script_path)
+    full_path = os.path.join(base_path, relative_path)
+    if os.path.isfile(full_path):
+        return full_path
+    else:
+        return pkg_resources.resource_filename(__name__, relative_path)
