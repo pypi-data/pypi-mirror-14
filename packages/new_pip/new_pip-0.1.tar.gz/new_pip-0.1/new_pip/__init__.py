ec854ed9ec4abfbfdf9b1c94f3bf6d5db57b5634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package new_pip/0.1/new_pip-0.1.tar.gz

---
+++ packages/new_pip/new_pip-0.1.tar.gz/new_pip-0.1/new_pip/__init__.py
@@ -0,0 +1,6 @@
+class Email:
+	def check_email(self, email):
+		if email == 'oscar@oscar.com':
+			return True
+		else: 
+			return False
---
+++ packages/new_pip/new_pip-0.1.tar.gz/new_pip-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='new_pip',
+      version='0.1',
+      description="This tells us a joke",
+      url='http://github.com/oscarvazquez/new_pip',
+      author='Oscar Vazquez',
+      author_email='oscar.vazquez2012@gmail.com',
+      license='MIT',
+      packages=['new_pip'],
+      zip_safe=False)
