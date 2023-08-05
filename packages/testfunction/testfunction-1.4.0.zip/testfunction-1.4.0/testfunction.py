797a4f5bdd4c5cf6f2feb75012b471a4a9823d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:43 +0000

    Add package testfunction/1.4.0/testfunction-1.4.0.zip

---
+++ packages/testfunction/testfunction-1.4.0.zip/testfunction-1.4.0/testfunction.py
@@ -0,0 +1,16 @@
+"""这是testfunction.py模块，提供了一个名为print_lol（）的函数,
+用来打印列表，其中包含或不包含嵌套列表"""
+
+def print_lol(the_list,indent=False,level=0,fn=sys.stdout):
+	"""这个函数有一个位置参数，名为the_list,可以用于任何Python列表，包含或
+	不包含嵌套列表均可，所提供列表中的各个数据项会递归地打印到屏幕上，而且各占一行"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,level+1,fn)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t",end='',file=fn)
+			print(each_item,file=fn)
+
+
