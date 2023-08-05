6fdfe82c5050d1403dc10916f76f5a82c0860273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:25 +0000

    Add package pyslibtesseract/0.0.12/pyslibtesseract-0.0.12.tar.gz

---
+++ packages/pyslibtesseract/pyslibtesseract-0.0.12.tar.gz/pyslibtesseract-0.0.12/setup.py
@@ -0,0 +1,45 @@
+from distutils.command import build as build_module
+from setuptools import setup
+
+
+class build(build_module.build):
+    def run(self):
+        # Compile C code at src/cppcode
+        from subprocess import Popen, PIPE
+        import os
+
+        my_path = os.path.dirname(os.path.realpath(__file__))
+
+        print('Runing cmake at', my_path + '/src/cppcode/')
+        p = Popen(['cmake', '.'], stdout=PIPE, cwd=my_path + '/src/cppcode/')
+        print(p.stdout.read())
+        assert(p.wait() == 0)
+
+        if not os.path.exists(my_path + '/src/cppcode/Makefile'):
+            raise RuntimeError('Makefile was not generated!')
+
+        print('Runing make', my_path + '/src/cppcode/')
+        p = Popen(['make'], stdout=PIPE, cwd=my_path + '/src/cppcode/')
+        print(p.stdout.read())
+        assert(p.wait() == 0)
+
+        if not os.path.exists(my_path + '/src/cppcode/libpyslibtesseract.so'):
+            raise RuntimeError('pyslibtesseract.so was not generated!')
+
+        build_module.build.run(self)
+
+# Python setup
+setup(
+    name='pyslibtesseract',
+    version='0.0.12',
+    author='Bruno Macabeus',
+    description=('Integration of Tesseract for Python using a shared library'),
+    keywords='python-tesseract OCR Python',
+    url='https://github.com/brunomacabeusbr/pyslibtesseract',
+    packages=['pyslibtesseract'],
+    package_dir={'pyslibtesseract': 'src'},
+    cmdclass={
+        'build': build,
+    },
+    package_data={'pyslibtesseract': ['cppcode/main.cpp', 'cppcode/CMakeLists.txt', 'cppcode/libpyslibtesseract.so']},
+)
