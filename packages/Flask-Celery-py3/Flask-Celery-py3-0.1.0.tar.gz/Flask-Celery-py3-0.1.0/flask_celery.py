5c14d9d02e8116d66150d6e0b2e2fa422b3fb342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package Flask-Celery-py3/0.1.0/Flask-Celery-py3-0.1.0.tar.gz

---
+++ packages/Flask-Celery-py3/Flask-Celery-py3-0.1.0.tar.gz/Flask-Celery-py3-0.1.0/flask_celery.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+from celery import Celery as _Celery
+
+__all__ = ['Celery']
+
+
+class _CeleryConfig(object):
+
+    def __init__(self, celery):
+        self.celery = celery
+
+
+class Celery(_Celery):
+
+    def __init__(self, app=None, **kwargs):
+        super(Celery, self).__init__(**kwargs)
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.conf.update(app.config)
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['celery'] = _CeleryConfig(self)
