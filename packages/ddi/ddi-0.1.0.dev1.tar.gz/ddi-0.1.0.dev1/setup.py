f5373863e83e121071ae3d0773e99f9bc3ebe8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package ddi/0.1.0.dev1/ddi-0.1.0.dev1.tar.gz

---
+++ packages/ddi/ddi-0.1.0.dev1.tar.gz/ddi-0.1.0.dev1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ddi",
+    version="0.1.0.dev1",
+    description="DDI related tools",
+    url="http://ddionrails.org",
+    author="Marcel Hebing",
+    author_email="marcel@ddionrails.org",
+)
