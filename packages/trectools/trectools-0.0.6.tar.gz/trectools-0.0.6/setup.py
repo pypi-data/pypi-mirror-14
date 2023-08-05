c37f37b9e026fb96715e6916e0d6b1750b45dbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package trectools/0.0.6/trectools-0.0.6.tar.gz

---
+++ packages/trectools/trectools-0.0.6.tar.gz/trectools-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+#from Cython.Build import cythonize
+
+from setuptools import find_packages, setup
+from setuptools.command.install import install as _install
+from setuptools.command.develop import develop as _develop
+
+
+requirements = [
+    "pandas>=0.15.0",
+    "numpy>=1.0.0",
+    "scikit-learn>=0.15",
+    "scipy >= 0.10.0",
+]
+
+setup(name='trectools',
+        version='0.0.6',
+        author='Joao Palotti',
+        author_email='joaopalotti@gmail.com',
+        license='LICENSE.txt',
+        install_requires=requirements,
+        packages=['trectools'],
+        #package_dir = {'': '.'},
+        url='http://pypi.python.org/pypi/trectools/',
+        description='Python toolkit to read and analyse TREC results.',
+        long_description=open('README.txt').read()
+)
+
