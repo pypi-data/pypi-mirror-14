415d3683d3225566531fc2ce3b272904e4d2645c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:49 +0000

    Add package mongous/0.1.3/mongous-0.1.3.tar.gz

---
+++ packages/mongous/mongous-0.1.3.tar.gz/mongous-0.1.3/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name='mongous',
+      version='0.1.3',
+      description='A (very) thin Mongo CRUD wrapper library',
+      author='Joshua Austin',
+      author_email='nesher181@gmail.com',
+      url='https://github.com/nonproftechie/mongous',
+      py_modules=["mongous"],
+      download_url='https://github.com/nonproftechie/mongous/tarball/0.1.2',
+      keywords=['database', 'mongo', 'mongous']
+     )
