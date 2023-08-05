4108a87c21027fe2276069a9aa34739bab8da589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:13 +0000

    Add package jyb_nester1/1.0.0/jyb_nester1-1.0.0.zip

---
+++ packages/jyb_nester1/jyb_nester1-1.0.0.zip/jyb_nester1-1.0.0/jyb_nester1.py
@@ -0,0 +1,10 @@
+"""blablabla"""
+def print_lol(the_list,bn=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,bn,level+1)
+        else:
+            if bn:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
---
+++ packages/jyb_nester1/jyb_nester1-1.0.0.zip/jyb_nester1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='jyb_nester1',
+    version='1.0.0',
+    py_modules=['jyb_nester1'],
+    author='hfpython',
+    author_email='sdwhsssb199@163.com',
+    url='www.diablo3.cc',
+    description='just have fun',
+    )
