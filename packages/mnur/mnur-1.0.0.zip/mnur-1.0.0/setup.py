ad31ebf98ec54c06b4c89e76d6597706f1374895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package mnur/1.0.0/mnur-1.0.0.zip

---
+++ packages/mnur/mnur-1.0.0.zip/mnur-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         ="mnur",
+        version      ="1.0.0",
+        py_modules   =["wester"],
+        author       ="hfpython",
+        author_email ="hfpython@headfirstlabs.com",
+        url          ="http://www.headfirstlabs.com",
+        description  ="A simple printer of nested lists",
+     )
