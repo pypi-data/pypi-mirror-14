f152d0c463e714f98af5d96d6d879abe593367c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package chainz/0.12/chainz-0.12.tar.gz

---
+++ packages/chainz/chainz-0.12.tar.gz/chainz-0.12/chainz/__init__.py
@@ -0,0 +1,9 @@
+"""
+chainz is a lightweight module for chaining, functional operations on iterables.
+
+It includes:
+chainz.Chain: A wrapper class for iterables with chaining, functional methods.
+chainz.utils: A set of helper functions that can be used with Chain.
+"""
+
+from .chain import Chain
