4a5a4863044ae9215d0dae26df4569ff74a01c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:03 +0000

    Add package packermate/0.7.0/packermate-0.7.0.tar.gz

---
+++ packages/packermate/packermate-0.7.0.tar.gz/packermate-0.7.0/packermate/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'packermate'
+__version__ = '0.7.0'
+__author__ = 'Warren Moore'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2015 Warren Moore'
+
+from .command import Builder, BuilderException
+from .config import Config, ConfigException
+from .vagrant import parse_version, get_vagrant_output_file_names, get_vagrant_box_metadata
+from .process import run_command, ProcessException
---
+++ packages/packermate/packermate-0.7.0.tar.gz/packermate-0.7.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name = 'packermate',
+    version = '0.7.0',
+    description = 'Generate and run Packer build configurations from a simple YAML definition',
+    long_description = readme,
+    license = 'MIT',
+    author = 'Warren Moore',
+    author_email = 'warren@wamonite.com',
+    url = 'https://github.com/wamonite/packermate',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    packages = ['packermate'],
+    entry_points = dict(console_scripts = ['packermate=packermate.script:run']),
+    package_data = {
+        '': [
+            'LICENSE',
+        ],
+        'packermate': [
+            'data/*.json',
+            'data/templates/*.template'
+        ]
+    },
+    install_requires = [
+        'pyaml',
+        'semantic_version',
+        'requests',
+    ],
+    setup_requires = [
+        'pytest-runner',
+    ],
+    tests_require = [
+        'pytest-cov'
+    ],
+    zip_safe = False
+)
