d66f66837fc797d7b9fe63859a3adae0d37e1a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:32 +0000

    Add package listprinting/1.3.0/listprinting-1.3.0.zip

---
+++ packages/listprinting/listprinting-1.3.0.zip/listprinting-1.3.0/listprinting.py
@@ -0,0 +1,16 @@
+"""This is the "listprinting.py" module and it provides one function called
+list_printing() which prints lists that may or may not include nested lists."""
+def list_printing(the_list, indent=False, level=0):
+    """This function takes one positional argument called "the_list", which
+is any Python list (or - possibly - nested lists). Each data item in the
+provided list is (recurcively) printed to the screen on it's own line.
+A second argument called "level" is used to insert tab-stops when a nested
+list is encountered."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            list_printing(each_item, indent, level + 1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end="")
+            print(each_item)
---
+++ packages/listprinting/listprinting-1.3.0.zip/listprinting-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = "listprinting",
+    version      = "1.3.0",
+    py_modules   = ["listprinting"],
+    author       = "eugurlubaylar",
+    author_email = "eugurlubaylar@hotmail.com",
+    url          = "http://www.eugurlubaylar.com",
+    description  = "A simple printer of nested lists",
+    )
