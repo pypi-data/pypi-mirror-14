5fabad368af82a12a1c7025461d5539609be8f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:17 +0000

    Add package deadbeef/0.0.0.0.0.12.0.1.14.12.4.7.5/deadbeef-0.0.0.0.0.12.0.1.14.12.4.7.5.tar.gz

---
+++ packages/deadbeef/deadbeef-0.0.0.0.0.12.0.1.14.12.4.7.5.tar.gz/deadbeef-0.0.0.0.0.12.0.1.14.12.4.7.5/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="deadbeef",
+    version="0.0.0.0.0.12.0.1.14.12.4.7.5",
+    author="Yan Shoshitaishvili",
+    description="deadbeef makes it easy to generate hexspeak!",
+    packages=['deadbeef']
+)
