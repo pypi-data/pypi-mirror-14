b09785613d40862ccd97efd23e3663a3f07f671a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:17 +0000

    Add package python-date-interval-generator/0.0.1/python-date-interval-generator-0.0.1.tar.gz

---
+++ packages/python-date-interval-generator/python-date-interval-generator-0.0.1.tar.gz/python-date-interval-generator-0.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+import sys
+
+here = path.abspath(path.dirname(__file__))
+
+# Python version-independent
+install_requires = ['dateutil']
+
+if sys.version_info < (3,4):
+    install_requires = ['enum34']
+
+# http://stackoverflow.com/a/26737672/877069
+#try:
+#    import pypandoc
+#    long_description = pypandoc.convert('README.md', 'rst')
+#except(IOError, ImportError):
+long_description = open('README.txt').read()
+
+setup(
+    name='python-date-interval-generator',
+    version='0.0.1',
+    description='Generates common sequential date intervals for a given date range',
+    # long_description=long_description, # TODO does this come 'magically'?
+    author='@nutso',
+    author_email='nutsoapps@gmail.com',
+    url='https://github.com/nutso/python-date-interval-generator',
+    # TODO license
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='date interval dateutil',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=install_requires,
+    extras_require={
+        'dev': [], # TODO
+        'test': [], # TODO
+    },
+    test_suite="tests",
+)
