223d4a8e6004011ba3a244d9e3fe80d945923049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package ship_it/0.5.0/ship_it-0.5.0.tar.gz

---
+++ packages/ship_it/ship_it-0.5.0.tar.gz/ship_it-0.5.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='ship_it',
+    version='0.5.0',
+    install_requires=['fabric', 'PyYaml', 'six', 'virtualenv'],
+    packages=['ship_it'],
+    url='github.com/robdennis/ship_it',
+    license='MIT',
+    author='Rob Dennis',
+    author_email='rdennis+ship_it@gmail.com',
+    description='thin build wrapper around fpm that enforces some best '
+                'practices for created deb/rpms of python applications'
+)
