f63d1d3c23d19d182a05d171487510bfe953e30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package gofast/0.2/gofast-0.2.tar.gz

---
+++ packages/gofast/gofast-0.2.tar.gz/gofast-0.2/setup.py
@@ -0,0 +1,18 @@
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+from distutils.core import setup
+setup(
+        name = 'gofast',
+        packages = ['gofast'], # this must be the same as the name above
+        version = '0.2',
+        description = 'A javascript style promises for python that uses multicore and threads combined.  Go Fast',
+        author = 'Greg McGregor',
+        author_email = 'greg@brightappsllc.com',
+        url = 'https://github.com/gregmcgregor/python-promises', # use the URL to the github repo
+        download_url = 'https://github.com/gregmcgregor/python-promises/archive/master.zip', # I'll explain this in a second
+        keywords = ['multicore', 'threads', 'futures', 'promise', 'mixmatch'], # arbitrary keywords
+        classifiers = [],
+)
