a1b4cf1668313b61544c86992ec6654ca049affa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package pyrpncalc/0.1.10/pyrpncalc-0.1.10.tar.gz

---
+++ packages/pyrpncalc/pyrpncalc-0.1.10.tar.gz/pyrpncalc-0.1.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pyrpncalc',
+      version='0.1.10',
+      description="""A Reverse Polish Notation (postfix) module with extensive operation support.""",
+      long_description="Use your favorite numeric input style, RPN, right on your desktop! This module is primarily designed for use in interactive mode, but may also be used by other modules. Run with 'python3 -m pyrpncalc'",
+      keywords="math calculator utility command line",
+      url='https://bitbucket.org/TheCDC/pyrpn',
+      author='Christopher Chen',
+      author_email='christopher.chen1995@gmail.com',
+      license='Public Domain',
+      packages=['pyrpncalc'],
+      downloads=["https://bitbucket.org/TheCDC/pyrpn/downloads"],
+      install_requires=[],
+      zip_safe=True)
