28d3aa6ff4a5426ab05d35f018495c8f229b37af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package simuvex/4.6.3.18/simuvex-4.6.3.18.tar.gz

---
+++ packages/simuvex/simuvex-4.6.3.18.tar.gz/simuvex-4.6.3.18/simuvex/procedures/libc___so___6/pthread_mutex_unlock.py
@@ -0,0 +1,10 @@
+import simuvex
+
+######################################
+# Doing nothing
+######################################
+
+class pthread_mutex_unlock(simuvex.SimProcedure):
+    def run(self):
+        _ = self.arg(0)
+        self.ret()
