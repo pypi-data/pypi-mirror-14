25c37dc8e2e701d66fe16998cf45c12c2259ff74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:34 +0000

    Add package django-user/0.2.1/django-user-0.2.1.tar.gz

---
+++ packages/django-user/django-user-0.2.1.tar.gz/django-user-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-user',
+      version='0.2.1',
+      description='Django User',
+      url='https://github.com/jacothiart/django-user',
+      author='Jaco Thiart',
+      author_email='jaco@jacothiart.com',
+      license='MIT',
+      packages=['user'],
+      zip_safe=False)
