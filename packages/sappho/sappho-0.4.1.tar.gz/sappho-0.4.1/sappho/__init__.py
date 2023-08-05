30594ae41cf3df839b8b4767b2e9d2fe6b6002b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:53 +0000

    Add package sappho/0.4.1/sappho-0.4.1.tar.gz

---
+++ packages/sappho/sappho-0.4.1.tar.gz/sappho-0.4.1/sappho/__init__.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    __version__ = "0.4.1"
+else:
+    from animatedsprite import AnimatedSprite
+    from tilemap import TileMap, Tilesheet, tmx_file_to_tilemaps
+    from layers import SurfaceLayers
+    from camera import Camera
