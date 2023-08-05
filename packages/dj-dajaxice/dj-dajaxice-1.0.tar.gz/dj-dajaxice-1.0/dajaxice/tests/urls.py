9d5f283aa7072846f11afc57e7c0990726561edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package dj-dajaxice/1.0/dj-dajaxice-1.0.tar.gz

---
+++ packages/dj-dajaxice/dj-dajaxice-1.0.tar.gz/dj-dajaxice-1.0/dajaxice/tests/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
+dajaxice_autodiscover()
+
+urlpatterns = [
+    # Dajaxice URLS
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+]
---
+++ packages/dj-dajaxice/dj-dajaxice-1.0.tar.gz/dj-dajaxice-1.0/examples/examples/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+from simple import views as simple_views
+
+dajaxice_autodiscover()
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'examples.views.home', name='home'),
+    # url(r'^examples/', include('examples.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    url(r'', simple_views.index)
+]
+
+urlpatterns += staticfiles_urlpatterns()
