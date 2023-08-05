20e08ebe3b03d0bf1b29f06957d36494646e8625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:41 +0000

    Add package chunyun/0.1/chunyun-0.1.tar.gz

---
+++ packages/chunyun/chunyun-0.1.tar.gz/chunyun-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="chunyun",
+    version="0.1",
+    description="数据库迁移工具",
+    url="http://github.com/erhuabushuo/chunyun",
+    author="疯人院主任",
+    author_email="erhuabushuo@gmail.com",
+    license="MIT",
+    packages=['chunyun'],
+    zip_safe=False
+)
