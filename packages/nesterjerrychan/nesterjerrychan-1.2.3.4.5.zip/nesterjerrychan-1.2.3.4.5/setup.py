d67d6364b00a5d0c82412703ef590e6db61a85f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package nesterjerrychan/1.2.3.4.5/nesterjerrychan-1.2.3.4.5.zip

---
+++ packages/nesterjerrychan/nesterjerrychan-1.2.3.4.5.zip/nesterjerrychan-1.2.3.4.5/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+		name        = 'nesterjerrychan',
+		version     = '1.2.3.4.5',
+		py_modules  = ['nesterjerrychan'],
+		author      = 'jerrychan',
+
+		)
