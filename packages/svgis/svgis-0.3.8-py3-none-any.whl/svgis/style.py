0cc373cb01c9d1a981549f17b8813ea83da48a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package svgis/0.3.8/svgis-0.3.8-py3-none-any.whl

---
+++ packages/svgis/svgis-0.3.8-py3-none-any.whl/svgis/style.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+import re
+from string import ascii_letters
+import os.path
+import logging
+try:
+    import xml.etree.cElementTree as ElementTree
+except ImportError:
+    import xml.etree.ElementTree as ElementTree
+import tinycss
+from . import dom
+
+'''
+Utilities for messing with SVG styling.
+'''
+
+
+def _register():
+    ElementTree.register_namespace('', dom.SVG_NS)
+
+
+def sanitize(string):
+    '''
+    Make input safe of use in an svg ID or class field.
+    Replaces blocks of whitespace with an underscore (``_``).
+    If the first character isn't an ascii letter, dash (``-``)
+    or underscore (``_``), an underscore is added to the beginning.
+
+    Args:
+        string (mixed): Input to sanitize. Will be immediately coerced to
+                        unicode. If that fails, returns an empty string.
+    '''
+    try:
+        string = re.sub(r'\s+', '_', str(string))
+        string = string if string[0] in ('_-' + ascii_letters) else '_' + string
+        return str(string)
+
+    except (AttributeError, IndexError):
+        return ''
+
+
+def pick(style):
+    '''
+    Fetch a CSS string.
+
+    Args:
+        style (str): Either a CSS string or the path of a css file.
+    '''
+    try:
+        _, ext = os.path.splitext(style)
+        if ext == '.css':
+            with open(style) as f:
+                return f.read()
+
+    except AttributeError:
+        # Probably style is None.
+        return None
+
+    except IOError:
+        logging.getLogger('svgis').warning("Couldn't read %s, proceeding with default style", style)
+
+    return style
+
+
+def rescale(svgfile, factor):
+    _register()
+    try:
+        svg = ElementTree.parse(svgfile)
+        scalar = 'scale({})'.format(factor)
+        g = svg.getroot().find(dom.ns('g'))
+        g.attrib['transform'] = (g.attrib.get('transform') + ' ' + scalar).strip()
+
+    except AttributeError:
+        raise
+
+    return ElementTree.tostring(svg.getroot(), encoding='utf-8').decode('utf-8')
+
+
+def add_style(svgfile, style, replace=False):
+    '''
+    Add to or replace the CSS style in an SVG file.
+
+    Args:
+        svgfile (str): Path to an SVG file or an SVG string.
+        newstyle (str): CSS string, or path to CSS file.
+        replace (bool): If true, replace the existing CSS with newstyle (default: False)
+    '''
+    _register()
+
+    if style == '-':
+        style = '/dev/stdin'
+
+    root, ext = os.path.splitext(style)
+
+    if ext == '.css' or root == '/dev/stdin':
+        with open(style) as f:
+            style = f.read()
+
+    try:
+        svg = ElementTree.parse(svgfile).getroot()
+    except IOError:
+        try:
+            svg = ElementTree.fromstring(svgfile)
+        except UnicodeDecodeError:
+            svg = ElementTree.fromstring(svgfile.encode('utf-8'))
+
+    if svg.find(dom.ns('defs')) is not None:
+        defs = svg.find(dom.ns('defs'))
+
+    else:
+        defs = ElementTree.Element(dom.ns('defs'))
+        svg.insert(0, defs)
+
+    if defs.find(dom.ns('style')) is not None:
+        style_element = defs.find(dom.ns('style'))
+
+        if not replace:
+            # Append CSS.
+            existing = style_element.text or ''
+            style = dom.cdata(existing + ' ' + style)
+            style_element.text = ''
+
+    else:
+        style_element = ElementTree.Element(dom.ns('style'))
+        defs.append(style_element)
+
+    style_element.text = dom.cdata(style)
+
+    return ElementTree.tostring(svg, encoding='utf-8').decode('utf-8')
+
+
+def inline(svg, style=None):
+    '''
+    Inline the CSS rules in an SVG. This is a very rough operation,
+    and full css precedence rules won't be respected. Ignores sibling
+    operators (``~``, ``+``), psuedo-selectors (e.g. ``:first-child``), and
+    attribute selectors (e.g. ``.foo[name=bar]``). Works best with rules like:
+
+    * ``.class``
+    * ``tag``
+    * ``tag.class``
+    * ``#layer .class``
+    * ``#layer tag``
+
+    Args:
+        svg (string): An SVG document.
+        css (string): CSS to use, instead of the CSS in the <defs> element of the SVG.
+    '''
+    _register()
+    try:
+        doc = ElementTree.fromstring(svg.encode('utf-8'))
+        wrap = doc.find('./' + dom.ns('g'))
+
+        if not style:
+            path = './' + dom.ns('defs') + '/' + dom.ns('style')
+            try:
+                style = doc.findall(path).pop().text
+            except IndexError:
+                style = ''
+
+        stylesheet = _parse_css(style)
+
+        for rule in stylesheet.rules:
+            dom.apply_rule(wrap, rule)
+
+        return ElementTree.tostring(doc, encoding='utf-8').decode('utf-8')
+
+    # Return plain old SVG.
+    except (AttributeError, NameError) as e:
+        logging.getLogger('svgis').warning("Unable to inline CSS becuase: %s", e)
+        return svg
+
+
+def _parse_css(stylesheet):
+    mini = re.sub(r'\s+([,>~+])\s+', r'\1', stylesheet)
+    return tinycss.make_parser().parse_stylesheet(mini)
---
+++ packages/svgis/svgis-0.3.8-py3-none-any.whl/svgis/svg.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+'''
+Create string versions of SVG elements.
+'''
+
+
+def _wrap(tag, contents=None, **kwargs):
+    '''Wrap contents in a tag'''
+    attribs = toattribs(**kwargs)
+    if contents:
+        return '<{0}{1}>{2}</{0}>'.format(tag, attribs, contents)
+    else:
+        return '<{0}{1}/>'.format(tag, attribs)
+
+
+def _element(tag, **kwargs):
+    return '<{}{}/>'.format(tag, toattribs(**kwargs))
+
+
+def _round(i, precision):
+    if precision is None:
+        return i
+    else:
+        return round(i, precision)
+
+
+def _fmtcoord(precision):
+    if precision is None:
+        return '{0[0]},{0[1]}'
+    else:
+        return '{{0[0]:.{0}f}},{{0[1]:.{0}f}}'.format(precision)
+
+
+def circle(point, precision=None, **kwargs):
+    '''
+    Create a svg circle element. Keyword arguments are mapped to attributes.
+
+    Args:
+        point (tuple): The center of the circle
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    return _element('circle', cx=_round(point[0], precision), cy=_round(point[1], precision), **kwargs)
+
+
+def text(string, start, precision=None, **kwargs):
+    '''
+    Create an svg text element.
+
+    Args:
+        string (str): text for element
+        start (tuple): starting coordinate
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    return _wrap('text', string, x=start[0], y=start[1], **kwargs)
+
+
+def rect(start, width, height, precision=None, **kwargs):
+    '''
+    Create an svg rect element.
+
+    Args:
+        start (tuple): starting coordinate
+        width (int): rect width
+        height (int): rect height
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    width = _round(width, precision)
+    height = _round(height, precision)
+
+    return _element('rect', x=start[0], y=start[1], width=width, height=height, **kwargs)
+
+
+def line(start, end, precision=None, **kwargs):
+    '''
+    Create an svg line element.
+
+    Args:
+        start (tuple): starting coordinate
+        end (tuple): ending coordinate
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    start = [_round(i, precision) for i in start]
+    end = [_round(i, precision) for i in end]
+
+    return _element('line', x1=start[0], y1=start[1], x2=end[0], y2=end[1], **kwargs)
+
+
+def _isstr(x):
+    return isinstance(x, str)
+
+
+def path(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg path element as a string.
+
+    Args:
+        coordinates (Sequence): A sequence of coordinates and string instructions
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    coords = [i if _isstr(i) else fmt.format(i) for i in coordinates]
+    return _element('path', d=' '.join(coords), **kwargs)
+
+
+def polyline(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg polyline element
+
+    Args:
+        coordinates (Sequence): x, y coordinates
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    points = ' '.join(fmt.format(c) for c in coordinates)
+    return _element('polyline', points=points, **kwargs)
+
+
+def polygon(coordinates, precision=None, **kwargs):
+    '''
+    Create an svg polygon element
+
+    Args:
+        coordinates (Sequence): x, y coordinates
+        precision (int): rounding precision
+
+    Returns:
+        str
+    '''
+    fmt = _fmtcoord(precision)
+    points = ' '.join(fmt.format(c) for c in coordinates)
+    return _element('polygon', points=points, **kwargs)
+
+
+def toattribs(**kwargs):
+    attribs = ' '.join('{}="{}"'.format(k, v) for k, v in list(kwargs.items()) if v is not None)
+
+    if len(attribs) > 0:
+        attribs = ' ' + attribs
+
+    return attribs
+
+
+def defstyle(style=None):
+    '''
+    Create a defs element that wraps a CSS style.
+
+    Args:
+        style (string): A CSS string.
+
+    Returns:
+        unicode
+    '''
+    if style:
+        return '<defs><style type="text/css"><![CDATA[{}]]></style></defs>'.format(style)
+    else:
+        return '<defs />'
+
+
+def group(members=None, **kwargs):
+    '''
+    Create a group with the given scale and translation.
+
+    Args:
+        members (Sequence): unicode SVG elements
+        kwargs (dict): elements of this dictionary will be converted to
+                        attributes of the group, i.e. key="value".
+
+    Returns:
+        unicode
+    '''
+    members = members or ''
+    return _wrap('g', ''.join(members), **kwargs)
+
+
+def drawing(size, members, viewbox=None, style=None):
+    '''
+    Create an SVG element.
+
+    Args:
+        size (tuple): width, height
+        members (list): Strings to add to output.
+        viewbox (Sequence): Four coordinates that describe an SVG viewBox.
+        style (string): CSS string.
+    '''
+    kwargs = {
+        'width': size[0],
+        'height': size[1],
+        'baseProfile': 'full',
+        'version': '1.1',
+        'xmlns': 'http://www.w3.org/2000/svg',
+    }
+    if viewbox:
+        kwargs['viewBox'] = '{},{},{},{}'.format(*viewbox)
+
+    contents = defstyle(style) + ''.join(members)
+    return _wrap('svg', contents, **kwargs)
