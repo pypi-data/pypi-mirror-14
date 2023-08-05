17d4afd4c96e73291537116e7059c6b29337a8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package hiphip/1.0.0-alpha.2/hiphip-1.0.0-alpha.2.tar.gz

---
+++ packages/hiphip/hiphip-1.0.0-alpha.2.tar.gz/hiphip-1.0.0-alpha.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='hiphip',
+  description='',
+  version='1.0.0-alpha.2',
+  url='https://github.com/nikitanovosibirsk/hiphip',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=find_packages()
+)
