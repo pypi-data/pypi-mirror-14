e8f9efc5e8c6c3df8b53d8e5aa3afafe7b403ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package ocflib/2016.3.9.23.31/ocflib-2016.3.9.23.31.tar.gz

---
+++ packages/ocflib/ocflib-2016.3.9.23.31.tar.gz/ocflib-2016.3.9.23.31/ocflib/infra/hosts.py
@@ -0,0 +1,22 @@
+"""Methods for working with OCF hosts."""
+import ldap3
+
+import ocflib.constants as constants
+import ocflib.infra.ldap as ldap
+
+
+# TODO: add switch here
+HOST_TYPES_WITH_IPV6 = frozenset({'server', 'desktop', 'dhcp'})
+
+
+def hosts_by_filter(ldap_filter):
+    """Return a list of hosts satisfying the LDAP filter."""
+
+    with ldap.ldap_ocf() as c:
+        c.search(
+            constants.OCF_LDAP_HOSTS,
+            ldap_filter,
+            attributes=ldap3.ALL_ATTRIBUTES,
+        )
+
+        return [entry['attributes'] for entry in c.response]
