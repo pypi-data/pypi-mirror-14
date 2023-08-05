2528e3890441eb73534f1db659243ff6eeacbe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:30 +0000

    Add package easyproc/0.1.0/easyproc-0.1.0.tar.gz

---
+++ packages/easyproc/easyproc-0.1.0.tar.gz/easyproc-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='easyproc',
+    version='0.1.0',
+    description='thin abstraction on subprocess.run to simplify admin scripts',
+    url='https://github.com/ninjaaron/easyproc',
+    author='Aaron Christianson',
+    author_email='ninjaaron@gmail.com',
+    keywords='shell pipe subprcess process scripting',
+    classifiers=['Programming Language :: Python :: 3.5'],
+    )
+
