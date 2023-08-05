a2742c7789e4a21be3c45a7afa2ca47535b59383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package ostrichlib/0.0.0.dev2/ostrichlib-0.0.0.dev2.tar.gz

---
+++ packages/ostrichlib/ostrichlib-0.0.0.dev2.tar.gz/ostrichlib-0.0.0.dev2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+import ostrich
+
+setup(
+    name='ostrichlib',
+    version=ostrich.__version__,
+    author=ostrich.__author__,
+    author_email='python@ostricher.com',
+    url='https://www.ostricher.com/python',
+    description=ostrich.__oneliner__,
+    packages=['ostrich'],
+    setup_requires=['pytest-runner'],
+    extras_require={
+        'test': ['pytest', 'pytest-cov', 'pytest-pep8'],
+    },
+    zip_safe=True,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
