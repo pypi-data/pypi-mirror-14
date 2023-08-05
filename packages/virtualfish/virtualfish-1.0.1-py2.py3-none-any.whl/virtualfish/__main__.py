a2f016740a373d17c9eeae8a1542681b2032221c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package virtualfish/1.0.1/virtualfish-1.0.1-py2.py3-none-any.whl

---
+++ packages/virtualfish/virtualfish-1.0.1-py2.py3-none-any.whl/virtualfish/__main__.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import os
+import sys
+
+
+if __name__ == "__main__":
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    commands = ['. {}'.format(os.path.join(base_path, 'virtual.fish'))]
+
+    for plugin in sys.argv[1:]:
+        path = os.path.join(base_path, plugin + '.fish')
+        if os.path.exists(path):
+            commands.append('. {}'.format(path))
+        else:
+            print('virtualfish loader error: plugin {} does not exist!'.format(plugin), file=sys.stderr)
+
+    commands.append('emit virtualfish_did_setup_plugins')
+    print(';'.join(commands))
