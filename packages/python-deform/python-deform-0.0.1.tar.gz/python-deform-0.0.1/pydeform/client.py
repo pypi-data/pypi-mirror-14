c285548bdbe0d18ec575d6fdf92fad2c98a1e08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package python-deform/0.0.1/python-deform-0.0.1.tar.gz

---
+++ packages/python-deform/python-deform-0.0.1.tar.gz/python-deform-0.0.1/pydeform/client.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+import requests
+
+from pydeform.auth import (
+    get_session_id,
+    get_session_http_auth_header,
+    get_token_http_auth_header,
+)
+from pydeform.utils import (
+    get_base_uri,
+    uri_join,
+)
+from pydeform.resources import (
+    CurrentProjectInfoResource,
+    ProjectListResource,
+    ProjectOneResource,
+    NonAuthUserResource,
+    SessionUserResource,
+    CollectionListResource,
+    CollectionOneResource,
+    DocumentListResource,
+    DocumentOneResource,
+)
+
+
+class Client(object):
+    def __init__(self,
+                 host,
+                 port=None,
+                 secure=True,
+                 requests_session=None,
+                 request_defaults=None,
+                 api_base_path='/api/'):
+        self.host = host
+        self.port = port
+        self.secure = secure
+        self.requests_session = requests_session or requests.Session()
+        self.request_defaults = request_defaults
+        self.api_base_path = api_base_path
+        self.user = NonAuthUserResource(
+            base_uri=get_base_uri(
+                host=self.host,
+                port=self.port,
+                secure=self.secure,
+                api_base_path=self.api_base_path
+            ),
+            auth_header=None,
+            requests_session=requests_session,
+            request_defaults=request_defaults
+        )
+
+    def auth(self, auth_type, auth_key, project_id=None):
+        if auth_type == 'session':
+            return SessionAuthClient(
+                auth_header=get_session_http_auth_header(auth_key),
+                host=self.host,
+                port=self.port,
+                secure=self.secure,
+                requests_session=self.requests_session,
+                request_defaults=self.request_defaults,
+                api_base_path=self.api_base_path,
+            )
+        elif auth_type == 'token':
+            if not project_id:
+                raise ValueError('You should provide project_id for token authentication')
+            return ProjectClient(
+                base_uri=get_base_uri(
+                    project=project_id,
+                    host=self.host,
+                    port=self.port,
+                    secure=self.secure,
+                    api_base_path=self.api_base_path
+                ),
+                auth_header=get_token_http_auth_header(auth_key),
+                requests_session=self.requests_session,
+                request_defaults=self.request_defaults,
+            )
+
+
+class SessionAuthClient(object):
+    def __init__(self,
+                 auth_header,
+                 host,
+                 port,
+                 secure,
+                 requests_session,
+                 request_defaults,
+                 api_base_path):
+        self.host = host
+        self.port = port
+        self.secure = secure
+        self.requests_session = requests_session
+        self.request_defaults = request_defaults
+        self.auth_header = auth_header
+        self.api_base_path = api_base_path
+        self.base_uri = get_base_uri(
+            host=self.host,
+            port=self.port,
+            secure=self.secure,
+            api_base_path=self.api_base_path
+        )
+        resource_kwargs = {
+            'base_uri': self.base_uri,
+            'auth_header': auth_header,
+            'requests_session': requests_session,
+            'request_defaults': request_defaults
+        }
+        self.user = SessionUserResource(**resource_kwargs)
+        self.projects = ProjectListResource(**resource_kwargs)
+        self.project = ProjectOneResource(**resource_kwargs)
+
+    def use_project(self, project_id):
+        return ProjectClient(
+            base_uri=get_base_uri(
+                project=project_id,
+                host=self.host,
+                port=self.port,
+                secure=self.secure,
+                api_base_path=self.api_base_path
+            ),
+            auth_header=self.auth_header,
+            requests_session=self.requests_session,
+            request_defaults=self.request_defaults,
+        )
+
+
+class ProjectClient(object):
+    def __init__(self,
+                 base_uri,
+                 auth_header,
+                 requests_session,
+                 request_defaults):
+        resource_kwargs = {
+            'base_uri': base_uri,
+            'auth_header': auth_header,
+            'requests_session': requests_session,
+            'request_defaults': request_defaults
+        }
+        self.base_uri = base_uri
+        self.auth_header = auth_header
+        self.request_session = requests_session
+        self.request_defaults = request_defaults
+        self.info = CurrentProjectInfoResource(**resource_kwargs)
+        self.collections = CollectionListResource(**resource_kwargs)
+        self.collection = CollectionOneResource(**resource_kwargs)
+        self.documents = DocumentListResource(**resource_kwargs)
+        self.document = DocumentOneResource(**resource_kwargs)
---
+++ packages/python-deform/python-deform-0.0.1.tar.gz/python-deform-0.0.1/pydeform/exceptions.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from requests.exceptions import (
+    ConnectionError as RequestsConnectionError,
+    ConnectTimeout as RequestsConnectTimeout,
+    ReadTimeout as RequestsReadTimeout,
+)
+
+
+class DeformException(Exception):
+    """Base Deform.io exception"""
+    message = 'Deform error'
+
+
+class HTTPError(DeformException):
+    """Errors produced at the HTTP layer."""
+    message = 'Http error'
+    errors = []
+
+    def __init__(self, requests_error):
+        # todo: test me
+        self.requests_error = requests_error
+        if self.requests_error.response is not None:
+            message = self.requests_error.response.json().get('result', {}).get('message')
+            if message:
+                self.message = message
+            errors = self.requests_error.response.json().get('result', {}).get('errors')
+            if errors:
+                self.errors = errors
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return self.message
+
+
+class AuthError(HTTPError):
+    """Errors due to invalid authentication credentials."""
+    message = 'Auth error'
+
+
+class ForbiddenError(HTTPError):
+    message = 'Forbidden error'
+
+
+class NotFoundError(HTTPError):
+    message = 'Not found error'
+
+
+class ValidationError(HTTPError):
+    message = 'Validation error'
+
+
+class ConflictError(HTTPError):
+    message = 'Conflict error'
+
+
+class ConnectionError(HTTPError):
+    """A Connection error occurred."""
+    message = 'Connection error'
+
+
+class Timeout(HTTPError):
+    """The request timed out.
+    Catching this error will catch both
+    :exc:`~pydeform.exceptions.ConnectTimeout` and
+    :exc:`~pydeform.exceptions.ReadTimeout` errors.
+    """
+    message = 'Timeout'
+
+
+class ConnectTimeout(ConnectionError, Timeout):
+    """The request timed out while trying to connect to the remote server.
+    Requests that produced this error are safe to retry.
+    """
+    message = 'Connect timeout'
+
+
+class ReadTimeout(Timeout):
+    """The server did not send any data in the allotted amount of time."""
+    message = 'Read timeout'
+
+
+STATUS_CODE_ERROR_MAP = {
+    401: AuthError,  # todo: test me
+    403: ForbiddenError,  # todo: test me
+    404: NotFoundError,  # todo: test me
+    409: ConflictError,  # todo: test me
+    422: ValidationError,  # todo: test me
+}
+
+REQUESTS_ERROR_MAP = {
+    RequestsConnectionError: ConnectionError,
+    RequestsConnectTimeout: ConnectTimeout, # todo: test me
+    RequestsReadTimeout: ReadTimeout,
+}
