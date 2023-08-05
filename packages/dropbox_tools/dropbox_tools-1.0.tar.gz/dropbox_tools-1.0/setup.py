8da2ab890dcce59d629702560ebaf9f8748a7d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:22 +0000

    Add package dropbox_tools/1.0/dropbox_tools-1.0.tar.gz

---
+++ packages/dropbox_tools/dropbox_tools-1.0.tar.gz/dropbox_tools-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name ='dropbox_tools',
+      version = '1.0',
+      description = 'A collection of command line tools for Dropbox',
+      url = 'http://github.com/agileronin/dropbox_tools',
+      author = 'Gregory Mundy',
+      license = 'MIT',
+      packages = ['dropbox_tools'],
+      install_requires = [
+          'dropbox'
+      ],
+      zip_safe = False)
+
