8ed7f5280b5b1aab2bca8bf0e25883d3f4147e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:08 +0000

    Add package icecake/0.2.0/icecake-0.2.0.tar.gz

---
+++ packages/icecake/icecake-0.2.0.tar.gz/icecake-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='icecake',
+    version='0.2.0',
+    py_modules=['icecake', 'templates'],
+    url="https://github.com/cbednarski/icecake",
+    author="Chris Bednarski",
+    author_email="banzaimonkey@gmail.com",
+    description="An easy and cool static site generator",
+    license="MIT",
+    install_requires=[
+        'Click',
+        'Jinja2',
+        'Markdown',
+        'Pygments',
+        'python-dateutil',
+        'Werkzeug',
+    ],
+    entry_points='''
+        [console_scripts]
+        icecake=icecake:cli
+    ''',
+    keywords="static site generator builder icecake"
+)
