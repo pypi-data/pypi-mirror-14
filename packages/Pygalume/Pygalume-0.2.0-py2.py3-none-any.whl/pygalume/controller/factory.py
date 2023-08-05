2cbd5a2ee7ba26195aa8bc944fc2525504967f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package Pygalume/0.2.0/Pygalume-0.2.0-py2.py3-none-any.whl

---
+++ packages/Pygalume/Pygalume-0.2.0-py2.py3-none-any.whl/pygalume/controller/factory.py
@@ -0,0 +1,50 @@
+from .database import DataBase
+from .api import API
+
+
+class Factory():
+
+    def __init__(self):
+        self._db = DataBase()
+        self._api = API()
+
+    def getLyrics(self, artist, music):
+
+        try:
+            if self._db.testIfExist(artist, music):
+                lyrics = self._db.getLyrics(artist, music)
+
+                if self._db.testIfExpired(lyrics):
+                    new_lyrics = self._api.getLyrics(artist, music)
+                    self._db.updateLyrics(lyrics, new_lyrics)
+
+            else:
+                lyrics = self._api.getLyrics(artist, music)
+                self._db.addLyrics(lyrics)
+        except:
+            raise
+
+        return lyrics
+
+    def getDiscography(self, artist):
+
+        try:
+            return self._api.getDiscography(artist)
+
+        except:
+            raise
+
+    def getSongs(self, artist, album):
+
+        try:
+            return self._api.getSongs(artist, album)
+
+        except:
+            raise
+
+    def getCachedSongs(self):
+
+        try:
+            return self._db.getCachedSongs()
+        except:
+            raise
