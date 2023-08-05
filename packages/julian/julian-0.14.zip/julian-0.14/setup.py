2a6ae08c75ed1dc4c033056bb72a0a44d3237a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:04 +0000

    Add package julian/0.14/julian-0.14.zip

---
+++ packages/julian/julian-0.14.zip/julian-0.14/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = ''
+
+
+setup(
+  name='julian',
+  packages=['julian'],
+  version='0.14',
+  description='Simple library for converting between Julian calendar dates and datetime objects',
+  long_description=long_description,
+  author='Daniel Zawada',
+  author_email='zawadadaniel@gmail.com',
+  url='https://github.com/dannyzed/julian',
+  download_url='https://github.com/dannyzed/julian/tarball/0.14',
+  keywords=['julian', 'calendar', 'datetime'],
+  classifiers=[],
+)
