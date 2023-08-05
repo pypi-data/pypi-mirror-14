70f6ced694cea10cdd3c78eb23862fa960c89030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:10 +0000

    Add package ListPrint_wang/1.3.0/ListPrint_wang-1.3.0.tar.gz

---
+++ packages/ListPrint_wang/ListPrint_wang-1.3.0.tar.gz/ListPrint_wang-1.3.0/ListSetup_wang.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+		name         = 'ListPrint_wang',
+		version      = '1.3.0',
+		py_modules   = ['ListPrint_wang'],
+		author       = 'WangCheng',
+		author_email = '378716031@q.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'You can use the second parameter(True/False) to control the indent', 
+
+	)
