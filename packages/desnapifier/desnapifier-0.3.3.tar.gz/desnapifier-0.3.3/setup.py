7966ba6f59c346f8e4618dbafd876cb54dd301a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package desnapifier/0.3.3/desnapifier-0.3.3.tar.gz

---
+++ packages/desnapifier/desnapifier-0.3.3.tar.gz/desnapifier-0.3.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "desnapifier",
+    version = "0.3.3",
+    description = "Convert Snap! projects to Scratch",
+    author = "Jeandre Kruger",
+    author_email = "Unknown",
+    url = "https://github.com/Jonathan50/desnapifier",
+    packages = [ "desnapifier" ]
+)
