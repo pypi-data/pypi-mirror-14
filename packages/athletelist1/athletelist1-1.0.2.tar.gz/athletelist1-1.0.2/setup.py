6de2e597375fdcb822246b4a45e477103392581d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:58 +0000

    Add package athletelist1/1.0.2/athletelist1-1.0.2.tar.gz

---
+++ packages/athletelist1/athletelist1-1.0.2.tar.gz/athletelist1-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'athletelist1',
+	version		= '1.0.2',
+	py_modules	= ['athletelist1'],
+	author		= 'dadongjun',
+	author_email	= '853027756@qq.com',
+	description	= 'A athletelist class',
+     )
