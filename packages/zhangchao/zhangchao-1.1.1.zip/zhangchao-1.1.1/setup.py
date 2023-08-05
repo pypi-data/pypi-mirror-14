8499e4ad271d6b4d9ee8acb96fdb50dd94511a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package zhangchao/1.1.1/zhangchao-1.1.1.zip

---
+++ packages/zhangchao/zhangchao-1.1.1.zip/zhangchao-1.1.1/setup.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016年3月15日
+
+@author: zsuper
+'''
+from distutils.core import setup
+setup(
+      name="zhangchao",
+      version='1.1.1',
+      py_modules=['zhangchao'],
+      author='zsuper',
+      author_email='zcjust2014@163.com',
+      description='A simple printer of nested lists',
+      
+      
+      
+      
+      )
