d6e83bdff073b3f36bccdf88ed95f3e818209ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package nesterjerrychan/1.1.5/nesterjerrychan-1.1.5.zip

---
+++ packages/nesterjerrychan/nesterjerrychan-1.1.5.zip/nesterjerrychan-1.1.5/nesterjerrychan.py
@@ -0,0 +1,15 @@
+
+
+def print_lol(the_list,indent=False,number=0):
+    for each_item in the_list:
+        if isinstance (each_item,list):
+            print_lol(each_item,indent,number+1)
+        else:
+            if indent :
+             for tab_stop in range(number):
+                 print ("\t",end='')
+             print(each_item)
+
+
+
+
---
+++ packages/nesterjerrychan/nesterjerrychan-1.1.5.zip/nesterjerrychan-1.1.5/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+		name        = 'nesterjerrychan',
+		version     = '1.1.5',
+		py_modules  = ['nesterjerrychan'],
+		author      = 'jerrychan',
+
+		)
