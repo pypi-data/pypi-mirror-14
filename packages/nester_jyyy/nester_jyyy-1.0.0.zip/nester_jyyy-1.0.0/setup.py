e33f7df4b54cfac2d46f3a6466617843d7479558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package nester_jyyy/1.0.0/nester_jyyy-1.0.0.zip

---
+++ packages/nester_jyyy/nester_jyyy-1.0.0.zip/nester_jyyy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nester_jyyy',
+        version     = '1.0.0',
+        py_modules  = ['nester_jyyyy'],
+        autor       = 'hfpython',
+        autor_email = 'hfpython@headfirstlabs.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested list',
+        )
