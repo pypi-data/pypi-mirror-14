9f8cd3d43733f349e30e5ff312f4f4683bf19d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:58 +0000

    Add package kaptl/0.4.3/kaptl-0.4.3.zip

---
+++ packages/kaptl/kaptl-0.4.3.zip/kaptl-0.4.3/app/exceptions.py
@@ -0,0 +1,22 @@
+class MSBuildNotFoundException(Exception):
+    pass
+
+
+class MSBuildFailedException(Exception):
+    pass
+
+
+class NoStackInfoException(Exception):
+    pass
+
+
+class WrongStackInfoException(Exception):
+    pass
+
+
+class NoRulesInfoException(Exception):
+    pass
+
+
+class NotKaptlProjectException(Exception):
+    pass
