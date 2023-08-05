b380553cd752e6cba5c5e6dad40aa0abb4e9e535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:36 +0000

    Add package Mynested2703/1.1.0/Mynested2703-1.1.0.zip

---
+++ packages/Mynested2703/Mynested2703-1.1.0.zip/Mynested2703-1.1.0/nested.py
@@ -0,0 +1,16 @@
+"""def print_lol(the_list):
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item)
+        else:
+            print(item)
+"""
+
+def print_lol(the_list, level):
+    for item in the_list:
+        if (isinstance(item, list)):
+            print_lol(item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end="")
+            print(item)
---
+++ packages/Mynested2703/Mynested2703-1.1.0.zip/Mynested2703-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Mynested2703",
+    version="1.1.0",
+    py_modules=['nested'],
+    author="lenott",
+    author_email="lenott69@gmail.com",
+    description="A simple print function of nested lists"
+)
