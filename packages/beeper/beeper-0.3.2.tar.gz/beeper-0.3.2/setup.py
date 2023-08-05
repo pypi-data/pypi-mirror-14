a5966e112958fb4d040d5bd6ce3603afa6a1c4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package beeper/0.3.2/beeper-0.3.2.tar.gz

---
+++ packages/beeper/beeper-0.3.2.tar.gz/beeper-0.3.2/setup.py
@@ -0,0 +1,35 @@
+"""
+Bundle virtualenv and project to an artifact.
+"""
+
+from setuptools import setup
+
+setup(
+    name='beeper',
+    version='0.3.2',
+    url='https://github.com/soasme/beeper.py',
+    license='MIT',
+    author='Ju Lin',
+    author_email='soasme@gmail.com',
+    description='Bundle virtualenv and project to an artifact.',
+    long_description=__doc__,
+    py_modules=['beeper'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'click>=1.0',
+        'PyYAML>=3.11',
+    ],
+    entry_points={
+        'console_scripts': [
+            'beeper = beeper:cli'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
