c070c91a6b8954ce492ea2c0496c32b7f1f9b583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package predicthq/0.0.2.dev1/predicthq-0.0.2.dev1-py2.py3-none-any.whl

---
+++ packages/predicthq/predicthq-0.0.2.dev1-py2.py3-none-any.whl/predicthq/endpoints/v1/events/schemas.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import, print_function
+
+from predicthq.endpoints.schemas import PaginatedMixin, SortableMixin, Model, ResultSet, \
+    ListType, StringType, GeoJSONPointType, StringListType, StringModelType, Area, \
+    ModelType, IntRange, IntType, DateRange, DateTimeType, FloatType, ResultType, \
+    DictType, DateType
+
+
+class SearchParams(PaginatedMixin, SortableMixin, Model):
+
+    class Options:
+        serialize_when_none = False
+
+    id = ListType(StringType)
+    q = StringType()
+    label = ListType(StringType)
+    category = ListType(StringType)
+    start = ModelType(DateRange)
+    end = ModelType(DateRange)
+    rank_level = ListType(IntType(min_value=1, max_value=5))
+    rank = ModelType(IntRange)
+    country = ListType(StringType)
+    within = StringListType(StringModelType(Area), separator="+")
+
+
+class Event(Model):
+
+    class Options:
+        serialize_when_none = True
+
+    id = StringType()
+    title = StringType()
+    description = StringType()
+    start = DateTimeType()
+    end = DateTimeType()
+    timezone = StringType()
+    duration = IntType()
+    category = StringType()
+    labels = ListType(StringType())
+    country = StringType()
+    rank = IntType()
+    location = GeoJSONPointType()
+    scope = StringType()
+    relevance = FloatType()
+
+
+class EventResultSet(ResultSet):
+
+    results = ResultType(Event)
+
+
+class TopEventsSearchParams(SortableMixin, Model):
+
+    limit = IntType(min_value=0, max_value=10)
+
+
+class CalendarParams(SearchParams):
+
+    top_events = ModelType(TopEventsSearchParams)
+
+
+class CalendarDay(Model):
+
+    date = DateType()
+    count = IntType()
+    top_rank = FloatType()
+    rank_levels = DictType(IntType)
+    categories = DictType(IntType)
+    labels = DictType(IntType)
+    top_events = ModelType(EventResultSet)
+
+
+class CalendarResultSet(ResultSet):
+
+    results = ResultType(CalendarDay)
