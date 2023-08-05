4db5e801039c21f0dbda3d698c5b1fcd30989a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package hidash/0.0.4/hidash-0.0.4.tar.gz

---
+++ packages/hidash/hidash-0.0.4.tar.gz/hidash-0.0.4/README.rst
@@ -0,0 +1,11 @@
+The Project can be explained as follows:
+
+- Provide the chart details in an xml file.
+
+- Provide the path to your xml in your settings.py.
+
+- Make Ajax API calls to fetch the chart data as supported by Google Charts generated directly from your database. Or, you can just use our
+
+- Javascript library to get the chart directly using an angular directive <hi-dash> without making any Ajax calls manually.
+
+- Use our APIs to generate reports of the charts in excel or pdf format.
---
+++ packages/hidash/hidash-0.0.4.tar.gz/hidash-0.0.4/hidash/static/js/hidash-app.js
@@ -0,0 +1,26 @@
+angular.module('dashboard', ['googlechart','ngMaterial'])
+.controller('ReportsController',function($scope, $http, $window, $location){
+	$scope.charts=[];
+
+	$scope.absUrl = $location.absUrl().split("?")[1];
+	console.log($scope.absUrl);
+	if (!$scope.absUrl) {
+			$scope.absUrl="group=default";
+		}
+    $http.get("/api/group_reports/?" + $scope.absUrl).success(function(data){
+    	for (var i=0;i<data.length;i++){
+    		$scope.charts[i]={};
+    		$scope.charts[i].data=data[i];
+    		$scope.charts[i].type=data[i].chart_type;
+    		$scope.charts[i].chart_id=data[i].chart_id;
+    		$scope.charts[i].query_id=data[i].query_id;
+    		var options = {
+    				  width: '100%',
+    				  height: 300,
+    				  is3D: true,
+    				  margin: 2
+    				};
+    		$scope.charts[i].options = options;
+    	}
+    });
+});;
