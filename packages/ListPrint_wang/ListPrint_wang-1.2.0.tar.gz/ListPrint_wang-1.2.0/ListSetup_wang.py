bebba0b394a90b14752f4ea90cf3ce7b2c58371f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package ListPrint_wang/1.2.0/ListPrint_wang-1.2.0.tar.gz

---
+++ packages/ListPrint_wang/ListPrint_wang-1.2.0.tar.gz/ListPrint_wang-1.2.0/ListSetup_wang.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+		name         = 'ListPrint_wang',
+		version      = '1.2.0',
+		py_modules   = ['ListPrint_wang'],
+		author       = 'WangCheng',
+		author_email = '378716031@q.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'A simple printer of nested lists', 
+
+	)
