2a6659097856f5ddb302cc1211e243243865d675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:05 +0000

    Add package frozendict/0.6/frozendict-0.6.tar.gz

---
+++ packages/frozendict/frozendict-0.6.tar.gz/frozendict-0.6/frozendict/__init__.py
@@ -0,0 +1,61 @@
+import collections
+import operator
+import functools
+
+
+class frozendict(collections.Mapping):
+
+    def __init__(self, *args, **kwargs):
+        self.__dict = dict(*args, **kwargs)
+        self.__hash = None
+
+    def __getitem__(self, key):
+        return self.__dict[key]
+
+    def copy(self, **add_or_replace):
+        return frozendict(self, **add_or_replace)
+
+    def __iter__(self):
+        return iter(self.__dict)
+
+    def __len__(self):
+        return len(self.__dict)
+
+    def __repr__(self):
+        return '<frozendict %s>' % repr(self.__dict)
+
+    def __hash__(self):
+        if self.__hash is None:
+            hashes = map(hash, self.items())
+            self.__hash = functools.reduce(operator.xor, hashes, 0)
+
+        return self.__hash
+
+
+class FrozenOrderedDict(collections.Mapping):
+
+    def __init__(self, *args, **kwargs):
+        self.__dict = collections.OrderedDict(*args, **kwargs)
+        self.__hash = None
+
+    def __getitem__(self, key):
+        return self.__dict[key]
+
+    def copy(self, **add_or_replace):
+        return FrozenOrderedDict(self, **add_or_replace)
+
+    def __iter__(self):
+        return iter(self.__dict)
+
+    def __len__(self):
+        return len(self.__dict)
+
+    def __repr__(self):
+        return '<FrozenOrderedDict %s>' % repr(self.__dict)
+
+    def __hash__(self):
+        if self.__hash is None:
+            hashes = map(hash, self.items())
+            self.__hash = functools.reduce(operator.xor, hashes, 0)
+
+        return self.__hash
