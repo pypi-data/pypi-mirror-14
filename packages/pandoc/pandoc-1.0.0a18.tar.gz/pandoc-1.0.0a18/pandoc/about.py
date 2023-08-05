abe27968f64d787621536a61e5b23015830ffb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package pandoc/1.0.0a18/pandoc-1.0.0a18.tar.gz

---
+++ packages/pandoc/pandoc-1.0.0a18.tar.gz/pandoc-1.0.0a18/pandoc/about.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+metadata = dict(
+  __name__     = "pandoc",
+  __version__  = "1.0.0a18", 
+  __license__  = "MIT License",
+  __author__   = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
+  __url__      = "https://github.com/boisgera/pandoc",
+  __summary__  = "Pandoc Documents for Python",
+  __keywords__ = """
+                 software development, topic/text processing, 
+                 python 2 / only, python / 2.7,
+                 alpha
+                 """
+)
+
+globals().update(metadata)
+__all__ = metadata.keys()
+
