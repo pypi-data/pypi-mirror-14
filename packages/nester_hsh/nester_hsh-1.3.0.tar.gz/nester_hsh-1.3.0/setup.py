3dee462445765e0b1293c6c3b942a082715f01e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:06 +0000

    Add package nester_hsh/1.3.0/nester_hsh-1.3.0.tar.gz

---
+++ packages/nester_hsh/nester_hsh-1.3.0.tar.gz/nester_hsh-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_hsh',
+    version = '1.3.0',
+    py_modules = ['nester_hsh'],
+    authonr = 'shihao_hu',
+    author_email = 'hushihao513@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
