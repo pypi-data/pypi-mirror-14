0bd980bef38a32123c17ec4a13a9631f340f4f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package nester_fatumepta/1.3.0/nester_fatumepta-1.3.0.tar.gz

---
+++ packages/nester_fatumepta/nester_fatumepta-1.3.0.tar.gz/nester_fatumepta-1.3.0/nester_fatumepta.py
@@ -0,0 +1,19 @@
+"""This is the 'nester.py' module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+
+
+def print_lol(the_list, indent = False, level = 0):
+    """This function takes a positional argument called 'the_list', which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line.
+    A second argument called “level" is used to insert tab-stops when a nested list
+    is encountered."""
+
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, indent, level + 1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t', end="")
+            print(item)
