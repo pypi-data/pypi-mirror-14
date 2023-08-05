62bdd581c8e7a1d137cce6a3ce9a83d3abd47431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package googlefonts-installer/0.2.0/googlefonts_installer-0.2.0.tar.gz

---
+++ packages/googlefonts-installer/googlefonts_installer-0.2.0.tar.gz/googlefonts_installer-0.2.0/setup.py
@@ -0,0 +1,47 @@
+"""Buchklub WBS (web-order-system)."""
+import os
+
+from setuptools import setup
+
+# IMPORTANT: follow semantic versioning (SemVer) scheme described at semver.org!
+VERSION = '0.2.0'
+
+
+# Runtime requirements.
+requires = []
+
+
+# Long description from readme and changelog.
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as fp:
+    README = fp.read()
+with open(os.path.join(here, 'CHANGELOG.rst')) as fp:
+    CHANGELOG = fp.read()
+
+
+setup(
+    name='googlefonts_installer',
+    description='Google fonts installer utility.',
+    long_description=README + '\n\n' + CHANGELOG,
+    version=VERSION,
+    url='https://github.com/fabianbuechler/googlefonts_installer',
+    license='All rights reserved',
+    author='Fabian Büchler',
+    author_email='fabian@buechler.io',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Fonts',
+    ],
+    entry_points={'console_scripts': [
+        'googlefonts-installer=googlefonts_installer:main'
+    ]},
+    install_requires=requires,
+    include_package_data=True,
+    packages=['.'],
+    zip_safe=False,
+)
