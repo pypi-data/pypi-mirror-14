3cfddfc86ec4a380b38e6e34cdd2102371100148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package svgis/0.3.8/svgis-0.3.8-py2-none-any.whl

---
+++ packages/svgis/svgis-0.3.8-py2-none-any.whl/svgis/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+from . import clip
+from . import convert
+from . import draw
+from . import errors
+from . import projection
+from . import svg
+from . import style
+from . import svgis
+from .svgis import map, SVGIS
+
+__version__ = '0.3.8'
+
+__all__ = [
+    'convert',
+    'clip',
+    'draw',
+    'errors',
+    'projection',
+    'style',
+    'svg',
+    'svgis',
+]
