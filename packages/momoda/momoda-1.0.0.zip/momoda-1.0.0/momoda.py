63c7355fe1fbdf1c98763ebcffe81433667d1aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package momoda/1.0.0/momoda-1.0.0.zip

---
+++ packages/momoda/momoda-1.0.0.zip/momoda-1.0.0/momoda.py
@@ -0,0 +1,12 @@
+
+"""这是“nester.py”模块，提供了一个名为hanshu（）的函数，这个函数的作用是打印列表，其中有可能包含（也有可能不包含）嵌套列表"""
+def hanshu(the_liebiao):
+    """这个函数去一个目标标识参数，名为the_liebiao，这可以是任何python列表（也可以是包含嵌套列表的列表）。所指定的列表中的每个数据项会（递归的）输入到屏幕上，每一个数据项各占一行。"""
+    for each_item in the_liebiao:
+        if isinstance(each_item,list):
+            hanshu(each_item)
+        else:
+            print(each_item)
+
+			
+
---
+++ packages/momoda/momoda-1.0.0.zip/momoda-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name         ='momoda',
+        version      ='1.0.0',
+        py_modules   =['momoda'],
+        auther       ='xiaojie',
+        author_emial ='1159442240@qq.com',
+        url          ='none',
+        descroption  ='i learn python for the firs time.' , 
+     )
