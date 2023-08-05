e450485e1e4e643f4ec4aa6caf3302a1d741749c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:30 +0000

    Add package biannester/1.3.1/biannester-1.3.1.zip

---
+++ packages/biannester/biannester-1.3.1.zip/biannester-1.3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='biannester',
+	version='1.3.1',
+	py_modules=['biannester'],
+	author='hfpython',
+	author_email='hfpython@headfirstlabs.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nester lists',
+	)
