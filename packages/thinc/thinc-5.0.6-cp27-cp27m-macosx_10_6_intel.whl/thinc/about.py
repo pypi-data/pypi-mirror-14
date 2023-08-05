107fb34700d1840ac98810eb20613389f6b25c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:33 +0000

    Add package thinc/5.0.6/thinc-5.0.6-cp27-cp27m-macosx_10_6_intel.whl

---
+++ packages/thinc/thinc-5.0.6-cp27-cp27m-macosx_10_6_intel.whl/thinc/about.py
@@ -0,0 +1,13 @@
+# inspired from:
+
+# https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+
+__name__ = 'thinc'
+__version__ = '5.0.6'
+__summary__ = 'Learn sparse linear models'
+__uri__ = 'https://github.com/spacy-io/thinc'
+__author__ = 'Matthew Honnibal'
+__email__ = 'matt@spacy.io'
+__license__ = 'MIT'
+__release__ = True
