4706a3d2a1ae2694e1dbe0177ff63827d02fe593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package proteome/5.2.0/proteome-5.2.0.tar.gz

---
+++ packages/proteome/proteome-5.2.0.tar.gz/proteome-5.2.0/proteome/ctags.py
@@ -0,0 +1,24 @@
+from proteome.project import Project
+from trypnv import ProcessExecutor, Job  # type: ignore
+
+
+class Ctags(ProcessExecutor):  # type: ignore
+
+    def gen(self, project: Project):
+        langs = ','.join(project.ctags_langs)
+        tag_file = project.tag_file
+        args = [
+            '-R',
+            '--languages={}'.format(langs),
+            '-f',
+            str(tag_file),
+            str(project.root)
+        ]
+        job = Job(
+            owner=project,
+            exe='ctags',
+            args=args,
+            loop=self.loop)
+        return self.run(job)
+
+__all__ = ('Ctags',)
