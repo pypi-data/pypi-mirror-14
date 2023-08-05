07fca9923ef902b57ef978bfa30be26b65398fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:07 +0000

    Add package hdf5_matlab_reader/0.1.2/hdf5_matlab_reader-0.1.2.tar.gz

---
+++ packages/hdf5_matlab_reader/hdf5_matlab_reader-0.1.2.tar.gz/hdf5_matlab_reader-0.1.2/hdf5_matlab_reader/tree_printer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+import sys
+import h5py
+
+def disp2(n,o):
+    print(n,o)
+    print(o.attrs.items())
+
+def tree(f):
+    h5_file = h5py.File(f, 'r')
+    h5_file.visititems(disp2)
+
+
+if __name__ == '__main__':
+    tree(sys.argv[1])
