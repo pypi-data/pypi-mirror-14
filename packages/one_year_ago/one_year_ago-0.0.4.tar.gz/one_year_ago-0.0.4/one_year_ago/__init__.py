ac561974c3275d4a346a34888f2ce0c18a326578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:17 +0000

    Add package one_year_ago/0.0.4/one_year_ago-0.0.4.tar.gz

---
+++ packages/one_year_ago/one_year_ago-0.0.4.tar.gz/one_year_ago-0.0.4/one_year_ago/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'XaviTorello'
+
+import pkg_resources
+
+from one_year_ago import *
+
+
+__version__ = pkg_resources.get_distribution(__package__).version
+#__version__ = 0.2
