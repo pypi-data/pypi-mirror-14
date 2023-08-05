aae85e365253202b1a486981c5caef37c10efac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package copysc/0.2.6/copysc-0.2.6.tar.gz

---
+++ packages/copysc/copysc-0.2.6.tar.gz/copysc-0.2.6/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r","") # Do not forget this line
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+    # pandoc is not installed, fallback to using raw contents
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
+
+setuptools.setup(
+    name="copysc",
+    version="0.2.6",
+    url="https://github.com/rv816/copysc",
+
+    author="Ryan Vass",
+    author_email="rvsax16@gmail.com",
+
+    description="Small utility that translates a shared screenshot url into a markdown-ready *.png url. Works with dropbox, screencloud, and others.",
+    long_description = long_description,
+    packages=setuptools.find_packages(),
+
+    install_requires=['pyperclip', 'bs4', 'requests'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
