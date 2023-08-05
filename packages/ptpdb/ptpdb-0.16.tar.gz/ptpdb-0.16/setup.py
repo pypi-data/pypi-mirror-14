526aab832253a030e5f4acec61b48df9bb6f1f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package ptpdb/0.16/ptpdb-0.16.tar.gz

---
+++ packages/ptpdb/ptpdb-0.16.tar.gz/ptpdb-0.16/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ptpdb',
+    author='Jonathan Slenders',
+    version='0.16',
+    url='https://github.com/jonathanslenders/ptpdb',
+    description='Python debugger (pdb) build on top of prompt_toolkit',
+    long_description='',
+    packages=find_packages('.'),
+    install_requires = [
+        'ptpython==0.31',
+        'prompt-toolkit==0.60',
+    ],
+)
