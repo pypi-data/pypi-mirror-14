e7b404e693b36cd385e8cba7c319a42130e18979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:00 +0000

    Add package dlcli/0.0.13/dlcli-0.0.13.tar.gz

---
+++ packages/dlcli/dlcli-0.0.13.tar.gz/dlcli-0.0.13/dlcli/api/tags.py
@@ -0,0 +1,17 @@
+import logging
+import requests
+import utils
+
+logger = logging.getLogger(__name__)
+
+
+class Tags(object):
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.headers = {"Token": ctx.parent.parent.params['key']}
+
+    def get_tags(self):
+        return requests.get(utils.build_api_url(self.ctx, 'tags'), headers=self.headers).json()
+
+    def delete_tag(self, tag):
+        return requests.delete(utils.build_api_url(self.ctx, 'tags') + '/' + tag, headers=self.headers)
