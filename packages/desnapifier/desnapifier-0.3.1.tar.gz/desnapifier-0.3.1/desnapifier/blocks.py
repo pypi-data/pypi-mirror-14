55d1214e6b5600e9c0891bb67880b8dd3278d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:37 +0000

    Add package desnapifier/0.3.1/desnapifier-0.3.1.tar.gz

---
+++ packages/desnapifier/desnapifier-0.3.1.tar.gz/desnapifier-0.3.1/desnapifier/blocks.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2016 Jeandre Kruger
+#
+# This file is part of desnapifier.
+#
+# desnapifier is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# desnapifier is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with desnapifier.  If not, see <http://www.gnu.org/licenses/>.
+
+blocks = {
+    # motion
+    "forward":       [ "forward:", 1, None ],
+    "turn":          [ "turnRight:", 1, None ],
+    "turnLeft":      [ "turnLeft:",  1, None ],
+
+    # looks
+    "doSayFor":      [ "say:duration:elapsed:from:", 2, None ],
+    "bubble":        [ "say:", 1, None ],
+    "doThinkFor":    [ "think:duration:elapsed:from:", 2, None ],
+    "doThink":       [ "think:", 1, None ],
+    "show":          [ "show", 0, None ],
+    "hide":          [ "hide", 0, None ],
+
+    # control/events
+    "receiveGo":     [ "whenGreenFlag", 0, None ],
+    "doWait":        [ "wait:elapsed:from:", 1, None ],
+
+    # operators
+    "reportProduct": [ "*", 2, None ]
+}
---
+++ packages/desnapifier/desnapifier-0.3.1.tar.gz/desnapifier-0.3.1/desnapifier/scripts.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2016 Jeandre Kruger
+#
+# This file is part of desnapifier.
+#
+# desnapifier is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# desnapifier is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with desnapifier.  If not, see <http://www.gnu.org/licenses/>.
+
+import kurt
+import notsupported, blocks
+
+class NotEnoughArgumentsError(Exception):
+    pass
+
+class TooManyArgumentsError(Exception):
+    pass
+
+def check_args(s, c, n):
+    if n < c:
+        raise NotEnoughArgumentsError("not enough arguments for %s block" % s)
+    if n > c:
+        raise TooManyArgumentsError("too many arguments for %s block" % s)
+
+def get_args(snap_block):
+    args_list = []
+    for child in snap_block:
+        if child.tag == "l":
+            args_list.append(child.text)
+        if child.tag == "block":
+            args_list.append(convert_block(child))
+    return args_list
+
+def convert_block(snap_block):
+    scratch_block = None
+
+    if not "s" in snap_block.attrib:
+        raise Exception("block has no s attribute!")
+
+    s = snap_block.attrib["s"]
+
+    # iterate over all possible blocks
+    for key in blocks.blocks:
+        if key == s:
+            if blocks.blocks[key][0] != None:
+                args = get_args(snap_block)
+                check_args(s, blocks.blocks[key][1], len(args))
+                scratch_block = kurt.Block(blocks.blocks[key][0], *args)
+            else:
+                if blocks.blocks[key][2] == None:
+                    raise Exception("Both block[0] and block[2] are none!")
+                scratch_block = blocks.blocks[key][2]()
+
+    # unknown block
+    if scratch_block == None:
+        raise notsupported.UnsupportedBlockError(s)
+
+    return scratch_block
+
+def convert_scripts(snap_scripts):
+    scratch_scripts = []
+
+    for script in snap_scripts.iter("script"):
+        scratch_script = kurt.Script()
+        for block in script:
+            if block.tag == "block":
+                scratch_script.append(convert_block(block))
+        scratch_scripts.append(scratch_script)
+
+    return scratch_scripts
