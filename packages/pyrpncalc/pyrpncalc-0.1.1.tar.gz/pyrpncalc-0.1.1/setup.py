90fc6d0c2a6353523471a01563d96b02ffde96de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package pyrpncalc/0.1.1/pyrpncalc-0.1.1.tar.gz

---
+++ packages/pyrpncalc/pyrpncalc-0.1.1.tar.gz/pyrpncalc-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyrpncalc',
+      version='0.1.1',
+      description="""A Reverse Polish Notation (postfix) module with extensive operation support.
+      It is primarily designed for use in interactive mode, but may also be used by other modules.""",
+      url='https://bitbucket.org/TheCDC/pyrpn',
+      author='Christopher Chen',
+      author_email='christopher.chen1995@gmail.com',
+      license='Public Domain',
+      packages=['pyrpncalc'],
+      zip_safe=False)	
