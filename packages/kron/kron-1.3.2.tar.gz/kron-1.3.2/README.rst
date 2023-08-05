7f87d26f228ec9d60f01fc931f19a4f931558a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package kron/1.3.2/kron-1.3.2.tar.gz

---
+++ packages/kron/kron-1.3.2.tar.gz/kron-1.3.2/README.rst
@@ -0,0 +1,308 @@
+Features
+========
+
+* Classes for durations, timestamps and timezones
+* Minimal non-standard dependencies
+  (`ntplib <https://pypi.python.org/pypi/ntplib>`_,
+  `pytz <https://pypi.python.org/pypi/pytz>`_,
+  `tzlocal <https://pypi.python.org/pypi/tzlocal>`_)
+* Microsecond accuracy
+* Timestamp internal storage is float epoch seconds in UTC
+* Duration internal storage is float seconds
+* Timezone name search by regular expression
+* Default timezone is local timezone
+* Comparison and arithmetic methods for timestamps and durations
+* Test-driven development methodology
+* Named formats
+* Helper methods for timezones and formats
+* Command line tool
+
+Installation
+============
+
+::
+
+    $ pip install kron
+
+Or::
+
+    $ git clone https://github.com/qtfkwk/kron.git
+    $ cd kron
+    $ python setup.py install
+
+Update
+======
+
+::
+
+    $ pip install -U kron
+
+Or::
+
+    $ cd kron
+    $ git pull
+    $ python setup.py install
+
+Examples
+========
+
+Python code/interpreter
+-----------------------
+
+::
+
+    >>> from kron import duration, timestamp, timezone
+    >>> now = timestamp()
+    >>> now.value
+    1457246861.121305
+    >>> now.str()
+    '2016-03-05 20:47:41 EST'
+    >>> now.str('UTC')
+    '2016-03-06 01:47:41 UTC'
+    >>> now.str(fmt='%A, %B %d, %Y')
+    'Saturday, March 05, 2016'
+    >>> t = timestamp(1257209442)
+    >>> t.value
+    1257209442.0
+    >>> t.str()
+    '2009-11-02 14:50:42 EST'
+    >>> t.str('UTC')
+    '2009-11-02 19:50:42 UTC'
+    >>> t.str(fmt='%A, %B %d, %Y')
+    'Monday, November 02, 2009'
+    >>> t.str(fmt='iso8601')
+    '2009-11-02T19:50:42Z'
+    >>> t.str(fmt='date')
+    'Monday, November 02, 2009'
+    >>> t.str(fmt='national')
+    'Mon Nov 02 14:50:42 EST 2009'
+    >>> t.str(fmt='rfc2822')
+    'Mon, 02 Nov 2009 14:50:42 -0500'
+    >>> for fmt in sorted(t._formats):
+    ...     print '%s: "%s" => "%s"' % (fmt, t._formats[fmt], t.str(fmt=fmt))
+    ...
+    HH: "%H" => "14"
+    HHMM: "%H%M" => "1450"
+    HHMMSS: "%H%M%S" => "145042"
+    HH_MM: "%H:%M" => "14:50"
+    HH_MM_SS: "%H:%M:%S" => "14:50:42"
+    MM: "%M" => "50"
+    SS: "%S" => "42"
+    abbr_date: "%a, %b %d, %Y" => "Mon, Nov 02, 2009"
+    abbr_month: "%b" => "Nov"
+    abbr_weekday: "%a" => "Mon"
+    ampm: "%p" => "PM"
+    base: "%Y-%m-%d %H:%M:%S" => "2009-11-02 14:50:42"
+    ccyy: "%Y" => "2009"
+    ccyymm: "%Y%m" => "200911"
+    ccyymmdd: "%Y%m%d" => "20091102"
+    date: "%A, %B %d, %Y" => "Monday, November 02, 2009"
+    dd: "%d" => "02"
+    hh: "%I" => "02"
+    hh_MM: "%I:%M" => "02:50"
+    hh_MM_SS: "%I:%M:%S" => "02:50:42"
+    hh_MM_SS_ampm: "%I:%M:%S %p" => "02:50:42 PM"
+    hh_MM_ampm: "%I:%M %p" => "02:50 PM"
+    hours: "%H" => "14"
+    hours12: "%I" => "02"
+    hours24: "%H" => "14"
+    iso8601: "%Y-%m-%dT%H:%M:%SZ" => "2009-11-02T19:50:42Z"
+    julian: "%j" => "306"
+    local: "%Y-%m-%d %H:%M:%S %Z" => "2009-11-02 14:50:42 EST"
+    microseconds: "%f" => "000000"
+    minutes: "%M" => "50"
+    mm: "%m" => "11"
+    mm_dd_yy: "%m/%d/%y" => "11/02/09"
+    mmdd: "%m%d" => "1102"
+    mon: "%b" => "Nov"
+    month: "%B" => "November"
+    national: "%a %b %d %X %Z %Y" => "Mon Nov 02 14:50:42 EST 2009"
+    national_date: "%x" => "11/02/09"
+    national_time: "%X" => "14:50:42"
+    rfc2822: "%a, %d %b %Y %H:%M:%S %z" => "Mon, 02 Nov 2009 14:50:42 -0500"
+    seconds: "%S" => "42"
+    tz: "%Z" => "EST"
+    tz_offset: "%z" => "-0500"
+    week_number_mon: "%W" => "44"
+    week_number_sun: "%U" => "44"
+    weekday: "%A" => "Monday"
+    year: "%Y" => "2009"
+    yy: "%y" => "09"
+    yymm: "%y%m" => "0911"
+    yymmdd: "%y%m%d" => "091102"
+    yyyy: "%Y" => "2009"
+    yyyy_mm_dd: "%Y/%m/%d" => "2009/11/02"
+    yyyymm: "%Y%m" => "200911"
+    yyyymmdd: "%Y%m%d" => "20091102"
+    >>> t = timestamp('1999-10-12 01:18:43', 'UTC')
+    >>> t.value
+    939709123.0
+    >>> t.str()
+    '1999-10-11 22:18:43 EDT'
+    >>> t.str('Los_Angeles')
+    '1999-10-11 19:18:43 PDT'
+    >>> t.str(fmt='%A, %B %d, %Y')
+    'Monday, October 11, 1999'
+    >>> d = now - t
+    >>> d.dict()
+    {'days': 5990, 'hours': 0, 'minutes': 28, 'seconds': 58, 'microseconds': 121305}
+    >>> local = timezone()
+    >>> local.name
+    'America/New_York'
+    >>> madrid = timezone('madrid')
+    >>> madrid.name
+    'Europe/Madrid'
+
+Command line tool
+-----------------
+
+::
+
+    $ kron -h
+    usage: kron.py [-h] [-T TIMEZONE] [-F FORMAT] [-t TIMEZONE] [-f FORMAT]
+                   [ARG [ARG ...]]
+    
+    positional arguments:
+      ARG          one or more timestamp input values; int/float epoch seconds,
+                   timestamp string in base or any format specified by -F;
+                   default: now
+    
+    optional arguments:
+      -h, --help   show this help message and exit
+      -T TIMEZONE  input timezone; default: local timezone
+      -F FORMAT    input format; default: "base" ("%Y-%m-%d %H:%M:%S")
+      -t TIMEZONE  output timezone; default: local timezone
+      -f FORMAT    output format; default: "basetz" ("%Y-%m-%d %H:%M:%S %Z")
+    $ kron
+    2016-03-11 00:41:46 EST
+    $ kron -t utc
+    2016-03-11 05:42:13 UTC
+    $ kron -f iso8601
+    2016-03-11T05:43:10Z
+    $ kron '2005-04-04 09:12:00'
+    2005-04-04 09:12:00 EDT
+    $ kron '2005-04-04 09:12:00' -f weekday
+    Monday
+    $ kron '2006-11-13 21:22:00' -T UTC
+    2006-11-13 16:22:00 EST
+    $ kron '2006-11-13 21:22:00' -T UTC -t Madrid -t los_angeles \
+    > -f iso8601 -f rfc2822
+    {
+        "2006-11-13 21:22:00": {
+            "Madrid": {
+                "iso8601": "2006-11-13T21:22:00Z",
+                "rfc2822": "Mon, 13 Nov 2006 22:22:00 +0100"
+            },
+            "los_angeles": {
+                "iso8601": "2006-11-13T21:22:00Z",
+                "rfc2822": "Mon, 13 Nov 2006 13:22:00 -0800"
+            }
+        }
+    }
+
+Versions
+========
+
+* 1.0.0 (2016-03-05): Initial release
+* 1.0.1 (2016-03-05): Finished rename
+* 1.1.0 (2016-03-06): More formats, improved documentation, fix
+  `issue #1 <https://github.com/qtfkwk/kron/issues/1>`_
+* 1.1.1 (2016-03-06): Added description to setup.py
+* 1.2.0 (2016-03-08): Helper methods for timezone and formats;
+  command line tool
+* 1.3.0 (2016-03-11): Converted timestamp internal storage to UTC
+  (`issue #2 <https://github.com/qtfkwk/kron/issues/2>`_);
+  added Network Time Protocol (RFC 1305) functionality via ntplib
+  module; added time, time_ntp, time_utc functions; improved
+  documentation
+* 1.3.1 (2016-03-11): Added version test
+* 1.3.2 (2016-03-11): Fixed classifers
+
+Discussion
+==========
+
+Dates and times are not one of Python's strengths. Even doing basic
+work requires using multiple standard and non-standard modules and
+effort to get it right. This module seeks to leverage the necessary
+modules for handling dates and times but provide a simple and uniform
+interface for doing so.
+
+Background
+==========
+
+Kron was started as a portfolio project to demonstrate proficiency in
+Python as well as practice the test-driven development (TDD) process
+in concert with git and Github. The topic was selected to address some
+personal points of pain experienced while working with dates and times
+in Python.
+
+The importance of correct representation of dates and times in the
+area of digital forensics cases cannot be overstated. While a myriad
+of poorly designed and implemented code contribute, the *core problem*
+is the absence of a simple abstraction to represent a specific point
+in time.
+
+Kron drastically simplifies working with dates and times by making the
+central "timestamp" class represent a specific point in time, and
+enabling it to be created, modified, and viewed in a few natural ways.
+
+See also
+========
+
+* Kron at `PyPI <https://pypi.python.org/pypi/kron>`_,
+  `Github <https://github.com/qtfkwk/kron>`_
+* Network Time Protocol (RFC 1305) at
+  `Wikipedia <https://en.wikipedia.org/wiki/Network_Time_Protocol>`_,
+  `IETF <https://tools.ietf.org/html/rfc1305>`_
+
+Issues
+======
+
+Please view/report any issues
+`here <https://github.com/qtfkwk/kron/issues>`_.
+
+Ideas
+=====
+
+* Command line tool
+    * Timezone searching
+    * List formats
+    * Duration calculations
+* Parser to find timestamps inside text/data/filesystems
+* Add clock, calendar/timeline, events...
+* Alternate output formats including visual/graphical...
+
+Author
+======
+
+qtfkwk <qtfkwk+kron@gmail.com>
+
+Legal
+=====
+
+::
+
+    Copyright (c) 2016, qtfkwk
+    All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+    
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
---
+++ packages/kron/kron-1.3.2.tar.gz/kron-1.3.2/kron.py
@@ -0,0 +1,441 @@
+#!/usr/bin/env python
+
+"""
+:Name: kron
+:Description: Uniform interface for dates and times in Python
+:Version: 1.3.2
+:File: kron.py
+:Author: qtfkwk <qtfkwk+kron@gmail.com>
+:Copyright: (C) 2016 by qtfkwk
+:License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+"""
+
+# Standard modules
+
+import argparse
+import datetime
+import json
+import re
+import calendar
+
+# External modules
+
+import ntplib
+import pytz
+import tzlocal
+
+# Variables
+
+version = '1.3.2'
+
+# Classes
+
+class timezone(object):
+    """represent timezone, provide search mechanism, default to local
+    timezone"""
+
+    @classmethod
+    def search(cls, name=None):
+        """resolve timezone given a name
+
+        if name is...
+        * None: returns local timezone name
+        * string that is an exact match in proper or lower case:
+          itself
+        * empty string ('') or wildcard regular expression ('.*'):
+          returns all timezone names
+        * any other string: used as a regular expression; multiple or
+          zero matches returns a list"""
+        if name == None:
+            return tzlocal.get_localzone().zone
+        if name in pytz.all_timezones:
+            return name
+        name_ = name.lower()
+        matches = []
+        for t in pytz.all_timezones:
+            t_ = t.lower()
+            if name_ == t_:
+                return t
+            if re.search(name, t) or re.search(name_, t_):
+                matches.append(t)
+        if len(matches) == 1:
+            return matches[0]
+        else:
+            return matches
+
+    def __init__(self, name=None):
+        """initialize a timezone object"""
+        self.original = name
+        self.name = self.search(name)
+        if isinstance(self.name, list):
+            if len(self.name) == 0:
+                raise TimezoneFailure('No timezone found for "%s"' % name)
+            else:
+                raise TimezoneMultiple('Found multiple possible timezones' + \
+                    ' for "%s": %s' % (name, ', '.join(self.name)))
+        self.pytz = pytz.timezone(self.name)
+
+class duration(object):
+    """represent a duration of time"""
+
+    _units = ('days', 'hours', 'minutes', 'seconds')
+    _values = dict(days=86400, hours=3600, minutes=60, seconds=1)
+
+    def __init__(self, value=0):
+        """initialize a duration object"""
+        self.value = round(value, 6)
+
+    def dict(self):
+        """return a dictionary with duration represented as common
+        units of time"""
+        v = int(self.value)
+        r = dict(days=0, hours=0, minutes=0, seconds=0)
+        r['microseconds'] = int((self.value - v) * 10**6 + 0.5)
+        for i in self._units:
+            r[i] = int(v / self._values[i])
+            v -= r[i] * self._values[i]
+        return r
+
+    def __cmp__(self, y):
+        """compare two duration objects"""
+        if isinstance(y, duration):
+            return cmp(self.value, y.value)
+
+    def __add__(self, y):
+        """add two durations or a duration and an int or float"""
+        if isinstance(y, duration):
+            return duration(self.value + y.value)
+        elif isinstance(y, (int, float)):
+            return duration(self.value + y)
+
+    def __sub__(self, y):
+        """subtract two durations or a duration and an int or float"""
+        if isinstance(y, duration):
+            return duration(self.value - y.value)
+        elif isinstance(y, (int, float)):
+            return duration(self.value - y)
+
+    def __mul__(self, y):
+        """multiply a duration by an int or float"""
+        if isinstance(y, (int, float)):
+            return duration(self.value * y)
+        else:
+            raise DurationMultiplyError
+
+    def __div__(self, y):
+        """divide a duration by an int or float"""
+        if isinstance(y, (int, float)):
+            return duration(self.value / y)
+        else:
+            raise DurationDivideError
+
+class _bdict(dict):
+    """enhanced dictionary to store the formats"""
+
+    def __missing__(self, key):
+        if key == None:
+            key = 'base'
+        return self[key] if key in self else key
+
+class timestamp(object):
+    """represent a specific point in time, provide multiple ways of
+    defining it and translating to other timezones and/or formats
+
+    inputs
+    * None: now
+    * epoch seconds as int or float in UTC
+    * string in base or some other format and/or timezone
+    """
+
+    _formats = _bdict(
+        base='%Y-%m-%d %H:%M:%S',
+        basetz='%Y-%m-%d %H:%M:%S %Z',
+        HH='%H',
+        HH_MM='%H:%M',
+        HH_MM_SS='%H:%M:%S',
+        HHMM='%H%M',
+        HHMMSS='%H%M%S',
+        MM='%M',
+        SS='%S',
+        abbr_date='%a, %b %d, %Y',
+        abbr_month='%b',
+        abbr_weekday='%a',
+        ampm='%p',
+        ccyy='%Y',
+        ccyymm='%Y%m',
+        ccyymmdd='%Y%m%d',
+        date='%A, %B %d, %Y',
+        dd='%d',
+        hh='%I',
+        hh_MM='%I:%M',
+        hh_MM_ampm='%I:%M %p',
+        hh_MM_SS='%I:%M:%S',
+        hh_MM_SS_ampm='%I:%M:%S %p',
+        hours='%H',
+        hours12='%I',
+        hours24='%H',
+        iso8601='%Y-%m-%dT%H:%M:%SZ',
+        julian='%j',
+        microseconds='%f',
+        minutes='%M',
+        mm='%m',
+        mm_dd_yy='%m/%d/%y',
+        mmdd='%m%d',
+        mon='%b',
+        month='%B',
+        national='%a %b %d %X %Z %Y',
+        national_date='%x',
+        national_time='%X',
+        rfc2822='%a, %d %b %Y %H:%M:%S %z',
+        rfc2822_='%a, %d %b %Y %H:%M:%S',
+        seconds='%S',
+        tz='%Z',
+        tz_offset='%z',
+        week_number_mon='%W',
+        week_number_sun='%U',
+        weekday='%A',
+        year='%Y',
+        yy='%y',
+        yymm='%y%m',
+        yymmdd='%y%m%d',
+        yyyy='%Y',
+        yyyy_mm_dd='%Y/%m/%d',
+        yyyymm='%Y%m',
+        yyyymmdd='%Y%m%d',
+    )
+
+    def __init__(self, value=None, tz=None, fmt=None, ntp=False):
+        """initialize a timestamp object; internal storage is float
+        epoch seconds with microsecond accuracy (values are rounded to
+        6 decimal places)"""
+        if isinstance(value, (int, float)):
+            self.value = float(value)
+        elif isinstance(value, (str, unicode)) and \
+        re.search(r'^\d+\.?\d*$', value):
+            self.value = float(value)
+        else:
+            self.value = time(value, tz, fmt, ntp)
+        self.value = round(self.value, 6)
+
+    def __cmp__(self, y):
+        """compare two timestamps"""
+        if isinstance(y, timestamp):
+            return cmp(self.value, y.value)
+        else:
+            raise TimestampComparisonError
+
+    def __sub__(self, y):
+        """subtract two timestamps to produce a duration object, or a
+        duration, int, or float from a timestamp to produce a new
+        timestamp object"""
+        if isinstance(y, timestamp):
+            return duration(abs(self.value - y.value))
+        elif isinstance(y, duration):
+            return timestamp(self.value - y.value)
+        elif isinstance(y, (int, float)):
+            return timestamp(self.value - y)
+
+    def __add__(self, y):
+        """add a duration, int, or float to a timestamp to produce a
+        new timestamp object"""
+        if isinstance(y, duration):
+            return timestamp(self.value + y.value)
+        elif isinstance(y, (int, float)):
+            return timestamp(self.value + y)
+
+    def __mul__(self, y):
+        """cannot multiply a timestamp"""
+        raise TimestampMultiplyError
+
+    def __div__(self, y):
+        """cannot divide a timestamp"""
+        raise TimestampDivideError
+
+    def str(self, tz=None, fmt=None):
+        """output the timestamp as a string in the given timezone and
+        format; format may be a named format in the _formats
+        dictionary, or any valid strftime format"""
+        d = datetime.datetime.fromtimestamp(self.value)
+        d = timezone().pytz.localize(d)
+        if fmt == 'iso8601':
+            tz = 'UTC'
+        if tz != pytz.utc:
+            tz = timezone(tz).pytz
+            d = tz.normalize(d.astimezone(tz))
+        if fmt == None:
+            fmt = 'basetz'
+        return d.strftime(self._formats[fmt])
+
+    def utc(self, fmt='basetz'):
+        """utc timezone helper"""
+        return self.str('UTC', fmt)
+
+    def rfc2822(self, tz=None):
+        """rfc2822 format helper"""
+        return self.str(tz, 'rfc2822')
+
+    def iso8601(self):
+        """iso8601 timezone/format helper"""
+        return self.str(fmt='iso8601')
+
+    def dict(self, tz=[None], fmt=['basetz']):
+        """convert the timestamp to multiple timezones and/or formats
+        and return results as a dictionary"""
+        if not isinstance(tz, list):
+            tz = [tz]
+        if not isinstance(fmt, list):
+            fmt = [fmt]
+        r = {}
+        for t in tz:
+            if t == None:
+                t = 'localtz'
+            r[t] = {}
+            for f in fmt:
+                if f == None:
+                    f = 'basetz'
+                r[t][f] = self.str(t if t != 'localtz' else None, f)
+        return r
+
+    def json(self, tz=[None], fmt=['basetz']):
+        """convert the dict to a pretty-printed json string"""
+        return _json(self.dict(tz, fmt))
+
+# Functions
+
+def time_utc(epoch=None, tz=None):
+    """similar to time.time(); output is float epoch seconds in utc"""
+    if epoch == None:
+        d = datetime.datetime.now()
+    elif isinstance(epoch, (int, float)):
+        d = datetime.datetime.fromtimestamp(epoch)
+    elif isinstance(epoch, datetime.datetime):
+        d = epoch
+    else:
+        raise TimeEpochError('epoch must be None, int, float, or datetime')
+    d = timezone(tz).pytz.localize(d)
+    r = calendar.timegm(d.utctimetuple())
+    r += d.microsecond / float(10**6)
+    return r
+
+def time_ntp(server='pool.ntp.org'):
+    """similar to time.time() except uses ntp and output is float
+    epoch seconds in utc"""
+    c = ntplib.NTPClient()
+    try:
+        res = c.request(server, version=3)
+    except:
+        raise NTPError
+    # convert from seconds since 1900 to seconds since 1970
+    r = res.tx_timestamp - 2208988800L
+    # convert to utc
+    r = time_utc(r)
+    return r
+
+def time(value=None, tz=None, fmt=None, ntp=False):
+    """similar to time.time() but more flexible and consistent;
+    optionally uses ntp or processes a string timestamp; output is
+    float epoch seconds in utc"""
+    if value == None:
+        # now
+        if tz != None:
+            raise TimeTimezoneError('defined tz while value is None')
+        if fmt != None:
+            raise TimeFormatError('defined fmt while value is None')
+        if ntp:
+            try:
+                r = time_ntp()
+            except NTPError:
+                r = time_utc()
+        else:
+            r = time_utc()
+    else:
+        # process as a string timestamp
+        d = datetime.datetime.strptime(value, timestamp._formats[fmt])
+        if fmt == 'iso8601':
+            tz = 'UTC'
+        r = time_utc(d, tz)
+    return r
+
+def _json(obj):
+    """drop-in replacement for json.dumps()"""
+    return json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
+
+def cli(argv=None):
+    """backend function for command line interface"""
+    p = argparse.ArgumentParser()
+    p.add_argument('-V', '--version', action='store_true', \
+        help='print version and exit')
+    p.add_argument('-T', metavar='TIMEZONE', action='store', \
+        help='input timezone; default: local timezone')
+    p.add_argument('-F', metavar='FORMAT', action='store', \
+        help='input format; default: "base" ("%%Y-%%m-%%d %%H:%%M:%%S")')
+    p.add_argument('-t', metavar='TIMEZONE', action='append', default=[], \
+        help='output timezone; default: local timezone')
+    p.add_argument('-f', metavar='FORMAT', action='append', default=[], \
+        help='output format; default: "basetz" ("%%Y-%%m-%%d %%H:%%M:%%S %%Z")')
+    p.add_argument('args', metavar='ARG', action='store', nargs='*', \
+        help='one or more timestamps; int/float epoch seconds,' + \
+        ' string in the base format or the format specified by -F;' + \
+        ' default: now')
+    a = p.parse_args(argv)
+    if a.version:
+        return version
+    for i in (a.args, a.t, a.f):
+        if i == None:
+            i = []
+        if i == []:
+            i.append(None)
+    if len(a.args) + len(a.t) + len(a.f) == 3:
+        return timestamp(a.args[0], a.T, a.F).str(a.t[0], a.f[0])
+    r = {}
+    for i in a.args:
+        r[i if i != None else 'now'] = timestamp(i, a.T, a.F).dict(a.t, a.f)
+    return _json(r)
+
+def main():
+    """main function"""
+    import sys
+    print cli(sys.argv[1:])
+
+# Error classes
+
+class KronError(Exception):
+    pass
+
+class TimezoneFailure(KronError):
+    pass
+
+class TimezoneMultiple(KronError):
+    pass
+
+class DurationMultiplyError(KronError):
+    pass
+
+class DurationDivideError(KronError):
+    pass
+
+class TimestampComparisonError(KronError):
+    pass
+
+class TimestampMultiplyError(KronError):
+    pass
+
+class TimestampDivideError(KronError):
+    pass
+
+class TimeTimezoneError(KronError):
+    pass
+
+class TimeFormatError(KronError):
+    pass
+
+class NTPError(KronError):
+    pass
+
+class TimeEpochError(KronError):
+    pass
+
+# Main
+
+if __name__ == '__main__':
+    main()
