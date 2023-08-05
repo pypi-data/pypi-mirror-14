1c31658fd4a42b19ded51e11b8c2a0ece2c86796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package fono/0.1.7/fono-0.1.7.tar.gz

---
+++ packages/fono/fono-0.1.7.tar.gz/fono-0.1.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import sys
+import fono
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    with open(path.join(here, 'README.md')) as f:
+        long_description = f.read()
+
+from pip.req import parse_requirements
+install_requires = [str(ir.req) for ir in parse_requirements(path.join(here, 'requirements.txt'), session=False)]
+
+setup(
+    name=fono.__title__,
+    version=fono.__version__,
+
+    description=fono.__summary__,
+    long_description=long_description,
+    license=fono.__license__,
+    url=fono.__uri__,
+
+    author=fono.__author__,
+    author_email=fono.__email__,
+
+    packages=["fono"],
+
+    entry_points={
+        "console_scripts": [
+            "fono = fono.run:main",
+        ],
+    },
+
+    install_requires=install_requires,
+)
