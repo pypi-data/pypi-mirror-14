c07e9424aead3e0f80323ea51345e80a28728e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package tay_say/0.1/tay_say-0.1.tar.gz

---
+++ packages/tay_say/tay_say-0.1.tar.gz/tay_say-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'tay_say',
+  packages = ['tay_say'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple lib that prints out a random Taylor Swift lyric',
+  author = 'Justin Beall',
+  author_email = 'jus.beall@gmail.com',
+  url = 'https://github.com/DEV3L/python-tay-say',
+  download_url = 'https://github.com/DEV3L/python-tay-say/tarball/0.1',
+  keywords = ['testing', 'dev3l', 'taylor swift'], # arbitrary keywords
+  classifiers = [],
+)
