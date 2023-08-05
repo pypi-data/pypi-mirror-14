899b8b077d0b823cdd593535b2b00f7fff59831d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:25 +0000

    Add package MPBS/0.0.3.8/MPBS-0.0.3.8.tar.gz

---
+++ packages/MPBS/MPBS-0.0.3.8.tar.gz/MPBS-0.0.3.8/MPBS/examples/mpbs/mpbs_client.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+'''
+Created on 2016年3月3日
+'''
+import time
+from MPBS.client import mpbs
+__author__ = 'chenjian'
+
+def main():
+    
+    
+
+    while True:
+        mpbs.request("tcp://localhost:5555", json={"a": 1})
+        break
+
+if __name__ == '__main__':
+    main()
