82866bcdd2f62960ca4e7e97cf9888f0e30f4713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package nester111/1.0.0.1111/nester111-1.0.0.1111.zip

---
+++ packages/nester111/nester111-1.0.0.1111.zip/nester111-1.0.0.1111/setup111.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ="nester111",
+    version ='1.0.0.1111',
+    py_modules=['nester'],
+    author='hfpython',
+    author_email='htpython.com',
+    url='http://www.headfirstlabs.com',
+    description="a simple printer of nested listed"
+)
