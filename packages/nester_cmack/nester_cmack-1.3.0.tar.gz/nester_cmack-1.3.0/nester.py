d8eaba62aa584d790b113b352e09b677cce89256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package nester_cmack/1.3.0/nester_cmack-1.3.0.tar.gz

---
+++ packages/nester_cmack/nester_cmack-1.3.0.tar.gz/nester_cmack-1.3.0/nester.py
@@ -0,0 +1,16 @@
+"""
+modole for list utilities
+"""
+def print_lol(the_list, indent=False, level=0):
+    """input: list object. can contain nested list
+    output: print item from all lists
+    """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, indent_level + 1)
+        else:
+            if indent:
+                for number_of_tabs in range(indent_level):
+                    print("\t", end='')
+                print(each_item)
+
