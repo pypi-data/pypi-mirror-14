de107f9b4279d569ced84511d89b344f26a6dce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package tuhin00/1.0.0/tuhin00-1.0.0.tar.gz

---
+++ packages/tuhin00/tuhin00-1.0.0.tar.gz/tuhin00-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'tuhin00',
+version = '1.0.0',
+py_modules = ['looperr'],
+author = 'aa t',
+author_email = 't@gd.comm',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple LOOpER',
+)
