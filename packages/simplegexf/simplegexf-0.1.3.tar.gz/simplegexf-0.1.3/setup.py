01889df798790a95a5022871a253dab2b3c96c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package simplegexf/0.1.3/simplegexf-0.1.3.tar.gz

---
+++ packages/simplegexf/simplegexf-0.1.3.tar.gz/simplegexf-0.1.3/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+
+setup(
+    name="simplegexf",
+    version="0.1.3",
+    description="A simple .gexf parser/writer for python",
+    license="MIT",
+    url="https://github.com/demux/simplegexf",
+    install_requires=['xmltodict==0.9.2'],
+    long_description=open("README.md").read(),
+    keywords="simplegexf gephi gexf",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
