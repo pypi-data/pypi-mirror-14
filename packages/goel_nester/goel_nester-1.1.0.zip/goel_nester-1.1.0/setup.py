de426d7afb392101a11bc62b4903a4c13ee0ecc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package goel_nester/1.1.0/goel_nester-1.1.0.zip

---
+++ packages/goel_nester/goel_nester-1.1.0.zip/goel_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'goel_nester',
+version = '1.1.0',
+py_modules = ['nester'],
+author = 'GOEL',
+author_email = 'GOEL@Shubham.com',
+url = 'http://www.notavaible.com',
+description = 'A simple printer of nested lists',
+)
