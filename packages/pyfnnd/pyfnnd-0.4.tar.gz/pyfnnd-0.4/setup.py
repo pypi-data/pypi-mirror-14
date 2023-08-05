8b9029d92fe4fb73384c341b47086636a2aa1fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package pyfnnd/0.4/pyfnnd-0.4.tar.gz

---
+++ packages/pyfnnd/pyfnnd-0.4.tar.gz/pyfnnd-0.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pyfnnd',
+    author='Alistair Muldal',
+    version='0.4',
+    author_email='alimuldal@gmail.com',
+    url='https://github.com/alimuldal/PyFNND',
+    description='A Python implementation of fast non-negative deconvolution',
+    packages=['pyfnnd'],
+)
