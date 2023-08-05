dff426b107ae20196f3dff61c8705eae1c7a498e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package signingworker/0.11/signingworker-0.11.tar.gz

---
+++ packages/signingworker/signingworker-0.11.tar.gz/signingworker-0.11/signingworker/task.py
@@ -0,0 +1,48 @@
+import os
+import json
+import logging
+from jose import jwt
+from jose.constants import ALGORITHMS
+from jsonschema import validate
+from signingworker.exceptions import TaskVerificationError
+
+log = logging.getLogger(__name__)
+
+
+def validate_task(task):
+    task_schema = json.load(
+        open(
+            os.path.join(os.path.dirname(__file__),
+                         "data", "signing_task_schema.json")
+        )
+    )
+    validate(task, task_schema)
+
+
+def task_cert_type(task):
+    """Extract task certificate type"""
+    certs = [s for s in task["scopes"] if
+             # TODO: remove the following line when migrated to project:releng
+             s.startswith("signing:cert:") or
+             s.startswith("project:releng:signing:cert:")]
+    log.debug("Certificate types: %s", certs)
+    if len(certs) != 1:
+        raise TaskVerificationError("Only one certificate type can be used")
+    return certs[0]
+
+
+def task_signing_formats(task):
+    """Extract last part of signing format scope"""
+    return [s.split(":")[-1] for s in task["scopes"]
+            # TODO: remove the following line when migrated to project:releng
+            if s.startswith("signing:format:") or
+            s.startswith("project:releng:signing:format:")]
+
+
+def validate_signature(task_id, token, pub_key, algorithms=[ALGORITHMS.RS512]):
+    claims = jwt.decode(token, pub_key, algorithms=algorithms)
+    if task_id != claims.get("taskId"):
+        raise TaskVerificationError(
+            "Task IDs do not match. Expected %s, got %s" %
+            (task_id, claims.get("taskId")))
+    return claims
