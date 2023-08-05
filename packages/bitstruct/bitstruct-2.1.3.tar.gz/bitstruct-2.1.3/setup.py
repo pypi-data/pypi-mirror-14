ccb83fe73095659be3f10842c37596637ea1a201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:06 +0000

    Add package bitstruct/2.1.3/bitstruct-2.1.3.tar.gz

---
+++ packages/bitstruct/bitstruct-2.1.3.tar.gz/bitstruct-2.1.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bitstruct',
+      version='2.1.3',
+      description=('This module performs conversions between Python values '
+                   'and C bit field structs represented as Python '
+                   'bytearrays.'),
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist, Ilya Petukhov',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords=['bit field', 'bit parsing', 'bit unpack', 'bit pack'],
+      url='https://github.com/eerimoq/bitstruct',
+      py_modules=['bitstruct'],
+      test_suite="tests")
