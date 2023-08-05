4a0f568f2fa19a726ffd2819f1621b1fb961166c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:40 +0000

    Add package ct_nester/1.1.0/ct_nester-1.1.0.zip

---
+++ packages/ct_nester/ct_nester-1.1.0.zip/ct_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ct_nester',
+    version='1.1.0',
+    py_modules=['ct_nester'],
+    author='chentong',
+    author_email='ct1260942799@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists for test',
+    )
+    
