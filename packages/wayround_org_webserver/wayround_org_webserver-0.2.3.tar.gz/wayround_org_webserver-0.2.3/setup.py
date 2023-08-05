013530479c412571eb80ddf7c94c078fb4922b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package wayround_org_webserver/0.2.3/wayround_org_webserver-0.2.3.tar.gz

---
+++ packages/wayround_org_webserver/wayround_org_webserver-0.2.3.tar.gz/wayround_org_webserver-0.2.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='wayround_org_webserver',
+    version='0.2.3',
+    description='homebrewn webserver',
+    author='Alexey Gorshkov',
+    author_email='animus@wayround.org',
+    url='https://github.com/AnimusPEXUS/wayround_org_webserver',
+    install_requires=[
+        'wayround_org_utils'
+        ],
+    packages=[
+        'wayround_org.webserver',
+        'wayround_org.webserver.modules',
+        ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        ],
+    entry_points={
+        'console_scripts': [
+            'wrows = wayround_org.webserver.main'
+            ],
+        }
+    )
