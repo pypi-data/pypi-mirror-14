c2fc879c5f5e8a37c5af35253f004f2ec38137e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package marvelous/0.0.4/marvelous-0.0.4.tar.gz

---
+++ packages/marvelous/marvelous-0.0.4.tar.gz/marvelous-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '0.0.4'
+
+setup(
+    name='marvelous',
+    version=version,
+    description="Marvel API python wrapper.",
+    long_description=open("README.rst", "r").read(),
+    author='Robert Kuykendall',
+    author_email='robert@rkuykendall.com',
+    url='http://github.com/rkuykendall/marvelous',
+    packages=find_packages(),
+    zip_safe=True,
+
+    # requests-cache should be added here when the version in pypi supports
+    # ignored_parameters. Unitl then cache will not work without manually
+    # installing the version from github.
+    #
+    # TODO: Cache this missing module in the code and print instructions
+    #       for installing it.
+    install_requires=['marshmallow', 'requests'],
+    )
