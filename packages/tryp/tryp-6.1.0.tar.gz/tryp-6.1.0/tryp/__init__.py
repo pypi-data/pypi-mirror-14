fdce44464a7153e98a663c983e7366955e342946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:29 +0000

    Add package tryp/6.1.0/tryp-6.1.0.tar.gz

---
+++ packages/tryp/tryp-6.1.0.tar.gz/tryp-6.1.0/tryp/__init__.py
@@ -0,0 +1,20 @@
+from fn import _  # type: ignore
+
+from tryp.maybe import Maybe, Just, Empty, may, flat_may  # type: ignore
+from tryp.list import List  # type: ignore
+from tryp.map import Map  # type: ignore
+from tryp.either import Left, Right, Either  # type: ignore
+from tryp.future import Future
+from tryp.boolean import Boolean  # type: ignore
+from tryp.func import curried, F  # type: ignore
+from tryp.anon import __
+from tryp.env_vars import env
+from tryp.task import Try
+
+development = False
+integration_test = 'TRYP_INTEGRATION' in env
+
+
+__all__ = ('Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_', 'Future',
+           'Boolean', 'development', 'flat_may', 'curried', '__', 'F', 'Left',
+           'Right', 'Either', 'env', 'Try')
---
+++ packages/tryp/tryp-6.1.0.tar.gz/tryp-6.1.0/tryp/lazy_list.py
@@ -0,0 +1,4 @@
+class LazyList(Generic[A]):
+    pass
+
+__all__ = ()
