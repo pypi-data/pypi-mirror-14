c16b0f5aaf04df87fd16d85472c0188a37a4e05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package new_html/0.3.0/new_html-0.3.0.tar.gz

---
+++ packages/new_html/new_html-0.3.0.tar.gz/new_html-0.3.0/new_html/new_html.py
@@ -0,0 +1,16 @@
+__version__ = "0.3.0"
+
+
+import sys
+from .stuff import Stuff
+
+
+def new_html():
+    print("Executing bootstrap version %s." % __version__)
+    print("List of argument strings: %s" % sys.argv[1:])
+    print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
+    print "this is new as well"
+
+
+class Boo(Stuff):
+    pass
---
+++ packages/new_html/new_html-0.3.0.tar.gz/new_html-0.3.0/setup.py
@@ -0,0 +1,28 @@
+import re
+from setuptools import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('new_html/new_html.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "new_html",
+    packages = ["new_html"],
+    entry_points = {
+        "console_scripts": ['html = new_html.new_html:main']
+        },
+    version = version,
+    description = "Python command line html creator",
+    long_description = long_descr,
+    author = "Oscar Vazquez",
+    author_email = "oscar.vazquez2012@gmail.com",
+    url = "https://github.com/oscarvazquez/command_line_tool_demo"
+)
