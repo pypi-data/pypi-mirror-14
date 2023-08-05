d7d92910428a685d22edc9dc50410f399f5b5533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package microbot/0.5.0/microbot-0.5.0-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-0.5.0-py2.py3-none-any.whl/microbot/test/factories/handler.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+from factory import DjangoModelFactory, SubFactory, Sequence
+from microbot.models import Handler, Request, UrlParam, HeaderParam
+from microbot.test.factories import BotFactory
+
+
+class RequestFactory(DjangoModelFactory):
+    class Meta:
+        model = Request
+    url_template = "https://api.github.com/users/jlmadurga"
+    method = Request.GET
+    
+class UrlParamFactory(DjangoModelFactory):
+    class Meta:
+        model = UrlParam
+    key = Sequence(lambda n: 'key%d' % n)
+    value_template = Sequence(lambda n: '{{value%d}}' % n)
+    request = SubFactory(RequestFactory)
+    
+class HeaderParamFactory(DjangoModelFactory):
+    class Meta:
+        model = HeaderParam
+    key = Sequence(lambda n: 'key%d' % n)
+    value_template = Sequence(lambda n: '{{value%d}}' % n)
+    request = SubFactory(RequestFactory)
+
+
+class HandlerFactory(DjangoModelFactory):
+    class Meta:
+        model = Handler
+    bot = SubFactory(BotFactory)
+    pattern = "/github_user" 
+    request = SubFactory(RequestFactory)
+    response_text_template = '<a href="{{ html_url }}">{{ login }}</a>\n<b>{{ location }}</b>:<i>{{ created_at }}</i>'
+    response_keyboard_template = '[["followers", "{{ name }}"]]'
