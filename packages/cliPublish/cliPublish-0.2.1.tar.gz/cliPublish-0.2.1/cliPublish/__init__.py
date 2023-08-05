9b7ed9a5dcdbee0330e4cf8d3520519e9b878e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package cliPublish/0.2.1/cliPublish-0.2.1.tar.gz

---
+++ packages/cliPublish/cliPublish-0.2.1.tar.gz/cliPublish-0.2.1/cliPublish/__init__.py
@@ -0,0 +1,1 @@
+from .cliPublish import main
---
+++ packages/cliPublish/cliPublish-0.2.1.tar.gz/cliPublish-0.2.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Based on https://github.com/pypa/sampleproject
+See also: https://packaging.python.org/en/latest/distributing.html
+"""
+
+import sys
+
+if sys.version_info.major < 3:
+    raise Exception("Python2 is not supported, install with pip3.")
+
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cliPublish',
+    version='0.2.1',
+
+    description='Convenient commandline utility to publish files on a webserver using rsync',
+    keywords='rsync upload cli',
+    long_description=long_description,
+
+    url='https://github.com/mheistermann/cliPublish',
+
+    author='Martin Heistermann',
+    author_email='sieve-git-pushdeploy@mheistermann.de',
+
+    license='GPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    packages=find_packages(),
+
+    install_requires=[],
+
+    entry_points={
+        'console_scripts': [
+            'cliPublish=cliPublish:main',
+        ],
+    },
+    package_data={
+        'cliPublish': ['README.md', 'cliPublish/remotes.conf'],
+    },
+    include_package_data=True,
+)
