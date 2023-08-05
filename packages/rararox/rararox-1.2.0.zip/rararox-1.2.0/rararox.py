08bb5fea0556de6a0a2469c0cbcac7199dc2a9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package rararox/1.2.0/rararox-1.2.0.zip

---
+++ packages/rararox/rararox-1.2.0.zip/rararox-1.2.0/rararox.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, level):
+	for each_list in the_list:
+		if isinstance(each_list, list):
+			print_lol(each_list, level+1)
+		else:
+                        for tab_stop in range(level):
+                                print ("\t")
+			print (each_list)
+			
---
+++ packages/rararox/rararox-1.2.0.zip/rararox-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'rararox',
+		version = '1.2.0',
+		py_modules = ['rararox'],
+		author = 'RahulR',
+		author_mail = 'rahulrijhwani@live.com',
+		url = 'http://getgobble.com',
+		description = 'Simple prrogram',
+		)
