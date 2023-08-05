546bde28a17da2aa47a66033e2c8699e95cf0f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package pidan/1.0.9.27/pidan-1.0.9.27.zip

---
+++ packages/pidan/pidan-1.0.9.27.zip/pidan-1.0.9.27/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.9.27',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = u'更新http.get_param_from_post函数，增加三个参数类型',
+    package_data = {
+        '': ['*.data'],
+    },
+)
