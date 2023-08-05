1925a02242da3ddc5fd5274bb476138ba2034b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:06 +0000

    Add package tox-setuptools/1.3.2/tox-setuptools-1.3.2.tar.gz

---
+++ packages/tox-setuptools/tox-setuptools-1.3.2.tar.gz/tox-setuptools-1.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tox-setuptools',
+    version='1.3.2',
+    py_modules=['tox_setuptools'],
+
+    entry_points={'distutils.commands': [
+        'test = tox_setuptools:ToxTest'
+    ]},
+
+    license='Apache 2',
+    description='Tox integration with setuptools',
+    url='https://github.com/2m/tox-setuptools',
+)
