5faaf41c3f8bdd3b3718f6b1aca7c13e9a63a112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package metarmonitor/0.8.0/metarmonitor-0.8.0.tar.gz

---
+++ packages/metarmonitor/metarmonitor-0.8.0.tar.gz/metarmonitor-0.8.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='metarmonitor',
+    version='0.8.0',
+    description='Monitor METAR and TAF reports',
+    url='www.metpod.co.uk',
+    license='MIT',
+    author='Mark Baker',
+    author_email='mark2182@mac.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['pillow', 'requests', 'twilio', 'apscheduler'],
+    package_data={
+        '': ['images/*.gif'],
+    },
+    entry_points={
+          'gui_scripts': [
+               'metarmonitor = metarmonitor.__main__:main'
+          ]
+    },
+)
