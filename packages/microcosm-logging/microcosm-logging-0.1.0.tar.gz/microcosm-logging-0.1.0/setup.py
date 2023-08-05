f5029736023f6309187336fc999c329f1c2244f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package microcosm-logging/0.1.0/microcosm-logging-0.1.0.tar.gz

---
+++ packages/microcosm-logging/microcosm-logging-0.1.0.tar.gz/microcosm-logging-0.1.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+project = "microcosm-logging"
+version = "0.1.0"
+
+setup(
+    name=project,
+    version=version,
+    description="Opinioned logging configuration",
+    author="Globality Engineering",
+    author_email="engineering@globality.com",
+    url="https://github.com/globality-corp/microcosm-logging",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    keywords="microcosm",
+    install_requires=[
+        "loggly-python-handler>=1.0.0",
+        "microcosm>=0.3.0",
+    ],
+    setup_requires=[
+        "nose>=1.3.6",
+    ],
+    dependency_links=[
+    ],
+    entry_points={
+        "microcosm.factories": [
+            "logger = microcosm_logging.factories:configure_logging"
+        ],
+    },
+    tests_require=[
+        "coverage>=3.7.1",
+        "mock>=1.0.1",
+        "PyHamcrest>=1.8.5",
+    ],
+)
