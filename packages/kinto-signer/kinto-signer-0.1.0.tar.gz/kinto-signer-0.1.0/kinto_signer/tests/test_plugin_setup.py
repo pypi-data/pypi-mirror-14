e785ade049ece026522383edc30d59f30febedc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:43 +0000

    Add package kinto-signer/0.1.0/kinto-signer-0.1.0.tar.gz

---
+++ packages/kinto-signer/kinto-signer-0.1.0.tar.gz/kinto-signer-0.1.0/kinto_signer/tests/test_plugin_setup.py
@@ -0,0 +1,19 @@
+from cliquet.tests.support import unittest
+
+from . import BaseWebTest
+
+
+class HelloViewTest(BaseWebTest, unittest.TestCase):
+
+    def test_capability_is_exposed(self):
+        self.maxDiff = None
+        resp = self.app.get('/')
+        capabilities = resp.json['capabilities']
+        self.assertIn('signer', capabilities)
+        expected = {
+            "description": "Provide signing capabilities to the server.",
+            "url": ("https://github.com/mozilla-services/kinto-signer"
+                    "#kinto-signer"),
+            "resources": ["source/collection1", "source/collection2"]
+        }
+        self.assertEqual(expected, capabilities['signer'])
