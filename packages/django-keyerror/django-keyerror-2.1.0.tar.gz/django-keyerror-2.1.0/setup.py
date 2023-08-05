84513f7d274d5a0d06e29ee88ca13e8af73a78e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:27 +0000

    Add package django-keyerror/2.1.0/django-keyerror-2.1.0.tar.gz

---
+++ packages/django-keyerror/django-keyerror-2.1.0.tar.gz/django-keyerror-2.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-keyerror',
+    version='2.1.0',
+    description='KeyError.com Django client',
+
+    url='https://keyerror.com/docs/django',
+    author="Chris Lamb",
+    author_email='chris@keyerror.com',
+
+    packages=find_packages(),
+)
