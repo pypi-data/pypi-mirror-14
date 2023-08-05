f1d239552263672d4b2bb1dc10f5f5427404a99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package pip-demo/0.1.0/pip_demo-0.1.0-py3-none-any.whl

---
+++ packages/pip-demo/pip_demo-0.1.0-py3-none-any.whl/new_demo/new_demo.py
@@ -0,0 +1,9 @@
+__version__ ="0.1.0"
+
+import sys
+class awesome_class:
+    def awesome_function(self):
+        print('this is an amazing function')
+        print('this is the sys arguments')
+        print(sys.argv)
+        
