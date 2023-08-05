882600477b841232a392d6cd1612a5dc3cfe081b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:02 +0000

    Add package panoramix/0.8.0/panoramix-0.8.0.tar.gz

---
+++ packages/panoramix/panoramix-0.8.0.tar.gz/panoramix-0.8.0/panoramix/static/assets/javascripts/featured.js
@@ -0,0 +1,18 @@
+var $ = window.$ = require('jquery');
+var jQuery = window.jQuery = $;
+var px = require('./modules/panoramix.js');
+
+require('datatables');
+require('../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css')
+require('bootstrap');
+
+$(document).ready(function () {
+  $('#dataset-table').DataTable({
+    bPaginate: false,
+    order: [
+      [1, "asc"]
+    ]
+  });
+  $('#dataset-table_info').remove();
+  $('#dataset-table').show();
+});
