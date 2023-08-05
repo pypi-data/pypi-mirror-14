8fbb6ab82af145a500b545ceda46adbab39cb61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package MaDdY/1.1.0/MaDdY-1.1.0.zip

---
+++ packages/MaDdY/MaDdY-1.1.0.zip/MaDdY-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MaDdY',
+    version='1.1.0',
+    py_modules=['MaDdY'],
+    author='akshat',
+    authoor_email='makshat3@gmailcom',
+    url='www.gmail.com',
+    description='A Simple Program',
+    )
+    
