30dc572e3a55f1ecbd87620a306881af5c67546a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:21 +0000

    Add package leonardo-news/2016.3.2/leonardo-news-2016.3.2.tar.gz

---
+++ packages/leonardo-news/leonardo-news-2016.3.2.tar.gz/leonardo-news-2016.3.2/news/widget/news/models.py
@@ -0,0 +1,22 @@
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from leonardo.module.web.models import ListWidget
+
+from articles.models import Article
+
+
+class NewsWidget(ListWidget):
+
+    number = models.IntegerField()
+    show_archive_button = models.BooleanField(
+        default=True, verbose_name=_("show archive button"))
+
+    def get_items(self):
+        return Article.objects.all()[:self.number]
+
+    class Meta:
+        abstract = True
+        verbose_name = _("last news")
+        verbose_name_plural = _("last news")
