291b24d67fa447c51e9e5d9f9460f62147d8dea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package django-clubhouse/0.0.2/django_clubhouse-0.0.2-py2.py3-none-any.whl

---
+++ packages/django-clubhouse/django_clubhouse-0.0.2-py2.py3-none-any.whl/clubhouse/contrib/admin/page.py
@@ -0,0 +1,32 @@
+# vim: ai ts=4 sts=4 et sw=4
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from mezzanine.pages.admin import PageAdmin
+
+from clubhouse import admin
+from clubhouse.contrib.models import ModularPage,PageContentBlock,\
+        PageAsideBlock,ContentBlock,AsideBlock
+from clubhouse.admin import BlockInline
+
+__all__ = ['ModularPageAdmin','PageContentBlockInline','PageAsideBlockInline']
+
+
+class PageContentBlockInline(BlockInline):
+    model = PageContentBlock
+    block_type = ContentBlock
+
+
+class PageAsideBlockInline(BlockInline):
+    model = PageAsideBlock
+    block_type = AsideBlock
+
+
+class ModularPageAdmin(PageAdmin):
+    inlines = (
+        PageAsideBlockInline,
+        PageContentBlockInline
+    )
+
+
+admin.site.register(ModularPage,ModularPageAdmin)
---
+++ packages/django-clubhouse/django_clubhouse-0.0.2-py2.py3-none-any.whl/django_clubhouse-0.0.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+# Django-Clubhouse
+An Open Source content management system build to compliment the [Mezzanine CMS](https://github.com/stephenmcd/mezzanine), by adding the concept of Modular Pages and block admin for multi-model change lists.
+
+## Installation
++ Install django-clubhouse with `pip install django-clubhouse` (Not yet submitted)
++ Add `clubhouse.core`, `clubhouse.contrib`, `clubhouse.forms` to `INSTALLED_APPS` in your Django Settings module
+
+
+
