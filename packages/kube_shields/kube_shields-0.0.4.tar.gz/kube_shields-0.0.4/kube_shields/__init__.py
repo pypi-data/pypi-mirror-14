8dcae62ce79ef153587f6a5302b4654608db6598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package kube_shields/0.0.4/kube_shields-0.0.4.tar.gz

---
+++ packages/kube_shields/kube_shields-0.0.4.tar.gz/kube_shields-0.0.4/kube_shields/__init__.py
@@ -0,0 +1,22 @@
+"""Kube shields."""
+
+
+import os
+from flask import Flask
+
+
+__author__ = "Adam Talsma"
+__author_email__ = "se-adam.talsma@ccpgames.com"
+__version__ = "0.0.4"
+
+
+app = Flask(__name__)
+
+SITE_NAME = os.environ.get("SHIELD_SITE_NAME", "shields")
+OTHER_SHIELDS = os.environ.get("OTHER_SHIELDS", "")
+OTHER_SHIELDS = OTHER_SHIELDS.split(" ") if OTHER_SHIELDS else False
+try:
+    with open(os.environ.get("INTRA_SHIELD_SECRET"), "r") as openkey:
+        INTRA_SECRET = openkey.read().strip()
+except:
+    INTRA_SECRET = "not secret"
