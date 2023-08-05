6808c985b9ee873841de1dbfb76d8b78061334a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:22 +0000

    Add package py2016/1.0.0/py2016-1.0.0.tar.gz

---
+++ packages/py2016/py2016-1.0.0.tar.gz/py2016-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py2016',
+    packages=['pkg1', 'pkg2'],
+    version='1.0.0',
+    description='My first python project',
+    author='jimlintw922',
+    author_email='jimlintw922@gmail.com',
+    url='https://github.com/jinsenglin/python/tree/master/src/py2016',
+    keywords=['example'],
+)
