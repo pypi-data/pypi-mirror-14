5d59c0e3f61d3d48b9c6460dc11a582c1035158e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:13 +0000

    Add package dnester/1.1.0/dnester-1.1.0.tar.gz

---
+++ packages/dnester/dnester-1.1.0.tar.gz/dnester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+		name = 'dnester',
+		version = '1.1.0',
+		py_modules = ['dnester'],
+		author = 'dizero',
+		author_email = 'dizero@tutanota.com',
+		description = 'A simple printer of nested lists',
+	)
