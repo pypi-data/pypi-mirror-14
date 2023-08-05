8844860413bdbc4d56af6534fb155b2c5eeecf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package kytea/0.1.3/kytea-0.1.3.tar.gz

---
+++ packages/kytea/kytea-0.1.3.tar.gz/kytea-0.1.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    from distutils.core import setup, Extension, find_packages
+
+ext_module = Extension('_Mykytea',
+                       sources=['lib/kytea/mykytea_wrap.cxx', 'lib/kytea/mykytea.cpp'],
+                       libraries=["kytea"],
+                       include_dirs=['lib/kytea']
+                       )
+
+import os
+import sys
+
+libdir = 'lib/kytea'
+sys.path.append('./lib/kytea')
+sys.path.append('./lib/test')
+
+import version
+
+setup(
+      name='kytea',
+      version='0.1.3',
+      author='Michiaki Ariga',
+      author_email='chezou@gmail.com',
+      description = ('An text analysis toolkit KyTea binding'),
+      license = 'MIT License',
+      keywords = 'KyTea NLP',
+      url = 'https://github.com/chezou/Mykytea-python',
+      download_url = 'https://github.com/chezou/Mykytea-python/tarball/0.1.3',
+      classifier    = [
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing :: Linguistic",
+      ],
+      ext_modules=[ext_module],
+      packages= find_packages(libdir),
+      package_dir={"": libdir},
+      py_modules=['Mykytea'],
+      test_suite= 'mykytea_test.suite'
+      )
