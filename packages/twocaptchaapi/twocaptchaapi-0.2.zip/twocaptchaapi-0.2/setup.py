518ffb214206ef12da0ee6a1b771ac84c2e28045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package twocaptchaapi/0.2/twocaptchaapi-0.2.zip

---
+++ packages/twocaptchaapi/twocaptchaapi-0.2.zip/twocaptchaapi-0.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+from distutils.core import setup
+
+setup(
+    name='twocaptchaapi',
+    version='0.2',
+    packages=['twocaptchaapi'],
+    url='https://github.com/athre0z/twocaptcha-api',
+    license='MIT',
+    author='Joel Höner (athre0z)',
+    author_email='joel@zyantific.com',
+    description='Python API implementation for 2Captcha.com',
+    download_url='https://github.com/athre0z/twocaptcha-api/archive/v0.2.tar.gz',
+    install_requires=[
+        'requests>=2.9',
+    ],
+)
