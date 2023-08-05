6fa4c519716e6d3250450a258dc14e9d71b1d831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:28 +0000

    Add package geo-squizzy/0.1.1/geo-squizzy-0.1.1.tar.gz

---
+++ packages/geo-squizzy/geo-squizzy-0.1.1.tar.gz/geo-squizzy-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+# TODO there is some problem with adding README.md file which have markup tokens
+long_description = 'None'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+# dev version syntax 0.1.dev4
+setup(
+    name='geo-squizzy',
+    version='0.1.1',
+    packages=find_packages(exclude=['research*']),
+    license='MIT',
+    description='GeoJSON-unknown-documents-model-creation',
+    long_description=long_description,
+    url='https://github.com/LowerSilesians/geo-squizzy',
+    author='Igor Miazek, Jakub Miazek',
+    author_email='t-32@o2.pl, the@grillazz.com'
+)
