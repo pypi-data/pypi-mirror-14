e21ef24fdddc17565e2f41e2b4e8fb03bc60b8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package desnapifier/0.6.1/desnapifier-0.6.1.tar.gz

---
+++ packages/desnapifier/desnapifier-0.6.1.tar.gz/desnapifier-0.6.1/desnapifier/notsupported.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2016 Jeandre Kruger
+#
+# This file is part of desnapifier.
+#
+# desnapifier is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# desnapifier is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with desnapifier.  If not, see <http://www.gnu.org/licenses/>.
+
+class InheritanceNotSupportedError(Exception):
+    pass
+
+class UnsupportedBlockError(Exception):
+    pass
