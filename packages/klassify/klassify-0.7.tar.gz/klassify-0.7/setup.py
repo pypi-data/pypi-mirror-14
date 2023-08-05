e605d1b4c60af57aa9bc8dbdbb9d5309691a0cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package klassify/0.7/klassify-0.7.tar.gz

---
+++ packages/klassify/klassify-0.7.tar.gz/klassify-0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='klassify',
+    version='0.7',
+    description='Redis based text classification service',
+    author='Fatih Erikli',
+    author_email='fatiherikli@gmail.com',
+    url='https://github.com/fatiherikli/klassify',
+    include_package_data=True,
+    packages=[
+        'klassify',
+        'klassify.backend',
+    ],
+    install_requires=[
+        'nltk==3.1',
+        'tornado==4.3',
+        'tornado-redis==2.4.18'
+    ],
+    classifiers=[
+        'Environment :: Web Environment'
+    ]
+)
