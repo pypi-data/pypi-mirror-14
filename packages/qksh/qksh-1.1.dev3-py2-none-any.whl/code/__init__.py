06228ea04753a805157156df7c31828863a26db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package qksh/1.1.dev3/qksh-1.1.dev3-py2-none-any.whl

---
+++ packages/qksh/qksh-1.1.dev3-py2-none-any.whl/code/__init__.py
@@ -0,0 +1,3 @@
+# TODO: import all the package here and parse all the CLI parameter.
+# TODO: implement qkshell, autocompletion, CLI command to get command output.
+# CMD2 package. maybe we can create the shell on ipython
