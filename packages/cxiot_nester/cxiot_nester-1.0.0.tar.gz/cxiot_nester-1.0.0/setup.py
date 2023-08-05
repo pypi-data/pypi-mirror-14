5a27d38db4b912b5a04e37c6af9df3605a4a45c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package cxiot_nester/1.0.0/cxiot_nester-1.0.0.tar.gz

---
+++ packages/cxiot_nester/cxiot_nester-1.0.0.tar.gz/cxiot_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        = 'cxiot_nester',
+    version     = '1.0.0',
+    py_modules  = ['cxiot_nester'],
+    author      = 'caoshuo',
+    author_email= 'cao87620@126.com',
+    url         = 'http://www.cxiot.com',
+    description = 'A simple test'
+)
