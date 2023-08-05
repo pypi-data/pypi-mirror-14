20d2a63c2f5d8352852bd7c89f182be836019224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package py-cricket/0.0.1/py-cricket-0.0.1.zip

---
+++ packages/py-cricket/py-cricket-0.0.1.zip/py-cricket-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'py-cricket',
+    version = '0.0.1',
+    description = 'Easy to install and simple way to access all Roanuz Cricket APIs. Its a library for showing Live Cricket Score, Cricket Schedule and Statistics',
+    author = 'Roanuz Softwares Private Ltd',
+    author_email = 'contact@roanuz.com',
+    url = 'https://github.com/roanuz/py-cricket', 
+    py_modules=['pycricket','pycricket_storagehandler'],
+    install_requires=['requests==2.5.1'],
+    entry_points='''
+        [console_scripts]
+        pycricket=pycricket:RcaAppApp
+    ''',
+)
