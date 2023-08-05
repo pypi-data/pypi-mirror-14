537af1eb35f53a7cb1a2052fbe9216128c733a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package branca/0.1.1/branca-0.1.1-py2-none-any.whl

---
+++ packages/branca/branca-0.1.1-py2-none-any.whl/branca/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import branca.colormap as colormap
+import branca.element as element
+
+__version__ = '0.1.1'
+
+__all__ = ['colormap',
+           'element',
+           ]
