2c4e5ae3cdf86b99c83fc0fbfd894a786388251b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:27 +0000

    Add package shine/0.1.7/shine-0.1.7.tar.gz

---
+++ packages/shine/shine-0.1.7.tar.gz/shine-0.1.7/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="shine",
+    version='0.1.7',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    scripts=['shine/bin/shined.py'],
+    install_requires=[],
+    url="https://github.com/dantezhu/shine",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="shine",
+)
---
+++ packages/shine/shine-0.1.7.tar.gz/shine-0.1.7/shine/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.7'
+
+from gateway.gateway import Gateway
+from forwarder.forwarder import Forwarder
+from worker.worker import Worker
+from trigger.trigger import Trigger
+from share.log import logger
