f5207a7eea31260cbac7cb745dcbfb3cec8f7deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:02 +0000

    Add package uoftscrapers/0.1.7/uoftscrapers-0.1.7.tar.gz

---
+++ packages/uoftscrapers/uoftscrapers-0.1.7.tar.gz/uoftscrapers-0.1.7/uoftscrapers/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import os
+import sys
+
+from .scrapers.coursefinder import CourseFinder
+from .scrapers.utmtimetable import UTMTimetable
+from .scrapers.utsctimetable import UTSCTimetable
+from .scrapers.utsgtimetable import UTSGTimetable
+from .scrapers.utsgcalendar import UTSGCalendar
+from .scrapers.buildings import Buildings
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logging.getLogger("uoftscrapers").addHandler(NullHandler())
