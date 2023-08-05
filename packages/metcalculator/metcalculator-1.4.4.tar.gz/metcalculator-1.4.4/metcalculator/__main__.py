7e870873d1464feb1b7522bfa28ba2757d214c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:09 +0000

    Add package metcalculator/1.4.4/metcalculator-1.4.4.tar.gz

---
+++ packages/metcalculator/metcalculator-1.4.4.tar.gz/metcalculator-1.4.4/metcalculator/__main__.py
@@ -0,0 +1,16 @@
+import sys
+import metcalculator
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    controller = metcalculator.Controller()
+    controller.run()
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do.
+
+if __name__ == "__main__":
+    main()
