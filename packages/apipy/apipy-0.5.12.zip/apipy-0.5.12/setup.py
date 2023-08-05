76d502b6fceff995be99bdbd48afc5118af5353b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package apipy/0.5.12/apipy-0.5.12.zip

---
+++ packages/apipy/apipy-0.5.12.zip/apipy-0.5.12/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'apipy',
+    packages = ['apipy'], # this must be the same as the name above
+    version = '0.5.12',
+    description = 'A Python 3 library for interacting with the Stack Exchange API.',
+    author = 'ArtOfCode',
+    author_email = 'hello@artofcode.co.uk',
+    download_url = 'https://github.com/ArtOfCode-/apipy-src/tarball/0.5.10'
+)
