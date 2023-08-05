bbba18505c65f8b8d56ca0e61e88a4c35ecb4e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package microbot/0.6.0/microbot-0.6.0-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-0.6.0-py2.py3-none-any.whl/microbot/models/__init__.py
@@ -0,0 +1,4 @@
+from microbot.models.telegram_api import (User, Chat, Message, Update)  # NOQA
+from microbot.models.bot import Bot  # NOQA
+from microbot.models.handler import Handler, Request, UrlParam, HeaderParam  # NOQA
+from microbot.models.environment_vars import EnvironmentVar  # NOQA
