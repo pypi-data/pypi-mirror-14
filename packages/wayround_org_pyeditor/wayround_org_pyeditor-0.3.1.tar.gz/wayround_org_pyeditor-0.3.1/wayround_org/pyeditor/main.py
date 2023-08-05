8c91e4b6c07a2c69a9c37f9566f420f5af00820f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package wayround_org_pyeditor/0.3.1/wayround_org_pyeditor-0.3.1.tar.gz

---
+++ packages/wayround_org_pyeditor/wayround_org_pyeditor-0.3.1.tar.gz/wayround_org_pyeditor-0.3.1/wayround_org/pyeditor/main.py
@@ -0,0 +1,13 @@
+
+from gi.repository import Gtk
+
+import wayround_org.pyeditor.main_window
+
+w = wayround_org.pyeditor.main_window.MainWindow()
+
+w.show()
+w.install_mode('dummy')
+w.projects.load_config()
+w.buffer_clip.load_config()
+
+Gtk.main()
