6e82cad9d7576bfb714944712c33f61bb803d85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package sn1g/1.0.0/sn1g-1.0.0.zip

---
+++ packages/sn1g/sn1g-1.0.0.zip/sn1g-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name           = 'sn1g',
+      version        = '1.0.0',
+      py_modules     = ['nester'],
+      author         = 'hfpython',
+      author_email   = '123@123.com',
+      url            = 'http://www.123.com',
+      description    = 'A simple printer of nested lists')
