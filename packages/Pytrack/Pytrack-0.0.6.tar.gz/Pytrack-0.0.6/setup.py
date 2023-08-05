12a0ac5130513045e8a9ea6c3b0c83f1dedc927a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package Pytrack/0.0.6/Pytrack-0.0.6.tar.gz

---
+++ packages/Pytrack/Pytrack-0.0.6.tar.gz/Pytrack-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	name = 'Pytrack',
+	packages = ['Pytrack'],
+	version = '0.0.6',
+	description = 'face tracking software for use with the raspberry pi.',
+	author = 'Ben Brook, Justin Brown, Ernesto Perez',
+	url = 'https://github.com/opentracking/pytrack',
+	download_url = '://github.com/opentracking/pytrack/archive/0.0.6.tar.gz',
+	keywords = ['track', 'pi', 'face', 'pytrack', 'motor'],
+	classifiers = [],
+	)
