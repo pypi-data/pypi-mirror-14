efdbb1b3d878fe84b2547e2e72085c2fe7dbf2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:33 +0000

    Add package rowingdata/0.2/rowingdata-0.2.zip

---
+++ packages/rowingdata/rowingdata-0.2.zip/rowingdata-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rowingdata',
+      version='0.2',
+      description='The rowingdata library to create colorful plots from CrewNerd, Painsled and other rowing data tools',
+      url='http://rowsandall.wordpress.com',
+      author='Sander Roosendaal',
+      author_email='roosendaalsander@gmail.com',
+      license='MIT',
+      packages=['rowingdata'],
+      zip_safe=False)
