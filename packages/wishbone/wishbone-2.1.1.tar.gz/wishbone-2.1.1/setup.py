9f065ba11b852a7ce6c6747ad3673d7ca7debde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package wishbone/2.1.1/wishbone-2.1.1.tar.gz

---
+++ packages/wishbone/wishbone-2.1.1.tar.gz/wishbone-2.1.1/setup.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#
+#  Copyright 2016 Jelle Smet <development@smetj.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+import os
+
+# Required to make pycrypto compile with PyPy
+# https://github.com/dlitz/pycrypto/pull/59
+if "PyPy" in sys.version:
+    print("We are running PyPY, disable gmp to prevent pycrypto build error.")
+    os.environ["with_gmp"] = "no"
+
+PROJECT = 'wishbone'
+VERSION = '2.1.1'
+
+install_requires = ['gevent==1.1rc4',
+                    'greenlet==0.4.9',
+                    'argparse==1.3.0',
+                    'prettytable==0.7.2',
+                    'python-daemon==1.6',
+                    'pyyaml==3.11',
+                    'msgpack-python==0.4.7',
+                    'pyzmq==14.7.0',
+                    'amqp==1.4.9',
+                    'jinja2==2.8',
+                    'jsonschema==2.5.1',
+                    'gearman==2.0.2',
+                    'pycrypto==2.6.1',
+                    'flask==0.10.1',
+                    'gevent_inotifyx==0.1.1',
+                    'requests==2.7.0',
+                    'colorama==0.3.3',
+                    'arrow==0.6.0',
+                    'elasticsearch==1.6.0',
+                    'importlib==1.0.3',
+                    'uplook==0.4.1',
+                    'pyjq==1.1']
+
+# Dirty hack to make readthedocs build the docs
+# For some reason mocking out jq as documented is not working
+
+if os.environ.get("READTHEDOCS", False):
+    dependency_links = []
+    install_requires.remove('pyjq==1.1')
+
+else:
+    dependency_links = [
+        'https://github.com/smetj/pyjq/tarball/master#egg=pyjq-1.1'
+    ]
+
+# Deps pulled in by other modules
+# 'lockfile==0.10.2'
+# 'MarkupSafe==0.23'
+# 'repoze.lru==0.6',
+# 'werkzeug==0.10.4',
+# 'itsdangerous==0.24',
+# 'inotifyx==0.2.2',
+# 'six==1.9.0',
+# 'python-dateutil==2.4.2',
+# 'urllib3==1.12',
+
+try:
+    with open('README.rst', 'rt') as f:
+        long_description = f.read()
+except IOError:
+    long_description = ''
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='Build composable event pipeline servers with minimal effort.',
+    long_description=long_description,
+
+    author='Jelle Smet',
+    author_email='development@smetj.net',
+
+    url='https://github.com/smetj/wishbone',
+    download_url='https://github.com/smetj/wishbone/tarball/master',
+    dependency_links=dependency_links,
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: System Administrators',
+                 ],
+    extras_require={
+        'testing': ['pytest'],
+    },
+    platforms=['Linux'],
+    test_suite='tests.test_wishbone',
+    cmdclass={'test': PyTest},
+    scripts=[],
+    provides=[],
+    install_requires=install_requires,
+    namespace_packages=[],
+    packages=find_packages(),
+    package_data={'': ['data/wordlist.txt', 'data/LICENCE', 'data/sse.html', 'data/banner.tmpl']},
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['wishbone = wishbone.bootstrap:main'],
+        'wishbone.flow': [
+            'fanout = wishbone.module.fanout:Fanout',
+            'funnel = wishbone.module.funnel:Funnel',
+            'match = wishbone.module.match:Match',
+            'roundrobin = wishbone.module.roundrobin:RoundRobin',
+            'ttl = wishbone.module.ttl:TTL',
+            'jsonvalidate = wishbone.module.jsonvalidate:JSONValidate',
+            'jq = wishbone.module.wb_jq:JQ'
+        ],
+        'wishbone.encode': [
+            'graphite = wishbone.module.graphite:Graphite',
+            'humanlogformat = wishbone.module.humanlogformat:HumanLogFormat',
+            'influxdb = wishbone.module.influxdb:InfluxDB',
+            'msgpack = wishbone.module.msgpackencode:MSGPackEncode',
+            'json = wishbone.module.jsonencode:JSONEncode'
+        ],
+        'wishbone.decode': [
+            'msgpack = wishbone.module.msgpackdecode:MSGPackDecode',
+            'json = wishbone.module.jsondecode:JSONDecode'
+        ],
+        'wishbone.function': [
+            'deserialize = wishbone.module.deserialize:Deserialize',
+            'modify = wishbone.module.modify:Modify',
+            'loglevelfilter = wishbone.module.loglevelfilter:LogLevelFilter',
+            'template = wishbone.module.template:Template'
+        ],
+        'wishbone.input': [
+            'amqp = wishbone.module.amqpin:AMQPIn',
+            'dictgenerator = wishbone.module.dictgenerator:DictGenerator',
+            'disk = wishbone.module.diskin:DiskIn',
+            'fresh = wishbone.module.fresh:Fresh',
+            'httpclient = wishbone.module.httpinclient:HTTPInClient',
+            'httpserver = wishbone.module.httpinserver:HTTPInServer',
+            'namedpipe = wishbone.module.namedpipein:NamedPipeIn',
+            'tcp = wishbone.module.tcpin:TCPIn',
+            'testevent = wishbone.module.testevent:TestEvent',
+            'udp = wishbone.module.udpin:UDPIn',
+            'gearman = wishbone.module.gearmanin:GearmanIn',
+            'zeromq_topic = wishbone.module.zmqtopicin:ZMQTopicIn',
+            'zeromq_pull = wishbone.module.zmqpullin:ZMQPullIn'
+        ],
+        'wishbone.output': [
+            'amqp = wishbone.module.amqpout:AMQPOut',
+            'disk = wishbone.module.diskout:DiskOut',
+            'elasticsearch = wishbone.module.elasticsearchout:ElasticSearchOut',
+            'email = wishbone.module.emailout:EmailOut',
+            'file = wishbone.module.fileout:FileOut',
+            'http = wishbone.module.httpoutclient:HTTPOutClient',
+            'null = wishbone.module.null:Null',
+            'stdout = wishbone.module.stdout:STDOUT',
+            'syslog = wishbone.module.wbsyslog:Syslog',
+            'tcp = wishbone.module.tcpout:TCPOut',
+            'udp = wishbone.module.udpout:UDPOut',
+            'uds = wishbone.module.udsout:UDSOut',
+            'sse = wishbone.module.sse:ServerSentEvents',
+            'zeromq_topic = wishbone.module.zmqtopicout:ZMQTopicOut',
+            'zeromq_push = wishbone.module.zmqpushout:ZMQPushOut'
+        ],
+        'wishbone.contrib.flow': [
+        ],
+        'wishbone.contrib.encode': [
+        ],
+        'wishbone.contrib.decode': [
+        ],
+        'wishbone.contrib.function': [
+        ],
+        'wishbone.contrib.input': [
+        ],
+        'wishbone.contrib.output': [
+        ]
+    }
+)
---
+++ packages/wishbone/wishbone-2.1.1.tar.gz/wishbone-2.1.1/wishbone/module/ircin.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ircin.py
+#
+#  Copyright 2016 Jelle Smet <development@smetj.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+from gevent import monkey; monkey.patch_all()
+from wishbone import Actor
+
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+
+
+class IRCBot(Actor):
+
+    '''**Joins an IRC channel to accept input.**
+
+    A simple IRC bot which joins a channel, reads all public and private
+    messages and sends these to the module's queues.
+
+
+    Parameters:
+
+        - server(str)("localhost")
+           |  The IRC server to connect to.
+
+        - port(int)(6667)
+           |  The IRC server port to connect to.
+
+        - nickname(str)("wishbone")
+           |  The bot's nickname.
+
+        - channels(list)(["wishbone"])
+           |  The list of channels to join.
+           |  Each channel is mapped to a queue.
+           |  The queue name has the # portion stripped off.
+
+    Queues:
+
+        - outbox
+           |  The messages of all channels.
+
+        - <channels>
+
+           |  For each channel in <channels> a queue is created receiving the
+           |  messages of only that channel.
+    '''
+
+    def __init__(self, actor_config, server="localhost", port=6667, nickname="wishbone", channels=["wishbone"]):
+        Actor.__init__(self, actor_config)
+
+        self.pool.createQueue("outbox")
+
+        for channel in self.kwargs.channels:
+            self.pool.createQueue(channel)
+
+    def preHook(self):
+
+
+
+
+
+class TestBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channels, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        # non-chat DCC messages are raw bytes; decode as text
+        text = e.arguments[0].decode('utf-8')
+        c.privmsg("You said: " + text)
+
+    def on_dccchat(self, c, e):
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = sorted(chobj.users())
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = sorted(chobj.opers())
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = sorted(chobj.voiced())
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        elif cmd == "dcc":
+            dcc = self.dcc_listen()
+            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+                ip_quad_to_numstr(dcc.localaddress),
+                dcc.localport))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
