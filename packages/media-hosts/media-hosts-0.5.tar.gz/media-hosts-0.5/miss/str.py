8ee96a19a06c338342d67d69b04c7f681882fae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package media-hosts/0.5/media-hosts-0.5.tar.gz

---
+++ packages/media-hosts/media-hosts-0.5.tar.gz/media-hosts-0.5/miss/str.py
@@ -0,0 +1,47 @@
+# This file is part of a program licensed under the terms of the GNU Lesser
+# General Public License version 3 (or at your option any later version)
+# as published by the Free Software Foundation.
+#
+# If you have not received a copy of the GNU Lesser General Public License
+# along with this file, see <http://www.gnu.org/licenses/>.
+
+
+def pstrip(s, prefix):
+    """Prefix strip
+
+    >>> pstrip('foo_oof', 'foo_')
+    'oof'
+    >>> pstrip('baroof', 'foo_')
+    'baroof'
+    """
+    i = len(prefix)
+    if s[:i] == prefix:
+        return s[i:]
+    return s
+
+def sstrip(s, suffix):
+    """Suffix strip
+
+    >>> sstrip('foo.oof', '.oof')
+    'foo'
+    >>> sstrip('baroof', '.oof')
+    'baroof'
+    """
+    i = - len(suffix)
+    if s[i:] == suffix:
+        return s[:i]
+    return s
+
+
+def unwrap(s, prefix, suffix):
+    """
+    >>> unwrap('FOO bar BAZ', 'FOO ', ' BAZ')
+    'bar'
+    >>> unwrap('x-baroof.z', 'x-', '.y')
+    'x-baroof.z'
+    """
+    i = len(prefix)
+    j = len(suffix)
+    if s[:i] == prefix and s[-j:] == suffix:
+        return s[i:-j]
+    return s
