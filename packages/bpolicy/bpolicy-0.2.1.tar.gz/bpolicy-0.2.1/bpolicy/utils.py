8cf9ffb98d9c4cda00181fe952e763ed5c6a7f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package bpolicy/0.2.1/bpolicy-0.2.1.tar.gz

---
+++ packages/bpolicy/bpolicy-0.2.1.tar.gz/bpolicy-0.2.1/bpolicy/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from .error import PolicyError
+
+
+def chained_policy(service, factories, store, prefix=""):
+    policy = None
+    for factory in reversed(factories):
+        policy = factory.instance(service, store, policy, prefix)
+    return policy
+
+
+def silent_check(policy, identity):
+    success = True
+    try:
+        policy.check(identity)
+    except PolicyError:
+        success = False
+    return success
