62dccd7d344b95dd67d38525d3771e14b5d3206d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package printk-timestamp-converter/2.2.1/printk-timestamp-converter-2.2.1.tar.gz

---
+++ packages/printk-timestamp-converter/printk-timestamp-converter-2.2.1.tar.gz/printk-timestamp-converter-2.2.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+if __name__ == '__main__':
+
+    try:
+        with open('README.rst', 'rt') as f:
+            long_description = f.read()
+    except:
+        long_descrption = ''
+
+    setup(name='printk-timestamp-converter',
+            version='2.2.1',
+            description='Utility for printing dmesg/kmsg/printk timestamps, taking into account drift.',       
+            long_description=long_description,
+            author='Tim Savannah',
+            author_email='kata198@gmail.com',
+            maintainer='Tim Savannah',
+            maintainer_email='kata198@gmail.com',
+            url='https://github.com/kata198/printk-timestamp-converter',
+            license='LGPLv2',
+            packages=['printk_timestamp_converter'],
+            keywords=['printk', 'timestamp', 'date', 'convert', 'drift', 'converter', 'dmesg', 'kmsg'],
+            scripts=['dmesg_format_dates', 'printk_time_convert', 'printk_mark_drift'],
+            classifiers=['Development Status :: 5 - Production/Stable',
+                'Operating System :: POSIX :: Linux',
+                'Topic :: System :: Operating System Kernels :: Linux',
+            ]
+    )
