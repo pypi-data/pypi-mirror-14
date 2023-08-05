5ebdd7db9e5be588d92ddf47ba1419a3d0c33455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package django-related-select/0.8/django-related-select-0.8.tar.gz

---
+++ packages/django-related-select/django-related-select-0.8.tar.gz/django-related-select-0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='django-related-select',
+    packages=['related_select'],
+    version='0.8',
+    description='Class-based View and django form field for related select boxes',
+    author='Zach McCormick',
+    author_email='zach.mccormick@smilecareclub.com',
+    url='https://github.com/CamelotVG/django-related-select',
+    download_url='https://github.com/CamelotVG/django-related-select/tarball/0.8',
+    keywords=['django', 'related', 'dependent', 'forms'],
+    classifiers=[],
+    install_requires=[
+        'django'
+    ],
+    test_suite='tests',
+    package_data={'': ['**/*.js']}
+)
