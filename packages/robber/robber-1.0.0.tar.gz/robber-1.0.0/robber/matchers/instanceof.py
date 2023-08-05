d2e6854732a3737d6b269eac9ebdba705a0e3c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package robber/1.0.0/robber-1.0.0.tar.gz

---
+++ packages/robber/robber-1.0.0.tar.gz/robber-1.0.0/robber/matchers/instanceof.py
@@ -0,0 +1,16 @@
+from robber import expect
+from robber.matchers.base import Base
+
+
+class Instanceof(Base):
+    """
+    expect(obj).to.be.an.instanceof(Klass)
+    """
+
+    def matches(self):
+        return isinstance(self.actual, self.expected)
+
+    def failure_message(self):
+        return 'Expected "%s" to be an instance of "%s"' % (self.actual, self.expected)
+
+expect.register('instanceof', Instanceof)
---
+++ packages/robber/robber-1.0.0.tar.gz/robber-1.0.0/robber/matchers/regexp.py
@@ -0,0 +1,27 @@
+import re
+from robber import expect
+from robber.matchers.base import Base
+
+
+class Match(Base):
+    """
+    expect('foo').to.match(r'foo')
+    """
+    def matches(self):
+        return bool(re.match(self.expected, self.actual))
+
+    def failure_message(self):
+        return 'Expected "%s" to match "%s"' % (self.actual, self.expected)
+
+class NotMatch(Base):
+    """
+    expect('bar').to.not_match(r'foo')
+    """
+    def matches(self):
+        return not re.match(self.expected, self.actual)
+
+    def failure_message(self):
+        return 'Expected "%s" to not match "%s"' % (self.actual, self.expected)
+
+expect.register('match', Match)
+expect.register('not_match', NotMatch)
