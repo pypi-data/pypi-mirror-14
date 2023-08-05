cc20e290f24788e722962c9afb4e0fa0fc2cb428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:23 +0000

    Add package predata-api-client/0.9.0/predata-api-client-0.9.0.tar.gz

---
+++ packages/predata-api-client/predata-api-client-0.9.0.tar.gz/predata-api-client-0.9.0/predata/config/constants.py
@@ -0,0 +1,4 @@
+PRODUCTION_HOSTNAME = "https://app.predata.com"
+DEVELOPMENT_HOSTNAME = "https://dev.predata.com"
+API_BASE_VERSION_1 = "/api/v1/"
+MAX_NUMBER_OF_RETRIES = 3
---
+++ packages/predata-api-client/predata-api-client-0.9.0.tar.gz/predata-api-client-0.9.0/predata/config/exceptions.py
@@ -0,0 +1,2 @@
+class PredataPythonClientExeception(Exception):
+    pass
