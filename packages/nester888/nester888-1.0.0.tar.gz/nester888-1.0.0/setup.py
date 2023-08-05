676ccefa2faa38921245af71463a16836388035b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:28 +0000

    Add package nester888/1.0.0/nester888-1.0.0.tar.gz

---
+++ packages/nester888/nester888-1.0.0.tar.gz/nester888-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'nester888',
+		version			= '1.0.0',
+		py_modules		= ['nester888'],
+		author			= '888kenwu',
+		author_email	= '888kenwu@gmail.com',
+		url				= '',
+		description		= 'A simple printer of nested lists',
+     )
