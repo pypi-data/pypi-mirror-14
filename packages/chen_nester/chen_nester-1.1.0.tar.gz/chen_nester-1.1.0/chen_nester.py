3ff4eecd61bb65a3e3d6be5045122f5f547b69d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:37 +0000

    Add package chen_nester/1.1.0/chen_nester-1.1.0.tar.gz

---
+++ packages/chen_nester/chen_nester-1.1.0.tar.gz/chen_nester-1.1.0/chen_nester.py
@@ -0,0 +1,13 @@
+"""This is the "nester.py" module and it provides one function called print_lol()
+   which prints lists that may or may not include nested lists ."""
+def print_lol(the_list):
+    """This function takes one positional argument called "the list", which
+       is any Python list."""
+  for each_item in (the_list, level):
+    if isinstance(each_item, list):
+      print_lol(each_item, level+1)
+    
+    else:
+      for tab_stop in range(level):
+        print("\t", end='')
+      print(each_item)
