7127b4071f697914aea9ac5c77e5dd797a0075b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:41 +0000

    Add package jsonpyes/1.2.7/jsonpyes-1.2.7.tar.gz

---
+++ packages/jsonpyes/jsonpyes-1.2.7.tar.gz/jsonpyes-1.2.7/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup 
+from setuptools import find_packages
+from jsonpyes import version
+
+from os import path
+
+# Try to build doc, converting from language markdown to rst using pandoc if it is installed
+import subprocess
+convert_the_doc_command = "pandoc --from=markdown --to=rst --output=README.rst README.md"
+
+result = subprocess.check_output(convert_the_doc_command , shell=True)
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+
+setup(name='jsonpyes',
+        version=version,
+        author="Alexander Liu",
+        license="GPL V3",
+        description="Import JSON raw data to ElasticSearch using Python in one line of commands",
+        long_description=long_description,
+        platforms=["Unix","Linux","OSX","Android","Windows"],
+        url="https://github.com/xros/jsonpyes",
+        download_url="https://github.com/xros/jsonpyes/tarball/" + version,
+        #py_modules=['elasticsearch','jsonpyes','simplejson'],
+        install_requires=[
+            'elasticsearch',
+            'simplejson',
+        ],
+        keywords=["elasticsearch","json","json2es","jsonpyes"],
+        classifiers=[],
+        # Make this script executable in command line
+        #scripts=['jsonpyes.py'],
+        scripts=['jsonpyes'],
+        #packages=['contrib', ],
+        packages=find_packages(),
+        include_package_data=True,
+)
