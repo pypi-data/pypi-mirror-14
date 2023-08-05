f9b0f3e24f88b75d49033795255c838191af4bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:36 +0000

    Add package pylogic/0.1.4/pylogic-0.1.4.zip

---
+++ packages/pylogic/pylogic-0.1.4.zip/pylogic-0.1.4/pylogic/case.py
@@ -0,0 +1,49 @@
+from pylogic.functions import compare_cases
+
+# Clase Hecho/Caso #
+
+
+class Case:
+    """
+    Basic type of object for the logical programming, consists of:
+    - A selector, used to identify the family of cases
+    - A tuple, containing the values associated to the case
+    """
+
+# Magic Methods #
+
+    def __init__(self, selector, *args):
+        self.selector = str(selector)
+        self.tupla = tuple(args)
+
+    def __str__(self):
+        return self.selector + str(self.tupla)
+
+    def __add__(self, base):
+        """ Implementation of + operand for the sake of conmutativity
+        :param base:
+        """
+
+        base.add_case(self)
+
+    def __eq__(self, case):
+        """ Implementation of equality between cases
+        :param case:
+        """
+
+        return self.selector == case.selector and self.tupla == case.tupla
+
+# Public Interface #
+
+    def tally(self, value):
+        """
+        Public interface of the class that allows the tally behaviour.
+        Validates if either the value is in the Case Tuple or, if value is a tuple,
+        tallies it to the Case Tuple.
+        :param value: object, tuple
+        """
+
+        if value in self.tupla or compare_cases(value, self.tupla):
+            return self.tupla
+        else:
+            return False
---
+++ packages/pylogic/pylogic-0.1.4.zip/pylogic-0.1.4/pylogic/predicate.py
@@ -0,0 +1,51 @@
+from pylogic.functions import format_rule, flat, eval_elems, _
+
+
+class Predicate:
+    """Advanced logical object that establishes rules associated to cases and other kind of rules, contains:
+    - A list of rules that are applied to the queries associated to it. One of them is a KnowledgeBase query
+    - A list of knowledgeBases, that are used in the queries
+    - A list of results, that is used for handling the responses
+    """
+
+    def __init__(self, selector="default"):
+        self.rules = []
+        self.base_rule = None
+        self.bases = []
+        self.main_rule(selector)
+        self._results = []
+
+    def __str__(self):
+        bases = []
+        for b in self.bases:
+            bases.append(str(b))
+        return "Rules: " + str(self.rules) + " Bases: " + str(bases) + " Results: " + str(self._results)
+
+    def add_base(self, base):
+        self.bases.append(base)
+
+    def add_rule(self, rule):
+        self.rules.append(format_rule(rule))
+
+    def main_rule(self, selector):
+        self.base_rule = lambda pred, tup: flat([base.tally(selector, *tup) for base in pred.bases])
+
+    def tally_main(self, params):
+        self._results = self.base_rule(self, params)
+
+    def tally_rules(self, params):
+        aux = []
+        for result in self._results:
+            if self.rules != []:
+                if True in [rule(self, result) for rule in self.rules]:
+                    aux.append(result)
+            else:
+                aux = self._results
+        self._results = aux
+
+    def tally(self, *params):
+        pars = tuple(params)
+        self.tally_main(pars)
+        self.tally_rules(pars)
+
+        return eval_elems(self._results)
