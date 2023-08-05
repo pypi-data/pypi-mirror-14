ab47e718ac902680c8b515ada6ecd02803b3adc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package django-email-from-template/2.1.1/django-email-from-template-2.1.1.tar.gz

---
+++ packages/django-email-from-template/django-email-from-template-2.1.1.tar.gz/django-email-from-template-2.1.1/email_from_template/utils.py
@@ -0,0 +1,26 @@
+from django.utils import lru_cache
+
+from . import app_settings
+
+@lru_cache.lru_cache()
+def get_render_method():
+    return from_dotted_path(app_settings.EMAIL_RENDER_METHOD)
+
+@lru_cache.lru_cache()
+def get_context_processors():
+    return [from_dotted_path(x) for x in app_settings.EMAIL_CONTEXT_PROCESSORS]
+
+def from_dotted_path(fullpath):
+    """
+    Returns the specified attribute of a module, specified by a string.
+
+    ``from_dotted_path('a.b.c.d')`` is roughly equivalent to::
+
+        from a.b.c import d
+
+    except that ``d`` is returned and not entered into the current namespace.
+    """
+
+    module, attr = fullpath.rsplit('.', 1)
+
+    return getattr(__import__(module, {}, {}, (attr,)), attr)
