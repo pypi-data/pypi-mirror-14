25d2964f992c711f457fd1bd342e45f012a9aed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package auxlib/0.0.32/auxlib-0.0.32.tar.gz

---
+++ packages/auxlib/auxlib-0.0.32.tar.gz/auxlib-0.0.32/auxlib/collection.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Common collection classes."""
+from __future__ import print_function, division, absolute_import
+
+
+# http://stackoverflow.com/a/14620633/2127762
+class AttrDict(dict):
+    """Sub-classes dict, and further allows attribute-like access to dictionary items.
+
+    Examples:
+        >>> d = AttrDict({'a': 1})
+        >>> d.a, d['a'], d.get('a')
+        (1, 1, 1)
+        >>> d.b = 2
+        >>> d.b, d['b']
+        (2, 2)
+    """
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
