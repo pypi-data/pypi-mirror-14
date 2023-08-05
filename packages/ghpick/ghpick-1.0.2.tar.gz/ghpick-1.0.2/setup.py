a04598661c047d66fb5a3ba56ed307a94343537a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package ghpick/1.0.2/ghpick-1.0.2.tar.gz

---
+++ packages/ghpick/ghpick-1.0.2.tar.gz/ghpick-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name = "ghpick",
+    version = "1.0.2",
+    packages = ["ghpick"],
+    install_requires = ['requests>=2.7.0', 'sh>=1.11'],
+    tests_require = ['vcrpy>=1.6.0','mock>=1.0.1','contextlib2>=0.4.0'],
+
+    # metadata for upload to PyPI
+    author = "Ryan Parr",
+    author_email = "whiskeyriver",
+    description = "Perform cherry-pick patching with the Github API",
+    license = "MIT",
+    keywords = "github githubapi cherry pick cherry-pick cherrypick merge",
+    url = "http://github.com/whiskeyriver/ghpick"
+)
