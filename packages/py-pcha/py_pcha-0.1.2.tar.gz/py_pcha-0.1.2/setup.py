c841f2fdcea13afcb7191d85f9741ecfd64ed493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:35 +0000

    Add package py-pcha/0.1.2/py_pcha-0.1.2.tar.gz

---
+++ packages/py-pcha/py_pcha-0.1.2.tar.gz/py_pcha-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='py_pcha',
+    version='0.1.2',
+    description='Python implemenation of PCHA algorithm',
+    url='https://github.com/ulfaslak/py_pcha',
+    download_url = 'https://github.com/ulfaslak/py_pcha/tarball/0.1',
+    author='Ulf Aslak',
+    author_email='ulfjensen@gmail.com',
+    license='MIT',
+    packages=['py_pcha'],
+    zip_safe=False,
+    keywords = ['Archetypal Analysis', 'AA', 'PCHA', 'Clustering', 'Machine Learning', 'Signal Processing'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ]
+)
