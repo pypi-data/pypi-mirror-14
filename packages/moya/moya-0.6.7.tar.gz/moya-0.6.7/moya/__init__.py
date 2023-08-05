7e55c14c639707b7644cb34dfc2b95c2e7d20411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package moya/0.6.7/moya-0.6.7.tar.gz

---
+++ packages/moya/moya-0.6.7.tar.gz/moya-0.6.7/moya/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import unicode_literals
+
+__author__ = "Will McGugan <admin@moyaproject.com>"
+
+__version__ = "0.6.7"
+
+# *** Don't forget to update version in setup.py ***
+
+__all__ = ['pilot', 'expose']
+
+
+from . import expose
+from .moyapilot import Pilot
+from .compat import text_type, PY2
+
+
+pilot = Pilot()
+
+import decimal
+
+if PY2:
+
+    decimal.Decimal.__moyarepr__ = lambda self, context: "d:'{}'".format(text_type(self))
+    _decimal_str = decimal.Decimal.__str__
+    decimal.Decimal.__str__ = lambda self: "{:f}".format(self.normalize())
+    decimal.Decimal.__unicode__ = decimal.Decimal.__str__
+
+
+else:
+    _decimal_normalize = decimal.Decimal.normalize
+
+    class MoyaDecimal(decimal.Decimal):
+
+        def __moyarepr__(self, context):
+            return "d:'{}'".format(_decimal_normalize(self))
+
+        def __str__(self):
+            return "{:f}".format(_decimal_normalize(self))
+
+        def __unicode__(self):
+            return "{:f}".format(_decimal_normalize(self))
+
+    decimal.Decimal = MoyaDecimal
---
+++ packages/moya/moya-0.6.7.tar.gz/moya-0.6.7/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = "0.6.7"
+
+# Don't forget to update version in moya/__init__.py
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.4"
+]
+
+long_desc = """Web development framework"""
+
+setup(
+    name='moya',
+    version=VERSION,
+    description="web development platform",
+    long_description=long_desc,
+    zip_safe=False,
+    license="MIT",
+    author="Will McGugan",
+    author_email="will@willmcgugan.com",
+    url="http://www.moyaproject.com",
+
+    entry_points={
+        "console_scripts": [
+            'moya = moya.command.app:main',
+            'moya-pm = moya.command.moyapi:main',
+            'moya-srv = moya.command.moyasrv:main',
+            'moya-doc = moya.command.moyadoc:main'
+        ]
+    },
+    scripts=[
+        'scripts/moya-workon',
+    ],
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={'': ['_*', 'docs/*']},
+
+    classifiers=classifiers,
+    install_requires=[
+        'pyparsing >= 2.0.7',
+        'webob',
+        'sqlalchemy',
+        'pytz',
+        'pygments',
+        'fs >= 0.5.4',
+        'iso8601',
+        'babel==1.3',
+        'postmarkup',
+        'polib',
+        'pillow == 2.9.0',
+        'pymysql',
+        'passlib',
+        'commonmark >= 0.6.3',
+        'requests',
+        'lxml',
+        'colorama',
+        'premailer',
+        'watchdog',
+        'bleach',
+        'beautifulsoup4'
+    ],
+    extras_require={
+        ':sys_platform=="linux2" or sys_platform=="linux3"': ["pyinotify"],
+        'dev': ['notify2']
+    },
+    setup_requires=["setuptools_git >= 0.3"]
+)
