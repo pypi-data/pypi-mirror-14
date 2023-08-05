a5546121dbf650cd1680cd5c2e7d2df4295a2cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:54 +0000

    Add package lyl_nost/1.0.0/lyl_nost-1.0.0.zip

---
+++ packages/lyl_nost/lyl_nost-1.0.0.zip/lyl_nost-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name            ='lyl_nost',
+    version         ='1.0.0',
+    py_modules      =['nost'],
+    author          ='LYL',
+    author_email    ='secret',
+    url             ='null',
+    description     ='first module'
+
+    )
