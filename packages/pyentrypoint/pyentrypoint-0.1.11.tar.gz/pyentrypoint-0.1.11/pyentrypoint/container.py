15eb3e1019bfcc207ec52ee85094027d5d8b483c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package pyentrypoint/0.1.11/pyentrypoint-0.1.11.tar.gz

---
+++ packages/pyentrypoint/pyentrypoint-0.1.11.tar.gz/pyentrypoint-0.1.11/pyentrypoint/container.py
@@ -0,0 +1,28 @@
+"""
+    Container object handle a single container link
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+class Container(object):
+
+    """Container handles a single container link"""
+
+    ip = None
+    environ = None
+    names = None
+    links = None
+
+    def __init__(self, ip, env, names, links=None):
+        self.ip = ip
+        self.environ = env
+        self.names = names
+        self.links = self._set_links(links)
+
+    def _set_links(self, links):
+        lst = []
+        for link in links:
+            if link.ip == self.ip:
+                lst.append(link)
+        self.links = tuple(lst)
