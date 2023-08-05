9b004f818e24c515fd042dafc6b65fbac67c3c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package simuvex/4.6.3.28/simuvex-4.6.3.28.tar.gz

---
+++ packages/simuvex/simuvex-4.6.3.28.tar.gz/simuvex-4.6.3.28/simuvex/procedures/libc___so___6/pthread_cond_signal.py
@@ -0,0 +1,10 @@
+import simuvex
+
+######################################
+# Doing nothing
+######################################
+
+class pthread_cond_signal(simuvex.SimProcedure):
+    def run(self):
+        _ = self.arg(0)
+        self.ret()
