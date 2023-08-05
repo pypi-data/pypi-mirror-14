e77a1383591ba073e120a85e027238942f3d1c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:05 +0000

    Add package tox-setuptools/1.0/tox-setuptools-1.0.tar.gz

---
+++ packages/tox-setuptools/tox-setuptools-1.0.tar.gz/tox-setuptools-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tox-setuptools',
+    version='1.0',
+    packages=find_packages(),
+
+    license='Apache 2',
+    description='Tox integration with setuptools',
+    url='https://github.com/2m/tox-setuptools',
+)
