2d4cc0d6b201bca9a7dd5472a1faa9304507daa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package commonfate/0.1.2/commonfate-0.1.2.tar.gz

---
+++ packages/commonfate/commonfate-0.1.2.tar.gz/commonfate-0.1.2/setup.py
@@ -0,0 +1,65 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        # Name of the project
+        name='commonfate',
+
+        # Version
+        version="0.1.2",
+
+        # Description
+        description='Common Fate Transform and Model',
+
+        url='https://github.com/aliutkus/commonfate',
+
+        # Your contact information
+        author='Antoine Liutkus',
+        author_email='antoine.liutkus@inria.fr',
+
+        # License
+        license='BSD',
+
+        # Packages in this project
+        # find_packages() finds all these automatically for you
+        packages=setuptools.find_packages(exclude=['tests']),
+
+        # Dependencies, this installs the entire Python scientific
+        # computations stack
+        install_requires=[
+            'numpy>=1.7',
+            'scipy>=0.9',
+            'tqdm',
+        ],
+
+        extras_require={
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+            ],
+            'docs': [
+                'sphinx',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ]
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Telecommunications Industry',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Multimedia :: Sound/Audio :: Analysis',
+            'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
+        ],
+
+        zip_safe=False,
+    )
