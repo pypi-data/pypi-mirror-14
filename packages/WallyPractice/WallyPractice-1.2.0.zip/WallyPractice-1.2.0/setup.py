16b87c1ba545cb6673ac6dd3926d299731cb1f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:49 +0000

    Add package WallyPractice/1.2.0/WallyPractice-1.2.0.zip

---
+++ packages/WallyPractice/WallyPractice-1.2.0.zip/WallyPractice-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+       name           = 'WallyPractice',
+       version        = '1.2.0',
+       py_modules     = ['nester'],
+       author         = 'hfpython',
+       author_email   = 'hfpython@headfirstlabs.com',
+       url            = 'http://www.headfirstlabs.com',
+       description    = 'A simle printer of nested lists',
+     )
