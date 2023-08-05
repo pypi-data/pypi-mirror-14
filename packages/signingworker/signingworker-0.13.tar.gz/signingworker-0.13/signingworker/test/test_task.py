37171e6c434519ebde7106fb8e606c4d26b6a05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:46 +0000

    Add package signingworker/0.13/signingworker-0.13.tar.gz

---
+++ packages/signingworker/signingworker-0.13.tar.gz/signingworker-0.13/signingworker/test/test_task.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from signingworker.task import task_signing_formats, task_cert_type
+from signingworker.exceptions import TaskVerificationError
+
+
+class TestTaskSigningFormats(TestCase):
+    def test_task_signing_formats(self):
+        task = {"scopes": ["project:releng:signing:cert:dep",
+                           "project:releng:signing:format:mar",
+                           "project:releng:signing:format:gpg"]}
+        self.assertEqual(["mar", "gpg"], task_signing_formats(task))
+
+
+class TestTaskCertType(TestCase):
+    def test_task_cert_type(self):
+        task = {"scopes": ["project:releng:signing:cert:dep",
+                           "project:releng:signing:type:mar",
+                           "project:releng:signing:type:gpg"]}
+        self.assertEqual("project:releng:signing:cert:dep", task_cert_type(task))
+
+    def test_task_cert_type_error(self):
+        task = {"scopes": ["project:releng:signing:cert:dep",
+                           "project:releng:signing:cert:notdep",
+                           "project:releng:signing:type:gpg"]}
+        self.assertRaises(TaskVerificationError, task_cert_type, task)
