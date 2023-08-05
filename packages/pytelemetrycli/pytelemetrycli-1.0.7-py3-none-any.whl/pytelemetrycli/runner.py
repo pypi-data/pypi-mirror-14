df89ed05cd5a560f126ef6ae0326fb40723ab3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:15 +0000

    Add package pytelemetrycli/1.0.7/pytelemetrycli-1.0.7-py3-none-any.whl

---
+++ packages/pytelemetrycli/pytelemetrycli-1.0.7-py3-none-any.whl/pytelemetrycli/runner.py
@@ -0,0 +1,111 @@
+import threading
+import time
+
+# Main class
+class Runner:
+    def __init__(self, transport, telemetry, plots, plotsLock, topics):
+
+        self.transport = transport
+        self.telemetryWrapper = telemetry
+        self.plots = plots
+        self.plotsLock = plotsLock
+        self.topics = topics
+
+        self.thread = None
+        self.running = threading.Event()
+        self.running.set()
+
+        self.connected = threading.Event()
+        self.connected.clear()
+
+        self.resetStats()
+
+    def connect(self,port,bauds):
+        options = dict()
+        options['port'] = port
+        options['baudrate'] = bauds
+        self.baudrate = bauds
+        self.transport.connect(options)
+        self.connected.set()
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+    def disconnect(self):
+        self.connected.clear()
+        self.transport.disconnect()
+
+    def terminate(self):
+        self.running.clear()
+        if self.thread:
+            self.thread.join()
+        try:
+            self.transport.disconnect()
+        except:
+            pass # Already disconnected
+
+    def stats(self):
+        return {
+            "baudspeed" : self.baudspeed,
+            "baudspeed_avg" : self.baudspeed_avg,
+            "baudratio" : self.baudspeed / self.baudrate,
+            "baudratio_avg" : self.baudspeed_avg / self.baudrate
+        }
+
+    def resetStats(self):
+        self.baudrate = 1.0
+        self.baudspeed = 0.0
+        self.lasttime = time.time()
+        self.lastamount = 0.0
+        self.baudspeed_avg = 0.0
+
+    def update(self):
+        # Update protocol decoding
+        self.telemetryWrapper.update()
+
+        # Protect the self.plots data structure from
+        # being modified from the main thread
+        self.plotsLock.acquire()
+
+        # Update baudspeed value
+        current = time.time()
+        difft = current - self.lasttime
+
+        if difft > 0.05 :
+            self.lasttime = current
+
+            current = self.transport.stats()['rx_bytes']
+            diff = current - self.lastamount
+            self.lastamount = current
+
+            self.baudspeed = diff / difft
+
+            # Compute rolling average baud speed on about 1 second window
+            n = 20
+            self.baudspeed_avg = (self.baudspeed + n * self.baudspeed_avg) / (n + 1)
+            # Need a dedicated flag with ls to display program parameters
+            #self.topics.process("baudspeed",self.baudspeed)
+            #self.topics.process("baudspeed_avg",self.baudspeed_avg)
+
+        # Poll each poll pipe to see if user closed them
+        plotToDelete = None
+        for p, i in zip(self.plots,range(len(self.plots))):
+            if p['ctrl'].poll():
+                if p['ctrl'].recv() == "closing":
+                    plotToDelete = i
+                    break
+
+        # Delete a plot if needed
+        if plotToDelete is not None:
+            self.plots[plotToDelete]['ctrl'].close()
+            topic = self.plots[plotToDelete]['topic']
+            self.topics.untransfer(topic)
+            self.plots.pop(plotToDelete)
+
+        self.plotsLock.release()
+
+    def run(self):
+        while self.running.is_set():
+            if self.connected.is_set():
+                self.update()
+            else:
+                time.sleep(0.5)
---
+++ packages/pytelemetrycli/pytelemetrycli-1.0.7-py3-none-any.whl/pytelemetrycli/ui/superplot.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import pyqtgraph as pg
+from multiprocessing import Process, Queue, Pipe
+import time, threading
+from sortedcontainers import SortedDict
+from enum import Enum
+
+class PlotType(Enum):
+    linear = 0,
+    indexed = 1
+
+class Superplot():
+    """
+Self-contained plotting class that runs in its own process.
+Plotting functionality (reset the graph, .. ?) can be controlled
+by issuing message-based commands using a multiprocessing Pipe
+
+    """
+    def __init__(self,name,plottype=PlotType.indexed):
+        self.name = name
+        self.plottype = plottype
+        self._clear()
+
+    def _clear(self):
+        # Process-local buffers used to host the displayed data
+        if self.plottype == PlotType.linear:
+            self.set = True
+            self.x = []
+            self.y = []
+        else:
+            self.xy = SortedDict()
+            # TODO : use this optimization, but for now raises issue
+            # Can't pickle dict_key views ??
+            #self.x = self.xy.keys()
+            #self.y = self.xy.values()
+            self.set = False
+
+    def start(self):
+        # The queue that will be used to transfer data from the main process
+        # to the plot
+        self.q = Queue()
+        main_pipe, self.in_process_pipe = Pipe()
+        self.p = Process(target=self.run)
+        self.p.start()
+        # Return a handle to the data queue and the control pipe
+        return self.q, main_pipe
+
+    def join(self):
+        self.p.join()
+
+    def _update(self):
+        # Empty data queue and process received data
+        while not self.q.empty():
+            item = self.q.get()
+            if self.plottype == PlotType.linear:
+                self.x.append(item[0])
+                self.y.append(item[1])
+            else:
+                # Seems pretty slow,
+                # TODO : Profile
+                # TODO : Eventually, need to find high performance alternative. Maybe numpy based
+                self.xy[item[0]] = item[1]
+
+        # Initialize view on data dictionnary only once for increased performance
+        if not self.set:
+            self.set = True
+            self.x = self.xy.keys()
+            self.y = self.xy.values()
+
+        # Refresh plot data
+        self.curve.setData(self.x,self.y)
+
+        try:
+            if self.in_process_pipe.poll():
+                msg = self.in_process_pipe.recv()
+                self._process_msg(msg)
+        except:
+            # If the polling failed, then the application most likely shut down
+            # So close the window and terminate as well
+            self.app.quit()
+
+    def _process_msg(self, msg):
+        if msg == "exit":
+            # TODO : Remove this line ? Redundant with send after app.exec_() ?
+            self.in_process_pipe.send("closing")
+            self.app.quit()
+        elif msg == "clear":
+            self._clear()
+
+    def run(self):
+        self.app = QtGui.QApplication([])
+        win = pg.GraphicsWindow(title="Basic plotting examples")
+        win.resize(1000,600)
+        win.setWindowTitle('pyqtgraph example: Plotting')
+        plot = win.addPlot(title=self.name)
+        self.curve = plot.plot(pen='y')
+
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self._update)
+        timer.start(50)
+
+        self.app.exec_()
+        try:
+            self.in_process_pipe.send("closing")
+        except:
+            pass
+            # Process was done, no need to process exception
+
+if __name__ == '__main__':
+    # This is function is responsible for faking some data (IO, serial port, etc)
+    # and forwarding it to the display
+    # it is run in a thread
+    def io_linear(running,q):
+        t = 0
+        while running.is_set():
+            for i in range(32):
+                s = np.sin(2 * np.pi * t)
+                q.put([t,s])
+                t += 0.01
+            time.sleep(0.0001)
+        print("Done")
+
+    # This is function is responsible for faking some data (IO, serial port, etc)
+    # and forwarding it to the display
+    # it is run in a thread
+    def io_indexed(running,q):
+        t = 0
+        while running.is_set():
+            t += 0.005
+            for i in range(128):
+                s = np.sin(2 * np.pi * t + i/10)
+                q.put([i,s])
+            time.sleep(0.01)
+        print("Done")
+    #To stop IO thread
+    run = threading.Event()
+    run.set()
+
+    # create the plot
+    s = Superplot("somePlot",PlotType.linear)
+    #s = Superplot("somePlot",PlotType.indexed)
+
+    # get the queue used to exchange data
+    q, ctrlPipe = s.start()
+
+    # start IO thread
+    t = threading.Thread(target=io_linear, args=(run,q))
+    #t = threading.Thread(target=io_indexed, args=(run,q))
+    t.start()
+
+    while True:
+        action = input("Type 'q' to quit. Type 'clear' to reset the graph. Type 'exit' to close the graph but stay on main thread.")
+        if action == 'clear':
+            ctrlPipe.send('clear')
+        elif action == 'exit':
+            ctrlPipe.send('exit')
+        elif action == 'q':
+            break
+
+    run.clear()
+    print("Waiting for IO thread to join...")
+    t.join()
+    print("Waiting for graph window process to join...")
+    s.join()
+    print("Process joined successfully. C YA !")
