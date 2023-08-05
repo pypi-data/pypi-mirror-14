fa30d78f6546d407fb2a2b0fc1c5d9d7a049334c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:38 +0000

    Add package brazilnum/0.8.6/brazilnum-0.8.6.tar.gz

---
+++ packages/brazilnum/brazilnum-0.8.6.tar.gz/brazilnum-0.8.6/brazilnum/util.py
@@ -0,0 +1,23 @@
+
+import re
+
+"""
+Helper functions for validating identifiers.
+
+"""
+
+NONDIGIT = re.compile(r'[^0-9]')
+
+
+def clean_id(identifier):
+    """Remove non-numeric characters from input."""
+    if isinstance(identifier, int):
+        return str(identifier)
+    return NONDIGIT.sub('', identifier)
+
+
+def pad_id(identifier, fmt):
+    """Pad an identifier with leading zeros."""
+    if not isinstance(identifier, int):
+        identifier = int(clean_id(identifier))
+    return fmt % identifier
