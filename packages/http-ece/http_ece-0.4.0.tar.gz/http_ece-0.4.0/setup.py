1dbd014aa984e38bf97d00458225d27e0483f2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package http-ece/0.4.0/http_ece-0.4.0.tar.gz

---
+++ packages/http-ece/http_ece-0.4.0.tar.gz/http_ece-0.4.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+from setuptools import setup
+
+setup(
+    name='http_ece',
+    version='0.4.0',
+    author='Martin Thomson',
+    author_email='martin.thomson@gmail.com',
+    scripts=[],
+    packages=['http_ece'],
+    description='Encrypted Content Encoding for HTTP',
+    long_description='Encipher HTTP Messages',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='crypto http',
+    install_requires=[
+        'pyelliptic', 'cryptography'
+    ],
+    url='https://github.com/martinthomson/encrypted-content-encoding',
+    license='MIT'
+)
