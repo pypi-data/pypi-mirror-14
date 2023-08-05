62e4c36b1de5b3f1f700a65b7502b87950c699bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package django-vkontakte-polls/0.7.3/django-vkontakte-polls-0.7.3.tar.gz

---
+++ packages/django-vkontakte-polls/django-vkontakte-polls-0.7.3.tar.gz/django-vkontakte-polls-0.7.3/settings_test.py
@@ -0,0 +1,9 @@
+INSTALLED_APPS = (
+    'vkontakte_api',
+    'vkontakte_places',
+    'vkontakte_users',
+    'vkontakte_groups',
+    'vkontakte_wall',
+    'm2m_history',
+)
+SOCIAL_API_TOKENS_STORAGES = []
