081650bdc6cee5d54bc85ad46f2c8858ec0125a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package sch_schachtler/1.3.0/sch_schachtler-1.3.0.zip

---
+++ packages/sch_schachtler/sch_schachtler-1.3.0.zip/sch_schachtler-1.3.0/sch_schachtler.py
@@ -0,0 +1,11 @@
+"""Dies ist das Modul 'schachtler.py'. Es stellt eine Funktion namens print_lvl(),
+die eine Liste mit beliebig vielen eingebetteten Listen ausgibt."""
+def print_lvl(liste, einzug=False, ebene=0):
+        for element in liste:
+                if isinstance(element, list):
+                        print_lvl(element, einzug, ebene+1)
+                else:
+                        if einzug:
+                                for tab_stop in range(ebene):
+                                        print("\t", end='')
+                        print(element)
---
+++ packages/sch_schachtler/sch_schachtler-1.3.0.zip/sch_schachtler-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'sch_schachtler',
+        version     = '1.3.0',
+        py_modules  = ['sch_schachtler'],
+        author      = 'hfpython',
+        author_email= 'hfpython@headfirstlabs.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'Einfache Ausgabe geschachtelter Listen',
+    )
