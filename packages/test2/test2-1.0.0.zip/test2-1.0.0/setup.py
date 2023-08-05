1d179af4356abab6fea50be97570b56e8feddbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package test2/1.0.0/test2-1.0.0.zip

---
+++ packages/test2/test2-1.0.0.zip/test2-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name  = 'test2',
+	version = '1.0.0',
+	py_modules = ['test_print_list'],
+	author = 'Alex Chen',
+	author_email = 'abc@abc.com',
+	url = 'http://abc.com',
+	description = 'An example of print list',
+)
