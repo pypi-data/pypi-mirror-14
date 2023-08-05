f0aade0a2427f45ae08cd6574968c3ba7f1297d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package mh-ui-testing/0.9.2/mh-ui-testing-0.9.2.tar.gz

---
+++ packages/mh-ui-testing/mh-ui-testing-0.9.2.tar.gz/mh-ui-testing-0.9.2/mh_cli/__init__.py
@@ -0,0 +1,23 @@
+__version__ = '0.9.2'
+
+import click
+click.disable_unicode_literals_warning = True
+
+from mh_cli.common import ClickState, config_options, pass_state
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(ClickState)
+
+@cli.command()
+@config_options
+@pass_state
+def config(state):
+    """Save typing by setting common options in a config file"""
+    state.save_config()
+
+from gi import gi
+from inbox import inbox
+from rec import rec
+from series import series
