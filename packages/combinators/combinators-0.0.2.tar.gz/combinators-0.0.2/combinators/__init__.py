16577fec6decd96f5ccf106c99f119f5a6bc0381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:37 +0000

    Add package combinators/0.0.2/combinators-0.0.2.tar.gz

---
+++ packages/combinators/combinators-0.0.2.tar.gz/combinators-0.0.2/combinators/__init__.py
@@ -0,0 +1,11 @@
+from functools import reduce, partial
+
+
+def stateless(initial_state, actions, dispatch):
+    return reduce(lambda state, f: f(state), (dispatch[action] for action in actions), initial_state)
+
+
+def statelesser(initial_state, actions, dispatch):
+    return reduce(lambda state, f: f(state),
+                  (partial(dispatch[name], *args) for (name, args) in actions),
+                  initial_state)
---
+++ packages/combinators/combinators-0.0.2.tar.gz/combinators-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+setup(
+    name='combinators',
+
+    version='0.0.2',
+
+    description='combinators',
+
+    url='https://github.com/faineance/combinators',
+
+    author='faineance',
+    author_email='faineance@users.noreply.github.com',
+
+    license='MIT',
+    classifiers=[
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+
+
+    packages=['combinators'],
+
+
+
+
+
+
+)
