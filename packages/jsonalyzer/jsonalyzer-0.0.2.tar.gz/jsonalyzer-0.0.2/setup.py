7eb831fdc30f7dd88649fb4d5922d27b62296017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:43 +0000

    Add package jsonalyzer/0.0.2/jsonalyzer-0.0.2.tar.gz

---
+++ packages/jsonalyzer/jsonalyzer-0.0.2.tar.gz/jsonalyzer-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+  name='jsonalyzer',
+  license='MIT',
+  version='0.0.2',
+  url='https://github.com/saurabh-hirani/jsonalyzer',
+  description=('Write callbacks for anlayzing json'),
+  author='Saurabh Hirani',
+  author_email='saurabh.hirani@gmail.com',
+  packages=find_packages(),
+  install_requires=[
+    'click',
+    'importlib',
+    'simplejson'
+  ],
+  entry_points = {
+    'console_scripts': [
+      'jsonalyzer = jsonalyzer.cmdline:jsonalyzer',
+    ]
+  }
+)
