7088241880d323265047fdd8cb8c59b74caa3978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:03 +0000

    Add package autopython/0.3/autopython-0.3.tar.gz

---
+++ packages/autopython/autopython-0.3.tar.gz/autopython-0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+import autopython
+import codecs
+
+with codecs.open('README.rst', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+  name = 'autopython',
+  version = autopython.__version__,
+  license = 'GPLv3',
+
+  description = 'Autoscripting for Python 3',
+  long_description = long_description,
+
+  packages = ['autopython'],
+  scripts = ['bin/autopython'],
+  
+  install_requires = [
+    'colorama',
+    'Pygments',
+  ],
+  
+  url = 'https://github.com/gosella/autopython',
+  download_url = 'https://github.com/gosella/autopython',
+
+  author = 'Germán Osella Massa',
+  author_email = 'german.osella@nexo.unnoba.edu.ar',
+  maintainer = 'Germán Osella Massa',
+  maintainer_email = 'german.osella@nexo.unnoba.edu.ar',
+  
+  keywords = ['python3', 'presentation', 'autoscripting'],
+  classifiers = [],
+)
