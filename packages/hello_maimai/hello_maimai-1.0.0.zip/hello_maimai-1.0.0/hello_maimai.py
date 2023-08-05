1e03c02eb60912bd6616b8fcd10f358485f874fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package hello_maimai/1.0.0/hello_maimai-1.0.0.zip

---
+++ packages/hello_maimai/hello_maimai-1.0.0.zip/hello_maimai-1.0.0/hello_maimai.py
@@ -0,0 +1,3 @@
+"""这是一个测试脚本"""
+def print_hello():
+	print("hello!")
---
+++ packages/hello_maimai/hello_maimai-1.0.0.zip/hello_maimai-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		 = 'hello_maimai',
+		version		 = '1.0.0',
+		py_modules	 = ['hello_maimai'],
+		author		 = 'maimai',
+		author_email = 'hfpython@headfirstlabs.com',
+		url			 = 'http://www.headfirstlabs.com',
+		description	 = 'A simple test',
+	 )
