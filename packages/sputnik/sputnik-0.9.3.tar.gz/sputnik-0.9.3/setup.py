88746f297411c34b02f15fc00ce23d2ba71e39a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package sputnik/0.9.3/sputnik-0.9.3.tar.gz

---
+++ packages/sputnik/sputnik-0.9.3.tar.gz/sputnik-0.9.3/setup.py
@@ -0,0 +1,48 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, 'sputnik', 'about.py')) as f:
+    about = {}
+    exec(f.read(), about)
+
+with open(os.path.join(base_dir, 'README.rst')) as f:
+    readme = f.read()
+
+
+setup(
+    zip_safe=True,
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__summary__'],
+    long_description=readme,
+    url=about['__uri__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    license=about['__license__'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sputnik = sputnik.__main__:main'
+        ]
+    },
+    install_requires=['semver'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering']
+)
