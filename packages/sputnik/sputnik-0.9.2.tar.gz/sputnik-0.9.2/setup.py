6e80656ecc4b605c40032cfbeb30493f2c6c2557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package sputnik/0.9.2/sputnik-0.9.2.tar.gz

---
+++ packages/sputnik/sputnik-0.9.2.tar.gz/sputnik-0.9.2/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "sputnik", "about.py")) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__summary__'],
+    url=about['__uri__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    license=about['__license__'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sputnik = sputnik.__main__:main'
+        ]
+    },
+    install_requires=['semver'],
+)
