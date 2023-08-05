77b377821500c60eb16ee7394eb559cebce8cd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package pylibcgns/0.0.6.1/pylibcgns-0.0.6.1.tar.gz

---
+++ packages/pylibcgns/pylibcgns-0.0.6.1.tar.gz/pylibcgns-0.0.6.1/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+import platform
+
+include_dirs = [np.get_include()]
+library_dirs = []
+if platform.system() == "Windows":
+    library_dirs += [r"C:\Program Files\cgns_15\lib"]
+    include_dirs += [r"C:\Program Files\cgns_15\include"]
+
+ext_modules = [Extension("pylibcgns",
+                     ["pylibcgns.pyx"],
+                     language='c',
+                     libraries=["cgns", "hdf5"],
+                     library_dirs=library_dirs,
+                     include_dirs=include_dirs,
+                     )]
+
+setup(
+  name = 'pylibcgns',
+  cmdclass = {'build_ext': build_ext},
+  version='0.0.6.1',
+  description='CGNS C-library wrapper',
+
+  author='Aleksandrov Sergey',
+  author_email='splavgm@gmail.com',
+
+  url='https://github.com/splav/pylibcgns',
+  download_url='https://github.com/splav/pylibcgns/archive/0.0.6.tar.gz',
+
+  license='MIT',
+
+  
+
+  ext_modules = ext_modules
+)
