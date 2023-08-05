9a322871d428aba3a0390324eda881c061187650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package onegov.election-day/0.7.0/onegov.election_day-0.7.0.tar.gz

---
+++ packages/onegov.election-day/onegov.election_day-0.7.0.tar.gz/onegov.election_day-0.7.0/docs/formatbeschreibung.md
@@ -0,0 +1,85 @@
+# Wahlen & Abstimmungen Webapplikation
+
+## Format Spezifikation Abstimmungen
+
+### Einleitung
+
+Am Abstimmungssonntag werden Resultate zu einzelnen Abstimmungen laufend publiziert. Bei der neuen "Wahlen & Abstimmungen" Webapplikation geschieht dies über ein Webinterface, das CSV oder Excel Dateien mit provisorischen oder definitiven Resultaten entgegennimmt.
+
+Dieses Dokument beschreibt das Format dieser CSV/Excel Dateien.
+
+### Dateiformat
+
+Als Dateiformat werden CSV, XLS oder XLSX Dateien akzeptiert. Bei Excel Dateien wird entweder das erste Arbeitsblatt, oder sofern vorhanden das Arbeitsblatt mit dem Namen "Resultate" verwendet.
+
+Die Dateien bestehen unabhängig vom verwendeten Dateiformat aus einer Kopfzeile und einer beliebigen Anzahl von Resultatzeilen. Die Kopfzeile enthält die Namen der Spalten und ist *zwingend erforderlich*.
+
+Pro Abstimmungsvorlage besteht in der Regel eine CSV/Excel Datei. Beinhaltet die Abstimmung jedoch ein Gegenvorschlag und eine Stichfrage, dann müssen drei Dateien geliefert werden: Eine Datei mit den Resultaten der Abstimmung, eine Datei mit den Resultaten des Gegenvorschlags und eine Datei mit den Resultaten der Stichfrage.
+
+### Felder
+
+Jede Zeile enthält das Resultat einer einzelnen Gemeinde, sofern diese vollständig ausgezählt wurde.
+Folgende Felder/Spalten werden dabei in der hier aufgelisteten Reihenfolge erwartet:
+
+**Bezirk**
+
+Der Bezirk in der sich die Gemeinde befindet. Ist die Gemeinde in keinem Bezirk, darf dieses Feld leer sein.
+  
+**BFS-Nummer**
+
+Die BFS-Nummer der Gemeinde zum Zeitpunkt der Abstimmung.
+
+**Gemeinde**
+
+Der Name der Gemeinde.
+
+**Ja Stimmen**
+
+Die Anzahl Ja Stimmen zu der Abstimmung.
+Ist der Text 'unbekannt' eingetragen, wird die Zeile ignoriert (noch nicht ausgezählt).
+
+**Nein Stimmen**
+
+Die Anzahl Nein Stimmen der Abstimmung.
+Ist der Text 'unbekannt' eingetragen, wird die Zeile ignoriert (noch nicht ausgezählt).
+
+**Stimmberechtigte**
+
+Die Anzahl Stimmberechtigter.
+Ist der Text 'unbekannt' eingetragen, wird die Zeile ignoriert (noch nicht ausgezählt).
+
+**Leere Stimmzettel**
+
+Die Anzahl leer eingelegter Stimmzettel.
+Ist der Text 'unbekannt' eingetragen, wird die Zeile ignoriert (noch nicht ausgezählt).
+
+**Ungültige Stimmzettel**
+
+Die Anzahl ungültiger Stimmzettel.
+Ist der Text 'unbekannt' eingetragen, wird die Zeile ignoriert (noch nicht ausgezählt).
+
+### Vorlage
+
+* XLS Vorlage: [https://raw.githubusercontent.com/OneGov/onegov.election_day/master/docs/vorlage.xls]()
+* CSV Vorlage: [https://raw.githubusercontent.com/OneGov/onegov.election_day/master/docs/vorlage.csv]()
+* XLSX Vorlage: [https://raw.githubusercontent.com/OneGov/onegov.election_day/master/docs/vorlage.xlsx]()
+
+### Beispiel
+
+«Schluss mit den Steuerprivilegien für Millionäre (Abschaffung der Pauschalbesteuerung)»
+
+Resultate des Kanton Zug: [https://github.com/OneGov/onegov.election_day/blob/master/docs/steuerprivilegien.csv]()
+
+| Bezirk | BFS-Nummer | Gemeinde    | Ja Stimmen | Nein Stimmen | Stimmberechtigte | Leere Stimmzettel | Ungültige Stimmzettel | 
+|--------|------------|-------------|------------|--------------|------------------|-------------------|-----------------------| 
+|        | 1711       | Zug         | 3515       | 6458         | 16914            | 123               | 123                   | 
+|        | 1706       | Oberägeri   | 575        | 1422         | 3639             | 123               | 123                   | 
+|        | 1709       | Unterägeri  | 901        | 1930         | 5325             | 123               | 123                   | 
+|        | 1704       | Menzingen   | 435        | 1126         | 2960             | 123               | 123                   | 
+|        | 1701       | Baar        | 2454       | 4967         | 13982            | 123               | 123                   | 
+|        | 1702       | Cham        | 1741       | 3525         | 9768             | 123               | 123                   | 
+|        | 1703       | Hünenberg   | 1063       | 2375         | 5925             | 123               | 123                   | 
+|        | 1708       | Steinhausen | 1127       | 2178         | 5993             | 123               | 123                   | 
+|        | 1707       | Risch       | 1008       | 2151         | 6115             | 123               | 123                   | 
+|        | 1710       | Walchwil    | 369        | 985          | 2057             | 123               | 123                   | 
+|        | 1705       | Neuheim     | 193        | 529          | 1299             | 123               | 123                   | 
---
+++ packages/onegov.election-day/onegov.election_day-0.7.0.tar.gz/onegov.election_day-0.7.0/onegov/election_day/app.py
@@ -0,0 +1,214 @@
+import re
+
+from cached_property import cached_property
+from onegov.core import Framework, utils
+from onegov.core.framework import transaction_tween_factory
+from onegov.core.filestorage import FilestorageFile
+from onegov.shared import asset
+from onegov.election_day.theme import ElectionDayTheme
+from onegov.election_day.models import Principal
+from webassets import Bundle
+
+
+class ElectionDayApp(Framework):
+    """ The election day application. Include this in your onegov.yml to serve
+    it with onegov-server.
+
+    """
+
+    serve_static_files = True
+
+    @property
+    def principal(self):
+        """ Returns the principal of the election day app. See
+        :class:`onegov.election_day.models.principal.Principal`.
+
+        """
+        return self.cache.get_or_create('principal', self.load_principal)
+
+    def load_principal(self):
+        """ The principal is defined in the ``principal.yml`` file stored
+        on the applications filestorage root.
+
+        If the file does not exist, the site root does not exist and therefore
+        a 404 is returned.
+
+        The structure of the yaml file is defined in
+        class:`onegov.election_app.model.Principal`.
+
+        """
+        assert self.has_filestorage
+
+        if self.filestorage.isfile('principal.yml'):
+            return Principal.from_yaml(
+                self.filestorage.open('principal.yml', encoding='utf-8').read()
+            )
+
+    @property
+    def logo(self):
+        """ Returns the logo as
+        :class:`onegov.core.filestorage.FilestorageFile`.
+
+        """
+        return self.cache.get_or_create('logo', self.load_logo)
+
+    def load_logo(self):
+        if self.filestorage.isfile(self.principal.logo):
+            return FilestorageFile(self.principal.logo)
+
+    @property
+    def theme_options(self):
+        assert self.principal.color is not None, """ No color defined, be
+        sure to define one in your principal.yml like this:
+
+            color: '#123456'
+
+        Note how you need to add apostrophes around the definition!
+        """
+
+        return {
+            'primary-color': self.principal.color
+        }
+
+    @property
+    def pages_cache(self):
+        """ A one minute cache for pages. """
+        return self.get_cache(self.application_id + ':5m', expiration_time=300)
+
+    @cached_property
+    def webassets_path(self):
+        return utils.module_path('onegov.election_day', 'assets')
+
+    @cached_property
+    def webassets_bundles(self):
+
+        jsminifier = 'rjsmin'
+
+        common = Bundle(
+            'js/modernizr.js',
+            'js/jquery.js',
+            'js/fastclick.js',
+            'js/foundation.js',
+            'js/underscore.js',
+            'js/stackable.js',
+            'js/common.js',
+            filters=jsminifier,
+            output='bundles/common.bundle.js'
+        )
+
+        form_js = Bundle(
+            'js/jquery.datetimepicker.js',
+            'js/datetimepicker.js',
+            asset('js/form_dependencies.js'),
+            filters=jsminifier,
+            output='bundles/jquery.datetimepicker.bundle.js'
+        )
+
+        form_css = Bundle(
+            'css/jquery.datetimepicker.css',
+            filters='cssmin',
+            output='bundles/jquery.datetimepicker.bundle.css'
+        )
+
+        d3 = Bundle(
+            'js/d3.js'
+        )
+
+        ballot_map = Bundle(
+            d3,
+            'js/d3tip.js',
+            'js/topojson.js',
+            'js/ballot-map.js',
+            'js/bar-chart.js',
+            filters=jsminifier,
+            output='bundles/d3.bundle.js'
+        )
+
+        bar_chart = Bundle(
+            d3,
+            'js/bar-chart.js',
+            filters=jsminifier,
+            output='bundles/bar_chart.bundle.js'
+        )
+
+        return {
+            'common': common,
+            'ballot_map': ballot_map,
+            'bar_chart': bar_chart,
+            'form_js': form_js,
+            'form_css': form_css,
+        }
+
+
+@ElectionDayApp.template_directory()
+def get_template_directory():
+    return 'templates'
+
+
+@ElectionDayApp.setting(section='core', name='theme')
+def get_theme():
+    return ElectionDayTheme()
+
+
+@ElectionDayApp.setting(section='i18n', name='localedirs')
+def get_i18n_localedirs():
+    return [
+        utils.module_path('onegov.election_day', 'locale'),
+        utils.module_path('onegov.form', 'locale'),
+        utils.module_path('onegov.user', 'locale')
+    ]
+
+
+@ElectionDayApp.setting(section='i18n', name='default_locale')
+def get_i18n_default_locale():
+    return 'de_CH'
+
+
+@ElectionDayApp.tween_factory(over=transaction_tween_factory)
+def micro_cache_anonymous_pages_tween_factory(app, handler):
+
+    cache_paths = (
+        '/',
+        '/ballot/.*',
+        '/vote/.*',
+        '/votes/.*',
+        '/election/.*',
+        '/elections/.*',
+    )
+
+    cache_paths = re.compile(r'^({})$'.format('|'.join(cache_paths)))
+
+    def micro_cache_anonymous_pages_tween(request):
+        """ Cache all pages for 5 minutes.
+
+        Logged in users are exempt of this cache. If a user wants to manually
+        bust the cache he or she just needs to refresh the cached page using
+        Shift + F5 as an anonymous user.
+
+        That is to say, we observe the Cache-Control header.
+
+        """
+
+        # no cache if the user is logged in
+        if request.is_logged_in:
+            return handler(request)
+
+        # only cache whitelisted paths
+        if not cache_paths.match(request.path_info):
+            return handler(request)
+
+        # allow cache busting through browser shift+f5
+        if request.headers.get('cache-control') == 'no-cache':
+            return handler(request)
+
+        # each page is cached once per language (and by application id as the
+        # pages_cache is bound to it)
+        key = ':'.join((request.locale, request.path_info))
+
+        return app.pages_cache.get_or_create(
+            key,
+            creator=lambda: handler(request),
+            should_cache_fn=lambda response: response.status_code == 200
+        )
+
+    return micro_cache_anonymous_pages_tween
