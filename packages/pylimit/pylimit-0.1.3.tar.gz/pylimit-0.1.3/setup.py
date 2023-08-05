23838d3fb0ef4bd3156b9a9d4e8f908bb64937a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:04 +0000

    Add package pylimit/0.1.3/pylimit-0.1.3.tar.gz

---
+++ packages/pylimit/pylimit-0.1.3.tar.gz/pylimit-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pylimit',
+  packages = ['pylimit'],
+  version = '0.1.3',
+  description = 'A distributed rate limiting library for python using Redis',
+  author='Biplap Sarkar',
+  author_email='biplap.sarkar@gmail.com',
+  url='https://github.com/biplap-sarkar/pylimit',
+  download_url='https://github.com/biplap-sarkar/pylimit/archive/v0.1.3.tar.gz',
+  keywords=['rate limiting', 'throttle', 'redis'],
+  classifiers=[],
+)
