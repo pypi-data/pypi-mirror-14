eddb99071dc109571473c7a0876375c48cd85c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package biobambam_tool/0.14/biobambam_tool-0.14-py2.py3-none-any.whl

---
+++ packages/biobambam_tool/biobambam_tool-0.14-py2.py3-none-any.whl/biobambam_tool/main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import sys
+
+import sqlalchemy
+
+from cdis_pipe_utils import pipe_util
+
+import tools.bamfixmateinformation as bamfixmateinformation
+import tools.bamindex as bamindex
+import tools.bammarkduplicates as bammarkduplicates
+import tools.bammarkduplicates2 as bammarkduplicates2
+import tools.bammdnm as bammdnm
+import tools.bammerge as bammerge
+#import tools.bamsort as bamsort
+import tools.bamtofastq as bamtofastq
+import tools.bamvalidate as bamvalidate
+
+def main():
+    parser = argparse.ArgumentParser('biobambam docker tool')
+
+    # Logging flags.
+    parser.add_argument('-d', '--debug',
+        action = 'store_const',
+        const = logging.DEBUG,
+        dest = 'level',
+        help = 'Enable debug logging.',
+    )
+    parser.set_defaults(level = logging.INFO)
+    
+    # Required flags.
+    parser.add_argument('--tool_name',
+                        required = True,
+                        help = 'biobambam tool'
+    )
+    parser.add_argument('--uuid',
+                        required = True,
+                        help = 'uuid string',
+    )
+
+    
+    # Optional db flags
+    parser.add_argument('--db_cred_s3url',
+                        required = False
+    )
+    parser.add_argument('--s3cfg_path',
+                        required = False
+    )
+
+    
+    # Tool flags
+    parser.add_argument('--bam_path',
+                        required = False
+    )
+    parser.add_argument('--reference_fasta_path',
+                        required = False
+    )
+
+    # setup required parameters
+    args = parser.parse_args()
+    tool_name = args.tool_name
+    uuid = args.uuid
+    if args.db_cred_s3url:
+        db_cred_s3url = args.db_cred_s3url
+        s3cfg_path = args.s3cfg_path
+    else:
+        db_cred_s3url = None
+
+    logger = pipe_util.setup_logging(tool_name, args, uuid)
+
+    if db_cred_s3url is not None: #db server case
+        conn_dict = pipe_util.get_connect_dict(db_cred_s3url, s3cfg_path, logger)
+        engine = sqlalchemy.create_engine(sqlalchemy.engine.url.URL(**conn_dict))
+    else: # local sqlite case
+        sqlite_name =  tool_name + '_' + uuid +'.db'
+        engine_path = 'sqlite:///' + sqlite_name
+        engine = sqlalchemy.create_engine(engine_path, isolation_level='SERIALIZABLE')
+
+    be_lenient = True
+    
+    if tool_name == 'bamfixmateinformation':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bamfixmateinformation(uuid, bam_path, engine, logger, be_lenient)
+    elif tool_name == 'bamindex':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bamindex(uuid, bam_path, engine, logger)
+    elif tool_name == 'bammarkduplicates':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bammarkduplicates(uuid, bam_path, engine, logger)
+    elif tool_name == 'bammarkduplicates2':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bammarkduplicates2(uuid, bam_path, engine, logger)
+    elif tool_name == 'bammdnm':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        reference_fasta_path = pipe_util.get_param(args, 'reference_fasta_path')
+        bammdnm(uuid, bam_path, reference_fasta_path, engine, logger)
+    elif tool_name == 'bammerge':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bammerge(uuid, bam_path, engine, logger)
+    elif tool_name == 'bamsort':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        reference_fasta_path = pipe_util.get_param(args, 'reference_fasta_path')
+        bamsort(uuid, bam_path, reference_fasta_path, engine, logger)
+    elif tool_name == 'bamtofastq':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bamtofastq(uuid, bam_path, engine, logger)
+    elif tool_name == 'bamvalidate':
+        bam_path = pipe_util.get_param(args, 'bam_path')
+        bamvalidate(uuid, bam_path, engine, logger, be_lenient)
+    else:
+        sys.exit('No recognized tool was selected')
+        
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/biobambam_tool/biobambam_tool-0.14-py2.py3-none-any.whl/biobambam_tool/tools/bammdnm.py
@@ -0,0 +1,32 @@
+import os
+
+from cdis_pipe_utils import df_util
+from cdis_pipe_utils import pipe_util
+from cdis_pipe_utils import time_util
+
+def bammdnm(uuid, bam_path, reference_fasta_path, engine, logger):
+    step_dir = os.getcwd()
+    work_dir = os.path.join(step_dir, 'mdnm')
+    bam_name = os.path.basename(bam_path)
+    out_bam_path = os.path.join(work_dir, bam_name)
+    metrics_name = bam_name+'.metrics'
+    out_metrics_path = os.path.join(work_dir, metrics_name)
+    tempfile = os.path.join(work_dir, 'tempfile')
+    logger.info('work_dir is: %s' % work_dir)
+    if pipe_util.already_step(work_dir, 'mdnm', logger):
+        logger.info('already completed step `bammdnm` of: %s' % bam_path)
+    else:
+        logger.info('running step `bammdnm` of %s: ' % bam_path)
+        os.makedirs(work_dir, exist_ok=True)
+        cpu_count = pipe_util.get_cpu_count()
+        cmd = ['bammdnm', 'I=' + bam_path, 'O=' + out_bam_path, 'M=' + out_metrics_path, 'verbose=0', 'level=-1', 'index=1',
+               'tempfile=' + tempfile, 'markthreads=' + str(cpu_count)]
+        output = pipe_util.do_command(cmd, logger)
+        df = time_util.store_time(uuid, cmd, output, logger)
+        df['bam_path'] = bam_path
+        unique_key_dict = {'uuid': uuid, 'bam_path': bam_path}
+        table_name = 'time_mem_biobambam_bammdnm'
+        df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
+        pipe_util.create_already_step(work_dir, 'mdnm', logger)
+        logger.info('completed running step `bammdnm` of: %s' % bam_path)
+    return
