a4e13460509b29997f54f7f41f595ae4671bcacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package business-rules/1.0.1/business-rules-1.0.1.tar.gz

---
+++ packages/business-rules/business-rules-1.0.1.tar.gz/business-rules-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import setuptools
+
+from business_rules import __version__ as version
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+description = 'Python DSL for setting up business intelligence rules that can be configured without code'
+
+setuptools.setup(
+        name='business-rules',
+        version=version,
+        description='{0}\n\n{1}'.format(description, history),
+        author='Venmo',
+        author_email='open-source@venmo.com',
+        url='https://github.com/venmo/business-rules',
+        packages=['business_rules'],
+        license='MIT'
+)
