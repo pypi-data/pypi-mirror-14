9938de4a0faa79eb51c98a8cfb3b6b550e663877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package json2xml/1.1.0/json2xml-1.1.0.tar.gz

---
+++ packages/json2xml/json2xml-1.1.0.tar.gz/json2xml-1.1.0/json2xml/json2xml.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+import requests
+import simplejson
+import urllib
+import dict2xml
+from bs4 import BeautifulSoup
+
+
+class Json2xml(object):
+
+    # -------------------------------
+    ##
+    # @Synopsis  This class could read a json file
+    # from the filesystem or get a file from across
+    # the Internet, and convert that json object to
+    # xml
+    #
+    # @Param data : Data to be fed into the system.
+    #
+    # @Returns  Null
+    # ---------------------------------
+    def __init__(self, data):
+        self.data = data
+
+    # -------------------------------
+    ##
+    # @Synopsis  Read JSON from a file in
+    # the system
+    # ---------------------------------
+    @classmethod
+    def fromjsonfile(cls, filename):
+        try:
+            json_data = open(filename)
+            data = simplejson.load(json_data)
+        except IOError as e:
+            print ("I/O error({0}): {1}".format(e.errno, e.strerror))
+            data = []
+        return cls(data)
+
+    # -------------------------------
+    ##
+    # @Synopsis  Fetches the JSON
+    # data from an URL Source.
+    #
+    # ---------------------------------
+    @classmethod
+    def fromurl(cls, url):
+        data = requests.get(url).json()
+        return cls(data)
+
+    # -------------------------------
+    ##
+    # @Synopsis  This method actually
+    # converts the json data that is converted
+    # into dict into XML
+    #
+    # @Returns XML
+    # ---------------------------------
+    def json2xml(self):
+        if self.data:
+            xmldata = dict2xml.dict2xml(self.data)
+            xml = BeautifulSoup(xmldata, "html.parser")
+            return xml
---
+++ packages/json2xml/json2xml-1.1.0.tar.gz/json2xml-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.0'
+
+setup(
+    name='json2xml',
+    version=version,
+    description='To covert json data to xml data',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com:vinitkumar/json2xml.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['BeautifulSoup4==4.4.1',
+                      'dict2xml==1.3',
+                      'simplejson==3.6.5',
+                      'six==1.9.0',
+                      'requests==2.9.1',
+                      ],
+)
