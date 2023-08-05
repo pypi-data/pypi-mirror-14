eceaef55657f480ff3788ee5475a69088a54cfaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:52 +0000

    Add package Grortir/0.1.6/Grortir-0.1.6.tar.gz

---
+++ packages/Grortir/Grortir-0.1.6.tar.gz/Grortir-0.1.6/grortir/main/model/core/abstract_stage.py
@@ -0,0 +1,21 @@
+"""Module represent single stage in process."""
+
+
+class AbstractStage:
+    """Class represent single stage in process."""
+
+    def __init__(self, input_vector=()):
+        """Constructor."""
+        self.input_vector = input_vector
+        self.control_params = []
+        self.output_vector = list(self.input_vector)
+
+    @staticmethod
+    def get_cost():
+        """Return cost of stage."""
+        raise NotImplementedError
+
+    @staticmethod
+    def get_quality():
+        """Return quality of stage."""
+        raise NotImplementedError
