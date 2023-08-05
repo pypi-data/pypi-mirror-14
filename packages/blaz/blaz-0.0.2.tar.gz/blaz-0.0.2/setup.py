a0358daa276a12701ca5afbc36986b5cee908e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package blaz/0.0.2/blaz-0.0.2.tar.gz

---
+++ packages/blaz/blaz-0.0.2.tar.gz/blaz-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='blaz',
+      version='0.0.2',
+      description='Composable tasks inside docker containers',
+      author='Alberto Miorin',
+      author_email='alberto.miorin@gmail.com',
+      url='https://github.com/amiorin/blaz',
+      license='MIT',
+      py_modules=['blaz'],
+      install_requires=['ansicolors'],
+      )
