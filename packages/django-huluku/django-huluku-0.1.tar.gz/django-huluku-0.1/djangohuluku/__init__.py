7718836617f807aa101f13df8fd1c76d6fbe20fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:06 +0000

    Add package django-huluku/0.1/django-huluku-0.1.tar.gz

---
+++ packages/django-huluku/django-huluku-0.1.tar.gz/django-huluku-0.1/djangohuluku/__init__.py
@@ -0,0 +1,8 @@
+#written by Eli Agbenu
+
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+
---
+++ packages/django-huluku/django-huluku-0.1.tar.gz/django-huluku-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-huluku',
+      version='0.1',
+      description='Huluku test package',
+      url='http://github.com/lefootballroi/django-huluku',
+      author='Eli Agbenu',
+      author_email='eli.anthony.agbenu@gmail.com',
+      license='MIT',
+      packages=['djangohuluku'],
+      zip_safe=False)
