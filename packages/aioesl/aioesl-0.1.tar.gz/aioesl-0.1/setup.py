71b70a43cfbed13b4aa8a2a0bd7e1e9e9de6f1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:29 +0000

    Add package aioesl/0.1/aioesl-0.1.tar.gz

---
+++ packages/aioesl/aioesl-0.1.tar.gz/aioesl-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="aioesl",
+    version="0.1",
+    description="asyncio based protocol for the FreeSWITCH's Event Socket",
+    author="Alexey Panyovin",
+    url="https://github.com/runtel/aioesl",
+    py_modules=["aioesl"],
+)
