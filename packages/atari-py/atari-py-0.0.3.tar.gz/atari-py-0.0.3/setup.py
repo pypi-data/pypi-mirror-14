8ba7770ed4e3f4e62320fd6f637f4209fec74a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package atari-py/0.0.3/atari-py-0.0.3.tar.gz

---
+++ packages/atari-py/atari-py-0.0.3.tar.gz/atari-py-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+import subprocess
+from distutils.command.build import build as DistutilsBuild
+
+with open(os.path.join(os.path.dirname(__file__), 'atari_py/package_data.txt')) as f:
+    package_data = [line.rstrip() for line in f.readlines()]
+
+class Build(DistutilsBuild):
+    def run(self):
+        subprocess.check_call(['make', 'build', '-C', 'atari_py/ale_interface'])
+        DistutilsBuild.run(self)
+
+setup(name='atari-py',
+      version='0.0.3',
+      description='Python bindings to Atari games',
+      url='https://github.com/rl-gym/atari-py',
+      author='OpenAI',
+      author_email='info@openai.com',
+      license='',
+      packages=['atari_py'],
+      package_data={'atari_py': package_data},
+      cmdclass={'build': Build},
+      install_requires=['numpy'],
+      tests_require=['nose2']
+)
