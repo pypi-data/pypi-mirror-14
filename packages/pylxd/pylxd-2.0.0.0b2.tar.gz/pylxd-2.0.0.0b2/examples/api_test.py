7c74090a8b460288a5d7580e255bc959a5282917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:41 +0000

    Add package pylxd/2.0.0.0b2/pylxd-2.0.0.0b2.tar.gz

---
+++ packages/pylxd/pylxd-2.0.0.0b2.tar.gz/pylxd-2.0.0.0b2/examples/api_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# Copyright (c) 2015 Canonical Ltd
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import uuid
+
+from pylxd import api
+
+# Let's pick a random name, avoiding clashes
+CONTAINER_NAME = str(uuid.uuid1())
+
+lxd = api.API()
+try:
+    lxd.container_defined(CONTAINER_NAME)
+except Exception as e:
+    print("Container doesnt exist: %s" % e)
+
+config = {'name': CONTAINER_NAME,
+          'source': {'type': 'none'}}
+lxd.container_init(config)
+if lxd.container_defined(CONTAINER_NAME):
+    print("Container is running")
+else:
+    print("Whoops!")
+containers = lxd.container_list()
+for x in containers:
+    lxd.container_destroy(x)
