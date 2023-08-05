0c35f7ad5fb596343216e7e4d000268424721a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:57 +0000

    Add package nester_orilifsks/1.3.0/nester_orilifsks-1.3.0.tar.gz

---
+++ packages/nester_orilifsks/nester_orilifsks-1.3.0.tar.gz/nester_orilifsks-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_orilifsks',
+        version = '1.3.0',
+        py_module = ['nester_orilifsks'],
+        author = 'changjoon',
+        author_email = 'mchang@mikejchang.com',
+        url = 'http://www.mikejchang.com',
+        description = 'a simple printer of nested lists',
+)
