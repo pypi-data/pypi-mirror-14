bdfb08f753554fafa745baccd01a58f90b4a2dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:23 +0000

    Add package dadongjun_nester/1.3.0/dadongjun_nester-1.3.0.tar.gz

---
+++ packages/dadongjun_nester/dadongjun_nester-1.3.0.tar.gz/dadongjun_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'dadongjun_nester',
+	version		= '1.3.0',
+	py_modules	= ['dadongjun_nester'],
+	author		= 'dadongjun',
+	author_email	= '853027756@qq.com',
+	description	= 'A simple printer of nested lists',
+     )
