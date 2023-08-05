cd9bff1ccbdd58ea0ae36977053f4d2db2028772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package AYQM_201603/1.4.0/AYQM_201603-1.4.0.zip

---
+++ packages/AYQM_201603/AYQM_201603-1.4.0.zip/AYQM_201603-1.4.0/print_L.py
@@ -0,0 +1,25 @@
+def print_list_1(the_list):
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+def print_list_2(the_list,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_list(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print ("\t",end="")
+			print(each_item)
+def print_list_3(the_list,indent=False,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_list(each_item,indent,level+1)
+		else:
+                    if indent:
+			for tab_stop in range(level):
+				print ("\t",end="")
+		    print(each_item)
