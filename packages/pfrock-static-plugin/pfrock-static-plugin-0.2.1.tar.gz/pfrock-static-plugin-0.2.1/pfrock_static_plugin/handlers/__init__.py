76f1942a1ba2f71d9063ba6359a3330f5c95f45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package pfrock-static-plugin/0.2.1/pfrock-static-plugin-0.2.1.tar.gz

---
+++ packages/pfrock-static-plugin/pfrock-static-plugin-0.2.1.tar.gz/pfrock-static-plugin-0.2.1/pfrock_static_plugin/handlers/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# coding=utf8
+
+import logging
+
+ROUTER_STATIC_FILE = 'file'
+ROUTER_STATIC_DIR = 'dir'
+
+ROUTER_PATH = "path"
+ROUTER = 'routes'
+
+logger = logging.getLogger('pfrock.static')
---
+++ packages/pfrock-static-plugin/pfrock-static-plugin-0.2.1.tar.gz/pfrock-static-plugin-0.2.1/pfrock_static_plugin/index.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from pfrock_static_plugin.handlers import ROUTER_STATIC_FILE, ROUTER_STATIC_DIR, ROUTER_PATH, ROUTER
+from pfrock_static_plugin.handlers.dir import FrockStaticDirHandler
+from pfrock_static_plugin.handlers.file import FrockStaticFileHandler
+
+STATIC_HANDLER_MAP = {
+    ROUTER_STATIC_DIR: FrockStaticDirHandler.get_handler,
+    ROUTER_STATIC_FILE: FrockStaticFileHandler.get_handler,
+}
+
+
+class PfrockStaticPlugin(object):
+    def get_handler(self, options, **kwargs):
+        handler_list = []
+
+        # url path
+        url_path = kwargs.get(ROUTER_PATH)
+
+        # nesting config
+        if ROUTER in options:
+            for route in options[ROUTER]:
+                handler = self.__parser_one(url_path, route)
+                if handler:
+                    handler_list.append(handler)
+        else:
+            handler = self.__parser_one(url_path, options)
+            if handler:
+                handler_list.append(handler)
+
+        return handler_list
+
+    def __parser_one(self, url_path, options):
+
+        for handler_type in STATIC_HANDLER_MAP:
+            if handler_type in options:
+                return STATIC_HANDLER_MAP[handler_type](url_path, options)
+
+        return None
