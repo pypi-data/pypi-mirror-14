28792811411e893f1b0f1b9b927f6199c827ab02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:36 +0000

    Add package gynester/1.2.0/gynester-1.2.0.zip

---
+++ packages/gynester/gynester-1.2.0.zip/gynester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+       name         ='gynester',
+       version      ='1.2.0',
+       py_modules   =['gynester'],
+       author       ='gypython',
+       author_email ='775171983@qq.com',
+       url          ='',
+       description  ='A simple printer of nester lists',
+    )
