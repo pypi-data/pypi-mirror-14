a9145da71a7c498d69e222164fdcf55c0c88e98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package nester_lghoo/1.0.0/nester_lghoo-1.0.0.zip

---
+++ packages/nester_lghoo/nester_lghoo-1.0.0.zip/nester_lghoo-1.0.0/setup.py
@@ -0,0 +1,15 @@
+'''
+Created on 2016. 3. 3.
+
+@author: LeeGeunHoo
+'''
+from distutils.core import setup
+
+setup (
+       name = 'nester_lghoo',
+       version = '1.0.0',
+       py_modules = ['nester_lghoo'],
+       author = 'lghoo',
+       author_email = 'lghoo@naver.com',
+       description = 'A simple printer of nested lists',
+       )
