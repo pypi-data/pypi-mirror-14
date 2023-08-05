5b424d2d18eb42a7f2d22e3fb84676b0acd9e821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:54 +0000

    Add package mltools/0.0.6/mltools-0.0.6.tar.gz

---
+++ packages/mltools/mltools-0.0.6.tar.gz/mltools-0.0.6/mltools/json_tools.py
@@ -0,0 +1,177 @@
+"""
+What: Contains functions for manipulating jsons and geojsons.
+Author: Kostas Stamatiou
+Created: 02/24/2016
+Contact: kostas.stamatiou@digitalglobe.com
+"""
+
+import geojson
+
+from sklearn.metrics import confusion_matrix
+
+
+def join_two_geojsons(file_1, file_2, output_file):
+	"""Join two geojsons into one. The spatial reference system of the 
+	   output file is the same as the one of file_1.
+
+	   Args:
+	       file_1 (str): Filename 1 (ext. geojson).
+	       file_2 (str): Filename 2 (ext. geojson).
+	       output_file (str): Output filename (ext. geojson).
+	"""
+
+	# get feature collections
+	with open(file_1) as f:
+	    feat_collection_1 = geojson.load(f)
+
+	with open(file_2) as f:
+	    feat_collection_2 = geojson.load(f)
+
+	feat_final = feat_collection_1['features'] + feat_collection_2['features']  
+
+	feat_collection_1['features'] = feat_final
+
+	# write to output file
+	with open(output_file, 'w') as f:
+	    geojson.dump(feat_collection_1, f) 
+
+    
+def split_geojson(input_file, file_1, file_2, ratio):
+	"""Split a geojson in two separate files.
+	   
+	   Args:
+	       input_file (str): Input filename (ext. geojson).
+	       file_1 (str): Output filename 1 (ext. geojson).
+	       file_2 (str): Output filename 2 (ext. geojson).
+	       ratio (float): Proportion of features in input_file that goes to 
+	                      file_1. ratio is from 0 to 1.
+	       output_file (str): Output filename (ext. geojson).
+	"""
+
+	# get feature collection
+	with open(input_file) as f:
+	    feat_collection = geojson.load(f)
+
+	features = feat_collection['features']
+	no_features = len(features)
+	no_features_1 = int(round(ratio*no_features))
+	feat_collection_1 = geojson.FeatureCollection(features[0:no_features_1])
+	feat_collection_2 = geojson.FeatureCollection(features[no_features_1:])
+
+	with open(file_1, 'w') as f:
+	    geojson.dump(feat_collection_1, f) 
+
+	with open(file_2, 'w') as f:
+		geojson.dump(feat_collection_2, f) 	
+
+
+def get_classes_from_geojson(input_file):
+	"""Reads a geojson with class_name property and returns a vector
+	   with all the class names.
+
+	   Args:
+	       input_file (str): Input filename (.geojson extension).
+
+	   Returns:
+	       A list of class names (list).     
+	"""
+
+	# get feature collections
+	with open(input_file) as f:
+	    feature_collection = geojson.load(f)
+
+	features = feature_collection['features']    
+	labels = [feat['properties']['class_name'] for feat in features]
+
+	return labels    
+
+
+def confusion_matrix_two_geojsons(file_1, file_2):
+	"""
+	Compute confusion matrix to evaluate the accuracy of a classification.
+	Ordered class names are used to index the matrix.
+
+	Args:
+	    file_1 (str): Ground truth filename (.geojson extension)
+	    file_2 (str): Prediction filename (.geojson extension)
+	    
+	Returns:
+	    An integer numpy array C, where C[i,j] is the number of observations 
+	    known to be in group i but predicted to be in group j (numpy array).     
+	"""
+
+	true_classes = get_classes_from_geojson(file_1)
+	pred_classes = get_classes_from_geojson(file_2)
+
+	C = confusion_matrix(true_classes, pred_classes)
+
+	return C
+
+
+def write_labels_to_geojson(labels, polygon_file, output_file):
+    """Adds labels to polygon_file to create output_file.
+       The number of labels must be equal to the number of features in 
+       polygon_file. If some of the features in polygon_file are already
+       labeled, the labels are overwritten. 
+
+       Args:
+           labels (list): Label list. 
+           polygon_file (str): Filename. Collection of unclassified 
+                               geometries in geojson or shp format.
+           output_file (str): Output filename (extension .geojson)
+    """
+
+    # get input feature collection
+    with open(polygon_file) as f:
+        feature_collection = geojson.load(f)
+
+    features = feature_collection['features']
+    no_features = len(features)
+    
+    # enter label information
+    for i in range(0, no_features):
+        feature, label = features[i], labels[i]
+        feature['properties']['class_name'] = label
+
+    feature_collection['features'] = features    
+
+    # write to output file
+    with open(output_file, 'w') as f:
+        geojson.dump(feature_collection, f)     
+
+    print 'Done!'  
+
+
+def write_values_to_geojson(values, property_name, polygon_file, output_file):
+    """Writes values of given property to polygon_file to create output_file.
+       The number of labels must be equal to the number of features in 
+       polygon_file. If some of the features in polygon_file already have 
+       values, the values are overwritten.
+
+       Args:
+           values (list): Value list. 
+           property_name (str): Property name.
+           polygon_file (str): Filename. Collection of unclassified 
+                               geometries in geojson or shp format.
+           output_file (str): Output filename (extension .geojson)
+    """
+
+    # get input feature collection
+    with open(polygon_file) as f:
+        feature_collection = geojson.load(f)
+
+    features = feature_collection['features']
+    no_features = len(features)
+    
+    # enter label information
+    for i in range(0, no_features):
+        feature, value = features[i], values[i]
+        feature['properties'][property_name] = value
+
+    feature_collection['features'] = features    
+
+    # write to output file
+    with open(output_file, 'w') as f:
+        geojson.dump(feature_collection, f)     
+
+    print 'Done!'      
---
+++ packages/mltools/mltools-0.0.6.tar.gz/mltools-0.0.6/mltools/pool_detector/pool_detector.py
@@ -0,0 +1,155 @@
+'''
+Pool detector built after simple_classifier.
+'''
+
+import sys
+import os
+import numpy as np
+import json 
+import geojson
+
+from .. import json_tools as jt
+from .. import feature_extractors as fe
+from .. import pixel_extractors as pe
+
+from sklearn.ensemble import RandomForestClassifier     
+
+def train_model(polygon_file, raster_file, classifier):
+    """Train classifier and output classifier parameters.
+
+       Args:
+           polygon_file (str): Filename. Collection of geometries in 
+                               geojson or shp format.
+           raster_file (str): Image filename.
+           classifier (object): Instance of one of many supervised classifier
+                                classes supported by scikit-learn.
+       
+       Returns:
+           Trained classifier (object).                                             
+    """
+
+    # get water signature
+
+    # compute feature vectors for each polygon
+    features = []
+    labels = []
+    for (feat, poly, data, label) in pe.extract_data(polygon_file, raster_file):        
+        feature_vector = fe.pool_features(data, raster_file)
+        features.append(feature_vector)
+        labels.append(label)
+                        
+    # train classifier
+    X, y = np.array(features), np.array(labels)
+    classifier.fit( X, y )
+
+    print 'Done!'    
+    return classifier
+
+
+def classify(polygon_file, raster_file, classifier):
+    """Deploy classifier and output estimated labels.
+
+       Args:
+           polygon_file (str): Filename. Collection of geometries in 
+                               geojson or shp format.
+           raster_file (str): Image filename.
+           classifier (object): Instance of one of many supervised classifier
+                                classes supported by scikit-learn.
+       
+       Returns:
+           List of labels (list).                                             
+    """
+
+    # compute feature vectors for each polygon
+    labels = []
+    for (feat, poly, data, label) in pe.extract_data(polygon_file, raster_file):        
+        feature_vector = fe.pool_features(data, raster_file)
+        try:
+            label = classifier.predict(feature_vector)    
+        except ValueError:
+            label = ''                       
+        labels.append(label)
+
+    print 'Done!'    
+    return labels  
+
+
+def classify_w_scores(polygon_file, raster_file, classifier):
+    """Deploy classifier and output estimated labels with confidence scores.
+
+       Args:
+           polygon_file (str): Filename. Collection of geometries in 
+                               geojson or shp format.
+           raster_file (str): Image filename.
+           classifier (object): Instance of one of many supervised classifier
+                                classes supported by scikit-learn.
+       
+       Returns:
+           List of labels (list) and vector of scores (numpy vector).                                             
+    """
+
+    class_names = classifier.classes_
+
+    # compute feature vectors for each polygon
+    labels, scores = []
+    for (feat, poly, data, label) in pe.extract_data(polygon_file, raster_file):        
+        feature_vector = fe.pool_features(data, raster_file)
+        try:
+            probs = classifier.predict_proba(feature_vector) 
+            ind = np.argmax(probs)
+            label, score = class_names[ind], probs[ind]
+        except ValueError:
+            label, score = '', 1.0                       
+        labels.append(label)
+        scores.append(score)
+
+    print 'Done!'    
+    return labels, np.array(scores)  
+
+
+def main(job_file):
+    """Runs the simple_lulc workflow.
+
+       Args:
+           job_file (str): Job filename (.json, see README of this repo) 
+    """    
+   
+    # get job parameters
+    job = json.load(open(job_file, 'r'))
+    image_file = job["image_file"]
+    train_file = job["train_file"]
+    target_file = job["target_file"]
+    output_file = job["output_file"]
+    algo_params = job["params"]       # these are parameters pertinent to the 
+                                      # algorithm
+    
+    # Using random forest classifier
+    n_estimators = algo_params["n_estimators"]
+    oob_score = algo_params["oob_score"]
+    class_weight = algo_params["class_weight"]
+    classifier = RandomForestClassifier(n_estimators = n_estimators, 
+                                        oob_score = oob_score, 
+                                        class_weight = class_weight)
+        
+    print "Train model"
+    trained_classifier = train_model(train_file, image_file, classifier)
+    
+    print "Classify"
+    # labels = classify(target_file, image_file, trained_classifier)
+    labels, scores = classify_w_scores(target_file, image_file, 
+                                       trained_classifier)
+
+    print "Write results"    
+    jt.write_values_to_geojson(labels, 'class_name', target_file, output_file)
+    jt.write_values_to_geojson(scores, 'score', target_file, output_file)
+
+    # Compute confusion matrix; this makes sense only if the target file
+    # contains known labels
+    print "Confusion matrix"
+    C = jt.confusion_matrix_two_geojsons(target_file, output_file)
+    print C
+
+    print "Normalized confusion matrix"
+    print C.astype(float)/C.sum(1)[:, None]
+
+    print "Done!"
