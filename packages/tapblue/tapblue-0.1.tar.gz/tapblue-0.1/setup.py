c315ce78b1e50526907971fe51eb66f2b35cd9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package tapblue/0.1/tapblue-0.1.tar.gz

---
+++ packages/tapblue/tapblue-0.1.tar.gz/tapblue-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='tapblue',
+      version='0.1',
+      description='The button for hackers and techies',
+      url='https://github.com/nchafni/tapblue',
+      author='Nezare Chafni',
+      author_email='nchafni@tap.blue',
+      license='MIT',
+      packages=['tapblue'],
+      install_requires=[
+                'pubnub',
+            ],
+      zip_safe=False)
