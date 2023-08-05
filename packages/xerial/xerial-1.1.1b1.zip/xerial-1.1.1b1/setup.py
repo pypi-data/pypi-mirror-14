dc13b092a5b85dc5ac4e7d143db91f16f9febc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:42 +0000

    Add package xerial/1.1.1b1/xerial-1.1.1b1.zip

---
+++ packages/xerial/xerial-1.1.1b1.zip/xerial-1.1.1b1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='xerial',
+    version='1.1.1b1',
+    author='Nicholas Petty',
+    author_email='nick@ihackeverything.com',
+    packages=['xerial'],
+    entry_points = {
+        "console_scripts": ['xerial = xerial.xerial']
+        },
+    package_data={'':['docs/*.md','LICENSE']},
+    include_package_data=True,
+    url='http://github.com/nickpetty/xerial',
+    license='GPL licence, see LICENSE',
+    description='Terminal Based Serial Client',
+    long_description=open('README.rst').read(),
+    install_requires=['pyserial'],
+    scripts=['bin/xerial'],
+    keywords="xerial serial"
+)
+
+
+
