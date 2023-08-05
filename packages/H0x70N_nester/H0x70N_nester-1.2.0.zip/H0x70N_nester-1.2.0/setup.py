0d5655f1ecd80697221c77ab55df6abd9ca5cd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:12 +0000

    Add package H0x70N_nester/1.2.0/H0x70N_nester-1.2.0.zip

---
+++ packages/H0x70N_nester/H0x70N_nester-1.2.0.zip/H0x70N_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'H0x70N_nester',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'H0x70N',
+    author_email = 'hoxton860729@gmail.com',
+    url = 'https://www.facebook.com/profile.php?id=100009535360857',
+    description = 'A simple printer of nested lists',
+    )
