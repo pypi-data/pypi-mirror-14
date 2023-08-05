2bf486c237c4adc590e37d0ae1ec0d744f75614c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package digimat.rws/0.1.6/digimat.rws-0.1.6.tar.gz

---
+++ packages/digimat.rws/digimat.rws-0.1.6.tar.gz/digimat.rws-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digimat.rws',
+    version='0.1.6',
+    description='Digimat RWS Client',
+    namespace_packages=['digimat'],
+    author='Frederic Hess',
+    author_email='fhess@splust.ch',
+    url='http://www.digimat.ch',
+    license='PSF',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'digimat.gapi',
+        'xlwt',
+        'Pillow',
+        'setuptools'
+    ],
+    dependency_links=[
+        ''
+    ],
+    zip_safe=False)
