cc700829deda9a573e45127b34b2d90cb620f425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package python-embedded-launcher/0.3.1/python-embedded-launcher-0.3.1.zip

---
+++ packages/python-embedded-launcher/python-embedded-launcher-0.3.1.zip/python-embedded-launcher-0.3.1/examples/demo_wx_classic/sample_application.py
@@ -0,0 +1,20 @@
+import wx
+
+def main():
+    app = wx.App(redirect=True)
+    top = wx.Frame(None, title="Hello World", size=(300, 200))
+    top.Show()
+
+    # close the console window as soon as the main window shows
+    try:
+        import launcher
+    except ImportError:
+        pass # ignore this when testing and running this file directly
+    else:
+        launcher.close_console()
+        top.SetFocus()
+
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
