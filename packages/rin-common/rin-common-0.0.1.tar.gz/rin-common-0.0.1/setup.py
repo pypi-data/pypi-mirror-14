8c8845a7abf6b703204010e02676c0297e218346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:01 +0000

    Add package rin-common/0.0.1/rin-common-0.0.1.tar.gz

---
+++ packages/rin-common/rin-common-0.0.1.tar.gz/rin-common-0.0.1/setup.py
@@ -0,0 +1,47 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rin-common',
+
+    version='0.0.1',
+
+    description='Common library for Resrouce-In drivers',
+    long_description=long_description,
+
+    install_requires=['pyaml'],
+
+    url='',
+
+    # Author details
+    author='Hiroyasu OHYAMA',
+    author_email='oyama-hiroyasu@dmm.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    data_files=[('etc', ['config/rin.yml.example'])],
+)
