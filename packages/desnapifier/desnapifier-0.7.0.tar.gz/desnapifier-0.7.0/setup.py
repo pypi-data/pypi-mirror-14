9e8245adafe0d209bee689d984ad777b999e113a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package desnapifier/0.7.0/desnapifier-0.7.0.tar.gz

---
+++ packages/desnapifier/desnapifier-0.7.0.tar.gz/desnapifier-0.7.0/setup.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2016 Jeandre Kruger
+#
+# This file is part of desnapifier.
+#
+# desnapifier is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# desnapifier is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with desnapifier.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name = "desnapifier",
+    version = "0.7.0",
+    description = "Convert Snap! projects to Scratch",
+    author = "Jeandre Kruger",
+    author_email = "Unknown",
+    url = "https://github.com/Jonathan50/desnapifier",
+    packages = [ "desnapifier" ]
+)
