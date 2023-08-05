0d9cf3deb052a88f12a110a22374090956c476b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package datalabsdk/0.1.7/datalabsdk-0.1.7.zip

---
+++ packages/datalabsdk/datalabsdk-0.1.7.zip/datalabsdk-0.1.7/setup.py
@@ -0,0 +1,21 @@
+__author__ = 'Vetom'
+
+from setuptools import setup
+
+setup(name='datalabsdk',
+      version='0.1.7',
+      description='Data Lab API for Python',
+      author='III Data Analytic Team',
+      author_email='patrickyeh@iii.org.tw',
+      install_requires=['numpy'],
+      url='https://github.com/patrickyeh/datalabsdk-python',
+      packages=['datalabsdk'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7']
+      )
