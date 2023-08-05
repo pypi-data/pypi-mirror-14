30eb7215fe20b7be491f63507d72ab23abf036ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package nester_fatumepta/1.2.0/nester_fatumepta-1.2.0.tar.gz

---
+++ packages/nester_fatumepta/nester_fatumepta-1.2.0.tar.gz/nester_fatumepta-1.2.0/nester_fatumepta.py
@@ -0,0 +1,25 @@
+"""This is the 'nester.py' module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+
+
+def print_lol(the_list, level = 0):
+    """This function takes a positional argument called 'the_list', which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line.
+    A second argument called “level" is used to insert tab-stops when a nested list
+    is encountered."""
+
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print('\t', end="")
+            print(item)
+
+
+movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Giliam', 91,
+          ['Graham Chapman',
+           ['Michael Palin', 'John Cleese', 'Teryy Giliam', 'Eric Idle', 'Terry Jones']]]
+
+print_lol(movies, 0)
