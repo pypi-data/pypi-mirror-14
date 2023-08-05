2f974904a3c9aab277d99fdb51583f94da8fbb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package coala/0.4.2.dev20160228144403/coala-0.4.2.dev20160228144403-py3-none-any.whl

---
+++ packages/coala/coala-0.4.2.dev20160228144403-py3-none-any.whl/coalib/output/JSONEncoder.py
@@ -0,0 +1,23 @@
+import collections
+import json
+from datetime import datetime
+
+from coalib.misc.Decorators import get_public_members
+
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if hasattr(obj, "__json__"):
+            return obj.__json__()
+        elif isinstance(obj, collections.Iterable):
+            return list(obj)
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        elif hasattr(obj, "__getitem__") and hasattr(obj, "keys"):
+            return dict(obj)
+        elif hasattr(obj, "__dict__"):
+            return {member: getattr(obj, member)
+                    for member in get_public_members(obj)}
+
+        return json.JSONEncoder.default(self, obj)
