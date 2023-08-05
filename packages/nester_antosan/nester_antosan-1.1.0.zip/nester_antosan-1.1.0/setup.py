dd2c8be59a287c060d9eb9fe88c6b0eb6b774f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:44 +0000

    Add package nester_antosan/1.1.0/nester_antosan-1.1.0.zip

---
+++ packages/nester_antosan/nester_antosan-1.1.0.zip/nester_antosan-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_antosan',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='antosan',
+    author_email='sandeantony@gmail.com',
+    url='http://mastechict.com',
+    description='A simple printer of nested, well formatted lists'
+)
