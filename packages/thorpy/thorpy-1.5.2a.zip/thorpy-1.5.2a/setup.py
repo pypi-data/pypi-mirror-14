9a0abe1f436811335b75894d9afa6ac1e75ce4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:49 +0000

    Add package thorpy/1.5.2a/thorpy-1.5.2a.zip

---
+++ packages/thorpy/thorpy-1.5.2a.zip/thorpy-1.5.2a/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='thorpy',
+      version='1.5.2a',
+      description='pygame GUI library',
+      author='Yann Thorimbert',
+      author_email='yann.thorimbert@gmail.com',
+      url='http://www.thorpy.org/',
+      packages=find_packages(),
+      include_package_data=True,
+      license='MIT')
