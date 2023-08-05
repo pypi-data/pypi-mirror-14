68667d2ec4e862e5eeba8ed9e275ff7fe1fa031d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package metricd/1.1.1/metricd-1.1.1.tar.gz

---
+++ packages/metricd/metricd-1.1.1.tar.gz/metricd-1.1.1/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+
+from setuptools import setup, find_packages
+
+from metricd import __version__
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    install_requires = [line.strip() for line in f.readlines()]
+
+with open('requirements-dev.txt') as f:
+    tests_requires = [line.strip() for line in f.readlines()]
+
+setup(
+        name='metricd',
+        version=__version__,
+        description='Monitoring agent for collecting and sending metric data',
+        long_description=long_description,
+        author='Osman Ungur',
+        author_email='osmanungur@gmail.com',
+        url='https://github.com/o/metric-agent',
+        license='MIT License',
+        packages=find_packages(),
+        install_requires=install_requires,
+        tests_require=tests_requires,
+        classifiers=[],
+        keywords=['monitoring', 'metric', 'agent', 'statistics', 'devops'],
+        scripts=glob('bin/*')
+)
