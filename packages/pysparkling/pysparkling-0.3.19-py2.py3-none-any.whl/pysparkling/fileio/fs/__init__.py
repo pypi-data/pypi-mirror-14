a65a4622e4aae700f1c7ce246c48112afec44d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package pysparkling/0.3.19/pysparkling-0.3.19-py2.py3-none-any.whl

---
+++ packages/pysparkling/pysparkling-0.3.19-py2.py3-none-any.whl/pysparkling/fileio/fs/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+from .file_system import FileSystem
+from .local import Local
+from .s3 import S3
+from .http import Http
+from .hdfs import Hdfs
+
+
+FILE_EXTENSIONS = [
+    (('file', ''), Local),
+    (('s3', 's3n'), S3),
+    (('http', 'https'), Http),
+    (('hdfs'), Hdfs),
+]
+
+
+def get_fs(path):
+    """Find the file system implementation for this path."""
+    scheme = ''
+    if '://' in path:
+        scheme = path.partition('://')[0]
+
+    for schemes, fs_class in FILE_EXTENSIONS:
+        if scheme in schemes:
+            return fs_class
+
+    return FileSystem
