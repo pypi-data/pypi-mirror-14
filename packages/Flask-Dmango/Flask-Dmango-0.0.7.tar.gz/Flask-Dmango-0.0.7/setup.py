b7d231e4618e823a1f8125e5df983dea503dd03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:43 +0000

    Add package Flask-Dmango/0.0.7/Flask-Dmango-0.0.7.tar.gz

---
+++ packages/Flask-Dmango/Flask-Dmango-0.0.7.tar.gz/Flask-Dmango-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+setup(
+    name='Flask-Dmango',
+    version='0.0.7',
+    url='https://github.com/jungkoo/flask-dmango',
+    license='BSD',
+    author='jeong mincheol',
+    author_email='deajang@gmail.com',
+    description='Contents managements support for Flask + MongoDB applications',
+    long_description=__doc__,
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(),
+    package_data={'':['templates/admin/*.html', 'templates/dmango/*.html']},
+    install_requires=[
+        'Flask-PyMongo >= 0.4.1',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    # setup_requires=['nose'],
+    # tests_require=['nose', 'coverage'],
+    # test_suite='nose.collector',
+)
