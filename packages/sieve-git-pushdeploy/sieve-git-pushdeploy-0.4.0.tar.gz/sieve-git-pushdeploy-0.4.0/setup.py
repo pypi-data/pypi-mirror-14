5d4eb45abda3c5810fd97d8ac40ca0ba0fccf051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package sieve-git-pushdeploy/0.4.0/sieve-git-pushdeploy-0.4.0.tar.gz

---
+++ packages/sieve-git-pushdeploy/sieve-git-pushdeploy-0.4.0.tar.gz/sieve-git-pushdeploy-0.4.0/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Based on https://github.com/pypa/sampleproject
+See: https://packaging.python.org/en/latest/distributing.html
+"""
+
+import sys
+
+if sys.version_info.major < 3:
+    raise Exception("Python2 is not supported, install with pip3.")
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sieve-git-pushdeploy',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.4.0',
+
+    description='Push a sieve script to your mailserver and have it applied automatically.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/mheistermann/sieve-git-pushdeploy',
+
+    # Author details
+    author='Martin Heistermann',
+    author_email='sieve-git-pushdeploy@mheistermann.de',
+
+    # Choose your license
+    license='GPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='git sieve pushdeployment',
+
+    packages=find_packages(),
+    install_requires=['managesieve3>=1.2'],
+    dependency_links = ['git+https://bitbucket.org/myuser/managesieve3.git/master#egg=managesieve3'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    #extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    #},
+
+    entry_points={
+        'console_scripts': [
+            'sieve_git_pushdeploy=sieve_git_pushdeploy.hooks:main',
+        ],
+    },
+    package_data={
+        'sieve_git_pushdeploy': ['README.md', 'sieve_git_pushdeploy/sieve.conf.example']
+    },
+    include_package_data=True,
+)
---
+++ packages/sieve-git-pushdeploy/sieve-git-pushdeploy-0.4.0.tar.gz/sieve-git-pushdeploy-0.4.0/sieve_git_pushdeploy/hooks.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2016 Martin Heistermann <github[]mheistermann.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import sys
+import logging
+import configparser
+import subprocess
+import ssl
+from managesieve3 import Managesieve
+
+
+CONFIG_FILENAME = "~/.config/sieve-git-pushdeploy/sieve.conf"
+
+class MessageException(Exception):
+    def __init__(self, message):
+        super(MessageException, self).__init__(self, message)
+        self.message = message
+
+class ConfigError(MessageException):
+    pass
+
+
+class SieveError(MessageException):
+    pass
+
+
+def read_config(git_path):
+    defaults = {
+        "scriptname": "main",
+        "file": "main.sieve",
+        "branch": "refs/heads/master",
+    }
+    config = configparser.SafeConfigParser(defaults)
+    config_path = os.path.expanduser(CONFIG_FILENAME)
+    with open(config_path) as fp:
+        config.readfp(fp)
+    if git_path not in config.sections():
+        raise ConfigError("Section [{}] missing".format(git_path))
+
+    conf = {k: config.get(git_path, k) for k in [
+        "host", "user", "pass", "file", "scriptname", "branch", "tls_hostname"]}
+
+    return conf
+
+
+def get_script(refname, filename):
+    try:
+        content =  subprocess.check_output(["git", "show",
+                                            refname + ":" + filename])
+        return content.decode('utf-8') # for managesieve3 API
+    except subprocess.CalledProcessError:
+        raise ConfigError("Can't find file '{}' in '{}'".format(
+            filename, refname))
+
+
+def connect(config):
+    conn = Managesieve(config["host"])
+    ctx = ssl.create_default_context()
+    conn.cmd_starttls(ssl_version=ssl.PROTOCOL_TLSv1_2,
+                      cert_reqs=ssl.CERT_REQUIRED,
+                      ca_certs="/etc/ssl/certs/ca-certificates.crt",
+                      )
+    # XXX conn._sock is hackish, should rather improve the managesieve3 lib.
+    ssl.match_hostname(conn._sock.getpeercert(), config["tls_hostname"])
+
+    conn.login_plain(username=config["user"],
+                     authuser=config["user"],
+                     password=config["pass"])
+
+    logging.info("current scripts; %s", conn.cmd_listscripts())
+    return conn
+
+
+def hook_update(config, branch, old_hash, new_hash):
+    """Check sieve script validity, reject updates with invalid scripts."""
+    script = get_script(new_hash, config["file"])
+    conn = connect(config)
+
+    is_okay, code, text = conn.cmd_checkscript(script)
+    if not is_okay:
+        raise SieveError("script invalid.: {}: {}".format(code, text))
+
+    if branch == config["branch"]:
+        conn.cmd_putscript(config["scriptname"], script)
+        conn.cmd_setactive(config["scriptname"])
+        print("Successfully uploaded sieve script.")
+        return 0
+    print("not uploading script in non-deployment branch {}".format(branch))
+    return 0
+
+hooks = {
+    'update': hook_update,
+    }
+
+def usage():
+    print("Usage: call this script with no arguments by symlinking" +
+          " it from .git/hooks/ (argv[0] must be the hook name)")
+    print("Implemented hooks:")
+    for name, func in hooks.items():
+        print("{}: {}".format(name, func.__doc__))
+
+
+def main():
+    name = os.path.split(sys.argv[0])[-1]
+    hook_args = sys.argv[1:]
+
+    try:
+        hook = hooks[name]
+    except KeyError:
+        print("Can't handle hook '{}'.".format(name))
+        usage()
+        sys.exit(1)
+
+    git_path = os.getcwd()
+    config = read_config(git_path)
+    logging.debug("config: {}", config)
+
+    try:
+        retval = hook(config, *hook_args)
+    except (ConfigError, SieveError) as e:
+        logging.error(e.message)
+        retval = 1
+    sys.exit(retval)
+
+
+if __name__ == "__main__":
+    main()
