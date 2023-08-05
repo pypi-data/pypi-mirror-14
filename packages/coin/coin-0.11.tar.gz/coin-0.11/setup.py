834f4e3121855397c3663caf8cb16476ce88502c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:14 +0000

    Add package coin/0.11/coin-0.11.tar.gz

---
+++ packages/coin/coin-0.11.tar.gz/coin-0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from os.path import join, dirname
+
+requirements = [
+    'urllib3==1.7.1'
+]
+
+version = open(join(dirname(__file__), 'version')).read().strip()
+print(version)
+
+setup(
+    name='coin',
+    version=version,
+    py_modules = ['coin'],
+    install_requires=requirements,
+    scripts=['coin'],
+    description='Copy Installer',
+    license='GPLv3',
+    author='Syncloud',
+    author_email='support@syncloud.it',
+    url='https://github.com/syncloud/coin'
+)
