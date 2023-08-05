cbbc875b57fb647334a8b80c5f992f3849d9282f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package chilero/0.3.5/chilero-0.3.5.tar.gz

---
+++ packages/chilero/chilero-0.3.5.tar.gz/chilero-0.3.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='chilero',
+    version='0.3.5',
+    namespace_packages=['chilero'],
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/dmonroy/chilero',
+    author='Darwin Monroy',
+    author_email='contact@darwinmonroy.com',
+    description='A micro framework... A la Tortrix!',
+    install_requires=[
+        'aiohttp>=0.21',
+    ]
+)
