0cd4b3aa57238c56f6e75ff2c1c5fdd41dc617a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package MPBS/0.0.3.1/MPBS-0.0.3.1.tar.gz

---
+++ packages/MPBS/MPBS-0.0.3.1.tar.gz/MPBS-0.0.3.1/MPBS/examples/mpbs/mpbs_server.py
@@ -0,0 +1,40 @@
+#coding=utf-8
+'''
+Created on 2016年3月3日
+'''
+from MPBS.core.base import MPBSHandler, Application, MPBSServer
+__author__ = 'chenjian'
+
+BROKER_URL = 'tcp://127.0.0.1:5556'
+
+class MainHanlder(MPBSHandler):
+    '''
+    '''
+    
+    def __init__(self, *args, **kwargs):
+        MPBSHandler.__init__(self, *args, **kwargs)
+        
+    def handle(self):
+        print self.request.uri, self.request.headers, self.request.body
+
+
+class App(Application):
+    '''
+    '''
+    
+    def __init__(self):
+        
+        
+        Application.__init__(self, 
+                             handlers = [
+                                         (r'/', MainHanlder),
+                                         (r'/v1/', MainHanlder)
+                                         ]
+                             )
+        
+def main():
+    MPBSServer(App(), BROKER_URL).start()
+
+if __name__ == '__main__':
+    main()
+    
