1ebd2f079da19326307117f002ef86111db6dcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:58 +0000

    Add package helloworld1995/0.1/helloworld1995-0.1.tar.gz

---
+++ packages/helloworld1995/helloworld1995-0.1.tar.gz/helloworld1995-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='helloworld1995',
+      version='0.1',
+      description='A hello world program to investigate packaging and PyPI',
+      author='Jaye Heffernan',
+      author_email='jayejaye@live.com.au',
+      license='MIT',
+      packages=['helloworld1995'],
+      zip_safe=False)
