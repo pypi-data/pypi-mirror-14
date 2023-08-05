f849b75c2aaee5420fb9846c7287448eb3a123e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package sanestack/0.0.2/sanestack-0.0.2.tar.gz

---
+++ packages/sanestack/sanestack-0.0.2.tar.gz/sanestack-0.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name='sanestack',
+      version='0.0.2',
+      description='Check if your dependencies are up to date.',
+      author='Michał Łowicki',
+      author_email='mlowicki@gmail.com',
+      url='https://github.com/mlowicki/sanestack',
+      packages=['sanestack'],
+      entry_points={'console_scripts': [
+          'sanestack = sanestack.main:main']
+      },
+      install_requires=[
+          'argcomplete==1.1.0',
+          'argh==0.26.1',
+          'requests==2.9.1',
+          'colorlog==2.6.1',
+      ],
+      zip_safe=False)
