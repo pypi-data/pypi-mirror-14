1e180908c736c095634f033b6067232749f6c7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package RDFCrawler/1.4.5/RDFCrawler-1.4.5.tar.gz

---
+++ packages/RDFCrawler/RDFCrawler-1.4.5.tar.gz/RDFCrawler-1.4.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Ignacio Molina Cuquerella'
+__email__ = 'imolina@centeropenmiddleware.com'
+
+setup(
+    name = 'RDFCrawler',
+    version = '1.4.5',
+    author = 'Ignacio Molina Cuquerella',
+    author_email = 'imolina@centeropenmiddleware.com',
+    description = 'A RESTful services for Python that explores and replicates '
+                  'a RDF topology.',
+    keywords = ['rdf', 'crawler', 'replication'],
+    url = 'https://github.com/ignaciomolina/RDFCrawler',
+    download_url = 'https://github.com/ignaciomolina/RDFCrawler/tarball/1.4.5',
+    packages = find_packages(),
+    scripts= ['rdf_service.py', 'rdf_crawler.py'],
+    requires=['flask', 'rdflib']
+)
