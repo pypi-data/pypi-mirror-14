02cb7607142deb75764bd9af8d60d6aef38d9aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package balafon/3.2.3/balafon-3.2.3.tar.gz

---
+++ packages/balafon/balafon-3.2.3.tar.gz/balafon-3.2.3/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('balafon').__version__
+
+setup(
+    name='balafon',
+    version=VERSION,
+    description='Contacts management',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Luc Jean',
+    author_email='ljean@apidev.fr',
+    license='CeCILL 2.1',
+    zip_safe=False,
+    install_requires=[
+        'django >= 1.6, <1.10',
+        'djangorestframework',
+        'apidev-coop_cms >= 1.1.4',
+        'beautifulsoup4',
+        'BeautifulSoup',
+        'django-wkhtmltopdf',
+        'xlwt',
+        'xlrd',
+        'django-simple-captcha',
+        'django-registration',
+        'model_mommy',
+        'django-tastypie',
+        'django-cors-headers',
+    ],
+    #dependency_links=[
+    #    'git+https://github.com/ljean/coop_cms.git@0ba4fcbd0d8ff1d01617ceafc56d7175758bc966#egg=apidev_coop_cms-dev',
+    #],
+    long_description=open('README.rst').read(),
+    url='https://github.com/ljean/balafon',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Topic :: Office/Business',
+    ],
+)
+
