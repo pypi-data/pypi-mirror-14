6fb912a8ec7e926e05e323eb4aa7e193d7e74cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:17 +0000

    Add package gff_toolkit/0.1.10/gff_toolkit-0.1.10.tar.gz

---
+++ packages/gff_toolkit/gff_toolkit-0.1.10.tar.gz/gff_toolkit-0.1.10/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+__author__ = 'rensholmer'
+__created__ = '17/08/15'
+
+import sys
+from setuptools import setup
+
+def main():
+	setup(name='gff_toolkit',
+		packages=['gff_toolkit'],
+		author='rens holmer',
+		author_email='rens.holmer@wur.nl',
+		version='0.1.10',
+		url='https://github.com/holmrenser/gff_toolkit',
+		install_requires=['intervaltree']
+		)
+
+if __name__ == '__main__':
+	main()
