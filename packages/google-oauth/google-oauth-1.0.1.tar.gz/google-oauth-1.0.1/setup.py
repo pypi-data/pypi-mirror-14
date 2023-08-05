8dce1d007a0a26e943c1af7daf93ab0ae03fcbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:58 +0000

    Add package google-oauth/1.0.0/google-oauth-1.0.1.tar.gz

---
+++ packages/google-oauth/google-oauth-1.0.1.tar.gz/google-oauth-1.0.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='google-oauth',
+    version='1.0.1',
+    packages=['google_oauth'],
+    description='OAuth2 for Google APIs',
+    long_description=readme(),
+    url='https://github.com/miedzinski/google-oauth2',
+    author='Dominik Miedziński',
+    license='MIT License',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+    ),
+    keywords='google oauth oauth2 api service jwt',
+    install_requires=[
+        'pyopenssl>=0.11',
+        'requests',
+        'six',
+    ],
+    test_suite='tests',
+)
