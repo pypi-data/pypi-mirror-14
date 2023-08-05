7882efae490a1602215de92809133c7e19b80f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package django-tos/0.4.0/django-tos-0.4.0.tar.gz

---
+++ packages/django-tos/django-tos-0.4.0.tar.gz/django-tos-0.4.0/tos/models.py
@@ -0,0 +1,89 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Django 1.4 compatability
+try:
+    from django.contrib.auth import get_user_model
+    USER_MODEL = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+    USER_MODEL = User
+
+
+class NoActiveTermsOfService(ValidationError):
+    pass
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TermsOfServiceManager(models.Manager):
+    def get_current_tos(self):
+        try:
+            return self.get(active=True)
+        except self.model.DoesNotExist:
+            raise NoActiveTermsOfService(
+                u'Please create an active Terms-of-Service'
+            )
+
+
+class TermsOfService(BaseModel):
+    active = models.BooleanField(
+                default=False,
+                verbose_name=_('active'),
+                help_text=_(
+                    u'Only one terms of service is allowed to be active'
+                )
+    )
+    content = models.TextField(verbose_name=_('content'), blank=True)
+    objects = TermsOfServiceManager()
+
+    class Meta:
+        get_latest_by = 'created'
+        ordering = ('-created',)
+        verbose_name = _('Terms of Service')
+        verbose_name_plural = _('Terms of Service')
+
+    def __unicode__(self):
+        active = 'inactive'
+        if self.active:
+            active = 'active'
+        return '{0}: {1}'.format(self.created, active)
+
+    def save(self, *args, **kwargs):
+        """ Ensure we're being saved properly """
+
+        if self.active:
+            TermsOfService.objects.exclude(id=self.id).update(active=False)
+
+        else:
+            if not TermsOfService.objects\
+                    .exclude(id=self.id)\
+                    .filter(active=True):
+                raise NoActiveTermsOfService(
+                    u'One of the terms of service must be marked active'
+                )
+
+        super(TermsOfService, self).save(*args, **kwargs)
+
+
+class UserAgreement(BaseModel):
+    terms_of_service = models.ForeignKey(TermsOfService, related_name='terms')
+    user = models.ForeignKey(USER_MODEL, related_name='user_agreement')
+
+    def __unicode__(self):
+        return u'%s agreed to TOS: %s' % (self.user.username,
+                                          unicode(self.terms_of_service))
+
+
+def has_user_agreed_latest_tos(user):
+    return UserAgreement.objects.filter(
+        terms_of_service=TermsOfService.objects.get_current_tos(),
+        user=user,
+    ).exists()
