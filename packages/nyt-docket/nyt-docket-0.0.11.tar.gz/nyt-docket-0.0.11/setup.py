95edf4dd69c64a194a393ef73a121e8ab52effad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:41 +0000

    Add package nyt-docket/0.0.11/nyt-docket-0.0.11.tar.gz

---
+++ packages/nyt-docket/nyt-docket-0.0.11.tar.gz/nyt-docket-0.0.11/setup.py
@@ -0,0 +1,34 @@
+import os.path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='nyt-docket',
+    version='0.0.11',
+    author='Jeremy Bowers',
+    author_email='jeremy.bowers@nytimes.com',
+    url='https://github.com/newsdev/nyt-docket',
+    description='Python client for parsing SCOTUS cases from the granted/noted and orders dockets.',
+    long_description=read('README.rst'),
+    packages=['docket'],
+    entry_points={
+        'console_scripts': (
+            'docket = docket:main',
+        ),
+    },
+    license="Apache License 2.0",
+    keywords='SCOTUS data parsing scraping legal law court',
+    install_requires=['beautifulsoup4==4.4.1','docopt==0.6.2','lxml==3.4.4','numpy==1.9.3','pdfminer==20140328','requests==2.7.0','wheel==0.24.0','cement==2.6.2','pymongo==3.2.1','clint'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules']
+)
