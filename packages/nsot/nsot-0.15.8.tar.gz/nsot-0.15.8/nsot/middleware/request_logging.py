997ccab75687a32978b5b41b3fa9b1b1d51e42f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:03 +0000

    Add package nsot/0.15.8/nsot-0.15.8.tar.gz

---
+++ packages/nsot/nsot-0.15.8.tar.gz/nsot-0.15.8/nsot/middleware/request_logging.py
@@ -0,0 +1,31 @@
+
+"""
+Middleware to log HTTP requests.
+"""
+
+from django.utils.log import getLogger
+from time import time
+
+
+class LoggingMiddleware(object):
+    def __init__(self):
+        self.logger = getLogger('nsot_server')
+
+    def process_request(self, request):
+        request.timer = time()
+        return None
+
+    def process_response(self, request, response):
+        if 'HTTP_X_FORWARDED_FOR' in request.META: #If
+            request_ip_path = '%s, %s' % (request.META.get('REMOTE_ADDR'), request.META.get('HTTP_X_FORWARDED_FOR'))
+        else:
+            request_ip_path = request.META.get('REMOTE_ADDR')
+        self.logger.info(
+            '%s %s %s (%s) %.2fms',
+            response.status_code,
+            request.method,
+            request.get_full_path(),
+            request_ip_path,
+            (time() - request.timer) * 1000  # ms
+        )
+        return response
