0d604ac2b873da79d5d9f555021ea1f5a6ca2acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:01 +0000

    Add package testfunction/1.2.0/testfunction-1.2.0.zip

---
+++ packages/testfunction/testfunction-1.2.0.zip/testfunction-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name    ='testfunction',
+       version ='1.2.0',
+       py_modules=['testfunction'],
+       author  ='shawnye1994',
+       author_email='shawnye1994@gmail.com',
+       url='none',
+       description='A simple test as beginer',
+       )
