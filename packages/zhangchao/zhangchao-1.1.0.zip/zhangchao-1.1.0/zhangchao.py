ccad73e5deb84b9df19d3521632a78d95918e921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:11 +0000

    Add package zhangchao/1.1.0/zhangchao-1.1.0.zip

---
+++ packages/zhangchao/zhangchao-1.1.0.zip/zhangchao-1.1.0/zhangchao.py
@@ -0,0 +1,13 @@
+'''
+Created on 2016年3月15日
+
+@author: zsuper
+'''
+def print_lol(the_list,level):
+    for each in the_list:
+        if isinstance(each,list):
+            print_lol(each,level+1)
+        else:
+            for tap_stop in range(level):
+                print("\t",end='')
+            print(each)
