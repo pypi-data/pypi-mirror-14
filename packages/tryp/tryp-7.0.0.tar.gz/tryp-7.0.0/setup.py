37ab3038218dd5ba90e63bbcdaf2cd665bc72d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package tryp/7.0.0/tryp-7.0.0.tar.gz

---
+++ packages/tryp/tryp-7.0.0.tar.gz/tryp-7.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from version import version
+
+setup(
+    name='tryp',
+    description='tryp tools',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/tryp',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'fn',
+        'toolz',
+    ]
+)
