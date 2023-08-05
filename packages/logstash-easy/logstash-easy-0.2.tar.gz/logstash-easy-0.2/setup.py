60142b799db2738c6fa94b14efbbd7dbdc1e7898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:15 +0000

    Add package logstash-easy/0.2/logstash-easy-0.2.tar.gz

---
+++ packages/logstash-easy/logstash-easy-0.2.tar.gz/logstash-easy-0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='logstash-easy',
+    version='0.2',
+    description='Very easy to use Logstash logging handler',
+    platforms="all",
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python',
+    ],
+    author='Alexander Tikhonov',
+    author_email='random.gauss@gmail.com',
+    url='https://github.com/Fatal1ty/logstash_easy',
+    py_modules=['logstash_easy'],
+    install_requires=[
+        'python-logstash',
+    ]
+)
