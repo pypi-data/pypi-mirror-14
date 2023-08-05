b40b76215c49e54f67978f53c1bb36ad2b0bedc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package django_shark/0.1.23/django_shark-0.1.23.zip

---
+++ packages/django_shark/django_shark-0.1.23.zip/django_shark-0.1.23/shark/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from shark.models import EditableText
+
+@admin.register(EditableText)
+class EditableTextAdmin(admin.ModelAdmin):
+    list_display = ['name', 'handler_name', 'last_used']
+    list_filter = ['last_used', 'handler_name']
+    ordering = ['handler_name', 'name']
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'handler_name')
+        }),
+        ('Content', {
+            'fields': ('content',),
+            'description': 'Enter the content for this text.'
+        })
+    )
