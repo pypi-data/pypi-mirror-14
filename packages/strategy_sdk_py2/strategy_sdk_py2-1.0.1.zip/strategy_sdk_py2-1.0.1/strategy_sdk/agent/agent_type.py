25a433fa7792bab08bbb23a14e344f57e4a1101a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package strategy_sdk_py2/1.0.1/strategy_sdk_py2-1.0.1.zip

---
+++ packages/strategy_sdk_py2/strategy_sdk_py2-1.0.1.zip/strategy_sdk_py2-1.0.1/strategy_sdk/agent/agent_type.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+from __future__ import absolute_import
+import unittest
+
+from utils.enum import EnumMetaCls, CommonNameValueEnum, can_not_init
+
+
+__author__ = u'yonka'
+
+
+class AgentType(CommonNameValueEnum):
+    __metaclass__ = EnumMetaCls
+
+    def __str__(self):
+        return u"AgentType(%d)" % self.v
+
+
+AgentType.TRADE = AgentType(1)
+AgentType.QUOTE = AgentType(2)
+
+AgentType.__init__ = can_not_init
+
+
+class TestAgentType(unittest.TestCase):
+    def test_init(self):
+        print AgentType.TRADE
+        res = False
+        try:
+            AgentType(1)
+        except AssertionError:
+            res = True
+        assert res
