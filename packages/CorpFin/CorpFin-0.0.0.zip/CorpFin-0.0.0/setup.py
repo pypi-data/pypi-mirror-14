42b764fd027e67d5833491065c0e8074aaf67aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package CorpFin/0.0.0/CorpFin-0.0.0.zip

---
+++ packages/CorpFin/CorpFin-0.0.0.zip/CorpFin-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name='CorpFin',
+      version='0.0.0',
+      packages=['CorpFin'],
+      url='https://github.com/MBALearnsToCode/CorpFin',
+      author='Vinh Luong (a.k.a. MBALearnsToCode)',
+      author_email='MBALearnsToCode@UChicago.edu',
+      description='Corporate Finance functionalities based on SymPy and Theano',
+      long_description='(please read README.md on GitHub)',
+      license='MIT License',
+      install_requires=['FrozenDict', 'NamedList', 'NumPy', 'Pandas', 'SymPy', 'Theano'],
+      classifiers=[],   # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='corporate finance corp fin financial sympy theano')
