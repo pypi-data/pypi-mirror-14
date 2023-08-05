e4f39b3272f53c5f77c1dc54a501f6676482b803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package dirq/1.7.1/dirq-1.7.1.tar.gz

---
+++ packages/dirq/dirq-1.7.1.tar.gz/dirq-1.7.1/dirq/utils.py
@@ -0,0 +1,38 @@
+"""
+Utilities used in dirq module.
+
+Author
+------
+
+Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
+
+License and Copyright
+---------------------
+
+ASL 2.0
+
+Copyright (C) CERN 2011-2016
+"""
+
+import sys
+
+_PY2 = sys.hexversion < 0x03000000
+_PY3 = not _PY2
+
+if _PY2:
+    VALID_STR_TYPES = (str, unicode)
+else:
+    VALID_STR_TYPES = (str, bytes)
+
+if _PY2:
+    VALID_INT_TYPES = (int, long)
+else:
+    VALID_INT_TYPES = (int, )
+
+
+def is_bytes(string):
+    """ Check if given string is a byte string. """
+    if _PY2:
+        return not isinstance(string, unicode)
+    else:  # python 3
+        return isinstance(string, bytes)
