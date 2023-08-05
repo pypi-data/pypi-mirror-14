234e9796902ec4b2ac7c32ad412d41f7e51a50ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package tryp/7.0.9/tryp-7.0.9.tar.gz

---
+++ packages/tryp/tryp-7.0.9.tar.gz/tryp-7.0.9/setup.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+exec(Path('version.py').read_text())
+
+setup(
+    name='tryp',
+    description='tryp tools',
+    version=version,  # NOQA
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/tryp',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'fn',
+        'toolz',
+    ]
+)
---
+++ packages/tryp/tryp-7.0.9.tar.gz/tryp-7.0.9/tryp/tc/functor.py
@@ -0,0 +1,26 @@
+import abc
+from typing import TypeVar, Generic, Callable
+
+from tryp.tc.base import TypeClass
+
+F = TypeVar('F')
+A = TypeVar('A')
+B = TypeVar('B')
+
+
+class Functor(Generic[F], TypeClass):
+
+    @abc.abstractmethod
+    def map(self, fa: F, f: Callable[[A], B]) -> F:
+        ...
+
+    def __truediv__(self, fa, f):
+        return self.map(fa, f)
+
+    def smap(self, fa: F, f: Callable[..., B]) -> F:
+        return self.map(fa, lambda v: f(*v))
+
+    def ssmap(self, fa: F, f: Callable[..., B]) -> F:
+        return self.map(fa, lambda v: f(**v))
+
+__all__ = ('Functor',)
