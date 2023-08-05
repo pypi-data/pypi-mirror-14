1583dcfa55d9e6669f0ea1e3ad0f8faf9d08f4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:13 +0000

    Add package gpssim/0.7.2.4/gpssim-0.7.2.4.tar.gz

---
+++ packages/gpssim/gpssim-0.7.2.4.tar.gz/gpssim-0.7.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import subprocess
+
+setup(
+    name = 'gpssim',
+    version = subprocess.check_output(['hg', 'log', '-r', 'limit(.::, 1)', '--template', '{latesttag}.{latesttagdistance}']),
+    description = 'A Python GPS simulation library',
+    author = 'Wei Li Jiang',
+    author_email = 'wjiang87@gmail.com',
+    url = 'https://bitbucket.org/wjiang/gpssim',
+    keywords = ['gps', 'nmea', 'simulator'],
+    install_requires=["pySerial>=2.5-rc2", "geographiclib"],
+    entry_points={'gui_scripts': ['gpssim = uilauncher:main']},
+    test_suite="tests",
+	py_modules=["uilauncher"],
+    packages=["gpssim"]
+)
