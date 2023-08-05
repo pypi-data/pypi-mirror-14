d3a716d7bcc2514dc667690d25de14c30759c186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:17 +0000

    Add package django_blogposts/0.3.0/django_blogposts-0.3.0.tar.gz

---
+++ packages/django_blogposts/django_blogposts-0.3.0.tar.gz/django_blogposts-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_blogposts",
+    version='0.3.0',
+    description='Django module for simple blog',
+    author='Spi4ka',
+    packages=[
+        "django_blogposts",
+        "django_blogposts.templatetags",
+        "django_blogposts.migrations",
+    ],
+    include_package_data=True,
+    package_data={'django_blogposts': ['django_blogposts/templates', 'django_blogposts/locale']},
+    install_requires=[
+        'pillow',
+        'pytils',
+        'django-autoslug==1.9.3'
+    ]
+)
