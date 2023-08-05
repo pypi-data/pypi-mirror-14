e8f7115130b74f30729ef6d5c5eefeb06699c327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:36 +0000

    Add package seqenv/1.0.8/seqenv-1.0.8.tar.gz

---
+++ packages/seqenv/seqenv-1.0.8.tar.gz/seqenv-1.0.8/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+      name             = 'seqenv',
+      version          = '1.0.8',
+      description      = 'Assign environment ontology (EnvO) terms to short DNA sequences',
+      license          = 'MIT',
+      url              = 'https://github.com/xapple/seqenv',
+      download_url     = 'https://github.com/xapple/seqenv/tarball/1.0.8',
+      author           = 'Lucas Sinclair',
+      author_email     = 'lucas.sinclair@me.com',
+      classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
+      packages         = ['seqenv', 'seqenv.common', 'seqenv.fasta', 'seqenv.seqsearch'],
+      scripts          = ['seqenv/seqenv'],
+      install_requires = ['biopython', 'sh', 'pandas', 'tqdm', 'biom-format', 'requests'],
+      long_description = open('README.md').read(),
+      long_description_markdown_filename='README.md'
+    )
