28980535ab47ce81df9d4221035136ff8d82e058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:00 +0000

    Add package mip2scout/0.1.4/mip2scout-0.1.4.tar.gz

---
+++ packages/mip2scout/mip2scout-0.1.4.tar.gz/mip2scout-0.1.4/mip2scout/exc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Custom exceptions and errors."""
+
+
+class MadelineExecutableNotFoundError(Exception):
+    pass
+
+
+class SinglePedigreeError(Exception):
+    pass
+
+
+class MadelineIncompatibleError(Exception):
+    pass
+
+
+class FrequencyError(Exception):
+    pass
+
+
+class ChanjoSampleLoadedError(Exception):
+    pass
