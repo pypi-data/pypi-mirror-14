31613c385733e2a9f260e3839cf068862adcf192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:45 +0000

    Add package PyNOMAD/0.1.7.post1/PyNOMAD-0.1.7.post1.tar.gz

---
+++ packages/PyNOMAD/PyNOMAD-0.1.7.post1.tar.gz/PyNOMAD-0.1.7.post1/nomad/__about__.py
@@ -0,0 +1,25 @@
+from astropy_helpers.git_helpers import get_git_devstr
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "PyNOMAD"
+__summary__ = "Routines for accessing a self-hosted local copy of the USNO NOMAD stellar catalog"
+__uri__ = "https://github.com/henryroe/PyNOMAD"
+
+# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
+__version__ = "0.1.7-1"
+
+# Indicates if this version is a release version
+RELEASE = 'dev' not in __version__
+
+if not RELEASE:
+    __version__ += get_git_devstr(False)
+
+__author__ = "Henry Roe"
+__email__ = "hroe@hroe.me"
+
+__license__ = "MIT License"
+__copyright__ = "2015 %s" % __author__
---
+++ packages/PyNOMAD/PyNOMAD-0.1.7.post1.tar.gz/PyNOMAD-0.1.7.post1/nomad/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .nomad import fetch_nomad_box, fetch_star_by_nomad_id, __version__, set_nomad_path, _nomad_dir, read_one_catalog_file
