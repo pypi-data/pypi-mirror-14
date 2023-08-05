b43f42d55d0f495fceeca57e29a4ebcd28ddc0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package git-app-version/0.2.0/git_app_version-0.2.0-py2.py3-none-any.whl

---
+++ packages/git-app-version/git_app_version-0.2.0-py2.py3-none-any.whl/git_app_version/helper/date.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# from pprint import pprint,pformat
+from datetime import datetime
+import iso8601
+# import tzlocal
+import pytz
+
+def utcnow():
+    return datetime.now(pytz.utc)
+
+def dateTimeFromIso8601(isodate, utc=False):
+    dt = iso8601.parse_date(isodate)
+    if utc:
+        return dt.astimezone(pytz.utc)
+
+    return dt
+
+def iso8601FromDateTime(dt):
+    try:
+        return dt.strftime('%Y-%m-%dT%H:%M:%S%z')
+    except Exception as exc:
+        return ''
+
+def timestampFromDateTime(dt):
+    try:
+        utcDt  = dt.replace(tzinfo=None) - dt.utcoffset()
+        return str(int((utcDt - datetime(1970, 1, 1)).total_seconds()))
+    except Exception as exc:
+        return ''
