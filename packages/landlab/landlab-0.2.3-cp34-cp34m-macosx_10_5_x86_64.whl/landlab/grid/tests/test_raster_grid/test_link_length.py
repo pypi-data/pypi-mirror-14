96de824ac450d797edad48805f86d100a3988c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package landlab/0.2.3/landlab-0.2.3-cp34-cp34m-macosx_10_5_x86_64.whl

---
+++ packages/landlab/landlab-0.2.3-cp34-cp34m-macosx_10_5_x86_64.whl/landlab/grid/tests/test_raster_grid/test_link_length.py
@@ -0,0 +1,41 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+from nose.tools import with_setup
+
+from landlab import RasterModelGrid
+
+
+def setup_unit_grid():
+    globals().update({
+        '_RMG': RasterModelGrid(4, 5)
+    })
+
+
+def setup_grid():
+    globals().update({
+        '_RMG': RasterModelGrid((4, 5), spacing=(3, 4))
+    })
+
+
+@with_setup(setup_unit_grid)
+def test_unit_spacing():
+    lengths = _RMG._calculate_link_length()
+    assert_array_equal(lengths, np.ones(31))
+
+
+@with_setup(setup_grid)
+def test_non_unit_spacing():
+    assert_array_equal(_RMG._calculate_link_length(),
+                       [3.] * 15 + [4.] * 16)
+
+
+@with_setup(setup_grid)
+def test_link_length():
+    assert_array_equal(_RMG.link_length,
+                       [3.] * 15 + [4.] * 16)
+
+
+@with_setup(setup_grid)
+def test_active_link_length():
+    assert_array_equal(_RMG.link_length[_RMG.active_links],
+                       [3.] * 9  + [4.] * 8 )
