9894509eb73da253e22e2b9b7804bde487eecf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package gew/0.0.6/gew-0.0.6.zip

---
+++ packages/gew/gew-0.0.6.zip/gew-0.0.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+
+setup(
+    name='gew',
+    version='0.0.6',
+    description='Old School RuneScape Grand Exchange wrapper',
+    long_description='',
+    url='https://github.com/Blankpanda/GE_wrapper',
+    author='Caleb Ellis',
+    author_email='elliscaleb1998@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='api development',
+    packages=find_packages(exclude=['examples', 'scripts']),
+    install_requires=[''],
+    #data_files=[('gew/res/items.json')],
+    data_files=[],
+
+)
