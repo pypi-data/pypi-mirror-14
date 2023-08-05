a1f096132844e03e1c87ea5675b705fd38ae5b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:14 +0000

    Add package nesterchanged/1.0.2/nesterchanged-1.0.2.tar.gz

---
+++ packages/nesterchanged/nesterchanged-1.0.2.tar.gz/nesterchanged-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nesterchanged',
+    version     = '1.0.2',
+    py_modules  = ['nesterchanged'],
+    author      = 'chemalle',
+    author_email= 'econobilidade@nexthinking.com',
+    url         = 'http://www.nexthinking.com',
+    description = 'A simple printer of nested lists'
+    )
