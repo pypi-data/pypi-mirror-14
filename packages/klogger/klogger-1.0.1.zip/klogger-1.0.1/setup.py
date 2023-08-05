99309d27f651fad79271121132692db10eefe448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package klogger/1.0.1/klogger-1.0.1.zip

---
+++ packages/klogger/klogger-1.0.1.zip/klogger-1.0.1/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="klogger", version="1.0.1", description="Use minimal efforts to log tasks.", author="Kang Min Yoo", author_email="kaniblurous@gmail.com", py_modules=["klogger"])
