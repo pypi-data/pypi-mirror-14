2798b0c75526a666c6b37c96eb272149bf589b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:44 +0000

    Add package cosmos-wfm/1.7.1/cosmos-wfm-1.7.1.tar.gz

---
+++ packages/cosmos-wfm/cosmos-wfm-1.7.1.tar.gz/cosmos-wfm-1.7.1/examples/ex2.py
@@ -0,0 +1,55 @@
+"""
+This is equivalent to example1, but using a helper function :meth:`one2one` for simple relationship patterns between
+stages. The one2one, many2one, etc functions are simple and it is highly recommended that you read through their
+code, and make your own functions for similar patterns.
+"""
+
+import os
+from cosmos.api import Cosmos, one2one
+from tools import echo, cat, word_count
+from cosmos.graph.draw import draw_stage_graph, draw_task_graph, pygraphviz_available
+
+
+def run_ex2(execution):
+    # Create two jobs that echo "hello" and "world" respectively (source nodes in the graph).
+    echos = [execution.add_task(echo,
+                                tags=dict(word=word),
+                                out_dir='{word}')
+             for word in ['hello', 'world']]
+
+    # Split each echo into two jobs (a one2many relationship).
+    cats = [execution.add_task(cat,
+                               tags=dict(n=n, **echo_task.tags),
+                               parents=[echo_task],
+                               out_dir='{word}/{n}')
+            for echo_task in echos
+            for n in [1, 2]]
+
+    # Count the words in the previous stage.  An example of a one2one relationship,
+    # the most common stage dependency pattern.  For each task in StageA, you create a single dependent task in StageB.
+    word_counts = one2one(cmd_fxn=word_count, parents=cats, tag=dict(chars=True))
+
+    # Cat the contents of all word_counts into one file.  Note only one node is being created who's parents are
+    # all of the WordCounts (a many2one relationship).
+    summarize = execution.add_task(cat,
+                                   dict(),
+                                   word_counts,
+                                   '',
+                                   'Summary_Analysis')
+
+    if pygraphviz_available:
+        # These images can also be seen on the fly in the web-interface
+        draw_stage_graph(execution.stage_graph(), '/tmp/ex2_task_graph.png', format='png')
+        draw_task_graph(execution.task_graph(), '/tmp/ex2_stage_graph.png', format='png')
+    else:
+        print 'Pygraphviz is not available :('
+
+    execution.run(max_attempts=1, max_cores=10)
+
+
+if __name__ == '__main__':
+    cosmos = Cosmos('sqlite:///%s/sqlite.db' % os.path.dirname(os.path.abspath(__file__)))
+    cosmos.initdb()
+
+    execution = cosmos.start('Example2', 'analysis_output/ex2', restart=True, skip_confirm=True)
+    run_ex2(execution)
---
+++ packages/cosmos-wfm/cosmos-wfm-1.7.1.tar.gz/cosmos-wfm-1.7.1/examples/ex_email.py
@@ -0,0 +1,31 @@
+from cosmos.api import Cosmos, signal_execution_status_change, ExecutionStatus
+from ex1 import run_ex1
+import os
+from cosmos.util.helpers import mkdir
+
+def run_ex3(execution):
+    @signal_execution_status_change.connect
+    def sig(ex):
+        msg = "%s %s" % (ex, ex.status)
+        if ex.status in [ExecutionStatus.successful, ExecutionStatus.failed, ExecutionStatus.killed]:
+            text_message(msg)
+            ex.log.info('Sent a text message')
+
+    def text_message(message):
+        from twilio.rest import TwilioRestClient
+
+        account = "XYZ"
+        token = "XYZ"
+        client = TwilioRestClient(account, token)
+
+        message = client.messages.create(to="+1231231234", from_="+1231231234", body=message)
+
+    run_ex1(execution)
+
+
+if __name__ == '__main__':
+    cosmos = Cosmos('sqlite:///%s/sqlite.db' % os.path.dirname(os.path.abspath(__file__)))
+    cosmos.initdb()
+
+    execution = cosmos.start('Example_Email', 'analysis_output/ex3', max_attempts=2, restart=True, skip_confirm=True)
+    run_ex1(execution)
