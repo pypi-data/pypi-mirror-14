d1aff9a5d8adc1096c04a291c3314e06d324879b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package hyperspy/0.8.3/hyperspy-0.8.3-1.tar.gz

---
+++ packages/hyperspy/hyperspy-0.8.3-1.tar.gz/hyperspy-0.8.3/hyperspy/tests/io/test_hdf5.py
@@ -0,0 +1,231 @@
+import os.path
+from os import remove
+import datetime
+
+import nose.tools as nt
+import numpy as np
+
+from hyperspy.io import load
+from hyperspy.signal import Signal
+
+my_path = os.path.dirname(__file__)
+
+data = np.array([4066., 3996., 3932., 3923., 5602., 5288., 7234., 7809.,
+                 4710., 5015., 4366., 4524., 4832., 5474., 5718., 5034.,
+                 4651., 4613., 4637., 4429., 4217.])
+example1_original_metadata = {
+    u'BEAMDIAM -nm': 100.0,
+    u'BEAMKV   -kV': 120.0,
+    u'CHOFFSET': -168.0,
+    u'COLLANGLE-mR': 3.4,
+    u'CONVANGLE-mR': 1.5,
+    u'DATATYPE': u'XY',
+    u'DATE': u'01-OCT-1991',
+    u'DWELLTIME-ms': 100.0,
+    u'ELSDET': u'SERIAL',
+    u'EMISSION -uA': 5.5,
+    u'FORMAT': u'EMSA/MAS Spectral Data File',
+    u'MAGCAM': 100.0,
+    u'NCOLUMNS': 1.0,
+    u'NPOINTS': 20.0,
+    u'OFFSET': 520.13,
+    u'OPERMODE': u'IMAG',
+    u'OWNER': u'EMSA/MAS TASK FORCE',
+    u'PROBECUR -nA': 12.345,
+    u'SIGNALTYPE': u'ELS',
+    u'THICKNESS-nm': 50.0,
+    u'TIME': u'12:00',
+    u'TITLE': u'NIO EELS OK SHELL',
+    u'VERSION': u'1.0',
+    u'XLABEL': u'Energy',
+    u'XPERCHAN': 3.1,
+    u'XUNITS': u'eV',
+    u'YLABEL': u'Counts',
+    u'YUNITS': u'Intensity'}
+
+
+class Example1:
+
+    def test_data(self):
+        nt.assert_equal(
+            [4066.0,
+             3996.0,
+             3932.0,
+             3923.0,
+             5602.0,
+             5288.0,
+             7234.0,
+             7809.0,
+             4710.0,
+             5015.0,
+             4366.0,
+             4524.0,
+             4832.0,
+             5474.0,
+             5718.0,
+             5034.0,
+             4651.0,
+             4613.0,
+             4637.0,
+             4429.0,
+             4217.0], self.s.data.tolist())
+
+    def test_original_metadata(self):
+        nt.assert_equal(
+            example1_original_metadata,
+            self.s.original_metadata.as_dictionary())
+
+
+class TestExample1_10(Example1):
+
+    def setUp(self):
+        self.s = load(os.path.join(
+            my_path,
+            "hdf5_files",
+            "example1_v1.0.hdf5"))
+
+
+class TestExample1_11(Example1):
+
+    def setUp(self):
+        self.s = load(os.path.join(
+            my_path,
+            "hdf5_files",
+            "example1_v1.1.hdf5"))
+
+
+class TestExample1_12(Example1):
+
+    def setUp(self):
+        self.s = load(os.path.join(
+            my_path,
+            "hdf5_files",
+            "example1_v1.2.hdf5"))
+
+    def test_date(self):
+        nt.assert_equal(
+            self.s.metadata.General.date,
+            datetime.date(
+                1991,
+                10,
+                1))
+
+    def test_time(self):
+        nt.assert_equal(self.s.metadata.General.time, datetime.time(12, 0))
+
+
+class TestLoadingNewSavedMetadata:
+
+    def setUp(self):
+        self.s = load(os.path.join(
+            my_path,
+            "hdf5_files",
+            "with_lists_etc.hdf5"))
+
+    def test_signal_inside(self):
+        nt.assert_true(
+            np.all(
+                self.s.data == self.s.metadata.Signal.Noise_properties.variance.data))
+
+    def test_empty_things(self):
+        nt.assert_equal(self.s.metadata.test.empty_list, [])
+        nt.assert_equal(self.s.metadata.test.empty_tuple, ())
+
+    def test_simple_things(self):
+        nt.assert_equal(self.s.metadata.test.list, [42])
+        nt.assert_equal(self.s.metadata.test.tuple, (1, 2))
+
+    def test_inside_things(self):
+        nt.assert_equal(
+            self.s.metadata.test.list_inside_list, [
+                42, 137, [
+                    0, 1]])
+        nt.assert_equal(self.s.metadata.test.list_inside_tuple, (137, [42, 0]))
+        nt.assert_equal(
+            self.s.metadata.test.tuple_inside_tuple, (137, (123, 44)))
+        nt.assert_equal(
+            self.s.metadata.test.tuple_inside_list, [
+                137, (123, 44)])
+
+    def test_binary_string(self):
+        import marshal
+        import types
+        f = types.FunctionType(
+            marshal.loads(
+                self.s.metadata.test.binary_string),
+            globals())
+        nt.assert_equal(f(3.5), 4.5)
+
+
+class TestSavingMetadataContainers:
+
+    def setUp(self):
+        self.s = Signal([0.1])
+
+    def test_save_unicode(self):
+        s = self.s
+        s.metadata.set_item('test', [u'a', u'b', u'\u6f22\u5b57'])
+        s.save('tmp.hdf5', overwrite=True)
+        l = load('tmp.hdf5')
+        nt.assert_is_instance(l.metadata.test[0], unicode)
+        nt.assert_is_instance(l.metadata.test[1], unicode)
+        nt.assert_is_instance(l.metadata.test[2], unicode)
+        nt.assert_equal(l.metadata.test[2], u'\u6f22\u5b57')
+
+    @nt.timed(1.0)
+    def test_save_long_list(self):
+        s = self.s
+        s.metadata.set_item('long_list', range(10000))
+        s.save('tmp.hdf5', overwrite=True)
+
+    def test_numpy_only_inner_lists(self):
+        s = self.s
+        s.metadata.set_item('test', [[1., 2], ('3', 4)])
+        s.save('tmp.hdf5', overwrite=True)
+        l = load('tmp.hdf5')
+        nt.assert_is_instance(l.metadata.test, list)
+        nt.assert_is_instance(l.metadata.test[0], list)
+        nt.assert_is_instance(l.metadata.test[1], tuple)
+
+    def test_numpy_general_type(self):
+        s = self.s
+        s.metadata.set_item('test', [[1., 2], ['3', 4]])
+        s.save('tmp.hdf5', overwrite=True)
+        l = load('tmp.hdf5')
+        nt.assert_is_instance(l.metadata.test[0][0], float)
+        nt.assert_is_instance(l.metadata.test[0][1], float)
+        nt.assert_is_instance(l.metadata.test[1][0], basestring)
+        nt.assert_is_instance(l.metadata.test[1][1], basestring)
+
+    def test_general_type_not_working(self):
+        s = self.s
+        s.metadata.set_item('test', (Signal([1]), 0.1, 'test_string'))
+        s.save('tmp.hdf5', overwrite=True)
+        l = load('tmp.hdf5')
+        nt.assert_is_instance(l.metadata.test, tuple)
+        nt.assert_is_instance(l.metadata.test[0], Signal)
+        nt.assert_is_instance(l.metadata.test[1], float)
+        nt.assert_is_instance(l.metadata.test[2], unicode)
+
+    def tearDown(self):
+        remove('tmp.hdf5')
+
+
+def test_none_metadata():
+    s = load(os.path.join(
+        my_path,
+        "hdf5_files",
+        "none_metadata.hdf5"))
+    nt.assert_is(s.metadata.should_be_None, None)
+
+
+def test_rgba16():
+    s = load(os.path.join(
+        my_path,
+        "hdf5_files",
+        "test_rgba16.hdf5"))
+    data = np.load(os.path.join(
+        my_path,
+        "npy_files",
+        "test_rgba16.npy"))
+    nt.assert_true((s.data == data).all())
---
+++ packages/hyperspy/hyperspy-0.8.3-1.tar.gz/hyperspy-0.8.3/hyperspy/tests/mva/test_decomposition.py
@@ -0,0 +1,194 @@
+import numpy as np
+
+import nose.tools
+from nose.tools import assert_true, raises
+from hyperspy import signals
+
+
+class TestNdAxes:
+
+    def setUp(self):
+        # Create three signals with dimensions:
+        # s1 : <Signal, title: , dimensions: (4, 3, 2|2, 3)>
+        # s2 : <Signal, title: , dimensions: (2, 3|4, 3, 2)>
+        # s12 : <Signal, title: , dimensions: (2, 3|4, 3, 2)>
+        # Where s12 data is transposed in respect to s2
+        dc1 = np.random.random((2, 3, 4, 3, 2))
+        dc2 = np.rollaxis(np.rollaxis(dc1, -1), -1)
+        s1 = signals.Signal(dc1.copy())
+        s2 = signals.Signal(dc2)
+        s12 = signals.Signal(dc1.copy())
+        for i, axis in enumerate(s1.axes_manager._axes):
+            if i < 3:
+                axis.navigate = True
+            else:
+                axis.navigate = False
+        for i, axis in enumerate(s2.axes_manager._axes):
+            if i < 2:
+                axis.navigate = True
+            else:
+                axis.navigate = False
+        for i, axis in enumerate(s12.axes_manager._axes):
+            if i < 3:
+                axis.navigate = False
+            else:
+                axis.navigate = True
+        self.s1 = s1
+        self.s2 = s2
+        self.s12 = s12
+
+    def test_consistensy(self):
+        s1 = self.s1
+        s2 = self.s2
+        s12 = self.s12
+        s1.decomposition()
+        s2.decomposition()
+        s12.decomposition()
+        np.testing.assert_array_almost_equal(s2.learning_results.loadings,
+                                             s12.learning_results.loadings)
+        np.testing.assert_array_almost_equal(s2.learning_results.factors,
+                                             s12.learning_results.factors)
+        np.testing.assert_array_almost_equal(s1.learning_results.loadings,
+                                             s2.learning_results.factors)
+        np.testing.assert_array_almost_equal(s1.learning_results.factors,
+                                             s2.learning_results.loadings)
+
+    def test_consistensy_poissonian(self):
+        s1 = self.s1
+        s1n000 = self.s1.inav[0, 0, 0]
+        s2 = self.s2
+        s12 = self.s12
+        s1.decomposition(normalize_poissonian_noise=True)
+        s2.decomposition(normalize_poissonian_noise=True)
+        s12.decomposition(normalize_poissonian_noise=True)
+        np.testing.assert_array_almost_equal(s2.learning_results.loadings,
+                                             s12.learning_results.loadings)
+        np.testing.assert_array_almost_equal(s2.learning_results.factors,
+                                             s12.learning_results.factors)
+        np.testing.assert_array_almost_equal(s1.learning_results.loadings,
+                                             s2.learning_results.factors)
+        np.testing.assert_array_almost_equal(s1.learning_results.factors,
+                                             s2.learning_results.loadings)
+        # Check that views of the data don't change. See #871
+        np.testing.assert_array_equal(s1.inav[0, 0, 0].data, s1n000.data)
+
+
+class TestGetExplainedVarinaceRatio:
+
+    def setUp(self):
+        s = signals.Signal(np.empty(1))
+        self.s = s
+
+    def test_data(self):
+        self.s.learning_results.explained_variance_ratio = np.asarray([2, 4])
+        np.testing.assert_array_equal(
+            self.s.get_explained_variance_ratio().data,
+            np.asarray([2, 4]))
+
+    @raises(AttributeError)
+    def test_no_evr(self):
+        self.s.get_explained_variance_ratio()
+
+
+class TestReverseDecompositionComponent:
+
+    def setUp(self):
+        s = signals.Signal(np.zeros(1))
+        self.factors = np.ones([2, 3])
+        self.loadings = np.ones([2, 3])
+        s.learning_results.factors = self.factors.copy()
+        s.learning_results.loadings = self.loadings.copy()
+        self.s = s
+
+    def test_reversal_factors_one_component_reversed(self):
+        self.s.reverse_decomposition_component(0)
+        assert_true((self.s.learning_results.factors[:, 0] ==
+                     self.factors[:, 0] * -1).all())
+
+    def test_reversal_loadings_one_component_reversed(self):
+        self.s.reverse_decomposition_component(0)
+        assert_true((self.s.learning_results.loadings[:, 0] ==
+                     self.loadings[:, 0] * -1).all())
+
+    def test_reversal_factors_one_component_not_reversed(self):
+        self.s.reverse_decomposition_component(0)
+        assert_true((self.s.learning_results.factors[:, 1:] ==
+                     self.factors[:, 1:]).all())
+
+    def test_reversal_loadings_one_component_not_reversed(self):
+        self.s.reverse_decomposition_component(0)
+        assert_true((self.s.learning_results.loadings[:, 1:] ==
+                     self.loadings[:, 1:]).all())
+
+    def test_reversal_factors_multiple_components_reversed(self):
+        self.s.reverse_decomposition_component((0, 2))
+        assert_true((self.s.learning_results.factors[:, (0, 2)] ==
+                     self.factors[:, (0, 2)] * -1).all())
+
+    def test_reversal_loadings_multiple_components_reversed(self):
+        self.s.reverse_decomposition_component((0, 2))
+        assert_true((self.s.learning_results.loadings[:, (0, 2)] ==
+                     self.loadings[:, (0, 2)] * -1).all())
+
+    def test_reversal_factors_multiple_components_not_reversed(self):
+        self.s.reverse_decomposition_component((0, 2))
+        assert_true((self.s.learning_results.factors[:, 1] ==
+                     self.factors[:, 1]).all())
+
+    def test_reversal_loadings_multiple_components_not_reversed(self):
+        self.s.reverse_decomposition_component((0, 2))
+        assert_true((self.s.learning_results.loadings[:, 1] ==
+                     self.loadings[:, 1]).all())
+
+
+class TestNormalizeComponents():
+
+    def setUp(self):
+        s = signals.Signal(np.zeros(1))
+        self.factors = np.ones([2, 3])
+        self.loadings = np.ones([2, 3])
+        s.learning_results.factors = self.factors.copy()
+        s.learning_results.loadings = self.loadings.copy()
+        s.learning_results.bss_factors = self.factors.copy()
+        s.learning_results.bss_loadings = self.loadings.copy()
+        self.s = s
+
+    def test_normalize_bss_factors(self):
+        s = self.s
+        s.normalize_bss_components(target="factors",
+                                   function=np.sum)
+        nose.tools.assert_true(
+            (s.learning_results.bss_factors == self.factors / 2.).all())
+        nose.tools.assert_true(
+            (s.learning_results.bss_loadings == self.loadings * 2.).all())
+
+    def test_normalize_bss_loadings(self):
+        s = self.s
+        s.normalize_bss_components(target="loadings",
+                                   function=np.sum)
+        nose.tools.assert_true(
+            (s.learning_results.bss_factors == self.factors * 2.).all())
+        nose.tools.assert_true(
+            (s.learning_results.bss_loadings == self.loadings / 2.).all())
+
+    def test_normalize_decomposition_factors(self):
+        s = self.s
+        s.normalize_decomposition_components(target="factors",
+                                             function=np.sum)
+        nose.tools.assert_true(
+            (s.learning_results.factors ==
+             self.factors / 2.).all())
+        nose.tools.assert_true(
+            (s.learning_results.loadings ==
+             self.loadings * 2.).all())
+
+    def test_normalize_decomposition_loadings(self):
+        s = self.s
+        s.normalize_decomposition_components(target="loadings",
+                                             function=np.sum)
+        nose.tools.assert_true(
+            (s.learning_results.factors ==
+             self.factors * 2.).all())
+        nose.tools.assert_true(
+            (s.learning_results.loadings ==
+             self.loadings / 2.).all())
