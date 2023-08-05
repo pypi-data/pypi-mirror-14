2bf38d6159f4a7db82722d87ee707bf2a76fa251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package py-poloniex/0.2/py-poloniex-0.2.tar.gz

---
+++ packages/py-poloniex/py-poloniex-0.2.tar.gz/py-poloniex-0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(name='py-poloniex',
+                 version='0.2',
+                 description='Python Poloniex API Wrapper',
+                 long_description=open('README.md').read().strip(),
+                 author='Thiago Fernandes Macedo',
+                 author_email='thiago@internetbudi.com.br',
+                 url='https://github.com/thiagof/py-poloniex',
+                 py_modules=['poloniex'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='poloniex bitcoin btc api exchange',
+                 classifiers=[])
