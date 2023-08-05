3ad2191ab6f624179c37c08fdc205844f7ae9702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package kickscraper/0.1.4/kickscraper-0.1.4.tar.gz

---
+++ packages/kickscraper/kickscraper-0.1.4.tar.gz/kickscraper-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='kickscraper',
+    version='0.1.4',
+    description='A scraper for Crowdfunding projects',
+    url='https://github.com/argaen/kickscraper',
+
+    author='argaen',
+    license='MIT',
+
+    keywords=['kickstarter api crowdfunding projects'],
+
+    packages=['kickscraper'],
+
+    install_requires=['requests>=2.9.1', 'beautifulsoup4>=4.4.1'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+
+)
