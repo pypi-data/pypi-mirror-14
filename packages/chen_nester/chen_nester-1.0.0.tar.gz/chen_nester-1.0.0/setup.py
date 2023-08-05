129391d8e9dda37275e5846f009f19456e0238c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package chen_nester/1.0.0/chen_nester-1.0.0.tar.gz

---
+++ packages/chen_nester/chen_nester-1.0.0.tar.gz/chen_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'chen_nester',
+        version = '1.0.0',
+        py_modules = ['chen_nester'],
+        author = 'ryan.chen',
+        author_email = '75730263@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
