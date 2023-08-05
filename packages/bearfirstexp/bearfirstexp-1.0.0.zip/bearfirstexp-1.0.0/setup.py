8b5a09c0ff3f21c4163376eae1281b7bb63c2410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:37 +0000

    Add package bearfirstexp/1.0.0/bearfirstexp-1.0.0.zip

---
+++ packages/bearfirstexp/bearfirstexp-1.0.0.zip/bearfirstexp-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='bearfirstexp',
+        version='1.0.0',
+        py_modules=['bearfirstexp'],
+        author='hfpython',
+        author_email='hfpython@headfirstrlabs.com',
+        url='http://www.headfirsttlabs.com',
+        description='A simple printer of nested lists',
+    )
