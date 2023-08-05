1639775c913b40fcddaaa23a4bc99f6d817f72db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:55 +0000

    Add package module_1st/1.3.0/module_1st-1.3.0.zip

---
+++ packages/module_1st/module_1st-1.3.0.zip/module_1st-1.3.0/module_1st.py
@@ -0,0 +1,14 @@
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+        	if indent:
+        		for tab_stop in range(level):
+        			print("\t", end='')
+        		print (each_item)
+
+
+list_test = ['a',1,['b'],'c',['d',['f']],'g']
+
+print_lol(list_test,1)
---
+++ packages/module_1st/module_1st-1.3.0.zip/module_1st-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'module_1st',
+    version = '1.3.0',
+    py_modules = ['module_1st'],
+    author = 'mars',
+    author_email = 'yuanhao310@163.com',
+    url = 'http://www.163.com',
+    description = 'A simple printer of nested lists_MARSCHEUNG',
+    )
