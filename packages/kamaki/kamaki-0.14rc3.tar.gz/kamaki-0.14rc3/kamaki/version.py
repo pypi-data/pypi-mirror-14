2e733dca1fdd0d6a115cd98af6cf209b6943ab57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package kamaki/0.14rc3/kamaki-0.14rc3.tar.gz

---
+++ packages/kamaki/kamaki-0.14rc3.tar.gz/kamaki-0.14rc3/kamaki/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.14rc3"
+__version_vcs_info__ = {
+    'branch': 'release-0.14',
+    'revid': 'ad20ae9',
+    'revno': 2583}
+__version_user_email__ = "gkorf@grnet.gr"
+__version_user_name__ = "Giorgos Korfiatis"
