26852d53cfa61b5ba53ac8e3daaf142b57cc87f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:31 +0000

    Add package featureflow/0.2/featureflow-0.2.tar.gz

---
+++ packages/featureflow/featureflow-0.2.tar.gz/featureflow-0.2/README.md
@@ -0,0 +1,4 @@
+# featureflow
+
+featureflow is a python library that allows users to build feature extraction pipelines in a declarative way, and 
+store the results for later use
---
+++ packages/featureflow/featureflow-0.2.tar.gz/featureflow-0.2/featureflow/encoder.py
@@ -0,0 +1,61 @@
+import json
+from extractor import Node, Aggregator
+import bz2
+from cPickle import dumps
+
+
+# TODO: Switch to using default json module, this simplejson import is dumb
+class IdentityEncoder(Node):
+    content_type = 'application/octet-stream'
+
+    def __init__(self, needs=None):
+        super(IdentityEncoder, self).__init__(needs=needs)
+
+    def _enqueue(self, data, pusher):
+        self._cache = data if data else ''
+
+
+class TextEncoder(IdentityEncoder):
+    content_type = 'text/plain'
+
+    def __init__(self, needs=None):
+        super(TextEncoder, self).__init__(needs=needs)
+
+
+class JSONEncoder(Aggregator, Node):
+    content_type = 'application/json'
+
+    def __init__(self, needs=None):
+        super(JSONEncoder, self).__init__(needs=needs)
+
+    def _process(self, data):
+        yield json.dumps(data)
+
+
+class PickleEncoder(Aggregator, Node):
+    content_type = 'application/octet-stream'
+
+    def __init__(self, needs=None):
+        super(PickleEncoder, self).__init__(needs=needs)
+
+    def _process(self, data):
+        yield dumps(data)
+
+
+class BZ2Encoder(Node):
+    content_type = 'application/octet-stream'
+
+    def __init__(self, needs=None):
+        super(BZ2Encoder, self).__init__(needs=needs)
+        self._compressor = None
+
+    def _finalize(self, pusher):
+        self._cache = ''
+
+    def _process(self, data):
+        if self._compressor is None:
+            self._compressor = bz2.BZ2Compressor()
+        compressed = self._compressor.compress(data)
+        if compressed: yield compressed
+        if self._finalized:
+            yield self._compressor.flush()
