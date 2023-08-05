2a9dc77ac03d3d330dae65bfdf080a59c3dc183c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package auxlib/0.0.32/auxlib-0.0.32-py2.py3-none-any.whl

---
+++ packages/auxlib/auxlib-0.0.32-py2.py3-none-any.whl/auxlib/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""auxiliary library to the python standard library"""
+from __future__ import absolute_import, division, print_function
+
+from .packaging import BuildPyCommand, SDistCommand, Tox, get_version
+
+__all__ = [
+    "__title__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+    "__summary__", "__homepage__",
+    "BuildPyCommand", "SDistCommand", "Tox", "get_version",
+]
+
+__version__ = "0.0.32"
+
+__title__ = "auxlib"
+__author__ = 'Kale Franz'
+__email__ = 'kale@franz.io'
+__homepage__ = 'https://github.com/kalefranz/auxlib'
+__license__ = "ISC"
+__copyright__ = "(c) 2015 Kale Franz. All rights reserved."
+__summary__ = __doc__
