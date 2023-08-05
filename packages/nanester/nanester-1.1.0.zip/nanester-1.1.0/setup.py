666d3078abbe42741c56323004f95b588a5df9b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package nanester/1.1.0/nanester-1.1.0.zip

---
+++ packages/nanester/nanester-1.1.0.zip/nanester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nanester',
+    version='1.1.0',
+    py_modules=['nanester'],
+    author='NA Personal Learning Studio',
+    author_email='neveragain1995@163.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nestedlists',
+
+)
