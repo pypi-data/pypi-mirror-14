a9d1925048fce0ab909915265148545818d7e364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package gdsync/0.0.1/gdsync-0.0.1.tar.gz

---
+++ packages/gdsync/gdsync-0.0.1.tar.gz/gdsync-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gdsync',
+    version='0.0.1',
+    description='Google Drive Synchronizer',
+    author='Masato Bito',
+    author_email='bito_m@uuum.jp',
+    url='https://github.com/uuum/gdsync',
+    license='MIT',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
