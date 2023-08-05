359bdd95b59f2f45625835e08b60767754f18375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package zihin_nester/1.3.0/zihin_nester-1.3.0.zip

---
+++ packages/zihin_nester/zihin_nester-1.3.0.zip/zihin_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='zihin_nester',
+    version='1.3.0',
+    py_modules=['nester'],
+    author='zihin',
+    author_email='zihin@outlook.com',
+    url='http://www.baidu.com',
+    description='This is my first test python program',
+    )
