ef08c62e27fac533a9e72aa4a1564d5428e9342c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:41 +0000

    Add package basho-erlastic/2.1.0/basho-erlastic-2.1.0.tar.gz

---
+++ packages/basho-erlastic/basho-erlastic-2.1.0.tar.gz/basho-erlastic-2.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import setuptools
+
+from erlastic import __version__ as version
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    with open('README.rst', 'w') as f:
+        f.write(long_description)
+except(IOError, ImportError):
+    with open('README.md') as f:
+        long_description = f.read()
+
+setuptools.setup(
+    name = 'basho-erlastic',
+    version = version,
+    description = 'Erlastic',
+    long_description=long_description,
+    author = 'Samuel Stauffer, Basho Technologies',
+    author_email = 'clients@basho.com',
+    url = 'http://github.com/basho/python-erlastic',
+    packages = ['erlastic'],
+    install_requires=['six'],
+    test_suite='tests',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
