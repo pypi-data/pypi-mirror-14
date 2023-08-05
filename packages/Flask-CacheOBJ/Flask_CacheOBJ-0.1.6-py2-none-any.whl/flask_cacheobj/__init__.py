456b9eb26f95079650adc1f738fec5a72f5c8704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package Flask-CacheOBJ/0.1.6/Flask_CacheOBJ-0.1.6-py2-none-any.whl

---
+++ packages/Flask-CacheOBJ/Flask_CacheOBJ-0.1.6-py2-none-any.whl/flask_cacheobj/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+flask_cacheobj
+~~~~~~~~~~~~~~
+
+Flask-CacheOBJ Extension
+
+:copyright: (c) 2016 Liwushuo Inc.
+:license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+from .api import CacheOBJ
+FlaskCacheOBJ = CacheOBJ
+
+from .msgpackable import Msgpackable
+
+from .cache import (
+    delete_cache,
+
+    cache_obj,
+
+    cache_hash,
+    hash_del,
+
+    cache_list,
+    list_add,
+    list_rem,
+    list_len,
+
+    exists,
+
+    set_add,
+    set_rem,
+    set_len,
+
+    cache_counter,
+    inc_counter,
+    dec_counter,
+    get_counter,
+    set_counter,
+)
+from .consts import __version__
