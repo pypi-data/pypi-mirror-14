95e6237f95f035f1c33683062672bdf198b1aeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package thutils/0.1.19/thutils-0.1.19.tar.gz

---
+++ packages/thutils/thutils-0.1.19.tar.gz/thutils-0.1.19/thutils/__init__.py
@@ -0,0 +1,51 @@
+
+import thutils.cache
+import thutils.common
+import thutils.gfile
+import thutils.gtime
+import thutils.loader
+import thutils.logger
+import thutils.main
+import thutils.option
+import thutils.subprocwrapper
+
+
+LIB_TMP_DIR = "/tmp/__golib__"
+
+
+class NotInstallError(Exception):
+    pass
+
+
+def initialize_library(
+        program_filename, options, output_dir_path="."):
+
+    import socket
+
+    if output_dir_path == ".":
+        if hasattr(options, "output_dir"):
+            output_dir_path = options.output_dir
+
+    dry_run = False
+    try:
+        dry_run = options.dry_run
+    except AttributeError:
+        pass
+
+    return_value = logger.logger.initialize(
+        program_filename,
+        dry_run=dry_run,
+        stdout_log_level=options.log_level,
+        print_stack_trace=options.stacktrace,
+        with_no_log=options.with_no_log,
+        output_dir_path=output_dir_path,
+    )
+
+    gfile.FileManager.initialize(dry_run)
+    cache.CommandCache.initialize()
+
+    logger.logger.debug(
+        "complete initialize library: hostname=" + socket.gethostname())
+    logger.logger.debug("options: " + common.dump_dict(options.__dict__))
+
+    return return_value
