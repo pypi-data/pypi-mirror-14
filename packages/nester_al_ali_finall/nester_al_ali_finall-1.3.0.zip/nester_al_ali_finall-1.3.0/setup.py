528421f10d06abde057d89c8f686ae17083f0f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package nester_al_ali_finall/1.3.0/nester_al_ali_finall-1.3.0.zip

---
+++ packages/nester_al_ali_finall/nester_al_ali_finall-1.3.0.zip/nester_al_ali_finall-1.3.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name = 'nester_al_ali_finall',
+	version = '1.3.0',
+	py_modules = ['nester'],
+	author = 'alisowska',
+	author_email = 'anna-lisowska@outlook.com',
+	description = 'A simple printer of nested list with service of indentation'
+	)
