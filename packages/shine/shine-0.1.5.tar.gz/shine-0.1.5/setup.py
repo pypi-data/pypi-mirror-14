f556adbb0f552fd9db9be8d28f6acd6088be8ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package shine/0.1.5/shine-0.1.5.tar.gz

---
+++ packages/shine/shine-0.1.5.tar.gz/shine-0.1.5/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="shine",
+    version='0.1.5',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    scripts=['shine/bin/shinectl.py'],
+    install_requires=['colorlog'],
+    url="https://github.com/dantezhu/shine",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="shine",
+)
---
+++ packages/shine/shine-0.1.5.tar.gz/shine-0.1.5/shine/share/constants.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+NAME = 'shine'
+
+# 系统返回码
+RET_INVALID_CMD = -10000
+RET_INTERNAL = -10001
+
+# 命令字
+CMD_CLIENT_REQ              = 10  # 透传client请求
+CMD_CLIENT_CREATED          = 15  # 客户端连接建立
+CMD_CLIENT_CLOSED           = 20  # 客户端连接被关闭
+
+CMD_WRITE_TO_WORKER         = 100 # trigger触发请求
+
+CMD_WORKER_ASK_FOR_JOB      = 210 # 请求任务
+
+CMD_WRITE_TO_CLIENT         = 220 # 回应
+CMD_WRITE_TO_USERS          = 230 # 主动下发
+CMD_CLOSE_CLIENT            = 240 # 关闭客户端(client_id为判断)
+CMD_CLOSE_USERS             = 250 # 关闭多个客户端
+CMD_LOGIN_CLIENT            = 260 # 登录用户
+CMD_LOGOUT_CLIENT           = 270 # 登出用户
+
+
+# worker的env
+WORKER_ENV_KEY = 'SHINE_WORKER'
+
+
+# 默认config参数
+DEFAULT_CONFIG = {
+    # 进程名
+    'NAME': NAME,
+
+    # gateway, worker, forwarder 需要
+    'DEBUG': False,
+
+    # gateway 需要
+    'GATEWAY_OUTER_HOST': None,
+    # gateway 需要
+    'GATEWAY_OUTER_PORT': None,
+    # gateway 的backlog
+    'GATEWAY_BACKLOG': 256,
+
+    # gateway, worker 需要
+    'GATEWAY_INNER_ADDRESS_LIST': None,
+
+    # worker, forwarder 需要
+    'FORWARDER_INPUT_ADDRESS_LIST': None,
+    # gateway, forwarder 需要
+    'FORWARDER_OUTPUT_ADDRESS_LIST': None,
+
+    # gateway, forwarder 需要
+    'REDIS_URL': None,  # 用来存储用户ID->proc_id的映射 以及 proc_id的集合
+    # gateway, forwarder 需要
+    'REDIS_USER_KEY_PREFIX': NAME + ':user:',  # 存储的user键前缀
+    # gateway 需要
+    'REDIS_USER_MAXAGE': None,  # user最长存储的秒数，因为有可能有些用户的数据没有正常清空
+    # gateway, forwarder 需要
+    'REDIS_PROCS_KEY': NAME + ':procs',  # 存储proc_id的集合
+}
