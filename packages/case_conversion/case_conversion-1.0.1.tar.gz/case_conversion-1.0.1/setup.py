084aeeb8230a51d94e0ec5bf58c9a2a1caeb568b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package case_conversion/1.0.1/case_conversion-1.0.1.tar.gz

---
+++ packages/case_conversion/case_conversion-1.0.1.tar.gz/case_conversion-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='case_conversion',
+  packages=['case_conversion'],
+  version='1.0.1',
+  description='convert between different types of cases',
+  license='MIT',
+  author='Alejandro Frias',
+  author_email='joker454@gmail.com',
+  url='https://github.com/AlejandroFrias/case-conversion',
+  download_url='https://github.com/AlejandroFrias/case-conversion/tarball/1.0.1',
+  keywords=['case', 'convert', 'camel', 'pascal', 'snake'],
+  classifiers=[],
+)
