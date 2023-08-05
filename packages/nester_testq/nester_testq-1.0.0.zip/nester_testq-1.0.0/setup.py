90fc49036b68bdd8ca0383700d2d9da6aa112810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package nester_testq/1.0.0/nester_testq-1.0.0.zip

---
+++ packages/nester_testq/nester_testq-1.0.0.zip/nester_testq-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester_testq',
+		version      = '1.0.0',
+		py_modules   = ['nester_testq'],
+		author       = 'Miew',
+		author_email = 'miew@miew.com',
+		url          = 'http://www.miew@miew.com',
+		description  = 'A simple printer of nested lists',
+    )
