e9c64e75bff140162b717c0a8c15cd7d191c6357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package django-autologin/0.0.5/django-autologin-0.0.5.tar.gz

---
+++ packages/django-autologin/django-autologin-0.0.5.tar.gz/django-autologin-0.0.5/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-autologin',
+
+    url="https://chris-lamb.co.uk/projects/django-autologin",
+    version='0.0.5',
+    description="Token generator and processor to provide automatic login links for users",
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
+    packages=find_packages(),
+
+    install_requires=(
+        'Django>=1.8',
+    ),
+)
