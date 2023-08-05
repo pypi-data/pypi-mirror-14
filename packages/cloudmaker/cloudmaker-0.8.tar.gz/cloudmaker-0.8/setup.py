ecc332e1452e3f919f30150e4639f868e14c0d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package cloudmaker/0.8/cloudmaker-0.8.tar.gz

---
+++ packages/cloudmaker/cloudmaker-0.8.tar.gz/cloudmaker-0.8/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core  import setup
+
+setup(name='cloudmaker',
+      version='0.8',
+      description='A provisioning tool for Digital Ocean cloud',
+      author='Randy May',
+      author_email='randy@mathysphere.com',
+      url='https://github.com/wrmay/cloudmaker',
+      packages=['cloudmaker'],
+      package_dir={'' : 'src/packages'},
+      scripts=['src/scripts/cloudmaker'],
+      requires=['Jinja2']
+     )
