c2d7ec5e18616ec87c41f52053261af06eb7ae23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package streampie/0.1/streampie-0.1.tar.gz

---
+++ packages/streampie/streampie-0.1.tar.gz/streampie-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+   name = "streampie",
+   py_modules = ["streampie"],
+   version = '0.1',
+   description = 'Streampie - a simple, parallel stream processing library',
+   author = 'Luka Malisa',
+   author_email = 'luka.malisha@gmail.com',
+   url = 'https://github.com/malisal/streampie',
+   keywords = ['stream', 'parallel', 'distributed'],
+   platforms=['any'],
+   classifiers = [],
+)
+
