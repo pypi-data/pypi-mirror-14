aeebe02da9ac30f0ab1fe7659736e9fdb5803a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package sedkit/0.1.0/SEDkit-0.1.0.tar.gz

---
+++ packages/sedkit/SEDkit-0.1.0.tar.gz/SEDkit-0.1.0/SEDkit/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# from .mcmc_fit import *
+from .SEDs import *
+from .syn_phot import *
+from .interact import *
+from .utilities import *
+
+__version__ = "0.1.0"
