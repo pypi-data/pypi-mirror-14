37d0b124f106d95d87b15656fb4873cfa9c0568f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package pycorm/0.2.13/pycorm-0.2.13-py2.py3-none-any.whl

---
+++ packages/pycorm/pycorm-0.2.13-py2.py3-none-any.whl/pycorm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Johannes Valbjørn'
+__email__ = 'johannes.valbjorn@gmail.com'
+__version__ = '0.2.13'
+
+from exceptions import SchemaValidationError, InheritanceNotSupportedError
+from pycorm import BaseModel, StringField, NumberField
