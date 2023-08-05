3acc0a53b4c9d8299ed29535b62d1dafe31f7a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package nester_xcc/1.2.0/nester_xcc-1.2.0.zip

---
+++ packages/nester_xcc/nester_xcc-1.2.0.zip/nester_xcc-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils. core import setup
+setup(
+    name ='nester_xcc',
+    version='1.2.0',
+    py_modules=['nester_xcc'],
+    author='xcc',
+    author_email='984827641@qq.com',
+    url='http://www.cnblogs.com/apple2016/',
+    description='嵌套列表的打印举例',
+    )
