b06b7106844568276f29521d7316bded18c4b5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:22 +0000

    Add package li/0.1.2/li-0.1.2-py2.py3-none-any.whl

---
+++ packages/li/li-0.1.2-py2.py3-none-any.whl/li/errors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Error classes."""
+
+import ecstasy
+
+class LicenseError(Exception):
+    """Any error thrown by the license utility."""
+
+    ERROR = ecstasy.beautify('<Error>: ', ecstasy.Color.Red)
+
+    def __init__(self, message):
+        """
+        Initializes the Exception super class.
+
+        A LicenseError may be thrown in any situation where the license utility
+        is no longer able to function normally. Such a situation may be, for
+        example, a cache miss or a connection timeout.
+        """
+        self.message = LicenseError.ERROR + message
+
+        super(LicenseError, self).__init__(self.message)
