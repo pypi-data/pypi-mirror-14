00f5499920ba4fdf92b1ddbbcbc869fc0982a97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package listprinttest/1.4.1/listprinttest-1.4.1.tar.gz

---
+++ packages/listprinttest/listprinttest-1.4.1.tar.gz/listprinttest-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'listprinttest',
+        version     = '1.4.1',
+        py_modules  = 'nester',
+        author      = 'yakumo17',
+        author_email= 'wo409697458@gmail.com',
+        url         = 'http://114.215.84.13',
+        description = 'A simple printer of nested lists',
+    )
