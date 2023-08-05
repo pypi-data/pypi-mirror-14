b39e5549dfec7959e5d7647b10d54c8eef6d006e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:14 +0000

    Add package gubser/0.5/gubser-0.5.tar.gz

---
+++ packages/gubser/gubser-0.5.tar.gz/gubser-0.5/setup.py
@@ -0,0 +1,19 @@
+#from distutils.core import setup
+from setuptools import find_packages,setup
+
+setup(
+  name = 'gubser',
+  packages = ['gubser'], # this must be the same as the name above
+  version = '0.5',
+  setup_requires=["cffi>=1.0.0"],
+  install_requires=["cffi>=1.0.0"],
+  #packages=find_packages(exclude=["cffi_build"]),
+  cffi_modules=["gubser/gubser_cffi.py:ffi"],
+  description = 'Gubser solution for ideal hydrodynamics, 2+1D viscous hydrodynamics ',
+  author = 'Long-Gang Pang',
+  author_email = 'lgpang@qq.com',
+  url = 'https://github.com/snowhitiger/gubser',
+  download_url = 'https://github.com/snowhitiger/gubser/tarball/0.5',
+  keywords = ['gubser', '2nd order', 'viscous', 'python'],
+  classifiers = [],
+)
