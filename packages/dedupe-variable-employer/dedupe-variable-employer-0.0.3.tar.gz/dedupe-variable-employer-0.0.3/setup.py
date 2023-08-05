36e12f1f0a9f9db9c3d77bacaa88d8dfe9a13e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:48 +0000

    Add package dedupe-variable-employer/0.0.3/dedupe-variable-employer-0.0.3.tar.gz

---
+++ packages/dedupe-variable-employer/dedupe-variable-employer-0.0.3.tar.gz/dedupe-variable-employer-0.0.3/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+
+setup(
+    name='dedupe-variable-employer',
+    url='https://github.com/datamade/dedupe-variables-employer',
+    version='0.0.3',
+    description='Employer variable type for dedupe',
+    packages=['dedupe.variables'],
+    install_requires=['companyparser==0.2',
+                      'parseratorvariable >= 0.0.10',
+                      'simplecosine'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
+    )
