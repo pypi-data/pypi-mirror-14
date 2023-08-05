95b0633037e2602ffaca040b45c6de90a969429d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:40 +0000

    Add package desolver/1.1.13/DESolver-1.1.13.zip

---
+++ packages/desolver/DESolver-1.1.13.zip/DESolver-1.1.13/desolver/__init__.py
@@ -0,0 +1,7 @@
+from .differentialsystem import *
+
+__all__ = ["odesystem", "bisectroot", "seval", "explicitrk4",
+           "explicitmidpoint", "implicitmidpoint", "heuns", "backeuler",
+           "foreuler", "eulertrap", "adaptiveheuneuler",
+           "sympforeuler", "init_namespace", "VariableMissing",
+           "LengthError", "warning", "odesystem"]
