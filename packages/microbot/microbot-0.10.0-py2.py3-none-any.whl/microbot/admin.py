02c958cd02347af6c5074aec68705a5765ae443d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:51 +0000

    Add package microbot/0.10.0/microbot-0.10.0-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-0.10.0-py2.py3-none-any.whl/microbot/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from microbot.models import Message, Chat, Update, User, Bot, Handler, EnvironmentVar, Request, Response, Hook, \
+    UrlParam, HeaderParam, Recipient
+
+admin.site.register(Message)
+admin.site.register(Chat)
+admin.site.register(User)
+admin.site.register(Update)
+admin.site.register(Bot)
+admin.site.register(Handler)
+admin.site.register(Request)
+admin.site.register(EnvironmentVar)
+admin.site.register(Response)
+admin.site.register(Hook)
+admin.site.register(UrlParam)
+admin.site.register(HeaderParam)
+admin.site.register(Recipient)
