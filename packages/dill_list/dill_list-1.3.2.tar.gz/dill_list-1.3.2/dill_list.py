a7440c5cec3cd961c8fe1df3e6761f0f5e2b1d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:44 +0000

    Add package dill_list/1.3.2/dill_list-1.3.2.tar.gz

---
+++ packages/dill_list/dill_list-1.3.2.tar.gz/dill_list-1.3.2/dill_list.py
@@ -0,0 +1,15 @@
+""" This is a function that performs recursive call on multi level arrays """
+
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,level+1,fh)
+		else:
+			try:
+				with open("print_lol.txt",'w+') as fh:
+					if indent:
+						for each_indent in range(level):
+							print("\t",end='', file=fh)
+					print(each_item,file=fh)
+			except IOError as err:
+				print("File could not be open due to: " + str(err))
