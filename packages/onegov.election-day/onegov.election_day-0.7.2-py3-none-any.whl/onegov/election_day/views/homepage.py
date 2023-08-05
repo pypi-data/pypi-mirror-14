3e6a5351e917dbbd413004d3e53b6a142e8776e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package onegov.election-day/0.7.2/onegov.election_day-0.7.2-py3-none-any.whl

---
+++ packages/onegov.election-day/onegov.election_day-0.7.2-py3-none-any.whl/onegov/election_day/views/homepage.py
@@ -0,0 +1,23 @@
+from onegov.core.security import Public
+from onegov.election_day import ElectionDayApp
+from onegov.election_day.layout import DefaultLayout
+from onegov.election_day.models import Principal, Archive
+
+
+@ElectionDayApp.html(model=Principal, template='homepage.pt',
+                     permission=Public)
+def view_principal(self, request):
+
+    return {
+        'layout': DefaultLayout(self, request),
+        'archive_items': Archive(request.app.session()).latest(),
+        'show_base_link': True
+    }
+
+
+@ElectionDayApp.html(model=Principal, template='opendata.pt', name='opendata',
+                     permission=Public)
+def view_opendata(self, request):
+    return {
+        'layout': DefaultLayout(self, request),
+    }
---
+++ packages/onegov.election-day/onegov.election_day-0.7.2-py3-none-any.whl/onegov/election_day/views/vote.py
@@ -0,0 +1,49 @@
+from morepath.request import Response
+from onegov.ballot import Ballot, Vote
+from onegov.core.csv import convert_list_of_dicts_to_csv
+from onegov.core.csv import convert_list_of_dicts_to_xlsx
+from onegov.core.security import Public
+from onegov.core.utils import normalize_for_url
+from onegov.election_day import ElectionDayApp
+from onegov.election_day.layout import DefaultLayout
+
+
+@ElectionDayApp.html(model=Vote, template='vote.pt', permission=Public)
+def view_vote(self, request):
+
+    layout = DefaultLayout(self, request)
+    request.include('ballot_map')
+
+    return {
+        'vote': self,
+        'layout': layout,
+        'counted': self.counted
+    }
+
+
+@ElectionDayApp.json(model=Vote, name='json', permission=Public)
+def view_vote_as_json(self, request):
+    return self.export()
+
+
+@ElectionDayApp.view(model=Vote, name='csv', permission=Public)
+def view_vote_as_csv(self, request):
+    return convert_list_of_dicts_to_csv(self.export())
+
+
+@ElectionDayApp.view(model=Vote, name='xlsx', permission=Public)
+def view_vote_as_xlsx(self, request):
+    return Response(
+        convert_list_of_dicts_to_xlsx(self.export()),
+        content_type=(
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        ),
+        content_disposition='inline; filename={}.xlsx'.format(
+            normalize_for_url(self.title)
+        )
+    )
+
+
+@ElectionDayApp.json(model=Ballot, permission=Public, name='by-municipality')
+def view_ballot_by_municipality(self, request):
+    return self.percentage_by_municipality()
