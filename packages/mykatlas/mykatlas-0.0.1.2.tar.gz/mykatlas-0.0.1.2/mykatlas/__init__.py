f20f065e877d8ea62f8ebb64580e0682c4b6d594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package mykatlas/0.0.1.2/mykatlas-0.0.1.2.tar.gz

---
+++ packages/mykatlas/mykatlas-0.0.1.2.tar.gz/mykatlas-0.0.1.2/mykatlas/__init__.py
@@ -0,0 +1,3 @@
+from mykatlas import version
+from base import sequence_parser_mixin
+
---
+++ packages/mykatlas/mykatlas-0.0.1.2.tar.gz/mykatlas-0.0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name='mykatlas',
+    version='0.0.1.2',
+    packages=[
+        'mykatlas',
+        'mykatlas.cmds',
+        'mykatlas.typing',
+        'mykatlas.typing.typer',
+        'mykatlas.typing.models',
+        'mykatlas.stats',
+        'mykatlas.cortex'],
+    license='MIT',
+    url='http://github.com/phelimb/atlas',
+    description='.',
+    author='Phelim Bradley',
+    author_email='wave@phel.im',
+    install_requires=[
+            'future',
+            'Biopython',
+            'mongoengine',
+            'pyvcf',
+            'ga4ghmongo'],
+    entry_points={
+        'console_scripts': [
+            'atlas = atlas.atlas_main:main',
+        ]})
