a34bb2b76b426c4b93494553bbd6c7680f5be34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:23 +0000

    Add package public/0.0.12/public-0.0.12.tar.gz

---
+++ packages/public/public-0.0.12.tar.gz/public-0.0.12/.setup/.long_description.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__all__=["long_description"]
+from os.path import *
+
+repo = dirname(dirname(__file__))
+if not repo: repo="."
+
+for name in ["README.rst","README","README.txt"]:
+    fullpath = join(repo,name)
+    if not exists(fullpath): continue
+    if not isfile(fullpath): continue
+    long_description = open(fullpath).read()
+
+if __name__=="__main__":
+	for k in __all__:
+		if k in globals():
+			print("%s: %s" % (k,globals()[k]))
+
