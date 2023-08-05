f69f53c909bb66d6ff42b4f4c297d999f5478dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:51 +0000

    Add package microbot/0.10.0/microbot-0.10.0.tar.gz

---
+++ packages/microbot/microbot-0.10.0.tar.gz/microbot-0.10.0/microbot/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+from microbot import views
+
+urlpatterns = [
+    url(r'^telegrambot/(?P<token>[-_:a-zA-Z0-9]+)/$', csrf_exempt(views.WebhookView.as_view()), name='telegrambot'),
+    url(r'^hook/(?P<key>\w+)/$', csrf_exempt(views.HookView.as_view()), name='hook'),
+    url(r'^api/bots/$', views.BotList.as_view(), name='bot-list'),
+    url(r'^api/bots/(?P<pk>[0-9]+)/$', views.BotDetail.as_view(), name='bot-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/env/$', views.EnvironmentVarList.as_view(), name='env-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/env/(?P<pk>[0-9]+)/$', views.EnvironmentVarDetail.as_view(), name='env-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/$', views.HandlerList.as_view(), name='handler-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<pk>[0-9]+)/$', views.HandlerDetail.as_view(), name='handler-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<pk>[0-9]+)/urlparams/$', views.UrlParameterList.as_view(), name='handler-urlparameter-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<handler_pk>[0-9]+)/urlparams/(?P<pk>[0-9]+)/$', views.UrlParameterDetail.as_view(), 
+        name='handler-urlparameter-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<pk>[0-9]+)/headerparams/$', views.HeaderParameterList.as_view(), name='handler-headerparameter-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<handler_pk>[0-9]+)/headerparams/(?P<pk>[0-9]+)/$', views.HeaderParameterDetail.as_view(), 
+        name='handler-headerparameter-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<pk>[0-9]+)/sourcestates/$', views.SourceStateList.as_view(), name='handler-sourcestate-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/handlers/(?P<handler_pk>[0-9]+)/sourcestates/(?P<pk>[0-9]+)/$', views.SourceStateDetail.as_view(), 
+        name='handler-sourcestate-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/hooks/$', views.HookList.as_view(), name='hook-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/hooks/(?P<pk>[0-9]+)/$', views.HookDetail.as_view(), name='hook-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/hooks/(?P<pk>[0-9]+)/recipients/$', views.RecipientList.as_view(), name='hook-recipient-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/hooks/(?P<hook_pk>[0-9]+)/recipients/(?P<pk>[0-9]+)/$', views.RecipientDetail.as_view(), name='hook-recipient-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/states/$', views.StateList.as_view(), name='state-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/states/(?P<pk>[0-9]+)/$', views.StateDetail.as_view(), name='state-detail'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/chatstates/$', views.ChatStateList.as_view(), name='chatstate-list'),
+    url(r'^api/bots/(?P<bot_pk>[0-9]+)/chatstates/(?P<pk>[0-9]+)/$', views.ChatStateDetail.as_view(), name='chatstate-detail'),
+    
+]
