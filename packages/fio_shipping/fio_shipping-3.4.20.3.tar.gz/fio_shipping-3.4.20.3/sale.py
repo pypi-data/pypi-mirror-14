42ccbc6339fb1c2b0b688a3eed2aadc948e31a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package fio_shipping/3.4.20.3/fio_shipping-3.4.20.3.tar.gz

---
+++ packages/fio_shipping/fio_shipping-3.4.20.3.tar.gz/fio_shipping-3.4.20.3/sale.py
@@ -0,0 +1,333 @@
+# -*- coding: utf-8 -*-
+"""
+    sale.py
+
+"""
+from trytond.model import fields
+from trytond.pool import PoolMeta, Pool
+from trytond.pyson import Eval, Not, Bool
+from trytond.transaction import Transaction
+
+__all__ = ['SaleLine', 'Sale']
+__metaclass__ = PoolMeta
+
+
+class Sale:
+    "Sale"
+    __name__ = 'sale.sale'
+
+    is_international_shipping = fields.Function(
+        fields.Boolean("Is International Shipping"),
+        'on_change_with_is_international_shipping'
+    )
+
+    weight = fields.Function(
+        fields.Float(
+            "Weight", digits=(16,  Eval('weight_digits', 2)),
+            depends=['weight_digits'],
+        ),
+        'get_weight'
+    )
+
+    weight_uom = fields.Function(
+        fields.Many2One('product.uom', 'Weight UOM'),
+        'get_weight_uom'
+    )
+    weight_digits = fields.Function(
+        fields.Integer('Weight Digits'), 'on_change_with_weight_digits'
+    )
+
+    available_carrier_services = fields.Function(
+        fields.One2Many("carrier.service", None, 'Available Carrier Services'),
+        getter="on_change_with_available_carrier_services"
+    )
+    carrier_service = fields.Many2One(
+        "carrier.service", "Carrier Service", domain=[
+            ('id', 'in', Eval('available_carrier_services'))
+        ], states={
+            "invisible": Not(Bool(Eval('carrier'))),
+            "readonly": Eval('state') != 'draft',
+        }, depends=['carrier', 'available_carrier_services', 'state']
+    )
+    carrier_cost_method = fields.Function(
+        fields.Char('Carrier Cost Method'),
+        "on_change_with_carrier_cost_method"
+    )
+
+    @fields.depends("carrier")
+    def on_change_with_carrier_cost_method(self, name=None):
+        if self.carrier:
+            return self.carrier.carrier_cost_method
+
+    def on_change_lines(self):
+        """Pass a flag in context which indicates the get_sale_price method
+        of carrier not to calculate cost on each line change
+        """
+        with Transaction().set_context({'ignore_carrier_computation': True}):
+            return super(Sale, self).on_change_lines()
+
+    @fields.depends("carrier")
+    def on_change_with_available_carrier_services(self, name=None):
+        if self.carrier:
+            return map(int, self.carrier.services)
+        return []
+
+    @classmethod
+    def __setup__(cls):
+        super(Sale, cls).__setup__()
+        cls._error_messages.update({
+            'warehouse_address_missing': 'Warehouse address is missing',
+        })
+
+    @fields.depends('weight_uom')
+    def on_change_with_weight_digits(self, name=None):
+        if self.weight_uom:
+            return self.weight_uom.digits
+        return 2
+
+    def get_weight_uom(self, name):
+        """
+        Returns weight uom for the sale
+        """
+        ModelData = Pool().get('ir.model.data')
+
+        return ModelData.get_id('product', 'uom_pound')
+
+    def get_weight(self, name):
+        """
+        Returns sum of weight associated with each line
+        """
+        return sum(map(
+            lambda line: line.get_weight(self.weight_uom, silent=True),
+            self.lines
+        ))
+
+    @fields.depends('party', 'shipment_address', 'warehouse')
+    def on_change_with_is_international_shipping(self, name=None):
+        """
+        Return True if international shipping
+        """
+        from_address = self._get_ship_from_address(silent=True)
+
+        if self.shipment_address and from_address and \
+                from_address.country and self.shipment_address.country and \
+                from_address.country != self.shipment_address.country:
+            return True
+        return False
+
+    def _get_ship_from_address(self, silent=False):
+        """
+        Usually the warehouse from which you ship
+        """
+        if not self.warehouse.address and not silent:
+            return self.raise_user_error('warehouse_address_missing')
+        return self.warehouse and self.warehouse.address
+
+    def add_shipping_line(self, shipment_cost, description):
+        """
+        This method takes shipping_cost and description as arguments and writes
+        a shipping line. It deletes any previous shipping lines which have
+        a shipment_cost.
+        :param shipment_cost: The shipment cost calculated according to carrier
+        :param description: Shipping line description
+        """
+        Sale = Pool().get('sale.sale')
+
+        Sale.write([self], {
+            'lines': [
+                ('create', [{
+                    'type': 'line',
+                    'product': self.carrier.carrier_product.id,
+                    'description': description,
+                    'quantity': 1,  # XXX
+                    'unit': self.carrier.carrier_product.sale_uom.id,
+                    'unit_price': shipment_cost,
+                    'shipment_cost': shipment_cost,
+                    'amount': shipment_cost,
+                    'taxes': [],
+                    'sequence': 9999,  # XXX
+                }]),
+                ('delete', [
+                    line for line in self.lines
+                    if line.shipment_cost is not None
+                ]),
+            ]
+        })
+
+    def _get_carrier_context(self):
+        "Pass sale in the context"
+        context = super(Sale, self)._get_carrier_context()
+        context = context.copy()
+        context['sale'] = self.id
+        return context
+
+    def apply_shipping_rate(self, rate):
+        """
+        This method applies shipping rate. Rate is a dictionary with following
+        minimum keys:
+            {
+                'display_name': Name to display,
+                'carrier_service': carrier.service active record,
+                'cost': cost,
+                'cost_currency': currency.currency active repord,
+                'carrier': carrier active record,
+            }
+
+        It also creates a shipment line by deleting all existing ones.
+        """
+        Currency = Pool().get('currency.currency')
+
+        self.carrier = rate['carrier']
+        self.carrier_service = rate['carrier_service']
+        self.save()
+
+        shipment_cost = rate['cost']
+        if self.currency != rate['cost_currency']:
+            shipment_cost = Currency.compute(
+                rate['cost_currency'], shipment_cost, self.currency
+            )
+
+        self.add_shipping_line(shipment_cost, rate['display_name'])
+
+    def get_shipping_rates(self, carriers=None, silent=False):
+        """
+        Gives a list of rates from carriers provided. If no carriers provided,
+        return rates from all the carriers.
+
+        List contains dictionary with following minimum keys:
+            [
+                {
+                    'display_name': Name to display,
+                    'carrier_service': carrier.service active record,
+                    'cost': cost,
+                    'cost_currency': currency.currency active repord,
+                    'carrier': carrier active record,
+                }..
+            ]
+        """
+        Carrier = Pool().get('carrier')
+
+        if carriers is None:
+            carriers = Carrier.search([])
+
+        rates = []
+        for carrier in carriers:
+            rates.extend(self.get_shipping_rate(carrier, silent))
+        return rates
+
+    def get_shipping_rate(self, carrier, carrier_service=None, silent=False):
+        """
+        Gives a list of rates from provided carrier and carrier service.
+
+        List contains dictionary with following minimum keys:
+            [
+                {
+                    'display_name': Name to display,
+                    'carrier_service': carrier.service active record,
+                    'cost': cost,
+                    'cost_currency': currency.currency active repord,
+                    'carrier': carrier active record,
+                }..
+            ]
+        """
+        Company = Pool().get('company.company')
+
+        if carrier.carrier_cost_method == 'product':
+            return [{
+                'display_name': carrier.rec_name,
+                'carrier_service': carrier_service,
+                'cost': carrier.carrier_product.list_price,
+                'cost_currency': Company(Transaction().context['company']).currency,  # noqa
+                'carrier': carrier,
+            }]
+        return []
+
+    @classmethod
+    def get_allowed_carriers_domain(cls):
+        """This method returns domain to seach allowed carriers
+
+        Downstream modules can inherit and update customize this domain.
+        """
+        return []
+
+    def create_shipment(self, shipment_type):
+        Shipment = Pool().get('stock.shipment.out')
+
+        with Transaction().set_context(ignore_carrier_computation=True):
+            shipments = super(Sale, self).create_shipment(shipment_type)
+
+        if shipment_type == 'out' and shipments and self.carrier and \
+                self.carrier_service:
+            Shipment.write(list(shipments), {
+                'carrier_service': self.carrier_service.id,
+            })
+        return shipments
+
+
+class SaleLine:
+    'Sale Line'
+    __name__ = 'sale.line'
+
+    @classmethod
+    def __setup__(cls):
+        super(SaleLine, cls).__setup__()
+        cls._error_messages.update({
+            'weight_required': 'Weight is missing on the product %s',
+        })
+
+    def get_weight(self, weight_uom, silent=False):
+        """
+        Returns weight as required for carriers
+
+        :param weight_uom: Weight uom used by carriers
+        :param silent: Raise error if not silent
+        """
+        ProductUom = Pool().get('product.uom')
+
+        if not self.product or self.quantity <= 0 or \
+                self.product.type == 'service':
+            return 0
+
+        if not self.product.weight:
+            if silent:
+                return 0
+            self.raise_user_error(
+                'weight_required',
+                error_args=(self.product.name,)
+            )
+
+        # Find the quantity in the default uom of the product as the weight
+        # is for per unit in that uom
+        if self.unit != self.product.default_uom:
+            quantity = ProductUom.compute_qty(
+                self.unit,
+                self.quantity,
+                self.product.default_uom
+            )
+        else:
+            quantity = self.quantity
+
+        weight = self.product.weight * quantity
+
+        # Compare product weight uom with the weight uom used by carrier
+        # and calculate weight if botth are not same
+        if self.product.weight_uom != weight_uom:
+            weight = ProductUom.compute_qty(
+                self.product.weight_uom,
+                weight,
+                weight_uom,
+            )
+
+        return weight
+
+
+class ReturnSale:
+    __name__ = 'sale.return_sale'
+
+    def do_return_(self, action):
+        Sale = Pool().get('sale.sale')
+        action, data = super(ReturnSale, self).do_return_(action)
+
+        Sale.write(Sale.browse(data['res_id']), {'carrier': None})
+
+        return action, data
