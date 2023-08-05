694ce39b5421e78fd971b336d92ea2f765e0003d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:49 +0000

    Add package Grortir/0.1.1/Grortir-0.1.1.tar.gz

---
+++ packages/Grortir/Grortir-0.1.1.tar.gz/Grortir-0.1.1/grortir/test/model/core/test_abstract_stage.py
@@ -0,0 +1,41 @@
+"""Module for testing."""
+
+from unittest import TestCase
+from unittest.mock import Mock
+
+from grortir.main.model.core.abstract_stage import AbstractStage
+
+
+class TestAbstractStage(TestCase):
+    """Class for testing AbstractStage."""
+
+    def test___init__(self):
+        """Constructor test."""
+        tested_object = AbstractStage()
+        self.assertIsNotNone(tested_object)
+
+    def test___init__arguments_passed(self):
+        """Check arguments in constructor."""
+        abstract_stage_mock = Mock()
+        input_vector = (1, 2, 3)
+        AbstractStage.__init__(abstract_stage_mock, input_vector)
+        self.assertEqual(abstract_stage_mock.input_vector, input_vector)
+
+    def test___init__output_mutability(self):
+        """Check fields in object."""
+        input_vector = (1, 2, 3)
+        tested_object = AbstractStage(input_vector)
+        tested_object.output_vector[2] += 1
+        self.assertEqual(tested_object.output_vector[2], 4)
+
+    def test_get_cost(self):
+        """Check get_cost method."""
+        tested_object = AbstractStage()
+        with self.assertRaises(NotImplementedError):
+            tested_object.get_cost()
+
+    def test_get_quality(self):
+        """Check get_quality method."""
+        tested_object = AbstractStage()
+        with self.assertRaises(NotImplementedError):
+            tested_object.get_quality()
