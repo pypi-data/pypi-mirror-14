9586349c014713cbffd5e33d59a0a276877d0d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package dhl_shipping/1.0.1/dhl_shipping-1.0.1.tar.gz

---
+++ packages/dhl_shipping/dhl_shipping-1.0.1.tar.gz/dhl_shipping-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='dhl_shipping',
+    version='1.0.1',
+    author='Hasanuzzaman Syed',
+    author_email='hasanuzzaman.syed@gmail.com',
+    packages=['dhl_shipping'],
+    url='http://pypi.python.org/pypi/dhl_shipping/',
+    license='LICENSE.txt',
+    description='DHL Shipping - Quote, Pick Up, Shipping, Label Creation, Tracking',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'xmltodict',
+    ],
+)
