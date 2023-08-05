51c47e31a205d328b9dd2500558fca429af6c0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package siskin/0.0.138/siskin-0.0.138.tar.gz

---
+++ packages/siskin/siskin-0.0.138.tar.gz/siskin-0.0.138/siskin/__init__.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+# pylint: disable=C0103
+
+"""
+Define version, disable some warnings and ensure temporary and log directories
+are there and writeable.
+"""
+
+from __future__ import print_function
+from siskin.configuration import Config
+import os
+import sys
+import tempfile
+
+# TODO(miku): 2.7 pytz seems to import module argparse twice?
+import warnings
+warnings.filterwarnings("ignore")
+
+# https://urllib3.readthedocs.org/en/latest/security.html#insecurerequestwarning
+# wrap into try-except, since at install-time urllib3 might not be installed yet
+try:
+    import urllib3
+    urllib3.disable_warnings()
+except ImportError:
+    pass
+
+__version__ = '0.0.138'
+
+config = Config.instance()
+
+tempfile.tempdir = config.get('core', 'tempdir', tempfile.gettempdir())
+logdir = config.get('core', 'logdir', '/var/log/siskin')
+
+for name, directory in (('core.tempdir', tempfile.tempdir), ('core.logdir', logdir)):
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory, 1777)
+        except OSError as err:
+            msg = '{0} does not exist and we cannot create it: {1}'.format(name, directory)
+            print(msg, file=sys.stderr)
+            sys.exit(1)
---
+++ packages/siskin/siskin-0.0.138.tar.gz/siskin-0.0.138/siskin/sources/amsl.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+#
+#  Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+#                 by The Finc Authors, http://finc.info
+#                 by Martin Czygan, <martin.czygan@uni-leipzig.de>
+#
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+#
+
+"""
+Electronic Resource Management System Based on Linked Data Technologies.
+
+http://amsl.technology
+
+Config:
+
+[amsl]
+
+isil-rel = https://x.com/static/about.json
+holdings = https://x.com/inhouseservices/list?do=holdings
+uri-download-prefix = https://x.y.z/OntoWiki/files/get?setResource=
+
+"""
+
+from gluish.utils import shellout
+from siskin.configuration import Config
+from siskin.task import DefaultTask
+import datetime
+import json
+import luigi
+import tempfile
+import zipfile
+
+config = Config.instance()
+
+class AMSLTask(DefaultTask):
+    TAG = 'amsl'
+
+class AMSLCollections(AMSLTask):
+    """ Get a list of ISIL, source and collection choices via JSON API. """
+
+    date = luigi.DateParameter(default=datetime.date.today())
+
+    def run(self):
+        output = shellout("""curl --fail "{link}" > {output} """, link=config.get('amsl', 'isil-rel'))
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
+
+class AMSLHoldings(AMSLTask):
+    """
+    Download AMSL tasks.
+    """
+    date = luigi.Parameter(default=datetime.date.today())
+
+    def run(self):
+        output = shellout(""" curl --fail "{link}" > {output} """, link=config.get('amsl', 'holdings'))
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
+
+class AMSLHoldingsISILList(AMSLTask):
+    """
+    Return a list of ISILs that are returned by the API.
+    """
+    date = luigi.Parameter(default=datetime.date.today())
+
+    def requires(self):
+        return AMSLHoldings(date=self.date)
+
+    def run(self):
+        output = shellout("jq -r '.[].ISIL' {input} | sort > {output}", input=self.input().path)
+        luigi.File(output).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
+
+class AMSLHoldingsFile(AMSLTask):
+    """
+    Access AMSL files/get?setResource= facilities.
+
+    The output is probably zipped.
+
+    One ISIL can have multiple files.
+
+    If the file is Zipped, it will be decompressed.
+    """
+    isil = luigi.Parameter(description='ISIL, case sensitive')
+    date = luigi.Parameter(default=datetime.date.today())
+
+    def requires(self):
+        return AMSLHoldings(date=self.date)
+
+    def run(self):
+        with self.input().open() as handle:
+            holdings = json.load(handle)
+
+        _, stopover = tempfile.mkstemp(prefix='siskin-')
+
+        for holding in holdings:
+            if holding["ISIL"] == self.isil:
+                link = "%s%s" % (config.get('amsl', 'uri-download-prefix'), holding['DokumentURI'])
+                downloaded = shellout("curl --fail {link} > {output} ", link=link)
+                try:
+                    _ = zipfile.ZipFile(downloaded)
+                    output = shellout("unzip -p {input} >> {output}", input=downloaded, output=stopover)
+                except zipfile.BadZipfile:
+                    # at least the file is not a zip.
+                    output = shellout("cat {input} >> {output}", input=downloaded, output=stopover)
+
+        luigi.File(stopover).move(self.output().path)
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
