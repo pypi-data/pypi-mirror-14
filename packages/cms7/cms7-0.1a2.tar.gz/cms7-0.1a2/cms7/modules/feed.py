c9362723c1f7d8c176ab86723d0a96fc1c900450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:15 +0000

    Add package cms7/0.1a2/cms7-0.1a2.tar.gz

---
+++ packages/cms7/cms7-0.1a2.tar.gz/cms7-0.1a2/cms7/modules/feed.py
@@ -0,0 +1,45 @@
+import logging
+
+from pathlib2 import PurePosixPath
+
+from feedgenerator.django.utils.feedgenerator import Atom1Feed, Rss201rev2Feed, Enclosure
+
+from . import Module
+from .blog import Blog
+
+from ..source import load_source
+from ..util import meta_get_one
+
+
+class Feed:
+    def __init__(self, parent, ftype):
+        self.parent = parent
+        self.ftype = ftype
+
+    def render(self, gs):
+        blog = self.parent.blog.get_api(gs)
+        feed = self.ftype('FREENODE', 'FREENODE', 'FREENODE')
+
+        for a in blog.articles[-15:]:
+            feed.add_item(a.title, 'FREENODE', a.source.text, author_name=a.author)
+
+        return feed.writeString('utf-8') + '\n'
+
+
+
+class FeedModule(Module):
+    def __init__(self, *a, module, output, **kw):
+        super().__init__(*a, **kw)
+
+        self.blog = self.config.module_id[module]
+
+        if not isinstance(self.blog, Blog):
+            self.log(logging.WARNING, "Configured with a module other than a blog. Expect errors.")
+
+        self.output = PurePosixPath(output)
+
+    def run(self, gen):
+        gen.add_render(self.output / 'atom', self.output / 'atom.xml',
+                       Feed(self, Atom1Feed).render)
+        gen.add_render(self.output / 'rss', self.output / 'rss.xml',
+                       Feed(self, Rss201rev2Feed).render)
---
+++ packages/cms7/cms7-0.1a2.tar.gz/cms7-0.1a2/cms7/util.py
@@ -0,0 +1,35 @@
+"""
+General utilities
+"""
+
+from urllib.parse import urlparse
+
+from .error import CMS7Error
+from .source import MarkdownSource
+
+_NOTHING = object()
+
+
+def meta_get_one(md, key, default=_NOTHING):
+    source = None
+    if isinstance(md, MarkdownSource):
+        source = md
+        md = md.meta
+    if key in md:
+        return md[key][0]
+    if default is not _NOTHING:
+        return default
+    if source:
+        raise CMS7Error('{!s} missing required metadata key: {!r}'
+                .format(source.source, key))
+    else:
+        raise KeyError(key)
+
+
+def is_relative_url(url):
+    url = urlparse(url)
+    if url.scheme or url.netloc or url.path.startswith('/'):
+        return False
+    if url.path == '':
+        return False
+    return True
