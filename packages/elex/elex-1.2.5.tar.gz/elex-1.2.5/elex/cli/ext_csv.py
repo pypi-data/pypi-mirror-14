9fcfbda115e1940d8f1509152ca911855afe8483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:31 +0000

    Add package elex/1.2.5/elex-1.2.5.tar.gz

---
+++ packages/elex/elex-1.2.5.tar.gz/elex-1.2.5/elex/cli/ext_csv.py
@@ -0,0 +1,43 @@
+import csv
+import sys
+from cement.core import handler, output
+
+
+class CSVOutputHandler(output.CementOutputHandler):
+    """
+    A custom CSV output handler
+    """
+    class Meta:
+        label = 'csv'
+        overridable = True
+
+    def render(self, data, template=None):
+        if not isinstance(data, (list, tuple)):
+            data = [data]
+
+        if len(data) == 0:
+            return
+
+        try:
+            # Properly terminate lines for Windows and Excel.
+            # See: https://github.com/newsdev/elex/issues/232
+            writer = csv.writer(sys.stdout, lineterminator='\r')
+            for i, row in enumerate(data):
+                if i == 0:
+                    writer.writerow(row.serialize().keys())
+                writer.writerow(row.serialize().values())
+        except IOError:
+            # Handle pipes that could close before output is done.
+            # See: http://stackoverflow.com/questions/15793886/
+            try:
+                sys.stdout.close()
+            except IOError:
+                pass
+            try:
+                sys.stderr.close()
+            except IOError:
+                pass
+
+
+def load(app):
+    handler.register(CSVOutputHandler)
