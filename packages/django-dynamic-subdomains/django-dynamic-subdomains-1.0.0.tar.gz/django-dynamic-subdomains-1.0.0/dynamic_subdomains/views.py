731565518d6bd8220a6f2ee61f35d79af901a004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:07 +0000

    Add package django-dynamic-subdomains/1.0.0/django-dynamic-subdomains-1.0.0.tar.gz

---
+++ packages/django-dynamic-subdomains/django-dynamic-subdomains-1.0.0.tar.gz/django-dynamic-subdomains-1.0.0/dynamic_subdomains/views.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+
+from .app_settings import app_settings
+
+def redirect_(request, host):
+    if not settings.DEBUG:
+        return HttpResponseForbidden()
+
+    response = HttpResponseRedirect(request.META.get('QUERY_STRING', '') or '/')
+    response.set_cookie(app_settings.COOKIE_NAME, host)
+    response.status_code = 307 # Re-submit POST requests
+
+    return response
