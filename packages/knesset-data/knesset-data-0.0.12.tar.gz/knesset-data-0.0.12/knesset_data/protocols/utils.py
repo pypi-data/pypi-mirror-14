d89c9df53809b2adbbf375fa9b46521c923f4e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package knesset-data/0.0.12/knesset-data-0.0.12.tar.gz

---
+++ packages/knesset-data/knesset-data-0.0.12.tar.gz/knesset-data-0.0.12/knesset_data/protocols/utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+import subprocess
+import os
+import xml.etree.ElementTree as ET
+from exceptions import AntiwordException
+
+
+logger = logging.getLogger(__name__)
+
+
+def antixml(str):
+    tree = ET.fromstring(str.replace("\n\n", ""))
+    text = ET.tostring(tree, encoding='utf8', method='text')
+    text = "\n".join([line.strip() if len(line.strip()) == 0 else line for line in text.split("\n")])
+    return text
+
+
+def antiword(filename):
+    cmd='antiword -x db '+filename+' > '+filename+'.awdb.xml'
+    try:
+        logger.debug(cmd)
+        output = subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
+    except subprocess.CalledProcessError, e:
+        raise AntiwordException(e.returncode, e.cmd, e.output)
+    logger.info(output)
+    with open(filename+'.awdb.xml','r') as f:
+        xmldata=f.read()
+    logger.debug('len(xmldata) = '+str(len(xmldata)))
+    os.remove(filename+'.awdb.xml')
+    return xmldata
