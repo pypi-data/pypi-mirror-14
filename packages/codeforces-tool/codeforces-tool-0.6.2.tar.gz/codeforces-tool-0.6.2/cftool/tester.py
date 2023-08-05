a99fa13c2cb3b67abcf051731fbce48b28a11b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package codeforces-tool/0.6.2/codeforces-tool-0.6.2.tar.gz

---
+++ packages/codeforces-tool/codeforces-tool-0.6.2.tar.gz/codeforces-tool-0.6.2/cftool/tester.py
@@ -0,0 +1,132 @@
+from pprint import pprint
+import subprocess
+import json
+import os
+import shlex
+from colorama import init, Fore
+from download import cfg, get_contest_task_file, find_contest, get_relative_problem_dir, find_or_create_global_contest, \
+                get_contest_submit_file_name, get_contest_task_submit_file
+from submit import get_problem_and_language
+
+def check_token(output_tk, expected_tk):
+    return output_tk == expected_tk
+
+def check_tokens(output, expected):
+    output_tk = output.split()
+    expected_tk = expected.split()
+    if len(output_tk) != len(expected_tk):
+        return "Difference at token count. Expected %d but found %d." % (len(expected_tk), len(output_tk))
+
+    for idx, tk in enumerate(output_tk):
+        if not check_token(tk, expected_tk[idx]):
+            return "Difference at token #%d. Expected '%s' but found '%s'." % (idx+1, expected_tk[idx], tk)
+
+def preprocess(contest, problem, language, task_file, submit_file):
+    print Fore.CYAN + "Pre-processing %s..." % (task_file)
+    preexec_list = language["preexec"] if "preexec" in language.keys() else []
+
+    for pre in preexec_list:
+        preexec_line = pre.replace("%{file}", task_file) \
+                        .replace("%{submit-file}", submit_file)
+
+        p = subprocess.Popen(shlex.split(preexec_line), cwd=contest["dir"])
+        p.wait()
+        if p.returncode != 0:
+            return False
+    return True
+
+def get_task_and_submit_files(contest, problem, language):
+    task_file = os.path.relpath(get_contest_task_file(contest, problem, language), contest["dir"])
+    submit_file = os.path.relpath(get_contest_task_submit_file(contest, problem, language), contest["dir"])
+    return (task_file, submit_file)
+
+def test_contest_problem(contest, problem, language=None, task_file=None, stream=False):
+    if not language:
+        language = cfg["languages"][cfg["languages"]["default"]]
+
+    problem_dir = os.path.join(contest["dir"], get_relative_problem_dir(contest, problem))
+    if not task_file:
+        (task_file, submit_file) = get_task_and_submit_files(contest, problem, language)
+    else:
+        base = os.path.basename(task_file)
+        submit_file = os.path.join(os.path.dirname(task_file),
+            '.'.join(base.split(".")[:-1] + ['pre'] + base.split(".")[-1:]))
+
+    preexec_ok = preprocess(contest, problem, language, task_file, submit_file)
+
+    if preexec_ok:
+        executable = language["exec"].replace("%{file}", task_file).replace("%{submit-file}", submit_file)
+        inputs = [file for file in os.listdir(problem_dir) if file.endswith(".in")]
+        for idx, input_file in enumerate(sorted(inputs)):
+            input_path = os.path.join(problem_dir, input_file)
+            ans_path = os.path.join(problem_dir, "%s.out" % (os.path.splitext(input_file)[0]))
+            input_string = None
+            ans_string = ""
+            with open(input_path, "rb") as f:
+                input_string = f.read()
+
+            if os.path.exists(ans_path):
+                with open(ans_path, "rb") as f:
+                    ans_string = f.read()
+
+            if input_string != None:
+                print Fore.YELLOW + "Executing test #%d (%s):" % (idx, os.path.splitext(input_file)[0])
+                print Fore.MAGENTA + "Input:"
+                print Fore.WHITE + input_string.strip()
+                print ""
+                print Fore.MAGENTA + "Output:"
+                p = subprocess.Popen(shlex.split(executable), bufsize=1 if stream else 0, cwd=contest["dir"], stdin=open(input_path, "rb"), stdout=subprocess.PIPE, stderr=(subprocess.STDOUT if cfg["mergeouterr"] else None))
+                # output_tuple = p.communicate(input=input_string)
+                output_print = ""
+
+                if stream:
+                    with p.stdout:
+                        for line in iter(p.stdout.readline, b''):
+                            print Fore.WHITE + line.rstrip()
+                            output_print += line
+                    p.wait() # wait for the subprocess to exit
+                    output_print = output_print.strip()
+                else:
+                    p.wait()
+                    output_print = p.communicate()[0].strip()
+                    print Fore.WHITE + output_print
+
+                ans_print = ans_string.strip()
+
+                # print Fore.WHITE + output_print
+                print ""
+                print Fore.MAGENTA + "Expected Output:"
+                print Fore.WHITE + ans_print
+                print ""
+                if p.returncode != 0:
+                    print Fore.MAGENTA + "Verdict: " + Fore.CYAN + "Execution error"
+                else:
+                    verdict = check_tokens(output_print, ans_print)
+                    if verdict:
+                        print Fore.MAGENTA + "Verdict: " + Fore.RED + verdict
+                    else:
+                        print Fore.MAGENTA + "Verdict: " + Fore.GREEN + "Accepted"
+                print ""
+
+            else:
+                print Fore.RED + "Error reading input files."
+    else:
+        print Fore.RED + "Error at some step of pre-processing."
+
+
+def test_single_problem(path, stream=False):
+    contest_id, idx, language = get_problem_and_language(path)
+    contest = find_or_create_global_contest(contest_id)
+    if not contest:
+        print "Failed testing problem. Contest does not exists."
+    else:
+        test_contest_problem(contest, idx, language, task_file=path, stream=stream)
+
+if __name__ == "__main__":
+    init(autoreset=True)
+    contest = find_contest()
+    if contest != None:
+        os.chdir(contest["dir"])
+        test_contest_problem(contest, "A")
+    else:
+        print "Contest could not be found."
---
+++ packages/codeforces-tool/codeforces-tool-0.6.2.tar.gz/codeforces-tool-0.6.2/cftool/tool.py
@@ -0,0 +1,187 @@
+import argparse
+from colorama import init, Fore
+from download import find_contest, get_contest_task_file, create_contest, find_or_create_global_contest, app_folder, \
+                    get_contest_task_submit_file
+from submit import submit_problem, get_index_and_language, get_problem_and_language
+from watch import get_status_table_string, get_standings_table_string, normal_buffer, alternate_buffer, clear_buffer
+from watch import get_last_table_string
+from tester import test_contest_problem, test_single_problem, get_task_and_submit_files, \
+                    preprocess
+import notify
+import editor
+import time
+import os
+import atexit
+import sys
+import signal
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+watch = parser.add_mutually_exclusive_group()
+watch.add_argument("-w", "--watch", action="store_true", help="watch contest status")
+watch.add_argument("-l", "--last", action="store_true", help="watch your 5 last submissions")
+
+group.add_argument("-d", "--download", metavar="contest-identifier", help="download contest")
+group.add_argument("-s", "--submit", metavar="problem-index", help="submit a problem to current contest")
+group.add_argument("-t", "--test",  help="test a problem from current contest")
+group.add_argument("-a", "--add", metavar="problem-index", help="add a custom test case to the problem")
+group.add_argument("-e", "--edit", metavar="problem-index", help="edit problem from current contest")
+group.add_argument("-f", "--folder", action="store_true", help="open folder in desired application")
+group.add_argument("-c", "--config", action="store_true", help="edit tool configurations")
+group.add_argument("-n", "--notify", action="store_true", help="watch for notifications")
+
+parser.add_argument("-x", "--single", action="store_true", help="modifier to run commands for a single file")
+parser.add_argument("-v", "--stream", action="store_true", help="print results in streaming way")
+
+args = parser.parse_args()
+
+init(autoreset=True)
+
+def dump_table(name, str):
+    file = open(os.path.join(app_folder, name), "wb")
+    file.write(str)
+    file.close()
+
+def confirmation(text="Are you sure you want to continue?"):
+    choices = {"Y":True, "y":True, "n": False, "N": False}
+
+    print text + " (Y/n)"
+    while True:
+        r = raw_input().strip()
+        if not r in choices:
+            print "Please, type only (Y/n) as response."
+            continue
+        else:
+            return choices[r]
+
+
+def contest_not_found():
+    print Fore.RED + "Contest could not be found."
+
+def get_absolute_path(path):
+    return os.path.normpath(os.path.join(os.getcwd(), path))
+
+def get_last_update():
+    return Fore.YELLOW + "Last update at %s." % (time.strftime("%H:%M:%S"))
+
+def sigint_handler(signal, frame):
+    sys.exit(0)
+
+def main():
+    signal.signal(signal.SIGINT, sigint_handler)
+    problem_submitted = False
+
+    if args.notify:
+        contest = find_contest()
+        if contest != None:
+            notify.process(contest)
+        return
+
+    if args.download:
+        if create_contest(args.download):
+            print Fore.GREEN + "Contest downloaded successfully."
+        else:
+            print Fore.RED + "Contest could not be downloaded."
+
+    elif args.submit:
+        print Fore.CYAN + "Problem: " + Fore.RESET + str(args.submit)
+        print Fore.RED + "Have you checked all the corner cases, constraints and overflow possibilities?" + Fore.RESET
+        if not confirmation():
+            sys.exit(0)
+
+        if args.single:
+            file = get_absolute_path(args.submit)
+            contest_id, idx, language = get_problem_and_language(file)
+            contest = find_or_create_global_contest(contest_id)
+            if not contest:
+                contest_not_found()
+            else:
+                if submit_problem(contest, idx, file, language):
+                    problem_submitted = True
+                    print Fore.GREEN + "Problem submitted. Make sure it was not identical to some previous submission."
+                else:
+                    print Fore.RED + "The problem could not be submitted."
+        else:
+            contest = find_contest()
+            if contest != None:
+                os.chdir(contest["dir"])
+
+                (index, language) = get_index_and_language(args.submit)
+                (task_file, submit_file) = get_task_and_submit_files(contest, index, language)
+                if preprocess(contest, index, language, task_file, submit_file):
+                    if submit_problem(contest, args.submit, get_contest_task_submit_file(contest, index, language), language):
+                        problem_submitted = True
+                        print Fore.GREEN + "Problem submitted. Make sure it was not identical to some previous submission."
+                    else:
+                        print Fore.RED + "The problem could not be submitted."
+                else:
+                    print Fore.RED + "There was an error in the preprocessing step."
+            else:
+                contest_not_found()
+
+    elif args.test:
+        if args.single:
+            test_single_problem(args.test[0], args=args.test[1:], stream=args.stream)
+        else:
+            contest = find_contest()
+            if contest != None:
+                os.chdir(contest["dir"])
+                (index, language) = get_index_and_language(args.test)
+
+                test_contest_problem(contest, index, language, stream=args.stream)
+            else:
+                contest_not_found()
+
+    elif args.add:
+        contest = find_contest()
+        if contest != None:
+            editor.add_test_contest_problem(contest, args.add)
+        else:
+            contest_not_found()
+
+    elif args.edit:
+        contest = find_contest()
+        if contest != None:
+            editor.edit_contest_problem(contest, args.edit)
+        else:
+            contest_not_found()
+
+    elif args.folder:
+        contest = find_contest()
+        if contest != None:
+            editor.edit_contest_folder(contest)
+        else:
+            contest_not_found()
+
+    elif args.config:
+        editor.edit_config()
+        # editor.edit_template()
+
+    if args.last:
+        atexit.register(normal_buffer)
+        alternate_buffer()
+        while 1:
+            last = get_last_table_string()
+            clear_buffer()
+            text = last
+            text += '\n'
+            if problem_submitted:
+                text += Fore.GREEN + "Request was sent. Check if problem was received by the server.\n"
+            text += get_last_update()+"\n"
+            print text
+            # dump_table("last", text)
+            time.sleep(3)
+
+    elif args.watch:
+        contest = find_contest()
+        if contest != None:
+            atexit.register(normal_buffer)
+            alternate_buffer()
+            while 1:
+                status = get_last_table_string(contest)
+                standings = get_standings_table_string(contest)
+                clear_buffer()
+                print status
+                print standings
+                print get_last_update()
+                time.sleep(3)
