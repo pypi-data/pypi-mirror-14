a0abe2cf48d0cce23afd6654daf9e7a33a0962c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:37 +0000

    Add package todo-md/0.0.2.1/todo-md-0.0.2.1.tar.gz

---
+++ packages/todo-md/todo-md-0.0.2.1.tar.gz/todo-md-0.0.2.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'todo-md',
+    version = '0.0.2.1',
+    author = 'onur satici',
+    author_email = 'onursatici@gmail.com',
+    packages = ['todo_md'],
+    install_requires = [
+        'termcolor'
+    ],
+    scripts = ['todo'],
+    description = 'txt file backed todo manager',
+    long_description = open('README.txt').read()
+)
