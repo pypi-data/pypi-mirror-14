7baf85105b735b15ea815f5e1819116e9fe96236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package nester_mark/1.0.0/nester_mark-1.0.0.zip

---
+++ packages/nester_mark/nester_mark-1.0.0.zip/nester_mark-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         =  'nester_mark',
+    version      =  '1.0.0',
+    py_modules   =  ['nester_mark'],
+    author       =  'mark',
+    author_email = 'mark920435@livemail.tw',
+    description  = 'A simple printer of nested lists',
+    )
