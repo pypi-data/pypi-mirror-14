a8e45756b7e0af02b19f7ff216a43015639e8f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package geonames_countries/0.1.1/geonames_countries-0.1.1.tar.gz

---
+++ packages/geonames_countries/geonames_countries-0.1.1.tar.gz/geonames_countries-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'geonames_countries',
+  packages = ['geonames_countries'],
+  version = '0.1.1',
+  description = 'A simple wrapper for country code and tld data.',
+  author = 'Marco Montagna',
+  author_email = 'marcojoemontagna@gmail.com',
+  url = 'https://github.com/mmontagna/geonames_countries',
+  download_url = 'https://github.com/mmontagna/geonames_countries/archive/v0.1.1.tar.gz',
+  keywords = ['tld', 'country code'],
+  classifiers = [],
+  include_package_data=True,
+  package_data={'geonames_countries': ['geonames_countries.json.gz']}
+)
---
+++ packages/geonames_countries/geonames_countries-0.1.1.tar.gz/geonames_countries-0.1.1/test/test_countries.py
@@ -0,0 +1,34 @@
+import unittest, time
+from geonames_countries import Countries
+
+class TestCountries(unittest.TestCase):
+
+  def test_findSingularProperties(self):
+    self.assertEquals('Australia', Countries.findACountryBytld('.au'))
+    self.assertEquals('AU', Countries.findAISOBytld('.au'))
+    self.assertEquals('.au', Countries.findAtldByCountry('Australia'))
+
+    self.assertIsNone(Countries.findAtldByCountry('Freedonia'))
+
+  def test_findManyProperties(self):
+    countries = Countries.findISOByCurrencyCode('EUR')
+    self.assertTrue('IE' in countries)
+    self.assertTrue('IT' in countries)
+    self.assertFalse('US' in countries) #What a day that'd be
+
+    #Go on break the build ;)
+    self.assertTrue('GB' in Countries.findISOByCurrencyCode('GBP'))
+    self.assertEquals([], Countries.findISOByCurrencyCode('Freedonia'))
+
+  def test_findDictionaries(self):
+    countries = Countries.findByCurrencyCode('EUR')
+
+    #We only get EUR
+    self.assertTrue(all([x['CURRENCYCODE'] == 'EUR' for x in countries]))
+    self.assertTrue('IT' in [x['ISO'] for x in countries])
+    self.assertTrue('IE' in [x['ISO'] for x in countries])
+    self.assertEquals([], Countries.findByCurrencyCode('GALLEON'))
+
+if __name__ == "__main__":
+  suite = unittest.TestLoader().loadTestsFromTestCase(TestCountries)
+  unittest.TextTestRunner(verbosity=2).run(suite)
