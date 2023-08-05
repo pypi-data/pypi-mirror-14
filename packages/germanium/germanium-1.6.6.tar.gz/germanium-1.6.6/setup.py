bfa3490d2b4f00baa948a5941d5fa350ac125f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package germanium/1.6.6/germanium-1.6.6.tar.gz

---
+++ packages/germanium/germanium-1.6.6.tar.gz/germanium-1.6.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='germanium',
+    version='1.6.6',
+    description='The germanium project: Selenium WebDriver testing API that doesn\'t disappoint.',
+    long_description = readme,
+    author='Bogdan Mustiata',
+    author_email='bogdan.mustiata@gmail.com',
+    license='BSD',
+
+    install_requires=['selenium==2.52.0'],
+    packages=['germanium',
+              'germanium.locators',
+              'germanium.selectors',
+              'germanium.static',
+              'germanium.util'],
+    package_data={'germanium': ['*.js']}
+)
