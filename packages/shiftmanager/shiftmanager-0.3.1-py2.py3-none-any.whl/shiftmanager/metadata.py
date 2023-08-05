ad8ac4314f6b7f9b4e2ea9821162a3b53d10aaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package shiftmanager/0.3.1/shiftmanager-0.3.1-py2.py3-none-any.whl

---
+++ packages/shiftmanager/shiftmanager-0.3.1-py2.py3-none-any.whl/shiftmanager/metadata.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'shiftmanager'
+project = 'shiftmanager'
+project_no_spaces = project.replace(' ', '')
+version = '0.3.1'
+description = 'Management tools for Amazon Redshift'
+authors = ['Jeff Klukas', 'Rob Story', 'Meli Lewis']
+authors_string = ', '.join(authors)
+emails = ['klukas@simple.com', 'rob@simple.com', 'meli@simple.com']
+license = 'BSD'
+copyright = '2015 ' + authors_string
+url = 'https://shiftmanager.readthedocs.org'
