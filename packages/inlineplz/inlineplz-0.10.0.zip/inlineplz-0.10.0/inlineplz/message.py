4487e330dac230f65168c0afa472f51befa44c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:49 +0000

    Add package inlineplz/0.10.0/inlineplz-0.10.0.zip

---
+++ packages/inlineplz/inlineplz-0.10.0.zip/inlineplz-0.10.0/inlineplz/message.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import os
+import traceback
+
+
+class Messages(object):
+
+    def __init__(self):
+        self.messages = {}
+
+    def add_message(self, path, line, message):
+        path = os.path.relpath(path).replace('\\', '/')
+        if (path, line) not in self.messages:
+            try:
+                self.messages[(path, line)] = Message(path, line)
+            except TypeError:
+                traceback.print_exc()
+        self.messages[(path, line)].append(message)
+
+    def add_messages(self, messages):
+        for message in messages:
+            self.add_message(*message)
+
+    def get_messages(self):
+        return self.messages.values()
+
+
+class Message(object):
+
+    def __init__(self, path, line_number):
+        self.path = os.path.relpath(path).replace('\\', '/')
+        self.line_number = int(line_number)
+        self.comments = set()
+
+    def __str__(self):
+        return """
+Message:
+    Path: {0}
+    Line number: {1}
+    Content: {2}
+        """.format(self.path, self.line_number, self.comments).strip()
+
+    def append(self, message):
+        self.comments.add(message)
