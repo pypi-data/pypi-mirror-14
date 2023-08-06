6860bcc19d2b901722db3eb07027ad060e9b1ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:09 +0000

    Add package smskit/0.0.3/smskit-0.0.3.tar.gz

---
+++ packages/smskit/smskit-0.0.3.tar.gz/smskit-0.0.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'requests>=2.9.1',
+    'lxml>=3.6.0'
+]
+VERSION = '0.0.3'
+
+setup(
+    name='smskit',
+    version=VERSION,
+    description='Abstack sms utils component.',
+    author='abstack',
+    author_email='mzj@abstack.com',
+    install_requires=install_requires,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='http://www.abstack.com/'
+)
