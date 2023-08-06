e13faf8b230a7efdf02e0287c044f181a47a70f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package sandpiper/0.5.3/sandpiper-0.5.3.tar.gz

---
+++ packages/sandpiper/sandpiper-0.5.3.tar.gz/sandpiper-0.5.3/sandpiper/adapter/inmemory.py
@@ -0,0 +1,24 @@
+from .abstract import Abstract
+from .abstract import NotSupported
+
+class InMemory(Abstract):
+    def __init__(self, storage=None):
+        self._storage = storage or {}
+
+    def get(self, key):
+        if key not in self._storage:
+            return None
+
+        return self._storage[key]
+
+    def set(self, key, value):
+        self._storage[key] = value
+
+    def remove(self, key):
+        if key not in self._storage:
+            return
+
+        del self._storage[key]
+
+    def find(self, *args, **kwargs):
+        raise NotSupported()
