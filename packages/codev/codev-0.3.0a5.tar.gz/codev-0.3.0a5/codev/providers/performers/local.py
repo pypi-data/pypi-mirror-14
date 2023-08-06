c77c80745cb8a71f5dad3d3b320a632402371f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package codev/0.3.0a5/codev-0.3.0a5.tar.gz

---
+++ packages/codev/codev-0.3.0a5.tar.gz/codev-0.3.0a5/codev/providers/performers/local.py
@@ -0,0 +1,47 @@
+from contextlib import contextmanager
+from subprocess import Popen, PIPE, call
+
+from codev.performer import CommandError, BasePerformer, Performer, OutputReader
+
+from logging import getLogger
+
+
+class LocalPerformer(BasePerformer):
+    def __init__(self, *args, **kwargs):
+        super(LocalPerformer, self).__init__(*args, **kwargs)
+        self.logger = getLogger(__name__)
+
+    def execute(self, command, logger=None, writein=None, max_lines=None):
+        self.logger.debug('Executing LOCAL command: %s' % command)
+        process = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
+
+        if writein:
+            # write writein to stdin
+            process.stdin.write(writein.encode())
+            process.stdin.flush()
+        process.stdin.close()
+
+        # read stdout asynchronously - in 'realtime'
+        output_reader = OutputReader(process.stdout, logger=logger or self.output_logger, max_lines=max_lines)
+
+        # wait for end of output
+        output = output_reader.output()
+
+        # wait for exit code
+        exit_code = process.wait()
+
+        if exit_code:
+            err = process.stderr.read().decode('utf-8').strip()
+            raise CommandError(command, exit_code, err)
+        return output
+
+    def send_file(self, source, target):
+        call(['cp', source, target])
+
+    @contextmanager
+    def get_fo(self, remote_path):
+        with open(remote_path) as fo:
+            yield fo
+
+
+Performer.register('local', LocalPerformer)
---
+++ packages/codev/codev-0.3.0a5.tar.gz/codev-0.3.0a5/codev/providers/provisions/ansible.py
@@ -0,0 +1,108 @@
+from codev.provision import Provisioner, BaseProvisioner
+from codev.configuration import BaseConfiguration
+# from os import environ
+import configparser
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+class AnsibleProvisionConfiguration(BaseConfiguration):
+    @property
+    def playbook(self):
+        return self.data.get('playbook', None)
+
+    @property
+    def version(self):
+        return self.data.get('version', None)
+
+    @property
+    def extra_vars(self):
+        return self.data.get('extra_vars', {})
+
+    @property
+    def env_vars(self):
+        return self.data.get('env_vars', {})
+
+
+class AnsibleProvision(BaseProvisioner):
+    configuration_class = AnsibleProvisionConfiguration
+
+    def install(self):
+        self.performer.execute('apt-get install python-dev python-pip -y --force-yes')
+        self.performer.execute('pip install setuptools')
+        self.performer.execute('pip install --upgrade markupsafe paramiko PyYAML Jinja2 httplib2 six ecdsa==0.11')
+
+        version_add = ''
+        if self.configuration.version:
+            version_add = '==%s' % self.configuration.version
+        self.performer.execute('pip install --upgrade ansible%s' % version_add)
+
+    def run(self, machines_groups):
+        playbook = self.configuration.playbook.format(infrastructure=self.infrastructure.name)
+
+        inventory = configparser.ConfigParser(allow_no_value=True, delimiters=('',))
+        for name, machines in machines_groups.items():
+            inventory.add_section(name)
+            for machine in machines:
+                # ansible node additional requirements
+                machine.install_package('python')
+                inventory.set(name, machine.host, '')
+
+        inventory_filepath = 'codev.ansible.inventory'
+
+        with open(inventory_filepath, 'w+') as inventoryfile:
+            inventory.write(inventoryfile)
+
+        template_vars = {
+            'working_dir': self.performer.working_dir
+        }
+
+        if self.configuration.extra_vars:
+            extra_vars = ' --extra-vars "{joined_extra_vars}"'.format(
+                joined_extra_vars=' '.join(
+                    [
+                        '{key}={value}'.format(
+                            key=key,
+                            value=value.format(**template_vars) if isinstance(value, str) else value
+                        ) for key, value in self.configuration.extra_vars.items()
+                    ]
+                )
+            )
+        else:
+            extra_vars = ''
+
+        # env = {}
+        # env.update(environ)
+        # env.update(self.configuration.env_vars)
+
+        if self.configuration.env_vars:
+            env_vars = '{joined_env_vars} '.format(
+                joined_env_vars=' '.join(
+                    [
+                        '{key}="{value}"'.format(
+                            key=key,
+                            value=value.format(**template_vars) if isinstance(value, str) else value
+                        ) for key, value in self.configuration.env_vars.items()
+                    ]
+                )
+            )
+        else:
+            env_vars = ''
+
+        self.performer.execute('{env_vars}ansible-playbook -i {inventory} {playbook}{extra_vars}'.format(
+            inventory=inventory_filepath,
+            playbook=playbook,
+            extra_vars=extra_vars,
+            env_vars=env_vars
+        ))
+        # self.performer.execute('{env_vars}ansible all -m ping -i {inventory}{extra_vars}'.format(
+        #     inventory=inventory_filepath,
+        #     playbook=playbook,
+        #     extra_vars=extra_vars,
+        #     env_vars=env_vars
+        # ))
+        return True
+
+
+Provisioner.register('ansible', AnsibleProvision)
