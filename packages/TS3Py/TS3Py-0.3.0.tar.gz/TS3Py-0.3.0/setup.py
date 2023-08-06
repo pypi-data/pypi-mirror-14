169b0e4d11f1d4336828d30bc58a483f805dd826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:34 +0000

    Add package TS3Py/0.3.0/TS3Py-0.3.0.tar.gz

---
+++ packages/TS3Py/TS3Py-0.3.0.tar.gz/TS3Py-0.3.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+
+    name='TS3Py',
+    version='0.3.0',
+    author='Thor77',
+    author_email='thor77@thor77.org',
+    description='A TS3Query-API',
+    keywords='ts3 pyts3 ts3py',
+    url='https://github.com/Thor77/TS3py',
+    long_description='''
+    A TS3Query-API.
+    Look at the doc or the github-page for further information!
+    ''',
+    py_modules=['ts3py', 'ts3utils'],
+
+)
