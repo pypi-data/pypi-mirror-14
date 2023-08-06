077f1320698d2353caea10b5b7c3f4e6cd2c5108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package contextio/1.10.2/contextio-1.10.2.tar.gz

---
+++ packages/contextio/contextio-1.10.2.tar.gz/contextio-1.10.2/contextio/lib/v2_0/errors.py
@@ -0,0 +1,7 @@
+class ArgumentError(Exception):
+    """Class to handle bad arguments."""
+    pass
+
+class MissingResourceId(Exception):
+    """Class to handle missing required argument on resource init."""
+    pass
