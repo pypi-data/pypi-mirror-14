76e589af64d4a9b47af5dcde47816f39ec7ef195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package germanium/1.7.2/germanium-1.7.2-py2-none-any.whl

---
+++ packages/germanium/germanium-1.7.2-py2-none-any.whl/germanium/util/__init__.py
@@ -0,0 +1,6 @@
+
+from .get_attributes import get_attributes_g
+from .mouse_actions import click_g, right_click_g, double_click_g, hover_g
+from .type_keys import type_keys_g
+from .drag_and_drop import drag_and_drop_g
+from germanium.impl import wait
