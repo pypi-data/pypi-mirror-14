f8cfa1972f7b3e352961bd41708bae72b4ceea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:29 +0000

    Add package simuvex/4.6.4.28/simuvex-4.6.4.28.tar.gz

---
+++ packages/simuvex/simuvex-4.6.4.28.tar.gz/simuvex-4.6.4.28/simuvex/procedures/libc___so___6/__init__.py
@@ -0,0 +1,18 @@
+
+#
+# offsets in struct _IO_FILE
+#
+_IO_FILE = {
+    'MIPS32': {
+        'size': 148,
+        'fd': 0x38,
+    },
+    'X86': {
+        'size': 148,
+        'fd': 0x38,
+    },
+    'AMD64': {
+        'size': 216,
+        'fd': 0x70,
+    },
+}
