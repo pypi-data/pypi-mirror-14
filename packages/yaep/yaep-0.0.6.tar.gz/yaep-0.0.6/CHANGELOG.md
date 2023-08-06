0b59019e5b54b543b3ead85df18346ef23eab759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package yaep/0.0.6/yaep-0.0.6.tar.gz

---
+++ packages/yaep/yaep-0.0.6.tar.gz/yaep-0.0.6/CHANGELOG.md
@@ -0,0 +1,8 @@
+A list of versions and changes to the project:
+
+- v0.0.6 - Remove setup_requires dependency on pypandoc.
+- v0.0.5 - Fix install error when installing from sdist (missing README)
+- v0.0.4 - Fix issue with pypandoc usage.
+- v0.0.3 - Fixed issue with boolean defaults, allow for type_class and UnsetExceptions.
+- v0.0.2 - Fixed issue with ConfigParser lowercasing keys.
+- v0.0.1 - Initial commit to GitHub.
---
+++ packages/yaep/yaep-0.0.6.tar.gz/yaep-0.0.6/setup.py
@@ -0,0 +1,48 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+
+try:
+    import pypandoc
+except ImportError:
+    class pypandoc(object):
+        @classmethod
+        def convert(self, data, type, format):
+            return data
+
+
+__version__ = 'Unset'
+with open('src/yaep/version.py') as vfp:
+    vd = vfp.read().strip()
+    __version__ = vd.split('=')[1].strip().strip('\'').strip('"')
+
+requirements = []
+if os.path.exists('requirements.txt'):
+    requirements = [r.strip() for r in open('requirements.txt')]
+
+
+readme = open('README.md').read()
+changelog = open('CHANGELOG.md').read()
+long_description = readme + '\n\n' + changelog
+
+
+config = {
+    'description': 'YAEP - Yet Another Environment Package',
+    'long_description': pypandoc.convert(long_description, 'rst', format='md'),
+    'author': 'James Kelly',
+    'author_email': 'pthread1981@gmail.com',
+    'license': 'ISC',
+    'url': 'https://github.com/jimjkelly/yaep',
+    'version': __version__,
+    'install_requires': requirements,
+    'tests_require': ['nose'],
+    'test_suite': 'nose.collector',
+    'packages': find_packages('src'),
+    'package_dir': {'': 'src'},
+    'scripts': [],
+    'name': 'yaep',
+    'zip_safe': False,
+}
+
+setup(**config)
