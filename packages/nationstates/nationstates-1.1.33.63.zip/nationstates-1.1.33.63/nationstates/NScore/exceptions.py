a5066ebb681f475e01924c6af6af5985cbae9061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:37 +0000

    Add package nationstates/1.1.33.63/nationstates-1.1.33.63.zip

---
+++ packages/nationstates/nationstates-1.1.33.63.zip/nationstates-1.1.33.63/nationstates/NScore/exceptions.py
@@ -0,0 +1,44 @@
+from requests.exceptions import ConnectionError
+from xml.parsers.expat import ExpatError
+
+class NSError(Exception):
+
+    """
+    BASE Exception
+    This Exception is used to detect if an Exception was thrown by
+    a nationstates module error instead of a python one
+    """
+    pass
+
+
+class NotFound(NSError):
+
+    """
+    Exception for a 404 NotFound Exception
+    """
+    pass
+
+
+class CollectError(NSError):
+    """Raised for errors in Api.collect()"""
+    pass
+
+
+class ShardError(NSError):
+    """Errors caused by incorrect Shard Object use"""
+
+
+class RateLimitCatch(NSError):
+    """Raised if the ratelimiter is unable to confirm rate limit saftey
+    """
+    pass
+
+
+class APIError(NSError):
+
+    """Api error
+    """
+    pass
+
+class APIRateLimitBan(APIError):
+    pass
