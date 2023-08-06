821aba84794da32eff44d97d15a1e4d1a28a78ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:38 +0000

    Add package nesterRM160408/1.4.0/nesterRM160408-1.4.0.tar.gz

---
+++ packages/nesterRM160408/nesterRM160408-1.4.0.tar.gz/nesterRM160408-1.4.0/nester.py
@@ -0,0 +1,16 @@
+"""This is the "nestor.py" module, and it provides one function
+called print_lol() which prints lists that may or may not include nested lists."""
+def print_lol(the_list, doIndent = False, level = 0, fh = sys.stdout):
+  """This function takes a positional agrument called "the_list", which is any
+  Python list (of, possibly, nested lists).  Each data item in the provided list 
+  is (recursively) printed to the screen on its own line. The level parameter
+  specifies how many tabs to indent the list elements with when printing.
+  The fy parameter specifies what output the data should be written to."""
+  for each_item in the_list:
+    if isinstance(each_item, list):
+      print_lol(each_item, doIndent, level + 1, fh)
+    else:
+      if doIndent:
+        for tab_stop in range(level):
+          print('\t', end='', file=fh)
+      print(each_item, file=fh)
