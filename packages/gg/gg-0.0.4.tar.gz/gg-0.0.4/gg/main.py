c696f7e1b79965a5688f8c7f5a50b3dd8664e196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:44 +0000

    Add package gg/0.0.4/gg-0.0.4.tar.gz

---
+++ packages/gg/gg-0.0.4.tar.gz/gg-0.0.4/gg/main.py
@@ -0,0 +1,44 @@
+import os
+from pkg_resources import iter_entry_points
+
+import click
+
+from . import state
+
+DEFAULT_CONFIG_FILE = os.path.expanduser('~/.gg.json')
+
+
+class Config(object):
+    def __init__(self):
+        self.verbose = False  # default
+        self.config_file = DEFAULT_CONFIG_FILE
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+@click.group()
+@click.option(
+    '-v', '--verbose',
+    is_flag=True
+)
+@click.option(
+    '--configfile',
+    default=DEFAULT_CONFIG_FILE,
+    help='Path to the config file'
+)
+@pass_config
+def cli(config, configfile, verbose):
+    """A glorious command line tool to make your life with git, GitHub
+    and Bugzilla much easier."""
+    config.verbose = verbose
+    config.configfile = configfile
+    if not os.path.isfile(configfile):
+        state.write(configfile, {})
+
+
+# Simply loading all installed packages that have this entry_point
+# will be enough. Each plugin automatically registers itself with the
+# cli click group.
+for entry_point in iter_entry_points(group='gg.plugin', name=None):
+    entry_point.load()
