65c7ce296a875d915559ace3e3faeafa0a32d102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:15 +0000

    Add package budy/0.1.6/budy-0.1.6-py2.py3-none-any.whl

---
+++ packages/budy/budy-0.1.6-py2.py3-none-any.whl/budy/models/bundle.py
@@ -0,0 +1,270 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Budy
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Budy.
+#
+# Hive Budy is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Budy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Budy. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import commons
+
+import appier
+
+from . import base
+from . import bundle_line
+
+class Bundle(base.BudyBase):
+
+    key = appier.field(
+        index = True,
+        safe = True,
+        immutable = True
+    )
+
+    currency = appier.field(
+        index = True
+    )
+
+    country = appier.field(
+        index = True
+    )
+
+    sub_total = appier.field(
+        type = commons.Decimal,
+        index = True,
+        initial = commons.Decimal(0.0),
+        safe = True
+    )
+
+    discount = appier.field(
+        type = commons.Decimal,
+        index = True,
+        initial = commons.Decimal(0.0),
+        safe = True
+    )
+
+    taxes = appier.field(
+        type = commons.Decimal,
+        index = True,
+        initial = commons.Decimal(0.0),
+        safe = True
+    )
+
+    total = appier.field(
+        type = commons.Decimal,
+        index = True,
+        initial = commons.Decimal(0.0),
+        safe = True
+    )
+
+    shipping_cost = appier.field(
+        type = commons.Decimal,
+        index = True,
+        initial = commons.Decimal(0.0),
+        safe = True
+    )
+
+    referrals = appier.field(
+        type = appier.references(
+            "Referral",
+            name = "name"
+        ),
+        eager = True
+    )
+
+    @classmethod
+    def validate(cls):
+        return super(Bundle, cls).validate() + [
+            appier.not_duplicate("key", cls._name()),
+
+            appier.not_null("sub_total"),
+            appier.gte("sub_total", 0.0),
+
+            appier.not_null("discount"),
+            appier.gte("discount", 0.0),
+
+            appier.not_null("taxes"),
+            appier.gte("taxes", 0.0),
+
+            appier.not_null("total"),
+            appier.gte("total", 0.0),
+
+            appier.not_null("shipping_cost"),
+            appier.gte("shipping_cost", 0.0)
+        ]
+
+    @classmethod
+    def list_names(cls):
+        return ["id", "key", "total", "currency"]
+
+    @classmethod
+    def order_name(self):
+        return ["id", -1]
+
+    @classmethod
+    def line_cls(cls):
+        return bundle_line.BundleLine
+
+    def pre_create(self):
+        base.BudyBase.pre_create(self)
+        if not hasattr(self, "key") or not self.key:
+            self.key = self.secret()
+        self.description = self.key[:8]
+
+    def pre_save(self):
+        base.BudyBase.pre_save(self)
+        self.calculate()
+
+    def empty_s(self):
+        for line in self.lines: line.delete()
+        self.lines = []
+        self.save()
+
+    def add_line_s(self, line):
+        line.save()
+        self.lines.append(line)
+        self.save()
+        return line
+
+    def remove_line_s(self, line_id):
+        match = None
+        for line in self.lines:
+            if not line.id == line_id: continue
+            match = line
+            break
+        if not match: return
+        self.lines.remove(match)
+        self.save()
+        match.delete()
+
+    def add_product_s(
+        self,
+        product,
+        quantity = 1.0,
+        size = None,
+        scale = None,
+        attributes = None,
+        increment = True
+    ):
+        cls = self.__class__
+
+        _line = None
+
+        if not product: raise appier.OperationalError(
+            message = "No product defined"
+        )
+
+        for line in self.lines:
+            is_same = line.product.id == product.id
+            is_same &= line.size == size
+            is_same &= line.scale == scale
+            is_same &= line.attributes == attributes
+            if not is_same: continue
+            _line = line
+
+        if _line:
+            if not increment: return _line
+            _line.quantity += quantity
+            _line.save()
+            self.save()
+            return _line
+
+        _line = cls.line_cls()(
+            product = product,
+            quantity = quantity,
+            size = size,
+            scale = scale,
+            attributes = attributes
+        )
+        self.add_line_s(_line)
+
+        return _line
+
+    def add_update_line_s(self, line, increment = True):
+        return self.add_product_s(
+            line.product,
+            quantity = line.quantity,
+            size = line.size,
+            scale = line.scale,
+            attributes = line.attributes,
+            increment = increment
+        )
+
+    def merge_s(self, bag_id, increment = False):
+        cls = self.__class__
+        if bag_id == self.id: return
+        bag = cls.get(id = bag_id)
+        for line in bag.lines:
+            line = line.clone()
+            self.add_update_line_s(line, increment = increment)
+        self.refresh_s()
+
+    def refresh_s(self, currency = None, country = None, force = False):
+        currency = currency or self.currency
+        country = country or self.country
+        is_dirty = self.is_dirty(currency = currency, country = country)
+        if not is_dirty and not force: return False
+        lines = self.lines if hasattr(self, "lines") else []
+        for line in lines:
+            is_dirty = line.is_dirty(
+                currency = currency,
+                country = country
+            )
+            if not is_dirty: continue
+            line.calculate(currency = currency, country = country)
+            line.save()
+        self.currency = currency
+        self.country = country
+        self.save()
+        return True
+
+    def calculate(self):
+        lines = self.lines if hasattr(self, "lines") else []
+        self.sub_total = sum(line.total for line in lines)
+        self.total = self.sub_total - self.discount + self.taxes
+
+    def is_dirty(self, currency = None, country = None):
+        dirty = False
+        lines = self.lines if hasattr(self, "lines") else []
+        for line in lines: dirty |= line.is_dirty(
+            currency = currency,
+            country = country
+        )
+        return dirty
+
+    @appier.operation(name = "Fix Sub Total")
+    def fix_sub_total_s(self):
+        if self.sub_total: return
+        self.sub_total = self.total
+        self.save()
---
+++ packages/budy/budy-0.1.6-py2.py3-none-any.whl/budy/test/order.py
@@ -0,0 +1,311 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Budy
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Budy.
+#
+# Hive Budy is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Budy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Budy. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import commons
+import logging
+import unittest
+
+import appier
+
+import budy
+
+class OrderTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = budy.BudyApp(level = logging.ERROR)
+
+    def tearDown(self):
+        self.app.unload()
+        adapter = appier.get_adapter()
+        adapter.drop_db()
+
+    def test_basic(self):
+        product = budy.Product(
+            short_description = "product",
+            gender = "Male",
+            price = 10.0
+        )
+        product.save()
+
+        self.assertEqual(product.short_description, "product")
+        self.assertEqual(product.gender, "Male")
+        self.assertEqual(product.price, 10.0)
+
+        order = budy.Order()
+        order.save()
+
+        self.assertEqual(type(order.key), appier.legacy.UNICODE)
+        self.assertEqual(type(order.total), commons.Decimal)
+        self.assertEqual(len(order.lines), 0)
+        self.assertEqual(order.id, 1)
+        self.assertEqual(order.reference, "BD-000001")
+        self.assertEqual(order.currency, None)
+        self.assertEqual(order.total >= 0.0, True)
+        self.assertEqual(order.paid, False)
+        self.assertEqual(order.date, None)
+        self.assertEqual(order.notification_sent, False)
+
+        self.assertRaises(appier.AssertionError, order.mark_paid_s)
+
+        order = order.reload()
+
+        self.assertEqual(type(order.key), appier.legacy.UNICODE)
+        self.assertEqual(type(order.total), commons.Decimal)
+        self.assertEqual(len(order.lines), 0)
+        self.assertEqual(len(order.reference), 9)
+        self.assertEqual(order.id, 1)
+        self.assertEqual(order.reference, "BD-000001")
+        self.assertEqual(order.currency, None)
+        self.assertEqual(order.total >= 0.0, True)
+        self.assertEqual(order.status, "created")
+        self.assertEqual(order.paid, False)
+        self.assertEqual(order.date, None)
+        self.assertEqual(order.notification_sent, False)
+        self.assertEqual(order.reference.startswith("BD-"), True)
+
+        self.assertRaises(appier.AssertionError, order.mark_paid_s)
+
+        order_line = budy.OrderLine(quantity = 2.0)
+        order_line.product = product
+        order_line.save()
+        order.add_line_s(order_line)
+
+        self.assertEqual(order_line.quantity, 2.0)
+        self.assertEqual(order_line.total, 20.0)
+        self.assertEqual(order.total, 20.0)
+        self.assertEqual(len(order.lines), 1)
+
+        self.assertRaises(appier.AssertionError, order.mark_paid_s)
+
+        address = budy.Address(
+            first_name = "first name",
+            last_name = "last name",
+            address = "address",
+            city = "city"
+        )
+        address.save()
+
+        order.shipping_address = address
+        order.billing_address = address
+        order.email = "username@email.com"
+        order.save()
+
+        order.mark_paid_s()
+
+        self.assertEqual(order.status, "paid")
+        self.assertEqual(order.paid, True)
+
+        self.assertRaises(appier.AssertionError, order.mark_paid_s)
+
+    def test_referral(self):
+        order = budy.Order()
+        order.save()
+
+        referral = budy.Referral(name = "name")
+        referral.save()
+
+        order.set_referral_s(referral)
+
+        self.assertEqual(len(order.referrals), 1)
+        self.assertEqual(order.referrals[0].name, "name")
+
+    def test_store(self):
+        product = budy.Product(
+            short_description = "product",
+            gender = "Male",
+            price = 10.0
+        )
+        product.save()
+
+        order = budy.Order()
+        order.save()
+
+        order_line = budy.OrderLine(quantity = 2.0)
+        order_line.product = product
+        order_line.save()
+        order.add_line_s(order_line)
+
+        self.assertEqual(order.currency, None)
+        self.assertEqual(order.payment_currency, None)
+
+        order.refresh_s(
+            currency = order.payment_currency or order.currency,
+            country = order.shipping_country or order.country
+        )
+
+        self.assertEqual(order.currency, None)
+        self.assertEqual(order.payment_currency, None)
+
+        store = budy.Store(name = "store", currency_code = "GBP")
+        store.save()
+
+        account = budy.BudyAccount(
+            username = "account",
+            email = "account@account.com",
+            password = "password",
+            password_confirm = "password",
+            store = store
+        )
+        account.save()
+
+        order.account = account
+        order.save()
+
+        self.assertEqual(order.account.username, "account")
+        self.assertEqual(order.store.name, "store")
+        self.assertEqual(order.currency, None)
+        self.assertEqual(order.payment_currency, "GBP")
+
+        order.refresh_s(
+            currency = order.payment_currency or order.currency,
+            country = order.shipping_country or order.country
+        )
+
+        self.assertEqual(order.currency, "GBP")
+        self.assertEqual(order.payment_currency, "GBP")
+
+    def test_voucher(self):
+        product = budy.Product(
+            short_description = "product",
+            gender = "Male",
+            price = 10.0
+        )
+        product.save()
+
+        order = budy.Order()
+        order.save()
+
+        order_line = budy.OrderLine(quantity = 2.0)
+        order_line.product = product
+        order_line.save()
+        order.add_line_s(order_line)
+
+        self.assertEqual(order_line.quantity, 2.0)
+        self.assertEqual(order_line.total, 20.0)
+        self.assertEqual(order.total, 20.0)
+        self.assertEqual(len(order.lines), 1)
+
+        address = budy.Address(
+            first_name = "first name",
+            last_name = "last name",
+            address = "address",
+            city = "city"
+        )
+        address.save()
+
+        order.billing_address = address
+        order.email = "username@email.com"
+        order.save()
+
+        voucher = budy.Voucher(amount = 5.0)
+        voucher.save()
+
+        order.add_voucher_s(voucher)
+
+        self.assertEqual(order.sub_total, 20.0)
+        self.assertEqual(order.discount, 5.0)
+        self.assertEqual(order.total, 15.0)
+        self.assertEqual(order.payable, 15.0)
+        self.assertEqual(isinstance(order.sub_total, commons.Decimal), True)
+        self.assertEqual(isinstance(order.discount, commons.Decimal), True)
+        self.assertEqual(isinstance(order.total, commons.Decimal), True)
+        self.assertEqual(isinstance(order.payable, commons.Decimal), True)
+
+        order.use_vouchers_s()
+        voucher = voucher.reload()
+
+        self.assertEqual(voucher.is_valid(), False)
+        self.assertEqual(voucher.used_amount, 5.0)
+        self.assertEqual(voucher.open_amount, 0.0)
+        self.assertEqual(voucher.usage_count, 1)
+
+        order.unmark_paid_s()
+
+        small_voucher = budy.Voucher(amount = 1.0)
+        small_voucher.save()
+
+        order.set_voucher_s(small_voucher)
+
+        self.assertEqual(order.sub_total, 20.0)
+        self.assertEqual(order.discount, 1.0)
+        self.assertEqual(order.total, 19.0)
+        self.assertEqual(order.payable, 19.0)
+        self.assertEqual(isinstance(order.sub_total, commons.Decimal), True)
+        self.assertEqual(isinstance(order.discount, commons.Decimal), True)
+        self.assertEqual(isinstance(order.total, commons.Decimal), True)
+        self.assertEqual(isinstance(order.payable, commons.Decimal), True)
+
+        large_voucher = budy.Voucher(amount = 100.0)
+        large_voucher.save()
+
+        order.set_voucher_s(large_voucher)
+
+        self.assertEqual(order.sub_total, 20.0)
+        self.assertEqual(order.discount, 20.0)
+        self.assertEqual(order.total, 0.0)
+        self.assertEqual(order.payable, 0.0)
+
+        order.use_vouchers_s()
+        voucher = large_voucher.reload()
+
+        self.assertEqual(voucher.is_valid(), True)
+        self.assertEqual(voucher.used_amount, 20.0)
+        self.assertEqual(voucher.open_amount, 80.0)
+        self.assertEqual(voucher.usage_count, 1)
+
+        order.unmark_paid_s()
+
+        percent_voucher = budy.Voucher(percentage = 10.0)
+        percent_voucher.save()
+
+        order.set_voucher_s(percent_voucher)
+
+        self.assertEqual(order.sub_total, 20.0)
+        self.assertEqual(order.discount, 2.0)
+        self.assertEqual(order.total, 18.0)
+        self.assertEqual(order.payable, 18.0)
+
+        order.use_vouchers_s()
+        voucher = percent_voucher.reload()
+
+        self.assertEqual(voucher.is_valid(), True)
+        self.assertEqual(voucher.percentage, 10.0)
+        self.assertEqual(voucher.used_amount, 0.0)
+        self.assertEqual(voucher.open_amount, 0.0)
+        self.assertEqual(voucher.usage_count, 1)
