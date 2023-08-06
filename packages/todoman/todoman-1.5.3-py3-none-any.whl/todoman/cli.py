c32381e68cc26ef44d7b38c433765f818535df90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:41 +0000

    Add package todoman/1.5.3/todoman-1.5.3-py3-none-any.whl

---
+++ packages/todoman/todoman-1.5.3-py3-none-any.whl/todoman/cli.py
@@ -0,0 +1,242 @@
+import glob
+import re
+from os.path import expanduser, isdir, join
+
+import click
+
+from .configuration import load_config
+from .main import dump_idfile, get_todo, task_sort_func
+from .model import Database, Todo
+from .ui import EditState, TodoEditor, TodoFormatter
+
+with_id_arg = click.argument('id', type=click.IntRange(0))
+
+
+def _validate_lists_param(ctx, param=None, lists=None):
+    if lists:
+        return [_validate_list_param(ctx, name=l) for l in lists]
+    else:
+        return ctx.obj['db'].values()
+
+
+def _validate_list_param(ctx, param=None, name=None):
+    if name in ctx.obj['db']:
+        return ctx.obj['db'][name]
+    else:
+        raise click.BadParameter(
+            "{}. Available lists are: {}"
+            .format(name, ', '.join(ctx.obj['db']))
+        )
+
+
+def _validate_due_param(ctx, param, due):
+    try:
+        return ctx.obj['formatter'].unformat_date(due)
+    except ValueError as e:
+        raise click.BadParameter(e)
+
+
+@click.group(invoke_without_command=True)
+@click.option('--human-time/--no-human-time', default=True,
+              help=('Accept informal descriptions such as "tomorrow" instead '
+                    'of a properly formatted date.'))
+@click.pass_context
+@click.version_option(prog_name='todoman')
+def cli(ctx, human_time):
+    config = load_config()
+    ctx.obj = {}
+    ctx.obj['config'] = config
+    ctx.obj['formatter'] = TodoFormatter(
+        config.get('main', 'date_format', fallback='%Y-%m-%d'),
+        human_time
+    )
+    ctx.obj['db'] = databases = {}
+
+    for path in glob.iglob(expanduser(config["main"]["path"])):
+        if not isdir(path):
+            continue
+        db = Database(path)
+        if databases.setdefault(db.name, db) is not db:
+            raise RuntimeError('Detected two databases named {}'
+                               .format(db.name))
+
+    if not ctx.invoked_subcommand:
+        ctx.invoke(cli.commands["list"])
+
+
+try:
+    import click_repl
+    click_repl.register_repl(cli)
+    click_repl.register_repl(cli, name="shell")
+except ImportError:
+    pass
+
+
+@cli.command()
+@click.argument('summary', nargs=-1)
+@click.option('--list', '-l', callback=_validate_list_param,
+              help='The list to create the task in.', required=True)
+@click.option('--due', '-d', default='', callback=_validate_due_param,
+              help=('The due date of the task, in the format specified in the '
+                    'configuration file.'))
+@click.option('--interactive', '-i', is_flag=True,
+              help='Go into interactive mode before saving the task.')
+@click.pass_context
+def new(ctx, summary, list, due, interactive):
+    '''
+    Create a new task with SUMMARY.
+    '''
+
+    todo = Todo()
+    todo.summary = ' '.join(summary)
+    todo.due = due
+
+    if interactive:
+        ui = TodoEditor(todo, ctx.obj['db'].values(), ctx.obj['formatter'])
+        if ui.edit() != EditState.saved:
+            ctx.exit(1)
+        click.echo()  # work around lines going missing after urwid
+
+    if not todo.summary:
+        raise click.UsageError('No SUMMARY specified')
+
+    list.save(todo)
+    print(ctx.obj['formatter'].detailed(todo, list))
+
+
+@cli.command()
+@click.pass_context
+@with_id_arg
+def edit(ctx, id):
+    '''
+    Edit a task interactively.
+    '''
+    todo, database = get_todo(ctx.obj['db'], id)
+    ui = TodoEditor(todo, ctx.obj['db'].values(), ctx.obj['formatter'])
+    state = ui.edit()
+    if state == EditState.saved:
+        database.save(todo)
+    elif state == EditState.deleted:
+        click.echo('Deleting {} ({})'.format(todo.uid, todo.summary))
+        database.delete(todo)
+
+
+@cli.command()
+@click.pass_context
+@with_id_arg
+def show(ctx, id):
+    '''
+    Show details about a task.
+    '''
+    todo, database = get_todo(ctx.obj['db'], id)
+    print(ctx.obj['formatter'].detailed(todo, database))
+
+
+@cli.command()
+@click.pass_context
+@click.argument('ids', nargs=-1, required=True, type=click.IntRange(0))
+def done(ctx, ids):
+    '''
+    Mark a task as done.
+    '''
+    for id in ids:
+        todo, database = get_todo(ctx.obj['db'], id)
+        todo.is_completed = True
+        database.save(todo)
+
+
+def _abort_if_false(ctx, param, value):
+    if not value:
+        ctx.abort()
+
+
+@cli.command()
+@click.pass_context
+@click.confirmation_option(
+    prompt='Are you sure you want to delete all done tasks?'
+)
+def flush(ctx):
+    '''
+    Delete done tasks.
+    '''
+    for database in ctx.obj['db'].values():
+        for todo in database.todos.values():
+            if todo.is_completed:
+                click.echo('Deleting {} ({})'.format(todo.uid, todo.summary))
+                database.delete(todo)
+
+
+@cli.command()
+@click.pass_context
+@click.argument('ids', nargs=-1, required=True, type=click.IntRange(0))
+@click.confirmation_option(
+    prompt='Are you sure you want to delete all those tasks?'
+)
+def delete(ctx, ids):
+    '''
+    Delete tasks.
+    '''
+    for id in ids:
+        todo, database = get_todo(ctx.obj['db'], id)
+        click.echo('Deleting {} ({})'.format(todo.uid, todo.summary))
+        database.delete(todo)
+
+
+@cli.command()
+@click.pass_context
+@click.option('--all', '-a', is_flag=True, help='Also show finished tasks.')
+@click.argument('lists', nargs=-1, callback=_validate_lists_param)
+@click.option('--urgent', is_flag=True, help='Only show urgent tasks.')
+@click.option('--location', help='Only show tasks with location containg TEXT')
+@click.option('--category', help='Only show tasks with category containg TEXT')
+@click.option('--grep', help='Only show tasks with message containg TEXT')
+def list(ctx, lists, all, urgent, location, category, grep):
+    """
+    List unfinished tasks.
+
+    If no arguments are provided, all lists will be displayed. Otherwise, only
+    todos for the specified list will be displayed.
+
+    eg:
+      \b
+      - `todo list' shows all unfinished tasks from all lists.
+      - `todo list work' shows all unfinished tasks from the list `work`.
+
+    This is the default action when running `todo'.
+    """
+
+    pattern = re.compile(grep) if grep else None
+    # FIXME: When running with no command, this somehow ends up empty:
+    lists = lists or ctx.obj['db'].values()
+
+    todos = sorted(
+        (
+            (database, todo)
+            for database in lists
+            for todo in database.todos.values()
+            if (not todo.is_completed or all) and
+               (not urgent or todo.priority) and
+               (not location or location in todo.location) and
+               (not category or category in todo.categories) and
+               (not pattern or (
+                   pattern.search(todo.summary) or
+                   pattern.search(todo.description)
+                   ))
+        ),
+        key=lambda x: task_sort_func(x[1]),
+        reverse=True
+    )
+    ids = {}
+
+    for index, (database, todo) in enumerate(todos, start=1):
+        ids[index] = (database.name, todo.filename)
+        try:
+            print("{:2d} {}".format(
+                index,
+                ctx.obj['formatter'].compact(todo, database)
+            ))
+        except Exception as e:
+            print("Error while showing {}: {}"
+                  .format(join(database.name, todo.filename), e))
+
+    dump_idfile(ids)
---
+++ packages/todoman/todoman-1.5.3-py3-none-any.whl/todoman/main.py
@@ -0,0 +1,68 @@
+import json
+import logging
+import os
+import sys
+from os.path import join
+
+import xdg.BaseDirectory
+
+logging.basicConfig(level=logging.ERROR)
+
+ID_FILE = join(xdg.BaseDirectory.xdg_cache_home, 'todoman', 'ids')
+
+
+def load_idfile():
+    try:
+        with open(ID_FILE) as f:
+            return dict(json.load(f))
+    except (ValueError, OSError, IOError):
+        pass
+
+    return {}
+
+
+def dump_idfile(ids):
+    assert isinstance(ids, dict)
+    dirname = os.path.dirname(ID_FILE)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    with open(ID_FILE, 'w') as f:
+        json.dump(list(ids.items()), f)
+
+
+def task_sort_func(todo):
+    """
+    Auxiliary function used to sort todos.
+
+    We put the most important items on the bottom of the list because the
+    terminal scrolls with the output.
+
+    Items with an immediate due date are considered more important that
+    those for which we have more time.
+    """
+
+    rv = (
+        -todo.priority,
+        todo.is_completed,
+        (todo.due.timestamp() if todo.due else float('inf')),
+        (-todo.created_at.timestamp() if todo.created_at else 0),
+        todo.uid  # make ordering deterministic, even if it makes no sense
+    )
+    return rv
+
+
+def get_todo(databases, todo_id):
+    ids = load_idfile()
+    if not ids:
+        print("List all tasks with `todo` to know the task number.")
+        sys.exit(1)
+
+    try:
+        db_name, todo_filename = ids[todo_id]
+        database = databases[db_name]
+        todo = database.todos[todo_filename]
+        return todo, database
+    except KeyError:
+        print("No todo with id {}.".format(todo_id))
+        sys.exit(-2)
+        # raise ValueError("No such todo {}.".format(todo_id)) from e
