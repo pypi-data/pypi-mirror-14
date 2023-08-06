fdb89007e36abdf783a487276edbcfa4e953fbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:45 +0000

    Add package dudan-utils/0.0.1/dudan-utils-0.0.1.zip

---
+++ packages/dudan-utils/dudan-utils-0.0.1.zip/dudan-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'dudan-utils',
+	version = '0.0.1',
+	py_modules = ['dudan_utils', 'test'],
+	author = 'dudan',
+	author_email = 'lendo.du@gmail.com',
+	url = 'http://www.uumind.com',
+	description = 'A utilities of devlopment.'
+) 
