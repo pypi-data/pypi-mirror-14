975b15dfe0d93b1e3322c6417db35c6597ee4f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package skymonitor/0.0.6/skymonitor-0.0.6.tar.gz

---
+++ packages/skymonitor/skymonitor-0.0.6.tar.gz/skymonitor-0.0.6/skyconf/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#! coding: utf-8
+
+import os
+import sys
+sys.path.append("/ect/skyline/")
+
+DEBUG = False
+
+try:
+    from test_conf import * # noqa
+except:
+    raise Exception("Please configure /etc/skyline/ directory!")
+#from dev_conf import * # noqa
+#from online_conf import * # noqa
+
+
+def get_email_list(alert_name):
+    global WARNIG_ALERT
+    for w in WARNING_ALERT:
+        if w.get("name", None) == alert_name:
+            return w.get("emails", [])
+
+
+def get_phone_list(alert_name):
+    global WARNIG_ALERT
+    for w in WARNING_ALERT:
+        if w.get("name", None) == alert_name:
+            return w.get("phones", [])
+
+
+def get_host_name():
+    hostname = os.popen("hostname").read().strip()
+    return hostname
