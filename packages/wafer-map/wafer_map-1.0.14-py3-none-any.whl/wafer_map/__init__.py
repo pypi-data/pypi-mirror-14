b7f89a1967b443cc6dab36fe31960992dc469989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:23 +0000

    Add package wafer-map/1.0.14/wafer_map-1.0.14-py3-none-any.whl

---
+++ packages/wafer-map/wafer_map-1.0.14-py3-none-any.whl/wafer_map/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+wafer_map
+=========
+
+Plots up a wafer map. Used in semiconductor processing and analysis.
+
+Please see README.md and CHANGELOG.md.
+
+"""
+
+### Constants ###############################################################
+__version__ = "1.0.14"
+__project_url__ = "https://github.com/dougthor42/wafer_map"
+__project_name__ = "wafer_map"
+__description__ = "Semiconductor Wafer Mapping"
+__long_descr__ = __doc__
+#__all__ = ['wm_app', 'wm_constants', 'wm_core', 'wm_frame', 'wm_info',
+#           'wm_legend', 'wm_utils']
