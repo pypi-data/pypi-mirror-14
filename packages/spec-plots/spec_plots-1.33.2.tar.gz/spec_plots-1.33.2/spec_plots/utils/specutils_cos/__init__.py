b814daa7c64ceb5bbb6824422a9741bc4d26c020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:54 +0000

    Add package spec-plots/1.33.2/spec_plots-1.33.2.tar.gz

---
+++ packages/spec-plots/spec_plots-1.33.2.tar.gz/spec_plots-1.33.2/spec_plots/utils/specutils_cos/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. module:: __init__
+
+   :synopsis: Used to treat "specutils_cos" as a directory and package.
+
+.. moduleauthor:: Scott W. Fleming <fleming@stsci.edu>
+"""
+
+from .cosspectrum import COSSpectrum
+from .check_segments import check_segments
+from .extract_subspec import extract_subspec
+from .plotspec import plotspec
+from .readspec import readspec
+from .get_segment_names import get_segment_names
