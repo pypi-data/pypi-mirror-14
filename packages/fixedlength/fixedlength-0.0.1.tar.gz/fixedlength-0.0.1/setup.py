ead90cd8a81a95ade0e1a332b916de9db8e5fbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:14 +0000

    Add package fixedlength/0.0.1/fixedlength-0.0.1.tar.gz

---
+++ packages/fixedlength/fixedlength-0.0.1.tar.gz/fixedlength-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="fixedlength",
+    description="Fixed length record parser",
+    version="0.0.1",
+    url="http://example.com", # TODO
+    author="Sam Geeraerts",
+    author_email="samgee@elmundolibre.be",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ]
+)
