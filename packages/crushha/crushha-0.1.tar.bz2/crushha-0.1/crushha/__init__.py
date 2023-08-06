37e2184674cb14735c71fd95ec62d8f3b621a647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:55 +0000

    Add package crushha/0.1/crushha-0.1.tar.bz2

---
+++ packages/crushha/crushha-0.1.tar.bz2/crushha-0.1/crushha/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Crush HA: crush maps from ceph used for defining HA"""
+
+__author__ = "Da_Blitz"
+__email__ = "code@pocketnix.org"
+__license__ = "BSD"
+__version__ = "0.1"
+
+import logging
+
+log = logging.getLogger(__name__)
