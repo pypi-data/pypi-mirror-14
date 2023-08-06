6642a63863136d062aac79e9507f9ab3ce8d66a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package machi/0.0.0/machi-0.0.0.zip

---
+++ packages/machi/machi-0.0.0.zip/machi-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='machi',
+      version='0.0.0',
+      description='Machina Time Series Generation and Backtesting',
+      url='https://machi.na',
+      author='Akram Kamal',
+      author_email='akamal@machi.na',
+      license='MIT',
+      packages=['machi'],
+      install_requires=['requests'],
+      zip_safe=False)
