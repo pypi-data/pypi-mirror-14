bcdf5d00e44e503f67a97734dcf42a41a949a74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:43 +0000

    Add package tcp_rsync/0.1rc0/tcp_rsync-0.1rc0.tar.gz

---
+++ packages/tcp_rsync/tcp_rsync-0.1rc0.tar.gz/tcp_rsync-0.1rc0/tcp_rsync/metadata.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'tcp_rsync'
+VERSION = '0.1.rc0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = (
+    '1. Quick Deployment Of Rsync Daemon For TCP Transmission; '
+    '2. Encapsulation For Commonly Used Operations. '
+)
