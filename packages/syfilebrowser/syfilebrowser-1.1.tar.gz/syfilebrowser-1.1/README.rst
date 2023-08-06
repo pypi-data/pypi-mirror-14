54da0daa15b98e1e1a958ec416fb1f3730588cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:54 +0000

    Add package syfilebrowser/1.1/syfilebrowser-1.1.tar.gz

---
+++ packages/syfilebrowser/syfilebrowser-1.1.tar.gz/syfilebrowser-1.1/README.rst
@@ -0,0 +1,8 @@
+
+Overview
+========
+
+syfilebrowser was created to be referenced as a 
+dependency for the `ShuyuCMS <http://shuyucms.com/>`_.
+
+This repository exists for bug fixes and minor enhancements.
---
+++ packages/syfilebrowser/syfilebrowser-1.1.tar.gz/syfilebrowser-1.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="syfilebrowser",
+    version="1.1",
+    description="a dependency for the ShuyuCMS.",
+    long_description=open("README.rst").read(),
+    author="Feichi Long",
+    author_email="feichi@longfeichi.com",
+    maintainer="Feichi Long",
+    maintainer_email="feichi@longfeichi.com",
+    url="http://zhidaoii.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
