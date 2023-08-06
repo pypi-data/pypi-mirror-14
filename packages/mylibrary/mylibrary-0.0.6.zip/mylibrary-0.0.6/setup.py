81d74f0222d5b39d51fc1b5ad4d4e01a3cd0a877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:46 +0000

    Add package mylibrary/0.0.6/mylibrary-0.0.6.zip

---
+++ packages/mylibrary/mylibrary-0.0.6.zip/mylibrary-0.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'mylibrary',
+    version = '0.0.6',
+    keywords = ('simple', 'test'),
+    description = 'just a simple test',
+    license = 'Apache License 2.0',
+    author = 'breeze',
+    author_email = 'whitney_0323@163.com', 
+    url          = 'https://github.com/rainmanwy/robotframework-SikuliLibrary', 
+    packages=['src'],
+
+)
