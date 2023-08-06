710d79dd48b814e3ac9cd771ef4d8c953894ae3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:36 +0000

    Add package hypertrack/0.2.3/hypertrack-0.2.3-py2.py3-none-any.whl

---
+++ packages/hypertrack/hypertrack-0.2.3-py2.py3-none-any.whl/hypertrack/test/helper.py
@@ -0,0 +1,34 @@
+import os
+
+from mock import Mock, patch
+
+
+base_url = os.environ.get('HYPERTRACK_API_BASE_URL')
+secret_key = os.environ.get('HYPERTRACK_SECRET_KEY')
+
+DUMMY_CUSTOMER = {
+}
+
+DUMMY_DESTINATION = {
+}
+
+DUMMY_FLEET = {
+}
+
+DUMMY_DRIVER = {
+}
+
+DUMMY_HUB = {
+}
+
+DUMMY_TASK = {
+}
+
+DUMMY_TRIP = {
+}
+
+DUMMY_GPSLOG = {
+}
+
+DUMMY_EVENT = {
+}
---
+++ packages/hypertrack/hypertrack-0.2.3-py2.py3-none-any.whl/hypertrack/test/test_exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import sys
+import unittest2
+
+from hypertrack.exceptions import HyperTrackException
+
+
+class ExceptionsTests(unittest2.TestCase):
+
+    def test_formatting(self):
+        excp = HyperTrackException(u'café')
+
+        if sys.version_info > (3, 0):
+            assert str(excp) == u'café'
+        else:
+            assert str(excp) == 'caf\xc3\xa9'
+            assert unicode(excp) == u'café'
