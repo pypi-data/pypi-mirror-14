8d7052591a216036c5bb21a71e8f0d676a516ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:30 +0000

    Add package mx_datagenerator/0.4.5/mx_datagenerator-0.4.5.tar.gz

---
+++ packages/mx_datagenerator/mx_datagenerator-0.4.5.tar.gz/mx_datagenerator-0.4.5/mx_datagen/__init__.py
@@ -0,0 +1,8 @@
+from mx_datagen.names import Name
+from mx_datagen.email import Email
+from mx_datagen.phone import Phone
+from mx_datagen.dates import Date
+from mx_datagen.postalcode import PostalCode
+from mx_datagen.company import Company
+
+print Date.last_payment_day()
