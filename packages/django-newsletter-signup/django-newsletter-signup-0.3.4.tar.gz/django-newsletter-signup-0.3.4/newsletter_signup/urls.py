27358c098fc08c76975b5089a11934d75e6ff3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:59 +0000

    Add package django-newsletter-signup/0.3.4/django-newsletter-signup-0.3.4.tar.gz

---
+++ packages/django-newsletter-signup/django-newsletter-signup-0.3.4.tar.gz/django-newsletter-signup-0.3.4/newsletter_signup/urls.py
@@ -0,0 +1,21 @@
+"""URLs for the newsletter_signup app."""
+from django.conf.urls import patterns, url
+
+from . import views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^signup/$',
+        views.NewsletterSignupView.as_view(),
+        name='newsletter_signup'),
+    url(r'^verify/(?P<uuid>.*)/',
+        views.NewsletterVerifyView.as_view(),
+        name='newsletter_verify'),
+    url(r'^unsubscribe/$',
+        views.NewsletterUnsubscribeView.as_view(),
+        name='newsletter_unscubscribe'),
+    url(r'^success/$',
+        views.NewsletterSignupSuccessView.as_view(),
+        name='newsletter_signup_success'),
+)
