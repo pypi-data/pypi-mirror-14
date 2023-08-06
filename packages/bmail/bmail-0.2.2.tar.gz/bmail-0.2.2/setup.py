7f2d4b7e86acb496f081790adea190aaf1360509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:36 +0000

    Add package bmail/0.2.2/bmail-0.2.2.tar.gz

---
+++ packages/bmail/bmail-0.2.2.tar.gz/bmail-0.2.2/setup.py
@@ -0,0 +1,43 @@
+
+config = {
+  "name": "bmail",
+  "version": "0.2.2",
+  "description": "Email library",
+  "url": "https://github.com/BlackEarth/bmail",
+  "author": "Sean Harrison",
+  "author_email": "sah@blackearthgroup.com",
+  "license": "LGPL 3.0",
+  "classifiers": [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    "Programming Language :: Python :: 3"
+  ],
+  "entry_points": {},
+  "install_requires": ["bl"],
+  "extras_require": {
+    "dev": [],
+    "test": []
+  },
+  "package_data": {
+    "": ["*.jar", "*.sql", "*.xml", "*.xsl", "*.md", "*.TEMPLATE"]
+  },
+  "data_files": ["setup.json"],
+  "scripts": []
+}
+
+import os, json
+from setuptools import setup, find_packages
+from codecs import open
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(path, 'README.rst'), encoding='utf-8') as f:
+    read_me = f.read()
+
+if __name__=='__main__':
+    setup(
+        long_description=read_me,
+        packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+        **config
+    )
