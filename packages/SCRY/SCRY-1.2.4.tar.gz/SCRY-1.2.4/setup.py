f9f78ba30b844eca6f55813f4a0d16ee7d358db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:47 +0000

    Add package SCRY/1.2.4/SCRY-1.2.4.tar.gz

---
+++ packages/SCRY/SCRY-1.2.4.tar.gz/SCRY-1.2.4/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+def get_resources():
+    rsc = list()
+    for d, subdirs, files in os.walk('scry/resources'):
+        rsc += [os.path.join(d[5:],f) for f in files]
+    return rsc
+
+setup(
+    name         = 'SCRY',
+    version      = '1.2.4',
+    description  = 'SPARQL Compatible seRvice laYer',
+    author       = 'Bas Stringer',
+    author_email = 'b.stringer@vu.nl',
+    url          = 'http://www.few.vu.nl/',
+    license      = 'MIT',
+    keywords     = 'scry sparql rdf linked open data service services custom customized extension',
+    classifiers  = ['Development Status :: 4 - Beta',
+                    'Environment :: Web Environment',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Information Technology',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python :: 2',
+                    'Programming Language :: Python :: 2.7',
+                    'Topic :: Internet :: WWW/HTTP :: WSGI',
+                    'Topic :: Scientific/Engineering',
+                    'Topic :: Software Development'],
+
+    install_requires     = ['pip>=8.0.0','flask>=0.10.1','rdflib>=4.2.1','sparqlwrapper>=1.7.6'],
+    packages             = ['scry','scry.query','scry.services','scry.app'],
+    package_data         = {'scry':get_resources()},
+    #include_package_data = True,
+    
+    entry_points = {'console_scripts'       : ['scry = scry.cmd:cmd'],
+                    'rdf.plugins.sparqleval': ['evalService = scry.query.patch:evalService']}
+)
