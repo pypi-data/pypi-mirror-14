40f120ec2dbeef57d60219bc64b1eac4774e2a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package epubcheck/0.1.1/epubcheck-0.1.1-py2.py3-none-any.whl

---
+++ packages/epubcheck/epubcheck-0.1.1-py2.py3-none-any.whl/epubcheck/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .wrap import validate
+
+__version__ = "0.1.1"
+__all__ = (validate, )
---
+++ packages/epubcheck/epubcheck-0.1.1-py2.py3-none-any.whl/epubcheck/samples/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from os import path
+
+ROOT = path.abspath(path.dirname(__file__))
+
+EPUB3_VALID = path.join(ROOT, 'childrens-literature.epub')
+EPUB3_INVALID = path.join(ROOT, 'childrens-literature-invalid.epub')
