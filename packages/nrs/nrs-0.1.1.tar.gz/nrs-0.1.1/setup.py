f5fe67838683e9fb5c1f3110ebd2af3a2e81b020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:38 +0000

    Add package nrs/0.1.1/nrs-0.1.1.tar.gz

---
+++ packages/nrs/nrs-0.1.1.tar.gz/nrs-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+bzlib = Extension('nrs.ext._bzlib', [
+    'nrs/ext/bzlib.i',
+    'nrs/ext/bzlib.c',
+    'nrs/ext/decompress.c',
+    'nrs/ext/huffman.c'
+], depends=['nrs/ext/bzlib.h'])
+
+setup(name='nrs',
+      version='0.1.1',
+      description='NSIS Reversing Suite',
+      author='isra17',
+      author_email='isra017@gmail.com',
+      url='https://github.com/isra17/nrs',
+      packages=['nrs','nrs.ext', 'nrs.ida'],
+
+      install_requires=['future'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      ext_modules=[bzlib]
+    )
+
