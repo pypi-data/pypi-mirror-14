ee98fa6edd170667bc895e0beb311bab2ae7375b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:57 +0000

    Add package numpy/1.10.3/numpy-1.10.3-cp26-cp26mu-manylinux1_x86_64.whl

---
+++ packages/numpy/numpy-1.10.3-cp26-cp26mu-manylinux1_x86_64.whl/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.10.3'
+version = '1.10.3'
+full_version = '1.10.3'
+git_revision = 'ea8aa221f98d0bbd24884e9d6e31fbb613f478f1'
+release = True
+
+if not release:
+    version = full_version
