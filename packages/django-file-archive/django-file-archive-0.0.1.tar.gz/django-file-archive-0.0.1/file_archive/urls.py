9fc290f77ec8c31254949d9db7321d1fbf08f0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:57 +0000

    Add package django-file-archive/0.0.1/django-file-archive-0.0.1.tar.gz

---
+++ packages/django-file-archive/django-file-archive-0.0.1.tar.gz/django-file-archive-0.0.1/file_archive/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+import .views
+
+
+urlpatterns = [
+    url( r'^(?P<slug>[\w\-]+)$', views.redirect_to_file, name='file_archive' ),
+]
---
+++ packages/django-file-archive/django-file-archive-0.0.1.tar.gz/django-file-archive-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-file-archive",
+    version = "0.0.1",
+    packages = find_packages(),
+    author = "Edmund von der Burg",
+    author_email = "evdb@ecclestoad.co.uk",
+    description = "Simple facility for uploading files in the Django admin",
+    license = "AGPL",
+    keywords = "django upload",
+    install_requires = [
+        'Django>=1.8',
+    ]
+)
