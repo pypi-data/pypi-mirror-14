4348ca80a6dca6e7e8b10a36539e18f751ed9c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:46 +0000

    Add package Token/1.4.0/Token-1.4.0.tar.gz

---
+++ packages/Token/Token-1.4.0.tar.gz/Token-1.4.0/Token/demos/demo1.py
@@ -0,0 +1,39 @@
+import Token
+
+Provider = Token.ProviderService('dev')
+Bank = Token.BankService('dev')
+
+
+sk1, pk1 = Token.create_keypair()
+sk2, pk2 = Token.create_keypair()
+
+alice = Provider.create_member('alice', '123', pk1)
+bob = Provider.create_member('bob', '123', pk2)
+
+
+Token.set_context('bank-test', Token.keys["bank"])
+
+access = Bank.create_access('alice', 'alice', 'test')
+access2 = Bank.create_access('bob', 'bob', 'test')
+
+
+Token.set_context(alice, sk1)
+Provider.create_alias('ALICE'+pk1[:5])
+
+alice_acc = Provider.create_account('test', access.id, "Personal Checking")
+Provider.get_account(alice_acc.id)
+
+
+Token.set_context(bob, sk2)
+bob_acc = Provider.create_account('test', access2.id, "Merchant Business Account")
+token = Provider.create_token('ALICE'+pk1[:5])
+
+Token.set_context(alice, sk1)
+Provider.endorse_token(token.id, alice_acc.id)
+
+
+Token.set_context(bob, sk2)
+payment = Provider.create_payment(token.id, bob_acc.id, 2, "EUR")
+
+Token.set_context(alice, sk1)
+Provider.get_account(alice_acc.id)
