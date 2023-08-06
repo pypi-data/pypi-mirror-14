084f57bb85cb450a204a3b505e7d91b9652b1123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:25 +0000

    Add package multilabel/0.2.2/multilabel-0.2.2.zip

---
+++ packages/multilabel/multilabel-0.2.2.zip/multilabel-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'multilabel',
+  py_modules = ['multilabel'],
+  version = '0.2.2',
+  description = 'A Python module that will create multi-line xlabels in matplotlib with left-side titles',
+  author = 'Ross Cohen',
+  author_email = 'rjc55@njit.edu',
+  url = 'https://github.com/BossColo/multilabel',
+  download_url = 'https://github.com/BossColo/multilabel/tarball/0.2.2',
+  keywords = ['Python', 'matplotlib', 'xlabels'],
+  classifiers = [],
+)
