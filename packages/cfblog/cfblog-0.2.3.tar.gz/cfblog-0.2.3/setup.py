87fc827dda8dbad7cc239bc72219ffacd461f6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:22 +0000

    Add package cfblog/0.2.3/cfblog-0.2.3.tar.gz

---
+++ packages/cfblog/cfblog-0.2.3.tar.gz/cfblog-0.2.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except Exception:
+    long_description = ""
+
+setup(
+    name="cfblog",
+    description="Awesome CMS for Django",
+    long_description=long_description,
+
+    version="0.2.3",
+    author='Vinay Karanam, Ashish Nayan',
+    author_email="vinayinvicible@gmail.com, nayanashish@gmail.com",
+
+    url='https://github.com/Coverfox/cfblog/',
+    license='BSD',
+
+    install_requires=[
+        "beautifulsoup4",
+        "Django>=1.7,<1.9",
+        "jsonfield",
+        "django-tagging",
+        "mistune",
+    ],
+    packages=['cfblog', 'cfblog.migrations'],
+    include_package_data=True,
+
+    zip_safe=True,
+)
