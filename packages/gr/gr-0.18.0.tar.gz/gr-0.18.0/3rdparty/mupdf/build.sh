f6573fce6e6eb7cb77640a62b3cd5a6b64da098c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package gr/0.18.0/gr-0.18.0.tar.gz

---
+++ packages/gr/gr-0.18.0.tar.gz/gr-0.18.0/3rdparty/mupdf/build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+cwd=`pwd`
+src="mupdf-1.6-source"
+if [ "$1" = "" ]; then
+  dest=`pwd`/../build
+else
+  dest=$1
+fi
+mkdir -p ${dest}/src
+cd ${dest}/src
+
+if [ ! -d "${src}" ]; then
+  if [ `which curl` ]; then
+    cmd="curl -O"
+  else
+    cmd="wget"
+  fi
+  ${cmd} http://mupdf.com/downloads/archive/${src}.tar.gz
+  tar -xf ${src}.tar.gz
+fi
+patch -N -p0 --dry-run --silent &>/dev/null <${cwd}/mupdf.patch &&\
+patch -p0 <${cwd}/mupdf.patch
+
+opts="prefix=${dest} HAVE_MUJS=no HAVE_CURL=no XCFLAGS=-fPIC"
+if [ `uname` = "Darwin" ]; then
+  opts="${opts} HAVE_X11=no"
+fi
+
+make -C ${src} ${opts}
+make -C ${src} ${opts} install
+
+cd ${cwd}
+
