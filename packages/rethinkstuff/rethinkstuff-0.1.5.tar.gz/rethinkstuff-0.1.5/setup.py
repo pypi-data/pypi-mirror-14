f910d353f70aa1b29e117243792f074dba8be74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:15 +0000

    Add package rethinkstuff/0.1.5/rethinkstuff-0.1.5.tar.gz

---
+++ packages/rethinkstuff/rethinkstuff-0.1.5.tar.gz/rethinkstuff-0.1.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='rethinkstuff',
+    version='0.1.5',
+    packages=['rethinkstuff'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=['rethinkdb'],
+    url='https://github.com/nlevitt/rethinkstuff',
+    author='Noah Levitt',
+    author_email='nlevitt@archive.org',
+    description='Rudimentary rethinkdb python library with some smarts, perhaps some dumbs',
+    long_description=open('README.rst').read(),
+)
