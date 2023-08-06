e72f75d74c832f1b60a1e76db243b9abf65ab099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:23 +0000

    Add package djangoshop-stripe/0.1.4/djangoshop-stripe-0.1.4.tar.gz

---
+++ packages/djangoshop-stripe/djangoshop-stripe-0.1.4.tar.gz/djangoshop-stripe-0.1.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+import shop_stripe
+try:
+    from pypandoc import convert
+except ImportError:
+    def convert(filename, fmt):
+        with open(filename) as fd:
+            return fd.read()
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
+
+setup(
+    author="Jacob Rief",
+    author_email="jacob.rief@gmail.com",
+    name='djangoshop-stripe',
+    version=shop_stripe.__version__,
+    description='Stripe Payment Provider Integration for django-shop',
+    long_description=convert('README.md', 'rst'),
+    url='https://github.com/jrief/djangoshop-stripe',
+    license='MIT License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'stripe>=1.26.0',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
