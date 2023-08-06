df9904245ccd049aa1b88a4ba28595c21e26b926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package test0408/1.0.0/test0408-1.0.0.tar.gz

---
+++ packages/test0408/test0408-1.0.0.tar.gz/test0408-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'test0408',
+    version = '1.0.0',
+    py_modules = ['test0408'],
+    author = 'irvin0916',
+    author_email = 'irvin0916@gmail.com',
+    url = 'https://www.google.com.tw',
+    description = 'just a testing',
+    )
