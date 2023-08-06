18de608018c6bc04c57ff605dd87133f47fe2de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:07 +0000

    Add package unifi/1.2.5/unifi-1.2.5.tar.gz

---
+++ packages/unifi/unifi-1.2.5.tar.gz/unifi-1.2.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import sys
+
+if sys.version_info[0] == 2:
+    from commands import getoutput
+elif sys.version_info[0] == 3:
+    from subprocess import getoutput
+
+
+setup(name='unifi',
+      version='1.2.5',
+      description='API towards Ubiquity Networks UniFi controller',
+      author='Jakob Borg',
+      author_email='jakob@nym.se',
+      url='https://github.com/calmh/unifi-api',
+      packages=['unifi'],
+      scripts=['unifi-low-snr-reconnect', 'unifi-ls-clients', 'unifi-save-statistics', 'unifi-log-roaming'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: System :: Networking']
+     )
