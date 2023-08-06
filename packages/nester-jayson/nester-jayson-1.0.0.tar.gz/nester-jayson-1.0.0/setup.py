7925afb152c96463459f6b737df3b26213cc8351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package nester-jayson/1.0.0/nester-jayson-1.0.0.tar.gz

---
+++ packages/nester-jayson/nester-jayson-1.0.0.tar.gz/nester-jayson-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup	(
+	name = 'nester-jayson',
+	version = '1.0.0',
+	py_modules = ['nester_jayson'],
+	author = 'jaysonpy',
+	author_email = 'huangzhenjie@126.com',
+	url = 'http://www.inewbird.com',
+	description = 'A simple printer of nested lists',
+	)
