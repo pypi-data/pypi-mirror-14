6511a254d57f5107e3a52438fc91605065935de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:54 +0000

    Add package haproxytool/0.5.3/haproxytool-0.5.3.tar.gz

---
+++ packages/haproxytool/haproxytool-0.5.3.tar.gz/haproxytool-0.5.3/haproxytool/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# _                                     _              _
+# | |__   __ _ _ __  _ __ _____  ___   _| |_ ___   ___ | |
+# | '_ \ / _` | '_ \| '__/ _ \ \/ / | | | __/ _ \ / _ \| |
+# | | | | (_| | |_) | | | (_) >  <| |_| | || (_) | (_) | |
+# |_| |_|\__,_| .__/|_|  \___/_/\_\\__, |\__\___/ \___/|_|
+#             |_|                  |___/'
+"""
+haproxytool
+~~~~~~~~~~~
+
+A tool to manage HAProxy via stats socket.
+"""
+__title__ = 'haproxytool'
+__author__ = 'Pavlos Parissis'
+__version__ = '0.5.3'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015-2016 Pavlos Parissis'
+
+OUR_CMDS = [
+    'haproxy',
+    'frontend',
+    'backend',
+    'server',
+    'dump',
+    'haproxy',
+    'map',
+    'acl'
+]
