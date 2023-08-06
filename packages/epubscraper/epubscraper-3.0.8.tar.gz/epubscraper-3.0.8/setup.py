cd7728ca4435042ca124a8d22ceb6caf13dd9b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package epubscraper/3.0.8/epubscraper-3.0.8.tar.gz

---
+++ packages/epubscraper/epubscraper-3.0.8.tar.gz/epubscraper-3.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='epubscraper',
+    version='3.0.8',
+    description='Post generator for eboipotro.github.io',
+    author='Utsob Roy',
+    author_email='uroybd@gmail.com',
+    url='https://eboipotro.github.io/',
+    py_modules = ['epubscraper'],
+    install_requires=[
+        'xmltodict',
+        'pillow'
+      ],
+)
