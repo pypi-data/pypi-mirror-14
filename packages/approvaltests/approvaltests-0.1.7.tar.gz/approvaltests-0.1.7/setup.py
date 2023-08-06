0b5cc2b253721293792749e106fd9053c41beb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package approvaltests/0.1.7/approvaltests-0.1.7.tar.gz

---
+++ packages/approvaltests/approvaltests-0.1.7.tar.gz/approvaltests-0.1.7/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='approvaltests',
+    version='0.1.7',
+    description='Assertion/verification library to aid testing',
+    author='ApprovalTests Contributors',
+    author_email='jamesrcounts@outlook.com',
+    url='https://github.com/approvals/ApprovalTests.Python',
+    packages=['approvaltests'],
+    include_package_data = True
+)
