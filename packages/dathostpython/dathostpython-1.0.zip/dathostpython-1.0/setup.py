e0c2734cc160b1dd81b8b22facd955ecca846fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:14 +0000

    Add package dathostpython/1.0/dathostpython-1.0.zip

---
+++ packages/dathostpython/dathostpython-1.0.zip/dathostpython-1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+
+setup(
+  name = 'dathostpython',
+  packages = ['dathostpython'],
+  version = '1.0',
+  description = 'A python wrapper for the Dathost API',
+  long_description = 'README.rst',
+  author = 'Miles Budden',
+  author_email = 'miles@budden.net',
+  url = 'https://github.com/pbexe/dathost-python',
+  keywords = ['dathost', 'hosting', 'wrapper'],
+  classifiers = [],
+)
