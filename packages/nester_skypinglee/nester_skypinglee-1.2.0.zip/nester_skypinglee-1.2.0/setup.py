3fae5b691daa7a209e66c0297e111137b1cac753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:56 +0000

    Add package nester_skypinglee/1.2.0/nester_skypinglee-1.2.0.zip

---
+++ packages/nester_skypinglee/nester_skypinglee-1.2.0.zip/nester_skypinglee-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name= 'nester_skypinglee',
+    version= '1.2.0',
+    py_modules = ['nester_skypinglee'],
+    author= 'python lee',
+    author_email= 'siskssk@python.com',
+    url = 'http://pypi.python.org/pypi',
+    description='A simple printer of nested lists',
+)
