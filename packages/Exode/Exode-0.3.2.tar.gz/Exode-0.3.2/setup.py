efab1cdaab8b0fdbdce4596a131415bc2dccf554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:57 +0000

    Add package Exode/0.3.2/Exode-0.3.2.tar.gz

---
+++ packages/Exode/Exode-0.3.2.tar.gz/Exode-0.3.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'Exode',
+  packages = ['Exode'],
+  package_data={'Exode':['*']},
+  install_requires=['pyserial'],
+  version = '0.3.2',
+  description = 'Exode is a Pythons library for communication between Arduino microcontroller boards and a connected computer.',
+  author = 'Lenselle Nicolas',
+  author_email = 'lenselle.nicolas@gmail.com',
+  url = 'https://github.com/sne3ks/Exode',
+  download_url = 'https://github.com/sne3ks/Exode/tarball/0.3',
+  license='APACHE 2.0'
+)
