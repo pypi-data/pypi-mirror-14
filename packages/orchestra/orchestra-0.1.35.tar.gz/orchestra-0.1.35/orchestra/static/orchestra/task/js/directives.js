5f4480fe2dfcc454f196ae61a8b102c65a9cfb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:53 +0000

    Add package orchestra/0.1.35/orchestra-0.1.35.tar.gz

---
+++ packages/orchestra/orchestra-0.1.35.tar.gz/orchestra-0.1.35/orchestra/static/orchestra/task/js/directives.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('orchestra.task.directives')
+    .directive('dynamicLoad', dynamicLoad);
+
+
+  dynamicLoad.$inject = ['$compile'];
+
+  function dynamicLoad($compile) {
+    return {
+      restrict: 'A',
+      replace: true,
+      link: function(scope, ele, attrs) {
+        scope.$watch(attrs.dynamicLoad, function(html) {
+          ele.html(html);
+          $compile(ele.contents())(scope);
+        });
+      }
+    };
+  }
+
+})();
