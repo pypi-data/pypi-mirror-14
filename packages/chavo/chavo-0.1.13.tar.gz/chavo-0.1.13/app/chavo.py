82e86aed4ff0fdb2239d2cb7fcae64b433faf663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:12 +0000

    Add package chavo/0.1.13/chavo-0.1.13.tar.gz

---
+++ packages/chavo/chavo-0.1.13.tar.gz/chavo-0.1.13/app/chavo.py
@@ -0,0 +1,16 @@
+__version__ = "0.1.13"
+
+import sys
+import os
+from package_store import *
+
+class main:
+	def __init__(self):
+		if len(sys.argv) > 1:
+			self.check_packages()
+		else:
+			print 'No function called, available packages:\n\toscar'
+
+	def check_packages(self):
+		if sys.argv[1] == 'oscar':
+			Oscar(sys.argv[2:])
---
+++ packages/chavo/chavo-0.1.13.tar.gz/chavo-0.1.13/setup.py
@@ -0,0 +1,28 @@
+import re
+from setuptools import setup, find_packages
+
+
+version = re.search(
+   '^__version__\s*=\s*"(.*)"',
+   open('app/chavo.py').read(),
+   re.M
+   ).group(1)
+
+
+with open("README.rst", "rb") as f:
+   long_descr = f.read().decode("utf-8")
+
+setup(
+   name = "chavo",
+   packages = find_packages(),
+   package_data={'': ['*.html', '*.css']},
+   entry_points = {
+       "console_scripts": ['chavo = app.chavo:main']
+       },
+   version = version,
+   description = "Command line html creator",
+   long_description = long_descr,
+   author = "Oscar Vazquez",
+   author_email = "oscar.vazquez2012@gmail.com",
+   url = "https://github.com/oscarvazquez/chavo"
+)
