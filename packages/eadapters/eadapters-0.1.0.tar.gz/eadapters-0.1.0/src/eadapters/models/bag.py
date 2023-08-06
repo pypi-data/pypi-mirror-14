71178bb8c268ba3f151d3221f13647b9f6f8f6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:23 +0000

    Add package eadapters/0.1.0/eadapters-0.1.0.tar.gz

---
+++ packages/eadapters/eadapters-0.1.0.tar.gz/eadapters-0.1.0/src/eadapters/models/bag.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import appier
+
+from . import base
+
+class EBag(base.EBase):
+
+    currency = appier.field()
+
+    total = appier.field(
+        type = float
+    )
+
+    lines = appier.field(
+        type = appier.references(
+            "SBagLine",
+            name = "id"
+        )
+    )
---
+++ packages/eadapters/eadapters-0.1.0.tar.gz/eadapters-0.1.0/src/eadapters/models/budy/bd_product.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from . import bd_common
+
+from .. import product
+
+class BDProduct(product.EProduct, bd_common.BDCommon):
+
+    @classmethod
+    def wrap(cls, models, build = True, handler = None, **kwargs):
+        def handler(model):
+            model.update(
+                name = model["short_description"],
+                currency = "GBP"
+            )
+
+        return super(BDProduct, cls).wrap(
+            models,
+            build = build,
+            handler = handler,
+            **kwargs
+        )
+
+    def unwrap(self, **kwargs):
+        result = product.EProduct.unwrap(self, **kwargs)
+        result.update(
+            short_description = self.name
+        )
+        return result
