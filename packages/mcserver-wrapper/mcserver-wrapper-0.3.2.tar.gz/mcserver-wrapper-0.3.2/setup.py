08a23cff3f45ebe19d617c1414888b20f928114b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:30 +0000

    Add package mcserver-wrapper/0.3.2/mcserver-wrapper-0.3.2.tar.gz

---
+++ packages/mcserver-wrapper/mcserver-wrapper-0.3.2.tar.gz/mcserver-wrapper-0.3.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from os import path
+import pypandoc
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='mcserver-wrapper',
+    version='0.3.2',
+    description="Minecraft server wrapper",
+    long_description=long_description,
+    url="https://github.com/Cynerd/mcserver-wrapper",
+    author="Cynerd",
+    author_email="cynerd@email.cz",
+    license="GPLv2",
+    install_requires=['pypandoc'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        ],
+    keywords='Minecraft wrapper server',
+
+    packages=['mcwrapper'],
+    entry_points={
+        'console_scripts': [
+            'mcwrapper=mcwrapper:main'
+            ]
+        }
+    )
