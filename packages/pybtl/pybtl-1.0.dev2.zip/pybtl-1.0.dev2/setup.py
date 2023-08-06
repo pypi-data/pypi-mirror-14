a8377b2aba294687b996f31331a24be0efa2db11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:31 +0000

    Add package pybtl/1.0.dev2/pybtl-1.0.dev2.zip

---
+++ packages/pybtl/pybtl-1.0.dev2.zip/pybtl-1.0.dev2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+        """ Return content of specified file """
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pybtl',
+    description=('Library for serializing python objects as tightly packed '
+                 'bit arrays'),
+    keywords='binary serialization deserialization bits',
+    version='1.0.dev2',
+    author='Outernet Inc',
+    author_email='apps@outernet.is',
+    license='BSD',
+    url='https://github.com/Outernet-Project/pybtl',
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    install_requires=[
+        'bitarray',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+)
