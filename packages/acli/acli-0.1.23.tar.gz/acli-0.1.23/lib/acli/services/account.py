04935bd966530cbeed2a893f7b3e92f2e0983a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package acli/0.1.23/acli-0.1.23.tar.gz

---
+++ packages/acli/acli-0.1.23.tar.gz/acli-0.1.23/lib/acli/services/account.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, print_function, unicode_literals)
+from acli.output.account import output_account_info
+from acli.connections import get_client
+
+
+def account_info(aws_config):
+    """
+    @type aws_config: Config
+    """
+    iam_client = get_client(client_type='iam', config=aws_config)
+    users = iam_client.list_users(MaxItems=1)
+    if users.get('Users'):
+        account_id = users.get('Users')[0]['Arn'].split(':')[4]
+        aliases = iam_client.list_account_aliases().get('AccountAliases')
+        output_account_info(account_id=account_id,
+                            account_aliases=aliases)
---
+++ packages/acli/acli-0.1.23.tar.gz/acli-0.1.23/lib/acli/services/elb.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, print_function, unicode_literals)
+from acli.output.elb import output_elbs, output_elb_info
+from acli.connections import get_client
+import botocore.exceptions
+
+
+def get_elb_list(aws_config):
+    """
+    @type aws_config: Config
+    """
+    elb_client = get_client(client_type='elb', config=aws_config)
+    elbs = elb_client.describe_load_balancers().get('LoadBalancerDescriptions')
+    return elbs
+
+
+def get_elb(aws_config, elb_name=None):
+    """
+    @type aws_config: Config
+    @type elb_name: unicode
+    """
+    if elb_name:
+        try:
+            elb_client = get_client(client_type='elb', config=aws_config)
+            elbs = elb_client.describe_load_balancers(LoadBalancerNames=[elb_name])
+            if elbs and elbs.get('LoadBalancerDescriptions'):
+                return elbs.get('LoadBalancerDescriptions')[0]
+        except botocore.exceptions.ClientError:
+            exit('ELB: {0} could not be found.'.format(elb_name))
+
+
+def elb_list(aws_config):
+    """
+    @type aws_config: Config
+    """
+    output_elbs(elbs=get_elb_list(aws_config))
+
+
+def elb_info(aws_config=None, elb_name=None):
+    """
+    @type aws_config: Config
+    @type elb_name: unicode
+    """
+    output_elb_info(elb=get_elb(aws_config,
+                                elb_name=elb_name))
