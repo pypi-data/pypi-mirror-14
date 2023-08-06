d98f6de2342c25a584831ecf46fb603814db994d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:40 +0000

    Add package nester_fbmm/1.0.0/nester_fbmm-1.0.0.tar.gz

---
+++ packages/nester_fbmm/nester_fbmm-1.0.0.tar.gz/nester_fbmm-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name         =   'nester_fbmm',
+	version      =   '1.0.0',	 
+	py_modules   =   ['nester'],
+	author	     =   'bmorapython',
+	author_email =   'fbmora88@gmail.com',
+	url          =   'http://www.headfirstlabs.com',
+        description  =	 'A simple printer of nested lists',
+)
