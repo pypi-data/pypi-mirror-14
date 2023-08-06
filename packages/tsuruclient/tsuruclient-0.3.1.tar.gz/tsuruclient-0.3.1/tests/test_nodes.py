8024916d862bd50ccae617ecd244c0767923e07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:15 +0000

    Add package tsuruclient/0.3.1/tsuruclient-0.3.1.tar.gz

---
+++ packages/tsuruclient/tsuruclient-0.3.1.tar.gz/tsuruclient-0.3.1/tests/test_nodes.py
@@ -0,0 +1,59 @@
+from tsuruclient import client
+
+import json
+import unittest
+import httpretty
+
+
+class NodesTestCase(unittest.TestCase):
+    def setUp(self):
+        httpretty.enable()
+
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
+    def test_create_node(self):
+        node_data = {
+            "Status": "ready",
+            "Metadata": {
+                "pool": "tsuru2",
+                "iaas": "ec2",
+                "LastSuccess": "2015-11-16T18:44:36-02:00",
+            },
+            "Address": "http://127.0.0.3:4243"
+        }
+        url = "http://target/docker/node"
+        httpretty.register_uri(
+            httpretty.POST,
+            url,
+            body=json.dumps(node_data),
+            status=200
+        )
+
+        cl = client.Client("http://target", "abc123")
+
+        data = {"address": "127.0.0.3:4243", "pool": "tsuru2"}
+        result = cl.nodes.create(**data)
+
+        self.assertDictEqual(result.json(), node_data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+        self.assertEqual("false", httpretty.last_request().querystring["register"][0])
+
+        result = cl.nodes.create(register=False, **data)
+
+        self.assertDictEqual(result.json(), node_data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+        self.assertEqual("false", httpretty.last_request().querystring["register"][0])
+
+        result = cl.nodes.create(register=True, **data)
+
+        self.assertDictEqual(result.json(), node_data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+        self.assertEqual("true", httpretty.last_request().querystring["register"][0])
+
+        result = cl.nodes.create(register="invalid", **data)
+
+        self.assertDictEqual(result.json(), node_data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+        self.assertEqual("false", httpretty.last_request().querystring["register"][0])
---
+++ packages/tsuruclient/tsuruclient-0.3.1.tar.gz/tsuruclient-0.3.1/tsuruclient/nodes.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+from base import Manager as Base
+
+
+class Manager(Base):
+    """
+    Manage Node resources.
+    """
+
+    def create(self, register=False, **kwargs):
+        """
+        Create a node.
+        """
+        register = "true" if register is True else "false"
+        response = requests.post(
+            "{}/docker/node?register={}".format(self.target, register),
+            data=json.dumps(kwargs),
+            headers=self.headers,
+            stream=True
+        )
+        return response
