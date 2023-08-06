09d7a38f3cc9667a0183ea17e508238179a6a184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:36 +0000

    Add package ovh/0.4.2/ovh-0.4.2.tar.gz

---
+++ packages/ovh/ovh-0.4.2.tar.gz/ovh-0.4.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    setup_requires=['d2to1'],
+    d2to1=True,
+    package_data={
+        'ovh.vendor.requests': ['*.pem'],
+    },
+    include_package_data=True,
+    tests_require=[
+        "coverage==3.7.1",
+        "mock==1.0.1",
+        "nose==1.3.3",
+        "yanc==0.2.4",
+    ],
+)
