bce34b25d16dfd30c1766dafc4100ea8c9eeef28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package planemo/0.24.1/planemo-0.24.1.tar.gz

---
+++ packages/planemo/planemo-0.24.1.tar.gz/planemo-0.24.1/planemo/commands/cmd_brew_init.py
@@ -0,0 +1,27 @@
+import click
+
+import urllib
+from tempfile import mkstemp
+
+from planemo.cli import pass_context
+from planemo.io import shell
+
+
+INSTALL_SCRIPT = "https://raw.github.com/Homebrew/linuxbrew/go/install"
+
+
+@click.command('brew_init')
+@pass_context
+def cli(ctx):
+    """Download linuxbrew install and run it with ruby. Linuxbrew is a fork
+    of Homebrew (http://brew.sh/linuxbrew/).
+
+    For more information on installing linuxbrew and pre-requisites see
+    https://github.com/Homebrew/linuxbrew#installation.
+
+    Homebrew or linuxbrew are required in order to use the other commands
+    ``brew`` and ``brew_shell``.
+    """
+    fname = mkstemp('install_brew')
+    urllib.urlretrieve(INSTALL_SCRIPT, fname)
+    shell(["ruby", fname])
