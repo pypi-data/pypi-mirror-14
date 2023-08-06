d4241b048380d84f79aeb081b98c6a391fdab792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:08 +0000

    Add package super_nester/1.2.0/super_nester-1.2.0.zip

---
+++ packages/super_nester/super_nester-1.2.0.zip/super_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name         = 'super_nester',
+        version      = '1.2.0',
+        py_modules   = ['nester'],
+        author       = 'super',
+        author_email = '690947509@qq.com',
+        url          = 'http://weibo.com/ranranhihi',
+        description  = 'A simple printer of nested lists',
+    )
