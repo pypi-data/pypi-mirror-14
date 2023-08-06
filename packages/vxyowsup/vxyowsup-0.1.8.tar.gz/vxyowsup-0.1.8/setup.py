0dac91d8f6b6dd8a808d317f391ace1c6b0d4220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:06 +0000

    Add package vxyowsup/0.1.8/vxyowsup-0.1.8.tar.gz

---
+++ packages/vxyowsup/vxyowsup-0.1.8.tar.gz/vxyowsup-0.1.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vxyowsup",
+    version="0.1.8",
+    url='http://github.com/praekelt/vumi-yowsup',
+    license='BSD',
+    description="A WhatsApp transport for Vumi.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vumi',
+        'Twisted>=13.1.0',
+        'yowsup2',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
