6e9a3c392ea1bd04695e9d3d85af16c0178b6272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package ifs/0.8.0/ifs-0.8.0.tar.gz

---
+++ packages/ifs/ifs-0.8.0.tar.gz/ifs-0.8.0/ifs/source/golang.py
@@ -0,0 +1,9 @@
+version = '1.6'
+version_cmd = 'go version'
+version_re = 'go version go([\d\.]+)'
+download_url = 'https://storage.googleapis.com/golang/goVERSION.linux-amd64.tar.gz'
+install_script = """
+tar -C /usr/local -xzf goVERSION.linux-amd64.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" > /etc/profile.d/golang.sh
+grep "/usr/local/go/bin" /etc/bash.bashrc >/dev/null || echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/bash.bashrc
+"""
