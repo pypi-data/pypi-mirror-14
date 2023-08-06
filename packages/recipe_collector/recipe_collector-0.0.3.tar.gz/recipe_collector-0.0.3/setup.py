c412160a419626bcfa531959a6ce6d311edf0c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:15 +0000

    Add package recipe_collector/0.0.3/recipe_collector-0.0.3.tar.gz

---
+++ packages/recipe_collector/recipe_collector-0.0.3.tar.gz/recipe_collector-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='recipe_collector',
+    version='0.0.3',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    description='Collect and process recipes from various sources',
+    url='https://github.com/projectweekend/recipe-collector',
+    license='MIT',
+    packages=find_packages(),
+    py_modules=[],
+    scripts=[],
+    install_requires=[],
+)
