add2e96f84b8f907d1698b4331b75e23b3747c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:21 +0000

    Add package jeff919_nester/1.4.0/jeff919_nester-1.4.0.zip

---
+++ packages/jeff919_nester/jeff919_nester-1.4.0.zip/jeff919_nester-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name         = 'jeff919_nester',
+    version      = '1.4.0',
+    py_modules   = ['jeff919_nester'],
+    author       = 'jeffenjoy',
+    author_email = 'jeffenjoy9199@gmail.com',
+    url          = 'https://www.facebook.com/jeffenjoy9199',
+    description  = 'A simple printer of nested lists',
+    )
+
