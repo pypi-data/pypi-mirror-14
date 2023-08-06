faa42bfa623ffa39603e03bb18b37b6f4ca09cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package wy_nester/1.2.0/wy_nester-1.2.0.zip

---
+++ packages/wy_nester/wy_nester-1.2.0.zip/wy_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+name='wy_nester',
+version='1.2.0',
+py_modules=['wy_nester'],
+author='wy',
+author_email='wangyoyo91@163.com',
+url='http://www.headfirstlabs.com',
+description='wy',
+)
+
