6d004362454fff6f3022d6047c5382a63d507966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:43 +0000

    Add package flowtool-git/0.7.16/flowtool-git-0.7.16.tar.gz

---
+++ packages/flowtool-git/flowtool-git-0.7.16.tar.gz/flowtool-git-0.7.16/flowtool_git/config.py
@@ -0,0 +1,36 @@
+from flowtool.style import colors, echo
+from collections import namedtuple, defaultdict
+from .common import local_git_command
+
+def list_config(local=False):
+    if local:
+        return local_git_command().config('--local', '--list')
+    else:
+        return local_git_command().config('--list')
+
+
+def getconfig_simple(local=True):
+    dump = list_config(local)
+    config = defaultdict(dict)
+    for line in dump.split('\n'):
+        key, value = line.split('=', 1)
+        k1, k2 = key.split('.', 1)
+        config[k1][k2] = value
+    return config
+
+
+#ConfigHook = namedtuple('ConfigHook', ['name', 'active', 'key', 'value'])
+
+#def gather_config_hooks(repo=None):
+    #cfg = getconfig_simple(repo)
+    #found = []
+    #for key in [k for k in cfg if k.startswith('hooks.')]:
+        #echo.yellow('configured hook:', key)
+        #info = ConfigHook(
+            #name=key[6:],
+            #active=True,
+            #key=key,
+            #value=cfg[key],
+        #)
+        #found.append(info)
+    #return found
