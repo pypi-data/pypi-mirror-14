c2bdd3f41cdd7516141df43b6fc69cbad5c13826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:13 +0000

    Add package greenenv/0.0.2/greenenv-0.0.2-py3.5.egg

---
+++ packages/greenenv/greenenv-0.0.2-py3.5.egg/with_venv/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .with_venv import ExtendedEnvBuilder, artifact_path
+
+__author__ = 'Andrew B Godbehere'
+__email__ = 'andrew.godbehere@sumupanalytics.com'
+__version__ = '0.0.1'
+
+
+env_factory = ExtendedEnvBuilder()
