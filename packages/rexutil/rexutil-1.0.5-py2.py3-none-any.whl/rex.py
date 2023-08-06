b9e8af16edec164cab5960cf26e3c509aee4500d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:35 +0000

    Add package rexutil/1.0.5/rexutil-1.0.5-py2.py3-none-any.whl

---
+++ packages/rexutil/rexutil-1.0.5-py2.py3-none-any.whl/rex.py
@@ -0,0 +1,428 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Copyright 2015 Behzad Dastur
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+'''
+REX: A general purpose string and pattern matching utility
+built as a wrapper on python re to provide additional functionalit
+and ease of use. For more details check the README.md file.'''
+
+import re
+import os
+
+__version__ = "0.0.1"
+
+
+class REX(object):
+    '''
+    REX: Class instance.
+    '''
+    def __init__(self, pattern, filename=None):
+        '''
+        Initialization.
+        '''
+        self.user_pattern = pattern
+        self.search_file = filename
+        self.rex_patternstr = None
+        self.rex_pattern = None
+        # Results store the list of result object.
+        # result object type REXResult
+        self.matches = []
+        self.res_count = 0
+
+
+class REXResult(object):
+    '''
+    REX Result class.
+    '''
+    def __init__(self, reobj, loc):
+        '''
+        Initialization.
+        '''
+        self.reobj = reobj
+        self.loc = loc
+        self.named_groups = reobj.groupdict()
+
+
+def reformat_pattern(pattern, compile=False):
+    '''
+    Apply the filters on user pattern to generate a new regular expression
+    pattern.
+    A user provided variable, should start with an alphabet, can be
+    alphanumeric and can have _.
+    '''
+    # User pattern: (w:<name>)  --> Changes to (?P<name>\w)
+    rex_pattern = re.sub(r'\(w:<([\w\d_]+)>\)', '(?P<\\1>\w+)', pattern)
+
+    # User pattern: (d:<name>)  --> change to (?P<name>\d)
+    rex_pattern = re.sub(r'\(d:<([\w\d_]+)>\)', '(?P<\\1>\d+)', rex_pattern)
+
+    # User pattern: (W:<name>)  --> change to (?P<name>\w)
+    rex_pattern = re.sub(r'\(W:<([\w\d_]+)>\)', '(?P<\\1>\W+)', rex_pattern)
+
+    # User pattern: (any:<anyvalue> --> change to (?P<anyvalue>.*)
+    rex_pattern = re.sub(r'\(any:<([\w\d_]+)>\)', '(?P<\\1>.*)', rex_pattern)
+
+    # User pattern: (ip:<ipaddr>) --> Change to (?P<ipaddr>\d+\.\d+\.\d+\.\d+)
+    rex_pattern = re.sub(r'\(ip:<([\w\d_]+)>\)',
+                         '(?P<\\1>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',
+                         rex_pattern)
+
+    # User pattern: (mac:<macaddr>) --> Change to (?P<mac>\w\w:\w\w:\w\w:..)
+    rex_pattern = re.sub(r'\(mac:<([\w\d_]+)>\)',
+                         '(?P<\\1>\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)', rex_pattern)
+
+    # User pattern: (decimal:<name>) --> Change to (?P<name>\d*\.\d+|\d+)
+    rex_pattern = re.sub(r'\(decimal:<([\w\d_]+)>\)',
+                         '(?P<\\1>\d*\.\d+|\d+)', rex_pattern)
+
+    # User pattern: (measurement:<name> --> change to \
+    # (?P<name>\d*\.\d+|\d+)(\w+|\w+/\w+)
+    rex_pattern = re.sub(r'\(measurement:<([\w\d_]+)>\)',
+                         '(?P<\\1>\d*\.\d+|\d+)(?P<\\1_unit>\w+|\w+/\w+)',
+                         rex_pattern)
+
+
+    ######################################
+    # Timestamp patterns.
+    # User pattern: (ts[n]:<timestamp>) -->
+    # Converted to: The below options.
+    ######################################
+
+    # User pattern: (ts1:<timestamp>)
+    # Keystone, nova, libvirt, cinder
+    # Example: 2015-11-13 06:38:04.571
+    rex_pattern = re.sub(
+        r'\(ts1:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d{4})-(\d{1,2})-(\d{1,2}) (\d+):(\d+):(\d+)\.(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts2:<timestamp>)
+    # contrail
+    # Example: 2015-11-13 Fri 13:14:51:907.395 PST
+    rex_pattern = re.sub(
+        r'\(ts2:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d{4})-(\d{1,2})-(\d{1,2}) (\w{3}) (\d+):(\d+):(\d+):(\d+)\.(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts3:<timestamp>)
+    # apache2
+    # Example: Thu Aug 20 14:18:34 2015
+    rex_pattern = re.sub(
+        r'\(ts3:<([\w\d_]+)>\)',
+        '(?P<\\1>(\w{3}) (\w{3}) (\d{1,2}) (\d+):(\d+):(\d+) (\d{4}))',
+        rex_pattern)
+
+    # User pattern: (ts4:<timestamp>)
+    # Example: 02/Nov/2015:09:03:19 -0800
+    rex_pattern = re.sub(
+        r'\(ts4:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d+)\/(\w{3})\/(\d{4}):(\d+):(\d+):(\d+) -(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts5:<timestamp>)
+    # ceph logs.
+    # Example: 2015-11-13 06:25:29.436844
+    rex_pattern = re.sub(
+        r'\(ts5:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d{4})-(\d{1,2})-(\d{1,2}) (\d+):(\d+):(\d+)\.(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts6:<timestamp>)
+    # cassandra
+    # Example:2015-10-23 12:38:15
+    rex_pattern = re.sub(
+        r'\(ts6:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d{4})-(\d{1,2})-(\d{1,2}) (\d+):(\d+):(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts7:<timestamp>)
+    # haproxy
+    # Example: 13/Nov/2015:06:25:05.465
+    rex_pattern = re.sub(
+        r'\(ts7:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d+)\/(\w{3})\/(\d{4}):(\d+):(\d+):(\d+)\.(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts8:<timestamp>)
+    # mysql
+    # Example: 12:03:28
+    rex_pattern = re.sub(
+        r'\(ts8:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d+):(\d+):(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts9:<timestamp>)
+    # reddis
+    # Example: 08 Nov 06:26:05.084
+    rex_pattern = re.sub(
+        r'\(ts9:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d+) (\w{3}) (\d+):(\d+):(\d+)\.(\d+))',
+        rex_pattern)
+
+    # user pattern: (ts10:<timestamp>)
+    # supervisord, zookeeper
+    # Example: 2015-06-30 10:59:18,133
+    rex_pattern = re.sub(
+        r'\(ts10:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d{4})-(\d{1,2})-(\d{1,2}) (\d+):(\d+):(\d+),(\d+))',
+        rex_pattern)
+
+    # User pattern: (ts11:<timestamp>)
+    # dmesg
+    # Example: 11148214.574583
+    rex_pattern = re.sub(
+        r'\(ts11:<([\w\d_]+)>\)',
+        '(?P<\\1>(\d+)\.(\d+))',
+        rex_pattern)
+
+    # Finally if no prefix is specified take default action.
+    rex_pattern = re.sub(r'\(<([\w\d_]+)>\)', '(?P<\\1>.*)', rex_pattern)
+
+    if compile:
+        return re.compile(rex_pattern)
+
+    return rex_pattern
+
+
+def match_string(pattern, search_string):
+    '''
+    Match a pattern in a string
+    '''
+
+    rexobj = REX(pattern, None)
+
+    rexpatstr = reformat_pattern(pattern)
+    #print "rexpatstr: ", rexpatstr
+
+    rexpat = re.compile(rexpatstr)
+    rexobj.rex_patternstr = rexpatstr
+    rexobj.rex_pattern = rexpat
+
+    line_count = 1
+    for line in search_string.splitlines():
+        line = line.strip()
+        mobj = rexpat.match(line)
+        if mobj:
+            populate_resobj(rexobj, mobj, line_count)
+        line_count += 1
+
+    return rexobj
+
+
+def populate_resobj(rexobj, mobj, loc):
+    '''
+    Popuate the result object and append it to the
+    rexobj results.
+    '''
+    resobj = REXResult(mobj, loc)
+    rexobj.matches.append(resobj)
+    rexobj.res_count += 1
+
+
+def match_file(pattern, filename):
+    '''
+    The function will match a pattern in a file and return
+    a rex object, which will have all the matches found in the file.
+    '''
+
+    # Validate user data.
+
+    if pattern is None:
+        return None
+
+    if os.stat(filename).st_size == 0:
+        return None
+
+    rexobj = REX(pattern, filename)
+
+    rexpatstr = reformat_pattern(pattern)
+    #print "rexpatstr: ", rexpatstr
+
+    rexpat = re.compile(rexpatstr)
+    rexobj.rex_patternstr = rexpatstr
+    rexobj.rex_pattern = rexpat
+
+    sfile = open(filename, 'r')
+    data = sfile.read()
+    sfile.close()
+
+    line_count = 1
+    for line in data.splitlines():
+        mobj = rexpat.match(line)
+        if mobj:
+            populate_resobj(rexobj, mobj, line_count)
+
+        line_count += 1
+
+    return rexobj
+
+
+def parse_lrvalue_string(search_string,
+                         delimiter=":"):
+    '''
+    The function takes a multi-line output/string with the format
+    "name/descr   : value", and converts it to a dictionary object
+    with key value pairs, where key is built from the name/desc
+    part and value as the value.
+
+    eg:  "Serial Number: FCH1724V1GT" will be translated to
+    dict['serial_number'] = "FCH1724V1GT"
+    '''
+    mac_search_pattern = r"(.*) *%s ([\w|\d]+.*)" % delimiter
+    search_pattern = r"(.*) *%s *(.*)" % delimiter
+
+    rexdict = {}
+    for line in search_string.splitlines():
+        line = line.strip()
+        mobj = re.match(mac_search_pattern, line)
+        if mobj:
+            key = mobj.group(1).lower()
+            key = "_".join(key.split()[0:3])
+            key = key.strip()
+            rexdict[key] = mobj.group(2)
+            continue
+
+        mobj = re.match(search_pattern, line)
+        if mobj:
+            key = mobj.group(1).lower()
+            key = "_".join(key.split()[0:3])
+            key = key.strip()
+            rexdict[key] = mobj.group(2)
+
+    return rexdict
+
+
+def parse_multi_lrvalue_string(search_string, split_string,
+                               delimiter=":"):
+    '''
+    The function is an extension of the parse_lrvalue_string() API.
+    The function takes a multi-line output/string of the format
+    "Category: xyz
+       name: foo
+       id: bar
+     Category: abc
+       name: foox
+       id: barx
+    :
+    "
+    It splits the output based on the splitstring passed as
+    argument (eg "Category"), and converts the individual
+    lines of the form "name:  value" to a dictionary object with
+    key value pairs. The key is built from the name (LV) part.
+    eg "First Name: Behzad" --> dict[first_name] = "Behzad"
+    '''
+    dictlist = []
+    for out in search_string.split(split_string):
+        tdict = parse_lrvalue_string(split_string + out,
+                                     delimiter=delimiter)
+        dictlist.append(tdict)
+
+    return dictlist
+
+
+def parse_tabular_string(search_string,
+                         header_keys,
+                         delimiter=None,
+                         merge_list=None):
+    '''
+    Given a string in a tabular format, parse it and return a
+    dictionary
+    @args:
+        search_string: This is a string in tabular format (e.g.: output of df
+        command)
+        header_keys: This is a list of strings for the headers.
+        delimiter(optional): Default is None, which translates to spaces
+        merge_list(optional): In some cases 2 fields need to be merged as they
+        are one value.
+    '''
+    first_line = True
+    parsed_results = []
+    for line in search_string.splitlines():
+        if first_line:
+            first_line = False
+        else:
+            result = {}
+            row = line.split()
+            if merge_list:
+                for mergeset in merge_list:
+                    fidx = mergeset[0]
+                    lidx = mergeset[1]
+                    try:
+                        row[fidx] = "_".join(row[fidx:(lidx+1)])
+                        row.remove(row[lidx])
+                    except IndexError:
+                        pass
+
+            if len(row) != len(header_keys):
+                print "Incorrect fields len "
+                continue
+            key_count = 0
+            for column in row:
+                result[header_keys[key_count]] = column
+                key_count += 1
+            parsed_results.append(result)
+
+    return parsed_results
+
+
+def dump_rexobj_results(rexobj, options=None):
+    '''
+    print all the results.
+    '''
+    print("-" * 60)
+    print("Match count: ", rexobj.res_count)
+    matches = rexobj.matches
+    for match in matches:
+        print("Loc:", match.loc, ":: ")
+        for key in match.named_groups.keys():
+            print("%s: %s" %
+                  (key, match.named_groups[key]))
+        print("")
+
+        #done = False
+        #cnt = 0
+        #while not done:
+        #    try:
+        #        print "%d: %s" % (cnt, match.reobj.group(cnt)),
+        #        cnt += 1
+        #    except IndexError:
+        #        print ""
+        #        done = True
+
+
+def get_match_value(rexobj, key, index=0):
+    '''
+    Return a matched value for the key for a specific match from the
+    results.
+    '''
+    if rexobj is None:
+        return None
+
+    if rexobj.res_count == 0:
+        return None
+
+    try:
+        return rexobj.matches[index].named_groups[key]
+    except IndexError:
+        return None
+    except KeyError:
+        return None
+
+
+
+
---
+++ packages/rexutil/rexutil-1.0.5-py2.py3-none-any.whl/rexutil-1.0.5.dist-info/DESCRIPTION.rst
@@ -0,0 +1,246 @@
+rex
+====
+[![PyPI Version](https://img.shields.io/pypi/v/rexutil.svg)](https://pypi.python.org/pypi/rexutil)
+
+A simple python module which serves as an abstraction to the python
+regular expression (re) module in some use cases.
+
++ [Introduction] (https://github.com/bdastur/rex/blob/master/README.md#introduction)
++ [Getting Started] (https://github.com/bdastur/rex/blob/master/README.md#getting-started)
++ [Features] (https://github.com/bdastur/rex/blob/master/README.md#features)
+  + [LR Value Parsing] (https://github.com/bdastur/rex/blob/master/README.md#lrvalue)
+  + [Multiple LR Value Parsing] (https://github.com/bdastur/rex/blob/master/README.md#mlrvalue)
+  + [Table formatParsing] (https://github.com/bdastur/rex/blob/master/README.md#tabular)
+  + [RE Pattern Abstractions] (https://github.com/bdastur/rex/blob/master/README.md#pabstractions)
+
+
+------
+# Introduction:<a name="introduction"></a>
+
+The rex module was born out of necessity. I found myself writing code to parse a lot of command line output for some switches which did not have a good API interface and also in some cases where CLI was the best option to get the data I needed.
+
+In a lot of cases the output format was very similar where I could generalize and try to come up with a common API that can be used for any such CLI. I could not find anything that would fit the bill, and rex was born.
+
+------
+# Getting Started:<a name="getting-started"></a>
+
+
+## Installing using pip:
+```
+   pip install rexutil
+```
+
+## Installing (git clone):
+```
+git clone https://github.com/bdastur/rex -b master
+```
+
+## Usage:
+```
+   import rex
+```
+
+------
+# Features:<a name="features"></a>
+The module provides the following main abstractions:
+
+## **1. Parsing a LR Value String:**<a name="lrvalue"></a>
+Consider the output of a CLI "show chassis" below from a switch. 
+```
+  chassis : UCS43
+  Serial no : USC788dc
+  Firmware : 12.01(99)
+  PID : UCS4343
+  UUID : URY888dR7R7R8-090-99
+  Description : Ucs M3 Series Chassis
+
+```
+The LRValue parser will parse this output and return a dictionary:
+```
+{ 
+  'firmware': '12.01(99)', 
+  'description': 'Ucs M3 Series Chassis', 
+  'serial_no': 'USC788dc',
+  'pid': 'UCS4343', 
+  'chassis': 'UCS43', 
+  'uuid': 'URY888dR7R7R8-090-99'
+ }
+```
+
+## **2. Parsing multiple LR Value strings.**<a name="mlrvalue"></a>
+A similar use case is where you have multiple  blocks of LRValue paris.
+Consider the example of a CLI to display adapters.
+```
+Physical Drive Number 21:                                            
+  Controller: SLOT-4                                               
+  Health: Good                                                     
+  Status: Unconfigured Good                                        
+  Manufacturer: ATA                                                
+  Model: ST91000640NS                                              
+  Predictive Failure Count: 0                                      
+  Drive Firmware: CC03                                             
+  Coerced Size: 952720 MB                                          
+  Type: HDD                                                    
+Physical Drive Number 22:                                            
+  Controller: SLOT-4                                               
+  Health: Good                                                     
+  Status: Unconfigured Good                                        
+  Manufacturer: ATA                                                
+  Model: ST91000640NS                                              
+  Predictive Failure Count: 0                                      
+  Drive Firmware: CC03                                             
+  Coerced Size: 952720 MB                                
+```
+
+In this case the LRValue parser will parse the output and
+return a list of dictionary object
+```
+[ { },
+  { 'coerced_size': '952720 MB',
+    'controller': 'SLOT-4',
+    'drive_firmware': 'CC03',
+    'health': 'Good',
+    'manufacturer': 'ATA',
+    'model': 'ST91000640NS',
+    'physical_drive_number': '',
+    'predictive_failure_count': '0',
+    'status': 'Unconfigured Good',
+    'type': 'HDD'},
+  { 'coerced_size': '952720 MB',
+    'controller': 'SLOT-4',
+    'drive_firmware': 'CC03',
+    'health': 'Good',
+    'manufacturer': 'ATA',
+    'model': 'ST91000640NS',
+    'physical_drive_number': '',
+    'predictive_failure_count': '0',
+    'status': 'Unconfigured Good'}]
+```
+
+## **3. Parsing a tabular format string.**<a name="tabular"></a>
+Very common output format is a tabular format with fields seperated by 
+delimiters (spaces, |, etc).
+
+An example here is the "df" command:
+```
+Filesystem     1K-blocks     Used Available Use% Mounted on
+/dev/md1        95926932 27688272  63359156  31% /
+udev           132014724        4 132014720   1% /dev
+tmpfs           52809712      448  52809264   1% /run
+none                5120        0      5120   0% /run/lock
+none           132024280        0 132024280   0% /run/shm
+cgroup         132024280        0 132024280   0% /sys/fs/cgroup
+/dev/md0          943544    28960    865820   4% /boot
+/dev/md2       806415824 63998212 701447384   9% /var
+```
+
+Here the taubular string parser will parse the output, and return a list
+of dictionary objects as below:
+```
+[ { '1k-blocks': '95926932',
+    'Available': '63359156',
+    'Filesystem': '/dev/md1',
+    'Mounted_on': '/',
+    'usage%': '31%',
+    'used': '27688272'},
+  { '1k-blocks': '132014724',
+    'Available': '132014720',
+    'Filesystem': 'udev',
+    'Mounted_on': '/dev',
+    'usage%': '1%',
+    'used': '4'},
+  { '1k-blocks': '52809712',
+    'Available': '52809264',
+    'Filesystem': 'tmpfs',
+    'Mounted_on': '/run',
+    'usage%': '1%',
+    'used': '448'},
+  { '1k-blocks': '5120',
+    'Available': '5120',
+    'Filesystem': 'none',
+    'Mounted_on': '/run/lock',
+    'usage%': '0%',
+    'used': '0'},
+  { '1k-blocks': '132024280',
+    'Available': '132024280',
+    'Filesystem': 'none',
+    'Mounted_on': '/run/shm',
+    'usage%': '0%',
+    'used': '0'},
+  { '1k-blocks': '132024280',
+    'Available': '132024280',
+    'Filesystem': 'cgroup',
+    'Mounted_on': '/sys/fs/cgroup',
+    'usage%': '0%',
+    'used': '0'},
+  { '1k-blocks': '943544',
+    'Available': '865820',
+    'Filesystem': '/dev/md0',
+    'Mounted_on': '/boot',
+    'usage%': '4%',
+    'used': '28960'},
+  { '1k-blocks': '806415824',
+    'Available': '701447384',
+    'Filesystem': '/dev/md2',
+    'Mounted_on': '/var',
+    'usage%': '9%',
+    'used': '63998212'}]
+
+```
+
+
+## **4. Common (re) Pattern Abstractions**<a name="pabstractions"></a>
+Another use case where parsing strings is needed is in logs and other program output. 
+Some of the common patterns which can abstract a lot of pain in writing regular expression strings
+are below:
+
+### 1. IP Address:
+Here is an example of a log from haproxy:
+```
+Nov 16 16:35:06 testhost1 haproxy[37217]: 192.16.41.8:45133 [16/Nov/2015:16:32:04.152] mysql mysql/mysql1 1/0/182236 12736 -- 647/261/261/261/0 0/0
+Nov 16 16:35:06 testhost1 haproxy[37217]: 192.16.41.8:45100 [16/Nov/2015:16:32:03.525] mysql mysql/mysql1 1/0/182932 13077 -- 647/261/261/261/0 0/0
+Nov 16 16:35:06 testhost1 haproxy[37217]: 192.16.41.8:45131 [16/Nov/2015:16:32:04.105] mysql mysql/mysql1 1/0/182483 12592 -- 647/261/261/261/0 0/0
+Nov 16 16:35:07 testhost1 haproxy[37217]: 192.16.41.8:45182 [16/Nov/2015:16:32:05.396] mysql mysql/mysql1 1/0/182246 8352 -- 647/261/261/261/0 0/0
+Nov 16 16:35:07 testhost1 haproxy[37217]: 192.16.41.4:55572 [16/Nov/2015:16:32:07.711] mysql mysql/mysql1 1/0/180119 16915 -- 647/261/261/261/0 0/0
+
+```
+
+Consider we want to parse the log and get all the ip addresses and ports. Here is how we can do that
+using the rex reformat pattern API.
+
+First we define our search pattern:
+```
+# Get the ipaddress and port no from the output.                         
+pattern = ".* (ip:<ipaddr>):(d:<port>).*"
+```
+
+Invoke the rex "reformat_pattern() API.
+```                                              
+rexpat = rex.reformat_pattern(pattern)                                   
+```
+
+Now we can use this as a pattern to search. In this case the re.finditer to iterate
+through all the matches:
+```                                                                          
+for mobj in re.finditer(rexpat, data):                                   
+    print "IP ADDR: %s, PORT: %s" % \                                    
+        (mobj.group(1), mobj.group(2))    
+```
+
+This returns the output:
+```
+IP ADDR: 192.16.41.8, PORT: 45133
+IP ADDR: 192.16.41.8, PORT: 45100
+IP ADDR: 192.16.41.8, PORT: 45131
+IP ADDR: 192.16.41.8, PORT: 45182
+IP ADDR: 192.16.41.4, PORT: 55572
+```
+
+### 2. MAC Addresses:
+
+### 3. Timestamps:
+
+
+
+
+
