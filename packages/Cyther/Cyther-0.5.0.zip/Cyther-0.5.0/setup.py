05987aa737d743452107c373595360d3897556fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:04 +0000

    Add package Cyther/0.5.0/Cyther-0.5.0.zip

---
+++ packages/Cyther/Cyther-0.5.0.zip/Cyther-0.5.0/setup.py
@@ -0,0 +1,50 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+NAME = 'Cyther'
+VERSION = '0.5.0'
+SHORT_DESCRIPTION = 'The Cross-Platform Cython/Python Compiler'
+LONG_DESCRIPTION = open('README.txt').read()
+PACKAGES = ['cyther']
+SCRIPTS = ['cytherize.py']
+DATA_FILES = [('cyther', ['CHANGELOG.txt', 'README.txt', 'TODO.txt', 'LICENSE.txt'])]
+PLATFORMS = ['Windows', 'MacOS', 'POSIX', 'Unix']
+AUTHOR = 'Nicholas C. Pandolfi'
+AUTHOR_EMAIL = 'npandolfi@wpi.edu'
+URL = 'https://github.com/nickpandolfi/Cyther'
+LICENSE = 'MIT'
+KEYWORDS = ['Cyther', 'Cython', 'Python', 'MinGW32',
+            'vcvarsall.bat', 'vcvarsall not found',
+            'setup.py', 'gcc', 'Python 3',
+            'user-friendly', 'command-line',
+            'script', 'auto-compiler']
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Environment :: Console',
+               'Topic :: Software Development :: Compilers',
+               'Topic :: Software Development :: Build Tools',
+               'Topic :: Desktop Environment :: File Managers',
+               'Intended Audience :: Developers',
+               'Intended Audience :: Science/Research',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Cython',
+               'License :: OSI Approved :: MIT License']
+
+setup(name=NAME,
+      version=VERSION,
+      description=SHORT_DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      packages=PACKAGES,
+      scripts=SCRIPTS,
+      data_files=DATA_FILES,
+      platforms=PLATFORMS,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      license=LICENSE,
+      keywords=KEYWORDS,
+      classifiers=CLASSIFIERS)
