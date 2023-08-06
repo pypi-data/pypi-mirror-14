9eb1d6029eb54fe0755a336c8cba3de5ebf9ff99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:18 +0000

    Add package hux/1.1.1/hux-1.1.1.zip

---
+++ packages/hux/hux-1.1.1.zip/hux-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name = 'hux',
+        version = '1.1.1',
+        py_modules = ['print_log'],
+        author = 'hx',
+        author_email = 'hux464@163.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists'
+
+
+    )
