ce16407fe4a2c5b8235b59ed86c622fa24af7b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:20 +0000

    Add package bob.bio.face/2.0.4/bob.bio.face-2.0.4.zip

---
+++ packages/bob.bio.face/bob.bio.face-2.0.4.zip/bob.bio.face-2.0.4/bob/bio/face/test/test_scripts.py
@@ -0,0 +1,24 @@
+import bob.bio.base.test.utils
+import bob.bio.face
+
+@bob.bio.base.test.utils.grid_available
+def test_baselines():
+  # test that all of the baselines would execute
+  from bob.bio.face.script.baselines import available_databases, all_algorithms, main
+
+  with bob.bio.base.test.utils.Quiet():
+    for database in available_databases:
+      parameters = ['-d', database, '--dry-run']
+      main(parameters)
+      parameters.append('--grid')
+      main(parameters)
+      parameters.extend(['-e', 'HTER'])
+      main(parameters)
+
+    for algorithm in all_algorithms:
+      parameters = ['-a', algorithm, '--dry-run']
+      main(parameters)
+      parameters.append('-g')
+      main(parameters)
+      parameters.extend(['-e', 'HTER'])
+      main(parameters)
