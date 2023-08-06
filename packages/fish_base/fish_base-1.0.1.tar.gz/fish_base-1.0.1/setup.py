01972a058e269f36312434a3fe4aeb2f9b85a598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package fish_base/1.0.1/fish_base-1.0.1.tar.gz

---
+++ packages/fish_base/fish_base-1.0.1.tar.gz/fish_base-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fish_base',
+    version='1.0.1',
+    packages=['fish_base'],
+    url='https://github.com/chinapnr/fish_base',
+    license='',
+    author='david.yi',
+    author_email='wingfish@gmail.com',
+    description='some useful functions for python'
+)
