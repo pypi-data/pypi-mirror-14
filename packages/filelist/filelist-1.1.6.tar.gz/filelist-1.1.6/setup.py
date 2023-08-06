08cda918748b3ee2f0008c016d1a10d53b99c4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:15 +0000

    Add package filelist/1.1.6/filelist-1.1.6.tar.gz

---
+++ packages/filelist/filelist-1.1.6.tar.gz/filelist-1.1.6/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setup(
+        name='filelist',
+
+        version='1.1.6',
+
+        description='Easily list some files in a directory, and exclude others.',
+        long_description=long_description,
+
+        url='https://github.com/Berzeg/filelist',
+
+        author="Hashem Berzeg",
+        author_email="h9.11.2s@gmail.com",
+
+        packages=find_packages(),
+
+        license="MIT",
+
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+
+            'Intended Audience :: Developers',
+            'Topic :: Software Development',
+
+            'License :: OSI Approved :: MIT License',
+
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+
+        keywords='directory file walk crawl .gitignore .pyignore ignore',
+    )
