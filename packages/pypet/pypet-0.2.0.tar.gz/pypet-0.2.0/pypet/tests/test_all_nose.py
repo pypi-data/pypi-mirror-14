20ff798f2e8c94a4f3ae278b34d406686116391a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:43 +0000

    Add package pypet/0.2.0/pypet-0.2.0.tar.gz

---
+++ packages/pypet/pypet-0.2.0.tar.gz/pypet-0.2.0/pypet/tests/test_all_nose.py
@@ -0,0 +1,23 @@
+__author__ = 'Robert Meyer'
+
+import getopt
+import sys
+import os
+import sys
+if (sys.version_info < (2, 7, 0)):
+    import unittest2 as unittest
+else:
+    import unittest
+
+from pypet.tests.testutils.ioutils import discover_tests, TEST_IMPORT_ERROR
+
+class NoseTestDummy(unittest.TestCase):
+    pass
+
+suite = discover_tests(predicate= lambda class_name, test_name, tags:
+                                                class_name != TEST_IMPORT_ERROR)
+suite_dict = {}
+for case in suite:
+    class_name = case.__class__.__name__
+    globals()[class_name] = case.__class__
+
