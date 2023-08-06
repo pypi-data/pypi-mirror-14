23378f93075b7429d336e3fc31a945ec45f95e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:09 +0000

    Add package MovieSerieTorrent/1.0.8/MovieSerieTorrent-1.0.8.tar.gz

---
+++ packages/MovieSerieTorrent/MovieSerieTorrent-1.0.8.tar.gz/MovieSerieTorrent-1.0.8/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+with open('requirements.txt') as requirements:
+    install_requires = requirements.read().splitlines()
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(
+    name='MovieSerieTorrent',
+    version='1.0.8',
+    packages=find_packages(),
+    install_requires=install_requires,
+    author="Petit Jonathan",
+    author_email="petit.jonathan16@gmail.com",
+    description="Parser and Renamer for torrents files (Movies and series)",
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    url='https://github.com/JonathanPetit/Parser-Renamer',
+    license= 'MIT',
+    keywords = 'parser renamer formatting python torrents torrent files file movie serie movies series',
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+    ],
+)
