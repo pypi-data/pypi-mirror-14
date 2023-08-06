aa13e62f8affa79c4c1c8128adedc5bcc89707b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:08 +0000

    Add package lazygit/0.5/lazygit-0.5.tar.gz

---
+++ packages/lazygit/lazygit-0.5.tar.gz/lazygit-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+  name = 'lazygit',
+  packages = ['lazygit'], # this must be the same as the name above
+  version = '0.5',
+  description = 'Automating the daily git push operations',
+  author = 'Mayank Saxena',
+  author_email = 'mayank26saxena@gmail.com',
+  url = 'https://github.com/mayank26saxena/LazyGit', # use the URL to the github repo
+  download_url = 'https://github.com/mayank26saxena/LazyGit/tarball/0.5', # I'll explain this in a second
+  keywords = ['github', 'push', 'commit', 'lazygit'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+        "os",
+        "system"
+  ],  
+  entry_points={
+        'console_scripts': [
+            'lazygit = lazygit.main:main'
+        ],
+  }
+)
