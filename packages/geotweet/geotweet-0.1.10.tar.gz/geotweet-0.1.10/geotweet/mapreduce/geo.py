2b523d08167f175c354772b81c4a77222c747a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package geotweet/0.1.10/geotweet-0.1.10.tar.gz

---
+++ packages/geotweet/geotweet-0.1.10.tar.gz/geotweet-0.1.10/geotweet/mapreduce/geo.py
@@ -0,0 +1,110 @@
+import sys
+import os
+from os.path import dirname
+import re
+import json
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+import Geohash
+
+
+try:
+    # when running on EMR a geotweet package will be loaded onto PYTHON PATH
+    from geotweet.mapreduce.utils.reader import FileReader
+    from geotweet.mapreduce.utils.words import WordExtractor
+    from geotweet.mapreduce.utils.lookup import CachedCountyLookup
+    print "geotweet load"
+except ImportError:
+    # runing locally
+    from utils.reader import FileReader
+    from utils.words import WordExtractor
+    from utils.lookup import CachedCountyLookup
+
+
+"""
+https://en.wikipedia.org/wiki/Geohash
+
+geohash
+precision   width   height
+4           39.1km  19.5km
+5           4.9km   4.9km
+6           1.2km   609.4m
+7           152.9m  152.4m
+8           38.2m   19m
+"""
+GEOHASH_PRECISION = 7
+MIN_WORD_COUNT = 5      # ignore low occurences
+
+
+class MRGeoWordCount(MRJob):
+    """
+    Count word occurences for US tweets by entire county, by State and County
+    
+    A geojson file of US counties is downloaded from an S3 bucket. A RTree index
+    is built using the bounding box of each county, and is used for determining
+    State and County for each tweet.
+    
+    """
+
+    def steps(self):
+        return [
+            MRStep(
+                mapper_init=self.mapper_init,
+                mapper=self.mapper,
+                combiner=self.combiner,
+                reducer=self.reducer
+            )
+        ]
+
+    def mapper_init(self):
+        """ Download counties geojson from S3 and build spatial index and cache """
+        self.counties = CachedCountyLookup()
+        self.extractor = WordExtractor()
+    
+    def mapper(self, _, line):
+        data = json.loads(line)
+        # ignore HR geo-tweets for job postings
+        if data['description'] and self.hr_filter(data['description']):
+            return
+        # convert coordinates to geohash
+        lat = data['lonlat'][1]
+        lon = data['lonlat'][0]
+        geohash = Geohash.encode(lat, lon, precision=GEOHASH_PRECISION)
+        # spatial lookup for state and county
+        state, county = self.counties.get(geohash) 
+        if not state or not county:
+            return
+        # count words
+        for word in self.extractor.run(data['text']):
+            yield word, 1
+            yield "{0}|{1}".format(word, state), 1
+            yield "{0}|{1}|{2}".format(word, state, county), 1
+    
+    def hr_filter(self, text):
+        """ check if description of twitter using contains job related key words """
+        expr = "|".join(["(job)", "(hiring)", "(career)"])
+        return re.findall(expr, text)
+  
+    def combiner(self, key, values):
+        yield key, sum(values)
+    
+    def reducer(self, key, values):
+        total = int(sum(values))
+        if total < MIN_WORD_COUNT:
+            return
+        # parse key of the form ('word', 'word|state', 'word|state|county')
+        parts = key.split('|')
+        state = county = ''
+        if len(parts) >= 2:
+            state = parts[1]
+        if len(parts) == 3:
+            state = parts[1]
+            county = parts[2]
+        # output results
+        yield (parts[0], state, county), total
+
+
+if __name__ == '__main__':
+    MRGeoWordCount.run()
---
+++ packages/geotweet/geotweet-0.1.10.tar.gz/geotweet-0.1.10/setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='geotweet',
+    version='0.1.10',
+    description='Fetch geographic tweets from Twitter Streaming API',
+    author='Jeffrey Alan Meyers',
+    author_email='jeffrey.alan.meyers@gmail.com',
+    url='https://github.com/meyersj/geotweet',
+    packages=[
+        'geotweet',
+        'geotweet.mapreduce',
+        'geotweet.mapreduce.utils',
+        'geotweet.twitter',
+        'geotweet.mongo',
+        'geotweet.geo'
+    ],
+    scripts=['bin/geotweet', 'bin/geoloader'],
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'argparse',
+        'boto3',
+        'python-twitter',
+        'requests',
+        'mrjob',
+        'pymongo',
+        'Geohash',
+        'Shapely',
+        'Rtree',
+        'lxml'
+    ]
+)
