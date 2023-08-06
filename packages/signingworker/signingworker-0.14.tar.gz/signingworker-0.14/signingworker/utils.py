2db37ab821c3c903ed4ca8b1f7a8a82cec32bea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:43 +0000

    Add package signingworker/0.14/signingworker-0.14.tar.gz

---
+++ packages/signingworker/signingworker-0.14.tar.gz/signingworker-0.14/signingworker/utils.py
@@ -0,0 +1,40 @@
+import json
+import hashlib
+import functools
+import logging
+from collections import namedtuple
+
+log = logging.getLogger(__name__)
+# Mapping between signing client formats and file extensions
+DETACHED_SIGNATURES = [
+    ('gpg', '.asc', 'text/plain')
+]
+
+
+def get_hash(path, hash_type="sha512"):
+    h = hashlib.new(hash_type)
+    with open(path, "rb") as f:
+        for chunk in iter(functools.partial(f.read, 4096), ''):
+            h.update(chunk)
+    return h.hexdigest()
+
+
+def load_signing_server_config(config):
+    log.debug("Loading signing server config from %s", config)
+    SigningServer = namedtuple("SigningServer", ["server", "user", "password",
+                                                 "formats"])
+    with open(config) as f:
+        raw_cfg = json.load(f)
+
+    cfg = {}
+    for signing_type, server_data in raw_cfg.iteritems():
+        cfg[signing_type] = [SigningServer(*s) for s in server_data]
+    log.debug("Signing server config loaded from %s", config)
+    return cfg
+
+
+def get_detached_signatures(signing_formats):
+    """Returns a list of tuples with detached signature types and corresponding
+    file extensions"""
+    return [(sig_type, sig_ext, sig_mime) for sig_type, sig_ext, sig_mime in
+            DETACHED_SIGNATURES if sig_type in signing_formats]
