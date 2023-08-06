86ec4ad25f6b0b1d239bde6b5434709eeaa2b2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:34 +0000

    Add package see/1.2.0/see-1.2.0.zip

---
+++ packages/see/see-1.2.0.zip/see-1.2.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='see',
+      version='1.2.0',
+      description='A human-readable alternative to dir',
+      author='Liam Cooke',
+      author_email='see@araile.com',
+      url='http://araile.github.io/see/',
+      py_modules=['see'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: Freely Distributable',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development',
+      ])
