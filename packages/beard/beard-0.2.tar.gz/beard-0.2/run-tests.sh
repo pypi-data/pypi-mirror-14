84f00413b59e1a997fd4af440b3ae71ea82d7c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:03 +0000

    Add package beard/0.2/beard-0.2.tar.gz

---
+++ packages/beard/beard-0.2.tar.gz/beard-0.2/run-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This file is part of Beard.
+# Copyright (C) 2016 CERN.
+#
+# Beard is a free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+set -e
+
+pydocstyle --convention=pep257 --match-dir='^((?!ext).)*$' beard
+check-manifest --ignore miniconda.sh
+python setup.py test
