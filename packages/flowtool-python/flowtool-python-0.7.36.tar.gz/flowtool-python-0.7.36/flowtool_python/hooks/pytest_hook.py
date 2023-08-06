834963528480a60132ff131f18f0bbedff82f588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package flowtool-python/0.7.36/flowtool-python-0.7.36.tar.gz

---
+++ packages/flowtool-python/flowtool-python-0.7.36.tar.gz/flowtool-python-0.7.36/flowtool_python/hooks/pytest_hook.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import git
+import click
+import fnmatch
+from flowtool.style import echo, colors
+from flowtool.style import debug
+from flowtool_git.common import local_repo
+from flowtool_githooks.discovering import find_files_named_in_project
+
+import pytest
+
+def run_pytest(*args):
+    """ Run pytest and return its exit status. """
+    pytest_args = [
+        '--doctest-modules',
+        '--ignore=setup.py',
+    ] + list(args)
+    errno = pytest.main(pytest_args)
+
+    return errno
+
+
+def pytest_setup(cmd=None):
+    """ Setup function for pytest hook(s). """
+    if cmd == 'install':
+        pass
+    elif cmd == 'uninstall':
+        pass
+
+
+PYTEST_CONFIGS = ['pytest.ini', 'tox.ini']
+
+
+@click.command()
+@click.argument('args', nargs=-1)
+def pytest_hook(args=()):
+    """ Run pytest in discovered directories. """
+
+    locations = list(find_files_named_in_project(PYTEST_CONFIGS))
+    if not locations:
+        return
+    echo.bold(
+        '->',
+        'Will run',
+        colors.yellow('pytest'),
+        'in',
+        len(locations),
+        'dir(s).',
+    )
+    continues = 0
+    hook_return = 0
+    fails = 0
+    restore_dir = os.getcwd()
+    for loc in locations:
+        echo.white('->', colors.cyan(loc))
+        os.chdir(loc)
+        returncode = run_pytest(loc)
+        if returncode:
+            fails += 1
+            hook_return |= returncode
+            if fails > continues:
+                sys.exit(returncode)
+        os.chdir(restore_dir)
+    if hook_return:
+        sys.exit(hook_return)
