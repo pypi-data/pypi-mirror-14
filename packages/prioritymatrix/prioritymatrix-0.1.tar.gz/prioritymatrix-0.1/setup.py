32263366cc44a6298aa54f597441b9e159cca5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:25 +0000

    Add package prioritymatrix/0.1/prioritymatrix-0.1.tar.gz

---
+++ packages/prioritymatrix/prioritymatrix-0.1.tar.gz/prioritymatrix-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='prioritymatrix',
+      version='0.1',
+      description='Priority Matrix API',
+      author='Samuel Carmona',
+      url = 'https://github.com/samuc/prioritymatrix-python',
+      download_url = 'https://github.com/samuc/prioritymatrix-python/tarball/0.1',
+      author_email='samuel@appfluence.com',
+      license='MIT',
+      packages=['prioritymatrix'],
+      zip_safe=False)
