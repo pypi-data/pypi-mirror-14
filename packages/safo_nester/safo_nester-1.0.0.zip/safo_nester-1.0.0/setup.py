b79c0fd34dcd602491a5ce526fd9402ed6498ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:46 +0000

    Add package safo_nester/1.0.0/safo_nester-1.0.0.zip

---
+++ packages/safo_nester/safo_nester-1.0.0.zip/safo_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='safo_nester',
+	version='1.0.0',
+	py_modules=['safo_nester'],
+	author='safo',
+	author_email='wstshifu@gmail.com',
+	description='A simple printer of nested lists'
+	)
