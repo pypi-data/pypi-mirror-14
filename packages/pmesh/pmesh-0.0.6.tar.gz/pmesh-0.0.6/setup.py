f4b2422dd8c10e2bef09135944f38f2a2746ea5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:41 +0000

    Add package pmesh/0.0.6/pmesh-0.0.6.tar.gz

---
+++ packages/pmesh/pmesh-0.0.6.tar.gz/pmesh-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
+import numpy
+import os
+
+def myext(*args):
+    return Extension(*args, include_dirs=["./", numpy.get_include()])
+
+extensions = [
+        myext("pmesh._domain", ["pmesh/_domain.pyx"]),
+        ]
+
+setup(
+    name="pmesh", version="0.0.6",
+    author="Yu Feng",
+    description="Particle Mesh in Python",
+    package_dir = {'pmesh': 'pmesh'},
+    install_requires=['cython', 'numpy'],
+    packages= ['pmesh'],
+    requires=['numpy', 'mpi4py'],
+    ext_modules = cythonize(extensions)
+)
+
