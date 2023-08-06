908d1e4ee40e2e0b26d5c23ad55f940a7700a20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:50 +0000

    Add package valleybackups/0.1.3/valleybackups-0.1.3.zip

---
+++ packages/valleybackups/valleybackups-0.1.3.zip/valleybackups-0.1.3/valleybackups/commands/cmd_set_config.py
@@ -0,0 +1,16 @@
+import click
+from valleybackups.config_context import pass_config
+
+
+@click.command()
+@click.argument('section', required=True, type=str)
+@click.argument('setting', required=True, type=str)
+@click.argument('value', required=True, type=str)
+@pass_config
+def cli(config, section, setting, value):
+    """Sets value to a specific configuration item"""
+    click.echo("Changing option %s.%s to %s" % (section, setting, value))
+
+    config.handler.set_config(section, setting, value)
+
+    config.handler.save_config()
