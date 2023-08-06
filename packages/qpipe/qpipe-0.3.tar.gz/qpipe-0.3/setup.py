fc1887b7d871aa173182d4f4e047533cddd3d375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:51 +0000

    Add package qpipe/0.3/qpipe-0.3.tar.gz

---
+++ packages/qpipe/qpipe-0.3.tar.gz/qpipe-0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+version = '0.3'
+setup(
+  name = 'qpipe',
+  packages = ['qpipe'],
+  version = version,
+  description = 'Quick and simple pipeline/flow-based multiprocessing',
+  author = 'Dan Kinder',
+  author_email = 'dkinder.is.me@gmail.com',
+  url = 'https://github.com/dankinder/qpipe',
+  download_url = 'https://github.com/dankinder/qpipe/tarball/' + version,
+  keywords = ['multiprocessing', 'threading', 'pipeline', 'flow', 'pipe'],
+  classifiers = [],
+)
