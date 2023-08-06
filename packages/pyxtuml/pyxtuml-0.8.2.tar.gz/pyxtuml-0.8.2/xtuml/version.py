e3d4d313c200620d95005de1d66c4430027d23e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package pyxtuml/0.8.2/pyxtuml-0.8.2.tar.gz

---
+++ packages/pyxtuml/pyxtuml-0.8.2.tar.gz/pyxtuml-0.8.2/xtuml/version.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+
+name = 'pyxtuml'
+date = '2016-04-09'
+version = '0.8'
+release = '0.8.2'
+
+complete_string = '%s v%s (%s)' % (name, release, date)
