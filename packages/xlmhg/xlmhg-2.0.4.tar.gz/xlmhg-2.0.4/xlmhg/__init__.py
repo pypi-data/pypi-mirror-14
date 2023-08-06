6861bab50a53782ef45e14f04a437072c8ced92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:07 +0000

    Add package xlmhg/2.0.4/xlmhg-2.0.4.tar.gz

---
+++ packages/xlmhg/xlmhg-2.0.4.tar.gz/xlmhg-2.0.4/xlmhg/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+from xlmhg.test import xlmhg_test
+
+__all__ = ['xlmhg_test']
