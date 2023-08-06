318fd03d823040f3f7d40b124e052da30af871c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package mapdepot/0.1.1/mapdepot-0.1.1.tar.gz

---
+++ packages/mapdepot/mapdepot-0.1.1.tar.gz/mapdepot-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Map Depot Indexer.
+
+Command Line Interface Module
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    REQUIRES = f.readlines()
+
+setup(
+    name='mapdepot',
+    version='0.1.1',
+    description='Map Depot\'s helpful scripts for indexing their data.',
+    packages=['mapdepot', 'mapdepot.schemas'],
+    install_requires=[REQUIRES],
+    entry_points={
+        'console_scripts': [
+            'mapdepot = mapdepot.cli:cli'
+        ]
+    }
+)
