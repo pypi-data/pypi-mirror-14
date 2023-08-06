28c34e8154b969b8330872132540a994a51f1799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package esl/0.0.4/esl-0.0.4.tar.gz

---
+++ packages/esl/esl-0.0.4.tar.gz/esl-0.0.4/setup.py
@@ -0,0 +1,24 @@
+# @Author: BingWu Yang <detailyang>
+# @Date:   2016-03-30T20:27:36+08:00
+# @Email:  detailyang@gmail.com
+# @Last modified by:   detailyang
+# @Last modified time: 2016-03-31T13:10:15+08:00
+# @License: The MIT License (MIT)
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'esl',
+    version = '0.0.4',
+    keywords = ('esl', 'eva-shell-language'),
+    description = 'eva shell language for eva-shell',
+    license = 'MIT License',
+    install_requires = ['ply>=3.8'],
+
+    author = 'detailyang',
+    author_email = 'detailyang@gmail.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
