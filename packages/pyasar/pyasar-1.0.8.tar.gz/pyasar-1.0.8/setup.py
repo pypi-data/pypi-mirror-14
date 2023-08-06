3611ae470660d556bcac30e1734dee7e499d0785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:48 +0000

    Add package pyasar/1.0.8/pyasar-1.0.8.tar.gz

---
+++ packages/pyasar/pyasar-1.0.8.tar.gz/pyasar-1.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyasar',
+    packages=['asar'],
+    version='1.0.8',
+    author='Swen Kooij (Photonios)',
+    author_email='photonios@outlook.com',
+    description='Library for working with Electron\'s ASAR archives.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/Photonios/pyasar.git',
+    keywords=['asar', 'electron', 'pyasar'],
+)
