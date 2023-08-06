364738e961055fbaf2567bd74f099197c86705c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:49 +0000

    Add package mingli/1.1.1/mingli-1.1.1.tar.gz

---
+++ packages/mingli/mingli-1.1.1.tar.gz/mingli-1.1.1/mingli.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, level):
+""" It is an example. """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_list, level+1)
+        else:
+            for tab_sop in range(level):
+                print('\t', end='')
+            print(each_list)
