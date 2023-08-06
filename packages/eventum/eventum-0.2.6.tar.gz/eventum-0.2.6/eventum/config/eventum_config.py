b95b3a8bf9205262c51dbc3539bc2f4537a5272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:16 +0000

    Add package eventum/0.2.6/eventum-0.2.6.tar.gz

---
+++ packages/eventum/eventum-0.2.6.tar.gz/eventum-0.2.6/eventum/config/eventum_config.py
@@ -0,0 +1,43 @@
+from os import path, pardir
+import eventum
+
+# Default Values
+CSRF_ENABLED = True
+MONGODB_SETTINGS = {
+    'DB': 'eventum'
+}
+EVENTUM_DEFAULT_PROFILE_PICTURE = 'img/default_profile_picture.png'
+EVENTUM_DEFAULT_EVENT_IMAGE = 'img/default_event_image.jpg'
+EVENTUM_GOOGLE_AUTH_ENABLED = True
+EVENTUM_APP_LOG_NAME = 'app.log'
+EVENTUM_WERKZEUG_LOG_NAME = 'werkzeug.log'
+EVENTUM_LOG_FILE_MAX_SIZE = 256
+EVENTUM_URL_PREFIX = '/admin'
+EVENTUM_ALLOWED_UPLOAD_EXTENSIONS = set(['.png', '.jpg', '.jpeg', '.gif'])
+
+EVENTUM_BASEDIR = eventum.__path__[0]
+
+# Static folder
+EVENTUM_STATIC_FOLDER = path.join(EVENTUM_BASEDIR,
+                                  'static/')
+# SCSS folder
+EVENTUM_SCSS_FOLDER = path.join(EVENTUM_STATIC_FOLDER,
+                                'eventum_scss/')
+# Template folder
+EVENTUM_TEMPLATE_FOLDER = path.join(EVENTUM_BASEDIR,
+                                    'templates/')
+
+######################
+# Must be overridden #
+######################
+
+EVENTUM_INSTALLED_APP_CLIENT_SECRET_PATH = None
+EVENTUM_INSTALLED_APP_CREDENTIALS_PATH = None
+EVENTUM_CLIENT_SECRETS_PATH = None
+EVENTUM_PRIVATE_CALENDAR_ID = None
+EVENTUM_PUBLIC_CALENDAR_ID = None
+EVENTUM_UPLOAD_FOLDER = None
+EVENTUM_DELETE_FOLDER = None
+
+# Will be set elsewhere, do not set directly
+EVENTUM_GOOGLE_CLIENT_ID = None
