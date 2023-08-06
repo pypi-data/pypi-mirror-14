f5da61ea54681dba68145b4d75a230dc191cd8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:30 +0000

    Add package blutils/0.1/blutils-0.1.zip

---
+++ packages/blutils/blutils-0.1.zip/blutils-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'blutils',
+  packages = ['doc','src'],
+  version = '0.1',
+  description = 'A bunch of usefull functions',
+  author = 'J.B',
+  author_email = 'javier.barcala@balidea.com',
+  url = 'https://github.com/anbaalo/util',
+  download_url = 'https://github.com/anbaalo/util/tarball/0.1',
+  keywords = [],
+  classifiers = [],
+)
