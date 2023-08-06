aff9808ad5ce52e52d3963edab42e178595998d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:36 +0000

    Add package sdklib/0.5.2.1/sdklib-0.5.2.1.tar.gz

---
+++ packages/sdklib/sdklib-0.5.2.1.tar.gz/sdklib-0.5.2.1/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='sdklib',
+    version='0.5.2.1',
+    description = 'SDK helper library',
+    author='Ivan Martin',
+    author_email='ivanmar_91@hotmail.com',
+    url='https://github.com/ivanprjcts/sdklib',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['sdklib.util.parser', 'sdklib.sdklib', 'sdklib.util.bytearray', 'sdklib.util.urlvalidator',
+                'sdklib.util.timetizer', 'sdklib.util.file'],
+    packages=['sdklib', 'sdklib.util'],
+)
