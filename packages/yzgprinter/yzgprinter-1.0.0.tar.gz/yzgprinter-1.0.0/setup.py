2c02b7b51ad7d65a43bec17d0d328b8b126d81c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:21 +0000

    Add package yzgprinter/1.0.0/yzgprinter-1.0.0.tar.gz

---
+++ packages/yzgprinter/yzgprinter-1.0.0.tar.gz/yzgprinter-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name = 'yzgprinter',
+      version = '1.0.0',
+      py_modules = ['yzgprinter'],
+      author = 'yinzhenguo',
+      author_email = '15158128396@163.com',
+      url = 'http://www.csesteel.com',
+      description = 'A simple printer of nested lists',
+)
+                                  
