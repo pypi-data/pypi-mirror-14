895712b323a0cd151f9270e8a51b17a84e6c35a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package begin_nester/1.2.0/begin_nester-1.2.0.tar.gz

---
+++ packages/begin_nester/begin_nester-1.2.0.tar.gz/begin_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'begin_nester',
+    version = '1.2.0',
+    py_modules = ['begin_nester'],
+    author = 'sungwon',
+    author_email = 'admin@ansee.kr',
+    url = 'http://blog.ansee.kr',
+    description = 'A simple printer of nested lists',
+    ) 
+    
