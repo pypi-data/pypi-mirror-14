3bc9d3e7fdb16721cb4da033a8f9a2bb4ebf3f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:37 +0000

    Add package django-phrase/1.0.3/django-phrase-1.0.3.tar.gz

---
+++ packages/django-phrase/django-phrase-1.0.3.tar.gz/django-phrase-1.0.3/phrase/utils.py
@@ -0,0 +1,25 @@
+from phrase import settings as phrase_settings
+from django.utils.encoding import python_2_unicode_compatible
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+@python_2_unicode_compatible
+class PhraseDelegate:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        logger.info("Rendering PhraseApp Key: %s" % (self.name))
+        return self.__normalized_name()
+
+    def __normalized_name(self):
+        normalized = "%sphrase_%s%s" % (phrase_settings.PHRASE_PREFIX, self.__safe_name(), phrase_settings.PHRASE_SUFFIX)
+        return normalized
+
+    def __safe_name(self):
+        name = self.name
+        name = name.replace("<", "[[[[[[html_open]]]]]]")
+        name = name.replace(">", "[[[[[[html_close]]]]]]")
+        return name
