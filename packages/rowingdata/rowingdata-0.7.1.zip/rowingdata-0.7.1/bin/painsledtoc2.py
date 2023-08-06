921ede4bee7ee6c6b3d7b0f700c936397e28e39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:50 +0000

    Add package rowingdata/0.7.1/rowingdata-0.7.1.zip

---
+++ packages/rowingdata/rowingdata-0.7.1.zip/rowingdata-0.7.1/bin/painsledtoc2.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+import rowingdata
+from sys import argv
+
+readFile = argv[1]
+
+try:
+    rowerFile = argv[2]
+except IndexError:
+    rowerFile = "defaultrower.txt"
+
+rower = rowingdata.getrower(rowerFile)
+
+row = rowingdata.rowingdata(readFile,rower=rower)
+
+row.uploadtoc2(rowerFile=rowerFile)
+
+
+
+print "done "+readFile
