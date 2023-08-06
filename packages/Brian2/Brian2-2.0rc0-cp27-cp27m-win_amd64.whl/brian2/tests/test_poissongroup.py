23c27108db28475062e9b3a17c6fe8245a36f3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:13 +0000

    Add package Brian2/2.0rc0/Brian2-2.0rc0-cp27-cp27m-win_amd64.whl

---
+++ packages/Brian2/Brian2-2.0rc0-cp27-cp27m-win_amd64.whl/brian2/tests/test_poissongroup.py
@@ -0,0 +1,68 @@
+from numpy.testing.utils import assert_equal
+from nose import with_setup
+from nose.plugins.attrib import attr
+
+from brian2 import *
+from brian2.devices.device import reinit_devices
+
+
+@attr('standalone-compatible')
+@with_setup(teardown=reinit_devices)
+def test_single_rates():
+    # Specifying single rates
+    P0 = PoissonGroup(10, 0*Hz)
+    Pfull = PoissonGroup(10, 1. / defaultclock.dt)
+
+    # Basic properties
+    assert len(P0) == len(Pfull) == 10
+    assert len(repr(P0)) and len(str(P0))
+    spikes_P0 = SpikeMonitor(P0)
+    spikes_Pfull = SpikeMonitor(Pfull)
+    run(2*defaultclock.dt)
+    assert_equal(spikes_P0.count, np.zeros(len(P0)))
+    assert_equal(spikes_Pfull.count, 2 * np.ones(len(P0)))
+
+@attr('standalone-compatible')
+@with_setup(teardown=reinit_devices)
+def test_rate_arrays():
+    P = PoissonGroup(2, np.array([0, 1./defaultclock.dt])*Hz)
+    spikes = SpikeMonitor(P)
+    run(2*defaultclock.dt)
+
+    assert_equal(spikes.count, np.array([0, 2]))
+
+@attr('standalone-compatible')
+@with_setup(teardown=reinit_devices)
+def test_propagation():
+    # Using a PoissonGroup as a source for Synapses should work as expected
+    P = PoissonGroup(2, np.array([0, 1./defaultclock.dt])*Hz)
+    G = NeuronGroup(2, 'v:1')
+    S = Synapses(P, G, on_pre='v+=1')
+    S.connect(j='i')
+    run(2*defaultclock.dt)
+
+    assert_equal(G.v[:], np.array([0., 2.]))
+
+
+@attr('standalone-compatible')
+@with_setup(teardown=reinit_devices)
+def test_poissongroup_subgroup():
+    # It should be possible to take a subgroup of a PoissonGroup
+    P = PoissonGroup(4, [0, 0, 0, 0]*Hz)
+    P1 = P[:2]
+    P2 = P[2:]
+    P2.rates = 1./defaultclock.dt
+    G = NeuronGroup(4, 'v:1')
+    S1 = Synapses(P1, G[:2], on_pre='v+=1')
+    S1.connect(j='i')
+    S2 = Synapses(P2, G[2:], on_pre='v+=1')
+    S2.connect(j='i')
+    run(2*defaultclock.dt)
+
+    assert_equal(G.v[:], np.array([0., 0., 2., 2.]))
+
+if __name__ == '__main__':
+    test_single_rates()
+    test_rate_arrays()
+    test_propagation()
+    test_poissongroup_subgroup()
