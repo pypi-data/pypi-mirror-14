bc977cbf525f3c8aa5d0c3097302e7280dcd22f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package xpm/0.3/xpm-0.3.tar.gz

---
+++ packages/xpm/xpm-0.3.tar.gz/xpm-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "xpm",
+    version = "0.3",
+    author = "Tal Wrii",
+    author_email = "talwrii@gmail.com",
+    description = "Library to output XPM files",
+    license = "BSD",
+    keywords = "xpm image PIL graphics",
+    url = "https://github.com/talwrii/xpm",
+    py_modules=['xpm'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Multimedia :: Graphics",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
