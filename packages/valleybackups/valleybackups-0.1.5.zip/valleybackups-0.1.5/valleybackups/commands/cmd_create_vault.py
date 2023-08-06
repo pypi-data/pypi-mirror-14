10966e20ef6e283701358617356c23b9dcf66574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:45 +0000

    Add package valleybackups/0.1.5/valleybackups-0.1.5.zip

---
+++ packages/valleybackups/valleybackups-0.1.5.zip/valleybackups-0.1.5/valleybackups/commands/cmd_create_vault.py
@@ -0,0 +1,19 @@
+import click
+
+from valleybackups.config_context import pass_config
+from valleybackups import db
+
+
+@click.command()
+@click.argument('vault_name', type=str)
+@pass_config
+def cli(config, vault_name):
+    """Creates a new Vault in Glacier"""
+    if config.service == "Glacier":
+        response = config.glacier.create_vault(vault_name)
+        if response:
+            click.echo("Vault %s successfully created" % vault_name)
+            config.handler.set_config('glacier', 'VAULT_NAME', vault_name)
+            config.handler.save_config()
+            db.init_mapping()
+            db.create_vault(vault_name, "Glacier")
