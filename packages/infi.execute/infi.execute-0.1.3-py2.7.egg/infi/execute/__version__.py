ef47fb5bf166d2bece4d514155d0fd50f78e1cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:44 +0000

    Add package infi.execute/0.1.3/infi.execute-0.1.3-py2.7.egg

---
+++ packages/infi.execute/infi.execute-0.1.3-py2.7.egg/infi/execute/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd88f8d3c12fbd720ff7c3d47d5d8a644b444bf0e'
+__git_head_subject__ = 'Finished Release v0.1.3'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-04-03 12:59:59'
