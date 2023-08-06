cd77805fd595ad770798584cbea25f4924a6445c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package planemo/0.24.1/planemo-0.24.1-py2.py3-none-any.whl

---
+++ packages/planemo/planemo-0.24.1-py2.py3-none-any.whl/planemo/commands/cmd_cwl_script.py
@@ -0,0 +1,24 @@
+"""
+"""
+from __future__ import print_function
+
+import click
+
+from planemo.cli import pass_context
+from planemo import options
+from planemo.cwl import to_script
+
+
+@click.command("cwl_script")
+@options.required_tool_arg()
+@options.required_job_arg()
+@click.option('--no_container', is_flag=True, default=False)
+@click.option('outdir', '--output_dir', type=click.Path())
+@click.option('basedir', '--base_dir', type=click.Path(), default=".")
+@pass_context
+def cli(ctx, path, job_path, **kwds):
+    """This compiles simple common workflow language workflows to a shell
+    script.
+    """
+    to_script(ctx, path, job_path, **kwds)
+    return 0
