4eed4fd6eadf1e9afb34637e3609811f344004bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:00 +0000

    Add package utlts/0.1.0/utlts-0.1.0.tar.gz

---
+++ packages/utlts/utlts-0.1.0.tar.gz/utlts-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        name='utlts',
+        packages=find_packages(),
+        version='0.1.0',
+        description='Utility functions functions.',
+        long_description='',
+        url='https://github.com/asalomatov/utlts',
+        author='Andrei Salomatov',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License', 
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            ],
+        install_requires=[
+            'pandas>=0.17',
+            'numpy',
+            'pysam>=0.8.3',
+            'collections',
+            'subprocces',
+            'tempfile',
+            'errno',
+            'glob',
+        ],
+)
