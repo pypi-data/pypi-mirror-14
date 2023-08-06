c8dbb9aeae0d534a8c3feb694c0af3320debc0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:27 +0000

    Add package django-lumberjack/0.2.2/django-lumberjack-0.2.2.tar.gz

---
+++ packages/django-lumberjack/django-lumberjack-0.2.2.tar.gz/django-lumberjack-0.2.2/lumberjack/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class LogEntry(models.Model):
+    level       = models.CharField(max_length=16)
+    message     = models.TextField()
+    logged_at   = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        app_label = 'lumberjack'
+
+
+class LogTag(models.Model):
+    tag         = models.CharField(max_length=64)
+    log_entries = models.ManyToManyField(LogEntry, related_name='tags')
+    
+    class Meta:
+        app_label = 'lumberjack'
