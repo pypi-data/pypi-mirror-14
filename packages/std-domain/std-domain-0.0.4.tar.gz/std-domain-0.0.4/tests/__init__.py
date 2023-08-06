a99b4398f4f9b3adc482dcbd5bec868030cb2e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package std-domain/0.0.4/std-domain-0.0.4.tar.gz

---
+++ packages/std-domain/std-domain-0.0.4.tar.gz/std-domain-0.0.4/tests/__init__.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+    def tearDown(self):
+        super(TestCase, self).tearDown()
