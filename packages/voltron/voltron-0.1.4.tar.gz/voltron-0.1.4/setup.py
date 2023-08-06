6776c3e027456e41d0d25b52413233635d3a186e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:42 +0000

    Add package voltron/0.1.4/voltron-0.1.4.tar.gz

---
+++ packages/voltron/voltron-0.1.4.tar.gz/voltron-0.1.4/setup.py
@@ -0,0 +1,34 @@
+import sys
+from setuptools import setup, find_packages
+
+
+requirements = [
+    'scruffington>=0.3.2',
+    'flask',
+    'flask_restful',
+    'blessed',
+    'pygments',
+    'requests',
+    'requests_unixsocket',
+    'six'
+]
+if sys.platform == 'win32':
+    requirements.append('cursor')
+
+setup(
+    name="voltron",
+    version="0.1.4",
+    author="snare",
+    author_email="snare@ho.ax",
+    description=("A debugger UI"),
+    license="MIT",
+    keywords="voltron debugger ui gdb lldb vdb vivisect vtrace windbg cdb pykd",
+    url="https://github.com/snare/voltron",
+    packages=find_packages(exclude=['tests', 'examples']),
+    install_requires=requirements,
+    package_data={'voltron': ['config/*']},
+    entry_points={
+        'console_scripts': ['voltron=voltron:main']
+    },
+    zip_safe=False
+)
