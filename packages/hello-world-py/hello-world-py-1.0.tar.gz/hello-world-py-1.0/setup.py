fe5eb5e1a2c6fc8d14f6297f654a7c3a9b0b59b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package hello-world-py/1.0/hello-world-py-1.0.tar.gz

---
+++ packages/hello-world-py/hello-world-py-1.0.tar.gz/hello-world-py-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hello-world-py',
+  version='1.0',
+  description='A basic hello world package',
+  author='zurfyx',
+  url='https://github.com/zurfyx',
+  packages=['hello'],
+ )
