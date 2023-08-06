d5b3611d6638e4637d2e1b752d8a31f3b8332506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:23 +0000

    Add package girder/1.5.2/girder-1.5.2.tar.gz

---
+++ packages/girder/girder-1.5.2.tar.gz/girder-1.5.2/clients/web/src/girder-version.js
@@ -0,0 +1,11 @@
+/*eslint-disable */
+girder.versionInfo = 
+{
+  "git": true,
+  "SHA": "cfcc8795f58ea19471f8fc801cf5e0e688b77612",
+  "shortSHA": "cfcc879",
+  "date": "2016-04-26T18:16:56",
+  "apiVersion": "1.5.2"
+}
+;
+/*eslint-enable */
---
+++ packages/girder/girder-1.5.2.tar.gz/girder-1.5.2/clients/web/src/models/AssetstoreModel.js
@@ -0,0 +1,29 @@
+girder.models.AssetstoreModel = girder.Model.extend({
+    resourceName: 'assetstore',
+
+    capacityKnown: function () {
+        var cap = this.get('capacity');
+        return cap && cap.free !== null && cap.total !== null;
+    },
+
+    capacityString: function () {
+        var cap = this.get('capacity');
+        return girder.formatSize(cap.free) + ' free of ' +
+            girder.formatSize(cap.total) + ' total';
+    },
+
+    import: function (params) {
+        girder.restRequest({
+            path: 'assetstore/' + this.get('_id') + '/import',
+            type: 'POST',
+            data: params,
+            error: null
+        }).done(_.bind(function (resp) {
+            this.trigger('g:imported', resp);
+        }, this)).error(_.bind(function (resp) {
+            this.trigger('g:error', resp);
+        }, this));
+
+        return this;
+    }
+});
