3b2fc4f59e6393d91e792c7be3294878139b58cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package fluent-logger/0.4.1/fluent-logger-0.4.1.tar.gz

---
+++ packages/fluent-logger/fluent-logger-0.4.1.tar.gz/fluent-logger-0.4.1/fluent/sender.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import socket
+import threading
+import time
+import traceback
+
+import msgpack
+
+
+_global_sender = None
+
+
+def _set_global_sender(sender):
+    """ [For testing] Function to set global sender directly
+    """
+    global _global_sender
+    _global_sender = sender
+
+
+def setup(tag, **kwargs):
+    global _global_sender
+    _global_sender = FluentSender(tag, **kwargs)
+
+
+def get_global_sender():
+    return _global_sender
+
+
+class FluentSender(object):
+    def __init__(self,
+                 tag,
+                 host='localhost',
+                 port=24224,
+                 bufmax=1 * 1024 * 1024,
+                 timeout=3.0,
+                 verbose=False,
+                 **kwargs):
+
+        self.tag = tag
+        self.host = host
+        self.port = port
+        self.bufmax = bufmax
+        self.timeout = timeout
+        self.verbose = verbose
+
+        self.socket = None
+        self.pendings = None
+        self.lock = threading.Lock()
+
+        try:
+            self._reconnect()
+        except Exception:
+            # will be retried in emit()
+            self._close()
+
+    def emit(self, label, data):
+        cur_time = int(time.time())
+        self.emit_with_time(label, cur_time, data)
+
+    def emit_with_time(self, label, timestamp, data):
+        try:
+            bytes_ = self._make_packet(label, timestamp, data)
+        except Exception:
+            bytes_ = self._make_packet(label, timestamp,
+                                       {"level": "CRITICAL",
+                                        "message": "Can't output to log",
+                                        "traceback": traceback.format_exc()})
+        self._send(bytes_)
+
+    def _make_packet(self, label, timestamp, data):
+        if label:
+            tag = '.'.join((self.tag, label))
+        else:
+            tag = self.tag
+        packet = (tag, timestamp, data)
+        if self.verbose:
+            print(packet)
+        return msgpack.packb(packet)
+
+    def _send(self, bytes_):
+        self.lock.acquire()
+        try:
+            self._send_internal(bytes_)
+        finally:
+            self.lock.release()
+
+    def _send_internal(self, bytes_):
+        # buffering
+        if self.pendings:
+            self.pendings += bytes_
+            bytes_ = self.pendings
+
+        try:
+            # reconnect if possible
+            self._reconnect()
+
+            # send message
+            self.socket.sendall(bytes_)
+
+            # send finished
+            self.pendings = None
+        except Exception:
+            # close socket
+            self._close()
+            # clear buffer if it exceeds max bufer size
+            if self.pendings and (len(self.pendings) > self.bufmax):
+                # TODO: add callback handler here
+                self.pendings = None
+            else:
+                self.pendings = bytes_
+
+    def _reconnect(self):
+        if not self.socket:
+            if self.host.startswith('unix://'):
+                sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                sock.settimeout(self.timeout)
+                sock.connect(self.host[len('unix://'):])
+            else:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(self.timeout)
+                sock.connect((self.host, self.port))
+            self.socket = sock
+
+    def _close(self):
+        if self.socket:
+            self.socket.close()
+        self.socket = None
