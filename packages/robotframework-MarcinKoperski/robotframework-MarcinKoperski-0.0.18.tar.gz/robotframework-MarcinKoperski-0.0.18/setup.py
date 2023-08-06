6a1d8f6ae3c6a3803415400d4cd539dd559b8a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package robotframework-MarcinKoperski/0.0.18/robotframework-MarcinKoperski-0.0.18.tar.gz

---
+++ packages/robotframework-MarcinKoperski/robotframework-MarcinKoperski-0.0.18.tar.gz/robotframework-MarcinKoperski-0.0.18/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2015 Cutting Edge QA
+
+
+"""Setup script for Robot's Marcin Koperski bundle distributions"""
+
+from setuptools import setup
+
+import sys, os
+
+src_path = os.path.join(os.path.dirname(__file__), 'src')
+sys.path.insert(0, src_path)
+
+__version_file_path__ = os.path.join(src_path, 'TestToolsMK', 'VERSION')
+__version__ = open(__version_file_path__, 'r').read().strip()
+
+
+setup(
+    name=u'robotframework-MarcinKoperski', version=__version__,
+    description=u'RobotFramework Marcin Koperski bundle',
+    author=u'Marcin Koperski',
+    author_email=u'marcin.koperski+github[at].gmail.com', license='AGPL',
+    url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski',
+    download_url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski/archive/master.zip'.format(version=__version__), keywords=['robotframework'],
+    package_dir={u'': 'src'}, packages=['TestToolsMK'], exclude_package_data={'': ['.git', '.git/*', '.idea', '.gitignore', 'doc/*', 'atest/*']},
+    package_data={'TestToolsMK': ['VERSION']},
+    install_requires=[
+        u'robotframework>=3.0',
+        u'robotframework-archivelibrary',
+        u'robotframework-databaselibrary', u'robotframework-imaplibrary', u'robotframework-pabot', u'robotframework-ride==1.5.2.1',
+        u'robotframework-selenium2library>=1.7.4',
+        u'robotframework-testmanagement>=0.1.10',  # u'robotframework-sshlibrary>=2.1.1',
+        u'robotframework-sudslibrary',
+        u'robotframework-appiumlibrary',
+        u'gspread', u'unicodecsv',
+        u'oauth2client==1.5.2',
+        u'selenium',
+        u'robotframework-httplibrary',
+
+    ]
+)
---
+++ packages/robotframework-MarcinKoperski/robotframework-MarcinKoperski-0.0.18.tar.gz/robotframework-MarcinKoperski-0.0.18/src/TestToolsMK/sqlite_keywords.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Cutting Edge QA
+
+import csv
+import os
+
+from robot.libraries import DateTime
+
+from robot_instances import *
+import sqlite3
+
+
+class SQLITE_Keywords(object):
+    def open_database_sqlite_file(self, path, **kwargs):
+        """
+        connect(database[, timeout, isolation_level, detect_types, factory])
+
+        Opens a connection to the SQLite database file *database*. You can use
+        ":memory:" to open a database connection to a database that resides in
+        RAM instead of on disk.
+        """
+
+        self.conn = sqlite3.connect(path, **kwargs)
+        self.conn.enable_load_extension(True)
+        self.cursor = self.conn.cursor()
+
+    def open_database_sqlite_using_csv_file(self, path, table):
+        """
+        Open csv file as database in ram it will not persist
+        """
+        self.conn = sqlite3.connect(':memory:')
+        self.cursor = self.conn.cursor()
+        self.conn.execute(".mode csv")
+        self.conn.execute(".import "+path+" "+table)
+
+    def execute_sql_string_sqlite(self, sqlString):
+        self.cursor.execute(sqlString)
+        temp = self.cursor.fetchall();
+        self.conn.commit()
+        return temp
+
+    def execute_sql_string_script_sqlite(self, sqlScriptString):
+        self.cursor.executescript(sqlScriptString)
+        temp = self.cursor.fetchall();
+        self.conn.commit()
+        return temp
+
+    def save_database_sqlite_to_file(self, path):
+        self.cursor(".save "+path)
+
+
+
+    def disconnect_database_sqlite(self):
+        self.conn.close()
+
+
