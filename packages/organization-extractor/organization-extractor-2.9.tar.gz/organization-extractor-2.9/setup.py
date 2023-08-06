66cdbdc0bb31031862f2016784615b3360b43522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:58 +0000

    Add package organization-extractor/2.9/organization-extractor-2.9.tar.gz

---
+++ packages/organization-extractor/organization-extractor-2.9.tar.gz/organization-extractor-2.9/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'organization-extractor',
+  packages = ['organization_extractor'],
+  version = '2.9',
+  description = 'Extracts organizations from text',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/organization-extractor',
+  download_url = 'https://github.com/DanielJDufour/organization-extractor/tarball/download',
+  keywords = ['arabic','farsi','french','kurdish','kurmanci','language','organizations','orgs','parsing','python','sorani','spanish','tagging','turkish'],
+  classifiers = [],
+)
