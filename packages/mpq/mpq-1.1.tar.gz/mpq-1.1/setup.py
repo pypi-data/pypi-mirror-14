f247c2ce00e095cb9d8a57b9f066db602e7916d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:54 +0000

    Add package mpq/1.1/mpq-1.1.tar.gz

---
+++ packages/mpq/mpq-1.1.tar.gz/mpq-1.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+
+
+CLASSIFIERS = [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.3",
+	"Programming Language :: Python :: 3.4",
+	"Programming Language :: Python :: 3.5",
+]
+
+DEBUG = False
+compile_args = []
+if DEBUG:
+	compile_args.append("-O0")
+
+module = Extension("mpq.storm",
+	sources=["mpq/stormmodule.cc"],
+	language="c++",
+	libraries=["storm"],
+	extra_compile_args=compile_args
+)
+
+setup(
+	name="mpq",
+	ext_modules=[module],
+	packages=["mpq"],
+	author="Jerome Leclanche",
+	author_email="jerome@leclan.ch",
+	classifiers=CLASSIFIERS,
+	description="Python bindings for StormLib",
+	download_url="http://github.com/hearthsim/python-mpq/tarball/master",
+	url="https://github.com/hearthsim/python-mpq",
+	version="1.1",
+)
