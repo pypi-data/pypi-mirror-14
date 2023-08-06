7ad4af1b3a617abe3a2839f205e045c05863cc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package pyaudioduplexfinder/0.2.0/pyaudioduplexfinder-0.2.0-py3.5.egg

---
+++ packages/pyaudioduplexfinder/pyaudioduplexfinder-0.2.0-py3.5.egg/pyaudioduplexfinder/pyauduxfind.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from pyaudioduplexfinder import PyAudioDupleyFinder
+
+import click
+
+@click.command()
+@click.option('--debug/--no-debug', default=False)
+@click.option('--analyze', is_flag=True)
+@click.argument('pathname', type=click.Path(exists=True))
+@click.option('--hash-type', type=click.Choice(['md5', 'sha1', 'sha256', 'sha512']), default='sha256')
+@click.option('--saveresult/--no-saveresult', default=True)
+def cli(debug, analyze, pathname,  hash_type, saveresult):
+    click.clear()
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+    pyaudufind = PyAudioDupleyFinder(pathname, ('.mp3', '.wav'))
+    if analyze:
+        pyaudufind.analyze(hash_type, debug)
+        duplfile = pyaudufind.get_duplicate_list()
+        pyaudufind.print_duplicate_list(duplfile)
+        if saveresult:
+            pyaudufind.save_duplicates_to_yaml(duplfile)
+    else:
+        pyaudufind.print_file_list()
+
+
+if __name__ == '__main__':
+    cli()
