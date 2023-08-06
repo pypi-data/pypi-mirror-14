22ca8a369936b58c8dbdacb5582d30c09d79046d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package mypackagemx3292016/0.2/mypackagemx3292016-0.2.zip

---
+++ packages/mypackagemx3292016/mypackagemx3292016-0.2.zip/mypackagemx3292016-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mypackagemx3292016',
+  packages = ['mypackagemx3292016'], # this must be the same as the name above
+  version = '0.2',
+  description = 'My first python package uploding to PYPI',
+  author = 'Jason Li ',
+  author_email = 'lmx423@gmail.com',
+  url = 'https://github.com/MinxianLi/mypackagemx3292016', # use the URL to the github repo
+  download_url = 'https://github.com/MinxianLi/mypackagemx3292016/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
