eb54d4b8911ae889872de98ece7a7738b19a1baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package slackpy/1.4.1/slackpy-1.4.1.tar.gz

---
+++ packages/slackpy/slackpy-1.4.1.tar.gz/slackpy-1.4.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__author__ = 'Takahiro Ikeuchi'
+
+requires = ['requests']
+tests_requires = ['requests', 'pytest']
+extras_requires = {
+    ':python_version<"3.4"': ["enum34"],
+}
+
+setup(
+    name="slackpy",
+    version="1.4.1",
+    py_modules=['slackpy', 'commandline'],
+    package_dir={'': 'slackpy'},
+    install_requires=requires,
+    extras_require=extras_requires,
+    description="Simple and Useful Slack client library",
+    long_description=open('README.rst').read(),
+    author='Takahiro Ikeuchi',
+    author_email='takahiro.ikeuchi@gmail.com',
+    url='https://github.com/iktakahiro/slackpy',
+    keywords=["Slack", "Slack Client"],
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Logging",
+        "Topic :: Communications :: Chat"
+    ],
+    entry_points={
+        "console_scripts": [
+            "slackpy=commandline:main",
+        ],
+    },
+    tests_require=tests_requires
+)
