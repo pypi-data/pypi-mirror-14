14ce719c714f4ba6083c4389c2ae4354dbc0ef40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:40 +0000

    Add package inlineplz/0.12.1/inlineplz-0.12.1.zip

---
+++ packages/inlineplz/inlineplz-0.12.1.zip/inlineplz-0.12.1/inlineplz/util/git.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+
+def current_sha():
+    return subprocess.check_output(
+        ['git', 'rev-parse', 'HEAD']
+    ).strip().decode('utf-8', errors='replace')
+
+
+def diff(start, end):
+    return subprocess.check_output(
+        ['git', 'diff', '-M', start + '..' + end]
+    ).strip().decode('utf-8', errors='replace')
+
+
+def parent_sha(sha):
+    return subprocess.check_output(
+        ['git', 'rev-list', '--parents', '-n', '1', sha]
+    ).strip().decode('utf-8', errors='replace').split()[1]
