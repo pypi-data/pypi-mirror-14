9fd2d6597cec9a1ebb715dc82e3806d148ebae0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:17 +0000

    Add package slackminion/0.5.3/slackminion-0.5.3.tar.gz

---
+++ packages/slackminion/slackminion-0.5.3.tar.gz/slackminion-0.5.3/slackminion/plugins/core/user.py
@@ -0,0 +1,48 @@
+from slackminion.plugin.base import BasePlugin
+
+
+class UserManager(BasePlugin):
+    """
+    Loads and stores user information
+    """
+    def on_load(self):
+
+        self._dont_save = True  # Don't save this plugin's state on shutdown
+        self.users = {}
+        self.admins = {}
+        if 'bot_admins' in self._bot.config:
+            self.admins = self._bot.config['bot_admins']
+        setattr(self._bot, 'user_manager', self)
+
+        super(UserManager, self).on_load()
+
+    def get(self, userid):
+        """Retrieve user by userid"""
+        if userid in self.users:
+            return self.users[userid]
+        return None
+
+    def get_by_username(self, username):
+        """Retrieve user by username"""
+        res = filter(lambda x: x.username == username, self.users.values())
+        if len(res) > 0:
+            return res[0]
+        return None
+
+    def set(self, user):
+        """
+        Adds a user object to the user manager
+
+        user - a SlackUser object
+        """
+
+        self.log.info("Loading user rights for %s/%s", user.userid, user.username)
+        self.load_user_rights(user)
+        self.log.info("Added user: %s/%s", user.userid, user.username)
+        self.users[user.userid] = user
+        return user
+
+    def load_user_rights(self, user):
+        """Sets permissions on user object"""
+        if user.username in self.admins:
+            user.is_admin = True
