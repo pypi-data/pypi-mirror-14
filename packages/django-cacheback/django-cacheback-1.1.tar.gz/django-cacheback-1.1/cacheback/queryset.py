f0e482320bc40e6cc5ccb814f162816c0383cafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:36 +0000

    Add package django-cacheback/1.1/django-cacheback-1.1.tar.gz

---
+++ packages/django-cacheback/django-cacheback-1.1.tar.gz/django-cacheback-1.1/cacheback/queryset.py
@@ -0,0 +1,46 @@
+from cacheback.base import Job
+
+
+class QuerySetJob(Job):
+    """
+    Helper class for wrapping ORM reads
+    """
+
+    def __init__(self, model, lifetime=None, fetch_on_miss=None, task_options=None):
+        """
+        :model: The model class to use
+        """
+        super(QuerySetJob, self).__init__()
+        self.model = model
+        if lifetime is not None:
+            self.lifetime = lifetime
+        if fetch_on_miss is not None:
+            self.fetch_on_miss = fetch_on_miss
+        if task_options is not None:
+            self.task_options = task_options
+
+    def key(self, *args, **kwargs):
+        return "%s-%s" % (
+            self.model.__name__,
+            super(QuerySetJob, self).key(*args, **kwargs)
+        )
+
+    def get_constructor_kwargs(self):
+        return {'model': self.model,
+                'lifetime': self.lifetime}
+
+
+class QuerySetGetJob(QuerySetJob):
+    """
+    For ORM reads that use the ``get`` method.
+    """
+    def fetch(self, *args, **kwargs):
+        return self.model.objects.get(**kwargs)
+
+
+class QuerySetFilterJob(QuerySetJob):
+    """
+    For ORM reads that use the ``filter`` method.
+    """
+    def fetch(self, *args, **kwargs):
+        return self.model.objects.filter(**kwargs)
