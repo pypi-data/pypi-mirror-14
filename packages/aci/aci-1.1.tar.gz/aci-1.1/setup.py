2139097e201bf4425f79e28dc6a6cbce672c2c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:11 +0000

    Add package aci/1.1/aci-1.1.tar.gz

---
+++ packages/aci/aci-1.1.tar.gz/aci-1.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name = 'aci',  
+      version = '1.1',
+      keywords = 'Analyzer CRISPR Indel',
+      description = 'Analyzer of CRISPR Indel', 
+      long_description = 'Analyze indel at target region sequence from CRISPR experiment.',
+      license = 'GPLv3',
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Programming Language :: Python :: 2.7',
+      ],
+      author = 'Yingxiang Li',  
+      author_email = 'xlccalyx@gmail.com', 
+      maintainer = 'Yingxiang Li',
+      url = 'www.calyx.biz',
+      download_url = 'http://www.calyx.biz/downloading--1997936733.html',
+      packages = ['aci'],
+      package_dir = {'aci': 'aci'},
+      scripts = ['aci/aci'],
+      package_data = {'aci': ['VarScan.v2.3.9.jar', 'example.input.tab']}
+) 
+
