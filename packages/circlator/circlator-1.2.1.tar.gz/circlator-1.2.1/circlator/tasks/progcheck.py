1296745988088ba9e8dd09a030e1ceffa6b91b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:29 +0000

    Add package circlator/1.2.1/circlator-1.2.1.tar.gz

---
+++ packages/circlator/circlator-1.2.1.tar.gz/circlator-1.2.1/circlator/tasks/progcheck.py
@@ -0,0 +1,10 @@
+import argparse
+import sys
+from circlator import versions
+
+def run():
+    parser = argparse.ArgumentParser(
+        description = 'Checks all dependencies are found and are correct versions',
+        usage = 'circlator progcheck')
+    options = parser.parse_args()
+    versions.get_all_versions(sys.stdout, raise_error=False)
