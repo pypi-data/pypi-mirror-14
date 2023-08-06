a9d155a29bfcc1acc27ce15d449419912005fb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:28 +0000

    Add package predictitpy/0.1.1/predictitpy-0.1.1.tar.gz

---
+++ packages/predictitpy/predictitpy-0.1.1.tar.gz/predictitpy-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='predictitpy',
+      version='0.1.1',
+      description='A very light wrapper around the PredictIt.org market data api.',
+      url='https://github.com/adamjoshuagray/predictitpy',
+      author='Adam J. Gray',
+      author_email='adam.joshua.gray@gmail.com',
+      license='MIT',
+      zip_safe=False)
