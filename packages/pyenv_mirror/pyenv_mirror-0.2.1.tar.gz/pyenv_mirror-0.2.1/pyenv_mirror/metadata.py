b24dda6df4f87efda98988cd8ba1b594e7923060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package pyenv_mirror/0.2.1/pyenv_mirror-0.2.1.tar.gz

---
+++ packages/pyenv_mirror/pyenv_mirror-0.2.1.tar.gz/pyenv_mirror-0.2.1/pyenv_mirror/metadata.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+NAME = 'pyenv_mirror'
+VERSION = '0.2.1'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = ''
