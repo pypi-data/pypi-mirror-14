0cdc97d4ce2d51f1f6602751fd3822e0d52794a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:46 +0000

    Add package tinycm/0.1.29/tinycm-0.1.29.tar.gz

---
+++ packages/tinycm/tinycm-0.1.29.tar.gz/tinycm-0.1.29/tinycm/backup.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import logging
+
+current_id = None
+
+
+def backup_file(path):
+    logger = logging.getLogger('tinycm')
+    if not current_id:
+        raise Exception('Backup requested before unique id is generated')
+    path = os.path.realpath(path)
+    logger.debug('Starting backup for {}'.format(path))
+    backup_dir = '/var/backups/tinycm'
+    backupfile = os.path.join(backup_dir, current_id, path[1:])
+    backupfile_path = os.path.dirname(backupfile)
+    os.makedirs(backupfile_path, 0o700, exist_ok=True)
+    logger.debug('Backup to: {}'.format(backupfile))
+    shutil.copy2(path, backupfile)
