d017cb5d1d8fca8f73303ba661de010409d1a5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:17 +0000

    Add package currencylayer/1.0/currencylayer-1.0.tar.gz

---
+++ packages/currencylayer/currencylayer-1.0.tar.gz/currencylayer-1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="currencylayer",
+    version="1.0",
+    url="https://github.com/Said007/currencylayer",
+
+    author="Said Ali",
+    author_email="said.ali@msn.com",
+
+    description="currencylayer python API client",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['requests>=2.9.1'],
+    keywords=['currency', 'currencylayer', 'exchangerate', 'rates'],  # arbitrary keywords
+
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
