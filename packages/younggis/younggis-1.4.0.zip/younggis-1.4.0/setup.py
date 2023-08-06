b5596508ca99b81019aecce36fa8f4b324c9ce68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:15 +0000

    Add package younggis/1.4.0/younggis-1.4.0.zip

---
+++ packages/younggis/younggis-1.4.0.zip/younggis-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ='younggis',
+    version='1.4.0',
+    py_modules=['nester'],
+    author='younggis',
+    author_email='younggis@163,com',
+    url='http://www.younggis.com',
+    description='A simple printer of nested lists',
+    )
