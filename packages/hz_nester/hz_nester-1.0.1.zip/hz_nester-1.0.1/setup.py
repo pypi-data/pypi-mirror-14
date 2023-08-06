d6faf0a61cd41cc833aa614cdb20c59c12329770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:16 +0000

    Add package hz_nester/1.0.1/hz_nester-1.0.1.zip

---
+++ packages/hz_nester/hz_nester-1.0.1.zip/hz_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name	= "hz_nester",
+	version = "1.0.1",
+	py_modules = ['hz_nester'],
+	author = "huoxu_huoxu",
+	author_email = "530607199@qq.com",
+	url = "http://github.com/huoxuhuoxu",
+	description = "demo python",
+)
