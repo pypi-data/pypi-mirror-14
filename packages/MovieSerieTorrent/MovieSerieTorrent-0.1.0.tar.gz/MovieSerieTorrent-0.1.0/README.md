fffe813bc83bec99d447a34c17ef310ce1572f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package MovieSerieTorrent/0.1.0/MovieSerieTorrent-0.1.0.tar.gz

---
+++ packages/MovieSerieTorrent/MovieSerieTorrent-0.1.0.tar.gz/MovieSerieTorrent-0.1.0/README.md
@@ -0,0 +1,48 @@
+# Parser-Renamer from torrent files V0.1.0
+> Extract informations from torrents files
+
+A python program to Parse and Rename torrents files. The parser extract a maximun informations from a torrent filename to finnaly get the title from movie or serie. The informations store in a dictonnary. The program use regex to get informations. In the futur these informations will recover to rename filename.
+
+##Informations extracted from file
+
+For Movie and Serie:
+* Title
+* Year
+* Language
+* Extention file
+* Quality
+
+Additionals informations for Serie:
+* Season
+* Episode
+
+Optionals informations:
+* Sites download
+* Resolution
+* Audio
+* Uploader
+* Codec
+
+
+##Issues
+
+* Renamer
+* Handler for terminal to set option
+* GUI
+
+##Small inspiration
+
+For Parser program:
+* GitHub : [parse-torrent-name 0.1.0](https://github.com/divijbindlish/parse-torrent-name)
+* Library : [Download](https://pypi.python.org/pypi/parse-torrent-name/)
+
+##Contact
+
+> Pull requests, commits or issues are welcome!
+
+* Mail: petit.jonathan16@gmail.com
+* GitHub
+
+##License
+
+MIT © Jonathan Petit
---
+++ packages/MovieSerieTorrent/MovieSerieTorrent-0.1.0.tar.gz/MovieSerieTorrent-0.1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import MovieSerieT
+import os
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='MovieSerieTorrent',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=install_requires,
+    author="Petit Jonathan",
+    author_email="petit.jonathan16@gmail.com",
+    description="Parser and Renamer for torrents files (Movies and series)",
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    url='https://github.com/JonathanPetit/Parser-Renamer',
+    license= 'MIT',
+    keywords = 'parser renamer formatting python torrents torrent files file movie serie',
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+    ],
+)
