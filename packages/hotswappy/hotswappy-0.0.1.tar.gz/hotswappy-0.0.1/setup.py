38488f84fffd46914c2cb2821cc2610e944cb427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package hotswappy/0.0.1/hotswappy-0.0.1.tar.gz

---
+++ packages/hotswappy/hotswappy-0.0.1.tar.gz/hotswappy-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "hotswappy",
+    version = "0.0.1",
+    author = "Tay Frost",
+    author_email = "taylor@taybird.com",
+    description = ("A plugin hot-loader."),
+    license = "BSD",
+    url = "https://bitbucket.org/taybird/hotswappy",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+    ],
+)
