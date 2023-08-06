ea92aa2edb7cb6d473afd1b5e33bff81b97ec627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package geotweet/0.1.8/geotweet-0.1.8.tar.gz

---
+++ packages/geotweet/geotweet-0.1.8.tar.gz/geotweet-0.1.8/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='geotweet',
+    version='0.1.8',
+    description='Fetch geographic tweets from Twitter Streaming API',
+    author='Jeffrey Alan Meyers',
+    author_email='jeffrey.alan.meyers@gmail.com',
+    url='https://github.com/meyersj/geotweet',
+    packages=['geotweet', 'geotweet.twitter', 'geotweet.mongo', 'geotweet.geo'],
+    scripts=['bin/geotweet', 'bin/geoloader'],
+    include_package_data=True,
+    install_requires=[
+        'argparse',
+        'boto3',
+        'python-twitter',
+        'requests',
+        'requests-oauthlib',
+        'mrjob',
+        'pymongo',
+        'Geohash',
+        'Shapely',
+        'Rtree',
+        'lxml'
+    ]
+)
