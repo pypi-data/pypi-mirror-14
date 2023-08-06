2d24ccab216da2551336843b53cfc77bf5f4b0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:42 +0000

    Add package shellcodepatterns/0.1/shellcodepatterns-0.1.tar.gz

---
+++ packages/shellcodepatterns/shellcodepatterns-0.1.tar.gz/shellcodepatterns-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	name = "shellcodepatterns",
+    packages = ["shellcodepatterns"],
+    version = "0.1",
+    author = "B3mB4m",
+    author_email = "b3mb4m@protonmail.com",
+    )
