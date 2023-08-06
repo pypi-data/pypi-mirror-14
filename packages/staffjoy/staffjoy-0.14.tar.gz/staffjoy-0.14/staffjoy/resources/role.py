ea6bf1a7f7c5fd910950cfc227d1067699871175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package staffjoy/0.14/staffjoy-0.14.tar.gz

---
+++ packages/staffjoy/staffjoy-0.14.tar.gz/staffjoy-0.14/staffjoy/resources/role.py
@@ -0,0 +1,33 @@
+from ..resource import Resource
+from .worker import Worker
+from .schedule import Schedule
+from .shift import Shift
+
+
+class Role(Resource):
+    PATH = "organizations/{organization_id}/locations/{location_id}/roles/{role_id}"
+    ID_NAME = "role_id"
+
+    def get_workers(self, **kwargs):
+        return Worker.get_all(parent=self, **kwargs)
+
+    def get_worker(self, id=id):
+        return Worker.get(parent=self, id=id)
+
+    def create_worker(self, **kwargs):
+        return Worker.create(parent=self, **kwargs)
+
+    def get_schedules(self, **kwargs):
+        return Schedule.get_all(parent=self)
+
+    def get_schedule(self, id):
+        return Schedule.get(parent=self, id=id)
+
+    def get_shifts(self, **kwargs):
+        return Shift.get_all(parent=self, **kwargs)
+
+    def get_shift(self, id):
+        return Shift.get(parent=self, id=id)
+
+    def create_shift(self, **kwargs):
+        return Shift.create(parent=self, **kwargs)
