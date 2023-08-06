1e6afa74a23ca04839852db5ec7cf56af937dfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:47 +0000

    Add package bokeh-metaplot/0.2.1/bokeh-metaplot-0.2.1.tar.gz

---
+++ packages/bokeh-metaplot/bokeh-metaplot-0.2.1.tar.gz/bokeh-metaplot-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'bokeh-metaplot',
+    packages = ['bokeh_metaplot'], # this must be the same as the name above
+    version = '0.2.1',
+    description = 'a enhanced bokeh plot type which can including some additional infomation inside',
+    author = 'Wu Young',
+    author_email = 'doomsplayer@gmail.com',
+    url = 'https://github.com/doomsplayer/bokeh-metaplot', # use the URL to the github repo
+    download_url = 'https://github.com/doomsplayer/bokeh-metaplot/tarball/0.1', # I'll explain this in a second
+    keywords = ['bokeh', 'plot', 'metadata'], # arbitrary keywords
+    requires = ['bokeh'],
+    classifiers = [],
+)
