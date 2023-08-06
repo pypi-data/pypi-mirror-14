eb7cc2575668df902e1315443bdb9669411c8562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:47 +0000

    Add package larch/3.2.7/larch-3.2.7-cp35-none-win_amd64.whl

---
+++ packages/larch/larch-3.2.7-cp35-none-win_amd64.whl/larch/util/attribute_dict.py
@@ -0,0 +1,32 @@
+
+
+class dicta(dict):
+	'''Dictionary with attribute access.'''
+	def __getattr__(self, name):
+		try:
+			return self[name]
+		except KeyError:
+			if '_helper' in self:
+				return self['_helper'](name)
+			raise AttributeError(name)
+	__setattr__ = dict.__setitem__
+	__delattr__ = dict.__delitem__
+	def __repr__(self):
+		if self.keys():
+			m = max(map(len, list(self.keys()))) + 1
+			return '\n'.join([k.rjust(m) + ': ' + repr(v) for k, v in self.items()])
+		else:
+			return self.__class__.__name__ + "()"
+
+
+
+
+
+
+
+class function_cache(dict):
+	def __getitem__(self, key):
+		if key not in self:
+			self[key] = z = dicta()
+			return z
+		return super().__getitem__(key)
