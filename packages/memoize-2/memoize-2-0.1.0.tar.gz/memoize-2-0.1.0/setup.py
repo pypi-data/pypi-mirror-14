533006b439048a17a8a5931be249736983c26834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:31 +0000

    Add package memoize-2/0.1.0/memoize-2-0.1.0.tar.gz

---
+++ packages/memoize-2/memoize-2-0.1.0.tar.gz/memoize-2-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup (
+    name='memoize-2',
+    version='0.1.0',
+    author='Rodney Gomes',
+    author_email='rodneygomes@gmail.com',
+    url='',
+    test_suite="tests",
+    keywords = ['memoize', 'library'],
+    py_modules = ['memoize'],
+    license='Apache 2.0 License',
+    description='',
+    long_description=open('README.md').read(),
+)
