f42c0188213b4ce6138793c783f139fdd380a3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:44 +0000

    Add package wikimon_bot/0.0.3/wikimon_bot-0.0.3.tar.gz

---
+++ packages/wikimon_bot/wikimon_bot-0.0.3.tar.gz/wikimon_bot-0.0.3/setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+setup(
+        name = 'wikimon_bot',
+        packages = ['wikimon_bot'], # this must be the same as the name above
+        version = '0.0.3',
+        description = 'Wikimon is a Python script which creates an artificial intelligent digital pet that learns through mirroring.',
+        author = 'FaceRecog Pte Ltd',
+        author_email = 'muhd.amrullah@facerecog.asia',
+        url = 'http://facerecog.github.io/wikimon/', # use the URL to the github repo
+        download_url = 'https://github.com/facerecog/wikimon/tarball/master',
+        keywords = ['bot', 'chat'],
+        classifiers = [],
+        scripts=['wikimon.py']
+        )
+
+
