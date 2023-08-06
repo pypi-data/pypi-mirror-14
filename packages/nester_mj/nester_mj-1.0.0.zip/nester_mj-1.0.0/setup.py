f1ee93ddcce02369825c371ce94ff42f094cba04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:24 +0000

    Add package nester_mj/1.0.0/nester_mj-1.0.0.zip

---
+++ packages/nester_mj/nester_mj-1.0.0.zip/nester_mj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nester_mj',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author = 'manojlingaiahpython',
+		author_email = 'manojl1985@gmail.com',
+		url = 'http://www.manojl711learning.com',
+		description = 'A simple printer of nested lines',
+	)
