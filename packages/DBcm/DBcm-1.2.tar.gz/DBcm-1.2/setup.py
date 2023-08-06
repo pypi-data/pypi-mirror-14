f6c2941716e7e3f38f27245974af1cb0747e378d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:17 +0000

    Add package DBcm/1.2/DBcm-1.2.tar.gz

---
+++ packages/DBcm/DBcm-1.2.tar.gz/DBcm-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DBcm',
+    version='1.2',
+    description='The Head First Python 2e Database Context Manager',
+    author='Paul Barry',
+    author_email='paul.james.barry@gmail.com',
+    url='http://www.headfirstlabs.com',
+    py_modules=['DBcm'],
+    dependency_links=['http://dev.mysql.com/downloads/connector/python/'],
+)
