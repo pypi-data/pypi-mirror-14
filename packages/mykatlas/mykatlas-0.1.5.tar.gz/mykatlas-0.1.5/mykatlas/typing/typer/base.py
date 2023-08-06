4685b4713da17ea091ab519bd4b77dce9c3ed3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:30 +0000

    Add package mykatlas/0.1.5/mykatlas-0.1.5.tar.gz

---
+++ packages/mykatlas/mykatlas-0.1.5.tar.gz/mykatlas-0.1.5/mykatlas/typing/typer/base.py
@@ -0,0 +1,36 @@
+MIN_CONF = -99999999
+MIN_LLK = -99999999
+
+
+class Typer(object):
+
+    def __init__(
+            self,
+            expected_depths,
+            contamination_depths=[],
+            error_rate=0.05,
+            ignore_filtered=False):
+        self.expected_depths = expected_depths
+        self.contamination_depths = contamination_depths
+        self.error_rate = error_rate
+        self.ignore_filtered = ignore_filtered
+
+    def type(self, l):
+        raise NotImplemented("Implemented in sub class")
+
+    def likelihoods_to_genotype(self, likelihoods, min_conf=MIN_CONF):
+        ml = max(likelihoods)
+        i = likelihoods.index(ml)
+        if i == 0:
+            if ml <= min_conf:
+                gt = "-/-"
+            else:
+                gt = "0/0"
+        elif i == 1:
+            gt = "0/1"
+        elif i == 2:
+            gt = "1/1"
+        return gt
+
+    def has_contamination(self):
+        return self.contamination_depths or len(self.expected_depths) > 1
---
+++ packages/mykatlas/mykatlas-0.1.5.tar.gz/mykatlas-0.1.5/mykatlas/version.py
@@ -0,0 +1,1 @@
+__version__ = "v0.1.5-0-g6c3f8b6-dirty"
