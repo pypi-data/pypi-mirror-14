ff4e2da253801ccde4a7e630400858adc561e534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:47 +0000

    Add package collective.gelf/0.9.0/collective.gelf-0.9.0.tar.gz

---
+++ packages/collective.gelf/collective.gelf-0.9.0.tar.gz/collective.gelf-0.9.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='collective.gelf',
+    version='0.9.0',
+    description='Graylog logging handler for Plone',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    keywords='',
+    author='Asko Soukka',
+    author_email='asko.soukka@iki.fi',
+    url='https://github.com/datakurre/collective.gelf',
+    license='EUPL',
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'ZConfig',
+        'pygelf'
+    ],
+    extras_require={'test': [
+    ]},
+    entry_points="""
+    """,
+)
