d27a7a9ea4daa7d976644c8ebd327f0c68ed96b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:20 +0000

    Add package kinto2xml/0.1.0/kinto2xml-0.1.0.tar.gz

---
+++ packages/kinto2xml/kinto2xml-0.1.0.tar.gz/kinto2xml-0.1.0/CHANGELOG.rst
@@ -0,0 +1,29 @@
+CHANGELOG
+#########
+
+This document describes changes between each past release.
+
+0.1.0 (2016-04-27)
+==================
+
+**Initial version**
+
+- Create collection with the definition of the JSON schema.
+- Fetch AMO blocklists information from the /blocked/blocklists.json AMO endpoint.
+- Handle import configuration on the CLI.
+  - Bucket / Collection names
+  - Remote AMO endpoint configuration
+  - Schema file path configuration
+  - Schema or not schema
+  - Verbosity level
+  - Server selection
+  - Auth credentials
+  - Importation type selection
+- Support for kinto-signer triggering
+- Full SSL support for Python 2.7
+- Full Python 2.7 and Python 3.4/3.5 support
+- Handle the enabled flag to activate records
+- Makefile rule to update the schema definition
+- Export kinto blocklists in XML blocklist file version 2
+- Export kinto blocklists in XML blocklist file version 3
+- XML verifier that create a diff of two XML files
---
+++ packages/kinto2xml/kinto2xml-0.1.0.tar.gz/kinto2xml-0.1.0/setup.py
@@ -0,0 +1,62 @@
+import codecs
+import os
+import sys
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
+
+with codecs.open(os.path.join(HERE, 'CHANGELOG.rst'), encoding='utf-8') as f:
+    CHANGELOG = f.read()
+
+
+REQUIREMENTS = [
+    'kinto-client',
+    'requests',
+    'lxml',
+    'six',
+    'xmltodict',
+]
+
+
+if sys.version_info < (2, 7, 9):
+    # Add OpenSSL dependencies to handle requests SSL warning.
+    REQUIREMENTS.append([
+        "pyopenssl",
+        "ndg-httpsclient",
+        "pyasn1"
+    ])
+
+
+ENTRY_POINTS = {
+    'console_scripts': [
+        'kinto2xml = kinto2xml.exporter:main',
+        'json2kinto = kinto2xml.importer:main',
+        'xml-verifier = kinto2xml.verifier:main',
+    ]
+}
+
+
+setup(name='kinto2xml',
+      version='0.1.0',
+      description='Generate a blocklists.xml file from the Kinto collections.',
+      long_description=README + "\n\n" + CHANGELOG,
+      license='Apache License (2.0)',
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      keywords="web services",
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.com',
+      url='https://github.com/mozilla-services/kinto2xml',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=REQUIREMENTS,
+      entry_points=ENTRY_POINTS)
