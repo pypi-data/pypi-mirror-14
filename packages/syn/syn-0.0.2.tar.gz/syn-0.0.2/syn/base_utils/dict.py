8d8391b98feb00a61dbc39626919006615116353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:12 +0000

    Add package syn/0.0.2/syn-0.0.2.tar.gz

---
+++ packages/syn/syn-0.0.2.tar.gz/syn-0.0.2/syn/base_utils/dict.py
@@ -0,0 +1,117 @@
+''' Various dict extensions.
+'''
+from functools import reduce
+
+#-------------------------------------------------------------------------------
+# AttrDict
+
+
+class AttrDict(dict):
+    '''A dict whose items can be accessed as attributes.
+    '''
+    __slots__ = ()
+
+    def __getattr__(self, attr):
+        if attr not in self:
+            raise AttributeError("No attribute '{}' present".format(attr))
+        return self[attr]
+
+    def __setattr__(self, attr, val):
+        self[attr] = val
+
+    def __delattr__(self, attr):
+        if attr not in self:
+            raise AttributeError("No attribute '{}' present".format(attr))
+        del self[attr]
+
+
+#-------------------------------------------------------------------------------
+# UpdateDict
+
+
+class UpdateDict(dict):
+    '''A dict with an extensible update() hook.
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateDict, self).__init__(*args, **kwargs)
+        self._update()
+
+    def __setitem__(self, key, value):
+        super(UpdateDict, self).__setitem__(key, value)
+        self._update()
+
+    def __delitem__(self, key):
+        super(UpdateDict, self).__delitem__(key)
+        self._update()
+
+    def _update(self):
+        raise NotImplementedError
+
+    def update(self, *args, **kwargs):
+        super(UpdateDict, self).update(*args, **kwargs)
+        self._update()
+
+
+#-------------------------------------------------------------------------------
+# GroupDict
+
+
+class GroupDict(AttrDict):
+    '''An AttrDict whose items are treated as sets.
+    '''
+
+    def combine(self, other):
+        for key, val in other.items():
+            if key not in self:
+                self[key] = val
+            else:
+                self[key].update(val)
+
+    def complement(self, *args):
+        '''Returns the difference of the union of all values and the union of the values in *args.
+        '''
+        universe = self.union()
+        to_diff = self.union(*args)
+        return universe.difference(to_diff)
+
+    def intersection(self, *args):
+        '''Returns the intersection of the values whose keys are in *args.  If *args is blank, returns the intersection of all values.
+        '''
+        values = self.values()
+        if args:
+            values = [val for key,val in self.items() if key in args]
+        return set(reduce(set.intersection, values))
+
+    def union(self, *args):
+        '''Returns the union of the values whose keys are in *args.  If *args is blank, returns the union of all values.
+        '''
+        values = self.values()
+        if args:
+            values = [val for key,val in self.items() if key in args]
+        return set(reduce(set.union, values))
+
+
+#-------------------------------------------------------------------------------
+# ReflexiveDict
+
+
+class ReflexiveDict(AttrDict):
+    '''An AttrDict for which each key == the associated value.
+    '''
+    def __init__(self, *args, **kwargs):
+        if args and not kwargs:
+            kwargs = {arg: arg for arg in args}
+            args = ()
+        super(ReflexiveDict, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        super(ReflexiveDict, self).__setitem__(key, key)
+
+
+#-------------------------------------------------------------------------------
+# __all__
+
+__all__ = ('AttrDict', 'UpdateDict', 'GroupDict', 'ReflexiveDict')
+
+#-------------------------------------------------------------------------------
---
+++ packages/syn/syn-0.0.2.tar.gz/syn-0.0.2/syn/base_utils/tests/test_dict.py
@@ -0,0 +1,110 @@
+from nose.tools import assert_raises
+from syn.base_utils import AttrDict, UpdateDict, GroupDict, ReflexiveDict
+
+#-------------------------------------------------------------------------------
+# AttrDict
+
+def test_attrdict():
+    obj = AttrDict(a = 1, b = 2)
+    print(obj)
+
+    assert obj['a'] == 1
+    assert obj.a == 1
+    assert obj['b'] == 2
+    assert obj.b == 2
+
+    del obj.b
+    assert 'b' not in obj
+    
+    def test_get_item(key):
+        return obj[key]
+    def test_del_item(key):
+        del obj[key]
+
+    assert_raises(KeyError, test_get_item, 'b')
+    assert_raises(AttributeError, getattr, obj, 'b')
+    assert_raises(KeyError, test_del_item, 'b')
+    assert_raises(AttributeError, delattr, obj, 'b')
+
+    obj.a = 3
+    assert obj['a'] == 3
+
+    obj.c = 5
+    assert obj['c'] == 5
+    assert obj.c == 5
+
+#-------------------------------------------------------------------------------
+# UpdateDict
+
+def test_updatedict():
+    assert_raises(NotImplementedError, UpdateDict)
+
+    class Dict(UpdateDict):
+        def _update(self):
+            self.count = len(self)
+
+    d = Dict(a = 1, b = 2)
+    assert d == dict(a = 1, b = 2)
+    assert d.count == 2
+    
+    d['a'] = 3
+    assert d == dict(a = 3, b = 2)
+    assert d.count == 2
+
+    d['c'] = 4
+    assert d == dict(a = 3, b = 2, c = 4)
+    assert d.count == 3
+    
+    del d['c']
+    assert d == dict(a = 3, b = 2)
+    assert d.count == 2
+
+    d.update(dict(a = 1, c = 3))
+    assert d == dict(a = 1, b = 2, c = 3)
+    assert d.count == 3
+
+#-------------------------------------------------------------------------------
+# GroupDict
+
+def test_groupdict():
+    dct = GroupDict(a = set([1, 2, 3]),
+                    b = set([3, 4, 5]))
+
+    assert dct.union() == set([1, 2, 3, 4, 5])
+    assert dct.intersection() == set([3])
+    assert dct.complement('a') == set([4, 5])
+    assert dct.complement('b') == set([1, 2])
+
+    dct2 = GroupDict(b = set([5, 6]),
+                     c = set([6, 7]))
+    dct.combine(dct2)
+    assert dct == dict(a = set([1, 2, 3]),
+                       b = set([3, 4, 5, 6]),
+                       c = set([6, 7]))
+
+    assert dct.union() == set(range(1, 8))
+    assert dct.union('a', 'c') == set([1, 2, 3, 6, 7])
+
+    assert dct.intersection() == set()
+    assert dct.intersection('a', 'b') == set([3])
+
+    assert dct.complement('a') == set(range(4, 8))
+    assert dct.complement('a', 'b') == set([7])
+
+#-------------------------------------------------------------------------------
+# ReflexiveDict
+
+def test_reflexivedict():
+    dct = ReflexiveDict('a', 'b', 'c')
+    assert dct == dict(a = 'a',
+                       b = 'b',
+                       c = 'c')
+
+    dct.d = 1
+    assert dct.d == 'd'
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__': # pragma: no cover
+    from syn.base_utils import run_all_tests
+    run_all_tests(globals(), verbose=True, print_errors=False)
