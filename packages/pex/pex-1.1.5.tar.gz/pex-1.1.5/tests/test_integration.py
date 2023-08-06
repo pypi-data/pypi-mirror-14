f1a655adfea877a31a15bb2166f7505c2d90c2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:20 +0000

    Add package pex/1.1.5/pex-1.1.5.tar.gz

---
+++ packages/pex/pex-1.1.5.tar.gz/pex-1.1.5/tests/test_integration.py
@@ -0,0 +1,51 @@
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+import sys
+
+import pytest
+from twitter.common.contextutil import environment_as, temporary_dir
+
+from pex.compatibility import WINDOWS
+from pex.testing import run_simple_pex_test
+from pex.util import named_temporary_file
+
+
+def test_pex_execute():
+  body = "print('Hello')"
+  _, rc = run_simple_pex_test(body, coverage=True)
+  assert rc == 0
+
+
+def test_pex_raise():
+  body = "raise Exception('This will improve coverage.')"
+  run_simple_pex_test(body, coverage=True)
+
+
+def test_pex_interpreter():
+  with named_temporary_file() as fp:
+    fp.write(b"print('Hello world')")
+    fp.flush()
+
+    env = os.environ.copy()
+    env['PEX_INTERPRETER'] = '1'
+
+    so, rc = run_simple_pex_test("", args=(fp.name,), coverage=True, env=env)
+    assert so == b'Hello world\n'
+    assert rc == 0
+
+
+@pytest.mark.skipif(WINDOWS, reason='No symlinks on windows')
+def test_pex_python_symlink():
+  with temporary_dir() as td:
+    with environment_as(HOME=td):
+      symlink_path = os.path.join(td, 'python-symlink')
+      os.symlink(sys.executable, symlink_path)
+      pexrc_path = os.path.join(td, '.pexrc')
+      with open(pexrc_path, 'w') as pexrc:
+        pexrc.write("PEX_PYTHON=%s" % symlink_path)
+
+      body = "print('Hello')"
+      _, rc = run_simple_pex_test(body, coverage=True)
+      assert rc == 0
