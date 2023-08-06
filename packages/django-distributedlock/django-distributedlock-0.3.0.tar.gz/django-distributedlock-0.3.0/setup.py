0fbc2d73dffda91f75e8f456efe5e203760ea150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:50 +0000

    Add package django-distributedlock/0.3.0/django-distributedlock-0.3.0.tar.gz

---
+++ packages/django-distributedlock/django-distributedlock-0.3.0.tar.gz/django-distributedlock-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-distributedlock',
+    version='0.3.0',
+    description="A distributed lock",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='distributed lock',
+    author='Erik Rivera',
+    author_email='erik.river@gmail.com',
+    maintainer='Paul Logston',
+    maintainer_email='code@logston.me',
+    license='BSD',
+    include_package_data=True,
+    test_suite='runtests.runtests',
+    tests_require=['gevent'],
+)
+
