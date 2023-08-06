fb131faf5e78b3409e566cde4d7d4594bd5193f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:29 +0000

    Add package xmldirector.plonecore/1.6.1/xmldirector.plonecore-1.6.1.zip

---
+++ packages/xmldirector.plonecore/xmldirector.plonecore-1.6.1.zip/xmldirector.plonecore-1.6.1/docker/start-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+git pull
+bin/develop update --force
+bin/buildout -c demo.cfg 
+echo starting existdb
+nohup /home/plone/exist/tools/wrapper/bin/exist.sh console &
+sleep 60 
+cd /home/plone/xmldirector.plonecore
+echo setting up plone
+bin/instance run src/xmldirector.demo/democontent/setup-plone.py docker
+echo starting plone
+bin/instance fg
