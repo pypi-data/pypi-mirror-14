ee6192ec8575c049465d9296b8067afa75977361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package matrixize/1.1.0/matrixize-1.1.0.zip

---
+++ packages/matrixize/matrixize-1.1.0.zip/matrixize-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='matrixize',
+	version='1.1.0',
+	py_modules= ['matrixize'],
+	author='ftayyab',
+	author_email='faizantayyab@yahoo.com',
+	url='none',
+	description='Convert List to Matrix',
+	)
