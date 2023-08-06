bcbad0e214e20aecead5dc30b4eeb29616a96e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package bond_zlj/0.1.0/bond_zlj-0.1.0.tar.gz

---
+++ packages/bond_zlj/bond_zlj-0.1.0.tar.gz/bond_zlj-0.1.0/bond_zlj/basic_classes.py
@@ -0,0 +1,28 @@
+from functions import pricing
+
+class Bond:
+    par_value = 100
+    coupon_rate = 0.05
+    maturity = 5
+    price = 100
+    ytm = 0.05
+    interval = 1
+
+    def __init__(self, par_value=100, coupon_rate=0.05, maturity=5, price=100, ytm=0.05, interval=1):
+        self.par_value = par_value
+        self.coupon_rate = coupon_rate
+        self.maturity = maturity
+        self.price = price
+        self.ytm = ytm
+        self.interval = interval
+
+    def calc_price(self):
+        self.price = pricing(self.par_value, self.coupon_rate, self.maturity, self.ytm, self.interval)
+        return self.price
+
+    def calc_ytm(self):
+        grid = [0.0001 * x for x in range(1, 2000)]
+        price_grid = [pricing(self.par_value, self.coupon_rate, self.maturity, x, self.interval) for x in grid]
+        index, price = reduce(lambda x, y: x if abs(x[1] - self.price) < abs(y[1] - self.price) else y, enumerate(price_grid))
+        self.ytm = grid[index]
+        return self.ytm
---
+++ packages/bond_zlj/bond_zlj-0.1.0.tar.gz/bond_zlj-0.1.0/setup.py
@@ -0,0 +1,44 @@
+import codecs
+import os
+import sys
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+NAME = "bond_zlj"
+PACKAGES = ["bond_zlj"]
+DESCRIPTION = "this is a package for bond calculation"
+LONG_DESCRIPTION = read("README.rst")
+KEYWORDS = "finance bond calculation"
+AUTHOR = "LinjunZhou"
+AUTHOR_EMAIL = "zhoulinjun1994@163.com"
+URL = "http://www.baidu.com/"
+VERSION = "0.1.0"
+LICENSE = "MIT"
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+)
+
