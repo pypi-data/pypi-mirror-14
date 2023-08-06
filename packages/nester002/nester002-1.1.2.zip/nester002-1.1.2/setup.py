5b8c637bd690954c4724ee7b19eaf9644f592c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:51 +0000

    Add package nester002/1.1.2/nester002-1.1.2.zip

---
+++ packages/nester002/nester002-1.1.2.zip/nester002-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='nester002',
+	version		='1.1.2',
+	py_moules	=['nester'],
+	author		='hfpython',
+	author_email='geamdjum@gmaul.com',
+	url			='',
+	description	='A simple printer of nester lists',
+)
