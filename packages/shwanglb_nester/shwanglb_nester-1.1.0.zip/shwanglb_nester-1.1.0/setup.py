11e9be06e4c3d2a7f5f2f42c194dbf185c1ec8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:52 +0000

    Add package shwanglb_nester/1.1.0/shwanglb_nester-1.1.0.zip

---
+++ packages/shwanglb_nester/shwanglb_nester-1.1.0.zip/shwanglb_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="shwanglb_nester",
+    version="1.1.0",
+    py_modules=["shwanglb_nester"],
+    author="wlb",
+    author_email="shwanglb@126.com",
+    description="A simple printer of nested lists",
+    )
