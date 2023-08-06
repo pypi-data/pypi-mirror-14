685a4787f3316ec0b1e2edf57740031128791d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:57 +0000

    Add package izigraph/0.11.2/izigraph-0.11.2.tar.gz

---
+++ packages/izigraph/izigraph-0.11.2.tar.gz/izigraph-0.11.2/izigraph/graph_exporter.py
@@ -0,0 +1,16 @@
+from .ngv_exporter import NgvExporter
+
+
+class GraphExporter:
+    def __init__(self, driver=None):
+        if driver == "ngv":
+            self.__exporter = NgvExporter()
+        else:
+            raise Exception("Driver: %s not found\n" % driver)
+        self.__driver = driver
+
+    def export(self, graph, routes=None):
+        return self.__exporter.export(graph, routes)
+
+    def driver(self):
+        return self.__driver
