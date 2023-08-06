dac747e38d0954f06c2191ec6912dbbf6c1bc46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:23 +0000

    Add package mongoengine-goodjson/0.9.3/mongoengine_goodjson-0.9.3.tar.gz

---
+++ packages/mongoengine-goodjson/mongoengine_goodjson-0.9.3.tar.gz/mongoengine_goodjson-0.9.3/mongoengine_goodjson/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""JSON serializer/deserializer for humans and MongoEngine."""
+
+from .encoder import GoodJSONEncoder
+from .decoder import generate_object_hook
+from .document import Document, EmbeddedDocument
+from .queryset import QuerySet
+
+__all__ = (
+    "GoodJSONEncoder", "generate_object_hook",
+    "Document", "EmbeddedDocument", "QuerySet"
+)
