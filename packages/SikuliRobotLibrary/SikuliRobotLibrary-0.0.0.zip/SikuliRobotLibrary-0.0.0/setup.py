54d5c4ebb9141cd12a355ea5301f672c72ab5d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:09 +0000

    Add package SikuliRobotLibrary/0.0.0/SikuliRobotLibrary-0.0.0.zip

---
+++ packages/SikuliRobotLibrary/SikuliRobotLibrary-0.0.0.zip/SikuliRobotLibrary-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+
+name='SikuliRobotLibrary',
+author='Wang Yang',
+homePage='https://github.com/rainmanwy/robotframework-SikuliLibrary',
+Keywords='robotframework testing testautomation sikuli UI',
+License='Apache License 2.0',
+
+)
