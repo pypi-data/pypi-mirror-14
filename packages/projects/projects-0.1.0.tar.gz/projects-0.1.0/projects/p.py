c798c9e1146cfae820f82774dc0add95aa32632d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:48 +0000

    Add package projects/0.1.0/projects-0.1.0.tar.gz

---
+++ packages/projects/projects-0.1.0.tar.gz/projects-0.1.0/projects/p.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def load_config():
+    pass
+
+
+def main(args):
+    print(args)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ packages/projects/projects-0.1.0.tar.gz/projects-0.1.0/test/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+from projects import main
+
+
+class BasicFunctionalityTests(unittest.TestCase):
+    def test__main_function_is_callable(self):
+        main('')
+
+
+
+
+¡
