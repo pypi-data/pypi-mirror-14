536ddc7f94aa64973626bf854a671c770a8bc1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:22 +0000

    Add package pyahocorasick/1.1.0/pyahocorasick-1.1.0.tar.gz

---
+++ packages/pyahocorasick/pyahocorasick-1.1.0.tar.gz/pyahocorasick-1.1.0/setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+from sys import version_info as version
+
+def get_readme():
+    with open('README.rst', 'rt') as f:
+        return f.read()
+
+if version.major not in [2, 3]:
+    raise ValueError("Python %s is not supported" % version)
+
+if version.major == 3:
+    macros = [
+        ('AHOCORASICK_UNICODE', ''),    # when defined unicode strings are supported
+    ]
+else:
+    macros = []
+
+module = Extension(
+    'ahocorasick',
+    sources = [
+        'pyahocorasick.c'
+    ],
+    define_macros = macros,
+    depends = [
+        'common.h',
+        'Automaton.c',
+        'Automaton.h',
+        'Automaton_pickle.c',
+        'AutomatonItemsIter.c',
+        'AutomatonItemsIter.h',
+        'AutomatonSearchIter.c',
+        'AutomatonSearchIter.h',
+        'trie.c',
+        'trie.h',
+        'slist.c',
+        'utils.c',
+        'trienode.c',
+        'trienode.h',
+    ],
+)
+
+setup(
+    name             = 'pyahocorasick',
+    version          = '1.1.0',
+
+    ext_modules      = [module],
+
+    description      = "Aho-Corasick automaton",
+    author           = "Wojciech Muła",
+    author_email     = "wojciech_mula@poczta.onet.pl",
+    maintainer       = "Wojciech Muła",
+    maintainer_email = "wojciech_mula@poczta.onet.pl",
+    url              = "http://github.com/WojciechMula/pyahocorasick",
+    platforms        = ["Linux", "Windows"],
+    license          = "BSD (3 clauses)",
+    long_description = get_readme(),
+    keywords         = [
+        "aho-corasick",
+        "trie",
+        "automaton",
+        "dictionary",
+    ],
+    classifiers      = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Editors :: Text Processing",
+    ],
+)
---
+++ packages/pyahocorasick/pyahocorasick-1.1.0.tar.gz/pyahocorasick-1.1.0/trienode.c
@@ -0,0 +1,70 @@
+/*
+	This is part of pyahocorasick Python module.
+	
+	Trie implementation
+
+	Author    : Wojciech Muła, wojciech_mula@poczta.onet.pl
+	License   : 3-clauses BSD (see LICENSE)
+*/
+
+#include "trienode.h"
+
+static TrieNode*
+trienode_new(const TRIE_LETTER_TYPE letter, char eow) {
+	TrieNode* node = (TrieNode*)memalloc(sizeof(TrieNode));
+	if (node) {
+		node->output.integer = 0;
+		node->output.object = NULL;
+		node->fail		= NULL;
+
+		node->n		= 0;
+		node->letter	= letter;
+		node->eow	    = eow;
+		node->pickle	= 0;
+		node->next	= NULL;
+	}
+
+	return node;
+}
+
+
+static TrieNode* PURE
+trienode_get_next(TrieNode* node, const TRIE_LETTER_TYPE letter) {
+	ASSERT(node);
+	int i;
+	for (i=0; i < node->n; i++)
+		if ((node)->next[i]->letter == letter)
+			return node->next[i];
+
+	return NULL;
+}
+
+
+static TrieNode* PURE
+trienode_get_ith_unsafe(TrieNode* node, size_t index) {
+    ASSERT(node);
+
+    return node->next[index];
+}
+
+
+static TrieNode*
+trienode_set_next(TrieNode* node, const TRIE_LETTER_TYPE letter, TrieNode* child) {
+	ASSERT(node);
+	ASSERT(child);
+	ASSERT(trienode_get_next(node, letter) == NULL);
+
+	const int n = node->n;
+	TrieNode** next = (TrieNode**)memrealloc(node->next, (n + 1) * sizeof(TrieNode*));
+	if (next) {
+		node->next = next;
+		node->next[n] = child;
+		node->n += 1;
+
+		return child;
+	}
+	else
+		return NULL;
+}
+
+
