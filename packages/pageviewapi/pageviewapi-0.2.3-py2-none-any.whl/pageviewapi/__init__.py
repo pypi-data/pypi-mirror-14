4991963aa1d3a9bd44168b8eb9a80dbdcc086927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:50 +0000

    Add package pageviewapi/0.2.3/pageviewapi-0.2.3-py2-none-any.whl

---
+++ packages/pageviewapi/pageviewapi-0.2.3-py2-none-any.whl/pageviewapi/__init__.py
@@ -0,0 +1,9 @@
+"""Python client for wikimedia pageview api."""
+
+from pageviewapi.client import per_article
+from pageviewapi.client import top
+from pageviewapi.client import aggregate
+from pageviewapi.client import Access
+from pageviewapi.client import Agent
+
+from pageviewapi.client import __version__
