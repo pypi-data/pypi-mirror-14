f6ffa026d4b724a68c6198283be5708cbbcdc8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:46 +0000

    Add package oide-slurm-assist/0.7.2/oide-slurm-assist-0.7.2.tar.gz

---
+++ packages/oide-slurm-assist/oide-slurm-assist-0.7.2.tar.gz/oide-slurm-assist-0.7.2/oideslurm/handlers.py
@@ -0,0 +1,58 @@
+import tornado.web
+import json
+
+import oide.lib.decorators
+import oideslurm.settings as app_settings
+from oideslurm.mixins.slurm_mixin import SlurmCmdMixin
+from oide.lib.handlers.base import BaseHandler
+from oideslurm.config_utils import ConfigLoader
+
+
+
+class FormConfigHandler(BaseHandler):
+
+    @oide.lib.decorators.authenticated
+    def get(self):
+        schema = ConfigLoader.getFormConfigs()
+        ctx = {
+            'formSchema': schema
+            }
+        self.write(ctx)
+
+class JobListHandler(BaseHandler,SlurmCmdMixin):
+
+    @oide.lib.decorators.authenticated
+    def get(self):
+        # This method should return all sacct data for
+        # the running user.
+        sacct_out = self.run_sacct()
+        json_obj = json.dumps(sacct_out)
+        self.write(json_obj)
+
+
+    @oide.lib.decorators.authenticated
+    def post(self):
+        # This method should take the filepath of an
+        # sbatch file as an argument, and then schedule
+        # a job with the indicated file using sbatch.
+        # The jobid of the scheduled job should be returned.
+        return_code,output = self.job_submit(filepath=json.loads(self.request.body)['content'])
+	if return_code != 0:
+	    self.set_status(500)
+	    self.write(output)
+	else:
+	    self.write("Successful Submission")
+
+class JobHandler(BaseHandler,SlurmCmdMixin):
+
+    @oide.lib.decorators.authenticated
+    def get(self,jobid):
+        # This method should retrieve sacct data
+        # for the job specified by jobid
+        self.write(self.run_sacct(jobid=jobid))
+
+    @oide.lib.decorators.authenticated
+    def delete(self,jobid):
+        # This method should dequeue the job specified
+        # by jobid, via scancel.
+        pass
---
+++ packages/oide-slurm-assist/oide-slurm-assist-0.7.2.tar.gz/oide-slurm-assist-0.7.2/oideslurm/mixins/slurm_mixin.py
@@ -0,0 +1,42 @@
+import tornado.web
+import subprocess
+
+
+class SlurmCmdMixin(tornado.web.RequestHandler):
+    # This class will contain any methods necessary
+    # for running slurm commands and parsing output.
+
+    def run_sacct(self,**kwargs):
+        # Handle keyword arguments, translate them into
+        # corresponding options for sacct via subprocess
+
+        output = []
+        options = ['-P','--format=JobId,Start,End,State,AllocCPUS,QOS,NodeList,TotalCPU,CPUTime,NNodes']
+
+        cmd = ['sacct'] + options
+        cmd_out = subprocess.check_output(cmd).split('\n')[:-1] # [:-1] because the last element of the list is ""
+        field_names = cmd_out[0].split('|')
+
+        # exclude the first row since it's name fields
+        for row in cmd_out[1:]:
+            job = {}
+            row = row.split('|')
+            job['JobID'] = row[0][1:] # delete a space at a head ,e.g. ' 908033' -> '908033'
+            for i in range(1,len(field_names)):
+                job[field_names[i]] = row[i]
+            output.append(job)
+
+        return output
+    
+    def job_submit(self,**kwargs):
+
+        file_path = kwargs["filepath"]
+        
+        cmd = ['sbatch'] + [file_path]
+	try:
+	    #df = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+	    #output, err = df.communicate()
+	    cmd_out = subprocess.check_output(cmd,stderr=subprocess.STDOUT)
+	    return (0,cmd_out)
+        except subprocess.CalledProcessError as e:
+	    return (e.returncode,e.output)
