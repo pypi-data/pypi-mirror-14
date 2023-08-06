e7081e24cfbf3bee0012fbeb41dec951da4257ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package logGetter/0.0.1/logGetter-0.0.1.tar.gz

---
+++ packages/logGetter/logGetter-0.0.1.tar.gz/logGetter-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'logGetter',
+    version = '0.0.1',
+    keywords = ('get log', 'oss'),
+    description = 'get log from oss test',
+    license = 'gd License',
+
+    author = 'ccg',
+    author_email = 'chenggang.ccg@alibaba-inc.com',
+    
+    packages = find_packages(),
+    platforms = 'all',
+)
