ea43f7233ea177a7a5d9f38be5bedf4f4d2a30dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:53 +0000

    Add package exerc_nester/1.0.0/exerc_nester-1.0.0.tar.gz

---
+++ packages/exerc_nester/exerc_nester-1.0.0.tar.gz/exerc_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name            = 'exerc_nester',
+        version         = '1.0.0',
+        py_modules		= ['exerc_nester'],
+        author          = 'Allef',
+        author_email    = 'allef@gmail.com',
+        url             = 'http://',
+        description     = 'A simple printer of nested lists',
+    )
