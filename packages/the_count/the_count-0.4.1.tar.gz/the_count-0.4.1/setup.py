a33b1a04fcf836631d791c375ef6cf215be926a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:44 +0000

    Add package the_count/0.4.1/the_count-0.4.1.tar.gz

---
+++ packages/the_count/the_count-0.4.1.tar.gz/the_count-0.4.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'the_count',
+    version = '0.4.1',
+    description = 'Handy class for counting things',
+    author = 'Brian Lauber',
+    author_email = 'constructible.truth@gmail.com',
+    packages = find_packages(exclude = ["tests"]),
+    install_requires = [],
+    test_suite = 'tests',
+    tests_require = ["pytest>=2.8.2"]
+)
