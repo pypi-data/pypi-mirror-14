9b841557e02785a48821b4cd1a67406449c3d40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:14 +0000

    Add package mayan-edms/2.1rc1/mayan-edms-2.1rc1.tar.gz

---
+++ packages/mayan-edms/mayan-edms-2.1rc1.tar.gz/mayan-edms-2.1rc1/mayan/apps/user_management/serializers.py
@@ -0,0 +1,43 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+from rest_framework import serializers
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    users_count = serializers.SerializerMethodField()
+
+    class Meta:
+        extra_kwargs = {
+            'url': {'view_name': 'rest_api:group-detail'}
+        }
+        fields = ('id', 'name', 'url', 'users_count')
+        model = Group
+
+    def get_users_count(self, instance):
+        return instance.user_set.count()
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    groups = GroupSerializer(many=True)
+
+    class Meta:
+        extra_kwargs = {
+            'url': {'view_name': 'rest_api:user-detail'}
+        }
+        fields = (
+            'first_name', 'date_joined', 'email', 'groups', 'id', 'is_staff',
+            'is_active', 'is_superuser', 'last_login', 'last_name',
+            'password', 'url', 'username',
+        )
+        model = get_user_model()
+        read_only_fields = ('last_login', 'date_joined')
+        write_only_fields = ('password',)
+
+    def restore_object(self, attrs, instance=None):
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        if 'password' in attrs:
+            user.set_password(attrs['password'])
+        return user
