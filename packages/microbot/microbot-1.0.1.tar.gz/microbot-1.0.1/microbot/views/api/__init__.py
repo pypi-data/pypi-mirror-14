99324e1a01ed3f2ecc76c7c073077dec05ba8d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:57 +0000

    Add package microbot/1.0.1/microbot-1.0.1.tar.gz

---
+++ packages/microbot/microbot-1.0.1.tar.gz/microbot-1.0.1/microbot/views/api/__init__.py
@@ -0,0 +1,6 @@
+from microbot.views.api.bot import BotList, BotDetail  # NOQA
+from microbot.views.api.environment_vars import EnvironmentVarList, EnvironmentVarDetail  # NOQA
+from microbot.views.api.handler import (HandlerList, HandlerDetail, HeaderParameterDetail, HeaderParameterList,  # NOQA
+                                        UrlParameterList, UrlParameterDetail, SourceStateList, SourceStateDetail)  # NOQA
+from microbot.views.api.hook import HookList, HookDetail, RecipientList, RecipientDetail  # NOQA
+from microbot.views.api.state import StateList, StateDetail, ChatStateList, ChatStateDetail  # NOQA
