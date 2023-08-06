8193bef95351315f250feb61f185e5985e791bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package quaternions/0.1/quaternions-0.1.zip

---
+++ packages/quaternions/quaternions-0.1.zip/quaternions-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='quaternions',
+      version='0.1',
+      description='A package to handle quaternions',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      keywords='quaternion transformations',
+      url='https://github.com/IRIM-Technology-Transition-Lab/quaternions',
+      author='Michael Sobrepera',
+      author_email='mjsobrep@live.com',
+      license='Copyright (c) 2016 GTRC',
+      packages=['quaternion'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite="quaternion.tests"
+      )
