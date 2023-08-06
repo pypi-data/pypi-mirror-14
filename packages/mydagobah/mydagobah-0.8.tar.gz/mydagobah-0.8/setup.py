28672da7c36a33dbf7fe234daeb95a0e7ca57ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:25 +0000

    Add package mydagobah/0.8/mydagobah-0.8.tar.gz

---
+++ packages/mydagobah/mydagobah-0.8.tar.gz/mydagobah-0.8/setup.py
@@ -0,0 +1,20 @@
+# _*_ coding: utf-8 _*_
+
+from distutils.core import setup
+
+setup(
+        name = "mydagobah",
+        packages = [
+            "mydagobah",
+            ],
+        version = "0.8",
+        description = "expanded to dagobah, this is a script to import job in batch mode",
+        author = "xuanxuan",
+        author_email = "13060404095@163.com",
+        url = "https:github.com/littlegump/mydagobah.git",
+        keywords = ['dagobah', 'module_task', 'distribute hundreds of tasks'],
+        classifiers = [
+            "Programming Language :: Python",
+            ],
+        )
+
