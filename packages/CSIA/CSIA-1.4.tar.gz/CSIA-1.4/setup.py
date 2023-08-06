bf9801d006af212b4c72db1e9b7d79839b8cef4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:26 +0000

    Add package CSIA/1.4/CSIA-1.4.tar.gz

---
+++ packages/CSIA/CSIA-1.4.tar.gz/CSIA-1.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name = 'CSIA',  
+      version = '1.4',
+      keywords = 'CRISPR Sequence Indel Analyze',
+      description = 'CRISPR Sequence Indel Analyze', 
+      long_description = 'After CRISPR experiment on DNA, sequencing target region and analyze indel frequency.',
+      license = 'MIT',
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      ],
+      author = 'Yingxiang Li',  
+      author_email = 'xlccalyx@gmail.com', 
+      maintainer = 'Yingxiang Li',
+      url = 'www.calyx.biz',
+      download_url = 'http://www.calyx.biz/uploads/2/1/9/2/21925892/csia-1.4.tar.gz',
+      packages = ['CSIA'],
+      package_dir = {'CSIA': 'CSIA'},
+      scripts = ['CSIA/CSIA'],
+      package_data = {'CSIA': ['VarScan.v2.3.9.jar']}
+) 
+
