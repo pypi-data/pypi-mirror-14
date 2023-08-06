3fec82ab67f3956c03c264d4232dc3975b01691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:57 +0000

    Add package sunnyfunction1/1.0.0/sunnyfunction1-1.0.0.zip

---
+++ packages/sunnyfunction1/sunnyfunction1-1.0.0.zip/sunnyfunction1-1.0.0/sunnyfunction1.py
@@ -0,0 +1,7 @@
+""" My First Module. Just a recursive function to print list """
+def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+		     print_lol(each_item)
+		else:
+			print(each_item)
