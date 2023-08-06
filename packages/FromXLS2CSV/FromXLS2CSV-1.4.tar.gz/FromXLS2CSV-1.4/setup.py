cf37bfc2bb2afe6ef3b4c6146f40ba0ae5652eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:05 +0000

    Add package FromXLS2CSV/1.4/FromXLS2CSV-1.4.tar.gz

---
+++ packages/FromXLS2CSV/FromXLS2CSV-1.4.tar.gz/FromXLS2CSV-1.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name="FromXLS2CSV",    # Nombre de la app (CamelCase)
+    version="1.4",          # Version
+    author="Miriam Olid", # Nombre del Autor
+    author_email="molidg@gmail.com", # E-mail del autor
+    packages=["fromxls2csv"],    # Paquetes a incluir
+    license="GPL v3.0",     # Licencia
+    url='http://www.python.org',
+    description="Conversor de archivos XLS a CSV",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: Spanish",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
