fab58f66bb57379cc2ac61b899a08f3839eefd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:26 +0000

    Add package nexmo/1.0.3/nexmo-1.0.3.tar.gz

---
+++ packages/nexmo/nexmo-1.0.3.tar.gz/nexmo-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='nexmo',
+  version='1.0.3',
+  description='Python client for the Nexmo API',
+  long_description='Python client for the Nexmo API',
+  url='http://github.com/nexmo/python-nexmo',
+  author='Tim Craft',
+  author_email='mail@timcraft.com',
+  license='MIT',
+  packages=['nexmo'],
+  platforms=['any'],
+  install_requires=['requests'])
