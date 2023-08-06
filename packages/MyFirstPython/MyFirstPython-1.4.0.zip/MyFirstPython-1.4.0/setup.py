3f65a1dbdf04d0068ca341860334af83740f1314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package MyFirstPython/1.4.0/MyFirstPython-1.4.0.zip

---
+++ packages/MyFirstPython/MyFirstPython-1.4.0.zip/MyFirstPython-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'MyFirstPython',
+	version      = '1.4.0',
+	py_modules   = ['MyFirstPython'],
+	author       = 'Mr.WRX',
+	author_email = 'test@gmail.com',
+	url          = 'http://www.test.com',
+	description  = 'This is my first python program,and I make some new change to improve it.',
+)
