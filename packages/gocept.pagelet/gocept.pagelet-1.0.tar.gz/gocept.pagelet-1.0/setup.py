63dd5bb426e304a4402e1dfdd219bc375fe94579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package gocept.pagelet/1.0/gocept.pagelet-1.0.tar.gz

---
+++ packages/gocept.pagelet/gocept.pagelet-1.0.tar.gz/gocept.pagelet-1.0/setup.py
@@ -0,0 +1,59 @@
+import os.path
+from setuptools import setup, find_packages
+
+
+def read(rel_path):
+    rel_path = rel_path.split('/')
+    base_path = os.path.dirname(__file__)
+    path = (base_path,) + tuple(rel_path)
+    return file(os.path.join(*path)).read()
+
+setup(
+    name='gocept.pagelet',
+    version='1.0',
+    author="Christian Zagrodnick",
+    author_email="mail@gocept.com",
+    description="Easier z3c.pagelet handling",
+    long_description='\n\n'.join([
+        read('README.rst'),
+        read('COPYRIGHT.txt'),
+        read('CHANGES.rst'),
+        read('src/gocept/pagelet/README.rst')]),
+    license="ZPL 2.1",
+    url='https://bitbucket.org/gocept/gocept.pagelet',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='easy z3c.pagelet zope3 pagelet zope',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'zope.publisher',
+        'zope.viewlet',
+        'z3c.template',
+        'z3c.pagelet',
+        'zope.browsermenu',
+        'zope.browserpage',
+    ],
+    extras_require=dict(
+        test=['zope.testing'])
+)
