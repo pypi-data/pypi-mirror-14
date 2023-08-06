0182258a4937d726807840508533c76a73d3a776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:24 +0000

    Add package asdl/0.1.4/asdl-0.1.4.tar.gz

---
+++ packages/asdl/asdl-0.1.4.tar.gz/asdl-0.1.4/README.rst
@@ -0,0 +1,51 @@
+python-asdl
+=================
+
+.. image:: https://travis-ci.org/fpoli/python-asdl.svg?branch=master
+    :target: https://travis-ci.org/fpoli/python-asdl
+
+A copy of the ASDL parser used in CPython 3.5, cleaned a bit and packaged here.
+
+
+Installation
+------------
+
+.. code-block:: bash
+
+    $ pip install asdl
+
+
+Usage
+-----
+
+.. code-block:: python
+
+    from asdl import ASDLParser, parse
+
+    syntax = """
+        module Lambda {
+            term =
+                Lambda(name x, term body) |
+                Apply(term function, term argument) |
+                Variable(name x)
+        }
+    """
+
+    print(ASDLParser().parse(syntax))
+
+    print(asdl.parse("./test/Python.asdl"))
+
+Output:
+
+.. code-block::
+
+    Module(Lambda, [Type(term, Sum([Constructor(Lambda, [Field(name, x), Field(term, body)]), Constructor(Apply, [Field(term, function), Field(term, argument)]), Constructor(Variable, [Field(name, x)])]))])
+
+    Module(Python, [Type(mod, Sum([Constructor(Module, [Field(stmt, body, seq=True)]), Constructor(Interactive, [Field(stmt, body, seq=True)]), Constructor(Expression, [Field(expr, body)]), Constructor(Suite, ...
+
+
+License
+-------
+
+Same as CPython: Python Software Foundation License (LICENSE file included
+here).
---
+++ packages/asdl/asdl-0.1.4.tar.gz/asdl-0.1.4/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+version = "0.1.4"
+
+description = "A copy of the ASDL parser used in CPython 3.5."
+
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_descr = f.read()
+
+setup(
+    name = "asdl",
+    version = version,
+
+    description = description,
+    long_description = long_descr,
+    license = "PSFL",
+    url = "https://github.com/fpoli/python-asdl",
+
+    author = "Federico Poli",
+    author_email = "federpoli@gmail.com",
+
+    py_modules = ["asdl"],
+
+    extras_require = {
+        "dev": [
+            "twine",
+            "nose",
+            "pep8 == 1.4.6"
+        ]
+    },
+
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Compilers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
