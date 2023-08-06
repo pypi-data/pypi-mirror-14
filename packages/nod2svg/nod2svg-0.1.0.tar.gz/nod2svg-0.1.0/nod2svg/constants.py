dbc73c149e61e95fb87b52334514a2d2a12e341e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:08 +0000

    Add package nod2svg/0.1.0/nod2svg-0.1.0.tar.gz

---
+++ packages/nod2svg/nod2svg-0.1.0.tar.gz/nod2svg-0.1.0/nod2svg/constants.py
@@ -0,0 +1,30 @@
+""":mod:`nod2svg.constants` --- Containing all string constants
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+NODE = 'Node'
+NOTE = 'Note'
+AUTHOR = 'Author'
+CITYBLOCK = 'CityBlock'
+CITYBLOCKFLIPPED = CITYBLOCK + 'Flipped'
+COLOR = 'Color'
+COMMENT = 'Comment'
+DIRECT = 'Direct'
+DONT_PLAY_NOTE = 'DontPlay' + NOTE
+EDGE = 'Edge'
+ELEMENTS = 'Elements'
+FROM_NODE = 'From' + NODE
+PATH = 'Path'
+STYLE = 'Style'
+STYLE_ANNOTATION_COLOR = STYLE + 'Annotation' + COLOR
+STYLE_BACKGROUND_COLOR = STYLE + 'Background' + COLOR
+TEXT = 'Text'
+TEXTBOX = TEXT + 'Box'
+TICKPOS = 'TickPos'
+TITLE = 'Title'
+TO_NODE = 'To' + NODE
+TYPE = 'Type'
+WORMHOLE = 'IsWormhole'
+X = 'X'
+Y = 'Y'
+GRID_TICK = 166320
+STRING_FLOAT_FORMAT='{0:.2f}'
