1f393524c6682a93aed8b472deec779df1f6deb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:26 +0000

    Add package ppmessage-mqtt/1.0.7/ppmessage-mqtt-1.0.7.tar.gz

---
+++ packages/ppmessage-mqtt/ppmessage-mqtt-1.0.7.tar.gz/ppmessage-mqtt-1.0.7/ppmessage_mqtt/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010-2016 YVertical, PPMessage
+# Ding Guijin, guijin.ding@ppmessage.com
+# Ning Ben, ben.ning@yvertical.com
+#
+# All rights reserved
+#
+
+from .ppmt import ppmt_main
+
+def mqtt_server():
+    ppmt_main()
+    return
+
+__version__ = "1.0.4"
