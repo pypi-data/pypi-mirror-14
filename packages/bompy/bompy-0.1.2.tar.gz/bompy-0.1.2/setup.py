1b2edabe1fc3ee991d54de49d350e4fa4d7e0475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:53 +0000

    Add package bompy/0.1.2/bompy-0.1.2.tar.gz

---
+++ packages/bompy/bompy-0.1.2.tar.gz/bompy-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bompy',
+      version='0.1.2',
+      py_modules=['bompy'],
+      description='A very light wrapper around the www.bom.gov.au weather data api.',
+      url='https://github.com/adamjoshuagray/bompy',
+      author='Adam J. Gray',
+      author_email='adam.joshua.gray@gmail.com',
+      license='MIT',
+      install_requires=['pandas'],
+      zip_safe=False)
