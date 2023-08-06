454079fb4e52a31669b2de8b7f1b32035d9de236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package QVScriptTools/0.123.dev0/QVScriptTools-0.123.dev0.zip

---
+++ packages/QVScriptTools/QVScriptTools-0.123.dev0.zip/QVScriptTools-0.123.dev0/tests/test_text.py
@@ -0,0 +1,29 @@
+import unittest, re, os
+from qvstools.text import *
+
+class TestBlock(unittest.TestCase):
+	def setUp(self):
+		print ("SETUP!")
+
+	def tearDown(self):
+		print ("TEAR DOWN!")
+
+	def test_detect_encoding(self):
+		paths = [
+			r"testdata\ETLTubeData_Subbified.qvw.log",
+			r"testdata\Subbify_TestInput.qvs",
+			r"testdata\ETLTubeData.qvs",
+			r"testdata\TestPRJ-prj\LoadScript.txt",
+			r"testdata\TestPRJ-prj\SH01.xml"
+			]
+
+		for path in paths:
+			result = detect_encoding(path)
+			#print(result)
+			with open(path,'r',encoding=result) as f:
+				#print(f.read(50))
+				pass
+
+if __name__ == '__main__':
+        unittest.main()
+
