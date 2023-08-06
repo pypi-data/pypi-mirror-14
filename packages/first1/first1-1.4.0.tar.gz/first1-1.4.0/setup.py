779c114a48c2855f49a570e93e00be8f2556c9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:05 +0000

    Add package first1/1.4.0/first1-1.4.0.tar.gz

---
+++ packages/first1/first1-1.4.0.tar.gz/first1-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+       name = 'first1',
+       version = '1.4.0',
+       py_modules = ['first1'],
+       author = 'free_share',
+       author_email = 'free_share@gmail.com',
+       description = 'A simple printer of nested lists', 
+     )
