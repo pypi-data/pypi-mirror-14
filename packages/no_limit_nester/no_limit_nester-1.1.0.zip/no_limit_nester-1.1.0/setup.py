66b136993a040bb91febc9baa736503ea29b955e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:36 +0000

    Add package no_limit_nester/1.1.0/no_limit_nester-1.1.0.zip

---
+++ packages/no_limit_nester/no_limit_nester-1.1.0.zip/no_limit_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='no_limit_nester',
+	version='1.1.0',
+	py_modules= ['no_limit_nester'],
+	author='ftayyab',
+	author_email='faizantayyab@yahoo.com',
+	url='none',
+	description='A simple printer for nested lists',
+	)
+
