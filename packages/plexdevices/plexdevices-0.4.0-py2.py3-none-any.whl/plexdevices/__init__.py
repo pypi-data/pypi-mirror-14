79f421c48b0f440978cf94c32b3dd15c756fe9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:09 +0000

    Add package plexdevices/0.4.0/plexdevices-0.4.0-py2.py3-none-any.whl

---
+++ packages/plexdevices/plexdevices-0.4.0-py2.py3-none-any.whl/plexdevices/__init__.py
@@ -0,0 +1,17 @@
+__title__ = 'plexdevices'
+__version__ = '0.4.0'
+__author__ = 'Cory Parsons'
+
+from plexdevices.exceptions import DeviceConnectionsError, PlexTVError, RemoteCallbackError
+from plexdevices.api import create_session, create_remote
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
