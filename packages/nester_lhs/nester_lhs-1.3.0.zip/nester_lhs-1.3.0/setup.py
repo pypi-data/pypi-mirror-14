06edb52ca807cbfeca746c0553561f51bbe80df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:17 +0000

    Add package nester_lhs/1.3.0/nester_lhs-1.3.0.zip

---
+++ packages/nester_lhs/nester_lhs-1.3.0.zip/nester_lhs-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester_lhs',
+        version      = '1.3.0',
+        py_module    = ['nester_lhs'],
+        author       = 'hseokool',
+        author_email = 'stone043@nate.com',
+        url          = 'nate.com',
+        description  = 'A simple printer of nested lists',
+)
