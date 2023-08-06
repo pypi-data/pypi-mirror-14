4fe79780df9d42ed8f7accc1bce78319cb80bc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package epubscraper/1.0/epubscraper-1.0.tar.gz

---
+++ packages/epubscraper/epubscraper-1.0.tar.gz/epubscraper-1.0/epubscraper.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import xmltodict
+import re
+import json
+
+"""
+Book subject and types data
+"""
+
+typelib = ["উপন্যাস", "বড়গল্প", "ছোটগল্প", "কবিতা", "প্রবন্ধ", "নাটক", "গান", "সমগ্র"]
+
+sublib = {"Absurd": "স্বপ্নবাস্তবতা", "Action": "দুঃসাহসিক", "Adventure": "দুঃসাহসিক", "Autobiography": "আত্নজীবনী", "Biography": "জীবনী", "Children's literature": "শিশুসাহিত্য", "Classic": "চিরায়ত সাহিত্য", "Collection": "সমগ্র", "Comedy": "রম্যরচনা", "Crime": "অপরাধ", "Detective": "গোয়েন্দা", "Drama": "নাটক", "Essay": "প্রবন্ধ", "Fable": "রূপকথা", "Fairy tale": "রূপকথা", "Fanciful": "স্বপ্নবাস্তবতা", "Fantasy": "রূপকথা", "Folklore": "লোকসাহিত্য", "Historical fiction": "ঐতিহাসিক উপন্যাস", "History": "ইতিহাস", "Horror": "ভৌতিক", "Humour": "রস", "Legend": "উপ্যাখান", "Magical realism": "জাদুবাস্তবতা", "Memoir": "স্মৃতিচারণ", "Mystery": "রহস্য", "Mythology": "পুরাণ", "Novel": "উপন্যাস", "Novella": "বড়গল্প", "Philosophy": "দর্শন", "Play": "নাটক", "Poem": "কবিতা", "Politics": "রাজনীতি", "Realistic fiction": "বাস্তবিক", "Religion": "ধর্ম", "Romance": "প্রেম", "Satire": "ব্যাঙ্গরচনা", "Science": "বিজ্ঞান", "Science fiction": "কল্পবিজ্ঞান", "Song": "গান", "Speech": "ভাষণ", "Story": "ছোটগল্প", "Short story": "ছোটগল্প", "Surreal": "স্বপ্নবাস্তবতা", "Travel": "ভ্রমণকাহিনী"}
+
+def namef (string):
+    return string.split('/')[-1]
+
+def imscrap (booklink, imagedir, imagename, imagelink, overwrite):
+    """
+    Scrap Cover image from epub.
+    """
+    target = os.path.join(imagedir, imagename)
+    temporary = os.path.join(imagedir, namef(imagelink))
+    if overwrite == True:
+        try:
+            os.remove(target)
+        except:
+            pass
+    if os.path.isfile(target) == True:
+        return "/images/" + imagename
+    else:
+        subprocess.call(["unzip", "-j", booklink, imagelink, "-d", imagedir])
+        subprocess.call(["convert", "-resize", "225x318", temporary, target])
+        os.remove(temporary)
+        return "/images/" + imagename
+    
+def link_parser(dstring):
+    """
+    Github repository link perser
+    """
+    data = dstring.split('(http')
+    if len(data) == 2:
+        return [data[0].strip(), 'http' + data[1].split(')')[0].strip()]
+    else:
+        return [data[0].strip(), ""]
+
+def book_keeper (booklink):
+    """
+    Book cataloger based on metadata
+    """
+    meta = xmltodict.parse(subprocess.check_output(["unzip", "-p", booklink, "OEBPS/content.opf"]))['package']['metadata']
+    catalog = {'author': [], 'publisher': [], 'translator': [], 'editor': [], 'illustrator': [], 'subject': []}
+    for key, val in meta.items():
+        if key == 'dc:title':
+            catalog['title'] = val
+            print (catalog['title'])
+        elif key == 'dc:creator':
+            if isinstance(val, list):
+                for v in val:
+                    if v['@opf:role'] == 'aut':
+                        catalog['author'] == catalog['author'].append(link_parser(v['#text']))
+            else:
+                if val['@opf:role'] == 'aut':
+                    catalog['author'] == catalog['author'].append(link_parser(val['#text']))
+        elif key == 'dc:language':
+            catalog['language'] = val
+        elif key == 'dc:description':
+            catalog['description'] = val
+        elif key == 'dc:publisher':
+            if isinstance(val, list):
+                for v in val:
+                    catalog['publisher'] == catalog['publisher'].append(link_parser(v))
+            else:
+                catalog['publisher'] == catalog['publisher'].append(link_parser(val))
+        elif key == 'dc:type' or key == 'dc:subject':
+            try:
+                if isinstance(val, list):
+                    for v in val:
+                        catalog['subject'] == catalog['subject'].append(sublib[v.capitalize()])
+                else:
+                    catalog['subject'] == catalog['subject'].append(sublib[val.capitalize()])
+            except:
+                pass
+        elif key == 'dc:contributor':
+            if isinstance(val, list):
+                for v in val:
+                    role = v['@opf:role']
+                    if role == 'trl':
+                        catalog['translator'] == catalog['translator'].append(link_parser(v['#text']))
+                    elif role == 'edt':
+                        catalog['editor'] == catalog['editor'].append(link_parser(v['#text']))
+                    elif role == 'ill':
+                        catalog['illustrator'] == catalog['illustrator'].append(link_parser(v['#text']))
+            else:        
+                role = val['@opf:role']
+                if role == 'trl':
+                    catalog['translator'] == catalog['translator'].append(link_parser(val['#text']))
+                elif role == 'edt':
+                    catalog['editor'] == catalog['editor'].append(link_parser(val['#text']))
+                elif role == 'ill':
+                    catalog['illustrator'] == catalog['illustrator'].append(link_parser(val['#text']))
+        elif key == 'dc:date':
+            if isinstance(val, list):
+                for v in val:
+                    event = v['@opf:event']
+                    if event == 'publication' or event == 'creation':
+                        catalog['pubdate'] = v['#text']
+                    elif event == 'modification':
+                        catalog['moddate'] = v['#text']
+            else:
+                event = val['@opf:event']
+                if event == 'publication' or event == 'creation':
+                    catalog['pubdate'] = val['#text']
+                elif event == 'modification':
+                    catalog['moddate'] = val['#text']
+        elif key == 'meta':
+            for v in val:
+                if v['@name'] == 'cover':
+                    catalog['cover'] = "OEBPS/Images/" + v['@content']
+    subject = catalog['subject']
+    precount = len(subject)
+    for s in subject:
+        if s in typelib:
+            catalog['type'] = s
+            catalog['subject'].remove(s)
+    if len(catalog['subject']) == precount:
+        catalog['type'] = 'অন্যান্য'
+    return catalog
+
+def file_linker(path, link, files, imagedir, overwrite):
+    """
+    Add file and image links.
+    """
+    out = []
+    for f in files:
+        meta = book_keeper(f)
+        filepath = re.sub(path, '', f)
+        imagelink = meta['cover']
+        imgname = re.sub('-+', "-", re.sub('[_/ ]+', "-", meta['title'])) + "-" + re.sub(' +', "-", meta['type']) + "-" + re.sub(' +', "-", meta['author'][0][0]) + "." + namef(imagelink).split('.')[-1]
+        image = imscrap(f, imagedir, imgname, imagelink, overwrite)
+        meta['cover'] = image
+        meta['link'] = link + "/raw/master" + filepath
+        out.append(meta)
+    return out
+
+def mapmaker(path, outdir, overwrite):
+    """
+    Get data of a whole repo.
+    """
+    imagedir = os.path.join(outdir, "images")
+    authorfile = os.path.join(outdir, "author")
+    metadata = subprocess.check_output(["git", "remote", "-v"], cwd=path).decode("utf-8").split("\n")[0].split("\t")[1][0:-12]
+    if "https" in metadata:
+        link = metadata
+    else:
+        link = metadata.replace('git@github.com:', 'https://github.com/')
+    dfiles = []
+    for p, subdirs, files in os.walk(path):
+        for f in [f for f in files if f.endswith(".epub")]:
+            dfiles.append(os.path.join(p, f))
+    return file_linker(path, link, dfiles, imagedir, overwrite)
+
+def authmaker(nestedvec, blogdir, name):
+    items = []
+    for author in nestedvec:
+        dname = author[0].replace(" ", "-")
+        page = os.path.join(blogdir, "_authors/", dname) + ".markdown"
+        if os.path.isfile(page):
+            items.append(" - " + name + ": \"" + author[0] + "\"\n   link: \"https://eboipotro.github.io/author/" + dname + "/\"")
+        else:
+            items.append(" - " + name + ": \"" + author[0] + "\"\n   link: \"" + author[-1] + "\"")
+    return "\n".join(items)
+
+def arraymaker(nestedvec, name=False):
+    if name == False:
+        dvec = []
+        for i in nestedvec:
+            dvec.append(i[0])
+        return "\"" + '\", \"'.join(dvec) + "\""
+    else:
+        dvec = []
+        for i in nestedvec:
+            dvec.append(" - " + name + ": \"" + i[0] + "\"\n   link: \"" + i[-1] + "\"")
+        return "\n".join(dvec)
+    
+def postmaker(dmap, blogdir):
+    """
+    Book page data creator
+    """
+    try:
+        moddate = dmap['moddate']
+    except:
+        moddate = dmap['pubdate']
+    try:
+        pubdate = dmap['pubdate']
+    except:
+        pubdate = dmap['moddate']
+    mname = re.sub('-+', "-", re.sub('[_/ ]+', "-", dmap['title'])) + "-" + re.sub(' +', "-", dmap['type']) + "-" + re.sub(' +', "-", dmap['author'][0][0])
+    filename = moddate + "-" + mname + ".markdown"
+    permalink = "/library/" + mname + "/"
+    categories = dmap['subject'] + [dmap['type']]
+    out = [
+        filename,
+        "---",
+        "layout: post",
+        "published: true",
+        "title: " + "\"" + dmap['title'] + "\"",
+        "tags: [" + arraymaker(dmap['author'] + dmap['translator']) + "]",
+        "categories: [\"" +  "\", \"".join(categories) + "\"]",
+        "img: \"" + dmap['cover'] + "\"",
+        "type: \"" + dmap['type'] + "\""]
+
+    if len(dmap['subject']) > 0:
+        out.append("subject: [\"" + "\", \"".join(dmap['subject']) + "\"]")
+    out = out + [
+        "dlink: \"" + dmap['link'] + "\"",
+        "lang: \"" + dmap['language'] + "\"",
+        "publishers: \n" + arraymaker(dmap['publisher'], "publisher"),
+        "moddate: \"" + moddate + "\"",
+        "pubdate: \"" + pubdate + "\"",
+        "comments: true",
+        "authors: \n" + authmaker(dmap['author'], blogdir, "author")]
+    if len(dmap['translator']) > 0:
+        out.append("translators: \n" + authmaker(dmap['translator'], blogdir, "translator"))
+    if len(dmap['illustrator']) > 0:
+        out.append("illustrators: \n" + arraymaker(dmap['translator'], "editor"))
+    if len(dmap['editor']) > 0:
+        out.append("editors: \n" + arraymaker(dmap['editor'], "editor"))
+    try:
+        out.append("description: \"" + dmap['description'] + "\"")
+    except:
+        pass
+    out.append("permalink: \"" + permalink + "\"")
+    out.append("---")
+    return out
+
+def printer(outdir, blogdir, dmap):
+    """
+    Book page creator
+    """
+    data = postmaker(dmap, blogdir)
+    dname = data[0][11:]
+    for f in os.listdir(outdir):
+        if re.search(dname, f):
+            os.remove(os.path.join(outdir, f))
+    with open(os.path.join(outdir, data[0]), 'w') as dfile:
+        for line in data[1:]:
+            dfile.write(line + "\n")
+
+def autgen(path, blogdir):
+    """
+    Author page creator
+    """
+    try:
+        with open(os.path.join(path, "author")) as dfile:
+            autdata = json.load(dfile)
+            for aut in autdata:
+                with open(os.path.join(blogdir, "_authors", aut['name'].replace(" ", "-")) + ".markdown", 'w') as autfile:
+                    autfile.truncate()
+                    dstr = "---\nlayout: author\ntitle: \"" + aut['name'] + "\"\nautlink: \"" + aut['link'] + "\"\npermalink: /author/" + aut['name'].replace(" ", "-") + "/\n"
+                    try:
+                        dstr = dstr + "img: \"" + aut['image'] + "\"\n"
+                    except:
+                        pass
+                    dstr = dstr + "---\n" + aut['description']
+                    autfile.write(dstr)
+    except:
+        pass
+
+
+def postgen(path, blogdir, overwrite=False):
+    """
+    Viola! Do everything!
+    """
+    autgen(path, blogdir)
+    data = mapmaker(path, blogdir, overwrite)
+    postdir = os.path.join(blogdir, "_posts")
+    for d in data:
+        printer(postdir, blogdir, d)
