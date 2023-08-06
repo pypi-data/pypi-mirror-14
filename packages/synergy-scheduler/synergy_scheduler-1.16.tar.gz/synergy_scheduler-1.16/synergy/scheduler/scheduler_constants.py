cb5cce67c928396bb2f6516e5c8a0a00d9bf185d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:03 +0000

    Add package synergy-scheduler/1.16/synergy_scheduler-1.16.tar.gz

---
+++ packages/synergy-scheduler/synergy_scheduler-1.16.tar.gz/synergy_scheduler-1.16/synergy/scheduler/scheduler_constants.py
@@ -0,0 +1,45 @@
+# Scheduler constants
+LAG_5_MINUTES = 5  # lag in minutes between finish of the timeperiod and beginning of its processing
+
+PROCESS_SCHEDULER = 'Scheduler'
+PROCESS_GC = 'GarbageCollector'
+PROCESS_MX = 'MX'
+
+TOKEN_SCHEDULER = 'scheduler'
+TOKEN_GC = 'gc'
+TOKEN_WERKZEUG = 'werkzeug'
+
+# State Machine names
+STATE_MACHINE_RECOMPUTING = 'recomputing'
+STATE_MACHINE_CONTINUOUS = 'continuous'
+STATE_MACHINE_DISCRETE = 'discrete'
+STATE_MACHINE_FREERUN = 'freerun'
+
+# dependency allows processing of the dependent timeperiod,
+# however finalization of the dependent timeperiod is not allowed until all blocking timeperiods are processed
+BLOCKING_NORMAL = 'blocking_normal'
+
+# any processing of dependent timeperiods is blocked until blocking timeperiods are processed.
+BLOCKING_DEPENDENCIES = 'blocking_dependencies'
+
+# any processing of higher time granularity is blocked until all children timeperiods are processed
+BLOCKING_CHILDREN = 'blocking_children'
+
+# MQ exchanges that are required by Synergy Scheduler
+EXCHANGE_MANAGED_WORKER = 'ex_managed_worker'
+EXCHANGE_FREERUN_WORKER = 'ex_freerun_worker'
+EXCHANGE_UTILS = 'ex_utils'
+
+# DB Collection/Table names
+COLLECTION_MANAGED_PROCESS = 'managed_process'
+COLLECTION_FREERUN_PROCESS = 'freerun_process'
+COLLECTION_UNIT_OF_WORK = 'unit_of_work'
+COLLECTION_UOW_LOG = 'uow_log'
+
+COLLECTION_JOB_HOURLY = 'job_hourly'
+COLLECTION_JOB_DAILY = 'job_daily'
+COLLECTION_JOB_MONTHLY = 'job_monthly'
+COLLECTION_JOB_YEARLY = 'job_yearly'
+
+QUEUE_UOW_STATUS = 'q_uow_status'
+QUEUE_JOB_STATUS = 'q_job_status'
