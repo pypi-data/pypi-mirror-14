963480c1ef549d93ae3f367cb39bd5cff89c961e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:56 +0000

    Add package fish_base/1.0.5/fish_base-1.0.5.tar.gz

---
+++ packages/fish_base/fish_base-1.0.5.tar.gz/fish_base-1.0.5/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='fish_base',
+    version='1.0.5',
+    packages=['fish_base'],
+    url='https://github.com/chinapnr/fish_base',
+    license='',
+    author='david.yi',
+    author_email='wingfish@gmail.com',
+    description='some useful functions for python',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+
+)
