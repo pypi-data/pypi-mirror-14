c33163886a07ea3e50d7920c8e2a7be0e516dc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package piscan/0.2.6/piscan-0.2.6.tar.gz

---
+++ packages/piscan/piscan-0.2.6.tar.gz/piscan-0.2.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='piscan',
+      version='0.2.6',
+      description='RXWave.com Uniden DMA Police Scanner agent',
+      keywords='RXwave Police-scanner Uniden DMA RXWave.com RX-Wave.com radio scanner',
+      url='http://www.rxwave.com',
+      author='Dave Kolberg',
+      author_email='dave@netagile.com',
+      license='MIT',
+      packages=['piscan'],
+      install_requires=['pika','boto','flask'],
+      zip_safe=False,
+      include_package_data=True,
+      package_dir={'piscan': 'piscan'},
+      package_data={
+        'data' : ['*'],
+        'frontend' : ['*'],
+      },
+      scripts=['bin/piscan-setup.sh'],
+)
