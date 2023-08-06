04df65039a5a8afd4558e8bcadc4c395b834a96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:35 +0000

    Add package rexutil/1.0.5/rexutil-1.0.5.tar.gz

---
+++ packages/rexutil/rexutil-1.0.5.tar.gz/rexutil-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md') as fhandle:
+    long_description = fhandle.read()
+
+
+setup(
+    name='rexutil',
+    version='1.0.5',
+    description='A python module for regular expression manipulations.',
+    long_description=long_description,
+    url="https://github.com/bdastur/rex",
+    author="Behzad Dastur",
+    author_email="bdastur@gmail.com",
+    license='Apache Software License',
+    classifier=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'License :: Apache Software License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+    ],
+
+    keywords='re regular expression utility matching development',
+    py_modules=['rex']
+)
+
