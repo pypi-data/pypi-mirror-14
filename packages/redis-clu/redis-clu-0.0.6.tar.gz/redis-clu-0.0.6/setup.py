a94cf5c6ce00ef356736162ed383af4d02ec1198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:01 +0000

    Add package redis-clu/0.0.6/redis-clu-0.0.6.tar.gz

---
+++ packages/redis-clu/redis-clu-0.0.6.tar.gz/redis-clu-0.0.6/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+version = '0.0.6'
+
+setup(
+    name='redis-clu',
+    version=version,
+    description='Redis Cluster Management Tool',
+    long_description=README,
+    url='https://github.com/baranbartu/redis-clu',
+    download_url='https://github.com/baranbartu/redis-clu/tarball/%s' % version,
+    author='Baran Bartu Demirci',
+    author_email='bbartu.demirci@gmail.com',
+    license='MIT',
+    keywords='redis cluster management replication master slave',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['redis-clu = redisclu.cli:main']
+    },
+    install_requires=[
+        'hiredis',
+        'redis',
+        'futures==3.0.3',
+    ]
+)
