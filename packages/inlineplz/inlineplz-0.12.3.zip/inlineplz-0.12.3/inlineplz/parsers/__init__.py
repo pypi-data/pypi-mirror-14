edec69bb6e0ae5dbaf98e4805f02d27e2a46b98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:39 +0000

    Add package inlineplz/0.12.3/inlineplz-0.12.3.zip

---
+++ packages/inlineplz/inlineplz-0.12.3.zip/inlineplz-0.12.3/inlineplz/parsers/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from inlineplz.parsers.prospector import ProspectorParser
+from inlineplz.parsers.eslint import ESLintParser
+from inlineplz.parsers.jscs import JSCSParser
+from inlineplz.parsers.jshint import JSHintParser
+from inlineplz.parsers.jsonlint import JSONLintParser
+from inlineplz.parsers.yamllint import YAMLLintParser
+from inlineplz.parsers.rstlint import RSTLintParser
+from inlineplz.parsers.markdownlint import MarkdownLintParser
+from inlineplz.parsers.stylint import StylintParser
+from inlineplz.parsers.rflint import RobotFrameworkLintParser
+
+
+PARSERS = {
+    'prospector': ProspectorParser,
+    'eslint': ESLintParser,
+    'jshint': JSHintParser,
+    'jscs': JSCSParser,
+    'jsonlint': JSONLintParser,
+    'yamllint': YAMLLintParser,
+    'rstlint': RSTLintParser,
+    'markdownlint': MarkdownLintParser,
+    'stylint': StylintParser,
+    'rflint': RobotFrameworkLintParser
+}
