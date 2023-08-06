4ecaa226be989459e19cb0c3816f92f4b7cf57aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:20 +0000

    Add package rednose/1.1.0/rednose-1.1.0.tar.gz

---
+++ packages/rednose/rednose-1.1.0.tar.gz/rednose-1.1.0/setup.py
@@ -0,0 +1,32 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(path.join(path.dirname(__file__), fname)).read()
+    except IOError:
+        return """A module for making nose pretty using colors."""
+
+setup(
+    packages=find_packages(exclude=['test', 'test.*']),
+    description='coloured output for nosetests',
+    entry_points={'nose.plugins.0.10': ['NOSETESTS_PLUGINS = rednose:RedNose']},
+    install_requires=['setuptools', 'python-termstyle >=0.1.7', 'colorama'],
+    long_description=read('README.rst'),
+    name='rednose',
+    py_modules=['rednose'],
+    url='https://github.com/JBKahn/rednose',
+    version='1.1.0',
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+    ],
+    keywords='test nosetests nose nosetest output colour console',
+    license='BSD',
+)
