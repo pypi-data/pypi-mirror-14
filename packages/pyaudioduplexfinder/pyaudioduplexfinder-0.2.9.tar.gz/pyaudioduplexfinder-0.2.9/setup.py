debbf65fb3081c65c68c8bad0ed940aa745a2483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:00 +0000

    Add package pyaudioduplexfinder/0.2.9/pyaudioduplexfinder-0.2.9.tar.gz

---
+++ packages/pyaudioduplexfinder/pyaudioduplexfinder-0.2.9.tar.gz/pyaudioduplexfinder-0.2.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyaudioduplexfinder',
+    version='0.2.9',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://pyaudioduplexfinder.b1t.uk',
+    license='MIT',
+    author='Oskar Malnowicz',
+    author_email='8@b1t.uk',
+    description='Find duplicate in audio files',
+    py_modules=['pyauduxfind'],
+    install_requires=[
+        'Click',
+        'colorama',
+        'pyyaml'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyaudioduplexfinder=pyaudioduplexfinder.pyauduxfind:cli',
+        ],
+    }
+)
