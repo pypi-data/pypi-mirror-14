c7ad856cb18aa8b1036cb0e4fc1eb2a1ccec1eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:01 +0000

    Add package dockbot/0.1.1/dockbot-0.1.1.tar.gz

---
+++ packages/dockbot/dockbot-0.1.1.tar.gz/dockbot-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dockbot',
+    version = '0.1.1',
+    description =
+    'A continuous integration system which uses Docker and Buildbot',
+    long_description = open('README.md', 'rt').read(),
+    author = 'Joseph Coffland',
+    author_email = 'joseph@cauldrondevelopment.com',
+    platforms = ['any'],
+    license = 'GPL 3+',
+    url = 'https://github.com/CauldronDevelopmentLLC/dockbot',
+    packages = ['dockbot'],
+    include_package_data = True,
+    eager_resources = ['data/*'],
+    entry_points = {
+        'console_scripts': [
+            'dockbot = dockbot:run',
+            'dockbot-publish = dockbot.publish:run',
+            'github-release = dockbot.github:run',
+            ]
+        }
+    )
