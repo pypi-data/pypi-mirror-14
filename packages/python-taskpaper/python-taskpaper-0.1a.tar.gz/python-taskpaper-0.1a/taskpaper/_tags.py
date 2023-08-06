20fe3148159f282769c7da0366f9547c6af61b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package python-taskpaper/0.1a/python-taskpaper-0.1a.tar.gz

---
+++ packages/python-taskpaper/python-taskpaper-0.1a.tar.gz/python-taskpaper-0.1a/taskpaper/_tags.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+This file contains code for handling tags in TaskPaper items.
+"""
+
+import collections
+
+import re
+
+try:
+    from collections.abc import MutableSequence
+except ImportError:  # pragma: no cover
+    from collections import MutableSequence
+
+
+TaskPaperTag = collections.namedtuple('TaskPaperTag', 'name value')
+
+
+# Regex for matching tags. http://guide.taskpaper.com/getting_started.html:
+#
+#  > To create a tag, type the @ symbol followed by a name. Tags can
+#  > optionally have a value in parentheses after the tag name:
+#  >   @priority(1)
+#
+# This regex is adapted from the birch.js variable tagRegexString.
+
+TAG_REGEX = re.compile(
+    r'(?:^|\s+)'                    # word boundary
+    r'@'                            # literal @ character
+    r'(?P<name>'                    # capturing group for tag name
+    r'(?:[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D'
+        r'\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF'
+        r'\uF900-\uFDCF\uFDF0-\uFFFD]|'
+                                    # first char of the name.
+                                    # taken directly from birch.js
+    r'[\-.0-9\u00B7\u0300-\u036F\u203F-\u2040])*)'
+                                    # other chars of the name/
+                                    # taken directly from birch.js
+    r'(?:\((?P<value>(?:\\\)|[^\)])*)\))?'
+                                    # tag value
+                                    # adapted from birch.js
+    r'(?=\s|$)',                  # another word boundary or end-of-line
+    flags=re.IGNORECASE
+)
+
+
+# Defining a collection of tags as an ABC is arguably overkill.  All I really
+# want is the ability to search the list by name only, e.g. given the item
+#
+#    - Pick up dry cleaning @hello(world)
+#
+# I should be able to write `'hello' in item.tags`, and get True.
+#
+# This also pulls the work of coercing new tags to a TaskPaperTag object
+# out of the TaskPaperDocument class.
+
+class TagCollection(MutableSequence):
+    """
+    Collection of tags that lets you search for inclusion by tag name
+    or tag value.
+    """
+    def __init__(self):
+        self._tags = []
+
+    def __repr__(self):
+        return repr(self._tags)
+
+    def __str__(self):
+        return str(self._tags)
+
+    @staticmethod
+    def _coerce_value_to_tag(value):
+        if isinstance(value, tuple) and len(value) == 2:
+            return TaskPaperTag(value[0], value[1] or '')
+        elif isinstance(value, str):
+            return TaskPaperTag(value, '')
+        else:
+            raise ValueError("Cannot coerce %r to TaskPaperTag." % value)
+
+    def __len__(self):
+        return len(self._tags)
+
+    def __getitem__(self, position):
+        return self._tags[position]
+
+    def __setitem__(self, position, value):
+        # If the tag in question is a 'done' tag, it can only go at the
+        # end, and it has to replace any existing done tag.
+        tag = self._coerce_value_to_tag(value)
+
+        if tag.name == 'done':
+            del self._tags[position]
+            self._tags.insert(0, tag)
+        else:
+            self._tags[position] = tag
+
+        self._rearrange_for_done()
+
+    def __delitem__(self, position):
+        del self._tags[position]
+
+    def insert(self, position, value):
+        # If the tag in question is a 'done' tag, it can only go at the
+        # end, and it has to replace any existing done tag.
+        tag = self._coerce_value_to_tag(value)
+
+        if tag.name == 'done':
+            self._tags.insert(0, tag)
+        else:
+            self._tags.insert(position, tag)
+
+        self._rearrange_for_done()
+
+    def _rearrange_for_done(self):
+        """
+        The 'done' tag can only ever go at the end, and must be unique
+        in a list of tags.  Rearrange the tag list to ensure this is
+        the case.
+        """
+        try:
+            done_tag = [tag for tag in self._tags if tag.name == 'done'].pop(0)
+        except IndexError:
+            return
+        new_tags = [tag for tag in self._tags
+                        if tag.name != 'done'] + [done_tag]
+        self._tags = new_tags
+
+    def __contains__(self, value):
+        # If we get a 2-tuple, assume that they're searching for an exact
+        # tag, and search accordingly.
+        if isinstance(value, tuple) and len(value) == 2:
+            return value in self._tags
+
+        # If we get a name, assume that they're searching for a tag with
+        # a given name, but don't care about the value.
+        elif isinstance(value, str):
+            return any(tag.name == value for tag in self._tags)
+
+        # No other search terms are acceptable.
+        else:
+            return False
+
+    def __eq__(self, other):
+        if len(self) != len(other):
+            return False
+        else:
+            return all(i == j for i, j in zip(sorted(self), sorted(other)))
+
+    def __ne__(self, other):
+        return not (self == other)
---
+++ packages/python-taskpaper/python-taskpaper-0.1a.tar.gz/python-taskpaper-0.1a/taskpaper/items.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+This file contains the TaskPaperItem class, and the associated setup code.
+"""
+
+from datetime import datetime
+import re
+
+from .exceptions import TaskPaperError
+from ._tags import TAG_REGEX, TaskPaperTag, TagCollection
+
+
+class TaskPaperItem(object):
+
+    # Number of spaces per indentation level
+    tab_size = 4
+
+    def __init__(self, text):
+        # Strip trailing whitespace from the name.
+        self._text = text.rstrip()
+
+        self.tags = TagCollection()
+        self.body_text = self._text
+
+        self._parse()
+
+    def _parse(self):
+        # Separate the list of tags from the body text
+        matches = TAG_REGEX.finditer(self._text)
+        if matches is not None:
+            for match in matches:
+                self.tags.append(match.groups())
+
+        self.body_text = re.sub(TAG_REGEX, '', self.body_text).strip()
+
+    def __repr__(self):
+        return '%s(text=%r)' % (type(self).__name__, self._text)
+
+    def __str__(self):
+        components = [self.body_text]
+        for tag in self.tags:
+            if tag.value:
+                components.append('@{tag.name}({tag.value})'.format(tag=tag))
+            else:
+                components.append('@{tag.name}'.format(tag=tag))
+        return ' '.join(components)
+
+    def set_tag(self, name, value):
+        """
+        If there is already a tag with the given name, updates the value.
+        If there isn't, creates a new tag.
+        """
+        for idx, tag in enumerate(self.tags):
+            if tag.name == name:
+                self.tags[idx] = (name, value)
+                return
+        else:
+            self.add_tag(name, value)
+
+    def add_tag(self, name, value=None):
+        """
+        Add a new tag with the given name and value.  Unlike set_tag,
+        this won't overwrite an existing tag.
+        """
+        if value is None:
+            self.tags.append(name)
+        else:
+            self.tags.append((name, value))
+
+    def remove_tag(self, name, value=None):
+        """
+        Remove all tags with the given name and value.  If 'value' is None,
+        every tag with this name is removed.
+        """
+        for tag in self.tags:
+            if (tag.name == name) and (value is None or tag.value == value):
+                self.tags.remove(tag)
+
+    @property
+    def done(self):
+        """
+        Returns True if an item is marked as 'done'; that is, whether it
+        has the @done tag.
+        """
+        return 'done' in self.tags
+
+    @done.setter
+    def done(self, value):
+        if value is False:
+            self.mark_undone()
+        elif value is True:
+            self.mark_done()
+        else:
+            raise ValueError("Expected True/False, got %r" % value)
+
+    def done_date(self):
+        """
+        Returns the date on which an item was marked as done().
+        Throws a TaskNotDone exception if the task is not actually
+        completed.
+        """
+        try:
+            done_tag = [tag for tag in self.tags if tag.name == 'done'].pop()
+        except IndexError:
+            raise TaskPaperError("Trying to get done date of an unfinished task")
+        return done_tag.value
+
+    def mark_done(self):
+        """
+        Marks a task as done, with today's date.  Does not change the date
+        if the task is already done.
+        """
+        if 'done' not in self.tags:
+            self.set_tag(name='done', value=str(datetime.today().date()))
+
+    def mark_undone(self):
+        """
+        Removes the done tag from a task.  Does nothing if the task does not
+        already have this tag set.
+        """
+        self.remove_tag(name='done')
+
+    def toggle_done(self):
+        """
+        Toggles the 'done' status of a task.
+        """
+        if self.done:
+            self.mark_undone()
+        else:
+            self.mark_done()
