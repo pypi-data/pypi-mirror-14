212562f526cbbacdf7e8850463ed593623524cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:33 +0000

    Add package growler/0.7.1/growler-0.7.1.tar.gz

---
+++ packages/growler/growler-0.7.1.tar.gz/growler-0.7.1/growler/__meta__.py
@@ -0,0 +1,20 @@
+#
+# metadata.py
+#
+"""Project Metadata"""
+
+package = 'growler'
+project = 'Web framework using standard python asyncio'
+
+version_info = (0, 7, 1)
+version = '.'.join(map(str, version_info))
+version_name = ''.join((project, 'version', version))
+
+date = "Apr 18, 2016"
+author = "Andrew Kubera"
+author_email = "andrew.kubera@gmail.com"
+copyright = "Copyright 2016, Andrew Kubera"
+
+url = "https://github.com/pyGrowler/Growler"
+
+license = 'Apache v2.0'
