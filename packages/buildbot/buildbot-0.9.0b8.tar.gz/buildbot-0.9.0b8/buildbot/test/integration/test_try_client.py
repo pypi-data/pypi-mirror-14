e442119624ea9fdb8da386c6970e93e258e814df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package buildbot/0.9.0b8/buildbot-0.9.0b8.tar.gz

---
+++ packages/buildbot/buildbot-0.9.0b8.tar.gz/buildbot-0.9.0b8/buildbot/test/integration/test_try_client.py
@@ -0,0 +1,361 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+import os
+
+import mock
+
+from twisted.cred import credentials
+from twisted.internet import defer
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.spread import pb
+from twisted.trial import unittest
+
+from buildbot import config
+from buildbot import master
+from buildbot import util
+from buildbot.clients import tryclient
+from buildbot.schedulers import trysched
+from buildbot.test.util import dirs
+from buildbot.test.util import www
+
+
+# wait for some asynchronous result
+@defer.inlineCallbacks
+def waitFor(fn):
+    while True:
+        res = yield fn()
+        if res:
+            defer.returnValue(res)
+        yield util.asyncSleep(.01)
+
+
+class FakeRemoteWorker(pb.Referenceable):
+    # the bare minimum to connect to a master and convince it that the worker is
+    # ready
+
+    def __init__(self, port):
+        self.port = port
+
+    @defer.inlineCallbacks
+    def start(self):
+        f = pb.PBClientFactory()
+        d = f.login(credentials.UsernamePassword('local1', 'localpw'), self)
+        reactor.connectTCP('127.0.0.1', self.port, f)
+        # we need to hold a reference to this, otherwise the broker will sever
+        # the connection
+        self.mind = yield d
+
+    def stop(self):
+        self.mind.broker.transport.loseConnection()
+        self.mind = None
+
+    # Worker methods
+
+    def remote_print(self, message):
+        log.msg("from master: %s" % (message,))
+
+    def remote_getSlaveInfo(self):
+        return {}
+
+    def remote_getCommands(self):
+        return {}
+
+    def remote_getVersion(self):
+        return '0.0'
+
+    def remote_setMaster(self, master):
+        pass
+
+    def remote_setBuilderList(self, wanted):
+        return {'a': self}
+
+    def remote_startBuild(self):
+        return
+
+
+class Schedulers(dirs.DirsMixin, www.RequiresWwwMixin, unittest.TestCase):
+
+    def setUp(self):
+        global BuildmasterConfig
+        BuildmasterConfig = {}
+        self.basedir = os.path.abspath('basedir')
+        self.setUpDirs(self.basedir)
+
+        self.configfile = os.path.join(self.basedir, 'master.cfg')
+        open(self.configfile, "w").write(
+            'from buildbot.test.integration.test_try_client \\\n'
+            'import masterConfig\n'
+            'BuildmasterConfig = masterConfig()\n')
+
+        self.master = None
+        self.sch = None
+        self.worker = None
+
+        def spawnProcess(pp, executable, args, environ):
+            tmpfile = os.path.join(self.jobdir, 'tmp', 'testy')
+            newfile = os.path.join(self.jobdir, 'new', 'testy')
+            open(tmpfile, "w").write(pp.job)
+            os.rename(tmpfile, newfile)
+            log.msg("wrote jobfile %s" % newfile)
+            # get the scheduler to poll this directory now
+            d = self.sch.watcher.poll()
+            d.addErrback(log.err, 'while polling')
+
+            @d.addCallback
+            def finished(_):
+                st = mock.Mock()
+                st.value.signal = None
+                st.value.exitCode = 0
+                pp.processEnded(st)
+
+        self.patch(reactor, 'spawnProcess', spawnProcess)
+
+        def getSourceStamp(vctype, treetop, branch=None, repository=None):
+            return defer.succeed(
+                tryclient.SourceStamp(branch='br', revision='rr',
+                                      patch=(0, '++--')))
+        self.patch(tryclient, 'getSourceStamp', getSourceStamp)
+
+        self.output = []
+
+        # stub out printStatus, as it's timing-based and thus causes
+        # occasional test failures.
+        self.patch(tryclient.Try, 'printStatus', lambda _: None)
+
+        def output(*msg):
+            msg = ' '.join(map(str, msg))
+            log.msg("output: %s" % msg)
+            self.output.append(msg)
+        self.patch(tryclient, 'output', output)
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        if self.worker:
+            log.msg("stopping worker")
+            yield self.worker.stop()
+        if self.master:
+            log.msg("stopping master")
+            yield self.master.stopService()
+            if self.master.db.pool:
+                log.msg("stopping master db pool")
+                yield self.master.db.pool.shutdown()
+        log.msg("tearDown complete")
+        yield self.tearDownDirs()
+
+    def setupJobdir(self):
+        self.jobdir = os.path.join(self.basedir, 'jobs')
+        for sub in 'new', 'tmp', 'cur':
+            p = os.path.join(self.jobdir, sub)
+            os.makedirs(p)
+        return self.jobdir
+
+    @defer.inlineCallbacks
+    def startMaster(self, sch, startWorker=False):
+        BuildmasterConfig['schedulers'] = [sch]
+        self.sch = sch
+
+        BuildmasterConfig['status'] = []
+
+        # create the master and set its config
+        m = self.master = master.BuildMaster(self.basedir, self.configfile)
+        m.config = config.MasterConfig.loadConfig(
+            self.basedir, self.configfile)
+
+        # set up the db
+        yield m.db.setup(check_version=False)
+        yield m.db.model.create()
+
+        # stub out m.db.setup since it was already called above
+        m.db.setup = lambda: None
+
+        # mock reactor.stop (which trial *really* doesn't
+        # like test code to call!)
+        mock_reactor = mock.Mock(spec=reactor)
+        mock_reactor.callWhenRunning = reactor.callWhenRunning
+
+        # start the service
+        yield m.startService(_reactor=mock_reactor)
+        self.failIf(mock_reactor.stop.called,
+                    "startService tried to stop the reactor; check logs")
+
+        # wait until the scheduler is active
+        yield waitFor(lambda: self.sch.active)
+
+        # and, for Try_Userpass, until it's registered its port
+        if isinstance(self.sch, trysched.Try_Userpass):
+            def getSchedulerPort():
+                if not self.sch.registrations:
+                    return
+                self.serverPort = self.sch.registrations[0].getPort()
+                log.msg("Scheduler registered at port %d" % self.serverPort)
+                return True
+            yield waitFor(getSchedulerPort)
+
+        # now start the fake worker
+        if startWorker:
+            self.worker = FakeRemoteWorker(self.serverPort)
+            yield self.worker.start()
+
+    def runClient(self, config):
+        self.clt = tryclient.Try(config)
+        return self.clt.run(_inTests=True)
+
+    @defer.inlineCallbacks
+    def test_userpass_no_wait(self):
+        yield self.startMaster(
+            trysched.Try_Userpass('try', ['a'], 0, [('u', 'p')]),
+            startWorker=False)
+        yield self.runClient({
+            'connect': 'pb',
+            'master': '127.0.0.1:%s' % self.serverPort,
+            'username': 'u',
+            'passwd': 'p',
+        })
+        self.assertEqual(self.output, [
+            "using 'pb' connect method",
+            'job created',
+            'Delivering job; comment= None',
+            'job has been delivered',
+            'not waiting for builds to finish'
+        ])
+        buildsets = yield self.master.db.buildsets.getBuildsets()
+        self.assertEqual(len(buildsets), 1)
+
+    @defer.inlineCallbacks
+    def test_userpass_wait(self):
+        yield self.startMaster(
+            trysched.Try_Userpass('try', ['a'], 0, [('u', 'p')]),
+            startWorker=True)
+        yield self.runClient({
+            'connect': 'pb',
+            'master': '127.0.0.1:%s' % self.serverPort,
+            'username': 'u',
+            'passwd': 'p',
+            'wait': True,
+        })
+        self.assertEqual(self.output, [
+            "using 'pb' connect method",
+            'job created',
+            'Delivering job; comment= None',
+            'job has been delivered',
+            'All Builds Complete',
+            # XXX should be something like "build successful one two", but
+            # currently just drawn from the build status strings
+            'a: success (finished)',
+        ])
+        buildsets = yield self.master.db.buildsets.getBuildsets()
+        self.assertEqual(len(buildsets), 1)
+
+    @defer.inlineCallbacks
+    def test_userpass_list_builders(self):
+        yield self.startMaster(
+            trysched.Try_Userpass('try', ['a'], 0, [('u', 'p')]),
+            startWorker=False)
+        yield self.runClient({
+            'connect': 'pb',
+            'get-builder-names': True,
+            'master': '127.0.0.1:%s' % self.serverPort,
+            'username': 'u',
+            'passwd': 'p',
+        })
+        self.assertEqual(self.output, [
+            "using 'pb' connect method",
+            'The following builders are available for the try scheduler: ',
+            'a'
+        ])
+        buildsets = yield self.master.db.buildsets.getBuildsets()
+        self.assertEqual(len(buildsets), 0)
+
+    @defer.inlineCallbacks
+    def test_jobdir_no_wait(self):
+        jobdir = self.setupJobdir()
+        yield self.startMaster(
+            trysched.Try_Jobdir('try', ['a'], jobdir),
+            startWorker=False)
+        yield self.runClient({
+            'connect': 'ssh',
+            'master': '127.0.0.1',
+            'username': 'u',
+            'passwd': 'p',
+            'builders': 'a',  # appears to be required for ssh
+        })
+        self.assertEqual(self.output, [
+            "using 'ssh' connect method",
+            'job created',
+            'job has been delivered',
+            'not waiting for builds to finish'
+        ])
+        buildsets = yield self.master.db.buildsets.getBuildsets()
+        self.assertEqual(len(buildsets), 1)
+
+    @defer.inlineCallbacks
+    def test_jobdir_wait(self):
+        jobdir = self.setupJobdir()
+        yield self.startMaster(
+            trysched.Try_Jobdir('try', ['a'], jobdir),
+            startWorker=False)
+        yield self.runClient({
+            'connect': 'ssh',
+            'wait': True,
+            'host': '127.0.0.1',
+            'username': 'u',
+            'passwd': 'p',
+            'builders': 'a',  # appears to be required for ssh
+        })
+        self.assertEqual(self.output, [
+            "using 'ssh' connect method",
+            'job created',
+            'job has been delivered',
+            'waiting for builds with ssh is not supported'
+        ])
+        buildsets = yield self.master.db.buildsets.getBuildsets()
+        self.assertEqual(len(buildsets), 1)
+
+BuildmasterConfig = {}
+
+
+def masterConfig():
+    c = {}
+    from buildbot.worker import Worker
+    from buildbot.config import BuilderConfig
+    from buildbot.process.buildstep import BuildStep
+    from buildbot.process.factory import BuildFactory
+    from buildbot.process import results
+
+    class MyBuildStep(BuildStep):
+
+        def start(self):
+            self.finished(results.SUCCESS)
+
+    c['workers'] = [Worker("local1", "localpw")]
+    c['protocols'] = {'pb': {'port': 'tcp:0'}}
+    c['change_source'] = []
+    c['schedulers'] = []  # filled in above
+    f1 = BuildFactory()
+    f1.addStep(MyBuildStep(name='one'))
+    f1.addStep(MyBuildStep(name='two'))
+    c['builders'] = [
+        BuilderConfig(name="a", workernames=["local1"], factory=f1),
+    ]
+    c['status'] = []
+    c['title'] = "test"
+    c['titleURL'] = "test"
+    c['buildbotURL'] = "http://localhost:8010/"
+    c['db'] = {'db_url': "sqlite:///state.sqlite"}
+    c['mq'] = {'debug': True}
+    # test wants to influence the config, but we still return a new config each time
+    c.update(BuildmasterConfig)
+    return c
---
+++ packages/buildbot/buildbot-0.9.0b8.tar.gz/buildbot-0.9.0b8/buildbot/test/util/integration.py
@@ -0,0 +1,231 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+from __future__ import print_function
+from future.utils import itervalues
+
+import StringIO
+import os
+import sys
+import textwrap
+
+import mock
+
+from twisted.internet import defer
+from twisted.internet import reactor
+from twisted.trial import unittest
+
+from buildbot.master import BuildMaster
+from buildbot.process.results import SUCCESS
+from buildbot.process.results import statusToString
+from buildbot.test.util import dirs
+try:
+    from buildslave.bot import BuildSlave
+except ImportError:
+    BuildSlave = None
+
+
+class RunMasterBase(dirs.DirsMixin, unittest.TestCase):
+    proto = "null"
+    # If True the test cases must handle the configuration
+    # of the master in the self.master attribute themselves.
+    # The setupConfig could help the module in that task.
+    # Note that whether testCaseHandleTheirSetup is False or True
+    # in all cases, tearDown that stops the master defined in self.master
+    # will be called.
+    testCasesHandleTheirSetup = False
+
+    if BuildSlave is None:
+        skip = "buildbot-slave package is not installed"
+
+    @defer.inlineCallbacks
+    def setupConfig(self, configFunc):
+        """
+        Setup and start a master configured
+        by the function configFunc defined in the test module.
+        @type configFunc: string
+        @param configFunc: name of a function
+        without argument defined in the test module
+        that returns a BuildmasterConfig object.
+        """
+        self.basedir = os.path.abspath('basdir')
+        self.setUpDirs(self.basedir)
+        self.configfile = os.path.join(self.basedir, 'master.cfg')
+        workerclass = "Worker"
+        if self.proto == 'pb':
+            proto = '{"pb": {"port": "tcp:0:interface=127.0.0.1"}}'
+        elif self.proto == 'null':
+            proto = '{"null": {}}'
+            workerclass = "LocalWorker"
+        # We create a master.cfg, which loads the configuration from the
+        # test module. Only the worker config is kept there, as it should not
+        # be changed
+        open(self.configfile, "w").write(textwrap.dedent("""
+            from buildbot.plugins import worker
+            from {module} import {configFunc}
+            c = BuildmasterConfig = {configFunc}()
+            c['workers'] = [worker.{workerclass}("local1", "localpw")]
+            c['protocols'] = {proto}
+            """).format(module=self.__class__.__module__,
+                        configFunc=configFunc,
+                        proto=proto,
+                        workerclass=workerclass))
+        # create the master and set its config
+        m = BuildMaster(self.basedir, self.configfile)
+        self.master = m
+
+        # update the DB
+        yield m.db.setup(check_version=False)
+        yield m.db.model.upgrade()
+
+        # stub out m.db.setup since it was already called above
+        m.db.setup = lambda: None
+
+        # mock reactor.stop (which trial *really* doesn't
+        # like test code to call!)
+        mock_reactor = mock.Mock(spec=reactor)
+        mock_reactor.callWhenRunning = reactor.callWhenRunning
+
+        # start the service
+        yield m.startService(_reactor=mock_reactor)
+        self.failIf(mock_reactor.stop.called,
+                    "startService tried to stop the reactor; check logs")
+
+        if self.proto == 'pb':
+            # We find out the worker port automatically
+            workerPort = list(itervalues(m.pbmanager.dispatchers))[0].port.getHost().port
+
+            # create a worker, and attach it to the master, it will be started, and stopped
+            # along with the master
+            w = BuildSlave("127.0.0.1", workerPort, "local1", "localpw", self.basedir, False, False)
+        elif self.proto == 'null':
+            w = None
+        if w is not None:
+            w.setServiceParent(m)
+
+    def setUp(self):
+        if self.testCasesHandleTheirSetup:
+            return defer.succeed(None)
+        return self.setupConfig("masterConfig")
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        if not self._passed:
+            dump = StringIO.StringIO()
+            print("FAILED! dumping build db for debug", file=dump)
+            builds = yield self.master.data.get(("builds",))
+            for build in builds:
+                yield self.printBuild(build, dump, withLogs=True)
+        m = self.master
+        # stop the service
+        yield m.stopService()
+
+        # and shutdown the db threadpool, as is normally done at reactor stop
+        m.db.pool.shutdown()
+
+        # (trial will verify all reactor-based timers have been cleared, etc.)
+        self.tearDownDirs()
+        if not self._passed:
+            raise self.failureException(dump.getvalue())
+
+    @defer.inlineCallbacks
+    def doForceBuild(self, wantSteps=False, wantProperties=False,
+                     wantLogs=False, useChange=False):
+
+        # force a build, and wait until it is finished
+        d = defer.Deferred()
+
+        # in order to allow trigger based integration tests
+        # we wait until the first started build is finished
+        self.firstBuildId = None
+
+        def newCallback(_, data):
+            if self.firstBuildId is None:
+                self.firstBuildId = data['buildid']
+                newConsumer.stopConsuming()
+
+        def finishedCallback(_, data):
+            if self.firstBuildId == data['buildid']:
+                d.callback(data)
+
+        newConsumer = yield self.master.mq.startConsuming(
+            newCallback,
+            ('builds', None, 'new'))
+
+        finishedConsumer = yield self.master.mq.startConsuming(
+            finishedCallback,
+            ('builds', None, 'finished'))
+
+        if useChange is False:
+            # use data api to force a build
+            yield self.master.data.control("force", {}, ("forceschedulers", "force"))
+        else:
+            # use data api to force a build, via a new change
+            yield self.master.data.updates.addChange(**useChange)
+
+        # wait until we receive the build finished event
+        build = yield d
+        finishedConsumer.stopConsuming()
+        yield self.enrichBuild(build, wantSteps, wantProperties, wantLogs)
+        defer.returnValue(build)
+
+    @defer.inlineCallbacks
+    def enrichBuild(self, build, wantSteps=False, wantProperties=False, wantLogs=False):
+        # enrich the build result, with the step results
+        if wantSteps:
+            build["steps"] = yield self.master.data.get(("builds", build['buildid'], "steps"))
+            # enrich the step result, with the logs results
+            if wantLogs:
+                build["steps"] = list(build["steps"])
+                for step in build["steps"]:
+                    step['logs'] = yield self.master.data.get(("steps", step['stepid'], "logs"))
+                    step["logs"] = list(step['logs'])
+                    for log in step["logs"]:
+                        log['contents'] = yield self.master.data.get(("logs", log['logid'], "contents"))
+
+        if wantProperties:
+            build["properties"] = yield self.master.data.get(("builds", build['buildid'], "properties"))
+
+    @defer.inlineCallbacks
+    def printBuild(self, build, out=sys.stdout, withLogs=False):
+        # helper for debugging: print a build
+        yield self.enrichBuild(build, wantSteps=True, wantProperties=True, wantLogs=True)
+        print("*** BUILD %d *** ==> %s (%s)" % (build['buildid'], build['state_string'],
+                                                statusToString(build['results'])), file=out)
+        for step in build['steps']:
+            print("    *** STEP %s *** ==> %s (%s)" % (step['name'], step['state_string'],
+                                                       statusToString(step['results'])), file=out)
+            for url in step['urls']:
+                print("       url:%s (%s)" % (url['name'], url['url']), file=out)
+            for log in step['logs']:
+                print("        log:%s (%d)" % (log['name'], log['num_lines']), file=out)
+                if step['results'] != SUCCESS or withLogs:
+                    self.printLog(log, out)
+
+    def printLog(self, log, out):
+        print(" " * 8 + "*********** LOG: %s *********" % (log['name'],), file=out)
+        if log['type'] == 's':
+            for line in log['contents']['content'].splitlines():
+                linetype = line[0]
+                line = line[1:]
+                if linetype == 'h':
+                    # cyan
+                    line = "\x1b[36m" + line + "\x1b[0m"
+                if linetype == 'e':
+                    # red
+                    line = "\x1b[31m" + line + "\x1b[0m"
+                print(" " * 8 + line)
+        else:
+            print(log['contents']['content'], file=out)
+        print(" " * 8 + "********************************", file=out)
