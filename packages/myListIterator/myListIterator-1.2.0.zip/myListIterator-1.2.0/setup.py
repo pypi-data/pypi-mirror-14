a1412e8119fb362fde8ebfcbb12d042736253c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package myListIterator/1.2.0/myListIterator-1.2.0.zip

---
+++ packages/myListIterator/myListIterator-1.2.0.zip/myListIterator-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'myListIterator',
+    version         = '1.2.0',
+    py_modules      = ['myListIterator'],
+    author          = 'Hariharan',
+    author_email    = 'hariharan230890@gmail.com',
+    url             = 'https://url',
+    description     = 'A simple printer of nested lists'     
+    )
