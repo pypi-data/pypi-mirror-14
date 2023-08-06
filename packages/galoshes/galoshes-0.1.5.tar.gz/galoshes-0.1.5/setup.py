3e15e87358c7a312a83d19a2b426d0b2f4cf7d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:22 +0000

    Add package galoshes/0.1.5/galoshes-0.1.5.tar.gz

---
+++ packages/galoshes/galoshes-0.1.5.tar.gz/galoshes-0.1.5/setup.py
@@ -0,0 +1,45 @@
+'''Galoshes
+'''
+
+from distutils.core import setup
+import numpy as np
+from setuptools import find_packages
+
+CLASSIFIERS = [
+'Development Status :: 3 - Alpha',
+'Intended Audience :: Developers',
+'Intended Audience :: Science/Research',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python',
+'Topic :: Scientific/Engineering',
+'Topic :: Scientific/Engineering :: Mathematics',
+'Topic :: Scientific/Engineering :: Physics',
+'Operating System :: Microsoft :: Windows',
+'Operating System :: POSIX',
+'Operating System :: Unix',
+'Operating System :: MacOS',
+'Natural Language :: English',
+]
+
+with open('README.md') as fp:
+    LONG_DESCRIPTION = ''.join(fp.readlines())
+
+setup(
+    name = 'galoshes',
+    version = '0.1.5',
+    packages = find_packages(),
+    install_requires = ['numpy>=1.7',
+                       ],
+    author = 'Brendan Smithyman',
+    author_email = 'brendan@bitsmithy.net',
+    description = 'galoshes',
+    long_description = LONG_DESCRIPTION,
+    license = 'MIT',
+    keywords = 'dictionary class attribute',
+    url = 'https://github.com/bsmithyman/galoshes',
+    download_url = 'https://github.com/bsmithyman/galoshes',
+    classifiers = CLASSIFIERS,
+    platforms = ['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
+    use_2to3 = False,
+    include_dirs=[np.get_include()],
+)
