2343820934b8228ca475870a34b115a39db704c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:11 +0000

    Add package pytsite/0.65.1/PytSite-0.65.1.tar.gz

---
+++ packages/pytsite/PytSite-0.65.1.tar.gz/PytSite-0.65.1/pytsite/route_alias/__init__.py
@@ -0,0 +1,32 @@
+"""Route Alias Package Init
+"""
+from pytsite import odm as _odm, lang as _lang, router as _router, events as _events
+
+# Public API
+from . import _model as model
+from ._api import create, find, find_by_target, find_by_alias
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _router_pre_dispatch_handler(path_info: str):
+    """Router pre-dispatch event handler.
+    """
+    p = _odm.find('route_alias') \
+        .where('alias', '=', path_info) \
+        .where('language', '=', _lang.get_current()) \
+        .first()
+
+    if p:
+        _router.add_path_alias(p.f_get('alias'), p.f_get('target'))
+
+
+def __init():
+    from ._model import RouteAlias
+    _events.listen('pytsite.router.pre_dispatch', _router_pre_dispatch_handler)
+    _odm.register_model('route_alias', RouteAlias)
+
+
+__init()
