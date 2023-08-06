b749d31be15b3e9b2408d3c21c75999209487772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:38 +0000

    Add package skbold/0.2.3/skbold-0.2.3-py2.py3-none-any.whl

---
+++ packages/skbold/skbold-0.2.3-py2.py3-none-any.whl/skbold/core/__init__.py
@@ -0,0 +1,11 @@
+# Author: Lukas Snoek [lukassnoek.github.io]
+# Contact: lukassnoek@gmail.com
+# License: 3 clause BSD
+
+from .mvp import Mvp
+from .convert_to_epi import convert2epi
+from .convert_to_mni import convert2mni
+
+Mvp.__module__ = 'core'
+
+__all__ = ['Mvp', 'convert2epi', 'convert2mni']
