7f3106bffa67bce6d8f07a79bb24a39217afd807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:21 +0000

    Add package SCRY/1.1.10/SCRY-1.1.10.tar.gz

---
+++ packages/SCRY/SCRY-1.1.10.tar.gz/SCRY-1.1.10/scry/app/routes.py
@@ -0,0 +1,126 @@
+import flask
+
+from datetime import datetime
+from functools import wraps
+from werkzeug.exceptions import HTTPException
+from scry.app import app, DEFAULT_QUERY
+from scry.app.query_wrapper import QueryWrapper
+
+
+# Use this decorator AFTER the app.route decorator(s), NOT before.
+def host_only(func):
+    @wraps(func)
+    def host_only_func(*args, **kwargs):
+        if flask.request.remote_addr != '127.0.0.1':
+            e = HTTPException('The page you have requested may only be accessed by the host.')
+            e.code = 403
+            raise e
+            return
+        return func(*args, **kwargs)
+    return host_only_func
+
+
+@app.route('/')
+@app.route('/about')
+def about():
+    args = dict()
+    args.update(flask.request.args)
+    args.update({'page':'about'})
+    return flask.render_template('about.html', **args)
+
+
+@app.route('/services')
+def services():
+    args = dict()
+    args.update(flask.request.args)
+    args.update({'page':'services',
+                 'services':flask.g['services'],
+                 'procedures':flask.g['procedures']})
+    return flask.render_template('services.html', **args)
+
+
+@app.route('/query')
+def query():
+    args = dict()
+    args.update(flask.request.args)
+    args.update({'default_query':DEFAULT_QUERY,
+                 'page':'query'})
+    return flask.render_template('query.html', **args)
+
+# Use HTML anchors (#) -- not URL parameters (?) to get specific queries, endpoints, tab titles through YASQE.
+# Variables whose value you can specify: query, endpoint, contentTypeConstruct, contentTypeSelect, endpoint, requestMethod, tabTitle, headers, outFormat
+# Example: localhost:5000/query#query=SELECT%20*%20WHERE%20%7B%0A%20%20%3Fs%20%3Fp%20%3Fo%20.%0A%7D%20LIMIT%2010&tabTitle=Example%201
+
+
+@app.route('/update')
+#@host_only
+def update():
+    args = dict()
+    args.update(flask.request.args)
+    args.update({'page':'update'})
+    return flask.render_template('update.html', **args)
+
+
+@app.route('/contact')
+def contact():
+    args = dict()
+    args.update(flask.request.args)
+    args.update({'page':'contact'})
+    return flask.render_template('contact.html', **args)
+
+
+@app.route('/scry/', methods=['GET', 'POST'])
+def sparql():
+    flask.g['query_count'] += 1
+    wrapper = QueryWrapper(flask.request,flask.g)
+    response = wrapper.resolve()
+    response.headers['Access-Control-Allow-Origin'] = '*' # Enable CORS
+    return response
+
+
+@app.route('/shutdown', methods=['POST'])
+@host_only
+def shutdown():
+    func = flask.request.environ.get('werkzeug.server.shutdown')()
+    return 'SCRY stopped.'
+
+
+@app.route('/update_g', methods=['POST'])
+@host_only
+def update_g():
+    from scry.config   import get_conf
+    from scry.services import load, describe
+    conf = get_conf()
+    flask.g['allowed_ips']                     = set([ip.strip() for ip in conf.get('App','ips').strip().split('\n')])
+    flask.g['services'], flask.g['procedures'] = load()
+    flask.g['orb_description']                 = describe(flask.g['procedures'])
+    return 'Update succesful.'
+
+
+@app.route('/status')
+@host_only
+def status():
+    launch = flask.g['launch_time']
+    live   = datetime.now() - launch
+    mins   = live.seconds/60
+    hours  = mins/60
+    days   = hours/24
+
+    secs   = live.seconds - mins*60
+    mins  -= hours*60
+    hours -= days*24
+    plural = lambda n: 's' if n!=1 else ''
+    
+    if days:
+        time = '%i day%s, %i hour%s and %i minute%s' % (days, plural(days), hours, plural(hours), mins, plural(mins))
+    elif hours:
+        time = '%i hour%s, %i minute%s and %i second%s' % (hours, plural(hours), mins, plural(mins), secs, plural(secs))
+    elif mins:
+        time = '%i minute%s and %i second%s' % (mins, plural(mins), secs, plural(secs))
+    else:
+        time = '%i seconds' % secs
+    
+    res  = 'This SCRY instance has been live for %s.\n' % time +\
+           'It was launched on %s at %02i:%02i:%02i.\n' % (str(launch.date()), launch.hour, launch.minute, launch.second) +\
+           'Since then, it has received %i requests, of which %i were SPARQL queries.' % (flask.g['request_count'], flask.g['query_count'])
+    return res
---
+++ packages/SCRY/SCRY-1.1.10.tar.gz/SCRY-1.1.10/scry/query/core.py
@@ -0,0 +1,206 @@
+import re
+import pyparsing as pp
+import rdflib.plugins.sparql as sparql
+
+from rdflib.graph import Graph, ConjunctiveGraph
+from rdflib.term  import URIRef, Variable
+from tempfile     import mkdtemp
+from shutil       import rmtree
+
+from scry.query.contexts import OrbHandler, ProcedureHandler, VarProcedureHandler, ValuesHandler, BindHandler
+from scry.query.patch    import ServiceHandler
+from scry.utility        import SCRY, SCRYError
+
+
+
+class QueryHandler(object):
+    
+    def __init__(self, query, procedures, orb_description, user_environment):
+        
+        # Initialization attributes
+        self.query_string = query                             # The raw, unparsed query string
+        self.query        = sparql.prepareQuery(query)        # An object with .prologue and .algebra attributes, generated from a query string through RDFLib's SPARQL parser
+        self.procedures   = procedures                        # The loaded procedure dictionary, pointing from PAUs to procedures
+        self.orb_desc     = orb_description                   # An RDF graph describing these procedures in further detail
+        self.user_env     = user_environment                  # A dictionary for procedures to store/share whatever data they require
+        
+        # Execution attributes
+        self.query_type       = None                          # One of: 'SELECT', 'CONSTRUCT', 'ASK' or 'DESCRIBE'
+        self.triples          = list()                        # A list of triples parsed from the query algebra
+        self.prefixes         = str()                         # Prefix statements parsed from the front of a query string
+        self.service_clauses  = list()                        # A list of federated subqueries, marked by SERVICE <endpoint> {} clauses
+        self.service_handlers = list()                        # A list of service handlers -- subset of context_handlers
+        self.response_type    = None                          # The selected MIME type of the response
+        self.graph            = ConjunctiveGraph()            # The graph object against which 'query' will be resolved
+        self.orb_description  = False                         # When an OrbHandler adds the scry:orb_description context, this attribute stores the pointer to it
+        self.context_handlers = list()                        # A list of context handlers, needed for the bookkeeping of call_procedures()
+        self.var_binders      = dict()                        # A dictionary of ?variables used in the query, pointing to the context_handlers that bind values to them        
+        self.result           = None                          # An RDFLib Result object, generated through graph.query() at the end of resolve()
+        self.temp_dirs        = list()                        # A list of temporary directories generated for this query's service calls
+                                                              # Automatically cleaned up after resolving, even in case of errors
+
+        #from rdflib.plugins.sparql.algebra import pprintAlgebra
+        #print 'ALGEBRA'
+        #pprintAlgebra(self.query)
+        
+        # Share the service_handlers list through the query object's prologue:
+        self.query.prologue.service_handlers = self.service_handlers
+
+        # Include the orb_description graph by default:
+        g  = Graph(self.graph.store,SCRY.orb_description)
+        g += self.orb_desc
+
+    
+
+    def resolve(self):
+        # Invoke, in order, the following methods:
+        #
+        #   parse_query()
+        #   call_procedures()
+        #   graph.query()
+        #   cleanup()
+        #
+        # Finally, return the bound result set
+
+
+        try:
+            self.parse_query()        # Parse the triples and possibly a VALUES statement from the query -- sets the 'query', 'query_type', 'triples', 'values' and 'values_vars' attributes
+            self.call_procedures()    # Populate the 'graph' attribute's RDF graph by invoking the procedures encoded in 'triples'
+            self.eval_query()         # Evaluate 'query' against 'graph'                                 -- sets the 'result' attribute
+            self.cleanup()
+            return self.result
+
+        except SCRYError as e:
+            self.output = e.description
+            self.cleanup()
+            raise e
+
+
+    def parse_query(self):
+        self.query_type = self.query.algebra.name[0:-5].upper() # 'SELECT', 'CONSTRUCT', 'ASK' or 'DESCRIBE'
+
+        def parse_algebra(algebra):
+            if isinstance(algebra,sparql.algebra.CompValue):
+                n = algebra.name
+                if n == 'values':
+                    self.context_handlers.append(ValuesHandler(self,algebra))
+                elif n == 'Extend':
+                    self.context_handlers.append(BindHandler(self,algebra))
+                elif n == 'Graph' and algebra.term == SCRY.orb_description:
+                    self.context_handlers.append(OrbHandler(self,algebra))
+                    return
+                elif n == 'ServiceGraphPattern':
+                    if not self.service_clauses:
+                        parse_service_clauses(self.query_string)
+                    h = ServiceHandler(self,algebra,self.service_clauses.pop(0))
+                    self.context_handlers.append(h)
+                    self.service_handlers.append(h)
+                    return
+                for key in algebra:
+                    if key == 'triples':
+                        for t in algebra[key]:
+                            self.triples.append(t)
+                    parse_algebra(algebra[key])
+        
+        def parse_service_clauses(qry): # CAUTION: This breaks if there's curly closing brackets } in Literals (or URIs)...
+            service_pattern      = pp.CaselessLiteral('SERVICE') + pp.Word(pp.printables) + pp.nestedExpr('{','}')
+            self.service_clauses = [re.sub(r'\s+',' ',qry[p[1]:p[2]]) for p in service_pattern.scanString(qry)]
+            prefix_pattern       = pp.CaselessLiteral('PREFIX') + pp.Word(pp.alphanums) + pp.Literal(':') + pp.nestedExpr('<','>')
+            self.prefixes        = '\n'.join([qry[p[1]:p[2]] for p in prefix_pattern.scanString(qry)])
+        
+        parse_algebra(self.query.algebra)
+
+
+    def call_procedures(self):
+        # First,  determine which triples use SCRY predicates (input, output, OTHERS???) and assign them to the appropriate ProcedureHandlers
+        # Second, determine I/O dependence between the ContextHandlers
+        # Third,  execute context handlers in the appropriate order, passing on outputs as inputs where required
+        
+        call_dict     = dict() # A dictionary mapping Procedure Associated URIs to their call handlers
+        var_call_dict = dict() # Similar to ^, except mapping Variable URIs to their call handlers
+        def get_call_handler(uri):
+            if uri in call_dict:
+                return call_dict[uri]
+            
+            if isinstance(uri,URIRef):
+                short_uri = URIRef(uri.encode().split('?')[0]) # Crop off parameters if any are specified
+                if short_uri in self.procedures:
+                    proc = self.procedures[short_uri]
+                    handler        = ProcedureHandler(self,proc,uri)
+                    call_dict[uri] = handler
+                    self.context_handlers.append(handler)
+                else:
+                    raise SCRYError("A SCRY predicate was used with a subject URI that is not associated with any procedures: %s" % uri.encode())
+                return call_dict[uri]
+            elif isinstance(uri,Variable):
+                if uri not in var_call_dict:
+                    handler = VarProcedureHandler(self,uri)
+                    var_call_dict[uri] = handler
+                    self.context_handlers.append(handler)
+                return var_call_dict[uri]
+        
+        def parse_triples():
+            for t in self.triples:
+                if t[0] == SCRY.orb:
+#                   self.describe_orb()
+                    continue
+                pred = URIRef(t[1].encode().split('?')[0]) # Strip any specifiers/parameters from the predicate
+                if pred == SCRY.input:
+                    get_call_handler(t[0]).add_input(t)
+                elif pred == SCRY.output:
+                    get_call_handler(t[0]).add_output(t)
+                elif pred in [SCRY.author, SCRY.description, SCRY.provenance, SCRY.version]:
+                    get_call_handler(t[0]).add_description(t)
+        
+        output_dict = dict() # A dictionary mapping bound variables to the execution handlers that bind them as outputs
+        def set_dependencies():
+            
+            for h in self.context_handlers:
+                h.set_bound_vars()
+                for output in h.output_vars:
+                    if output not in output_dict:
+                        output_dict[output] = set()
+                    output_dict[output].add(h)
+            
+            output_vars = output_dict.keys()
+            for s in self.service_handlers:
+                s.set_bound_vars(output_vars or True)
+                for output in s.output_vars:
+                    if output not in output_dict:
+                        output_dict[output] = set()
+                    output_dict[output].add(s)
+            
+            for h in self.context_handlers:
+                for var in h.input_vars:
+                    try:
+                        h.dependencies = h.dependencies.union(output_dict[var])
+                    except KeyError as k:
+                        raise SCRYError("Unable to resolve depencies involving the Variable %s" % k)
+
+        def execute_all():
+            for h in self.context_handlers:
+                while not h.executed:
+                    indep = h.get_independent_handler(list())
+                    indep.execute()
+
+        parse_triples()
+        set_dependencies()
+        execute_all()
+
+
+    def eval_query(self):
+        
+        self.result = self.graph.query(self.query)
+        
+
+    def get_temp_dir(self):
+        path = mkdtemp()
+        self.temp_dirs.append(path)
+        return path
+
+
+    def cleanup(self):
+        for path in self.temp_dirs:
+            rmtree(path)
+            
+### END OF QueryHandler
