a4046cd619895e3717bc369cdbc4429c28c48a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:48 +0000

    Add package aioftp/0.6.1/aioftp-0.6.1-py3-none-any.whl

---
+++ packages/aioftp/aioftp-0.6.1-py3-none-any.whl/aioftp/__init__.py
@@ -0,0 +1,19 @@
+"""ftp client/server for asyncio"""
+
+from .client import *  # noqa
+from .server import *  # noqa
+from .errors import *  # noqa
+from .common import *  # noqa
+from .pathio import *  # noqa
+
+__version__ = "0.6.1"
+version = tuple(map(int, str.split(__version__, ".")))
+
+__all__ = (
+    client.__all__ +
+    server.__all__ +
+    errors.__all__ +
+    common.__all__ +
+    pathio.__all__ +
+    ("version", "__version__")
+)
