f2242df31bd10d11fccf57df243de49c78c537ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:12 +0000

    Add package django-forms-ajaxified/0.2/django-forms-ajaxified-0.2.tar.gz

---
+++ packages/django-forms-ajaxified/django-forms-ajaxified-0.2.tar.gz/django-forms-ajaxified-0.2/forms_ajaxified/tests/test_app/urls.py
@@ -0,0 +1,20 @@
+"""Dummy URLs for the tests of the forms_ajaxified app."""
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'dummy-form/create/$',
+        views.DummyCreateView.as_view(),
+        name='dummy_create_view'),
+    url(r'dummy-form/delete/(?P<pk>\d+)/$',
+        views.DummyDeleteView.as_view(),
+        name='dummy_delete_view'),
+    url(r'dummy-form/(?P<pk>\d+)/$',
+        views.DummyUpdateView.as_view(),
+        name='dummy_form_view'),
+    url(r'$',
+        views.DummyTemplateView.as_view(),
+        name='dummy_template_view'),
+]
