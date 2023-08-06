4651537f92bbe264f3a7ac63136772b5efb8e335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package crimson_antispam/0.2.0/crimson_antispam-0.2.0.tar.gz

---
+++ packages/crimson_antispam/crimson_antispam-0.2.0.tar.gz/crimson_antispam-0.2.0/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='crimson_antispam',
+    packages=['antispam'],
+    version='0.2.0',
+    include_package_data=True,
+    license='BSD License',
+    description='Anti-spam package for django framework',
+    author='Tony J',
+    author_email='tony@crimsonhack.com',
+    url='https://github.com/tony-joseph/crimson_antispam',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
