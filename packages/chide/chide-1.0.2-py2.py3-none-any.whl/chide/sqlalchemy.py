025bfe45dc084795dac28af66544c7c64dfcb96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:16 +0000

    Add package chide/1.0.2/chide-1.0.2-py2.py3-none-any.whl

---
+++ packages/chide/chide-1.0.2-py2.py3-none-any.whl/chide/sqlalchemy.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+
+from sqlalchemy import inspect
+
+from .collection import Collection as BaseCollection
+
+
+class Collection(BaseCollection):
+    """
+    A specialised :class:`chide.Collection` for make sample declaratively
+    mapped objects when using SQLAlchemy.
+    """
+
+    @staticmethod
+    def identify(type_, attrs):
+        """
+        This method returns the primary key that will be used for the
+        returned object, meaning that only one sample object will exist
+        and be returned for each primary key in a table.
+
+        It's called internally, so you don't have to worry about it.
+        """
+        mapper = inspect(type_)
+        key = [type_]
+        for prop in mapper._identity_key_props:
+            value = attrs.get(prop.key)
+            if value is None:
+                return None
+            key.append(value)
+        return tuple(key)
