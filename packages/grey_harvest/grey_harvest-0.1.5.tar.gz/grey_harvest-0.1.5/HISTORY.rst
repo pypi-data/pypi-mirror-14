24cb0a50a7e7cc39d266f6226812c40379fa863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:05 +0000

    Add package grey_harvest/0.1.5/grey_harvest-0.1.5.tar.gz

---
+++ packages/grey_harvest/grey_harvest-0.1.5.tar.gz/grey_harvest-0.1.5/HISTORY.rst
@@ -0,0 +1,26 @@
+.. :changelog:
+
+History
+=======
+
+0.1.5 (2016-04-20)
+------------------
+- Fixed connections errors that occur when specifying custom test domain
+
+0.1.4 (2016-04-19)
+------------------
+- Users can now filter for proxies running on specific ports
+
+0.1.3 (2015-05-26)
+------------------
+- Added documentation
+
+0.1.2 (2015-05-26)
+------------------
+
+- Corrected some build issues
+
+0.1.0 (2015-05-26)
+------------------
+
+- Initial release
---
+++ packages/grey_harvest/grey_harvest-0.1.5.tar.gz/grey_harvest-0.1.5/setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import setup
+
+def read(*paths):
+	with open(os.path.join(*paths), 'r') as fd:
+		return fd.read()
+
+setup(
+	name = 'grey_harvest',
+	version = '0.1.5',
+	description = 'Generate lists of free, reliable http(s) proxies.',
+	long_description=(read('README.rst')+'\n\n'+\
+					  read('HISTORY.rst')+'\n\n'+\
+					  read('AUTHORS.rst')+'\n\n'),
+	url = 'https://github.com/s0lst1c3/grey_harvest',
+	download_url = 'https://github.com/s0lst1c3/grey_harvest/tarball/0.1.2',
+	license = 'MIT',
+	author = 'Gabriel "s0lst1c3" Ryan',
+	author_email = 'gabriel@solstice.me',
+	py_modules = ['grey_harvest'],
+	include_package_data = True,
+	install_requires = [
+		'requests',
+		'pyOpenSSL',
+		'beautifulsoup4',
+		'lxml',
+		'argparse',
+	],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved :: MIT License',
+		'Natural Language :: English',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: POSIX',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Internet :: Proxy Servers',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Security',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: System :: Systems Administration',
+		'Topic :: Utilities',
+	],
+	entry_points = {
+		'console_scripts' : [
+			'grey_harvest=grey_harvest:main',
+		],
+	},
+)
