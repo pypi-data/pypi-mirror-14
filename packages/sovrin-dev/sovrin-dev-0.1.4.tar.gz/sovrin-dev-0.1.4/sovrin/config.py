975e9eaab81748f88bf378300b271490e523e74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:41 +0000

    Add package sovrin-dev/0.1.4/sovrin-dev-0.1.4.tar.gz

---
+++ packages/sovrin-dev/sovrin-dev-0.1.4.tar.gz/sovrin-dev-0.1.4/sovrin/config.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+nodeReg = OrderedDict([
+    ('Suncoast', ('127.0.0.1', 8001)),
+    ('VACU', ('127.0.0.1', 8003)),
+    ('WSECU', ('127.0.0.1', 8005)),
+    ('CUSO', ('127.0.0.1', 8007))
+])
+
+cliNodeReg = OrderedDict([
+    ('SuncoastC', ('127.0.0.1', 8002)),
+    ('VACUC', ('127.0.0.1', 8004)),
+    ('WSECUC', ('127.0.0.1', 8006)),
+    ('CUSOC', ('127.0.0.1', 8008))
+])
+
+baseDir = "~/.sovrin"
+
+logFilePath = "cli.log"
+outFilePath = "cli_output.log"
+
+
+GraphDB = {
+    "user": "root",
+    "password": "password",
+    "startScript": "/opt/orientdb/bin/server.sh",
+    "shutdownScript": "/opt/orientdb/bin/shutdown.sh"
+}
