98ead762b0a8074754021d18300193eefc631eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:15 +0000

    Add package Tan.PrintAllFilesAndDir/1.1.0/Tan.PrintAllFilesAndDir-1.1.0.tar.gz

---
+++ packages/Tan.PrintAllFilesAndDir/Tan.PrintAllFilesAndDir-1.1.0.tar.gz/Tan.PrintAllFilesAndDir-1.1.0/setup.py
@@ -0,0 +1,12 @@
+#encoding: utf-8
+from distutils.core import setup
+setup(
+    name='Tan.PrintAllFilesAndDir',
+    version='1.1.0',
+    py_modules=['PrintAllFiles'],
+    author='tan1905',
+    author_email='sorrygame@foxmail.com',
+    description='递归打印指定目录下所有的文件和目录\n'
+                '参数0:目录名\n'
+                '参数1:指定n,缩进4*n空格\n'
+)
