497b8c159bf2505c052ddc95a97c4e35db39ece1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:47 +0000

    Add package geotweet/0.1.14/geotweet-0.1.14.tar.gz

---
+++ packages/geotweet/geotweet-0.1.14.tar.gz/geotweet-0.1.14/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+try:
+    from setuptools import setup
+    print "setuptools"
+except ImportError:
+    from distutils.core import setup
+    print "distutils"
+
+setup(
+    name='geotweet',
+    version='0.1.14',
+    description='Fetch geographic tweets from Twitter Streaming API',
+    author='Jeffrey Alan Meyers',
+    author_email='jeffrey.alan.meyers@gmail.com',
+    url='https://github.com/meyersj/geotweet',
+    packages=[
+        'geotweet',
+        'geotweet.twitter',
+        'geotweet.geomongo',
+        'geotweet.mapreduce',
+        'geotweet.mapreduce.utils'
+    ],
+    scripts=['bin/geotweet'],
+    include_package_data=True,
+    install_requires=[
+        'setuptools>=7.0',
+        'argparse',
+        'boto3',
+        'mrjob',
+        'python-twitter',
+        'pymongo',
+        'Geohash',
+        'Shapely',
+        'Rtree'
+    ]
+)
