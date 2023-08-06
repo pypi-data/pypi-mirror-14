18667c74ab819bb852846c26396b612e78c3cc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:11 +0000

    Add package gaiabase/0.0.4/gaiabase-0.0.4.tar.gz

---
+++ packages/gaiabase/gaiabase-0.0.4.tar.gz/gaiabase-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='gaiabase',
+    version='0.0.4',
+    author='Mingli Yuan',
+    author_email='mingli.yuan@gmail.com',
+    packages=['gaia','gaia.cloud','gaia.db'],
+    package_dir={'gaia': 'gaia'},
+    url='git+https://github.com/caiyunapp/gaiabase.git',
+    license='LICENSE',
+    description='The base library for gaia-based projects',
+    install_requires=[
+        "PyYAML"
+    ],
+)
