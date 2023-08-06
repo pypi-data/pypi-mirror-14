c7a56462e723bf8ae45af049e74379fefc9cf804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:18 +0000

    Add package sierraex_nester/1.2.0/sierraex_nester-1.2.0.zip

---
+++ packages/sierraex_nester/sierraex_nester-1.2.0.zip/sierraex_nester-1.2.0/sierraex_nester.py
@@ -0,0 +1,11 @@
+"""comments"""
+
+def print_lol(lst, tabs=0):
+    """comments bla bla"""
+    for i in lst:
+        if isinstance(i, list):
+            print_lol(i, tabs+1)
+        else:
+            for t in range(tabs):
+                print("\t", end='')
+            print(i)
