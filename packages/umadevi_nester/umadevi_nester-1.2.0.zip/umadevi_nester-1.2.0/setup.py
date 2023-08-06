e8eba79b5ed2f04a56647cb102d9bdc784ab2622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:44 +0000

    Add package umadevi_nester/1.2.0/umadevi_nester-1.2.0.zip

---
+++ packages/umadevi_nester/umadevi_nester-1.2.0.zip/umadevi_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'umadevi_nester',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'umadevi',
+    author_email = 'umadevi.samu@gmail.com',
+    url = 'http://www.smallbees.com',
+    description = 'A simple printer of nested lists',
+ )
