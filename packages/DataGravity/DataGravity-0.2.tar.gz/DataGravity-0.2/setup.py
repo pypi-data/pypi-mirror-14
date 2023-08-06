5078954c5bf8d63c7962c5445e5b21227701a58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:36 +0000

    Add package DataGravity/0.2/DataGravity-0.2.tar.gz

---
+++ packages/DataGravity/DataGravity-0.2.tar.gz/DataGravity-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='DataGravity',
+      version='0.2',
+      description='DataGravity Discovery Engine Library',
+      url='https://github.com/datagravityinc/DataGravityDEL',
+      author='Andrew Hay',
+      author_email='ahay@datagravity.com',
+      license='MIT',
+      packages=['DataGravity'],
+      zip_safe=False)
