64c44f7ecb4695c5199a855dbfe4311a77a299a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:19 +0000

    Add package python-db/0.0.0/python-db-0.0.0.tar.gz

---
+++ packages/python-db/python-db-0.0.0.tar.gz/python-db-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'python-db',
+    packages = ['python-db'],
+    version = "0.0.0",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/python-db",
+    download_url = 'https://github.com/jasontrigg0/python-db/tarball/0.0.0',
+    scripts=[],
+    install_requires=[],
+    keywords = [],
+    classifiers = [],
+)
