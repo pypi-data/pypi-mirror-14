708e60e0d8330a6a32c705fc4b2c115cb26da9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package hidash/0.1.7/hidash-0.1.7.tar.gz

---
+++ packages/hidash/hidash-0.1.7.tar.gz/hidash-0.1.7/hidash/router.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from hidash import views
+
+urls = [
+     url(r'^charts/(?P<chart_id>\w*\.json)/$', views.dispatch_chart),
+     url(r'^charts/(?P<chart_id>\w*\.xls)/$', views.dispatch_xls),
+     url(r'^reports/(?P<chart_id>\w*\.pdf)/$', views.dispatch_pdf),
+     url(r'^show_reports.json/$', views.dispatch_group_reports_as_json),
+     url(r'^show_reports/$', views.dispatch_group_reports),
+      ]
