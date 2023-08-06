287a72f10a3a7dcdb63951079fdba1f2e227cc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:31 +0000

    Add package unit-conversion/1.0.2/unit_conversion-1.0.2.zip

---
+++ packages/unit-conversion/unit_conversion-1.0.2.zip/unit_conversion-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unit_conversion',
+    version='1.0.2',
+    packages=('unit_conversion', ),
+    url = 'https://github.com/TheKewlStore/PyUnitConversion',
+    license = '',
+    author = 'Ian Davis',
+    author_email = 'thekewlstore@gmail.com',
+    description = "Simple unit conversion API for python (milliseconds, seconds, minutes, hours, days)",
+    )
