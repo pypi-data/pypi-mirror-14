309c6f071930408e4e9773db60cc1c844f98374d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:29 +0000

    Add package django-logit/1.0.1/django_logit-1.0.1-py2.py3-none-any.whl

---
+++ packages/django-logit/django_logit-1.0.1-py2.py3-none-any.whl/logit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from functools import wraps
+
+logger = logging.getLogger('logit')
+
+
+def logit(func):
+    @wraps(func)
+    def with_logging(request, *args, **kwargs):
+        logger.debug(func.__name__)
+        logger.debug(request.GET)
+        logger.debug(request.POST)
+        return func(request, *args, **kwargs)
+    return with_logging
