f49228227f17a136d6d12a4c2c668afa37eeabd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package pyyp/0.0.1/pyyp-0.0.1.tar.gz

---
+++ packages/pyyp/pyyp-0.0.1.tar.gz/pyyp-0.0.1/pyyp/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+pyyp
+~~~~
+
+:copyright: (c) 2016 by Kent Wang.
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'pyyp'
+__version__ = '0.0.1'
+__author__ = 'Kent Wang'
+__email__ = 'pragkent@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Kent Wang'
+
+from .api import Yunpian
