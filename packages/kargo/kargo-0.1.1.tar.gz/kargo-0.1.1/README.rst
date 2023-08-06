c4fbbaf0f9385fc28ed943c45b91506a5dfb7a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package kargo/0.1.1/kargo-0.1.1.tar.gz

---
+++ packages/kargo/kargo-0.1.1.tar.gz/kargo-0.1.1/README.rst
@@ -0,0 +1,177 @@
+Kargo wrapper
+=================
+This tool helps to deploy a kubernetes cluster with ansible.
+
+
+**Note**: The following choices are done automatically for redundancy.
+According to the number of nodes on your cluster:
+
+* The 2 firsts nodes will have master components installed
+* The 3 firsts nodes will be members of the etcd cluster
+
+You should have at least 3 nodes but you can spawn only one instance for tests purposes.
+
+
+Requirements
+=================
+
+* **Ansible v2.x**
+* The current user must have its ssh **public key** installed on the remote servers.
+* The remote user (option --user) must be in the sudoers with no password
+
+
+
+Installation
+=================
+
+::
+
+    pip2 install kargo
+
+
+Config file
+-----------
+
+A config file can be updated (yaml). (default:
+*/etc/kargo/kargo.yml* )
+This file contains default values for
+some parameters that don't change frequently
+Note these values are overwritten by the command line
+
+::
+
+    inventory_path: "/usr/lib/kargo/ansible/inventory"
+    loglevel: "info"
+    aws_access_key: "<aws_key>"
+    aws_secret_key: "<aws_secret_key>"
+    key_name: "<aws_keypair_name>"
+    image: "<aws_ami>"
+    instance_type: "<aws_instance_type>"
+    group: "<aws_security_group>"
+    vpc_subnet_id: "<aws_vpc_id>"
+    region: "<aws_region>"
+
+    gce_sshkey_path: "/home/foo/.ssh/id_rsa"
+    ...
+
+Basic usage
+-----------
+
+Generate inventory for a baremetal cluster
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The following options are mandatory
+
+On **baremetal**
+
+::
+
+    usage: kargo prepare [-h] --nodes N [N ...] [-p KARGO_PATH]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      --nodes N [N ...]     List of nodes
+      -p KARGO_PATH, --path KARGO_PATH
+                            Where the Ansible playbooks are installed
+
+
+The command below will just clone the git repository and creates the inventory
+The hostvars must be separated by a **comma without spaces**
+
+::
+
+    kargo preprare --nodes node1[ansible_ssh_host=10.99.21.1] node2[ansible_ssh_host=10.99.21.2] node3[ansible_ssh_host=10.99.21.3]
+
+
+
+Run instances and generate the inventory on Clouds
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+**AWS**
+
+
+::
+
+    usage: kargo aws [-h] [--access-key AWS_ACCESS_KEY]
+                         [--secret-key AWS_SECRET_KEY] [--type INSTANCE_TYPE]
+                         [--keypair KEY_NAME] [--region REGION]
+                         [--security-group GROUP] [--vpc-id AWS_VPC_ID]
+                         [--vpc-subnet VPC_SUBNET_ID] [--ami AWS_AMI] --instances
+                         COUNT
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      --access-key AWS_ACCESS_KEY
+                            AWS access key
+      --secret-key AWS_SECRET_KEY
+                            AWS secret key
+      --type INSTANCE_TYPE  AWS instance type
+      --keypair KEY_NAME    AWS key pair name
+      --region REGION       AWS region
+      --security-group GROUP
+                            AWS security group
+      --vpc-id AWS_VPC_ID   EC2 VPC id
+      --vpc-subnet VPC_SUBNET_ID
+                            EC2 VPC regional subnet
+      --ami AWS_AMI         AWS AMI
+      --instances COUNT     Number of nodes
+
+
+if the config file is filled with the proper information you just need to run the following command
+
+
+::
+
+    kargo aws --instances 3 [--coreos]
+
+
+**GCE**
+**warn** : not implemented yet
+
+example:
+
+
+::
+
+    kargo gce --instances 3 --image <gce_image> --type=<aws_machine_type> --zone=<gce_zone> \
+    [--sshkey <keypath>] [--coreos]
+
+
+Deploy cluster
+~~~~~~~~~~~~~~
+
+example: Deploy a kubernetes cluster on CoreOS servers located on GCE
+
+
+::
+
+    kargo deploy -u core -p /kargo-dc1 --gce --coreos
+
+
+::
+
+    usage: kargo deploy [-h] [-n {flannel,weave,calico}] [--aws] [--gce]
+                            [--upgrade] [--coreos] [--non-interactive]
+                            [-p KARGO_PATH] [--ansible_opts ANSIBLE_OPTS]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -n {flannel,weave,calico}, --network-plugin {flannel,weave,calico}
+      --aws                 Kubernetes deployment on AWS
+      --gce                 Kubernetes deployment on GCE
+      --upgrade             Upgrade Kubernetes cluster
+      --coreos              bootstrap python on CoreOS
+      --non-interactive     Don't prompt user for input
+      -p KARGO_PATH, --path KARGO_PATH
+                            Where the Ansible playbooks are installed
+      --ansible_opts ANSIBLE_OPTS
+                            Ansible options
+
+
+- default network plugin : flannel (vxlan) default
+- default kargo_path : "/home/<current_user>/kargo"
+- inventory path : "<kargo_path>/inventory/inventory.cfg".
+- The option ``--inventory`` allows to use an existing inventory (file or dynamic)
+- You can use all Ansible's variables with
+``--ansible_opts '-e foo=bar -e titi=toto -vvv'``
+**Note** : the value must be enclosed by simple quotes
---
+++ packages/kargo/kargo-0.1.1.tar.gz/kargo-0.1.1/src/kargo/deploy.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of Kargo.
+#
+#    Foobar is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+kargo.deploy
+~~~~~~~~~~~~
+
+Deploy a kubernetes cluster. Run the ansible-playbbook
+"""
+
+import sys
+import os
+import re
+import signal
+from subprocess import PIPE, STDOUT, Popen, check_output, CalledProcessError
+from kargo.common import get_logger, query_yes_no, run_command, which
+from ansible.utils.display import Display
+display = Display()
+playbook_exec = which('ansible-playbook')
+ansible_exec = which('ansible')
+
+
+class RunPlaybook(object):
+    '''
+    Run the Ansible playbook to deploy the kubernetes cluster
+    '''
+    def __init__(self, options):
+        self.options = options
+        self.inventorycfg = os.path.join(
+            options['kargo_path'],
+            'inventory/inventory.cfg'
+        )
+        self.logger = get_logger(
+            options.get('logfile'),
+            options.get('loglevel')
+        )
+        self.logger.debug(
+            'Running ansible-playbook command with the following options: %s'
+            % self.options
+        )
+
+    def ssh_prepare(self):
+        '''
+        Run ssh-agent and store identities
+        '''
+        try:
+            sshagent = check_output('ssh-agent')
+        except CalledProcessError as e:
+            display.error('Cannot run the ssh-agent : %s' % e.output)
+        # Set environment variables
+        ssh_envars = re.findall('\w*=[\w*-\/.*]*', sshagent)
+        for v in ssh_envars:
+            os.environ[v.split('=')[0]] = v.split('=')[1]
+        # Store ssh identity
+        try:
+            proc = Popen(
+                'ssh-add', stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True
+            )
+            proc.stdin.write('password\n')
+            proc.stdin.flush()
+            response_stdout, response_stderr = proc.communicate()
+            display.display(response_stdout)
+        except CalledProcessError as e:
+            display.error('Failed to store ssh identity : %s' % e.output)
+        if response_stderr:
+            display.error(response_stderr)
+            self.logger.critical(
+                'Deployment stopped because of ssh credentials'
+                % self.filename
+            )
+            os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
+            sys.exit(1)
+
+    def check_ping(self):
+        '''
+         Check if hosts are reachable
+        '''
+        display.banner('CHECKING SSH CONNECTIONS')
+        cmd = [
+            ansible_exec, '--ssh-extra-args', '-o StrictHostKeyChecking=no',
+            '-u', '%s' % self.options['ansible_user'],
+            '-b', '--become-user=root', '-m', 'ping', 'all',
+            '-i', self.inventorycfg
+        ]
+        rcode, emsg = run_command('SSH ping hosts', cmd)
+        if rcode != 0:
+            self.logger.critical('Cannot connect to hosts: %s' % emsg)
+            os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
+            sys.exit(1)
+        display.display('All hosts are reachable', color='green')
+
+    def deploy_kubernetes(self):
+        '''
+        Run the ansible playbook command
+        '''
+        cmd = [
+            playbook_exec, '--ssh-extra-args', '-o StrictHostKeyChecking=no',
+            '-e', 'kube_network_plugin=%s' % self.options['network_plugin'],
+            '-u',  '%s' % self.options['ansible_user'],
+            '-b', '--become-user=root', '-i', self.inventorycfg,
+            os.path.join(self.options['kargo_path'], 'cluster.yml')
+        ]
+        if 'ansible_opts' in self.options.keys():
+            cmd = cmd + self.options['ansible_opts'].split(' ')
+        for cloud in ['aws', 'gce']:
+            if self.options[cloud]:
+                cmd = cmd + ['-e', 'cloud_provider=%s' % cloud]
+        display.display(' '.join(cmd), color='bright blue')
+        if self.options['interactive']:
+            query_yes_no(
+                'Run kubernetes cluster deployment with the above command ?'
+            )
+        display.banner('RUN PLAYBOOK')
+        self.logger.info(
+            'Running kubernetes deployment with the command: %s' % cmd
+        )
+        rcode, emsg = run_command('Run deployment', cmd)
+        if rcode != 0:
+            self.logger.critical('Deployment failed: %s' % emsg)
+            os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
+            sys.exit(1)
+        display.display('Kubernetes deployed successfuly', color='green')
+        os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
