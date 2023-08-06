d264eae1af2a8740645f1302186c0f1fecd48971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package pyslink/0.1.0/pyslink-0.1.0.tar.gz

---
+++ packages/pyslink/pyslink-0.1.0.tar.gz/pyslink-0.1.0/README.md
@@ -0,0 +1,11 @@
+# pyslink
+
+Soft link a file/directory with python site-packages directory.
+
+Installation via pip:
+    
+    pip install pyslink
+    
+Link a package
+
+    pyslink path/to/main/module
---
+++ packages/pyslink/pyslink-0.1.0.tar.gz/pyslink-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+import pyslink as mod
+
+
+def read(name):
+    with open('./%s' % name, 'r') as f:
+        return f.read()
+
+
+def run():
+
+    setup(name='pyslink',
+          version=mod.__version__,
+          author='Luca Sbardella',
+          author_email="luca@quantmind.com",
+          url="https://github.com/quantmind/pyslink",
+          description=mod.__doc__,
+          license='BSD',
+          long_description=read('README.md'),
+          py_modules=['pyslink'],
+          zip_safe=False,
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5',
+              'Topic :: Utilities',
+              'Topic :: Software Development :: Libraries :: Python Modules'],
+          entry_points={
+              'console_scripts': [
+                  "pyslink = pyslink:main"
+              ]
+          })
+
+
+if __name__ == '__main__':
+    run()
