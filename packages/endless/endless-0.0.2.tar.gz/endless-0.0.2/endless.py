ed4435f35275e6ca5a196fe9c21d5358231eba4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:46 +0000

    Add package endless/0.0.2/endless-0.0.2.tar.gz

---
+++ packages/endless/endless-0.0.2.tar.gz/endless-0.0.2/endless.py
@@ -0,0 +1,27 @@
+from inspect import signature
+from functools import wraps
+
+
+def make(function):
+
+    @wraps(function)
+    def stackless_function_wrapper(*args, **kwargs):
+        signature_instance = signature(function)
+
+        def stackless_function(**kwargs):
+            stack = [function(**kwargs)]
+            value = None
+            while stack:
+                try:
+                    value = stack[-1].send(value)
+                    stack.append(function(**value))
+                    value = None
+                except StopIteration as e:
+                    stack.pop()
+                    value = e.value
+            return value
+
+        kwargs.update(signature_instance.bind_partial(*args).arguments)
+        return stackless_function(**kwargs)
+
+    return stackless_function_wrapper
---
+++ packages/endless/endless-0.0.2.tar.gz/endless-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='endless',
+    author='magniff',
+    version='0.0.2',
+    py_modules=['endless'],
+    zip_safe=False,
+)
