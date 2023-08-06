5778f30d19d753f170709a8b9f6b09e9fae4e40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:25 +0000

    Add package rowingdata/0.66/rowingdata-0.66.zip

---
+++ packages/rowingdata/rowingdata-0.66.zip/rowingdata-0.66/setup.py
@@ -0,0 +1,69 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+	return f.read()
+
+setup(name='rowingdata',
+
+      version='0.66',
+
+      description='The rowingdata library to create colorful plots from CrewNerd, Painsled and other rowing data tools',
+
+      long_description=readme(),
+
+      url='http://rowsandall.wordpress.com',
+
+      author='Sander Roosendaal',
+
+      author_email='roosendaalsander@gmail.com',
+
+      license='MIT',
+
+      packages=['rowingdata'],
+
+      keywords = 'rowing ergometer concept2',
+      
+      install_requires=[
+	  'numpy',
+	  'scipy',
+	  'matplotlib',
+	  'pandas',
+	  'mechanize',
+	  'python-dateutil',
+	  ],
+
+      zip_safe=False,
+      include_package_data=True,
+      # relative to the rowingdata directory
+      package_data={
+	  'rowingdata':[
+	      '2016-03-25-0758.CSV',
+	      '2016-03-25-0758.TCX',
+	      'example.csv',
+	      'painsled_desktop_example.csv',
+	      'RP_testdata.csv',
+	      'testdata.csv'
+	      ],
+	  'bin':[
+	      'testdata.csv',
+	      'crewnerddata.csv',
+	      'crewnerddata.tcx'
+	      ],
+	      
+	  },
+
+      scripts=[
+	  'bin/crewnerdplot.py',
+	  'bin/painsledplot.py',
+	  'bin/tcxtoc2.py',
+	  'bin/painsledtoc2.py',
+	  'bin/painsled_desktop_plot.py',
+	  'bin/painsled_desktop_toc2.py',
+	  'bin/painsledplottime.py',
+	  'bin/painsled_desktop_plottime.py',
+	  'bin/crewnerdplottime.py',
+	  'bin/roweredit.py',
+	  ]
+
+      )
