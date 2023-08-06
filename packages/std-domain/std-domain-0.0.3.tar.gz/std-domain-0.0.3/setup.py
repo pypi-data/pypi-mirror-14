2a12f6d88e547542d1704e7899d80f03a2ebb065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:41 +0000

    Add package std-domain/0.0.3/std-domain-0.0.3.tar.gz

---
+++ packages/std-domain/std-domain-0.0.3.tar.gz/std-domain-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+import sys
+
+if sys.version_info[0] != 3 or sys.version_info[1] < 3:
+    sys.exit('At least Python 3.3 is required for std-domain')
+
+dependencies = [
+    # Basic dependencies
+    'tldextract',
+    'idna',
+
+    # Testing dependencies
+    'nose',
+    'coverage',
+    'coveralls'
+]
+
+setup(
+    name="std-domain",
+    version="0.0.3",
+    author="Evan Darwin",
+    author_email="evan@relta.net",
+    description=("A library for standarized Domain objects"),
+    license="proprietary",
+    keywords="international domain library",
+    url="https://github.com/EvanDarwin/std-domain",
+    py_modules=['stddomain'],
+    install_requires=dependencies,
+    classifiers=[
+        "License :: Other/Proprietary License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Internationalization",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
