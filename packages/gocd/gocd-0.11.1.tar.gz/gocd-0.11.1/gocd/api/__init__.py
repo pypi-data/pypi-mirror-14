46ef8f45fa40b600e48de98f2ecb05cbd24655f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:42 +0000

    Add package gocd/0.11.1/gocd-0.11.1.tar.gz

---
+++ packages/gocd/gocd-0.11.1.tar.gz/gocd-0.11.1/gocd/api/__init__.py
@@ -0,0 +1,6 @@
+from .pipeline import Pipeline
+from .pipeline_groups import PipelineGroups
+from .artifact import Artifact
+from .stage import Stage
+
+__all__ = ['Pipeline', 'PipelineGroups', 'Artifact', 'Stage']
