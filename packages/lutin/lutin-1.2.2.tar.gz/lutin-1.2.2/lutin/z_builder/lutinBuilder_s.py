6834359339927cbecc311bfd25040dfe312b0ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:42 +0000

    Add package lutin/1.2.2/lutin-1.2.2.tar.gz

---
+++ packages/lutin/lutin-1.2.2.tar.gz/lutin-1.2.2/lutin/z_builder/lutinBuilder_s.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+## @author Edouard DUPIN
+##
+## @copyright 2012, Edouard DUPIN, all right reserved
+##
+## @license APACHE v2.0 (see license file)
+##
+
+##
+## ASM builder
+##
+from lutin import multiprocess
+from lutin import tools
+from lutin import depend
+
+##
+## Initialize the builder, if needed ... to get dependency between builder (for example)
+##
+def init():
+	pass
+
+##
+## Get the current builder type.
+## Return the type of builder
+##
+def get_type():
+	return "compiler"
+
+##
+## @brief Get builder input file type
+## @return List of extention supported
+##
+def get_input_type():
+	return ["s", "S"]
+
+##
+## @brief Get builder output file type
+## @return List of extention supported
+##
+def get_output_type():
+	return ["o"]
+
+##
+## @brief Get builder support multi-threading or not
+## @return True Multithreading supported
+## @return False Multithreading NOT supported
+##
+def get_support_multithreading():
+	return True
