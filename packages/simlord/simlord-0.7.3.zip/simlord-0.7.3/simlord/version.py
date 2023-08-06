2f8d80dd688bd191f206982fa8c8f6e04d273af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package simlord/0.7.3/simlord-0.7.3.zip

---
+++ packages/simlord/simlord-0.7.3.zip/simlord-0.7.3/simlord/version.py
@@ -0,0 +1,5 @@
+__version__ = "0.7.3"
+__author__ = "Bianca Stöcker"
+
+DESCRIPTION = "SimLoRD is a read simulator for long reads from third generation sequencing and is currently focused on the Pacific Biosciences SMRT error model."
+
