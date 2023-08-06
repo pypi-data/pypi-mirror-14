732bb5a23f9a841bd38d9d36124a5ede103b6dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package cltk/0.1.34/cltk-0.1.34.tar.gz

---
+++ packages/cltk/cltk-0.1.34.tar.gz/cltk-0.1.34/cltk/corpus/sanskrit/corpora.py
@@ -0,0 +1,11 @@
+SANSKRIT_CORPORA = [
+        {'name':'sanskrit_text_jnu',
+        'location':'remote',
+        'type':'text'},
+        {'name':'sanskrit_text_dcs',
+        'location':'remote',
+        'type':'text'},
+        {'name':'sanskrit_parallel_sacred_texts',
+        'location':'remote',
+        'type':'parallel'},
+        ]
