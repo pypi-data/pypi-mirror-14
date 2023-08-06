316cd2a9a915b7d0f0c9c4263b703196ec28f1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:58 +0000

    Add package questionnaire/0.4.2/questionnaire-0.4.2.tar.gz

---
+++ packages/questionnaire/questionnaire-0.4.2.tar.gz/questionnaire-0.4.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath, 'r') as fp:
+        return fp.read()
+
+setup(
+    name='questionnaire',
+    version='0.4.2',
+    description='Uses https://github.com/wong2/pick to prompt user to fill' \
+                ' out a questionnaire, and returns the results as a dict',
+    long_description=fread('README.md'),
+    keywords='terminal gui pick',
+    url='https://github.com/kylebebak/questionnaire',
+    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.2',
+    author='kylebebak',
+    author_email='kylebebak@gmail.com',
+    license='MIT',
+    packages=['questionnaire'],
+    install_requires=['pick==0.6.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
+)
