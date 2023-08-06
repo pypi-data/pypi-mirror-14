2f32a32bf6003d1d3585cc0316410528662f6809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package b3j0f.conf/0.3.7/b3j0f.conf-0.3.7.tar.bz2

---
+++ packages/b3j0f.conf/b3j0f.conf-0.3.7.tar.bz2/b3j0f.conf-0.3.7/b3j0f/conf/model/__init__.py
@@ -0,0 +1,10 @@
+
+__all__ = [
+	'Configuration', 'configuration',
+	'Category', 'category',
+	'Parameter'
+]
+
+from .conf import Configuration, configuration
+from .cat import Category, category
+from .param import Parameter
