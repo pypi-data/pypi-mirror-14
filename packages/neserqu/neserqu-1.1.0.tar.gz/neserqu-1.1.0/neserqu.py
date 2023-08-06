03fc85b66129e9a228c8f1a615c9286c696175fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package neserqu/1.1.0/neserqu-1.1.0.tar.gz

---
+++ packages/neserqu/neserqu-1.1.0.tar.gz/neserqu-1.1.0/neserqu.py
@@ -0,0 +1,9 @@
+"""this is a fuction print table"""
+def print_lol(the_list,level):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tag_stop in range(level):
+                 print("\t")
+            print(each_item)
