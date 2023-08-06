1f627dc696c0ce99331f2bf681bf4d5e3983c833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:11 +0000

    Add package PACE/1.0.0b6/PACE-1.0.0b6.tar.gz

---
+++ packages/PACE/PACE-1.0.0b6.tar.gz/PACE-1.0.0b6/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='PACE',
+    version='1.0.0b6',
+    packages=find_packages(),
+    url='https://github.com/Dispersive-Hydrodynamics-Lab/PACE',
+    license='LGPL3',
+    author='William Farmer',
+    author_email='willzfarmer@gmail.com',
+    description='Data Quality of Experimental Data',
+    long_description=('A tool for the Dispersive Hydrodynamics Lab at the University of Colorado, Boulder '
+                      'to use to determine data quality using conduit straightness algorithms and '
+                      'image noise values.'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='dispersive hydrodynamics data quality straightness',
+    install_requires=[
+        'typing',
+        'enforce',
+        'tqdm',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'scikit-learn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'PACE = PACE:main'
+        ]
+    }
+
+)
