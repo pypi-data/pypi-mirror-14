d6d3b2a7c83db82f84fb6d58ba08bd25d21527a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:16 +0000

    Add package flake8-commas/0.1.3/flake8-commas-0.1.3.tar.gz

---
+++ packages/flake8-commas/flake8-commas-0.1.3.tar.gz/flake8-commas-0.1.3/setup.py
@@ -0,0 +1,56 @@
+import io
+import os
+from setuptools import setup
+
+__dir__ = os.path.dirname(__file__)
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+long_description = read('README.rst')
+
+about = {}
+with open(os.path.join(__dir__, 'flake8_commas', '__about__.py')) as file:
+    exec(file.read(), about)
+
+
+setup(
+    name='flake8-commas',
+    author='Trevor Creech',
+    version=about['__version__'],
+    install_requires=[
+        'pep8',
+    ],
+    url='http://github.com/zedlander/flake8-commas/',
+    long_description=long_description,
+    description='Flake8 lint for trailing commas.',
+    packages=['flake8_commas'],
+    test_suite='test',
+    include_package_data=True,
+    entry_points={
+        'flake8.extension': [
+            'flake8_commas = flake8_commas:CommaChecker',
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
