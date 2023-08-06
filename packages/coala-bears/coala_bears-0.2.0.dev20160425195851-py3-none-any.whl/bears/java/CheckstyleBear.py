112abcb03a413ea54f356ece63e03e9cc8866032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:40 +0000

    Add package coala-bears/0.2.0.dev20160425195851/coala_bears-0.2.0.dev20160425195851-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.2.0.dev20160425195851-py3-none-any.whl/bears/java/CheckstyleBear.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, join
+
+from coalib.bearlib.abstractions.Linter import linter
+
+
+checkstyle_jar_file = join(dirname(abspath(__file__)), 'checkstyle.jar')
+google_checks = join(dirname(abspath(__file__)), 'google_checks.xml')
+
+
+@linter(executable='java',
+        output_format='regex',
+        output_regex=r'\[(?P<severity>WARN|INFO)\] *.+:'
+                     r'(?P<line>\d+)(?::(?P<column>\d+))?: *'
+                     r'(?P<message>.*?) *\[(?P<origin>[a-zA-Z]+?)\]',
+        prerequisite_check_command=('java', '-jar', checkstyle_jar_file, '-v'),
+        prerequisite_check_fail_message='jar file ' + checkstyle_jar_file +
+                                        ' not found.')
+class CheckstyleBear:
+    """
+    Check Java code for possible style, semantic and design issues.
+
+    For more information, consult
+    <http://checkstyle.sourceforge.net/checks.html>.
+    """
+
+    LANGUAGES = ("Java",)
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return '-jar', checkstyle_jar_file, '-c', google_checks, filename
