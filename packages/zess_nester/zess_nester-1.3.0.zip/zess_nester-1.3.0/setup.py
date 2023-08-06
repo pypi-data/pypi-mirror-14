d8f8b128e9a7db41a899e019a8cabc00d8f45477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:16 +0000

    Add package zess_nester/1.3.0/zess_nester-1.3.0.zip

---
+++ packages/zess_nester/zess_nester-1.3.0.zip/zess_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zess_nester',
+    version = '1.3.0',
+    py_modules = ['zess_nester'],
+    author = 'zess',
+    author_email = 'zess1982@126.com',
+    url = 'http://satellite.sinaapp.com/',
+    description = 'A functional printer of nested lists of zess',
+    )
