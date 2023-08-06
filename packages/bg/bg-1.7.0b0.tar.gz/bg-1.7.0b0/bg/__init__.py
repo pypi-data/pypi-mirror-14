a37c5290d9c5cae169bdb32fd8bf8b2a44030706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:09 +0000

    Add package bg/1.7.0b0/bg-1.7.0b0.tar.gz

---
+++ packages/bg/bg-1.7.0b0.tar.gz/bg-1.7.0b0/bg/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__author__ = "Sergey Aganezov"
+__email__ = "aganezov(at)gwu.edu"
+__status__ = "production"
+
+version = "1.7.0b"
+
+__all__ = ["bg_io",
+           "breakpoint_graph",
+           "edge",
+           "genome",
+           "kbreak",
+           "multicolor",
+           "tree",
+           "vertices",
+           "utils",
+           "distances"]
+
