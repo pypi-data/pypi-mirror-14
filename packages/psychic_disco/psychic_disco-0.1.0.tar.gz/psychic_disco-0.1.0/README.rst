62ed06a9660d9190de5d4215d032addc3552a547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:51 +0000

    Add package psychic_disco/0.1.0/psychic_disco-0.1.0.tar.gz

---
+++ packages/psychic_disco/psychic_disco-0.1.0.tar.gz/psychic_disco-0.1.0/README.rst
@@ -0,0 +1,12 @@
+=============
+Psychic Disco
+=============
+-----------------------------------
+Pythonic Microservices on AWS Lambda
+-----------------------------------
+
+Installing
+==========
+Like so::
+
+ pip install psychic_disco
---
+++ packages/psychic_disco/psychic_disco-0.1.0.tar.gz/psychic_disco-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='psychic_disco',
+      version='0.1.0',
+      description='Pythonic Microservices on AWS Lambda',
+      url='http://github.com/robertdfrench/psychic-disco',
+      author='Robert D. French',
+      author_email='robert@robertdfrench.me',
+      license='MIT',
+      packages=['psychic_disco'],
+      zip_safe=False)
