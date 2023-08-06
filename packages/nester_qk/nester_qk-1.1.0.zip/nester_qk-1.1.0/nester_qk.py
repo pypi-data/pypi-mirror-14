a8c1d862247a46820aed9e901fa730505e7b1597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:37 +0000

    Add package nester_qk/1.1.0/nester_qk-1.1.0.zip

---
+++ packages/nester_qk/nester_qk-1.1.0.zip/nester_qk-1.1.0/nester_qk.py
@@ -0,0 +1,13 @@
+"""nester.py模块，提供一个名为print_lol()的函数，用于打印列表，并且可以将多重嵌套的类别打印出来"""
+
+def print_lol(the_list, level):
+    """该函数有一个参数：the_list,通过判断参数是否为列表，如果是就利用递归的方式全部打印出来,
+    第二个参数：level，用于在嵌套循环中插入制表符"""
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for TAB in range(level):
+                print("\t", end='')
+            print(each_item)
