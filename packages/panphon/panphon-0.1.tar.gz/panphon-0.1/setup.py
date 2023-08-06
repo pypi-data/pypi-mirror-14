e2a4aeda225187e5ddd2da14d427255ccb196c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:20 +0000

    Add package panphon/0.1/panphon-0.1.tar.gz

---
+++ packages/panphon/panphon-0.1.tar.gz/panphon-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='panphon',
+      version='0.1',
+      description='Tools for using the International Phonetic Alphabet with phonological features',
+      url='https://github.com/dmort27/panphon',
+      download_url='https://github.com/dmort27/panphon/tarball/0.1',
+      author='David R. Mortensen',
+      author_email='dmortens@cs.cmu.edu',
+      license='MIT',
+      install_requires=['setuptools',
+                        'unicodecsv',
+                        'PyYAML',
+                        'regex'],
+      scripts=['panphon/bin/apply_diacritics.py',
+               'panphon/bin/validate_ipa.py'],
+      packages=['panphon'],
+      package_dir={'panphon': 'panphon'},
+      package_data={'panphon': ['data/*.csv', 'data/*.yml']},
+      zip_safe=True
+      )
