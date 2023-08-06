e7329a5eeb51ef09379413cdc2bbcfa3ebe0fa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:43 +0000

    Add package nester_list_print/1.1.0/nester_list_print-1.1.0.zip

---
+++ packages/nester_list_print/nester_list_print-1.1.0.zip/nester_list_print-1.1.0/nester_list_print.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+
+
+"""
+This is nester.py module whilud provide a function named print_log().
+print_log could  print list which maybe include nested list
+
+"""
+def print_log(the_list,indent=False,level=0):
+    """
+    :param the_list:
+    :return: print each item in the_list
+    """
+    for list_item in the_list:
+        if isinstance(list_item,list):
+            print_log(list_item,indent,level+1)
+        else:
+            if(indent):
+                for tap_stop in range(level):
+                    print("\t",end='')
+            print(list_item)
+
---
+++ packages/nester_list_print/nester_list_print-1.1.0.zip/nester_list_print-1.1.0/setup.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+from distutils.core import setup
+
+setup(
+        name = 'nester_list_print',
+        version = '1.1.0',
+        py_modules = ['nester_list_print'],
+        author = 'Ryan',
+        author_email = 'qraccess@gmail.com',
+        url = '',
+        description = 'A simple printer of nested list',
+     )
