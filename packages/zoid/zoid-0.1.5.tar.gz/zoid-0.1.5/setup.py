f82759009c16f7449126d6ba47e1121c6e8d507a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:25 +0000

    Add package zoid/0.1.5/zoid-0.1.5.tar.gz

---
+++ packages/zoid/zoid-0.1.5.tar.gz/zoid-0.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'zoid',
+    description          = 'Commandline tool for easy hosting of project zomboid servers',
+    version              = '0.1.5',
+    author               = 'David Ewelt',
+    author_email         = 'uranoxyd@gmail.com',
+    url                  = 'https://bitbucket.org/uranoxyd/zoid/',
+    license              = 'BSD',
+    scripts              = ['scripts/zoidcmd.py'],
+    packages             = find_packages(),
+    include_package_data = True,
+    zip_safe             = False,
+
+    install_requires = [
+        'uconfig>=0.1.0',
+    ],
+    dependency_links = [
+        'https://pypi.python.org/pypi/uconfig',
+    ],
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
