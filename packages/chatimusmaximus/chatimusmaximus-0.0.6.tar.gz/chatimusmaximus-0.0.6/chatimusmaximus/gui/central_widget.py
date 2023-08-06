88db90fe63f99d0906167050b6520940e00e645e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:28 +0000

    Add package chatimusmaximus/0.0.6/chatimusmaximus-0.0.6.tar.gz

---
+++ packages/chatimusmaximus/chatimusmaximus-0.0.6.tar.gz/chatimusmaximus-0.0.6/chatimusmaximus/gui/central_widget.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtWidgets
+from .message_area import MessageArea
+from .command_line import CommandLine
+
+
+class CentralWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(CentralWidget, self).__init__(parent)
+        # create the text edit used to display the text
+        self.message_area = MessageArea(parent=self)
+
+        # duck type the slot onto the MainWindow for ease of access
+        self.chat_slot = self.message_area.chat_slot
+        self.command_line = CommandLine(parent=self)
+
+        # create vertical layout to stack the textedit on top
+        # of the `clear` button
+        vertical_layout = QtWidgets.QVBoxLayout()
+
+        # add the text edit and button to vertical layout
+        vertical_layout.addWidget(self.message_area)
+        vertical_layout.addWidget(self.command_line)
+        self.setLayout(vertical_layout)
