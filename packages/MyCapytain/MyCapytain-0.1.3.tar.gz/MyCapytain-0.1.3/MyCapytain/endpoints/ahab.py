dc679b353e7fbdde5ef60c4319ba7ee8295963ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:21 +0000

    Add package MyCapytain/0.1.3/MyCapytain-0.1.3.tar.gz

---
+++ packages/MyCapytain/MyCapytain-0.1.3.tar.gz/MyCapytain-0.1.3/MyCapytain/endpoints/ahab.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: MyCapytain.endpoints.ahab
+   :synopsis: Ahab endpoint implementation
+
+.. moduleauthor:: Thibault Clérice <leponteineptique@gmail.com>
+
+
+"""
+import MyCapytain.endpoints.proto
+import requests
+
+
+class Ahab(MyCapytain.endpoints.proto.Ahab):
+
+    """ 
+    Basic integration of the proto.CTS abstraction
+    """
+    pass
