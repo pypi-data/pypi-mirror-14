e0e72dbda65eb8d8be2a2972da83f783e5b1fa87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package python-wallpaper/0.2.5/python-wallpaper-0.2.5.tar.gz

---
+++ packages/python-wallpaper/python-wallpaper-0.2.5.tar.gz/python-wallpaper-0.2.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from distutils.extension import Extension
+
+
+try:
+    from Cython.Build import cythonize
+    ext_modules = cythonize("wallpaper/_natives.pyx")
+except ImportError:
+    ext_modules = [
+        Extension("wallpaper._natives", ["wallpaper._natives.c"]),
+    ]
+
+setup(
+    name='python-wallpaper',
+    version='0.2.5',
+    url='https://github.com/ondergetekende/python-wallpaper',
+    description=(
+        'python-wallpaper generates pseudorandom abstract wallpapers'
+    ),
+    author='Koert van der Veer',
+    author_email='koert@ondergetekende.nl',
+    packages=['wallpaper'],
+    ext_modules=ext_modules,
+    # setup_requires=["cython"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'generate-wallpaper = wallpapers:cmdline'
+        ]
+    },
+)
