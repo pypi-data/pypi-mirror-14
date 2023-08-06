0c084ce95ce5ec1b1965e430752a54ddb71943d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package infi.django_rest_utils/0.11.2/infi.django_rest_utils-0.11.2.tar.gz

---
+++ packages/infi.django_rest_utils/infi.django_rest_utils-0.11.2.tar.gz/infi.django_rest_utils-0.11.2/src/infi/django_rest_utils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.11.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.11.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd1bd5b50ec16bcffbb8b59cb88fa8e50724e8002'
+__git_head_subject__ = 'Finished Release v0.11.2'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-04-03 12:09:31'
