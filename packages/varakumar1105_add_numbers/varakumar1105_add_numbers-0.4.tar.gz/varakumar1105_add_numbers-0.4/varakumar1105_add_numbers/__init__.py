0f3161baf0ed5948a8f5b8df7c3376d0a45e4281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package varakumar1105_add_numbers/0.4/varakumar1105_add_numbers-0.4.tar.gz

---
+++ packages/varakumar1105_add_numbers/varakumar1105_add_numbers-0.4.tar.gz/varakumar1105_add_numbers-0.4/varakumar1105_add_numbers/__init__.py
@@ -0,0 +1,13 @@
+from .sub import subtract
+from .div import division
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+def add(a,b):
+    return "The sum ==> "+str(a+b);
+
+def joke2():
+    return (u'Wedas Nunst\u00fcck Ja! ... '
+            u'Beiherhund.')
