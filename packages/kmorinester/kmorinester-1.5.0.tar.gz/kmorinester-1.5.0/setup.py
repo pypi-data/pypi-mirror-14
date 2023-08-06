289b597b0beffd3def9def1220cd8f3a825b4ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:10 +0000

    Add package kmorinester/1.5.0/kmorinester-1.5.0.tar.gz

---
+++ packages/kmorinester/kmorinester-1.5.0.tar.gz/kmorinester-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'kmorinester',
+	version		= '1.5.0',
+	py_modules	= ['kmorinester'],
+	author		= 'Kalil Mori Lopes',
+	author_email	= 'kalil.eng@gmail.com',
+	url		= '',
+	description	= 'A simple printer of nested lists',
+)
