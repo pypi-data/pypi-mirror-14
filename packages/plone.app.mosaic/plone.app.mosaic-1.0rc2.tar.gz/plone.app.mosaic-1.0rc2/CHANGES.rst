d8887c3d390cf1ad6142361ab7f4fbd5712f8006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package plone.app.mosaic/1.0rc2/plone.app.mosaic-1.0rc2.tar.gz

---
+++ packages/plone.app.mosaic/plone.app.mosaic-1.0rc2.tar.gz/plone.app.mosaic-1.0rc2/CHANGES.rst
@@ -0,0 +1,232 @@
+Changelog
+=========
+
+1.0rc2 (2016-04-08)
+-------------------
+
+- Fixed TypeError during editing when no layout resource directory was
+  found.  [maurits]
+
+
+1.0rc1 (2016-04-07)
+-------------------
+
+New:
+
+- Allow users to save layouts they are creating
+  [vangheem]
+
+- If only one layout is available, auto-select it.
+  [vangheem]
+
+- Be able to show/hide content layouts.
+  [vangheem]
+
+- Add *Mosaic layout* into available views views when layout behavior
+  is enabled and remove the view when layout behavior is disabled from
+  a portal ype
+  [datakurre]
+
+- Ability to limit maximum amount of Mosaic columns by setting
+  ``data-max-columns`` attribute on ``data-panel`` -element (default: 4).
+  [neilferreira]
+
+- Align rich text editor right if the tile is more on the right side
+  of the page
+  [vangheem]
+
+Fixes:
+
+- Do not remove data-pat-tinymce body attribute as this is not
+  necessary with how tinymce is initialized anymore
+  [vangheem]
+
+- Fix use of rawhtml text tile
+  [vangheem]
+
+- Fix case where you could get an error on save because mosaic could not figure out
+  the tile type correctly
+  [vangheem]
+
+- Do not allow hitting enter for editing non-rich text fields
+  [vangheem]
+
+- Disable clicking links of content inside tiles while in edit mode
+  [vangheem]
+
+- Fix TinyMCE widget in add-form which was broken due to a change how the
+  settings are stored in Plone 5 vs 4. BBB compatible.
+  [jensens]
+
+- Fix tinymce toolbar scrolling out of view with large rich text tiles.
+  TinyMCE toolbar will now being sticky as the user scrolls down.
+  [vangheem]
+
+- Fix tiles not rendering correctly if they contain JavaScript patterns
+  when adding and moving them around.
+  [vangheem]
+
+- Fix do not add _layout multiple times to tile data
+  [vangheem]
+
+- Fix issue where spurious &nbsp; was getting saved to description
+  [vangheem]
+
+- Issue when registry configuration parsing would throw an error
+  [vangheem]
+
+- Fix issue where you would end up saving non-resolveuid urls to rawhtml
+  tiles and also prevent write conflicts when the editor sends out multiple
+  edits at the same time
+  [vangheem]
+
+- Fix to only show drag handlers if customizing layout
+  [vangheem]
+
+- fix issue where you would no longer be able to edit a rich text area
+  after you clear the whole area out
+  [vangheem]
+
+- Fix weird Firefox bug with TinyMCE that prevented data from being saved.
+  [vangheem]
+
+- Fix properties overlay to be scrollable.
+  [vangheem]
+
+- Fix issue where tile field wouldn't get saved back to form with
+  some refactoring.
+  [vangheem]
+
+- Fix issue where first fieldset would not show on properties modal.
+  [vangheem]
+
+
+1.0b3 (2015-09-29)
+------------------
+
+- Add ploneimage action for richtext tiles' toolbar
+  [datakurre]
+
+- Add transforms to set 'layout-' with active content layout name or
+  'layout-custom' into body class
+  [datakurre]
+
+
+1.0b2 (2015-09-16)
+------------------
+
+- Fix issue where mosaic-grid was enabled even a default grid system was set
+  [datakurre]
+
+- Fix issue where Plone body class was added twice
+  [datakurre]
+
+- Fix issue where contentLayout field was shown in tile menu
+  [datakurre]
+
+- Pin plone.app.standardtiles >= 1.0b3
+  [datakurre]
+
+
+1.0b1 (2015-09-16)
+------------------
+
+- Change layout behavior default view from ``view`` to ``layout_view``
+  [datakurre]
+
+- Change to enable *Mosaic layout* noly for Document, Event, Folder and News
+  Item by default
+  [datakurre]
+
+- Change text formatting actions from top toolbar to inline TinyMCE toolbars
+  [datakurre]
+
+- Change remove tile icon from inline close icons to top toolbar button
+  [datakurre]
+
+- Change BS3 as default grid system on Plone 5
+  [vangheem]
+
+- Change site layouts be disabled unless ``mosaic-sitelayouts`` feature is set
+  [datakurre]
+
+- Change displayemenu support be disabled unless ``mosaic-layoutmenu`` feature
+  is set
+  [datakurre]
+
+- Change *Custom layout* menu item to be called *Mosaic layout*
+  [datakurre]
+
+- Change install to no longer to make *Mosaic layout* the default by default
+  [datakurre]
+
+- Add layout editor control panel for Plone 5
+  [vangheem]
+
+- Add link and unlink actions
+  [datakurre]
+
+- Add table tile
+  [datakurre]
+
+- Remove grid system from example layouts (to use configured default grid)
+  [vangheem]
+
+- Remove previously provided TTW content layout examples
+  [datakurre]
+
+- Fixed to work with (and require) plone.app.blocks >= 3.0.0
+  [vangheem]
+
+- Fix GenericSetup profile registration (removed "for")
+  [agitator]
+
+- Fix issue where title field tile and content tile being use on same page
+  would cause weird issues with saving title values
+  [vangheem]
+
+- Fix situation where layout editor broke with broken or missing tiles
+  [vangheem]
+
+- Fix issue where check could not add tile with requried selection field
+  [datakurre]
+
+1.0a3 (2015-06-10)
+------------------
+
+- Add "Bootstrap 3 Demo" example site layouts for Plone 5
+  [datakurre]
+- Add support for plone.app.blocks' generic data grid transform
+  [datakurre]
+- Change the default site layouts in Plone 5 to use 12 column deco grids
+  [datakurre]
+- Fix various site layout support related issues
+  [datakurre]
+- Upgrade to plone.app.drafts >= 1.0b3 and plone.app.blocks >= 2.1.2
+  [datakurre]
+
+1.0a2 (2015-06-08)
+------------------
+
+- Add 'Center tile content' tile style to allow e.g. centering of image tiles
+  [datakurre]
+- Add site and page layout fields to be available on properties overlay
+  [datakurre]
+- Add HTML tag language transform to set correct langauge for HTML site layouts
+  [datakurHTML re]
+- Add HTML body tag class transform to set body class for HTML site layouts
+  [datakurre]
+- Add HTTP headers transform to ensure that the response headers normally set by
+  plone.httpheaders viewlet manager are also set for HTML site layouts
+  [datakurre]
+- Update example site and content layouts
+  [datakurre]
+- Fix to only cache site layout when it's not the default main_template
+  [datakurre]
+- Upgrade to plone.app.drafts >= 1.0b2 and plone.app.standardtiles >= 1.0b1
+  [datakurre]
+
+1.0a1 (2015-05-27)
+------------------
+
+- First technology preview release.
---
+++ packages/plone.app.mosaic/plone.app.mosaic-1.0rc2.tar.gz/plone.app.mosaic-1.0rc2/src/plone/app/mosaic/browser/static/js/mosaic.overlay.js
@@ -0,0 +1,236 @@
+/**
+ * This plugin is used to display an overlay
+ *
+ * @author Rob Gietema
+ * @licstart  The following is the entire license notice for the JavaScript
+ *            code in this page.
+ *
+ * Copyright (C) 2010 Plone Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @licend  The above is the entire license notice for the JavaScript code in
+ *          this page.
+ * @version 0.1
+ */
+
+/* global window: false */
+/*jslint white: true, browser: true, onevar: true, nomen: true,
+eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true,
+immed: true, strict: true, maxlen: 150, maxerr: 9999, quotmark: false */
+
+define([
+  'jquery'
+], function($) {
+  'use strict';
+
+  // Define mosaic namespace if it doesn't exist
+  if (typeof($.mosaic) === "undefined") {
+    $.mosaic = {};
+  }
+
+  // Define the overlay namespace
+  $.mosaic.overlay = {
+  };
+
+  /**
+   * Create a new instance of a mosaic overlay.
+   *
+   * @constructor
+   * @id jQuery.fn.mosaicOverlay
+   * @return {Object} Returns a jQuery object of the matched elements.
+   */
+  $.fn.mosaicOverlay = function () {
+
+    if (typeof($.mosaic.overlay_hide_fields) === "undefined") {
+      $.mosaic.overlay_hide_fields = true;
+    }
+
+    // Loop through matched elements
+    return this.each(function () {
+
+      // Get current object
+      var $el = $(this);
+      $(document.body, $.mosaic.document).append($el);
+
+      // Init overlay
+      $el.addClass("mosaic-overlay");
+      var $wrapper = $('<div class="mosaic-modal-wrapper" />');
+      $el.wrap($wrapper);
+
+      // Add lightbox
+      $(document.body, $.mosaic.document)
+        .prepend($(document.createElement("div"))
+          .addClass("mosaic-overlay-blocker")
+      );
+
+    });
+  };
+
+  /**
+   * Open the overlay
+   *
+   * @id jQuery.mosaic.overlay.open
+   * @param {String} mode Mode of the overlay
+   * @param {Object} tile_config Configuration of the tile
+   */
+  $.mosaic.overlay.open = function (mode, tile_config) {
+
+    // Local variables
+    var form, formtabs, tile_group, x, visible_tabs,
+      field_tile, field, fieldset;
+
+    // Expand the overlay
+    $('.mosaic-modal-wrapper').show().addClass('active');
+    $('.mosaic-overlay-blocker').show();
+    $('body').addClass('plone-modal-open');
+
+    // Get form
+    form = $(".mosaic-overlay").find("form");
+
+    // Clear actions
+    if ($(".mosaic-overlay-ok-button").length === 0) {
+      $(".mosaic-overlay .formControls").children("input").hide();
+      $(".mosaic-overlay .formControls").append(
+        $(document.createElement("input")).attr({
+          'type': 'button',
+          'value': 'Close'
+        })
+        .addClass('button-field context mosaic-overlay-ok-button')
+        .click(function () {
+          $.mosaic.overlay.close();
+        })
+      );
+      $(".mosaic-overlay").prepend(
+        $(document.createElement("button")).attr({
+          'title': 'Close',
+        })
+        .addClass('mosaic-overlay-close')
+        .html('&times;')
+        .click(function () {
+          $.mosaic.overlay.close();
+        })
+      );
+    }
+
+    if (mode === 'all' && $.mosaic.overlay_hide_fields) {
+
+      // Get form tabs
+      formtabs = form.find("nav");
+
+      // Show form tabs
+      formtabs.removeClass('mosaic-hidden');
+
+      // Show all fields
+      form.find("fieldset").children().removeClass('mosaic-hidden');
+
+      // Hide all fieldsets
+      form.find('fieldset').removeClass('active');
+
+      // Deselect all tabs
+      formtabs.find('a').removeClass('active');
+
+      // Hide layout field
+      form.find($.mosaic.options.customContentLayout_selector).addClass('mosaic-hidden');
+      form.find($.mosaic.options.contentLayout_selector).addClass('mosaic-hidden');
+
+      // Hide title and description
+      if($('.mosaic-IDublinCore-title-tile').size() > 0){
+        form.find('#formfield-form-widgets-IDublinCore-title')
+          .addClass('mosaic-hidden');
+      }else{
+        form.find('#formfield-form-widgets-IDublinCore-title')
+          .removeClass('mosaic-hidden');
+      }
+      if($('.mosaic-IDublinCore-description-tile').size() > 0){
+        form.find('#formfield-form-widgets-IDublinCore-description')
+          .addClass('mosaic-hidden');
+      }else{
+        form.find('#formfield-form-widgets-IDublinCore-description')
+          .removeClass('mosaic-hidden');
+      }
+
+      // Hide field which are on the wysiwyg area
+      for (x = 0; x < $.mosaic.options.tiles.length; x += 1) {
+        if ($.mosaic.options.tiles[x].name === 'fields') {
+          tile_group = $.mosaic.options.tiles[x];
+        }
+      }
+      for (x = 0; x < tile_group.tiles.length; x += 1) {
+        field_tile = tile_group.tiles[x];
+        if ($.mosaic.options.panels
+          .find(".mosaic-" + field_tile.name + "-tile")
+            .length !== 0) {
+          $($.mosaic.document.getElementById(field_tile.id))
+            .addClass('mosaic-hidden');
+        }
+      }
+
+      // Hide tab if fieldset has no visible items
+      form.find("fieldset").each(function () {
+        if ($(this).children("div:not(.mosaic-hidden)").length === 0) {
+          $('a[href=#fieldsetlegend-' +
+            $(this).attr('id').split('-')[1] + ']')
+            .addClass('mosaic-hidden');
+        }
+      });
+
+      // Get visible tabs
+      visible_tabs = formtabs.children(':not(.mosaic-hidden)');
+
+      // Select first tab
+      visible_tabs.eq(0).addClass('active');
+      var $fieldset = form.find('#fieldset-' +
+          visible_tabs.eq(0).attr('href').split('-')[1]);
+      if($fieldset.size() === 0){
+          $fieldset = form.find(
+              'fieldset:not(.mosaic-hidden)').eq(0);
+      }
+      $fieldset.addClass('active');
+    } else if (mode === 'field') {
+
+      // Get fieldset and field
+      field = $("#" + tile_config.id);
+      fieldset = field.parents("fieldset");
+
+      // Hide all fieldsets
+      form.find('fieldset').hide();
+
+      // Show current fieldset
+      fieldset.show();
+
+      // Hide all fields in current fieldset
+      fieldset.children().addClass('mosaic-hidden');
+
+      // Show current field
+      field.removeClass('mosaic-hidden');
+
+      // Hide form tabs
+      form.find("nav").addClass('mosaic-hidden');
+    }
+  };
+
+  /**
+   * Close the overlay
+   *
+   * @id jQuery.mosaic.overlay.close
+   */
+  $.mosaic.overlay.close = function () {
+
+    // Hide overlay
+    $('.mosaic-modal-wrapper').hide().removeClass('active');
+    $('.mosaic-overlay-blocker').hide();
+    $('body').removeClass('plone-modal-open');
+  };
+});
