49b4ed377c6fdb1cb2eab57e2b16843721ceee98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:48 +0000

    Add package pypgqueue/0.8.0/pypgqueue-0.8.0.tar.gz

---
+++ packages/pypgqueue/pypgqueue-0.8.0.tar.gz/pypgqueue-0.8.0/pypgq/model.py
@@ -0,0 +1,25 @@
+"""Models representing job queue tables."""
+from bidon.db.model import ModelBase
+
+
+class Job(ModelBase):
+  """Model of a job."""
+  table_name = "jobs"
+  timestamps = ("created_at", None)
+  attrs = dict(
+    name=None,
+    payload=None,
+    priority=None,
+    serialization_key_id=None,
+    started_at=None,
+    completed_at=None,
+    error_message=None)
+
+
+class SerializationKey(ModelBase):
+  """Model of a serialization key. No two jobs with the same serialization key will run at the
+  same time.
+  """
+  table_name = "serialization_keys"
+  timestamps = None
+  attrs = dict(key=None, active_job_id=None)
