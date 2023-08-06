989bd86fddf7ef9bd4e2b8e095309e147e822b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:48 +0000

    Add package fileter/0.1/fileter-0.1.zip

---
+++ packages/fileter/fileter-0.1.zip/fileter-0.1/fileter/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__all__ = ['FilesIterator', 'sources', 'iterators', 'filters', ]
+
+from files_iterator import FilesIterator
+import iterators
+import sources
+import filters
---
+++ packages/fileter/fileter-0.1.zip/fileter-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fileter',
+  packages = ['fileter'],
+  version = '0.1',
+  description = 'Lightweight python lib to iterate files and directories with smart filters.',
+  author = 'Ronen Ness',
+  author_email = 'ronenness@gmail.com',
+  url = 'https://github.com/RonenNess/Fileter',
+  download_url = 'https://github.com/RonenNess/Fileter/tarball/0.1',
+  keywords = ['files', 'directories', 'iteration', 'process files', 'filters', 'walk'],
+  classifiers = [],
+)
