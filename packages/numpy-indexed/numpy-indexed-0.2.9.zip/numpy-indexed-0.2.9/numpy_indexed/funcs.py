2961b50c7231f76f22b44aaeb28c477c6155446d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:52 +0000

    Add package numpy-indexed/0.2.9/numpy-indexed-0.2.9.zip

---
+++ packages/numpy-indexed/numpy-indexed-0.2.9.zip/numpy-indexed-0.2.9/numpy_indexed/funcs.py
@@ -0,0 +1,156 @@
+"""some useful functions of arraysetops type not currently present in numpy"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import *
+
+import numpy as np
+
+from numpy_indexed.grouping import GroupBy
+from numpy_indexed.index import LexIndex, as_index
+from numpy_indexed import semantics
+
+
+def indices(A, B, axis=semantics.axis_default, assume_contained=False):
+    """
+    vectorized numpy equivalent of list.index
+    find indices such that np.all( A[indices] == B)
+    as of yet, does not work on lexindex
+    could it ever? would need lex-compatible searchsorted
+    perhaps we could cast the lexindex to a struct array,
+    but this is better left to the user i feel
+
+    if assume_contained==true, it is assumed that the values in B are indeed present in A
+    if not, a key error is raised in case a value is missing
+
+    is using searchedsorted(sorter) wise? or is creating a sorted copy and inverting index more cache friendly?
+    probably sepends on the size of values. otoh, binary search has poor cache coherence anyway
+    """
+    A = np.asarray(A)
+    B = np.asarray(B)
+
+    Ai = as_index(A, axis)
+    if isinstance(Ai, LexIndex):
+        raise ValueError('Composite key objects not supported in indices function')
+    #use this for getting Ai.keys and Bi.keys organized the same way;
+    #sorting is superfluous though. make sorting a cached property?
+    #should we be working with cached properties generally?
+    Bi = as_index(B, axis, base=True)
+
+    # use raw private keys here, rather than public unpacked keys
+    insertion = np.searchsorted(Ai._keys, Bi._keys, sorter=Ai.sorter, side='left')
+
+    indices = Ai.sorter[insertion]
+
+    if not assume_contained:
+        if not np.alltrue(A[indices] == B):
+            raise KeyError('Not all keys in B are present in A')
+
+    return indices
+
+
+def count(keys, axis=semantics.axis_default):
+    """count the number of times each key occurs in the input set
+
+    Arguments
+    ---------
+    keys : indexable object
+
+    Returns
+    -------
+    unique : ndarray, [groups, ...]
+        unique keys
+    count : ndarray, [groups], int
+        the number of times each key occurs in the input set
+
+    Notes
+    -----
+    Can be seen as numpy work-alike of collections.Counter
+    Alternatively, as sparse equivalent of count_table
+    """
+    index = as_index(keys, axis, base=True)
+    return index.unique, index.count
+
+
+def count_table(*keys):
+    """count the number of times each key occurs in the input set
+
+    Arguments
+    ---------
+    keys : tuple of indexable objects, each having the same number of items
+
+    Returns
+    -------
+    unique : tuple of ndarray, [groups, ...]
+        unique keys for each input item
+        they form the axes labels of the table
+    table : ndarray, [keys[0].groups, ... keys[n].groups], int
+        the number of times each key-combination occurs in the input set
+
+    Notes
+    -----
+    Equivalent to R's pivot table or pandas 'crosstab'
+    Alternatively, dense equivalent of the count function
+    """
+    indices  = [as_index(k, axis=0) for k in keys]
+    uniques  = [i.unique  for i in indices]
+    inverses = [i.inverse for i in indices]
+    shape    = [i.groups  for i in indices]
+    table = np.zeros(shape, np.int)
+    np.add.at(table, inverses, 1)
+    return tuple(uniques), table
+
+
+def multiplicity(keys, axis=semantics.axis_default):
+    """return the multiplicity of each key, or how often it occurs in the set
+
+    Parameters
+    ----------
+    keys : indexable object
+
+    Returns
+    -------
+    ndarray, [keys.size], int
+        the number of times each input item occurs in the set
+    """
+    index = as_index(keys, axis)
+    return index.count[index.inverse]
+
+
+def rank(keys, axis=semantics.axis_default):
+    """where each item is in the pecking order.
+
+    Parameters
+    ----------
+    keys : indexable object
+
+    Returns
+    -------
+    ndarray, [keys.size], int
+        unique integers, ranking the sorting order
+
+    Notes
+    -----
+    we should have that index.sorted[index.rank] == keys
+    """
+    index = as_index(keys, axis)
+    return index.rank
+
+
+def incidence(boundary):
+    """
+    given an Nxm matrix containing boundary info between simplices,
+    compute indidence info matrix
+    not very reusable; should probably not be in this lib
+    """
+    return GroupBy(boundary).split(np.arange(boundary.size) // boundary.shape[1])
+
+
+def mode(pointset, return_indices=False):
+    """compute the mode, or most frequent occuring label in a set"""
+    index = as_index(pointset)
+    bin = np.argmax(index.count)
+    maxpoint = index.unique[bin]
+    if return_indices:
+        idx = index.sorter[index.start[bin]: index.stop[bin]]
+        return maxpoint, idx
+    else:
+        return maxpoint
---
+++ packages/numpy-indexed/numpy-indexed-0.2.9.zip/numpy-indexed-0.2.9/numpy_indexed/tests/test_indexed.py
@@ -0,0 +1,211 @@
+"""unit tests"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import *
+
+import pytest
+import numpy.testing as npt
+
+from numpy_indexed import *
+from numpy_indexed.utility import as_struct_array
+
+
+def test_group_by():
+
+    keys   = ["e", "b", "b", "c", "d", "e", "e", 'a']
+    values = [1.2, 4.5, 4.3, 2.0, 5.67, 8.08, 9.01,1]
+
+    print('two methods of splitting by group')
+    print('as list')
+    for k,v in zip(*group_by(keys)(values)):
+        print(k, v)
+    print('as iterable')
+    g = group_by(keys)
+    for k,v in zip(g.unique, g.split_sequence_as_iterable(values)):
+        print(k, list(v))
+    print('iterable as iterable')
+    for k, v in zip(g.unique, g.split_iterable_as_iterable(values)):
+        print(k, list(v))
+
+    print('some reducing group operations')
+    g = group_by(keys)
+    unique_keys, reduced_values = g.median(values)
+    print('per group median')
+    print(reduced_values)
+    unique_keys, reduced_values = g.mean(values)
+    print('per group mean')
+    print(reduced_values)
+    unique_keys, reduced_values = g.std(values)
+    print('per group std')
+    print(reduced_values)
+    reduced_values = g.reduce(np.array(values), np.minimum) #alternate way of calling
+    print('per group min')
+    print(reduced_values)
+    unique_keys, reduced_values = g.max(values)
+    print('per group max')
+    print(reduced_values)
+
+    print('per group sum using custom reduction')
+    print(group_by(keys, values, lambda x:x.sum()))
+
+
+def test_lex_median():
+    """for making sure i squased all bugs related to fancy-keys and median filter implementation"""
+    keys1  = ["e", "b", "b", "c", "d", "e", "e", 'a']
+    keys2  = ["b", "b", "b", "d", "e", "e", 'e', 'e']
+    values = [1.2, 4.5, 4.3, 2.0, 5.6, 8.8, 9.1, 1]
+
+    unique, median = group_by((keys1, keys2)).median(values)
+    for i in zip(zip(*unique), median):
+        print(i)
+
+
+def test_dict():
+    input = [
+    {'dept': '001', 'sku': 'foo', 'transId': 'uniqueId1', 'qty': 100},
+    {'dept': '001', 'sku': 'bar', 'transId': 'uniqueId2', 'qty': 200},
+    {'dept': '001', 'sku': 'foo', 'transId': 'uniqueId3', 'qty': 300},
+    {'dept': '002', 'sku': 'baz', 'transId': 'uniqueId4', 'qty': 400},
+    {'dept': '002', 'sku': 'baz', 'transId': 'uniqueId5', 'qty': 500},
+    {'dept': '002', 'sku': 'qux', 'transId': 'uniqueId6', 'qty': 600},
+    {'dept': '003', 'sku': 'foo', 'transId': 'uniqueId7', 'qty': 700}
+    ]
+
+    inputs = dict((k, [i[k] for i in input ]) for k in input[0].keys())
+    print(group_by((inputs['dept'], inputs['sku'])).mean(inputs['qty']))
+
+
+def test_fancy_keys():
+    """test Index subclasses"""
+    keys        = np.random.randint(0, 2, (20, 3)).astype(np.int8)
+    values      = np.random.randint(-1, 2, (20, 4))
+
+    #all these various datastructures should produce the same behavior
+    #multiplicity is a nice unit test, since it draws on most of the low level functionality
+    if semantics.backwards_compatible:
+        assert(np.all(
+            multiplicity(keys, axis=0) ==           #void object indexing
+            multiplicity(tuple(keys.T))))           #lexographic indexing
+        assert(np.all(
+            multiplicity(keys, axis=0) ==           #void object indexing
+            multiplicity(as_struct_array(keys))))   #struct array indexing
+    else:
+        assert(np.all(
+            multiplicity(keys) ==                   #void object indexing
+            multiplicity(tuple(keys.T))))           #lexographic indexing
+        assert(np.all(
+            multiplicity(keys) ==                   #void object indexing
+            multiplicity(as_struct_array(keys))))   #struct array indexing
+
+    #lets go mixing some dtypes!
+    floatkeys   = np.zeros(len(keys))
+    floatkeys[0] = 8.8
+    print('sum per group of identical rows using struct key')
+    g = group_by(as_struct_array(keys, floatkeys))
+    for e in zip(g.count, *g.sum(values)):
+        print(e)
+    print('sum per group of identical rows using lex of nd-key')
+    g = group_by(( keys, floatkeys))
+    for e in zip(zip(*g.unique), g.sum(values)[1]):
+        print(e)
+    print('sum per group of identical rows using lex of struct key')
+    g = group_by((as_struct_array( keys), floatkeys))
+    for e in zip(zip(*g.unique), g.sum(values)[1]):
+        print(e)
+
+    #showcase enhanced unique functionality
+    images = np.random.rand(4,4,4)
+    #shuffle the images; this is a giant mess now; how to find the unique ones?
+    shuffled = images[np.random.randint(0,4,200)]
+    #there you go
+    if semantics.backwards_compatible:
+        print(unique(shuffled, axis=0))
+    else:
+        print(unique(shuffled))
+
+
+def test_compact():
+    """demonstrate the most functionality in the least number of lines"""
+    key1 = list('abaabb')
+    key2 = np.random.randint(0,2,(6, 2))
+    values = np.random.rand(6,3)
+    g = group_by((key1, key2))
+    (unique1, unique2), median = group_by((key1, key2)).median(values)
+    print(unique1)
+    print(unique2)
+    print(median)
+
+
+def test_indices():
+    """test indices function"""
+    values = np.random.rand(20)
+    idx = [1, 2, 5, 7]
+
+    assert(np.alltrue(indices(values, values[idx]) == idx))
+    assert(np.alltrue(contains(values, values[idx])))
+    with pytest.raises(KeyError):
+        indices(values, [-1], assume_contained=False)
+
+
+def test_indices_object():
+    """test indices function with objectindex"""
+    A = np.array(
+        [[0, 1],
+         [0, 2],
+         [1, 1],
+         [0, 2]])
+    B = np.array([[0, 2]])
+    assert np.alltrue(indices(A, B) == 1)
+
+
+def test_setops_edgecase():
+    """test some edge cases like zero-length, etc"""
+    assert np.array_equal(intersection([1], [1]), [1])
+    assert np.array_equal(intersection([], []), [])
+
+    assert np.array_equal(count_selected([], []), [])
+    # test repeating values in both arguments
+    assert np.array_equal(count_selected([1, 2, 3, 1], [1, 1, 2]), [2, 2, 1])
+
+    assert np.array_equal(difference([1], []), [1])
+    assert difference([1], []).dtype == np.int
+
+    assert np.array_equal(union([], [], []), [])
+    assert np.array_equal(exclusive([], []), [])
+
+
+def test_setops():
+    """test generalized classic set operations"""
+    # edges exclusive to one of three sets
+    edges = np.random.randint(0, 9, size=(3, 100, 2))
+    print(exclusive(*edges))
+
+    # difference on object keys
+    edges = np.arange(20).reshape(10, 2)
+    assert(np.all(difference(edges[:8], edges[-8:]) == edges[:2]))
+
+    # unique on lex keys
+    key1 = list('abc')*10
+    key2 = np.random.randint(0,9,30)
+    print(unique( (key1, key2)))
+
+
+def test_count_table():
+    k = list('aababaababbbaabba')
+    i = np.random.randint(0, 10, len(k))
+    l, t = count_table(k, i)
+    print(l)
+    print(t)
+
+    l, t = count_table(*np.random.randint(0,4,(3,1000)))
+    print(l)
+    print(t)
+
+    l, t = count_table(np.random.randint(0,4,(1000,3)))
+    print(l)
+    print(t)
+
+
+def test_mode():
+    m, idx = mode([1, 2, 2, 1, 3, 1], True)
+    assert m == 1
+    npt.assert_equal(idx, [0, 3, 5])
