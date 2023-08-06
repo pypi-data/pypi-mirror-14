3895a92413bed0c5c11aa7e87531e646c595ca45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:19 +0000

    Add package hux/1.3.1/hux-1.3.1.zip

---
+++ packages/hux/hux-1.3.1.zip/hux-1.3.1/print_log.py
@@ -0,0 +1,16 @@
+__author__ = 'hx'
+#codeing =utf_8
+'''
+这是第一个python模块，包含了产用的print函数调用，并编写了一个嵌套调用的打印list的函数
+'''
+#定义一个嵌套处理函数，打印list清单的函数 print_log(movies)
+def print_list(the_list,level=0,indent=False):
+    for each_item in the_list:
+        if(isinstance(each_item,list)):
+            print_list(each_item,level+1,indent)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
+#函数end
