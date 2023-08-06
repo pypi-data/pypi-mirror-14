c99d10aa4f216118f3827890283090427aaebb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package nussl/0.1.5a3/nussl-0.1.5a3.tar.gz

---
+++ packages/nussl/nussl-0.1.5a3.tar.gz/nussl-0.1.5a3/nussl/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .spectral_utils import *
+from constants import *
+from .audio_signal import AudioSignal
+from separation_base import SeparationBase
+from Duet import Duet
+from Nmf import Nmf, DistanceType
+from Repet import Repet, RepetType
+
+
+__version__ = '0.1.5a3'
+
+__title__ = 'nussl'
+__description__ = 'A flexible sound source separation library.'
+__uri__ = 'https://github.com/interactiveaudiolab/nussl'
+
+__author__ = 'C. Grief, E. Manilow, F. Pishdadian'
+__email__ = 'ethanmanilow2015@u.northwestern.edu'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2016 Interactive Audio Lab'
