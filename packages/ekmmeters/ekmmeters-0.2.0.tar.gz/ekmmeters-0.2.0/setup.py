bf9aee672875611c2575368171d40738dc917910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package ekmmeters/0.2.0/ekmmeters-0.2.0.tar.gz

---
+++ packages/ekmmeters/ekmmeters-0.2.0.tar.gz/ekmmeters-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name = "ekmmeters",
+    version = "0.2.0",
+    license='MIT',
+    description='Python API for V3 and V4 EKM Omnimeters',
+    author = 'EKM Metering',
+    author_email = "info@ekmmetering.com",
+    url = 'https://github.com/jessicalh/ekmmeters',
+    download_url = 'https://github.com/jessicalh/ekmmeters/tarball/0.2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    py_modules=['ekmmeters'],
+    install_requires=[
+          'OrderedDict==1.1',
+          'pyserial==3.0.1',
+      ]
+)
