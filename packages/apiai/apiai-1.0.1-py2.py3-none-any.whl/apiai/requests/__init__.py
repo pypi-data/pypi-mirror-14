b63d425825e73015566b6c69beb39eb223850f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:40 +0000

    Add package apiai/1.0.1/apiai-1.0.1-py2.py3-none-any.whl

---
+++ packages/apiai/apiai-1.0.1-py2.py3-none-any.whl/apiai/requests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+apiai
+~~~~~~~~~~~~~~~~
+This module provides a ApiAI classes to manage requests.
+"""
+
+__all__ = ['Request', 'QueryRequest', 'TextRequest', 'VoiceRequest', 'UserEntitiesRequest']
+
+from .request       import Request
+from .query         import QueryRequest
+from .query         import TextRequest
+from .query         import VoiceRequest
+
+from .user_entities  import UserEntitiesRequest
