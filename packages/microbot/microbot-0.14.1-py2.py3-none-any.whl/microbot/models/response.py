496c869c399bafae6cfc5a7ec86d9fae99bc6f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package microbot/0.14.1/microbot-0.14.1-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-0.14.1-py2.py3-none-any.whl/microbot/models/response.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+import logging
+from jinja2 import Template
+from microbot.models.base import MicrobotModel
+
+logger = logging.getLogger(__name__)
+
+@python_2_unicode_compatible    
+class Response(MicrobotModel):
+    text_template = models.TextField(verbose_name=_("Text template"),
+                                     help_text=_("Template to generate text response. In jinja2."))
+    keyboard_template = models.TextField(null=True, blank=True, verbose_name=_("Keyboard template"),
+                                         help_text=_("Template to generate keyboard response. In jinja2."))
+    
+    class Meta:
+        verbose_name = _('Response')
+        verbose_name_plural = _('Responses')
+
+    def __str__(self):
+        return "(text:%s, keyboard:%s)" % (self.text_template, self.keyboard_template)
+    
+    def process(self, **context):
+        response_text_template = Template(self.text_template)
+        response_text = response_text_template.render(**context)
+        logger.debug("Response %s generates text  %s" % (self, response_text))
+        if self.keyboard_template:
+            response_keyboard_template = Template(self.keyboard_template)
+            response_keyboard = response_keyboard_template.render(**context)
+        else:
+            response_keyboard = None
+        logger.debug("Response %s generates keyboard  %s" % (self, response_keyboard))
+        return response_text, response_keyboard
