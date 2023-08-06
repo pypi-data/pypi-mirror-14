2d1df2d202f611dc63afebbadc8b2bee4fa2bf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:55 +0000

    Add package pyconfluence/1.0/pyconfluence-1.0.tar.gz

---
+++ packages/pyconfluence/pyconfluence-1.0.tar.gz/pyconfluence-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='pyconfluence',
+    packages=['pyconfluence'],
+    version='1.0',
+    description='An API wrapper for Atlassian Confluence',
+    author='Caleb Hawkins',
+    author_email='hawkins.caleb93@gmail.com',
+    url='https://github.com/muddytm/pyconfluence',
+    download_url='https://github.com/muddytm/pyconfluence/tarball/1.0',
+    keywords=['confluence'],
+    classifiers=[],
+)
