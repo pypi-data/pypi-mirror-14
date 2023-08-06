279017f12524472dbdaa72f61dcd31e36b93fd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:40 +0000

    Add package inlineplz/0.12.0/inlineplz-0.12.0.zip

---
+++ packages/inlineplz/inlineplz-0.12.0.zip/inlineplz-0.12.0/inlineplz/interfaces/base.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+
+class InterfaceBase(object):
+    """Abstract base class for review interfaces"""
+
+    def post_messages(self, messages, max_comments):
+        """
+
+        :param messages:
+        :return:
+        """
+        raise NotImplementedError()
