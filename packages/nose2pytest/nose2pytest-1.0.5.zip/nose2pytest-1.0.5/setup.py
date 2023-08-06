22cae8c1eed45fe38237c17e7cc5224a5083ab85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package nose2pytest/1.0.5/nose2pytest-1.0.5.zip

---
+++ packages/nose2pytest/nose2pytest-1.0.5.zip/nose2pytest-1.0.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+setup(
+        name='nose2pytest',
+        version='1.0.5',
+        packages=['nose2pytest'],
+        # py_modules=['assert_tools', 'nose2pytest'],
+        entry_points={
+            'console_scripts': [
+                'nose2pytest = nose2pytest.script:main',
+            ],
+            'pytest11': ['pytest_nose_assert_tools = nose2pytest.assert_tools'],
+        },
+        url='https://github.com/schollii/nose2pytest',
+        license='BSD-3',
+        author='Oliver Schoenborn',
+        author_email='oliver.schoenborn@gmail.com',
+        description='Convert nose.tools.assert_ calls found in your Nose test modules into raw asserts for pytest',
+        keywords='nose pytest conversion',
+
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 4 - Beta',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Testing',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: BSD License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 3.4',
+        ],
+)
