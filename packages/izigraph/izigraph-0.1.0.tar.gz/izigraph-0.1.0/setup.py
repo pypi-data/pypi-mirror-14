178340a3b5b94f1d4f61a6ceb63b93831532c395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:16 +0000

    Add package izigraph/0.1.0/izigraph-0.1.0.tar.gz

---
+++ packages/izigraph/izigraph-0.1.0.tar.gz/izigraph-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='izigraph',
+      version='0.1.0',
+      description='Package to create graphs',
+      url='https://github.com/gnkz/izigraph/tree/develop',
+      author='Gonzalo Sánchez',
+      author_email='sanchezv.gonzalo@gmail.com',
+      license='MIT',
+      packages=['izigraph'],
+      install_requires=[],
+      zip_safe=False)
