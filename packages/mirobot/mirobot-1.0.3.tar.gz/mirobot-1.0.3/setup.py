d2f3e82f8cf2dacdfaff221cecf3c5e30b102c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:58 +0000

    Add package mirobot/1.0.3/mirobot-1.0.3.tar.gz

---
+++ packages/mirobot/mirobot-1.0.3.tar.gz/mirobot-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mirobot',
+  packages = ['mirobot'],
+  version = '1.0.3',
+  description = 'A Python library to control Mirobot (http://mirobot.io)',
+  author = 'Ben Pirt',
+  author_email = 'ben@pirt.co.uk',
+  url = 'https://github.com/mirobot/mirobot-py',
+  download_url = 'https://github.com/mirobot/mirobot-py/tarball/v1.0.2',
+  keywords = ['robotics', 'control', 'mirobot'],
+  classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3', 'Topic :: Education', 'License :: OSI Approved :: MIT License'],
+  install_requires = ["websocket-client"],
+)
