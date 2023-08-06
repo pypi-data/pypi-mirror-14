c56d2dbfce6327fab67987db315269477bd06ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package RouterOS-api/0.14/RouterOS-api-0.14.tar.gz

---
+++ packages/RouterOS-api/RouterOS-api-0.14.tar.gz/RouterOS-api-0.14/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python                                                                                                                                        
+from setuptools import setup, find_packages
+
+setup(version='0.14',
+      name="RouterOS-api",
+      description='Python API to RouterBoard devices produced by MikroTik.',
+      author='Tomasz Wysocki',
+      author_email='tomasz@pozytywnie.pl',
+      packages=find_packages(),
+      test_suite="tests",
+      license="MIT",
+      install_requires=['six'],
+)
