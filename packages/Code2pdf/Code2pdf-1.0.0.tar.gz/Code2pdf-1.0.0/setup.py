927334c3ae9dd38df9cb3b72fa9ea58ecc978f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:01 +0000

    Add package Code2pdf/1.0.0/Code2pdf-1.0.0.tar.gz

---
+++ packages/Code2pdf/Code2pdf-1.0.0.tar.gz/Code2pdf-1.0.0/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+import os
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+setup(name='Code2pdf',
+      version='1.0.0',
+      install_requires=[
+          r for r in open('requirements.txt', 'r').read().split('\n') if r],
+      author='Tushar Gautam',
+      author_email='tushar.rishav@gmail.com',
+      packages=['Code2pdf', ],
+      entry_points={
+          'console_scripts': ['code2pdf=Code2pdf:main'],
+      },
+      license='GNU General Public License v3 (GPLv3)',
+      url='https://github.com/tushar-rishav/code2pdf/',
+      description="Converts given source code into pdf file with syntax \
+                    highlighting, line numbers and much more",
+      long_description=open('README').read(),
+      keywords=['pdf', 'markup',
+                'formatting', 'convert code to pdf', 'python'],
+      classifiers=[
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Monitoring'
+      ],
+      )
