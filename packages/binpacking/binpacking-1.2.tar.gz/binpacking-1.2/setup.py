d3f7f65579e3095470373054890aa23b87f6d6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:31 +0000

    Add package binpacking/1.2/binpacking-1.2.tar.gz

---
+++ packages/binpacking/binpacking-1.2.tar.gz/binpacking-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='binpacking',
+      version='1.2',
+      description='Aims at optimal distribution of weighted items to bins (either a fixed number of bins or a fixed number of volume per bin). Data may be in form of list, dictionary, list of tuples or csv-file.',
+      url='https://www.github.com/benmaier/binpacking',
+      author='Benjamin F. Maier',
+      author_email='bfmaier@physik.hu-berlin.de',
+      license='MIT',
+      packages=['binpacking'],
+      install_requires=[
+          'numpy',
+      ],
+      dependency_links=[
+          ],
+      entry_points = {
+            'console_scripts': [
+                    'binpacking = binpacking.binpacking_binary:main',
+                ],
+            },
+      zip_safe=False)
