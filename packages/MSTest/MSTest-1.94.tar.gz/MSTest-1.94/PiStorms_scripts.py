f60de01f355543395f0abca41f8d7aa7243a4761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:15 +0000

    Add package MSTest/1.93/MSTest-1.94.tar.gz

---
+++ packages/MSTest/MSTest-1.94.tar.gz/MSTest-1.94/PiStorms_scripts.py
@@ -0,0 +1,7 @@
+import sys, subprocess
+
+def install():
+    subprocess.call("bash /usr/local/include PiStorms_install.sh", shell=True)
+    
+def upgrade():
+    subprocess.call("bash /usr/local/include/PiStorms_upgrade.sh", shell=True)
