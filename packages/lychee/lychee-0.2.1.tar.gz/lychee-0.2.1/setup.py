e5f060eb46e9966a4993e4600f322b2f6c2de335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package lychee/0.2.1/lychee-0.2.1.tar.gz

---
+++ packages/lychee/lychee-0.2.1.tar.gz/lychee-0.2.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lychee',
+    version='0.2.1',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=['PyYAML', 'Jinja2', 'markdown'],
+
+    zip_safe=False,
+
+    description="I'm Lychee. A static blog generator.",
+    keywords='lychee static blog generator',
+
+    entry_points={
+        'console_scripts': [
+            'lychee=lychee.management:execute_from_command_line',
+        ],
+    }
+)
