b854c4377e820cdac81a22f9dd8eb388dfd823b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package nestercjg/1.0.0/nestercjg-1.0.0.zip

---
+++ packages/nestercjg/nestercjg-1.0.0.zip/nestercjg-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nestercjg',
+	version		= '1.0.0',
+	py_modules	= ['nestercjg'],
+	author		= 'cjgreen',
+	author_email	= '',
+	url		= '',
+	description	= 'A simple printer of nested lists',
+	)
