1266433d7eeb07172f56ca5ccc9b223dfdc9091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:37 +0000

    Add package sac/0.4.2/sac-0.4.2.tar.gz

---
+++ packages/sac/sac-0.4.2.tar.gz/sac-0.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'sac = sac:main'
+        ]
+    },
+    name='sac',
+    packages=[
+        'sac',
+        'sac.cli',
+        'sac.model'
+    ],
+    version='0.4.2',
+    description='Semantic Audio Companion',
+    author='Nikolaos Tsipas',
+    author_email='nicktgr15@yahoo.com',
+    url='https://github.com/nicktgr15/sac',
+    download_url='https://github.com/nicktgr15/sac/releases/tag/0.4.2',
+    keywords=['semantic', 'audio', 'sox', 'similarity', 'matrix'],
+    classifiers=[],
+    install_requires=['matplotlib', 'numpy', 'scipy']
+)
