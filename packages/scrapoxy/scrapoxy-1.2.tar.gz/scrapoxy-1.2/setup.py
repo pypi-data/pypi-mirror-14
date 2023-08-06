bab9c88074d1243c6c16a2bca16856a28e6022ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:53 +0000

    Add package scrapoxy/1.2/scrapoxy-1.2.tar.gz

---
+++ packages/scrapoxy/scrapoxy-1.2.tar.gz/scrapoxy-1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='scrapoxy',
+    packages=['scrapoxy'],
+    version='1.2',
+    description='Python API for Scrapoxy and Scrapy',
+    author='Fabien Vauchelles',
+    author_email='fabien@vauchelles.com',
+    url='https://github.com/fabienvauchelles/scrapoxy-python-api',
+    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.2',
+    keywords=['scrapoxy', 'scrapy', 'scraper', 'scraping'],
+    classifiers=[],
+)
