cf5f13ef6a1527d6c64d00a5e897f32124bda967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:10 +0000

    Add package sanitize1234/1.0.1/sanitize1234-1.0.1.zip

---
+++ packages/sanitize1234/sanitize1234-1.0.1.zip/sanitize1234-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='sanitize1234',
+	py_modules=['c:\\aaa\\sanitize'],
+        version='1.0.1',
+        url="http://www.baidu.com",
+        author="xiaoleil",
+        author_email="xiaoleil@amazon.com",
+	)
