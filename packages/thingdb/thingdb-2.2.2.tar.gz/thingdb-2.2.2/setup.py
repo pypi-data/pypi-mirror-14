db6d2dd34132227ab16deb173952a63c2d759861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:47 +0000

    Add package thingdb/2.2.2/thingdb-2.2.2.tar.gz

---
+++ packages/thingdb/thingdb-2.2.2.tar.gz/thingdb-2.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='thingdb',
+      version='2.2.2',
+      description="Python database using dict's",
+      url='https://github.com/itslukej/thing',
+      author='Luke James',
+      author_email='me@lukej.me',
+      license='GNU',
+      packages=find_packages(),
+      install_requires=['pycrypto'],
+      zip_safe=False)
