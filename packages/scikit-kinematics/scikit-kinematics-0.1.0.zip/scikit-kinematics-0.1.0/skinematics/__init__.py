358555bb82ee2c6413b4a35dbb07bc110594b619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package scikit-kinematics/0.1.0/scikit-kinematics-0.1.0.zip

---
+++ packages/scikit-kinematics/scikit-kinematics-0.1.0.zip/scikit-kinematics-0.1.0/skinematics/__init__.py
@@ -0,0 +1,8 @@
+# from skinematics.stuff import *
+from skinematics.version import __version__
+
+# If you want to use Numpy's testing framerwork, use the following.
+# Tests go under directory tests/, benchmarks under directory benchmarks/
+from numpy.testing import Tester
+test = Tester().test
+bench = Tester().bench
