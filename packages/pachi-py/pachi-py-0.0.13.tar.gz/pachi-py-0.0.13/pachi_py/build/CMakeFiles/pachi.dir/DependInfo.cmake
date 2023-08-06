3c34707802fed166d935d4026a7f38b5323b5502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:10 +0000

    Add package pachi-py/0.0.13/pachi-py-0.0.13.tar.gz

---
+++ packages/pachi-py/pachi-py-0.0.13.tar.gz/pachi-py-0.0.13/pachi_py/build/CMakeFiles/pachi.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/board.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/board.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/chat.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/chat.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/distributed/distributed.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/distributed/distributed.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/distributed/merge.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/distributed/merge.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/distributed/protocol.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/distributed/protocol.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/fbook.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/fbook.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/gtp.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/gtp.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/joseki/base.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/joseki/base.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/joseki/joseki.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/joseki/joseki.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/libpachi_globals.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/libpachi_globals.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/montecarlo/montecarlo.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/montecarlo/montecarlo.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/move.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/move.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/network.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/network.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/ownermap.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/ownermap.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/pattern.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/pattern.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/pattern3.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/pattern3.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/patternplay/patternplay.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/patternplay/patternplay.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/patternprob.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/patternprob.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/patternscan/patternscan.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/patternscan/patternscan.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/patternsp.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/patternsp.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/playout.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/playout.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/playout/light.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/playout/light.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/playout/moggy.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/playout/moggy.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/probdist.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/probdist.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/random.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/random.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/random/random.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/random/random.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/replay/replay.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/replay/replay.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/stone.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/stone.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/t-unit/test.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/t-unit/test.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/1lib.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/1lib.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/2lib.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/2lib.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/ladder.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/ladder.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/nakade.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/nakade.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/nlib.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/nlib.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/selfatari.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/selfatari.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/tactics/util.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/tactics/util.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/timeinfo.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/timeinfo.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/dynkomi.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/dynkomi.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/plugins.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/plugins.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/policy/generic.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/policy/generic.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/policy/ucb1.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/policy/ucb1.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/policy/ucb1amaf.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/policy/ucb1amaf.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/prior.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/prior.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/search.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/search.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/slave.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/slave.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/tree.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/tree.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/uct.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/uct.c.o"
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi/uct/walk.c" "/Users/gdb/rl-gym/pachi-py/pachi_py/build/CMakeFiles/pachi.dir/uct/walk.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/gdb/rl-gym/pachi-py/pachi_py/pachi"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
