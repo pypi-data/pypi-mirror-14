2f9adafcb847555775f298663f8f5ccd162aa8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:50 +0000

    Add package MovieSerieTorrent/1.0.15/MovieSerieTorrent-1.0.15.tar.gz

---
+++ packages/MovieSerieTorrent/MovieSerieTorrent-1.0.15.tar.gz/MovieSerieTorrent-1.0.15/MovieSerieTorrent/__init__.py
@@ -0,0 +1,5 @@
+from MovieSerieTorrent.parser import Parser
+from MovieSerieTorrent.renamer import *
+from MovieSerieTorrent.formatting import Formatting
+from tabulate import tabulate
+from fuzzywuzzy import fuzz
