c88fade97f840209ff37dd6fea3a971eb31f402a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:44 +0000

    Add package eve-peewee/0.0.3/eve-peewee-0.0.3.tar.gz

---
+++ packages/eve-peewee/eve-peewee-0.0.3.tar.gz/eve-peewee-0.0.3/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+install_requires = [
+    'Eve>=0.5',
+    'peewee>=2.6',
+]
+
+setup(
+    name='eve-peewee',
+    version='0.0.3',
+    description="Data layer for Eve powered by peewee.",
+    long_description=long_description,
+    author='Samuli Tuomola',
+    author_email='samuli.tuomola@gmail.com',
+    url='https://github.com/stt/eve-peewee',
+    license='BSD',
+    platforms=["any"],
+    packages=['eve_peewee'],
+    test_suite="eve_peewee.tests",
+    install_requires=install_requires,
+    extras_require={
+        'test': [
+            'pytest',
+            'mock',
+            ]
+        },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
