4bb6c0bc5c48969ef9dd59ee1e924fd7a16821a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package ctext/0.19/ctext-0.19.tar.gz

---
+++ packages/ctext/ctext-0.19.tar.gz/ctext-0.19/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.txt', "r", encoding="utf-8") as file:
+    long_desc = file.read()
+
+setup(
+    name = "ctext",
+    packages = ["ctext"],
+    version = "0.19",
+    description = "Chinese Text Project API wrapper",
+    author = "Donald Sturgeon",
+    author_email = "chinesetextproject@gmail.com",
+    url = "http://ctext.org/tools/api",
+    keywords = ["Chinese", "text", "API", "ctext"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    long_description = long_desc
+)
