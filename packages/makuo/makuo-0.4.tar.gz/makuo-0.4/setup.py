d5f2929a42f291d03ef39f6089e57eb73683f37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:15 +0000

    Add package makuo/0.4/makuo-0.4.tar.gz

---
+++ packages/makuo/makuo-0.4.tar.gz/makuo-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='makuo',
+      version='0.4',
+      description='makuosan client',
+      author='INADA Naoki',
+      author_email='songofacandy@gmail.com',
+      url='https://github.com/KLab/makuo-py',
+      py_modules=['makuo'],
+)
