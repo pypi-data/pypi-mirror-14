65474c065fe82c7a87e0cba968d36890f67cacc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:39 +0000

    Add package saki_10001_nester/1.1.0/saki_10001_nester-1.1.0.zip

---
+++ packages/saki_10001_nester/saki_10001_nester-1.1.0.zip/saki_10001_nester-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name = 'saki_10001_nester',
+        version = '1.1.0',
+        py_modules = ['nester'],
+        author = 'saki_10001',
+        description = 'aaaaaaaa....',
+    )
