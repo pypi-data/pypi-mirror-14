b6234a3c4a651c6db84388c45a8f2ba543a9323f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:33 +0000

    Add package skbold/0.2.1/skbold-0.2.1-py2-none-any.whl

---
+++ packages/skbold/skbold-0.2.1-py2-none-any.whl/skbold/postproc/__init__.py
@@ -0,0 +1,7 @@
+# Author: Lukas Snoek [lukassnoek.github.io]
+# Contact: lukassnoek@gmail.com
+# License: 3 clause BSD
+
+from .extract_roi_info import extract_roi_info
+
+__all__ = ['extract_roi_info']
