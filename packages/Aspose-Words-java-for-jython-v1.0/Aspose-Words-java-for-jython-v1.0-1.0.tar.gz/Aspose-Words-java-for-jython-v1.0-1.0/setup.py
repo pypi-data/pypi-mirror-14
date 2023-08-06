7e0501936f8a0bed25dfaf971b70f5a91de0f69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:00 +0000

    Add package Aspose-Words-java-for-jython-v1.0/1.0/Aspose-Words-java-for-jython-v1.0-1.0.tar.gz

---
+++ packages/Aspose-Words-java-for-jython-v1.0/Aspose-Words-java-for-jython-v1.0-1.0.tar.gz/Aspose-Words-java-for-jython-v1.0-1.0/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'masood.anwer'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Aspose-Words-java-for-jython-v1.0',
+    packages = find_packages(),
+    version = '1.0',
+    description = 'Aspose.Words for Jython allows you to use Aspose.Words for Java API in your Jython applications',
+    author='Fahad Adeel',
+    author_email='words@aspose.com',
+    url='',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
