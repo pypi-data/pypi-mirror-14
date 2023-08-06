ef99db427c1e9a1fe9d119ea032137e5529a8d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:56 +0000

    Add package django-mjml/0.0.1/django_mjml-0.0.1-py2-none-any.whl

---
+++ packages/django-mjml/django_mjml-0.0.1-py2-none-any.whl/mjml/apps.py
@@ -0,0 +1,23 @@
+from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
+from .tools import mjml_render
+
+
+def check_mjml_command():
+    try:
+        html = mjml_render('<mj-body></mj-body>')
+    except RuntimeError, e:
+        raise ImproperlyConfigured(e)
+    if '<html ' not in html:
+        raise ImproperlyConfigured(
+            'mjml command returns wrong result.\n'
+            'Check installation mjml. See https://github.com/mjmlio/mjml#installation'
+        )
+
+
+class MJMLConfig(AppConfig):
+    name = 'mjml'
+    verbose_name = 'Use MJML in Django templates'
+
+    def ready(self):
+        check_mjml_command()
