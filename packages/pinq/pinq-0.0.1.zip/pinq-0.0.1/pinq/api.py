e4955f55a68950cd245883baca17627b8079b514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:22 +0000

    Add package pinq/0.0.1/pinq-0.0.1.zip

---
+++ packages/pinq/pinq-0.0.1.zip/pinq-0.0.1/pinq/api.py
@@ -0,0 +1,32 @@
+"""
+pinq.api
+~~~~~~~~
+
+This module implements the pinq api.
+
+:copyright: (c) 2016 by David Shriver.
+:license: MIT, see LICENSE for more details.
+"""
+
+from collections import Iterable
+from .queryable import Queryable
+
+
+def as_queryable(iterable):
+    """Constructs a queryable object using `iterable` as the base data.
+
+    :param iterable: iterable object to make queryable.
+    :type iterable: Iterable
+    :return: a queryable object with the specified iterable as the underlying data
+    :rtype: :class:`Queryable <Queryable>` object
+    :raise TypeError: if iterable is not an Iterable
+
+    Usage::
+
+      >>> import pinq
+      >>> queryable = pinq.as_queryable(range(100))
+      range(0, 100)
+    """
+    if not isinstance(iterable, Iterable):
+        raise TypeError("Object must be iterable.")
+    return Queryable(iterable)
---
+++ packages/pinq/pinq-0.0.1.zip/pinq-0.0.1/setup.py
@@ -0,0 +1,18 @@
+"""
+pylinq setup script.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='pinq',
+    version='0.0.1',
+    description='LINQ for python.',
+    license='MIT',
+    author='David Shriver',
+    author_email='david.shriver@outlook.com',
+    url='https://github.com/dlshriver/pinq',
+    packages=[
+        'pinq',
+    ]
+)
