3713fbd3560dda4c65880818f8faeee4830e628a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:22 +0000

    Add package djangocms-blogit/0.4.4/djangocms-blogit-0.4.4.tar.gz

---
+++ packages/djangocms-blogit/djangocms-blogit-0.4.4.tar.gz/djangocms-blogit-0.4.4/blogit/urls.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, url
+from django.utils.translation import ugettext_lazy as _
+
+from blogit import settings as bs
+from blogit.views import (
+    CategoryListView, CategoryDetailView,
+    TagListView, TagDetailView,
+    PostYearArchiveView, PostMonthArchiveView, PostDayArchiveView,
+    PostListView, PostDetailView, PostDateDetailView)
+from blogit.feeds import PostRssFeed, PostAtomFeed
+
+
+pats = [
+    url(r'^$', PostListView.as_view(), name='blogit_post_list'),
+
+    url(r'^(?P<year>\d+)/(?P<month>[-\w\d]+)/(?P<day>\d+)/$',
+        PostDayArchiveView.as_view(), name='blogit_post_archive_day'),
+    url(r'^(?P<year>\d+)/(?P<month>[-\w\d]+)/$',
+        PostMonthArchiveView.as_view(), name='blogit_post_archive_month'),
+    url(r'^(?P<year>\d+)/$', PostYearArchiveView.as_view(),
+        name='blogit_post_archive_year'),
+
+    url(_(r'^categories/$'),
+        CategoryListView.as_view(), name='blogit_category_list'),
+    url(_(r'^categories/(?P<slug>[-\w\d]+)/$'),
+        CategoryDetailView.as_view(), name='blogit_category_detail'),
+
+    url(_(r'^tags/$'), TagListView.as_view(), name='blogit_tag_list'),
+    url(_(r'^tags/(?P<slug>[-\w\d]+)/$'),
+        TagDetailView.as_view(), name='blogit_tag_detail'),
+]
+
+if bs.RSS_FEED:
+    pats.extend([
+        url(_(r'^feeds/rss/$'), PostRssFeed()),
+        url(_(r'^feeds/rss/(?P<tag_slug>[-\w]+)/$'), PostRssFeed()),
+    ])
+
+if bs.ATOM_FEED:
+    pats.extend([
+        url(_(r'^feeds/atom/(?P<tag_slug>[-\w]+)/$'), PostAtomFeed()),
+        url(_(r'^feeds/atom/$'), PostAtomFeed()),
+    ])
+
+
+pats.extend([
+    url(r'^(?P<year>\d+)/(?P<month>[-\w\d]+)/(?P<day>\d+)'
+        r'/(?P<slug>[-\w\d]+)/$', PostDateDetailView.as_view(),
+        name='blogit_post_detail_date'),
+    url(r'^(?P<slug>[-\w\d]+)/$', PostDetailView.as_view(),
+        name='blogit_post_detail'),
+])
+
+
+urlpatterns = patterns('', *pats)
---
+++ packages/djangocms-blogit/djangocms-blogit-0.4.4.tar.gz/djangocms-blogit-0.4.4/docs/settings.rst
@@ -0,0 +1,54 @@
+########
+Settings
+########
+
+
+====
+Info
+====
+
+Thease settings are used to render the proper values in feeds:
+
+.. code:: python
+
+    BLOGIT_TITLE = 'Blogit'
+    BLOGIT_DESCRIPTION = 'This is a blog about everything'
+
+
+=====
+Feeds
+=====
+
+Feeds can be accessed on ``/<BLOGIT_FEED_URL>/rss/`` and
+``/<BLOGIT_FEED_URL>/atom/`` by default. Update feeds using thease settings:
+
+.. code:: python
+
+    BLOGIT_RSS_FEED = True
+    BLOGIT_ATOM_FEED = True
+    BLOGIT_FEED_LIMIT = 100
+    BLOGIT_FEED_ITEM_AUTHOR_NAME = None  # When none displays author info
+    BLOGIT_FEED_ITEM_AUTHOR_EMAIL = None  # When none displays author info
+    BLOGIT_FEED_ITEM_DESCRIPTION_FULL = False
+
+
+==========
+Pagination
+==========
+
+Pagination per page:
+
+.. code:: python
+
+    BLOGIT_POSTS_PER_PAGE = 5
+
+========
+Post url
+========
+
+By default post detail url is ``/<post_slug>/``. If you want to have a dated
+url like this ``/<year>/<month>/<day>/<post_slug>/``, change this setting:
+
+.. code:: python
+
+    BLOGIT_POST_DETAIL_DATE_URL = False
