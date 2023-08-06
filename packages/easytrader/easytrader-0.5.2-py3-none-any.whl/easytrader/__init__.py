7176a848c10724a22fb311dba5ca84a52344d3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:37 +0000

    Add package easytrader/0.5.2/easytrader-0.5.2-py3-none-any.whl

---
+++ packages/easytrader/easytrader-0.5.2-py3-none-any.whl/easytrader/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from .api import *
+from .httrader import HTTrader
+from .webtrader import WebTrader
+from .yhtrader import YHTrader
+from .yjbtrader import YJBTrader
+
+__version__ = '0.5.2'
+__author__ = 'shidenggui'
