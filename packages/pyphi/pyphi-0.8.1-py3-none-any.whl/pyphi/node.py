fda36ac89b250e218d367cf62a210d59a85bbdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:31 +0000

    Add package pyphi/0.8.1/pyphi-0.8.1-py3-none-any.whl

---
+++ packages/pyphi/pyphi-0.8.1-py3-none-any.whl/pyphi/node.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# node.py
+
+"""
+Represents a node in a subsystem. Each node has a unique index, its position
+in the network's list of nodes.
+"""
+
+import functools
+
+import numpy as np
+from marbl import Marbl
+
+from . import utils
+
+
+# TODO extend to nonbinary nodes
+@functools.total_ordering
+class Node:
+    """A node in a subsystem.
+
+    Attributes:
+        subsystem (Subsystem):
+            The subsystem the node belongs to.
+        index (int):
+            The node's index in the network.
+        network (Network):
+            The network the node belongs to.
+        label (str):
+            An optional label for the node.
+        state (int):
+            The state of this node.
+    """
+
+    def __init__(self, subsystem, index, label=None):
+        # This node's parent subsystem.
+        self.subsystem = subsystem
+        # This node's index in the list of nodes.
+        self.index = index
+        # This node's parent network.
+        self.network = subsystem.network
+        # Label for display.
+        self.label = label
+        # State of this node.
+        self.state = self.subsystem.state[self.index]
+        # Get indices of the inputs.
+        self._input_indices = utils.get_inputs_from_cm(
+            self.index, subsystem.connectivity_matrix)
+        self._output_indices = utils.get_outputs_from_cm(
+            self.index, subsystem.connectivity_matrix)
+
+        # Generate the node's TPM.
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # We begin by getting the part of the subsystem's TPM that gives just
+        # the state of this node. This part is still indexed by network state,
+        # but its last dimension will be gone, since now there's just a single
+        # scalar value (this node's state) rather than a state-vector for all
+        # the network nodes.
+        tpm_on = self.subsystem.tpm[..., self.index]
+        # Get the TPM that gives the probability of the node being off, rather
+        # than on.
+        tpm_off = 1 - tpm_on
+
+        # Marginalize-out non-input subsystem nodes and get dimension labels.
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # This list will hold the indices of the nodes that correspond to
+        # non-singleton dimensions of this node's on-TPM. It maps any subsystem
+        # node index to the corresponding dimension of this node's TPM with
+        # singleton dimensions removed. We need this for creating this node's
+        # Marbl.
+        self._dimension_labels = []
+        # This is the counter that will provide the actual labels.
+        current_non_singleton_dim_index = 0
+        # Iterate over all the nodes in the network, since we need to keep
+        # track of all singleton dimensions.
+        for i in range(self.network.size):
+            # Input nodes that are within the subsystem will correspond to a
+            # dimension in this node's squeezed TPM, so we map it to the index
+            # of the corresponding dimension and increment the corresponding
+            # index for the next one.
+            if i in self._input_indices and i in self.subsystem.node_indices:
+                self._dimension_labels.append(current_non_singleton_dim_index)
+                current_non_singleton_dim_index += 1
+            # Boundary nodes and non-input nodes have already been conditioned
+            # and marginalized-out, so their dimension in the TPM will be a
+            # singleton and will be squeezed out when creating a Marbl. So, we
+            # don't give them a dimension label.
+            else:
+                self._dimension_labels.append(None)
+            # TODO extend to nonbinary nodes
+            # Marginalize out non-input nodes that are in the subsystem, since
+            # the external nodes have already been dealt with as boundary
+            # conditions in the subsystem's TPM.
+            if (i not in self._input_indices
+                    and i in self.subsystem.node_indices):
+                tpm_on = tpm_on.sum(i, keepdims=True) / 2
+                tpm_off = tpm_off.sum(i, keepdims=True) / 2
+
+        # Combine the on- and off-TPM.
+        self.tpm = np.array([tpm_off, tpm_on])
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # Make the TPM immutable (for hashing).
+        self.tpm.flags.writeable = False
+
+        # Only compute the hash once.
+        self._hash = hash((self.index, self.subsystem))
+
+        # Deferred properties
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # ``inputs``, ``outputs``, and ``marbl`` must be properties because at
+        # the time of node creation, the subsystem doesn't have a list of Node
+        # objects yet, only a size (and thus a range of node indices). So, we
+        # defer construction until the properties are needed.
+        self._inputs = None
+        self._outputs = None
+        self._marbl = None
+        self._raw_marbl = None
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def get_marbl(self, normalize=True):
+        """Generate a Marbl for this node TPM."""
+        # We take only the part of the TPM giving the probability the node is
+        # on.
+        # TODO extend to nonbinary nodes
+        augmented_child_tpms = [
+            [child._dimension_labels[self.index],
+             getattr(child, 'tpm')[1].squeeze()] for child in self.outputs
+        ]
+        marbl = Marbl(getattr(self, 'tpm')[1], augmented_child_tpms,
+                      normalize=normalize)
+        return marbl
+
+    @property
+    def input_indices(self):
+        """The indices of nodes which connect to this node."""
+        return self._input_indices
+
+    @property
+    def output_indices(self):
+        """The indices of nodes that this node connects to."""
+        return self._output_indices
+
+    @property
+    def inputs(self):
+        """The set of nodes with connections to this node."""
+        if self._inputs is not None:
+            return self._inputs
+        else:
+            self._inputs = [node for node in self.subsystem.nodes if
+                            node.index in self._input_indices]
+            return self._inputs
+
+    @property
+    def outputs(self):
+        """The set of nodes this node has connections to."""
+        if self._outputs is not None:
+            return self._outputs
+        else:
+            self._outputs = [node for node in self.subsystem.nodes if
+                             node.index in self._output_indices]
+            return self._outputs
+
+    @property
+    def marbl(self):
+        """The normalized representation of this node's Markov blanket,
+        conditioned on the fixed state of boundary-condition nodes in the
+        current timestep."""
+        if self._marbl is not None:
+            return self._marbl
+        else:
+            self._marbl = self.get_marbl()
+            return self._marbl
+
+    @property
+    def raw_marbl(self):
+        """The un-normalized representation of this node's Markov blanket,
+        conditioned on the fixed state of boundary-condition nodes in the
+        current timestep."""
+        if self._raw_marbl is not None:
+            return self._raw_marbl
+        else:
+            self._raw_marbl = self.get_marbl(normalize=False)
+            return self._raw_marbl
+
+    def __repr__(self):
+        return (self.label if self.label is not None
+                else 'n' + str(self.index))
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        """Return whether this node equals the other object.
+
+        Two nodes are equal if they belong to the same subsystem and have the
+        same index (their TPMs must be the same in that case, so this method
+        doesn't need to check TPM equality).
+
+        Labels are for display only, so two equal nodes may have different
+        labels.
+        """
+        return (self.index == other.index and
+                self.subsystem == other.subsystem)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        return self.index < other.index
+
+    def __hash__(self):
+        return self._hash
+
+    # TODO do we need more than the index?
+    def to_json(self):
+        return self.index
---
+++ packages/pyphi/pyphi-0.8.1-py3-none-any.whl/pyphi/validate.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# validate.py
+
+"""
+Methods for validating common types of input.
+"""
+
+import numpy as np
+
+from . import config, constants, convert
+from .constants import EPSILON
+
+
+class StateUnreachableError(ValueError):
+    """Raised when the current state cannot be reached from any past state."""
+
+    def __init__(self, state, message):
+        self.state = state
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+def direction(direction):
+    """Validate that the given direction is one of the allowed constants."""
+    if direction not in constants.DIRECTIONS:
+        raise ValueError('Direction must be one of '
+                         '{}.'.format(constants.DIRECTIONS))
+    return True
+
+
+def tpm(tpm):
+    """Validate a TPM."""
+    see_tpm_docs = ('See documentation for pyphi.Network for more information '
+                    'TPM formats.')
+    # Cast to np.array.
+    tpm = np.array(tpm)
+    # Get the number of nodes from the state-by-node TPM.
+    N = tpm.shape[-1]
+    if tpm.ndim == 2:
+        if not ((tpm.shape[0] == 2**N and tpm.shape[1] == N) or
+                (tpm.shape[0] == tpm.shape[1])):
+            raise ValueError(
+                'Invalid shape for 2-D TPM: {}\nFor a state-by-node TPM, '
+                'there must be ' '2^N rows and N columns, where N is the '
+                'number of nodes. State-by-state TPM must be square. '
+                '{}'.format(tpm.shape, see_tpm_docs))
+        if (tpm.shape[0] == tpm.shape[1]
+                and not conditionally_independent(tpm)):
+            raise ValueError('TPM is not conditionally independent. See the '
+                             'conditional independence example in the '
+                             'documentation for more information.')
+    elif tpm.ndim == (N + 1):
+        if not (tpm.shape == tuple([2] * N + [N])):
+            raise ValueError(
+                'Invalid shape for N-D state-by-node TPM: {}\nThe shape '
+                'should be {} for {} nodes.'.format(
+                    tpm.shape, ([2] * N) + [N], N, see_tpm_docs))
+    else:
+        raise ValueError(
+            'Invalid state-by-node TPM: TPM must be in either 2-D or N-D '
+            'form. {}'.format(see_tpm_docs))
+    return True
+
+
+def conditionally_independent(tpm):
+    """Validate that the TPM is conditionally independent."""
+    tpm = np.array(tpm)
+    there_and_back_again = convert.state_by_node2state_by_state(
+        convert.state_by_state2state_by_node(tpm))
+    return np.all((tpm - there_and_back_again) < EPSILON)
+
+
+def connectivity_matrix(cm):
+    """Validate the given connectivity matrix."""
+    # Special case for empty matrices.
+    if cm.size == 0:
+        return True
+    if (cm.ndim != 2):
+        raise ValueError("Connectivity matrix must be 2-dimensional.")
+    if cm.shape[0] != cm.shape[1]:
+        raise ValueError("Connectivity matrix must be square.")
+    if not np.all(np.logical_or(cm == 1, cm == 0)):
+        raise ValueError("Connectivity matrix must contain only binary "
+                         "values.")
+    return True
+
+
+# TODO test
+def perturb_vector(pv, size):
+    """Validate a network's pertubation vector."""
+    if pv.size != size:
+        raise ValueError("Perturbation vector must have one element per node.")
+    if np.any(pv > 1) or np.any(pv < 0):
+        raise ValueError("Perturbation vector elements must be probabilities, "
+                         "between 0 and 1.")
+    return True
+
+
+def network(n):
+    """Validate a |Network|.
+
+    Checks the TPM, connectivity matrix, and perturbation vector.
+    """
+    tpm(n.tpm)
+    connectivity_matrix(n.connectivity_matrix)
+    perturb_vector(n.perturb_vector, n.size)
+    if n.connectivity_matrix.shape[0] != n.size:
+        raise ValueError("Connectivity matrix must be NxN, where N is the "
+                         "number of nodes in the network.")
+    return True
+
+
+def node_states(state):
+    """Check that the state contains only zeros and ones."""
+    if not all([n in (0, 1) for n in state]):
+        raise ValueError(
+            'Invalid state: states must consist of only zeros and ones.')
+
+
+def state_length(state, size):
+    """Check that the state is the given size."""
+    if len(state) != size:
+        raise ValueError('Invalid state: there must be one entry per '
+                         'node in the network; this state has {} entries, but '
+                         'there are {} nodes.'.format(len(state), size))
+    return True
+
+
+def state_reachable(subsystem):
+    """Return whether a state can be reached according to the network's TPM.
+
+    If ``constrained_nodes`` is provided, then nodes not in
+    ``constrained_nodes`` will be left free (their state will not considered
+    restricted by the TPM). Otherwise, any nodes without inputs will be left
+    free.
+    """
+    # If there is a row `r` in the TPM such that all entries of `r - state` are
+    # between -1 and 1, then the given state has a nonzero probability of being
+    # reached from some state.
+    # First we take the submatrix of the conditioned TPM that corresponds to
+    # the nodes that are actually in the subsystem...
+    tpm = subsystem.tpm[..., subsystem.node_indices]
+    # Then we do the subtraction and test.
+    test = tpm - np.array(subsystem.state)[list(subsystem.node_indices)]
+    if not np.any(np.logical_and(-1 < test, test < 1).all(-1)):
+        raise StateUnreachableError(
+            subsystem.state, 'This state cannot be reached according to the '
+                             'given TPM.')
+
+
+def cut(cut, node_indices):
+    """Check that the cut is for only the given nodes."""
+    if set(cut[0] + cut[1]) != set(node_indices):
+        raise ValueError('{} nodes are not equal to subsystem nodes '
+                         '{}'.format(cut, node_indices))
+
+
+def subsystem(s):
+    """Validate a |Subsystem|.
+
+    Checks its state and cut.
+    """
+    node_states(s.state)
+    cut(s.cut, s.node_indices)
+    if config.VALIDATE_SUBSYSTEM_STATES:
+        state_reachable(s)
+    return True
