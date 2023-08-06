c9b58b10e250b2b60cf517cdd6e79ddc4297d91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:12 +0000

    Add package endless/0.1/endless-0.1.tar.gz

---
+++ packages/endless/endless-0.1.tar.gz/endless-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='endless',
+    author='magniff',
+    description='Simple way to enlarge your pinus^W call stack.',
+    license='MIT',
+    url='https://github.com/magniff/endless',
+    version='0.1',
+    py_modules=['endless'],
+    zip_safe=False,
+)
