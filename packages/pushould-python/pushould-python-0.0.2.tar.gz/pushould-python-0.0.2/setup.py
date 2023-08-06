a8a9862af0bc72a95c58fcc3c8dafbd249009545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package pushould-python/0.0.2/pushould-python-0.0.2.tar.gz

---
+++ packages/pushould-python/pushould-python-0.0.2.tar.gz/pushould-python-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pushould-python',
+      version='0.0.2',
+      description='RealTime without pain.',
+      url='https://pushould.com',
+      author='Yu Hoshino',
+      author_email='yhoshino11@gmail.com',
+      license='MIT',
+      packages=['pushould'],
+      install_requires=['requests==2.9.1', 'nose==1.3.7'],
+      tests_require=['nose', 'httpretty'],
+      zip_safe=False)
