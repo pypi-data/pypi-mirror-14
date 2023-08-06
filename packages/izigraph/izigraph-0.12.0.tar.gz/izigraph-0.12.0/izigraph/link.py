05c038029f4a1a6e39100999c930ec9fb2ab93b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:07 +0000

    Add package izigraph/0.12.0/izigraph-0.12.0.tar.gz

---
+++ packages/izigraph/izigraph-0.12.0.tar.gz/izigraph-0.12.0/izigraph/link.py
@@ -0,0 +1,33 @@
+import izigraph
+
+
+class Link:
+    def __init__(self, source, destination, weight=1):
+        if not isinstance(weight, (int, float)):
+            raise ValueError
+        if not isinstance(source, izigraph.Node) or not isinstance(destination, izigraph.Node):
+            raise ValueError
+        self.__source = source
+        self.__destination = destination
+        self.__weight = weight
+
+    def source(self):
+        """
+        Return the source node
+        :return:izigraph.node.Node
+        """
+        return self.__source
+
+    def destination(self):
+        """
+        Return the destination node
+        :return:izigraph.node.Node
+        """
+        return self.__destination
+
+    def weight(self):
+        """
+        Returns the weight of the Link
+        :return:izigraph.node.Node
+        """
+        return self.__weight
