fdec5ee75bae38e9ef1dc6368238c6077f2bcf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:29 +0000

    Add package django-bit-category/0.4/django-bit-category-0.4.tar.gz

---
+++ packages/django-bit-category/django-bit-category-0.4.tar.gz/django-bit-category-0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+APP_NAME = "django-bit-category"
+VERSION = 0.4
+
+setup(
+    name=APP_NAME,
+    version=VERSION,
+    description="Django category app which uses tree-like structure using bitwise primary key.",
+
+    packages=[
+        'bitcategory',
+    ],
+    package_data={
+        "": ["static/bitcategory/*"],
+    },
+    include_package_data=True,
+
+    author='Tomas Peterka',
+    author_email='prestizni@gmail.com',
+    license="GPL v3",
+    url='http://pypi.python.org/pypi/{0}/'.format(APP_NAME),
+    keywords="django category hierarchy",
+
+    install_requires=[
+        "django>=1.5",
+    ],
+)
