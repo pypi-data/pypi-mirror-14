1b1a0bd4e2be388b8d3c0ee9b37a8e890f398cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:37 +0000

    Add package no_limit_nester/1.2.0/no_limit_nester-1.2.0.zip

---
+++ packages/no_limit_nester/no_limit_nester-1.2.0.zip/no_limit_nester-1.2.0/no_limit_nester.py
@@ -0,0 +1,10 @@
+""" This module nester.py() contains function to act on nested lists """
+
+def print_nested_list(input_list,level=0):
+	for item in input_list:
+		if isinstance(item,list):
+			print_nested_list(item,level+1)
+		else:
+			for i in range(level):
+				print('\t',end='')
+			print (item)
