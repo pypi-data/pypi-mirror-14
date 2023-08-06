7047f013098eab3ca8b12411807918b020c0562b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:49 +0000

    Add package jhlee25_kNN/1.1.0/jhlee25_kNN-1.1.0.tar.gz

---
+++ packages/jhlee25_kNN/jhlee25_kNN-1.1.0.tar.gz/jhlee25_kNN-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name='jhlee25_kNN',
+		version='1.1.0',
+		py_modules=['jhlee25_kNN'],
+		author='jaehyunlee',
+		author_email='jaehyunlee25@gmail.com',
+		url='',
+		description='test for distribution'
+	 )
