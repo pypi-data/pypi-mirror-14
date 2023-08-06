d6ae4c6f1b052c70867688d4b1d153929a5bcc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:23 +0000

    Add package librarypaste/3.2/librarypaste-3.2.tar.gz

---
+++ packages/librarypaste/librarypaste-3.2.tar.gz/librarypaste-3.2/setup.py
@@ -0,0 +1,52 @@
+import setuptools
+
+with open('README.txt') as readme_stream:
+    long_description = readme_stream.read()
+
+setup_params = dict(
+    name='librarypaste',
+    use_scm_version=True,
+    author='YouGov, Plc.',
+    author_email='open-source@yougov.com',
+    url='https://bitbucket.org/yougov/librarypaste/',
+    description='A simple pastebin implementation in Python',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'librarypaste=librarypaste.launch:main',
+        ],
+        'pmxbot_handlers': [
+            'librarypaste = librarypaste.pmxbot',
+        ],
+    },
+    install_requires=[
+        'pygments',
+        'genshi',
+        'cherrypy',
+        'PyYAML',
+        'requests',
+    ],
+    setup_requires=[
+        'setuptools_scm',
+        'pytest-runner>=2.1',
+    ],
+    tests_require=[
+        'pytest',
+        'pymongo>=3',
+        'jaraco.test',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
