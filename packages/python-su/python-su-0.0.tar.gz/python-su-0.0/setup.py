2021d4ee3c40d9150cc4d9f3090af89a2d5d0500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:41 +0000

    Add package python-su/0.0/python-su-0.0.tar.gz

---
+++ packages/python-su/python-su-0.0.tar.gz/python-su-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    author_email='contact@ionelmc.ro',
+    description='Simple Python-based setuid+setgid+setgroups+exec. A port of https://github.com/tianon/gosu',
+    install_requires=['pysu'],
+    long_description='''Use `pysu <https://pypi.python.org/pypi/pysu/>`_ instead.''',
+    maintainer='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    maintainer_email='contact@ionelmc.ro',
+    name='python-su',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/pysu/',
+    version="0.0",
+    zip_safe=False,
+)
