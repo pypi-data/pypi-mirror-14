67186954097fa54d91b120ec55b56c99de225742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package tahoe-lafs/1.11.0/tahoe-lafs-1.11.0.tar.bz2

---
+++ packages/tahoe-lafs/tahoe-lafs-1.11.0.tar.bz2/tahoe-lafs-1.11.0/src/allmydata/_version.py
@@ -0,0 +1,9 @@
+
+# This _version.py is generated from git metadata by the tahoe setup.py.
+
+__pkgname__ = "tahoe-lafs"
+real_version = "1.11.0"
+full_version = "04a3e7993f70ac87c208d61f4387a59f5f419367"
+branch = "master"
+verstr = "1.11.0"
+__version__ = verstr
