804346fd15a63384ca6dbc5265f1635ff26d2031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:36 +0000

    Add package nester_zb/1.0.0/nester_zb-1.0.0.zip

---
+++ packages/nester_zb/nester_zb-1.0.0.zip/nester_zb-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_zb',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'onlytest',
+    author_email =  'zb3215286@hotmail.com',
+    url  = '',
+    description = 'a simpler python',
+    )
