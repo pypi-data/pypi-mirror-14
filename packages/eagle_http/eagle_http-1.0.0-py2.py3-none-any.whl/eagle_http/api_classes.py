5fa562f1b2e4048be4e2317bd14fc5c1ae98bf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package eagle_http/1.0.0/eagle_http-1.0.0-py2.py3-none-any.whl

---
+++ packages/eagle_http/eagle_http-1.0.0-py2.py3-none-any.whl/eagle_http/api_classes.py
@@ -0,0 +1,12 @@
+from base_class import *
+from base_class import BaseCluster as MessageCluster
+from base_class import BaseCluster as TimeCluster
+from base_class import BaseCluster as InstantaneousDemand
+from base_class import BaseCluster as NetworkInfo
+from base_class import BaseCluster as PriceCluster
+from base_class import BaseCluster as DeviceInfo
+from base_class import BaseCluster as MessageCluster
+from base_class import BaseCluster as CurrentSummation
+from base_class import BaseCluster as ScheduleInfo
+from base_class import BaseCluster as BlockPriceDetail
+from base_class import BaseCluster as HistoryData
