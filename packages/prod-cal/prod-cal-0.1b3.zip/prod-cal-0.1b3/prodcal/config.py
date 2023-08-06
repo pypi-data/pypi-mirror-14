544652a0cfae213475da065fb2b541452aaa6e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package prod-cal/0.1b3/prod-cal-0.1b3.zip

---
+++ packages/prod-cal/prod-cal-0.1b3.zip/prod-cal-0.1b3/prodcal/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DEFAULT_LOCALE = "RU"
+
+LOCALE_SUPPORTING = {"RU": "Russian Federation",
+                     "UA": "Ukraine",
+                     "KZ": "Kazakhstan",
+                     }
