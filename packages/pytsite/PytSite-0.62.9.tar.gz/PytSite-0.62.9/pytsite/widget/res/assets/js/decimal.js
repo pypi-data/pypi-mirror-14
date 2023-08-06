c3da9dfcf0f9b761661bdcb80de68d525de2c95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:17 +0000

    Add package pytsite/0.62.9/PytSite-0.62.9.tar.gz

---
+++ packages/pytsite/PytSite-0.62.9.tar.gz/PytSite-0.62.9/pytsite/widget/res/assets/js/decimal.js
@@ -0,0 +1,10 @@
+$(window).on('pytsite.widget.init:pytsite.widget._input.Decimal', function (e, widget) {
+    var options = {
+        allowMinus: false
+    };
+
+    if (widget.em.data('allowMinus'))
+        options.allowMinus = true;
+
+    widget.em.find('input[type=text],input[type=tel],input[type=number]').inputmask('decimal', options);
+});
