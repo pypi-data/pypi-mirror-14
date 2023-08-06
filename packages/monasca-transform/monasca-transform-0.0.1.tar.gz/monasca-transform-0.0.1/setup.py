0ade21c5a6209e84ac8ab5539b067993a8531c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:55 +0000

    Add package monasca-transform/0.0.1/monasca-transform-0.0.1.tar.gz

---
+++ packages/monasca-transform/monasca-transform-0.0.1.tar.gz/monasca-transform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'monasca-transform',
+  packages = ['monasca_transform'],
+  version = '0.0.1',
+  description = 'Monasca Transform and Aggregation Engine',
+  author = 'OpenStack',
+  author_email = 'roland.hochmuth@hpe.com',
+  keywords = ['monitoring'],
+  classifiers = [],
+)
