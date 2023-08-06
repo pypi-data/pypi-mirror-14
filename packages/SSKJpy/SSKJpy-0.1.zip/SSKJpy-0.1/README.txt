54a3c53a3371523051bd391346b918181db767af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:04 +0000

    Add package SSKJpy/0.1/SSKJpy-0.1.zip

---
+++ packages/SSKJpy/SSKJpy-0.1.zip/SSKJpy-0.1/README.txt
@@ -0,0 +1,9 @@
+===========
+SSKJpy
+===========
+
+A Slovenian dictionary parser written in python
+
+Uses http://bos.zrc-sazu.si/sskj.html (official SSKJ) website.
+
+1. Use and examples can be found at https://github.com/DefaltSimon/SSKJpy/wiki
---
+++ packages/SSKJpy/SSKJpy-0.1.zip/SSKJpy-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='SSKJpy',
+    author="DefaltSimon",
+    version='0.1',
+    license='MIT',
+    packages=["sskjpy"],
+    description="A Slovenian dictionary parser written in python",
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.9.1",
+        "beautifulsoup4 >= 4.4.1",
+    ],
+)
