b30002f9a9c9968fb989ff76803ef1cdd7377497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:04 +0000

    Add package coyote-framework/0.1.0/coyote-framework-0.1.0.tar.gz

---
+++ packages/coyote-framework/coyote-framework-0.1.0.tar.gz/coyote-framework-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+__author__ = 'matt'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='coyote-framework',
+    version='0.1.0',
+    author='Shapeways',
+    author_email='api@shapeways.com',
+    packages=find_packages(),
+    url = 'https://github.com/Shapeways/coyote_framework',
+    download_url = 'https://github.com/Shapeways/coyote_framework/tarball/0.1',
+    description='Python-based testing framework',
+)
