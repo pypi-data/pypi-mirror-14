3a8a93a8b8b74797738a5e4e44141b252efd25a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:46 +0000

    Add package epz/1.2/epz-1.2.zip

---
+++ packages/epz/epz-1.2.zip/epz-1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='epz',
+    version='1.2',
+    packages=['epz'],
+    url='https://github.com/massimovassalli/epz',
+    download_url='https://github.com/massimovassalli/epz/releases/tag/v1.2',
+    keywords=['zmq','pyzmq','zeromq'],
+    license='',
+    author='Massimo Vassalli',
+    author_email='massimo.vassalli@nanobioscience.eu',
+    description=''
+)
