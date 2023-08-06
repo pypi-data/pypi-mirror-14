87effac391396f8e6e653a4ef50ab77031e1ba05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:47 +0000

    Add package sentry/8.2.5/sentry-8.2.5-py27-none-any.whl

---
+++ packages/sentry/sentry-8.2.5-py27-none-any.whl/sentry/static/sentry/app/views/organizationTeams/index.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import Reflux from 'reflux';
+
+import {t} from '../../locale';
+import ApiMixin from '../../mixins/apiMixin';
+import OrganizationHomeContainer from '../../components/organizations/homeContainer';
+import OrganizationState from '../../mixins/organizationState';
+import TeamStore from '../../stores/teamStore';
+import TooltipMixin from '../../mixins/tooltip';
+import {sortArray} from '../../utils';
+
+import ExpandedTeamList from './expandedTeamList';
+import AllTeamsList from './allTeamsList';
+import OrganizationStatOverview from './organizationStatOverview';
+
+const OrganizationTeams = React.createClass({
+  mixins: [
+    ApiMixin,
+    OrganizationState,
+    Reflux.listenTo(TeamStore, 'onTeamListChange'),
+    TooltipMixin({
+      selector: '.tip'
+    })
+  ],
+
+  getInitialState() {
+    return {
+      activeNav: 'your-teams',
+      teamList: sortArray(TeamStore.getAll(), function(o) {
+        return o.name;
+      }),
+      projectStats: {},
+    };
+  },
+
+  componentWillMount() {
+    this.fetchStats();
+  },
+
+  // TODO(dcramer): handle updating project stats when items change
+  fetchStats() {
+    this.api.request(this.getOrganizationStatsEndpoint(), {
+      query: {
+        since: new Date().getTime() / 1000 - 3600 * 24,
+        stat: 'received',
+        group: 'project'
+      },
+      success: (data) => {
+        this.setState({
+          projectStats: data
+        });
+      }
+    });
+  },
+
+  getOrganizationStatsEndpoint() {
+    let params = this.props.params;
+    return '/organizations/' + params.orgId + '/stats/';
+  },
+
+  onTeamListChange() {
+    let newTeamList = TeamStore.getAll();
+
+    this.setState({
+      teamList: sortArray(newTeamList, function(o) {
+        return o.name;
+      })
+    });
+
+    this.fetchStats();
+  },
+
+  toggleTeams(nav) {
+    this.setState({
+      activeNav: nav
+    });
+  },
+
+  render() {
+    if (!this.context.organization)
+      return null;
+
+    let access = this.getAccess();
+    let features = this.getFeatures();
+    let org = this.getOrganization();
+
+    let activeNav = this.state.activeNav;
+    let allTeams = this.state.teamList;
+    let activeTeams = this.state.teamList.filter((team) => team.isMember);
+
+    return (
+      <OrganizationHomeContainer>
+        <div className="row">
+          <div className="col-md-9">
+            <div className="team-list">
+              <ul className="nav nav-tabs border-bottom">
+                <li className={activeNav === 'your-teams' && 'active'}>
+                  <a onClick={this.toggleTeams.bind(this, 'your-teams')}>{t('Your Teams')}</a>
+                </li>
+                <li className={activeNav === 'all-teams' && 'active'}>
+                  <a onClick={this.toggleTeams.bind(this, 'all-teams')}>{t('All Teams')} <span className="badge badge-soft">{allTeams.length}</span></a>
+                </li>
+              </ul>
+              {activeNav == 'your-teams' ?
+                <ExpandedTeamList
+                    organization={org} teamList={activeTeams}
+                    projectStats={this.state.projectStats}
+                    hasTeams={allTeams.length !== 0}
+                    access={access}
+                    showAllTeams={this.toggleTeams.bind(this, 'all-teams')} />
+              :
+                <AllTeamsList
+                  organization={org} teamList={allTeams}
+                  access={access}
+                  openMembership={features.has('open-membership') || access.has('org:write')} />
+              }
+            </div>
+          </div>
+          <OrganizationStatOverview orgId={this.props.params.orgId} className="col-md-3 stats-column" />
+        </div>
+      </OrganizationHomeContainer>
+    );
+  }
+});
+
+export default OrganizationTeams;
---
+++ packages/sentry/sentry-8.2.5-py27-none-any.whl/sentry/web/frontend/generic.py
@@ -0,0 +1,48 @@
+"""
+sentry.web.frontend.generic
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import
+
+from django.views.generic import TemplateView as BaseTemplateView
+
+from sentry.web.helpers import render_to_response
+
+
+def static_media(request, **kwargs):
+    """
+    Serve static files below a given point in the directory structure.
+    """
+    from django.contrib.staticfiles.views import serve
+
+    module = kwargs.get('module')
+    path = kwargs.get('path', '')
+    version = kwargs.get('version')
+
+    if module:
+        path = '%s/%s' % (module, path)
+
+    response = serve(request, path, insecure=True)
+
+    # We need CORS for font files
+    if path.endswith(('.js', '.ttf', '.ttc', '.otf', '.eot', '.woff', '.woff2')):
+        response['Access-Control-Allow-Origin'] = '*'
+
+    # If we have a version, we can cache it FOREVER
+    if version is not None:
+        response['Cache-Control'] = 'max-age=315360000'
+
+    return response
+
+
+class TemplateView(BaseTemplateView):
+    def render_to_response(self, context, **response_kwargs):
+        return render_to_response(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            **response_kwargs
+        )
