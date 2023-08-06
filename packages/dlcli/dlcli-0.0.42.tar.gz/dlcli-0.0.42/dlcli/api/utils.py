28062357c4ff3f3833db9ccccba5d6c031d3169b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package dlcli/0.0.42/dlcli-0.0.42.tar.gz

---
+++ packages/dlcli/dlcli-0.0.42.tar.gz/dlcli-0.0.42/dlcli/api/utils.py
@@ -0,0 +1,247 @@
+import os
+import glob
+import logging
+import yaml
+import json
+import accounts
+import agents
+import plugins
+import dashboards
+import rules
+import links
+import orgs
+import click
+from terminaltables import SingleTable
+from termcolor import colored
+
+logger = logging.getLogger(__name__)
+
+
+def print_command_output(command_data):
+    for row in command_data:
+        if row[1] == 0:
+            print colored(str(row[0]), 'green')
+        else:
+            print colored(str(row[0]), 'red')
+        print row[2]
+
+
+def print_run_table(table_data):
+    table = SingleTable(table_data)
+    table.justify_columns = {0: 'left', 1: 'center', 2: 'left'}
+    table.inner_heading_row_border = False
+    table.inner_column_border = False
+    table.outer_border = False
+    max_width = table.column_max_width(2)
+    for index, row in enumerate(table_data):
+        table.table_data[index][2] = str(row[2][0:max_width].splitlines()[0])
+        if row[1] == 0:
+            table.table_data[index][1] = colored(str(row[1]), 'green')
+        elif row[1] == 1:
+            table.table_data[index][2] = colored(str(row[1]), 'yellow')
+        elif row[1] == 3:
+            table.table_data[index][2] = colored(str(row[1]), 'grey')
+        else:
+            table.table_data[index][2] = colored(str(row[1]), 'red')
+    print table.table
+
+
+def save_setting(ctx, setting):
+    settings_file = ctx.parent.parent.params['settingsfile']
+    try:
+        stream = open(settings_file, 'r')
+        data = yaml.load(stream)
+    except IOError:
+        data = {}
+    data.update({k: v for k, v in setting.iteritems() if v})
+    with open(settings_file, 'w') as yaml_file:
+        yaml_file.write(yaml.safe_dump(data,
+                                       default_flow_style=False,
+                                       explicit_start=True))
+
+
+def build_api_url(ctx, endpoint='', orglevel=False, accountlevel=False):
+    url = ctx.parent.parent.params['url']
+    org = ctx.parent.parent.params['org']
+    account = ctx.parent.parent.params['account']
+    if orglevel:
+        return url + '/orgs'
+    if accountlevel:
+        return url + '/orgs/' + org + '/accounts'
+    return url + '/orgs/' + org + '/accounts/' + account + '/' + endpoint
+
+
+def create_dir(path, directory):
+    new_directory = os.path.join(path, directory)
+    if not os.path.exists(new_directory):
+        os.makedirs(new_directory)
+    return new_directory
+
+
+def backup_account(ctx, account):
+    #  create directory structure
+    backupdir = ctx.parent.parent.params['backupdir']
+    org = ctx.parent.parent.params['org']
+    ctx.parent.parent.params['account'] = account
+
+    backup_dir = create_dir(os.getcwd(), backupdir)
+    org_dir = create_dir(backup_dir, org)
+    account_dir = create_dir(org_dir, account)
+
+    # backup agents
+    agent_dir = create_dir(account_dir, 'agents')
+    for agent_json in agents.Agents(ctx).get_agents():
+        agent_path = os.path.join(agent_dir, str(agent_json['name']) + '.json')
+        remove_keys = ['presence_state', 'created', 'modified', 'heartbeat']
+        for key in remove_keys:
+            if key in agent_json:
+                del agent_json[key]
+        with open(agent_path, 'w') as f:
+            f.write(json.dumps(agent_json, indent=4))
+
+    # backup dashboards
+    dashboard_dir = create_dir(account_dir, 'dashboards')
+    for d in dashboards.Dashboards(ctx).get_dashboards():
+        dashboard_path = os.path.join(dashboard_dir, str(d['name']) + '.yaml')
+        with open(dashboard_path, 'w') as f:
+            f.write(yaml.safe_dump(d,
+                                   default_flow_style=False,
+                                   explicit_start=True))
+
+    # backup plugins
+    plugin_dir = create_dir(account_dir, 'plugins')
+    for p in plugins.Plugins(ctx).get_plugins():
+        plugin_path = os.path.join(plugin_dir,
+                                   str(p['name']) + '.' + str(p['extension']))
+        with open(plugin_path, 'w') as f:
+            f.write(plugins.Plugins(ctx).export_plugin(p['name']))
+
+    # backup rules
+    rule_dir = create_dir(account_dir, 'rules')
+    for r in rules.Rules(ctx).get_rules():
+        rule_path = os.path.join(rule_dir, str(r['name']) + '.yaml')
+        with open(rule_path, 'w') as f:
+            rule_content = yaml.safe_load(rules.Rules(ctx).export_rule(r[
+                'id']))
+            if rule_content['actions']:
+                action_count = len(rule_content['actions'])
+                for i in range(action_count):
+                    try:
+                        del rule_content['actions'][i]['details']['status']
+                    except KeyError:
+                        continue
+            f.write(yaml.safe_dump(rule_content,
+                                   default_flow_style=False,
+                                   explicit_start=True))
+
+    # backup links
+    link_dir = create_dir(account_dir, 'links')
+    for l in links.Links(ctx).get_links():
+        link_path = os.path.join(link_dir, l['id'] + '.json')
+        link_json = links.Links(ctx).export_link(l['id'])
+        with open(link_path, 'w') as f:
+            f.write(json.dumps(link_json, indent=4))
+
+
+def backup_org(ctx, org):
+    ctx.parent.parent.params['org'] = org
+    _accounts = accounts.Accounts(ctx)
+    for a in _accounts.get_accounts():
+        backup_account(ctx, a['name'])
+
+
+def read_file_content(file_path):
+    try:
+        with open(file_path) as f:
+            return f.read()
+    except IOError as exc:
+        if exc.errno != os.errno.EISDIR:
+            raise
+
+
+def restore_account(ctx, account):
+    ctx.parent.parent.params['account'] = account
+    backup_dir = ctx.parent.parent.params['backupdir']
+    org_dir = ctx.parent.parent.params['org']
+    account_dir = ctx.parent.parent.params['account']
+
+    agents_dir = os.path.join(backup_dir, org_dir, account_dir, 'agents')
+    dashboards_dir = os.path.join(backup_dir, org_dir, account_dir,
+                                  'dashboards')
+    plugins_dir = os.path.join(backup_dir, org_dir, account_dir, 'plugins')
+    rules_dir = os.path.join(backup_dir, org_dir, account_dir, 'rules')
+    links_dir = os.path.join(backup_dir, org_dir, account_dir, 'links')
+
+    # restore agents
+    agent_files = glob.glob(agents_dir + '/*.json')
+    for agent_path in agent_files:
+        agent_json = json.loads(read_file_content(agent_path))
+        payload = {
+            "fingerprint": agent_json['id'],
+            "name": agent_json['name'],
+            "hostname": agent_json['hostname'],
+            "tag_names": ",".join(agent_json['tags']),
+            "mac": agent_json['mac'],
+            "os_name": agent_json['osName'],
+            "container_name": agent_json['container_name'],
+            "mode": agent_json['mode'],
+            "status": agent_json['status']
+        }
+        agents.Agents(ctx).register_agent(payload)
+
+    # restore dashboards
+    dashboard_files = glob.glob(dashboards_dir + '/*.yaml')
+    for dashboard_path in dashboard_files:
+        dashboards.Dashboards(ctx).import_dashboard(dashboard_path)
+
+    # restore plugins
+    plugin_files = glob.glob(plugins_dir + '/*')
+    for plugin_path in plugin_files:
+        plugins.Plugins(ctx).import_plugin(plugin_path)
+
+    # restore rules
+    rule_files = glob.glob(rules_dir + '/*.yaml')
+    for rule_path in rule_files:
+        rules.Rules(ctx).import_rule(rule_path)
+
+    # restore links
+    link_files = glob.glob(links_dir + '/*.json')
+    for link_path in link_files:
+        links.Links(ctx).import_link(link_path)
+
+
+def restore_org(ctx, org):
+    ctx.parent.parent.params['org'] = org
+    _accounts = accounts.Accounts(ctx)
+    for a in _accounts.get_accounts():
+        restore_account(ctx, a['name'])
+
+
+def agent_status_check(agent, status):
+    if status == 'all':
+        if agent['presence_state'] == 'online':
+            click.echo(click.style(agent['name'], fg='green'))
+        else:
+            click.echo(click.style(agent['name'], fg='red'))
+    if status == 'up':
+        if agent['presence_state'] == 'online':
+            click.echo(click.style(agent['name'], fg='green'))
+    if status == 'down':
+        if agent['presence_state'] != 'online':
+            click.echo(click.style(agent['name'], fg='red'))
+
+
+def search_agent(ctx, agent):
+    org_list = orgs.Orgs(ctx).get_orgs()
+    for _org in org_list:
+        ctx.parent.parent.params['org'] = _org['name']
+        account_list = accounts.Accounts(ctx).get_accounts()
+        for _account in account_list:
+            ctx.parent.parent.params['account'] = _account['name']
+            agent_list = agents.Agents(ctx).get_agents()
+            for _agent in agent_list:
+                if _agent['name'] == agent:
+                    click.echo('Organization: ' + _org['name'] + ' Account: ' + _account['name'] + ' Agent: ' + _agent['name'])
+
+
+
---
+++ packages/dlcli/dlcli-0.0.42.tar.gz/dlcli-0.0.42/dlcli/cli/get.py
@@ -0,0 +1,242 @@
+from ..cli import *
+import click
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@cli.group('get')
+@click.pass_context
+def get(ctx):
+    """gets things"""
+
+
+@click.command(short_help="Get accounts")
+@click.pass_context
+def accounts(ctx):
+    for a in Accounts(ctx).get_accounts():
+        click.echo(a['name'])
+
+
+@click.command(short_help="Get agents")
+@click.argument('status', type=click.Choice(['all', 'up', 'down']), default='all')
+@click.option('--tag', help='Tag Name', type=str, default=None)
+@click.pass_context
+def agents(ctx, status, tag):
+    for a in Agents(ctx).get_agents():
+        if tag:
+            if tag in a['tags']:
+                utils.agent_status_check(a, status)
+        else:
+            utils.agent_status_check(a, status)
+
+
+@click.command(short_help="Get an agent")
+@click.argument('name')
+@click.pass_context
+def agent(ctx, name):
+    click.echo(json.dumps(Agents(ctx).get_agent(name), indent=4))
+
+
+@click.command(short_help="Get dashboards")
+@click.pass_context
+def dashboards(ctx):
+    for dashboard in Dashboards(ctx).get_dashboards():
+        click.echo(dashboard['name'])
+
+
+@click.command(short_help="Get plugins")
+@click.pass_context
+def plugins(ctx):
+    for p in Plugins(ctx).get_plugins():
+        click.echo(p['name'])
+
+
+@click.command(short_help="Get links")
+@click.pass_context
+def links(ctx):
+    for l in Links(ctx).get_links():
+        click.echo(l['id'])
+
+
+@click.command(short_help="Get orgs")
+@click.pass_context
+def orgs(ctx):
+    for o in Orgs(ctx).get_orgs():
+        click.echo(o['name'])
+
+
+@click.command(short_help="Get rules")
+@click.pass_context
+def rules(ctx):
+    _rules = Rules(ctx)
+    for rule in _rules.get_rules():
+        for criteria in _rules.get_criteria(rule['name']):
+            if criteria['condition']['threshold']:
+                message = "on %s %s %s %d for %d seconds" % (
+                    criteria['scopes'][0]['type'],
+                    criteria['scopes'][0]['name'],
+                    criteria['condition']['operator'],
+                    criteria['condition']['threshold'],
+                    criteria['condition']['timeout'])
+            else:
+                message = 'on %s %s for %d seconds' % (
+                    criteria['scopes'][0]['type'],
+                    criteria['scopes'][0]['name'],
+                    criteria['condition']['timeout'])
+
+            if criteria['state'] == 'hit':
+                agent_names = []
+                triggered_by = criteria['triggered_by']
+                for agent_id in triggered_by:
+                    agent_names.append(Agents(ctx).get_agent_name_from_id(
+                        agent_id))
+                click.echo(click.style('%s %s %s triggered by %s',
+                                       fg='red') %
+                           (rule['name'], criteria['metric'], message,
+                            ','.join(map(str, agent_names))))
+            else:
+                click.echo(click.style('%s %s %s',
+                                       fg='green') %
+                           (rule['name'], criteria['metric'], message))
+
+
+@click.command(short_help="Get alerts")
+@click.pass_context
+def alerts(ctx):
+    _rules = Rules(ctx)
+    for r in _rules.get_rules():
+        for criteria in _rules.get_criteria(r['name']):
+            if criteria['condition']['threshold']:
+                criteria_type = criteria['scopes'][0]['type']
+                if criteria_type == 'tag':
+                    scope_key = 'id'
+                else:
+                    scope_key = 'name'
+                message = "on %s %s %s %d for %d seconds" % (
+                    criteria['scopes'][0]['type'],
+                    criteria['scopes'][0][scope_key],
+                    criteria['condition']['operator'],
+                    criteria['condition']['threshold'],
+                    criteria['condition']['timeout'])
+            else:
+                criteria_type = criteria['scopes'][0]['type']
+                if criteria_type == 'tag':
+                    scope_key = 'id'
+                else:
+                    scope_key = 'name'
+                message = 'on %s %s for %d seconds' % (
+                    criteria['scopes'][0]['type'],
+                    criteria['scopes'][0][scope_key],
+                    criteria['condition']['timeout'])
+
+            if criteria['state'] == 'hit':
+                agent_names = []
+                triggered_by = criteria['triggered_by']
+                for agent_id in triggered_by:
+                    agent_names.append(Agents(ctx).get_agent_name_from_id(
+                        agent_id))
+                click.echo(click.style('%s %s %s triggered by %s',
+                                       fg='red') %
+                           (r['name'], criteria['metric'], message,
+                            ','.join(map(str, agent_names))))
+
+
+@click.command(short_help="Get tags")
+@click.pass_context
+def tags(ctx):
+    for t in Tags(ctx).get_tags():
+        click.echo(t['name'])
+
+
+@click.command(short_help="Get metrics")
+@click.option('--agent', help='Agent Name', type=str, default=None)
+@click.option('--tag', help='Tag Name', type=str, default=None)
+@click.pass_context
+def metrics(ctx, agent, tag):
+    if not agent and not tag:
+        click.echo('Specify an agent or tag to get the metrics')
+        sys.exit(1)
+    if agent:
+        agent_id = None
+        agent_details = Agents(ctx).get_agents()
+        for a in agent_details:
+            if a['name'] == agent:
+                agent_id = a['id']
+        for metric in Metrics(ctx).get_agent_metrics(agent_id):
+            print metric['name']
+    if tag:
+        for metric in Metrics(ctx).get_tag_metrics(tag):
+            print metric['name']
+
+
+@click.command(short_help="Get series")
+@click.option('--agent', help='Agent Name', type=str, default=None)
+@click.option('--tag', help='Tag Name', type=str, default=None)
+@click.option('--resolution', help='Time between points', type=int, default=30)
+@click.option('--period', help='Length of time', type=int, default=600)
+@click.argument('metric')
+@click.pass_context
+def series(ctx, metric, agent, tag, resolution, period):
+    if not agent and not tag:
+        click.echo('Specify an agent or tag to get the metrics')
+        sys.exit(1)
+
+    if agent:
+        agent_id = None
+        agent_details = Agents(ctx).get_agents()
+        for a in agent_details:
+            if a['name'] == agent:
+                agent_id = a['id']
+        for s in Series(ctx).get_agent_series(agent_id, metric, resolution, period):
+            points = []
+            for point in s['points']:
+                if point['type'] == 'boolean':
+                    if point['all']:
+                        points.append(0)
+                    else:
+                        points.append(2)
+                else:
+                    points.append(point['avg'])
+            print ','.join(map(str, points))
+    if tag:
+        for s in Series(ctx).get_tag_series(tag, metric, resolution, period):
+            points = []
+            click.echo(click.style(s['source']['name'], fg='green'))
+            for point in s['points']:
+                if point['type'] == 'boolean':
+                    if point['all']:
+                        points.append(0)
+                    else:
+                        points.append(2)
+                else:
+                    points.append(point['avg'])
+            print ','.join(map(str, points))
+
+
+@click.command(short_help="Get user")
+@click.argument('tokens', required=False)
+@click.pass_context
+def user(ctx, tokens):
+    if tokens:
+        token_list = User(ctx).get_user_tokens()
+        for t in token_list:
+            print t['name']
+    else:
+        print User(ctx).get_user()['name'].lower()
+
+
+get.add_command(accounts)
+get.add_command(agents)
+get.add_command(agent)
+get.add_command(dashboards)
+get.add_command(links)
+get.add_command(orgs)
+get.add_command(plugins)
+get.add_command(rules)
+get.add_command(alerts)
+get.add_command(tags)
+get.add_command(metrics)
+get.add_command(series)
+get.add_command(user)
+
