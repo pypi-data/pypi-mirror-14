3b68104852bd38157e8f648f2fc4b0b4584eb04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:45 +0000

    Add package django-mozilla-product-details/0.11/django_mozilla_product_details-0.11-py2.py3-none-any.whl

---
+++ packages/django-mozilla-product-details/django_mozilla_product_details-0.11-py2.py3-none-any.whl/product_details/settings_defaults.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+
+# URL to clone product_details JSON files from.
+# Include trailing slash.
+PROD_DETAILS_URL = 'https://svn.mozilla.org/libs/product-details/json/'
+
+# Target dir to drop JSON files into (must be writable)
+PROD_DETAILS_DIR = os.path.join(os.path.dirname(__file__), 'json')
+
+# log level.
+LOG_LEVEL = logging.INFO
+
+# name of cache to use
+PROD_DETAILS_CACHE_NAME = 'default'  # django default
+
+# how long to cache
+PROD_DETAILS_CACHE_TIMEOUT = 60 * 60 * 12  # 12 hours
+
+# data storage class
+PROD_DETAILS_STORAGE = 'product_details.storage.PDFileStorage'
