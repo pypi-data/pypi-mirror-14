c4bf845c3a63898691db7c7fc638bd247cec6cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package geokey-epicollect/1.0.2/geokey-epicollect-1.0.2.tar.gz

---
+++ packages/geokey-epicollect/geokey-epicollect-1.0.2.tar.gz/geokey-epicollect-1.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from os.path import join
+from setuptools import setup, find_packages
+
+
+name = 'geokey-epicollect'
+version = __import__(name.replace('-', '_')).__version__
+repository = join('https://github.com/ExCiteS', name)
+
+setup(
+    name=name,
+    version=version,
+    description='Use EpiCollect to collect data for GeoKey',
+    url=repository,
+    download_url=join(repository, 'tarball', version),
+    author='ExCiteS',
+    author_email='excites@ucl.ac.uk',
+    license='MIT',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
+    include_package_data=True,
+    install_requires=['lxml==3.3.5'],
+)
