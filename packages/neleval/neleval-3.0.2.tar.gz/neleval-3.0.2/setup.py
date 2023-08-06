9dabe6604f39f09ba3c97609da84b127183556c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:08 +0000

    Add package neleval/3.0.2/neleval-3.0.2.tar.gz

---
+++ packages/neleval/neleval-3.0.2.tar.gz/neleval-3.0.2/setup.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+import sys
+import os
+from setuptools import setup
+
+
+def read_markdown(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    if not os.path.exists(path):
+        if 'sdist' in sys.argv:
+            print('WARNING: did not find %r' % filename, file=sys.stderro)
+        return
+    try:
+        import pypandoc
+    except ImportError:
+        if 'sdist' in sys.argv:
+            print('WARNING: Could not import pypandoc to convert README.md to RST!',
+                  file=sys.stderr)
+        return open(path).read()
+    return pypandoc.convert(path, 'rst')
+
+
+VERSION = '3.0.2'
+
+setup(name='neleval',
+      version=VERSION,
+      download_url='https://github.com/wikilinks/neleval/tree/v' + VERSION,
+
+      description='Evaluation utilities for named entity linking and (cross-document) coreference resolution',
+      author='Joel Nothman, Ben Hachey, Will Radford',
+      author_email='joel.nothman+neleval@gmail.com',
+      packages=['neleval'],
+      url='https://github.com/wikilinks/neleval',
+      keywords=['clustering', 'coreference', 'evaluation', 'entity disambiguation'],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering',
+      ],
+      licence='Apache 2.0',
+
+      long_description=read_markdown('README.md'),
+
+      entry_points={
+          'console_scripts': [
+              'neleval = neleval.__main__:main',
+          ],
+      },
+      install_requires=[
+          'numpy',
+      ],
+      extras_require={
+          'significance': [
+              'joblib',
+          ],
+          'ceaf': [
+              'scipy',
+          ],
+          'plots': [
+              'matplotlib',
+          ],
+          'dev': [
+              'pyflakes',
+              'nose',
+          ],
+      }
+)
