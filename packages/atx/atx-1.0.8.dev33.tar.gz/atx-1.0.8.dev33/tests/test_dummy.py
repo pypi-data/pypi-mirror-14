118559fb1a5fa1a272fbab5eca57f504584375d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:06 +0000

    Add package atx/1.0.8.dev33/atx-1.0.8.dev33.tar.gz

---
+++ packages/atx/atx-1.0.8.dev33.tar.gz/atx-1.0.8.dev33/tests/test_dummy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#coding: utf-8
+
+import os
+import time
+
+import pytest
+import atx
+
+
+d = atx.connect(platform='dummy')
+
+def setup_function(f):
+    d.resolution = (1280, 720)
+
+def test_setget_resolution():
+    assert d.resolution == (720, 1280)
+
+    d.resolution = None # None is also OK to set
+    assert d.resolution is None
+
+    d.resolution = (200, 400)
+    assert d.resolution == (200, 400)
+
+    with pytest.raises(TypeError):
+        d.resolution = [1,3]
+    with pytest.raises(TypeError):
+        d.resolution = 720
+    assert d.resolution == (200, 400)
+    
+def teardown_function(f):
+    print 'teardown'
+    
+def test_screenshot():
+    screen = d.screenshot()
+    assert screen is not None
+
+def test_region_screenshot():
+    nd = d.region(atx.Bounds(100, 100, 600, 300))
+    rs = nd.region_screenshot()
+    assert rs is not None
+    assert rs.size == (500, 200)
+
+def test_assert_exists():
+    d.assert_exists('media/system-app.png')
+
+    with pytest.raises(atx.AssertExistsError):
+        d.assert_exists('media/haima.png', timeout=0.1)
