61d287fd6c022bbaa813f1ac1ecfd5cf751ec81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:14 +0000

    Add package pyfalcon/1.1.2/pyfalcon-1.1.2.tar.gz

---
+++ packages/pyfalcon/pyfalcon-1.1.2.tar.gz/pyfalcon-1.1.2/pyfalcon/macro.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+"""
+pyfalcon.macro.
+
+~~~~~~~~~~~~~~~
+
+Macro defines.
+"""
+
+
+class CounterType(object):
+    """Open falcon counter type."""
+
+    GAUGE = "GAUGE"
+    COUNTER = "COUNTER"
