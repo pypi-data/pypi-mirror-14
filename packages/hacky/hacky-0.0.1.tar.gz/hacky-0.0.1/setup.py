4a96b5075c13471133aea5c6067c7fb2cac46399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:57 +0000

    Add package hacky/0.0.1/hacky-0.0.1.tar.gz

---
+++ packages/hacky/hacky-0.0.1.tar.gz/hacky-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+
+hacky_module = Extension('hacky', sources = ['hacky.c'])
+
+
+setup(
+    name='hacky',
+    author='magniff',
+    version='0.0.1',
+    url='https://github.com/magniff/hacky',
+    description='Low level tinkering utils.',
+    ext_modules=[hacky_module]
+)
