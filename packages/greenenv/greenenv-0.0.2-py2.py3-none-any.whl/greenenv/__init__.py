ea7f914efab4434228df55972808a9aef97cb1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:13 +0000

    Add package greenenv/0.0.2/greenenv-0.0.2-py2.py3-none-any.whl

---
+++ packages/greenenv/greenenv-0.0.2-py2.py3-none-any.whl/greenenv/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .greenenv import ExtendedEnvBuilder, artifact_path
+
+__author__ = 'Andrew B Godbehere'
+__email__ = 'andrew.godbehere@sumupanalytics.com'
+__version__ = '0.0.2'
+
+
+env_factory = ExtendedEnvBuilder()
