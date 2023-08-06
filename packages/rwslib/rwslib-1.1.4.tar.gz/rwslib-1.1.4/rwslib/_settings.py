26363bff352efaa71f9670da1889e549c3a2b487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:36 +0000

    Add package rwslib/1.1.4/rwslib-1.1.4.tar.gz

---
+++ packages/rwslib/rwslib-1.1.4.tar.gz/rwslib-1.1.4/rwslib/_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'isparks'
+
+accounts = {'innovate' : {'username': 'isparks',
+                         'password':'Jupiter103'},
+            'otherurl' : {'username': 'username',
+                         'password':'password'},
+            'rsayer' : {'username': 'defuser',
+                         'password':'password'},
+
+           }
