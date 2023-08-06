d98f587302055a6fe04d79568937647dbb5d9e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:49 +0000

    Add package rowingdata/0.45/rowingdata-0.45.zip

---
+++ packages/rowingdata/rowingdata-0.45.zip/rowingdata-0.45/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+	return f.read()
+
+setup(name='rowingdata',
+
+      version='0.45',
+
+      description='The rowingdata library to create colorful plots from CrewNerd, Painsled and other rowing data tools',
+
+      long_description=readme(),
+
+      url='http://rowsandall.wordpress.com',
+
+      author='Sander Roosendaal',
+
+      author_email='roosendaalsander@gmail.com',
+
+      license='MIT',
+
+      packages=['rowingdata'],
+
+      keywords = 'rowing ergometer concept2',
+      
+      install_requires=[
+	  'numpy',
+	  'scipy',
+	  'matplotlib',
+	  'pandas',
+	  'mechanize'
+	  ],
+
+      zip_safe=False)
