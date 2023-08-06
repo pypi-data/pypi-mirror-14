766bba0b1b729171873d371894835d474e960014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:35 +0000

    Add package dave131_print_list/1.0.0/dave131_print_list-1.0.0.zip

---
+++ packages/dave131_print_list/dave131_print_list-1.0.0.zip/dave131_print_list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup #从python发布工具中导入"setup"函数
+
+setup(
+    name         = 'dave131_print_list',
+    version      ='1.0.0',
+    py_modules   =['dave131_print_list'],#将模块的元数据与setup函数的参数关联
+    author       ='LiYuan',
+    author_email ='364498159@qq.com',
+    url          ='qq.com',
+    description  ='a simple printer of nester lists',
+    )
