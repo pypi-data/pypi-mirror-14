914382409cf5f914c5973080f19a89ecbc10b05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package dHydra/0.10.1/dHydra-0.10.1.tar.gz

---
+++ packages/dHydra/dHydra-0.10.1.tar.gz/dHydra-0.10.1/dHydra/core/Globals.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+"""
+全局变量在这里定义
+"""
+import traceback
+import sys
+import os
+
+print("加载: Globals.py")
+
+# producer
+PRODUCER_HASH = dict()
+PRODUCER_NAME = dict()
+
+actionDict = dict()
