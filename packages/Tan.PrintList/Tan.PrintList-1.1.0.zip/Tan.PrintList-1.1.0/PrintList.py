a6104106808244786852d46d6078aec455ac7012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package Tan.PrintList/1.1.0/Tan.PrintList-1.1.0.zip

---
+++ packages/Tan.PrintList/Tan.PrintList-1.1.0.zip/Tan.PrintList-1.1.0/PrintList.py
@@ -0,0 +1,10 @@
+#这个函数可以遍历整个列表并打印每一项
+def PaintList (the_list,Level):
+    for k in the_list:
+        if isinstance(k,list):
+            PaintList(k,Level+1)
+        else:
+            for tab_stop in range(Level):
+                print("\t",end='')
+            print(k)
+
---
+++ packages/Tan.PrintList/Tan.PrintList-1.1.0.zip/Tan.PrintList-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='Tan.PrintList',
+    version='1.1.0',
+    py_modules=['PrintList'],
+    author='tan1995',
+    author_email='sorrygame@foxmail.com',
+    url='http://www.tan1905.com',
+    description='能打印嵌套列表项的函数,有自动缩进功能'
+)
