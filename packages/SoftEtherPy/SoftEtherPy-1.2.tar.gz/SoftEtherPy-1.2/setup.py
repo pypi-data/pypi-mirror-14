e64e57d3d68d1f149bece266cb0db91a536860c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:32 +0000

    Add package SoftEtherPy/1.2/SoftEtherPy-1.2.tar.gz

---
+++ packages/SoftEtherPy/SoftEtherPy-1.2.tar.gz/SoftEtherPy-1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='SoftEtherPy',
+    version='1.2',
+    description='SoftEther VPN Server Python Management API',
+    author='Pervolo',
+    author_email='info@pervolo.com',
+    url='https://github.com/mindcollapse/SoftEtherPy',
+    packages=['softether'],
+)
