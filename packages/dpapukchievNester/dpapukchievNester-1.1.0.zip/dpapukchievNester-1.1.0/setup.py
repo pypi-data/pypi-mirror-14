f984be97a5439e3729f45b4854dc7e05eb0a27b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:51 +0000

    Add package dpapukchievNester/1.1.0/dpapukchievNester-1.1.0.zip

---
+++ packages/dpapukchievNester/dpapukchievNester-1.1.0.zip/dpapukchievNester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'dpapukchievNester',
+version = '1.1.0',
+py_modules = ['dpapukchievNester'],
+author = 'Daniel Papukchiev',
+author_email = 'dpapukchiev@gmail.com',
+url = 'http://athena.fhict.nl/users/i333086',
+description = 'A simple printer of nested lists',
+)
