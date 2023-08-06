cabc547ddce9ee1770de5556ad32d4bc92048e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:26 +0000

    Add package saspy/1.1.1/saspy-1.1.1.tar.gz

---
+++ packages/saspy/saspy-1.1.1.tar.gz/saspy-1.1.1/saspy/sascfg.py
@@ -0,0 +1,57 @@
+#
+# Copyright SAS Institute
+#
+#  Licensed under the Apache License, Version 2.0 (the License);
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# Configuration names for SAS - python List
+# 
+# SAS_config_names=['default', 'sasother']
+#
+SAS_config_names = ['default']
+
+# Configuration options for pysas - python Dict
+# valid key are:
+# 
+# 'lock_down' - True | False. True = Prevent runtime overrides of SAS_Config values below
+#
+SAS_config_options = {'lock_down': True}
+
+# These need path to SASHome and version number - python Dict
+# The default path to the sas start up script is: /opt/sasinside/SASHome/SASFoundation/9.4/sas
+# A usual install path is: /opt/sasinside/SASHome
+#
+# Since python uses utf-8, running SAS with encoding=utf-8 is the expected use case. By default
+# linux SAS runs in Latin1, which works fine as long as you stick with the lower half of the code page.
+# SAS is installed with a link ('sas') to the bin/sas_en startup script, and the link can be swapped
+# to point to the utf8 start up script (bin/sas_u8), or another link made (sasutf8) to point to that.  
+#
+# valid keys are:
+# 'saspath' - path to SAS startup script i.e.: /opt/sasinside/SASHome/SASFoundation/9.4/sas
+# 'options' - SAS options to include in the start up command line - Python List
+#
+#
+#
+default  = {'saspath': '/opt/sasinside/SASHome/SASFoundation/9.4/bin/sas_u8',
+            }
+
+# sas_en   = {'saspath': '/opt/sasinside/SASHome/SASFoundation/9.4/sas',
+#             }
+#
+# sas_utf8 = {'saspath': '/opt/sasinside/SASHome/SASFoundation/9.4/sasutf8',
+#             }
+#
+# sasother = {'saspath' : '/some/other/directory/SASHome/SASFoundation/9.4/sas',
+#             'options' : ["-autoexec", "/my/home_dir/my_autoexec.sas", "-set",
+#                          "Env_var", "Value", "-fullstimer"]
+#             }
+
