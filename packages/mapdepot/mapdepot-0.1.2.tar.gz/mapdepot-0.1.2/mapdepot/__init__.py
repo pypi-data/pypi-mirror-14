e5ec0b5cfaedf713bceecf59f6521d364aef96ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:31 +0000

    Add package mapdepot/0.1.2/mapdepot-0.1.2.tar.gz

---
+++ packages/mapdepot/mapdepot-0.1.2.tar.gz/mapdepot-0.1.2/mapdepot/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""MapDepot Index."""
+
+from __future__ import absolute_import
+import mapdepot.schemas  # noqa
+from mapdepot.cli import cli  # noqa
+
+__title__ = 'mapdepot'
+__author__ = 'Denis Carriere'
+__author_email__ = 'carriere.denis@gmail.com'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2016 Denis Carriere'
