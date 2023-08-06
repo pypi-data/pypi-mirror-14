284b5313b822f8a3729cc8c32c70ff4a2e3272e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:47 +0000

    Add package elevation/0.9.9/elevation-0.9.9.tar.gz

---
+++ packages/elevation/elevation-0.9.9.tar.gz/elevation-0.9.9/CHANGELOG.rst
@@ -0,0 +1,23 @@
+
+0.9.9 (2016-04-01)
+------------------
+
+- Enforce the no-bulk-download policy.
+
+
+0.9.8 (2016-03-31)
+------------------
+
+- Make ``clean`` remove empty tiles as they may be due to temporary server failures.
+
+
+0.9.7 (2016-03-30)
+------------------
+
+- Fixed user visible documentation.
+
+
+0.9.6 (2016-03-30)
+------------------
+
+- Initial public beta release.
---
+++ packages/elevation/elevation-0.9.9.tar.gz/elevation-0.9.9/tests/test_cli.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 B-Open Solutions srl - http://bopen.eu
+#
+
+# python 2 support via python-future
+from __future__ import absolute_import, unicode_literals
+
+import click.testing
+
+import subprocess
+
+from elevation import cli
+
+
+def test_eio_selfcheck(mocker):
+    runner = click.testing.CliRunner()
+    with mocker.patch('subprocess.check_output'):
+        result = runner.invoke(cli.selfcheck)
+    assert not result.exception
+    assert subprocess.check_output.call_count == 5
+
+
+def test_eio_info(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s info' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 1
+
+
+def test_eio_seed(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s seed --bounds 12.5 42 12.5 42' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 2
+
+
+def test_eio_clip(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s clip --bounds 12.5 42 12.5 42' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 3
+
+
+def test_eio_clean(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s clean' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 1
+
+
+def test_eio_distclean(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s distclean' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 1
+
+
+def test_eio(mocker, tmpdir):
+    root = tmpdir.join('root')
+    runner = click.testing.CliRunner()
+    options = '--cache_dir %s seed --bounds 12.5 42 12.5 42' % str(root)
+    with mocker.patch('subprocess.check_call'):
+        result = runner.invoke(cli.eio, options.split())
+    assert not result.exception
+    assert subprocess.check_call.call_count == 2
