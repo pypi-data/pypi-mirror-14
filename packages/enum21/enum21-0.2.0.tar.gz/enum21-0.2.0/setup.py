c452653665f34173070ab46941684e1c313bb7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:23 +0000

    Add package enum21/0.2.0/enum21-0.2.0.tar.gz

---
+++ packages/enum21/enum21-0.2.0.tar.gz/enum21-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='enum21',
+    packages=['enum21'],
+    version='0.2.0',
+    description='Enum implementation for Python.',
+    author='Brian S Morgan',
+    author_email='brian.s.morgan@gmail.com',
+    url='https://github.com/bmorgan21/python-enum'
+)
