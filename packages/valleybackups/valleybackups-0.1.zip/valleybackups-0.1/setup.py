9e05fe0d3487efa3404e65bef0ff14aa1c516f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package valleybackups/0.1/valleybackups-0.1.zip

---
+++ packages/valleybackups/valleybackups-0.1.zip/valleybackups-0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='valleybackups',
+    version='0.1',
+    description='Command-Line Based Backup Software',
+    url='https://github.com/valleyworks/valleybackups',
+    download_url='https://github.com/valleyworks/valleybackups/archive/v0.1.zip',
+    author='ValleyWorks',
+    author_email='nahuel.chaves@valleyworks.us',
+    #packages=['valleybackups'],
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='backup glacier aws',
+    py_modules=[
+        'valleybackups.db',
+        'valleybackups.server',
+        'valleybackups.extensions.glacier'
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'pony',
+        'boto3==1.2.6',
+        'Flask'
+    ],
+    setup_requires=[
+        'Click',
+        'pony',
+        'boto3==1.2.6',
+        'Flask'
+    ],
+    entry_points='''
+    [console_scripts]
+    valleybackups=valleybackups.cli:cli
+    '''
+)
---
+++ packages/valleybackups/valleybackups-0.1.zip/valleybackups-0.1/valleybackups/config_handler.py
@@ -0,0 +1,50 @@
+import ConfigParser
+from os.path import join, dirname, isfile
+
+
+class ConfigurationHandler:
+    def __init__(self):
+        self.config_parser = ConfigParser.SafeConfigParser()
+        self.config_file = join(dirname(__file__), 'valleybackups.conf')
+
+        if isfile(self.config_file):
+            pass
+        else:
+            self.create_config_file()
+
+        self.config_parser.read(self.config_file)
+
+    def create_config_file(self):
+        self.config_parser.add_section('base')
+        self.config_parser.set('base', 'ACCESS_KEY_ID', '')
+        self.config_parser.set('base', 'SECRET_ACCESS_KEY', '')
+        self.config_parser.set('base', 'AWS_ACCOUNT_ID', '')
+
+        self.config_parser.add_section('glacier')
+        self.config_parser.set('glacier', 'VAULT_NAME', '')
+
+        self.save_config()
+
+    def get_config(self, section, option):
+        return self.config_parser.get(section, option)
+
+    def set_config(self, section, option, value):
+        self.config_parser.set(section, option, value)
+
+    def check_config(self):
+        config_ok = False
+        for section_name in self.config_parser.sections():
+            for name, value in self.config_parser.items(section_name):
+                if value != '':
+                    config_ok = True
+                else:
+                    config_ok = False
+        return config_ok
+
+    def get_parser(self):
+        return self.config_parser
+
+    def save_config(self):
+        opened_file = open(self.config_file, 'w')
+        self.config_parser.write(opened_file)
+        opened_file.close()
