8dfd557f4020c0e6b553c49f9fb94c567377774f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:20 +0000

    Add package bnlp/0.2/bnlp-0.2.tar.gz

---
+++ packages/bnlp/bnlp-0.2.tar.gz/bnlp-0.2/bnlp/statements.py
@@ -0,0 +1,42 @@
+def getStatementsFromTextArabic(text):
+    statements = []
+
+    d = {}
+    d['\u0648\u0642\u0627\u0644\u062a'] = 'said'
+
+    # example
+    # text= """ï»®ï»ïºï» ïºï» ï»¤ïºïº£ïºªïº ïºïºïº´ï»£ ï»¢ïº ï» ïº³ ïºï»¸ï»¤ï»§ ïºï» ï»ï»®ï»¤ï»³ ïºï»¸ï»£ïº®ï»´ï»ï»³ ïºï»§ "ï»¡ï»®ïº ïºï» ïº¤ï»³ïºï» ï»¼ï»£ ï»­ïºï» ï»ï»¤ï» ï»³ïºïº ï»­ïºï» ï»ï»®ïº ïº´ïºï»³ïºïº ïºï» ïº§ïºïº»ïº ïºïºï»ïº©ï»®ï»ïº ïºï»ºïº³ï»¼ï»¤ï»³ïº".""" ï»ïºï»§ ï»²ï»¤ïºïº© ïºï»ï»¯ ïºï
+» ïºï»£ï»®ï»´ï» ï»­ïºï»ºï» 
+
+#    for statement in finditer(ur"(?P<verb>\u0648\u0642\u0627\u0644)(?P<speaker>.*)(?:aannn)?(?P<quote>\"[^\"]*\")", text, MULTILINE):
+#        statements.append({'speaker': statement.group('speaker'), 'verb': statement.group('verb'), 'quote': statement.group('quote')})
+
+    #text = """ï»®ïº¼ï»ï»«ïº ïºï» ïºï»´ïº ïºï»¸ïºï»´ïº¿  ïºÙ"ïºï» ï»ïºïº­ïºïº ïºï» ïºï»®ï»³ïº ïºï» ï»¤ï»¤ï»´ïºïº""""
+    # described
+#    for statement in finditer(r"(?P<verb>\ufeee\ufebc\ufed4\ufeeb\ufe8d)(?P<speaker>.*)(?:ïºÙ)?(?P<quote>\"[^\"]*\")", text, rMULTILINE):
+#    for statement in finditer(ur"(?P<verb>\
+#        statements.append({'speaker': statement.group("speaker"), 'verb': statement.group("verb"), 'quote': statement.group("quote")})
+
+    #Said wa/andthe agency for research and development (UNITAR) "statement "
+    #note past tense
+    #note: sometimes speaker will have acronymn in it like above, but sometimes won't have it at all
+    for statement in finditer(ur"(?P<verb>\u0648\u0642\u0627\u0644\u062a) \u0648?(?P<speaker>[^\"]*) \"(?P<quote>[^\"]*)\"",text, MULTILINE):
+        statements.append({'speaker': statement.group("speaker"), 'verb': statement.group("verb"), 'quote': statement.group("quote")})
+
+    # this is like in Arabic news
+    # "Angela Merkel": Bla blab bla
+    # not sure this is their way of quoting or is actually more likely paraphrasing
+    # commenting out for now just in case
+    #for statement in re.findit("^[\"'](?P<speaker>[^\"']*)[\"']: ?(?P<quote>[^\.\n$]*)$", text, re.MULTILINE):
+   #    statements.append({'speaker': statement.group("speaker"), 'verb': None, 'quote': statement.group("quote")})
+
+    return statements
+gsfta = getStatementsFromTextArabic
+
+def getStatementTypeArabic(text):
+    d = {}
+    d[u'\u062a\u0635\u0631\u064a\u062d \u0635\u062d\u0641\u064a'] = "Press Release"
+    result = re.search(ur'(?P<type>\u062a\u0635\u0631\u064a\u062d \u0635\u062d\u0641\u064a)', text)
+
+def isPressRelease(text):
+    return any(word in text for word in ["Address", "Adress", "Advisory", "Press", "Statement", "Communique", "Remarks", "Speech", "Comments", "Media", "Note", "Media Advisory", "Media Statement", "Media Stetment", "Media Note"])
---
+++ packages/bnlp/bnlp-0.2.tar.gz/bnlp-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'bnlp',
+  packages = ['bnlp'],
+  version = '0.2',
+  description = 'Better Natural Language Processing, a collection of open source nlp methods used by Better Know The Opposition',
+  author = 'Better Know The Opposition',
+  author_email = 'hello@betterknowtheopposition.com',
+  url = 'https://github.com/BKTO/bnlp',
+  download_url = 'https://github.com/BKTO/bnlp',
+  keywords = ['nlp'],
+  classifiers = [],
+)
