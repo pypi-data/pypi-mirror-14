9af3cb52725e89e1166bd880b2f2c7d7b647eaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:23 +0000

    Add package Logentries/0.16/Logentries-0.16.tar.gz

---
+++ packages/Logentries/Logentries-0.16.tar.gz/Logentries-0.16/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Logentries',
+    version='0.16',
+    author='Mark Lacomber',
+    author_email='marklacomber@gmail.com',
+    packages=['logentries'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Logentries/',
+    license='LICENSE.txt',
+    description='Python Logger plugin to send logs to Logentries',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "certifi",
+    ],
+)
