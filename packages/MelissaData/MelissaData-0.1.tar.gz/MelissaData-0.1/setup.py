a9e844bbbcd680153461841fc57aca727644b7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:28 +0000

    Add package MelissaData/0.1/MelissaData-0.1.tar.gz

---
+++ packages/MelissaData/MelissaData-0.1.tar.gz/MelissaData-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MelissaData",
+    version="0.1",
+    license="MIT",
+    description="A Python interface for MelissaData JSON API",
+    author="Greg McCoy",
+    author_email="gregmccoy@gfa.org",
+    url="https://github.com/gregmccoy/melissadata",
+    packages = find_packages(),
+)
