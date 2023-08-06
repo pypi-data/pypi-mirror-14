ad32ad2e2d6c1235a74281c827a05e107b39dcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:58 +0000

    Add package fapistrano/0.8.0/fapistrano-0.8.0-py2.py3-none-any.whl

---
+++ packages/fapistrano/fapistrano-0.8.0-py2.py3-none-any.whl/fapistrano/plugins/fis.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import show, run, env, cd, show, hide
+
+from .. import signal, configuration
+
+def init():
+    configuration.setdefault('fis_output', False)
+    configuration.setdefault('fis_domains', False)
+    configuration.setdefault('fis_md5', True)
+    configuration.setdefault('fis_optimize', True)
+    configuration.setdefault('fis_pack', True)
+    configuration.setdefault('fis_conf', 'fis-conf.js')
+    signal.register('deploy.updated', build_fis_assets)
+    # FIXME: asset fis install
+
+def build_fis_assets():
+    output = show if env.fis_output else hide
+    with output('output'):
+        with cd('%(release_path)s/%(fis_source)s' % env):
+            cmd = (
+                'fis release '
+                '--file %(fis_conf)s '
+                '--dest %(fis_dest)s '
+            ) % env
+            if env.fis_md5:
+                cmd += '--md5 '
+            if env.fis_optimize:
+                cmd += '--optimize '
+            if env.fis_pack:
+                cmd += '--pack '
+            if env.fis_domains:
+                cmd += '--domains '
+            run(cmd)
