52bb402c941724e90d8fee6f840de4adea7c6fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:29 +0000

    Add package germanium/1.7.1/germanium-1.7.1-py3-none-any.whl

---
+++ packages/germanium/germanium-1.7.1-py3-none-any.whl/germanium/locators/__init__.py
@@ -0,0 +1,9 @@
+
+from .CompositeLocator import CompositeLocator
+from .CssLocator import CssLocator
+from .DeferredLocator import DeferredLocator
+from .StaticElementLocator import StaticElementLocator
+from .XPathLocator import XPathLocator
+from .JsLocator import JsLocator
+
+from .PositionalFilterLocator import PositionalFilterLocator
