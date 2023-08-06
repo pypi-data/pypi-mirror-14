d15dbf5de280783ec4172a14ead6149f80892513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package word2color/0.1/word2color-0.1.tar.gz

---
+++ packages/word2color/word2color-0.1.tar.gz/word2color-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(author='Nelson Liu, Aakash Sethi',
+      author_email='nfliu@uw.edu, aakash.k.sethi@gmail.com',
+      description='Convert descriptions of colors to colors',
+      license='MIT',
+      long_description='A tool for transforming natural language descriptions of colors to web-standard color names',
+      name='word2color',
+      packages=['word2color'],
+      url='https://github.com/nelson-liu/word2color',
+      version='0.1',
+      zip_safe=False
+)
