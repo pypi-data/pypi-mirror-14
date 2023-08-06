dda8b83b9b2ba8095148f8077b786d6f15ddbb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:13 +0000

    Add package hitchselenium/0.5.2/hitchselenium-0.5.2.tar.gz

---
+++ packages/hitchselenium/hitchselenium-0.5.2.tar.gz/hitchselenium-0.5.2/hitchselenium/__init__.py
@@ -0,0 +1,9 @@
+from hitchselenium.step_library import SeleniumStepLibrary
+from hitchselenium.selenium_service import SeleniumService
+from hitchselenium.profile import Profile
+from hitchselenium.server import run
+
+
+UNIXPACKAGES = [
+    "firefox", "xvfb", "xauth", "xserver-xorg", "dbus-x11", "ca-certificates",
+]
