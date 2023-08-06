4b22e733aa27f42bf83aff7bd2c3cd6ada2a51fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:54 +0000

    Add package Maria_MENG/1.0.0/Maria_MENG-1.0.0.tar.gz

---
+++ packages/Maria_MENG/Maria_MENG-1.0.0.tar.gz/Maria_MENG-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='Maria_MENG',
+    version='1.0.0',
+    py_modules=['Maria_MENG'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
