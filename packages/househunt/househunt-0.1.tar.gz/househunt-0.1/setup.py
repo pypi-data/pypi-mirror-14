0e3da32ac4f7e7b16b0d5acf47d442849bed351b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:49 +0000

    Add package househunt/0.1/househunt-0.1.tar.gz

---
+++ packages/househunt/househunt-0.1.tar.gz/househunt-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'househunt',
+    packages = ['househunt'],
+    version = '0.1',
+    description = 'Python module to search Redfin and combine with results from the Zillow API',
+    author = 'AlThor880',
+    author_email = 'althor880@gmail.com',
+    url = 'https://github.com/althor880/househunt',
+    download_url = 'https://github.com/althor880/househunt/tarball/0.1',
+    keywords = ['house', 'realty', 'zillow', 'redfin', 'api'],
+    classifiers = [],
+)
