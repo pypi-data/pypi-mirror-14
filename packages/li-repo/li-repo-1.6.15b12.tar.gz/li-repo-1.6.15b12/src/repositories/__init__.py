2149a7954e47742b575808df63e34e9f01e2e38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package li-repo/1.6.15b12/li-repo-1.6.15b12.tar.gz

---
+++ packages/li-repo/li-repo-1.6.15b12.tar.gz/li-repo-1.6.15b12/src/repositories/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import models
+import catalogo.models
+import cliente.models
+import configuracao.models
+import faturamento.models
+import marketing.models
+import marketplace.models
+import pedido.models
+import plataforma.models
