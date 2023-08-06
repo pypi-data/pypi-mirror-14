ba9b286da8a1015d880117662da4deb17110d895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:00 +0000

    Add package pentaho-rest-api/1.0.2/pentaho-rest-api-1.0.2.tar.gz

---
+++ packages/pentaho-rest-api/pentaho-rest-api-1.0.2.tar.gz/pentaho-rest-api-1.0.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+
+
+def get_requirements():
+    """Simply read the requirements.txt file and returns the list of the dependencies.
+    :returns: list of requirements
+    """
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+
+    return requirements
+
+reqs = get_requirements()
+
+setup(
+    name='pentaho-rest-api',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.2',
+
+    description='Python library for the pentaho BA REST API',
+    long_description='Python library for the pentaho BA REST API',
+
+    # The project's main homepage.
+    url='git@github.com:AlayaCare/pentaho-rest-api.git',
+
+    # Author details
+    author='Saqib Khalil Yawar',
+    author_email='saqib.yawar@alayacare.com',
+
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License (GPL)'
+    ],
+
+    # What does your project relate to?
+    keywords='pentaho rest api development',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=reqs,
+
+    extras_require={
+        'dev': reqs,
+        'test': reqs,
+    },
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'users_api=pentaho-rest-api.scripts.info:info',
+    #     ],
+    # },
+)
