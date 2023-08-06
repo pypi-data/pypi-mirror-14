22143f8faea2691afb08d61557202e9a7957664c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:06 +0000

    Add package mydagobah/0.2/mydagobah-0.2.tar.gz

---
+++ packages/mydagobah/mydagobah-0.2.tar.gz/mydagobah-0.2/setup.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+from distutils.core import setup
+setup(
+        name = 'mydagobah',
+        packages = ['mydagobah'],
+        version = '0.2',
+        description = 'distribute the tasks',
+        author = '轩轩',
+        author_email = '13060404095@163.com',
+        url = 'https://github.com/littlegump/mydagobah.git',
+        download_url = 'https://github.com/littlegump/mydagobah/tarball/0.2',
+        keywords = ['dagobah', 'module_task', 'distribute hundreds of tasks'],
+        classifiers = [],
+        )
