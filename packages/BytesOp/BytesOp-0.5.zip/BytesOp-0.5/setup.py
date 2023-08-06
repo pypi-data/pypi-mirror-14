e88cc2dbb492075c5d30bef850ee8340eced2b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:18 +0000

    Add package BytesOp/0.5/BytesOp-0.5.zip

---
+++ packages/BytesOp/BytesOp-0.5.zip/BytesOp-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+
+module = Extension("bytesop", sources=["bytesop.cpp"])
+setup(
+    name="BytesOp",
+    author="Jan Brohl",
+    author_email="janbrohl@t-online.de",
+    url="https://github.com/janbrohl/BytesOp",
+    version="0.5",
+    description="Module for bitwise operations on bytestrings.",
+    ext_modules=[module],
+    test_suite="test_bytesop.TestOps",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
