592bff14118bee99226daaeaa02e2e8e65f03557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:20 +0000

    Add package pgl_hello_world/0.0.1/pgl_hello_world-0.0.1.tar.gz

---
+++ packages/pgl_hello_world/pgl_hello_world-0.0.1.tar.gz/pgl_hello_world-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+name='pgl_hello_world',
+author='Daniel Mbaluka',
+author_email='danielmbaluka@palvac.co.ke',
+version='0.0.1',
+url='http://palvac.co.ke'
+    )
