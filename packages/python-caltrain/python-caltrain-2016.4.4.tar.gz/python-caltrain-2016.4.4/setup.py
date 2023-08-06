dab6d313f3c478f6abb421a7f3bbec2cb613c2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:38 +0000

    Add package python-caltrain/2016.4.4/python-caltrain-2016.4.4.tar.gz

---
+++ packages/python-caltrain/python-caltrain-2016.4.4.tar.gz/python-caltrain-2016.4.4/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+from io import open
+
+setup(
+    name='python-caltrain',
+    packages=find_packages(exclude=["tests"]),
+    version='2016.4.4',
+    description='A library for working with raw Caltrain scheduling'
+                'data in Python',
+    author='Dillon Dixon',
+    author_email='dillondixon@gmail.com',
+    url='https://github.com/ownaginatious/python-caltrain',
+    download_url='https://github.com/ownaginatious/python-caltrain'
+                 '/tarball/2016.4.4',
+    license='MIT',
+    keywords=['caltrain', 'python'],
+    zip_safe=True,
+    include_package_data=True,
+    exclude_package_data={'': ['README.rst', 'LICENSE']},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    install_requires=[line.strip()
+                      for line in open("requirements.txt", "r",
+                                       encoding="utf-8").readlines()],
+)
