3c2e0ffe4c2882bf385c564d536aeca8e0ec62fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package Heralding/0.1.6/Heralding-0.1.6.tar.gz

---
+++ packages/Heralding/Heralding-0.1.6.tar.gz/Heralding-0.1.6/heralding/honeypot.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2013 Johnny Vestergaard <jkv@unixcluster.dk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import _socket
+import logging
+
+
+import gevent
+import gevent.event
+from gevent import Greenlet
+from gevent.server import StreamServer
+
+
+from heralding.capabilities import handlerbase
+from heralding.reporting.file_logger import FileLogger
+from heralding.reporting.zmq_logger import ZmqLogger
+
+logger = logging.getLogger(__name__)
+
+
+class Honeypot(object):
+    def __init__(self, config):
+        """
+        :param config: configuration dictionary.
+        """
+        assert config != None
+        self.readyForDroppingPrivs = gevent.event.Event()
+        self.config = config
+        self._servers = []
+        self._server_greenlets = []
+
+    def start(self):
+        """ Starts services. """
+
+        # start activity logging
+        if 'activity_logging' in self.config:
+            if 'file' in self.config['activity_logging'] and self.config['activity_logging']['file']['enabled']:
+                logFile = self.config['activity_logging']['file']['filename']
+                FileLogger(logFile).start()
+            if 'zmq' in self.config['activity_logging'] and self.config['activity_logging']['zmq']['enabled']:
+                zmq_url = self.config['activity_logging']['zmq']['url']
+                client_pub_key = self.config['activity_logging']['zmq']['client_public_key']
+                client_secret_key = self.config['activity_logging']['zmq']['client_secret_key']
+                server_pub_key = self.config['activity_logging']['zmq']['server_public_key']
+                ZmqLogger(zmq_url, client_pub_key, client_secret_key, server_pub_key).start()
+
+        for c in handlerbase.HandlerBase.__subclasses__():
+
+            cap_name = c.__name__.lower()
+            if cap_name in self.config['capabilities']:
+                if not self.config['capabilities'][cap_name]['enabled']:
+                    continue
+                port = self.config['capabilities'][cap_name]['port']
+                # carve out the options for this specific service
+                options = self.config['capabilities'][cap_name]
+                # capabilities are only allowed to append to the session list
+                cap = c(options)
+
+                try:
+                    # Convention: All capability names which end in 's' will be wrapped in ssl.
+                    if cap_name.endswith('s'):
+                        server = StreamServer(('0.0.0.0', port), cap.handle_session,
+                                              keyfile=self.key, certfile=self.cert)
+                    else:
+                        server = StreamServer(('0.0.0.0', port), cap.handle_session)
+
+                    logger.debug('Adding {0} capability with options: {1}'.format(cap_name, options))
+                    self._servers.append(server)
+                    server_greenlet = Greenlet(server.start())
+                    self._server_greenlets.append(server_greenlet)
+
+                except _socket.error as ex:
+                    logger.error("Could not start {0} server on port {1}. Error: {2}".format(c.__name__, port, ex))
+                else:
+                    logger.info('Started {0} capability listening on port {1}'.format(c.__name__, port))
+
+        self.readyForDroppingPrivs.set()
+        gevent.joinall(self._server_greenlets)
+
+    def stop(self):
+        """Stops services"""
+
+        for s in self._servers:
+            s.stop()
+
+        for g in self._server_greenlets:
+            g.kill()
+
+        logger.info('All workers stopped.')
+
+    def blokUntilReadyForDroppingPrivs(self):
+        self.readyForDroppingPrivs.wait()
