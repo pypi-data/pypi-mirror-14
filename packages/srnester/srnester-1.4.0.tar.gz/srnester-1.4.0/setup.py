b6679fbea789883ec5da7daf25a04699d98641f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:28 +0000

    Add package srnester/1.4.0/srnester-1.4.0.tar.gz

---
+++ packages/srnester/srnester-1.4.0.tar.gz/srnester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'srnester',
+	version	= '1.4.0',
+	py_module	= ['srnester'],
+	author	= 'serin9',
+	author_email	= 'cheche_serin@naver.com',
+	url	= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+)
