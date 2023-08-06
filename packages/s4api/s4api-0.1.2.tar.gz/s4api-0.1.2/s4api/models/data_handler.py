0eef55e68098ca8a2ab73219d3a1a3ce56579584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package s4api/0.1.2/s4api-0.1.2.tar.gz

---
+++ packages/s4api/s4api-0.1.2.tar.gz/s4api-0.1.2/s4api/models/data_handler.py
@@ -0,0 +1,39 @@
+# S4 Python3 client library
+# Copyright 2016 Ontotext AD
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class DataHandler:
+    def __init__(self):
+        self.swaggerTypes = {
+            "data_source": "DataSource",
+            "name": "str",
+            "input_stream": "InputStream",
+            "content": "Object",
+            "content_type": "str",
+            "output_stream": "OutputStream",
+            "all_commands": "list[CommandInfo]",
+            "preferred_commands": "list[CommandInfo]",
+            "transfer_data_flavors": "list[DataFlavor]"
+
+        }
+        self.data_source = None  # DataSource
+        self.name = None  # str
+        self.input_stream = None  # InputStream
+        self.content = None  # Object
+        self.content_type = None  # str
+        self.output_stream = None  # OutputStream
+        self.all_commands = None  # list[CommandInfo]
+        self.preferred_commands = None  # list[CommandInfo]
+        self.transfer_data_flavors = None  # list[DataFlavor]
---
+++ packages/s4api/s4api-0.1.2.tar.gz/s4api-0.1.2/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+description = ("Ontotext Self-Service Semantic Suite's SDK " +
+               "for Python3. For more information, " +
+               "visit http://s4.ontotext.com")
+
+setup(
+    name="s4api",
+    version="0.1.2",
+    author="Svetlin Slavov",
+    author_email="svetlin.slavov@ontotext.com",
+
+    packages=["s4api", "s4api.models"],
+    include_package_data=True,
+
+    url="https://pypi.python.org/pypi/s4api",
+    license="Apache License 2.0",
+    description=description,
+
+    long_description=readme,
+
+    install_requires=[
+        "requests",
+    ],
+)
