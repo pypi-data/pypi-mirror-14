1fdce595cdef51da1fc21ee70105d715e573a10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:34 +0000

    Add package backlog/0.1.1/backlog-0.1.1.tar.gz

---
+++ packages/backlog/backlog-0.1.1.tar.gz/backlog-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+import backlog
+
+setup(
+    name='backlog',
+    version=backlog.__version__,
+    description=backlog.__doc__,
+    license='GPL',
+    url='https://github.com/dmtucker/backlog',
+    author='David Tucker',
+    author_email='david.michael.tucker@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='notes backlog todo lists',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    entry_points={'console_scripts': ['backlog=backlog.__main__:main']},
+)
