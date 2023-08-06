553d8b8f4a16cbaf5d05af8515b06828dbf3fee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:13 +0000

    Add package zen_sj_nester/1.2.0/zen_sj_nester-1.2.0.zip

---
+++ packages/zen_sj_nester/zen_sj_nester-1.2.0.zip/zen_sj_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name	='zen_sj_nester',
+version	='1.2.0',
+py_modules	=['nester'],
+author	='sjpython',
+author_email	='275201600@qq.com',
+url				='https://pypi.python.org/id/sjpython',
+description		='A simple printer of nester lists',
+)
