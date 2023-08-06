5be91fdcb5fc925eee34b8b7af5ce2ee06e7fc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:32 +0000

    Add package backslant/0.3.7/backslant-0.3.7-py3-none-any.whl

---
+++ packages/backslant/backslant-0.3.7-py3-none-any.whl/backslant-0.3.7.dist-info/DESCRIPTION.rst
@@ -0,0 +1,141 @@
+Backslant
+=========
+
+Is a template engine built in completely other way then before.
+
+First - you can use all python and you must use it if you want something more
+then just tags.
+
+Second - it completely iterative. You can feed iterators or generators as input and get iterative output.
+
+Third - it works through imports. If you want to get template just import it and use. If you want include
+other template - import it. If you want template in some dir, import it! Like ```from . import other_template```.
+Use absolute or relative imports.
+
+So, with this principles in mind, you can try backslant:
+
+    import backslant
+    # you need create __init__.py in templates folder to make this work
+    sys.meta_path.insert(0, backslant.BackslantFinder())
+
+    from mypkg.templates.home import index
+
+    for chunk in index.render(title='The Real Thing'):
+        print(chunk)
+
+Or, if you want send rendered html to browser:
+
+    from backslant import to_string
+    to_string(index.render(title='The Real Thing'))
+
+And templates/home/index.bs:
+
+    html
+        head
+            title
+                = options['title']
+        body
+            div.content#content
+                h1#header "Header"
+
+You can define a function:
+
+    - def render_form(method):
+        form(method=method)
+            input(type="text" value="123")
+
+End call it:
+
+    h1
+        == render_form('POST')
+
+Yes, its this simple, you just use python constructions. There `==` is shotrcut for `- yield from`.
+And `=` is a shortcut for `- yield`.
+
+And for now inheritance of templates you can made just with function.
+
+base.bs:
+
+    !doctype/ html
+    html
+    head
+        title "Page Title"
+    body
+        h1{'class': ' '.join(['main', 'content'], 'ng-app': 'Application'}
+            | Page Header
+        div.content
+            == options['content_block']()
+        div.footer
+            | Backslant © 2015
+
+index.bs:
+
+    - from . import base
+    - def content():
+        - for i in range(10):
+            p
+                = 'Paragraph {}'.format(i)
+    == base.render(content_block=content)
+
+I think about adding something like ruby blocks or something to made this a bit more simpler, but
+what can be more simple then functions define and call?
+
+But we have syntax sugar for this:
+
+    :call base.render(*options)
+        :content_block
+            - for i in range(10):
+                p
+                    = 'Paragraph {}'.format(i)
+        :footer_block
+            p "Index page"
+
+Arguments
+---------
+
+To define tag arguments you can use arg=`parentised python expression or variable name` or
+`tag.class {'a': 5, 'b': ' '.join(options.classes)}` form.
+
+
+Render or not render?
+---------------------
+
+When template compiled, we need it to place in module somehow. If you have any tags or calls in top level,
+then we definitely must place them into function. And we create `render` function for this purpose.
+Then you import template and call this `render`.
+
+But if you have not in top level, then will yield anything, then function is not needed - you can create library file.
+So - if you template on top level only defines functions and imports, then backslant will not implicitly cover
+it in `render` function, and this is the way to define your template libs. You can even distribute it on PyPi.
+
+
+Flask
+-----
+
+If you want to integrate backslant into existing project, it can be painful to rewrite all templates. So
+we have workaround:
+
+    from backslant.flask import extend_jinja2, include_jinja2
+
+And call it in template:
+
+    - from backslant.flask import extend_jinja2, include_jinja2
+
+    :call extend_jinja2('layouts/base.html')
+        - def content(ctx):
+            == include_jinja2('layouts/header.html')
+            div.container
+                div.page-header
+                    h1
+                        = options['company'].alias
+
+
+Afterwords
+----------
+
+I have completed examples with flask and http.server in examples folder. And you can compare perfomance with jinja2.
+Its almost equal.
+
+I will complete feature set soon, stay tuned.
+
+
---
+++ packages/backslant/backslant-0.3.7-py3-none-any.whl/backslant/parser.py
@@ -0,0 +1,129 @@
+from funcparserlib.parser import many, maybe, skip, some, forward_decl, NoParseError
+from funcparserlib.lexer import Token
+
+
+def combine_to_token(toks):
+    first = None
+    last = None
+    def inner(tks):
+        nonlocal first, last
+        if tks is None:
+            return
+        if isinstance(tks, str):
+            yield tks
+            return
+        if isinstance(tks, Token):
+            if tks.value not in ('INDENT', 'DEDENT'):
+                yield tks.value
+            if not first:
+                first = tks
+            last = tks
+            return
+        for tok in tks:
+            yield from inner(tok)
+    return Token('combine', ''.join(inner(toks)), start=(first.start if first else None), end=(last.end if last else None))
+
+
+def combine(toks):
+    def inner(tks):
+        if tks is None:
+            return
+        if isinstance(tks, str):
+            yield tks
+            return
+        if isinstance(tks, Token):
+            if tks.value not in ('INDENT', 'DEDENT'):
+                yield tks.value
+            return
+        for tok in tks:
+            yield from inner(tok)
+    return ''.join(inner(toks))
+
+
+sometok = lambda s: some(lambda tok: tok.value == s)
+sometype = lambda t: some(lambda tok: tok.type == t)
+newline = skip(sometype('NEWLINE'))
+name = sometype('NAME')
+space = sometype('SPACE')
+indent = sometype('INDENT')
+dedent = sometype('DEDENT')
+python_name = name + many(sometok('.') + name)
+to_end_of_line = many(some(lambda token: token.type != 'NEWLINE')) >> combine
+take_value = lambda tok: tok.value
+skip_space = skip(maybe(space))
+skip_whitespace = skip(many(space | newline | indent | dedent))
+
+def paren_match(start, end=None):
+    if not end:
+        end = start
+    parens = forward_decl()
+    no_paren_dent_nl = some(lambda tok: not tok.value in (start, end))
+    parens.define(sometok(start) + many(no_paren_dent_nl | parens) + sometok(end))
+    return parens
+
+
+python_call = (sometok('-') + skip_space + to_end_of_line) >> (lambda toks: ('python', toks[0].start, toks[1], tuple()))
+python_yield_from_call = (skip(sometok('=')) + sometok('=') + skip_space + to_end_of_line) >> (lambda toks: ('python_yield_from', toks[0].start, toks[1]))
+python_yield_call = (sometok('=') + skip_space + to_end_of_line) >> (lambda toks: ('python_yield', toks[0].start, toks[1]))
+
+
+macro = (skip(sometok(':')) + sometype('NAME') + to_end_of_line) >> (lambda toks: ('macro', toks[0].start, toks[0].value, toks[1]))
+string = (sometok('|') + to_end_of_line) >> (lambda tok: ('string', tok[0].start, tok[1]))
+string = string | sometype('STRING') >> (lambda tok: ('string', tok.start, tok.value))
+text = sometype('STRING') >> take_value
+
+
+# HTML attr
+html_name = name + many(sometok('-') + name)
+html_name_str = html_name >> combine
+attribute_value = (sometype('STRING') >> take_value) | ((paren_match('(', ')') | python_name) >> combine)
+single_attr = html_name_str
+pair_attr = (html_name_str + sometok('=') + attribute_value) >> (lambda toks: (toks[0], toks[2]))
+attribute = pair_attr | single_attr
+attributes = (
+    ((sometok('(') + skip_whitespace + many(attribute + skip_whitespace) + sometok(')')) >> (lambda toks: toks[1]))
+    | (paren_match('{', '}') >> combine)
+)
+dynamic_attrs = skip(sometok('*') + sometok('*')) + name >> (lambda tok: tok.value)
+tag_class = (sometok('.') + html_name_str) >> (lambda toks: ('class', toks[1]))
+tag_id = skip(sometok('#')) + html_name_str >> (lambda tok: ('id', tok))
+tag_name = (maybe(sometok('!')) + html_name + maybe(sometok('/'))) >> combine_to_token
+tag = (
+    tag_name
+    + many(tag_class | tag_id)
+    + maybe(skip_space + attributes)
+    + skip_space
+    + maybe(dynamic_attrs)
+    + skip_space
+    + maybe(text)
+    >> (lambda toks: ('tag', toks[0].start, [toks[0].value] + list(toks[1:])))
+)
+
+
+# lines and blocks
+definition = (tag | python_yield_from_call | python_yield_call | python_call | macro | string)
+line = forward_decl()
+block = (skip(sometype('INDENT')) + many(line) + skip(sometype('DEDENT')))
+
+
+# py blocks
+def py_expr(name):
+    expr = (sometok('-') + skip_space + sometok(name) + to_end_of_line) >> (lambda toks: ('python', toks[0].start, toks[1].value + toks[2]))
+    return skip_space + expr + skip(newline) + block
+
+def py_expr_combine(tok):
+    return (tok[0] + (tok[2:],), tok[1])
+
+block_for = py_expr('for') + maybe(py_expr('else'))
+block_while = py_expr('while') + maybe(py_expr('else'))
+block_if = py_expr('if') + many(py_expr('elif')) + maybe(py_expr('else'))
+block_try = py_expr('try') + many(py_expr('except')) + maybe(py_expr('else')) + maybe(py_expr('finally'))
+py_definition = (block_for | block_while | block_try | block_if) >> py_expr_combine
+
+
+# try python blocks first
+line.define(py_definition | (skip_space + maybe(definition) + newline + maybe(block)))
+
+
+bs_parser = (many(line) + skip(sometype('END')))
+
