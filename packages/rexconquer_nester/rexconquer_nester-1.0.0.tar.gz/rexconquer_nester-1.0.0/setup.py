4d5b8ffb18307bbb74b4f5f9162ca8576c4670c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:20 +0000

    Add package rexconquer_nester/1.0.0/rexconquer_nester-1.0.0.tar.gz

---
+++ packages/rexconquer_nester/rexconquer_nester-1.0.0.tar.gz/rexconquer_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name 	 = 'rexconquer_nester',
+    version	 = '1.0.0',
+    py_modules 	 = ['nester'],
+    author  	 = 'Rex',
+    author_email = 'Rex@1cloudtech.com',
+    url		 = 'www.1cloudtech.com',
+    description	 = 'A simple printer of nested lists',
+    )
