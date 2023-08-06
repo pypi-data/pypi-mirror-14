fb8ddace67e54daa1712ecfd8505582cd6bd2443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:31 +0000

    Add package scrawl/1.0.5/scrawl-1.0.5.zip

---
+++ packages/scrawl/scrawl-1.0.5.zip/scrawl-1.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+setup(
+    name='scrawl',
+    version='1.0.5',
+    description='A python note-taking console app ',
+    long_description="",
+    url='https://github.com/ten-c/bc-6-scrawl',
+    author='Ten Chege',
+    author_email='10.chege@gmail.com',
+    keywords='sample setuptools development',
+    # py_modules=['scrawl','scrawl2'],
+    packages=find_packages(),
+    package_data={
+        'scrawl': ['data.db'],
+    },
+    include_package_data = True,
+    install_requires=[
+        'Click',
+        'python-firebase',
+        'colorama',
+        # 'sqlite'
+    ],
+    entry_points='''
+        [console_scripts]
+        scrawl=scrawl.cli:scrawl
+        scrawl2=scrawl.cli:scrawl2
+    ''',
+)
+
+# packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+# package_data={
+#     'sample': ['package_data.dat'],
+# },
+# data_files=[('my_data', ['data/data_file'])],
