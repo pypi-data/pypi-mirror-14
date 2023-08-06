678000eef6652bd3cd2bf68df4e474aa57b0711a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:59 +0000

    Add package lawliet/2.2.8/lawliet-2.2.8.tar.gz

---
+++ packages/lawliet/lawliet-2.2.8.tar.gz/lawliet-2.2.8/lawliet/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from .app import Route
+from .handler.response import Res
+from .tools.cache import DoCache
+from .tools._cache import _DoCache
+from .tools.json_loads import str_json
+
+
+def Response(res=None, status=None, headers=None):
+    return Res(res, status, headers).response()
+
+
+def abort(code, res=None):
+    if code == 404:
+        res = {"errcode": 404, "errmsg": "page not find"}
+    elif code == 500:
+        res = {"errcode": 500, "errmsg": "page error"}
+    return Response(res=res, status=code)
+
+
+def redirect(url):
+    return Response(status=301, headers={"Location": url})
+
+
+def jsons(str):
+    return str_json(str)
+
+
+class Routes(object):
+    def __init__(self, *args):
+        Route.urls = args
+
+
+class Cache(DoCache):
+    pass
+
+
+class _Cache(_DoCache):
+    pass
