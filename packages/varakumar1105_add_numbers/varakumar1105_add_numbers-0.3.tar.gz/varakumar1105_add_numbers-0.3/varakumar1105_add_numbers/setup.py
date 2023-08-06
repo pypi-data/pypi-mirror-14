c6b92e9c6320499e13120d4ae4c11316df95eaa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:57 +0000

    Add package varakumar1105_add_numbers/0.3/varakumar1105_add_numbers-0.3.tar.gz

---
+++ packages/varakumar1105_add_numbers/varakumar1105_add_numbers-0.3.tar.gz/varakumar1105_add_numbers-0.3/varakumar1105_add_numbers/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='varakumar1105_add_numbers',
+      version='0.3',
+      description='Sample to add two numbers',
+      url='https://github.com/varakumar1105/varakumar1105_add_numbers',
+      author='VaraKumarJagarapu',
+      author_email='vignesh.varakumar@gmail.com',
+      license='MIT',
+      packages=['varakumar1105_add_numbers'],
+      zip_safe=False)
+#My First Commit with joke function
