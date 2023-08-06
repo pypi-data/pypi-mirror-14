4ffe46e5502767551e450e6d3b0abd678d67526d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:36 +0000

    Add package django-contactform-bootstrap/0.5.11/django_contactform_bootstrap-0.5.11.tar.gz

---
+++ packages/django-contactform-bootstrap/django_contactform_bootstrap-0.5.11.tar.gz/django_contactform_bootstrap-0.5.11/contact_form_bootstrap/settings.py
@@ -0,0 +1,25 @@
+__author__ = 'alainivars'
+
+ADMINS = (
+    ('Alain Ivars', 'contact@mydomain.com'),
+)
+
+CONTACT_FORM_SUBJECT_TEMPLATE_NAME = 'email_subject.txt'
+CONTACT_FORM_MESSAGE_TEMPLATE_NAME = 'email_message.txt'
+
+COMPANY_INFOS = {
+    'NAME': "my company",
+    'ADDRESS': "26 streets from here th there",
+    'ZIP': "1234",
+    'CITY': "Maybe-there",
+    'LAT': 48.81484460000001,
+    'LNG': 2.0523723999999675,
+    'PHONE': "+336 1234 5678",
+    'EMAIL': 'contact@mycompany.com',
+    'FACEBOOK': "Maybe-there",
+    'LINKEDIN': "Maybe-there",
+    'TWITTER': "Maybe-there",
+    'GOOGLEPLUS': "+Maybe-there",
+}
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
