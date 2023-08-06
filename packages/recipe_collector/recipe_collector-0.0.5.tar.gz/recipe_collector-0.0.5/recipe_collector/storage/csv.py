5ba802703bfbbc0845947f7b860d0fcb1c94659e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:16 +0000

    Add package recipe_collector/0.0.5/recipe_collector-0.0.5.tar.gz

---
+++ packages/recipe_collector/recipe_collector-0.0.5.tar.gz/recipe_collector-0.0.5/recipe_collector/storage/csv.py
@@ -0,0 +1,26 @@
+import csv
+from recipe_collector.utils.decorators import coroutine
+
+
+@coroutine
+def urls_csv(file_path, mode):
+    """
+    This coroutine receives tuples of title and url values and saves them to a CSV file.
+    """
+    with open(file_path, mode) as f:
+        csv_file = csv.writer(f, delimiter=',')
+        while True:
+            source_url = yield
+            csv_file.writerow([source_url])
+
+
+@coroutine
+def html_csv(file_path, mode):
+    """
+    This coroutine receives tuples of url and HTML values and saves them to a CSV file.
+    """
+    with open(file_path, mode) as f:
+        csv_file = csv.writer(f, delimiter=',', quotechar="'")
+        while True:
+            source_url, html = yield
+            csv_file.writerow([source_url, html])
