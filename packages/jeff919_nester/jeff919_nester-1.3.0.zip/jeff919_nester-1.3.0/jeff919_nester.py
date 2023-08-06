f3829b176e0ccd16d14132ea5675392fafb5f3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:41 +0000

    Add package jeff919_nester/1.3.0/jeff919_nester-1.3.0.zip

---
+++ packages/jeff919_nester/jeff919_nester-1.3.0.zip/jeff919_nester-1.3.0/jeff919_nester.py
@@ -0,0 +1,19 @@
+"""This is the nester.py module and it provides one function called
+printed print_lol() which prints lists that may or
+may not include nested lists."""
+
+def print_lol(the_list,indent=False, level=0):
+     """This function takes a positional argument called "the list",
+which is any Python list (of - possibly - nester lists). Each data item in
+the provide list is (recursively) ptinted to the screen on it`s own line.
+A second argument call "level" is used to insert tab-stop which a nested list
+is encountered."""
+     
+     for each_item in the_list:
+         if isinstance(each_item,list):
+             print_lol(each_item, indent , level+1)
+         else:
+              if indent:
+                   for tab_stop in range(level):
+                        print("\t", end='')
+              print(each_item)
