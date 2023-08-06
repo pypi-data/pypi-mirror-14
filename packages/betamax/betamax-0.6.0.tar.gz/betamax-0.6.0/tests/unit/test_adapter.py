06431b7d1d2b10a83122d020502b2f8f1335b93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:57 +0000

    Add package betamax/0.6.0/betamax-0.6.0.tar.gz

---
+++ packages/betamax/betamax-0.6.0.tar.gz/betamax-0.6.0/tests/unit/test_adapter.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import unittest
+
+# sys.path.insert(0, os.path.abspath('.'))
+# sys.stderr.write('%s' % str(sys.path))
+
+from betamax.adapter import BetamaxAdapter
+from requests.adapters import HTTPAdapter
+
+
+class TestBetamaxAdapter(unittest.TestCase):
+    def setUp(self):
+        self.adapter = BetamaxAdapter()
+
+    def tearDown(self):
+        self.adapter.eject_cassette()
+
+    def test_has_http_adatper(self):
+        assert self.adapter.http_adapter is not None
+        assert isinstance(self.adapter.http_adapter, HTTPAdapter)
+
+    def test_empty_initial_state(self):
+        assert self.adapter.cassette is None
+        assert self.adapter.cassette_name is None
+        assert self.adapter.serialize is None
+
+    def test_load_cassette(self):
+        filename = 'test'
+        self.adapter.load_cassette(filename, 'json', {
+            'record': 'none',
+            'cassette_library_dir': 'tests/cassettes/'
+        })
+        assert self.adapter.cassette is not None
+        assert self.adapter.cassette_name == filename
+
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.abspath('../..'))
+    unittest.main()
---
+++ packages/betamax/betamax-0.6.0.tar.gz/betamax-0.6.0/tests/unit/test_configure.py
@@ -0,0 +1,42 @@
+import copy
+import unittest
+
+from betamax.configure import Configuration
+from betamax.cassette import Cassette
+
+
+class TestConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.cassette_options = copy.deepcopy(
+            Cassette.default_cassette_options
+            )
+        self.cassette_dir = Configuration.CASSETTE_LIBRARY_DIR
+
+    def tearDown(self):
+        Cassette.default_cassette_options = self.cassette_options
+        Configuration.CASSETTE_LIBRARY_DIR = self.cassette_dir
+
+    def test_acts_as_pass_through(self):
+        c = Configuration()
+        c.default_cassette_options['foo'] = 'bar'
+        assert 'foo' in Cassette.default_cassette_options
+        assert Cassette.default_cassette_options.get('foo') == 'bar'
+
+    def test_sets_cassette_library(self):
+        c = Configuration()
+        c.cassette_library_dir = 'foo'
+        assert Configuration.CASSETTE_LIBRARY_DIR == 'foo'
+
+    def test_is_a_context_manager(self):
+        with Configuration() as c:
+            assert isinstance(c, Configuration)
+
+    def test_allows_registration_of_placeholders(self):
+        opts = copy.deepcopy(Cassette.default_cassette_options)
+        c = Configuration()
+
+        c.define_cassette_placeholder('<TEST>', 'test')
+        assert opts != Cassette.default_cassette_options
+        placeholders = Cassette.default_cassette_options['placeholders']
+        assert placeholders[0]['placeholder'] == '<TEST>'
+        assert placeholders[0]['replace'] == 'test'
