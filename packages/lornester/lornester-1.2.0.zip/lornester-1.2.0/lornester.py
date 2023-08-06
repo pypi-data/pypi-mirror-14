e58ef3d467f9a0dbc0a61952f9de35d01a93877d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:41 +0000

    Add package lornester/1.2.0/lornester-1.2.0.zip

---
+++ packages/lornester/lornester-1.2.0.zip/lornester-1.2.0/lornester.py
@@ -0,0 +1,15 @@
+# This is the nester module, simply 
+# for studying the upload onto PyPI and 
+# how to use it inside own computer.
+
+def print_lol(the_list, level=0):
+    # This function is for printing all 
+    # items inside a list, even the items 
+    # in nested lists.
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print ("\t", end ='')
+            print(each_item)
