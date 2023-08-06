b8b33fe52415a947cfcc37aebbbd7a2869aa14f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:49 +0000

    Add package django-icomoon/0.4.0/django-icomoon-0.4.0.tar.gz

---
+++ packages/django-icomoon/django-icomoon-0.4.0.tar.gz/django-icomoon-0.4.0/icomoon/__init__.py
@@ -0,0 +1,4 @@
+"""
+A Django app to deploy wefonts from Icomoon and display them
+"""
+__version__ = '0.4.0'
---
+++ packages/django-icomoon/django-icomoon-0.4.0.tar.gz/django-icomoon-0.4.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-icomoon',
+    version=__import__('icomoon').__version__,
+    description=__import__('icomoon').__doc__,
+    long_description=open('README.rst').read(),
+    author='David Thenon',
+    author_email='sveetch@gmail.com',
+    url='https://github.com/sveetch/django-icomoon',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        "Framework :: Django :: 1.8",
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'django-braces>=1.2.0',
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
