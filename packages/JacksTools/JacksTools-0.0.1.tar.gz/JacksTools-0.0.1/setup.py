055e7109bc250a0291f03b3da6d3588e0ccf0ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package JacksTools/0.0.1/JacksTools-0.0.1.tar.gz

---
+++ packages/JacksTools/JacksTools-0.0.1.tar.gz/JacksTools-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name = 'JacksTools', 
+version = '0.0.1',
+description = 'Collection of Tools that I commonly use',
+url = 'http://github.com/Rodot-/',
+author = 'John (Jack) T O\'Brien',
+author_email = 'john.t.obrien@drexel.edu',
+license = 'GNU',
+scripts = ['JacksTools/jmeans.py', 'JacksTools/jio.py', 'JacksTools/transformations.py'],
+packages = find_packages(),
+install_requires = ['numpy>=1.6.0','eventlet>=0.18.4','matplotlib>=1.5.1', 'astropy>=1.1.1'],
+zip_safe = False)
+
+
