18d303fdf321a25b67a96fc5733b8ece68df0b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:22 +0000

    Add package larlib/0.3.2/larlib-0.3.2.tar.gz

---
+++ packages/larlib/larlib-0.3.2.tar.gz/larlib-0.3.2/larlib/__init__.py
@@ -0,0 +1,31 @@
+import scipy
+import pyplasm
+
+from scipy import *
+from pyplasm import *
+#from triangle import *
+#from support import *
+from copy import copy
+from p2t import *
+
+from myfont import *
+from larstruct import *
+from lar2psm import *
+from simplexn import *
+from largrid import *
+from boundary import *
+from larcc import *
+from integr import *
+from inters import *
+from triangulation import *
+from architectural import *
+from boolean import *
+from hijson import *
+from iot3d import *
+from hospital import *
+from mapper import *
+from morph import *
+from splines import *
+from splitcell import *
+#from support import *
+from sysml import *
