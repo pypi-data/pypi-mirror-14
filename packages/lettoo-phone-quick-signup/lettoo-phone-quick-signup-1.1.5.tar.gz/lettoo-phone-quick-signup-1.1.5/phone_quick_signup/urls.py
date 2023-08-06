c694cd9f4f1df5c67920688c204bcad5aacb4830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:48 +0000

    Add package lettoo-phone-quick-signup/1.1.5/lettoo-phone-quick-signup-1.1.5.tar.gz

---
+++ packages/lettoo-phone-quick-signup/lettoo-phone-quick-signup-1.1.5.tar.gz/lettoo-phone-quick-signup-1.1.5/phone_quick_signup/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from .views import RegisterView, VerifyPhoneView, ResendPhoneView, PasswordResetView, PasswordResetConfirmView, \
+    ChangePhoneView
+
+urlpatterns = [
+    url(r'^$', RegisterView.as_view(), name='phone_quick_register'),
+    url(r'^verify-phone/$', VerifyPhoneView.as_view(), name='phone_quick_verify'),
+    url(r'^resend-phone/$', ResendPhoneView.as_view(), name='phone_quick_resend'),
+    url(r'^change-phone/$', ChangePhoneView.as_view(), name='phone_quick_change'),
+    url(r'^password/reset/$', PasswordResetView.as_view(), name='phone_quick_password_reset'),
+    url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(), name='phone_quick_password_reset_confirm'),
+]
