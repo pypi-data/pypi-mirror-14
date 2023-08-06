619e002ddb3af2ef2f97a9dbefee5186b7a97ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package ores/0.7.3/ores-0.7.3-py2.py3-none-any.whl

---
+++ packages/ores/ores-0.7.3-py2.py3-none-any.whl/ores/ores.py
@@ -0,0 +1,46 @@
+"""
+This script provides access to a set of utilities for ORES
+
+* label_reverted -- Labels reverted revisions
+* dev_server -- Starts a development WSGI server
+* celery_worker -- Starts a "ScoreProcessor" celery worker
+* precached -- Starts a daemon that requests scores for revisions as they happen
+
+{usage}
+Options:
+    -h | --help  Shows this documentation
+    <utility>    The name of the utility to run
+"""
+import sys
+import traceback
+from importlib import import_module
+
+
+USAGE = """Usage:
+    {progname} (-h | --help)
+    {progname} <utility> [-h | --help]
+""".format(progname=sys.argv[0])
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        sys.stderr.write(USAGE)
+        sys.exit(1)
+    elif sys.argv[1] in ("-h", "--help"):
+        sys.stderr.write(__doc__.format(usage=USAGE))
+        sys.exit(1)
+    elif sys.argv[1][:1] == "-":
+        sys.stderr.write(USAGE)
+        sys.exit(1)
+
+    module_name = sys.argv[1]
+    try:
+        sys.path.insert(0, ".")
+        module = import_module(".utilities." + module_name, package="ores")
+    except ImportError:
+        sys.stderr.write(traceback.format_exc())
+        sys.stderr.write("Could not find utility {0}.\n".format(module_name))
+        sys.exit(1)
+
+    module.main(sys.argv[2:])
