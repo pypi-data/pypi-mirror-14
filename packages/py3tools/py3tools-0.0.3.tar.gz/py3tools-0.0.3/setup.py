c34782784416480990e06170ef84b7b0991e10e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:21 +0000

    Add package py3tools/0.0.3/py3tools-0.0.3.tar.gz

---
+++ packages/py3tools/py3tools-0.0.3.tar.gz/py3tools-0.0.3/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name="py3tools",
+    version="0.0.3",
+    description="Python3 tools",
+    author='Chaluemwut Noyunsan',
+    packages=["py3tools"],
+    url='https://github.com/chaluemwut/pytools.git',
+    download_url='https://github.com/chaluemwut/pytools.git'
+)
