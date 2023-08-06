e3569001d5b70e1bb9e573fcf04ebef73afa5d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package pyramid-sqlalchemy-continuum/0.1/pyramid-sqlalchemy-continuum-0.1.tar.gz

---
+++ packages/pyramid-sqlalchemy-continuum/pyramid-sqlalchemy-continuum-0.1.tar.gz/pyramid-sqlalchemy-continuum-0.1/pyramid_sqlalchemy_continuum/__init__.py
@@ -0,0 +1,6 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Jordi Fernández <jfernandez@bioiberica.com>
+"""
+from .plugin import PyramidPlugin
---
+++ packages/pyramid-sqlalchemy-continuum/pyramid-sqlalchemy-continuum-0.1.tar.gz/pyramid-sqlalchemy-continuum-0.1/setup.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Jordi Fernández <jfernandez@bioiberica.com>
+"""
+from setuptools import setup
+
+setup(name='pyramid-sqlalchemy-continuum',
+      version='0.1',
+      description='Integration of SQLAlchemy-Continuum with Pyramid',
+      url='https://bitbucket.org/jfdezbio/pyramid-sqlalchemy-continuum',
+      author=u'Jordi Fernández',
+      author_email='jfernandez@bioiberica.com',
+      license='MIT',
+      packages=['pyramid_sqlalchemy_continuum'],
+      zip_safe=False)
