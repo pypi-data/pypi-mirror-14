bd7d0b7ec72ec120ec9789e4d7b53b18d72a4dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package textacy/0.2.0/textacy-0.2.0-py2.py3-none-any.whl

---
+++ packages/textacy/textacy-0.2.0-py2.py3-none-any.whl/textacy/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+import logging
+
+# subpackages
+from textacy import corpora
+from textacy import fileio
+from textacy import representations
+from textacy import tm
+# top-level modules
+from textacy import compat, data, math_utils, regexes_etc
+from textacy import lexicon_methods, preprocess, text_stats, text_utils
+from textacy import spacy_utils
+from textacy import extract
+from textacy import export, keyterms
+from textacy import texts
+
+from textacy.texts import TextDoc, TextCorpus
+
+__version__ = '0.2.0'
+
+logger = logging.getLogger('textacy')
+if len(logger.handlers) == 0:  # To ensure reload() doesn't add another one
+    logger.addHandler(logging.NullHandler())
