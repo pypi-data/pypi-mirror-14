8e95511bf9c8a2f61b37f822e8233486ecff7ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:55 +0000

    Add package monasca-analytics/0.0.1/monasca-analytics-0.0.1.tar.gz

---
+++ packages/monasca-analytics/monasca-analytics-0.0.1.tar.gz/monasca-analytics-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'monasca-analytics',
+  packages = ['monasca_analytics'],
+  version = '0.0.1',
+  description = 'Monasca Analytics and Anomaly Engine',
+  author = 'OpenStack',
+  author_email = 'roland.hochmuth@hpe.com',
+  keywords = ['monitoring'],
+  classifiers = [],
+)
