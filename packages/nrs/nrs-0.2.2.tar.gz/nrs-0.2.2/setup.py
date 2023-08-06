5e1f5b62d5cd59b0b2079b8623f3ba57992543df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:07 +0000

    Add package nrs/0.2.2/nrs-0.2.2.tar.gz

---
+++ packages/nrs/nrs-0.2.2.tar.gz/nrs-0.2.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+bzlib = Extension('nrs.ext.bzlib._bzlib', [
+    'nrs/ext/bzlib/bzlib.i',
+    'nrs/ext/bzlib/bzlib.c',
+    'nrs/ext/bzlib/decompress.c',
+    'nrs/ext/bzlib/huffman.c'
+], depends=['nrs/ext/bzlib/bzlib.h'])
+
+lzma = Extension('nrs.ext.lzma._lzma', [
+    'nrs/ext/lzma/lzma.i',
+    'nrs/ext/lzma/LZMADecode.c',
+], depends=['nrs/ext/lzma/LZMADecode.h'])
+
+setup(name='nrs',
+      version='0.2.2',
+      description='NSIS Reversing Suite',
+      long_description_markdown_filename='README.md',
+      author='isra17',
+      author_email='isra017@gmail.com',
+      url='https://github.com/isra17/nrs',
+      packages=['nrs','nrs.ext', 'nrs.ext.bzlib', 'nrs.ext.lzma', 'nrs.ida', \
+                'nrs.strings'],
+
+      install_requires=['future'],
+      setup_requires=['pytest-runner', 'setuptools-markdown'],
+      tests_require=['pytest'],
+      ext_modules=[bzlib, lzma]
+    )
+
