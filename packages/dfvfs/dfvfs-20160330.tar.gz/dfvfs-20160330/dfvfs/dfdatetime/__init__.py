ba6f7ba72b6a8305da9a2fa40af42c97af89f3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package dfvfs/20160330/dfvfs-20160330.tar.gz

---
+++ packages/dfvfs/dfvfs-20160330.tar.gz/dfvfs-20160330/dfvfs/dfdatetime/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Imports for the date and time values."""
---
+++ packages/dfvfs/dfvfs-20160330.tar.gz/dfvfs-20160330/dfvfs/file_io/vhdi_file_io.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""The VHD image file-like object."""
+
+import pyvhdi
+
+from dfvfs import dependencies
+from dfvfs.file_io import file_object_io
+from dfvfs.lib import errors
+from dfvfs.resolver import resolver
+
+
+dependencies.CheckModuleVersion(u'pyvhdi')
+
+
+class VHDIFile(file_object_io.FileObjectIO):
+  """Class that implements a file-like object using pyvhdi."""
+
+  def _OpenFileObject(self, path_spec):
+    """Opens the file-like object defined by path specification.
+
+    Args:
+      path_spec: the path specification (instance of PathSpec).
+
+    Returns:
+      A file-like object.
+
+    Raises:
+      PathSpecError: if the path specification is incorrect.
+    """
+    if not path_spec.HasParent():
+      raise errors.PathSpecError(
+          u'Unsupported path specification without parent.')
+
+    # TODO: add parent image support.
+    file_object = resolver.Resolver.OpenFileObject(
+        path_spec.parent, resolver_context=self._resolver_context)
+
+    vhdi_file = pyvhdi.file()
+    vhdi_file.open_file_object(file_object)
+
+    return vhdi_file
+
+  def get_size(self):
+    """Returns the size of the file-like object.
+
+    Raises:
+      IOError: if the file-like object has not been opened.
+    """
+    if not self._is_open:
+      raise IOError(u'Not opened.')
+
+    return self._file_object.get_media_size()
