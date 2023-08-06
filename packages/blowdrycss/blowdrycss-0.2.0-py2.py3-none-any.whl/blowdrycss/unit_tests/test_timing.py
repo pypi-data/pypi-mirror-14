54f56c378e11ecf4bcc5319dd5a56be6734515e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:09 +0000

    Add package blowdrycss/0.2.0/blowdrycss-0.2.0-py2.py3-none-any.whl

---
+++ packages/blowdrycss/blowdrycss-0.2.0-py2.py3-none-any.whl/blowdrycss/unit_tests/test_timing.py
@@ -0,0 +1,81 @@
+# python 2
+from __future__ import absolute_import
+
+# builtins
+from unittest import TestCase, main
+import sys
+from datetime import datetime
+from time import time
+from string import digits
+from io import StringIO
+
+# custom
+from blowdrycss.timing import Timer
+
+__author__ = 'chad nelson'
+__project__ = 'blowdrycss'
+
+
+class TestTiming(TestCase):
+    def test_seconds_to_string(self):
+        allowed = set(digits + '.')
+        timer = Timer()
+        time_string = timer.seconds_to_string(seconds_elapsed=time())
+        self.assertTrue(set(time_string) <= allowed)
+
+    def test_elapsed_end_set(self):
+        allowed = set(digits + '.')
+        timer = Timer()
+        timer.end = time()
+        self.assertTrue(set(timer.elapsed) <= allowed)
+
+    def test_elapsed_end_not_set(self):
+        allowed = set(digits + '.')
+        timer = Timer()
+        self.assertTrue(set(timer.elapsed) <= allowed)
+
+    def test_print_time(self):
+        timer = Timer()
+
+        # Checks variable date components and constant string output.
+        substrings = [
+            'Completed ', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
+            str(datetime.now().day)
+        ]
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            timer.print_time()
+
+            output = out.getvalue()
+            for substring in substrings:
+                self.assertTrue(substring in output, msg=substring)
+        finally:
+            sys.stdout = saved_stdout
+
+    def test_report(self):
+        timer = Timer()
+
+        # Checks variable date components and constant string output.
+        substrings = [
+            'Completed ', 'It took:', 'seconds', '=====', '.', str(datetime.now().year), str(datetime.now().month),
+            str(datetime.now().day)
+        ]
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            timer.report()
+
+            output = out.getvalue()
+            for substring in substrings:
+                self.assertTrue(substring in output, msg=substring)
+        finally:
+            sys.stdout = saved_stdout
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/blowdrycss/blowdrycss-0.2.0-py2.py3-none-any.whl/blowdrycss/unit_tests/test_watchdogwrapper_main.py
@@ -0,0 +1,110 @@
+# python 2
+from __future__ import absolute_import, print_function, unicode_literals, with_statement
+import _thread
+
+# builtins
+from unittest import TestCase, main
+import logging
+import sys
+from io import StringIO, open
+from time import sleep
+from os import path, remove
+
+# plugins
+from blowdrycss.utilities import unittest_file_path, change_settings_for_testing
+from blowdrycss import watchdogwrapper
+import blowdrycss_settings as settings
+
+change_settings_for_testing()
+
+
+class TestWatchdogWrapperMain(TestCase):
+    def monitor_delete_stop(self, file_path_to_delete):
+        """ Monitor console output. Delete file at file_path_to_delete. Stop watchdogwrapper.main()
+        Reference: http://stackoverflow.com/questions/7602120/sending-keyboard-interrupt-programmatically
+
+        """
+        substrings = [
+            '~~~ blowdrycss started ~~~',
+            'Auto-Generated CSS',
+            'Completed',
+            'blowdry.css',
+            'blowdry.min.css',
+        ]
+
+        saved_stdout = sys.stdout           # Monitor console
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            sleep(0.1)                      # Wait for main() to start.
+            remove(file_path_to_delete)     # Delete delete.html
+            sleep(0.25)                     # IMPORTANT: Must wait for output otherwise test will fail.
+
+            output = out.getvalue()
+
+            for substring in substrings:
+                self.assertTrue(substring in output, msg=substring + '\noutput:\n' + output)
+        finally:
+            sys.stdout = saved_stdout
+
+        _thread.interrupt_main()            # Stop watchdogwrapper.main().
+
+    def test_main_auto_generate_True(self):
+        # Integration test
+        logging.basicConfig(level=logging.DEBUG)
+        html_text = '<html></html>'
+        delete_dot_html = unittest_file_path(folder='test_examplesite', filename='delete.html')
+
+        # Create delete.html
+        with open(delete_dot_html, 'w') as _file:
+            _file.write(html_text)
+
+        self.assertTrue(path.isfile(delete_dot_html))
+
+        settings.auto_generate = True
+        _thread.start_new_thread(self.monitor_delete_stop, (delete_dot_html,))
+        watchdogwrapper.main()              # Caution: Nothing will run after this line.
+
+    def test_main_auto_generate_False(self):
+        # Integration test
+        logging.basicConfig(level=logging.DEBUG)
+        substrings = [
+            '~~~ blowdrycss started ~~~',
+            'Auto-Generated CSS',
+            'Completed',
+            'blowdry.css',
+            'blowdry.min.css',
+        ]
+        html_text = '<html></html>'
+        delete_dot_html = unittest_file_path(folder='test_examplesite', filename='delete.html')
+
+        # Create delete.html
+        with open(delete_dot_html, 'w') as _file:
+            _file.write(html_text)
+
+        self.assertTrue(path.isfile(delete_dot_html))
+
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+
+            settings.auto_generate = False
+            watchdogwrapper.main()
+
+            remove(delete_dot_html)     # Delete delete.html
+
+            sleep(0.25)                 # IMPORTANT: Must wait for output otherwise test will fail.
+
+            output = out.getvalue()
+
+            for substring in substrings:
+                self.assertTrue(substring in output, msg=substring + '\noutput:\n' + output)
+        finally:
+            sys.stdout = saved_stdout
+
+
+if __name__ == '__main__':
+    main()
+
