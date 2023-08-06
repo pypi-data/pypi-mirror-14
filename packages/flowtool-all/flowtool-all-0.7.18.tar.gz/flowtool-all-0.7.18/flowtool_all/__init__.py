5cd4a0d403e0f74e1e18150db9ba8d0022a5e83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:19 +0000

    Add package flowtool-all/0.7.18/flowtool-all-0.7.18.tar.gz

---
+++ packages/flowtool-all/flowtool-all-0.7.18.tar.gz/flowtool-all-0.7.18/flowtool_all/__init__.py
@@ -0,0 +1,35 @@
+""" Metapackage flowtool-all.
+
+    Let's try some 'integration tests' :-)
+
+    #>>> commands = (
+    #...    'ci',
+    #...    'clean-pycs',
+    #...    'clean-tags',
+    #...    'co',
+    #...    'feature',
+    #...    'githooks-config',
+    #...    'githooks-install',
+    #...    'githooks-introduce',
+    #...    'githooks-scripts',
+    #...    'githooks-status',
+    #...    'githooks-wipe',
+    #...    'release-now',
+    #...    'run-githook',
+    #...    'self-info',
+    #...    'self-update',
+    #...    'versioning-init',
+    #...    'versioning-update',
+    #... )
+    #>>> from pkg_resources import iter_entry_points
+    #>>> installed = list(iter_entry_points('flowtool_commands'))
+    #>>> names = [e.name for e in installed]
+    #>>> all(cmd in names for cmd in commands)
+    #True
+
+    >>> __version__ > '0'
+    True
+"""
+
+from ._version import get_version
+__version__ = get_version()
