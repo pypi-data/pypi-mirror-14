caa9c21c6b7de7eb68e451d92215d4df7c6d9d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package microbot/1.1.1/microbot-1.1.1-py2.py3-none-any.whl

---
+++ packages/microbot/microbot-1.1.1-py2.py3-none-any.whl/microbot/serializers/__init__.py
@@ -0,0 +1,7 @@
+from microbot.serializers.telegram_api import UserSerializer, ChatSerializer, MessageSerializer, UpdateSerializer, UserAPISerializer  # noqa
+from microbot.serializers.response import ResponseSerializer, ResponseUpdateSerializer  # noqa
+from microbot.serializers.bot import BotSerializer, BotUpdateSerializer  # noqa
+from microbot.serializers.state import StateSerializer, ChatStateSerializer, ChatStateUpdateSerializer  # noqa
+from microbot.serializers.handler import HandlerSerializer, HandlerUpdateSerializer, AbsParamSerializer  # noqa
+from microbot.serializers.hook import HookSerializer, HookUpdateSerializer, RecipientSerializer  # noqa
+from microbot.serializers.environment_vars import EnvironmentVarSerializer  # noqa
---
+++ packages/microbot/microbot-1.1.1-py2.py3-none-any.whl/microbot/tasks.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from celery import shared_task
+from microbot.models import Update, Bot, Hook
+import logging
+import traceback
+import sys
+
+
+logger = logging.getLogger(__name__)
+
+@shared_task
+def handle_update(update_id, bot_id):
+    try:
+        update = Update.objects.get(id=update_id)
+        bot = Bot.objects.get(id=bot_id)
+    except Update.DoesNotExist:
+        logger.error("Update %s does not exists" % update_id)
+    except Bot.DoesNotExist:
+        logger.error("Bot  %s does not exists or disabled" % bot_id)
+    except:
+        logger.error("Error handling update %s from bot %s" % (update_id, bot_id))
+    else:
+        try:
+            bot.handle(update)
+        except:           
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            logger.error("Error processing %s for bot %s" % (update, bot))
+            
+@shared_task
+def handle_hook(hook_id, data):
+    try:
+        hook = Hook.objects.get(id=hook_id)
+    except Hook.DoesNotExist:
+        logger.error("Hook %s does not exists" % hook_id)
+    else:
+        try:
+            hook.bot.handle_hook(hook, data)
+        except:           
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            logger.error("Error processing %s for bot %s" % (hook, hook.bot))
