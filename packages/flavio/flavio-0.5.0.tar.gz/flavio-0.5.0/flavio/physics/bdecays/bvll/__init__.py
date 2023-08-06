88c25e5c7ca45e7fb00a05ce908fa51c2008e1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:53 +0000

    Add package flavio/0.5.0/flavio-0.5.0.tar.gz

---
+++ packages/flavio/flavio-0.5.0.tar.gz/flavio-0.5.0/flavio/physics/bdecays/bvll/__init__.py
@@ -0,0 +1,10 @@
+r"""Functions for observables in exclusive semi-leptonic $B\to V\ell^+\ell^-$ FCNC decays."""
+
+
+from . import amplitudes
+from . import observables
+from . import qcdf
+from . import qcdf_interpolate
+from . import lfv
+from . import nonfactorizable
+from . import subleading
