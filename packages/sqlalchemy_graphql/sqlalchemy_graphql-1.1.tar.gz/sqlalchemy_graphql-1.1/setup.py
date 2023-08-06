5e6248fc1389e6b3139f389f1e4890b85c7872f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:56 +0000

    Add package sqlalchemy_graphql/1.1/sqlalchemy_graphql-1.1.tar.gz

---
+++ packages/sqlalchemy_graphql/sqlalchemy_graphql-1.1.tar.gz/sqlalchemy_graphql-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sqlalchemy_graphql",
+    version="1.1",
+    description="GraphQL extension for dealing with SQLAlchemy",
+    long_description=open("README.rst", "r").read(),
+    author="Adriel Velazquez",
+    author_email="adrielvelazquez@gmail.com",
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=[
+        "sqlalchemy > 0.9",
+        "graphql-epoxy"
+    ],
+    tests_require=['tox'],
+)
