989208ce64a6116c1f76c2d704d38d2291068a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:45 +0000

    Add package django-slug-helpers/0.0.2/django-slug-helpers-0.0.2.tar.gz

---
+++ packages/django-slug-helpers/django-slug-helpers-0.0.2.tar.gz/django-slug-helpers-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-slug-helpers",
+    version = "0.0.2",
+    packages = find_packages(),
+    author = "Mark Longair",
+    author_email = "mark-pypi@longair.net",
+    description = "A simple model for redirecting from old slugs",
+    license = "AGPL",
+    keywords = "django images",
+    install_requires = [
+        'Django>=1.8',
+    ]
+)
