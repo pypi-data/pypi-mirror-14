1a514aadc215fc1ec049566cc944c3132a623704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:39 +0000

    Add package logicmonitor_core/1.0.6.5.1/logicmonitor_core-1.0.6.5.1.tar.gz

---
+++ packages/logicmonitor_core/logicmonitor_core-1.0.6.5.1.tar.gz/logicmonitor_core-1.0.6.5.1/logicmonitor_core/Service.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+import logging
+import os
+import subprocess
+from subprocess import Popen
+
+
+class Service(object):
+    @staticmethod
+    def getStatus(name):
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Retrieving status of service {0}".format(name))
+
+        ret = -1
+
+        startType = Service._getType(name)
+
+        # Determine how to get the status of the service
+        if startType == "init.d":
+            p = (Popen(["/etc/init.d/{0}".format(name), "status"],
+                       stdout=subprocess.PIPE))
+            ret, err = p.communicate()
+        elif startType == "service":
+            p = (Popen(["service", name, "status"],
+                       stdout=subprocess.PIPE))
+            ret, err = p.communicate()
+        else:
+            ret = "Unknown error getting status for service {0}".format(name)
+
+        return ret
+
+    @staticmethod
+    def doAction(name, action):
+        logging.debug("Performing {0} on service {1}".format(action, name))
+
+        startType = Service._getType(name)
+
+        ret = -1
+
+        # Determine how to perform the action on the service
+        if startType == "init.d":
+            p = (Popen(["/etc/init.d/{0}".format(name), action],
+                       stdout=subprocess.PIPE))
+            msg = p.communicate()
+            ret = p.returncode
+        elif startType == "service":
+            p = (Popen(["service", name, action],
+                       stdout=subprocess.PIPE))
+            msg = p.communicate()
+            ret = p.returncode
+
+        else:
+            msg = ("Unknown error performing '{0}' on service {1}"
+                   .format(action, name))
+            ret = msg
+
+        return (ret, msg)
+
+    @staticmethod
+    def _getType(name):
+        logging.debug("Getting service {0} control type".format(name))
+
+        if os.path.isdir("/etc/init.d"):
+            print "INIT" # TODO DELETE
+            return "init.d"
+        else:
+            print "Not init" # TODO DELETE
+            result = -1
+
+        try:
+            print "Not try service" # TODO DELETE
+            p = (Popen(["service", "--status-all"],
+                       stdout=subprocess.PIPE))
+            print "open" # TODO DELETE
+            ret = p.communicate()
+            result = p.returncode
+            print "result" # TODO DELETE
+
+            if result == 0 or result == 1:
+                print "SERVICE" # TODO DELETE
+                logging.debug("Service control is via service")
+                return "service"
+        except:
+            print "except" # TODO DELETE
+            result = -1
+        print result # TODO DELETE
+        return result
