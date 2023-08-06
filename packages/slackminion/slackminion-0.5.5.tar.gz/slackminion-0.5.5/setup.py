776726b6f5838f1b1af6574090454567688399af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package slackminion/0.5.5/slackminion-0.5.5.tar.gz

---
+++ packages/slackminion/slackminion-0.5.5.tar.gz/slackminion-0.5.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from slackminion import version
+
+setup(
+        name='slackminion',
+        version=version,
+        packages=find_packages(exclude=['test_plugins']),
+        url='https://github.com/arcticfoxnv/slackminion',
+        license='MIT',
+        author='Nick King',
+        author_email='',
+        description='A python bot framework for slack',
+        install_requires=[
+            'bottle',
+            'MySQL-python',
+            'PyYAML',
+            'redis',
+            'requests',
+            'Rocket',
+            'six',
+            'slackclient',
+            'websocket-client',
+        ],
+        entry_points={
+            'console_scripts': [
+                'slackminion = slackminion.__main__:main',
+            ]
+        },
+        classifiers=[
+            "Programming Language :: Python",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Development Status :: 2 - Pre-Alpha",
+            "Topic :: Communications :: Chat",
+        ]
+)
