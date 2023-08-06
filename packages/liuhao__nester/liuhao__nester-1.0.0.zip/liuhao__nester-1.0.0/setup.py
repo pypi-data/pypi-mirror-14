c9f1b4370238a5ccc3551d3d219c9326bd7d4824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package liuhao__nester/1.0.0/liuhao__nester-1.0.0.zip

---
+++ packages/liuhao__nester/liuhao__nester-1.0.0.zip/liuhao__nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='liuhao__nester',
+    version     ='1.0.0',
+    py_modules  =['nester'],
+    author      ='hfpython',
+    author_mail ='hfpython@headfirstlabs.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+    )
