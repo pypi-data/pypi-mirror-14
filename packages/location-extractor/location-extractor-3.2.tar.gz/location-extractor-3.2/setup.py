ca079b0c4ffa3613b5051ac94041c403c055ff24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:22 +0000

    Add package location-extractor/3.2/location-extractor-3.2.tar.gz

---
+++ packages/location-extractor/location-extractor-3.2.tar.gz/location-extractor-3.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'location-extractor',
+  packages = ['location_extractor'],
+  package_dir = {'location_extractor': 'location_extractor'},
+  package_data = {'location_extractor': ['*']},
+  version = '3.2',
+  description = 'Extract locations from text',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/location-extractor',
+  download_url = 'https://github.com/DanielJDufour/location-extractor/tarball/download',
+  keywords = ['location','geo','python','tagging'],
+  classifiers = [],
+)
