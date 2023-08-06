0b80888dc7aba317c7ea8e51d04a92f779308cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:35 +0000

    Add package mac.py/1.0.2/mac.py-1.0.2.zip

---
+++ packages/mac.py/mac.py-1.0.2.zip/mac.py-1.0.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name = 'mac.py',
+      version = '1.0.2',
+      description = 'A python lib to search Manufacturer of mac address',
+      long_description = '',
+      author = 'hustcc',
+      author_email = 'i@hust.cc',
+      url = 'https://github.com/hustcc/mac.py',
+      license = 'MIT',
+      install_requires = [],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+      ],
+      keywords = 'mac.py,mac address,mac Manufacturer',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data = True,
+      package_data =  {'src':['macpy/oui_3.dict', 'macpy/oui_3.dict']}
+)
---
+++ packages/mac.py/mac.py-1.0.2.zip/mac.py-1.0.2/test/test_unitest.py
@@ -0,0 +1,34 @@
+#-*-coding: utf-8 -*-
+
+import unittest, time
+import sys, random
+from macpy import Mac
+
+
+class TestCase(unittest.TestCase):
+    ## init
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.mac = Mac()
+        
+    # exit
+    def tearDown(self):
+        pass
+    
+    # test search method
+    def test_search_mac(self):
+        r_1 = self.mac.search('00016C')
+        r_2 = self.mac.search('00:01:6C:06:A6:29')
+        r_3 = self.mac.search('00-01-6C-06-A6-29')
+        r_4 = self.mac.search('00')
+        r_5 = self.mac.search('')
+        
+        self.assertEqual(r_1, r_2)
+        self.assertEqual(r_2, r_3)
+        self.assertEqual(r_3, {'re': 'Brea  CA  92821', 'com': 'FOXCONN', 'addr': '105 S Puente St.', 'co': 'US'})
+        
+        self.assertEqual(r_4, None)
+        self.assertEqual(r_5, None)
+
+if __name__ =='__main__':
+    unittest.main()
