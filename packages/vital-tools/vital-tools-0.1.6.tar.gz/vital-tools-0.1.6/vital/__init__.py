5af2d1d3a14c26b071bfe2621f8bf1ad8f8c8704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:59 +0000

    Add package vital-tools/0.1.6/vital-tools-0.1.6.tar.gz

---
+++ packages/vital-tools/vital-tools-0.1.6.tar.gz/vital-tools-0.1.6/vital/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+   `Vital`
+    Essential tools for building Python 3 applications.
+--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--·--
+    The MIT License (MIT) © 2016 Jared Lunde
+    http://github.com/jaredlunde/vital-tools
+
+"""
+from vital import cache
+from vital import debug
+from vital import security
+from vital import tools
+
+
+__author__ = "Jared Lunde"
+__license__ = "MIT"
+__version__ = "0.1.6"
