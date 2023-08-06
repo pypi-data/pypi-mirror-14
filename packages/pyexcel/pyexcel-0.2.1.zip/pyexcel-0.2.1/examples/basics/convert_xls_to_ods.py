27476c1ac666294a2a34f39e5636689750280d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:41 +0000

    Add package pyexcel/0.2.1/pyexcel-0.2.1.zip

---
+++ packages/pyexcel/pyexcel-0.2.1.zip/pyexcel-0.2.1/examples/basics/convert_xls_to_ods.py
@@ -0,0 +1,40 @@
+"""
+convert_xls_to_ods.py
+:copyright: (c) 2014-2015 by Onni Software Ltd.
+:license: New BSD License, see LICENSE for more details
+
+This shows how to use **Book** to convert
+xls file to ods file.
+
+What this example implies is that you can do the conversion in
+between these formats:
+==== === === === === ==== ====
+     ods csv tsv xls xlsx xlsm
+==== === === === === ==== ====
+ods  y   y   y   y   y    y
+tsv  y   y   y   y   y    y
+csv  y   y   y   y   y    y   
+xls  y   y   y   y   y    y   
+xlsx y   y   y   y   y    y   
+xlsm y   y   y   y   y    y   
+---- --- --- --- --- ---- ----
+"""
+import os
+import pyexcel as pe
+# you will need to install pyexcel-ods or pyexcel-ods3
+# depending on your python version
+import pyexcel.ext.xls
+import pyexcel.ext.ods3
+
+
+def main(base_dir):
+    # Simple open the file using Book
+    book = pe.Book(filename=os.path.join(base_dir,"multiple-sheets.xls"))
+    
+    # Create a new book by creating a BookWriter instance
+    book.save_as("multiple-sheets.ods")
+    
+    # then you will have the book in ods
+
+if __name__ == '__main__':
+    main(os.getcwd())
---
+++ packages/pyexcel/pyexcel-0.2.1.zip/pyexcel-0.2.1/setup.py
@@ -0,0 +1,67 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+with open("README.rst", 'r') as readme:
+    README_txt = readme.read()
+
+dependencies = [
+    'pyexcel-io>=0.1.0',
+    'texttable>=0.8.2',
+]
+
+extras = {
+    'xls': ['pyexcel-xls>=0.1.0'],
+    'xlsx': ['pyexcel-xlsx>=0.1.0'],
+    'ods': ['pyexcel-ods3>=0.1.0'],
+}
+
+
+setup(
+    name='pyexcel',
+    author='C. W.',
+    version='0.2.1',
+    author_email='wangc_2011 (at) hotmail.com',
+    url='https://github.com/pyexcel/pyexcel',
+    description='A wrapper library that provides one API to read, manipulate and write data in different excel formats',
+    install_requires=dependencies,
+    extras_require=extras,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    long_description=README_txt,
+    zip_safe=False,
+    tests_require=['nose'],
+    keywords=[
+        'excel',
+        'python',
+        'pyexcel',
+        'tsv',
+        'tsvz'
+        'csv',
+        'csvz',
+        'xls',
+        'xlsx',
+        'ods'
+    ],
+    license='New BSD',
+    classifiers=[
+        'Topic :: Office/Business',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
