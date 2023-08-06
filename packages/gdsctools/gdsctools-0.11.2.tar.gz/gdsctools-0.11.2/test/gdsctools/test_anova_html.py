9817cc3a5838051a705abfd92fa66f865a40d15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:14 +0000

    Add package gdsctools/0.11.2/gdsctools-0.11.2.tar.gz

---
+++ packages/gdsctools/gdsctools-0.11.2.tar.gz/gdsctools-0.11.2/test/gdsctools/test_anova_html.py
@@ -0,0 +1,28 @@
+from gdsctools.anova import ANOVA
+from gdsctools.anova_report import ANOVAReport
+from gdsctools import ic50_test
+
+
+def test_html():
+    # same as above, could factorise
+    an = ANOVA(ic50_test)
+    features = an.features.df
+    features = features[features.columns[0:30]]
+    an = ANOVA(ic50_test, features)
+    #an.settings.include_media_factor = False
+    results = an.anova_all()
+
+    assert len(results.df) == 302 
+
+    r = ANOVAReport(gdsc=an, results=results)
+    assert len(r.get_significant_set()) == 3
+    # long but should cover everthinh.
+    try:
+        r.create_html_pages()
+    except Exception as err:
+        raise err
+    finally:
+        import shutil
+        shutil.rmtree('html_gdsc_anova')
+
+
