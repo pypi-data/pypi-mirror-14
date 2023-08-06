445c2cb72f212fb66c3da438a2caf2c3e5f2bc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:31 +0000

    Add package valleybackups/0.1.11/valleybackups-0.1.11.zip

---
+++ packages/valleybackups/valleybackups-0.1.11.zip/valleybackups-0.1.11/valleybackups/commands/cmd_download_file.py
@@ -0,0 +1,24 @@
+import click
+
+from valleybackups.config_context import pass_config
+from valleybackups.db import job_exists, get_job
+
+
+@click.command()
+@click.argument('job_id', type=str)
+@pass_config
+def cli(config, job_id):
+    """Gets a ready-to-be-downloaded file from Glacier. """
+
+    # TODO: Download file by Archive ID instead of Job ID
+    if config.service == "Glacier":
+        job = get_job(job_id)
+        if not job:
+            raise click.ClickException(message="The requested file does not exist in our database, "
+                                               "please check with list_uncompleted_jobs command")
+        if not job_exists(job.job_id):
+            raise click.ClickException(message="This file has not been uploaded from this machine")
+    try:
+        response = config.glacier.download_file(job.job_id)
+    except Exception as e:
+        raise click.ClickException(e.message)
