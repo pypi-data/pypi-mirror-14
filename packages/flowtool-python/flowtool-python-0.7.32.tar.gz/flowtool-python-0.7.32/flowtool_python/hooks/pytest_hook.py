229d91067ce30e2c1baf19348430bfe364b6e90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:32 +0000

    Add package flowtool-python/0.7.32/flowtool-python-0.7.32.tar.gz

---
+++ packages/flowtool-python/flowtool-python-0.7.32.tar.gz/flowtool-python-0.7.32/flowtool_python/hooks/pytest_hook.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import git
+import click
+import fnmatch
+from flowtool.style import echo, colors
+from flowtool.style import debug
+from flowtool_git.common import local_repo
+
+import pytest
+
+def run_pytest(*args):
+    """ Run pytest and return its exit status. """
+    pytest_args = [
+        '--doctest-modules',
+        '--ignore=setup.py',
+    ] + list(args)
+    errno = pytest.main(pytest_args)
+
+    return errno
+
+
+IGNORE_RECURSIVE = set([
+    '.git', '.tox', 'build', 'dist', 'test', 'tests', 'venv',
+])
+
+def find_pytest_configs(repo=None):
+    """ Find dirs with pytest compatible configs. """
+
+    look_for = set(['pytest.ini', 'tox.ini'])
+
+    if repo is None:
+        repo = local_repo()
+
+    def ignore_location(loc):
+        inside = loc.split(os.sep)
+        shall_ignore = IGNORE_RECURSIVE.intersection(inside)
+        return bool(shall_ignore)
+
+    for loc, _, files in os.walk(os.path.dirname(repo.git_dir)):
+        if look_for.intersection(files) and not ignore_location(loc):
+            yield loc
+
+
+def pytest_setup(cmd=None):
+    """ Setup function for pytest hook(s). """
+    if cmd == 'install':
+        pass
+    elif cmd == 'uninstall':
+        pass
+
+
+def pytest_hook(*args, **kwd):
+    """ Run pytest in discovered directories. """
+    locations = list(find_pytest_configs())
+    if not locations:
+        return
+    echo.bold('Will run pytest in %d dir(s).' % len(locations))
+    continues = 0
+    hook_return = 0
+    fails = 0
+    for loc in locations:
+        echo.bold('->', loc)
+        returncode = run_pytest(loc)
+        if returncode:
+            fails += 1
+            hook_return |= returncode
+            if fails > continues:
+                sys.exit(returncode)
+    if hook_return:
+        sys.exit(hook_return)
---
+++ packages/flowtool-python/flowtool-python-0.7.32.tar.gz/flowtool-python-0.7.32/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+import os
+
+from versioning import get_version, get_cmdclass
+
+# General Info
+
+setup_args = dict(
+    name='flowtool-python',
+    version=get_version(),
+    cmdclass=get_cmdclass(),
+    description='Shortcuts for python devs.',
+    author='Konstantin Martini',
+    author_email='k@tuxcode.org',
+    url='https://github.com/isnok/py-flowlib',
+)
+
+
+# Data Files
+
+setup_args.update(
+    packages=find_packages(),
+    zip_safe=False,
+)
+
+
+# Requirements
+
+setup_args.update(
+    install_requires=[
+        'flowtool-base',
+        'flowtool-githooks',
+        'gitpython',
+        'pylint',
+        'pytest',
+    ],
+)
+
+
+# Entry Points
+
+setup_args.update(
+    entry_points={
+        'flowtool_commands': [
+            'clean-pycs = flowtool_python.clean:clean',
+        ],
+        'console_scripts': [
+            '_flowtool_python.pylint_minimal = flowtool_python.hooks.pylint_minimal:pylint_minimal',
+            '_flowtool_python.pylint_commit_range = flowtool_python.hooks.pylint_minimal:pylint_pre_push',
+            '_flowtool_python.pytest_hook = flowtool_python.hooks.pytest_hook:pytest_hook',
+        ],
+        'flowtool_githooks.pre_commit': [
+            '_flowtool_python.pylint_minimal = flowtool_python.hooks.pylint_minimal:pylint_setup',
+            '_flowtool_python.pytest_hook = flowtool_python.hooks.pytest_hook:pytest_setup',
+        ],
+        'flowtool_githooks.commit_msg': [
+            '_flowtool_python.pylint_minimal = flowtool_python.hooks.pylint_minimal:pylint_setup',
+            '_flowtool_python.pytest_hook = flowtool_python.hooks.pytest_hook:pytest_setup',
+        ],
+        'flowtool_githooks.pre_push': [
+            '_flowtool_python.pylint_commit_range = flowtool_python.hooks.pylint_minimal:pylint_setup',
+        ],
+    },
+)
+
+
+# Techincal Details
+
+setup_args.update(
+    keywords=['git','flow','shell','local','remote','commandline'],
+    platforms=['Debian/GNU Linux'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        #"License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: System :: Software Distribution",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        #"Topic :: Internet",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Environment :: Console",
+    ],
+)
+
+setup(**setup_args)
