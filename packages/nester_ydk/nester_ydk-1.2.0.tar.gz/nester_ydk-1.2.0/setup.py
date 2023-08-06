128f695dca706d71b25fdccc04569473233da59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:08 +0000

    Add package nester_ydk/1.2.0/nester_ydk-1.2.0.tar.gz

---
+++ packages/nester_ydk/nester_ydk-1.2.0.tar.gz/nester_ydk-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	=	'nester_ydk',
+	version	=	'1.2.0',
+	py_modules	=	['nester_ydk'],
+	author		=	'YDK',
+	author_email	=	'generalshep@gmail.com',
+	url				=  'http://alps.co.kr',
+	description = 'Simple printer of nested lists'
+	)
