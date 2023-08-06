55c8b8b0cec7bd12f01f82c4c8fd1ed4ebe57f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package django-mailjet/0.1.1/django-mailjet-0.1.1.tar.gz

---
+++ packages/django-mailjet/django-mailjet-0.1.1.tar.gz/django-mailjet-0.1.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+version = '0.1.1'
+
+DESCRIPTION = "A Django email backend for Mailjet"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Framework :: Django',
+    'Framework :: Django :: 1.9',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='django-mailjet',
+    version=version,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    keywords="django, mailjet, email, email backend",
+    author='Dmitrii Gerasimenko',
+    author_email='kiddima@gmail.com',
+    url='http://github.com/kidig/django-mailjet/',
+    license='MIT',
+    packages=['django_mailjet'],
+    install_requires=["mailjet_rest>=v1.1.1", "django>=1.8"],
+    include_package_data=True,
+    tests_require=["mock", "six"],
+    test_suite="runtests.runtests",
+    zip_safe=False,
+)
