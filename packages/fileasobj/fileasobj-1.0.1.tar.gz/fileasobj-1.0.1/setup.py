3a94a781690e32cfd6d7ee3caf1fa810cb3d20b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:39 +0000

    Add package fileasobj/1.0.1/fileasobj-1.0.1.tar.gz

---
+++ packages/fileasobj/fileasobj-1.0.1.tar.gz/fileasobj-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='fileasobj',
+  packages=['fileasobj'],
+  version='1.0.1',
+  description='Manage a file as a Python list.',
+  author='John Hazelwood',
+  author_email='jhazelwo@users.noreply.github.com',
+  url='https://github.com/jhazelwo/python-fileasobj',
+  download_url='https://github.com/jhazelwo/python-fileasobj/tarball/1.0.1',
+  keywords=['python', 'file'],
+  classifiers=[],
+)
