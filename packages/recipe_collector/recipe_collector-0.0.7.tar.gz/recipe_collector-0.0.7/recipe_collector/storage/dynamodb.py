dd26bebcc5cdb1e8db9c038e720c1d6f9a5f5454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package recipe_collector/0.0.7/recipe_collector-0.0.7.tar.gz

---
+++ packages/recipe_collector/recipe_collector-0.0.7.tar.gz/recipe_collector-0.0.7/recipe_collector/storage/dynamodb.py
@@ -0,0 +1,49 @@
+import boto3
+from recipe_collector.utils.decorators import coroutine
+
+
+@coroutine
+def urls_dynamodb(table_name, aws_access_key, aws_secret_key, aws_region):
+    """
+    This coroutine receives tuples of title and url values and saves them to AWS DynamoDB.
+
+    Args:
+    * table_name: A DynamoDB table name
+    * aws_access_key: An AWS access key
+    * aws_secret_key: An AWS secret key
+    * aws_region: An AWS region
+    """
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key,
+        region_name=aws_region)
+    table = session.resource('dynamodb').Table(table_name)
+    while True:
+        source_url = yield
+        table.put_item(Item={
+            'url': source_url
+        })
+
+
+@coroutine
+def html_dynamodb(table_name, aws_access_key, aws_secret_key, aws_region):
+    """
+    This coroutine receives tuples of url and HTML values and saves them to AWS DynamoDB.
+
+    Args:
+    * table_name: A DynamoDB table name
+    * aws_access_key: An AWS access key
+    * aws_secret_key: An AWS secret key
+    * aws_region: An AWS region
+    """
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key,
+        region_name=aws_region)
+    table = session.resource('dynamodb').Table(table_name)
+    while True:
+        source_url, html = yield
+        table.put_item(Item={
+            'url': source_url,
+            'html': html
+        })
