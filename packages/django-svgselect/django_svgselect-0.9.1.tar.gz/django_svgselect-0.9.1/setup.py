0fe2bf0aada19828a88a21906a1187787fab41aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:37 +0000

    Add package django-svgselect/0.9.1/django_svgselect-0.9.1.tar.gz

---
+++ packages/django-svgselect/django_svgselect-0.9.1.tar.gz/django_svgselect-0.9.1/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django_svgselect',
+    version='0.9.1',
+
+    description='A Django form widget that uses SVG files in place of Select widgets.',
+    long_description=long_description,
+
+    url='https://github.com/jmickela/django_svgselect',
+
+    author='Jason Mickela',
+    author_email='jason@rootid.in',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='django form widget svg',
+
+    packages=get_packages('django_svgselect'),
+    package_data=get_package_data('django_svgselect'),
+    install_requires=[],
+)
