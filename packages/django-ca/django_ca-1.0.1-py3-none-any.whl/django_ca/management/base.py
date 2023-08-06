a6f4e0324761a14af64f334f96b4986dfb28a6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:40 +0000

    Add package django-ca/1.0.1/django_ca-1.0.1-py3-none-any.whl

---
+++ packages/django-ca/django_ca-1.0.1-py3-none-any.whl/django_ca/management/base.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+import argparse
+import os
+import sys
+
+from OpenSSL import crypto
+
+from django.core.management.base import BaseCommand as _BaseCommand
+from django.core.management.base import CommandError
+from django.core.management.base import OutputWrapper
+from django.core.management.color import no_style
+from django.core.validators import URLValidator
+from django.utils import six
+
+from django_ca import ca_settings
+from django_ca.utils import is_power2
+from django_ca.models import Certificate
+from django_ca.models import CertificateAuthority
+
+
+class FormatAction(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        value = value.strip().upper()
+        if value == 'DER':
+            value = 'ASN1'
+        try:
+            value = getattr(crypto, 'FILETYPE_%s' % value)
+        except AttributeError:
+            parser.error('Unknown format "%s".' % value)
+        setattr(namespace, self.dest, value)
+
+
+class KeySizeAction(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        option_string = option_string or 'key size'
+
+        if not is_power2(value):
+            parser.error('%s must be a power of two (2048, 4096, ...)' % option_string)
+        elif value < ca_settings.CA_MIN_KEY_SIZE:
+            parser.error('%s must be at least %s bits.'
+                         % (option_string, ca_settings.CA_MIN_KEY_SIZE))
+        setattr(namespace, self.dest, value)
+
+
+class CertificateAuthorityAction(argparse.Action):
+    def __init__(self, allow_disabled=False, **kwargs):
+        super(CertificateAuthorityAction, self).__init__(**kwargs)
+        self.allow_disabled = allow_disabled
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        value = value.strip().upper()
+
+        qs = CertificateAuthority.objects.all()
+        if self.allow_disabled is False:
+            qs = qs.enabled()
+
+        try:
+            value = qs.get(serial=value)
+        except CertificateAuthority.DoesNotExist:
+            parser.error('%s: Unknown Certiciate Authority.' % value)
+
+        # verify that the private key exists
+        if not os.path.exists(value.private_key_path):
+            parser.error('%s: %s: Private key does not exist.' % (value, value.private_key_path))
+
+        # try to parse the private key
+        try:
+            value.key
+        except (OSError, crypto.Error):
+            raise parser.error(
+                '%s: %s: Could not read private key.' % (value, value.private_key_path))
+
+        setattr(namespace, self.dest, value)
+
+
+class URLAction(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        validator = URLValidator()
+        try:
+            validator(value)
+        except:
+            parser.error('%s: Not a valid URL.' % value)
+        setattr(namespace, self.dest, value)
+
+
+class MultipleURLAction(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        validator = URLValidator()
+        try:
+            validator(value)
+        except:
+            parser.error('%s: Not a valid URL.' % value)
+
+        if getattr(namespace, self.dest) is None:
+            setattr(namespace, self.dest, [])
+
+        getattr(namespace, self.dest).append(value)
+
+
+class BinaryOutputWrapper(OutputWrapper):
+    def __init__(self, out, style_func=None, ending=b'\n'):
+        super(BinaryOutputWrapper, self).__init__(out, style_func=None, ending=ending)
+
+    def write(self, msg, style_func=None, ending=None):
+        ending = self.ending if ending is None else ending
+        if six.PY3 is True and isinstance(msg, str):  # pragma: no cover
+            msg = msg.encode('utf-8')
+
+        if ending and not msg.endswith(ending):  # pragma: no cover
+            msg += ending
+        self._out.write(msg)
+
+
+class BaseCommand(_BaseCommand):
+    certificate_queryset = Certificate.objects.filter(revoked=False)
+    binary_output = False
+
+    # TODO/Django1.9: Only necessary in Django 1.8
+    requires_system_checks = True
+
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        if self.binary_output is True and six.PY3 is True:
+            self.stdout = BinaryOutputWrapper(stdout or sys.stdout.buffer)
+            self.stderr = BinaryOutputWrapper(stderr or sys.stderr.buffer)
+            self.style = no_style()
+        else:
+            super(BaseCommand, self).__init__(stdout, stderr, no_color=no_color)
+
+    def execute(self, *args, **options):
+        if self.binary_output is True:
+            if options.get('stdout'):  # pragma: no branch
+                self.stdout = BinaryOutputWrapper(options.pop('stdout'))
+            if options.get('stderr'):  # pragma: no branch
+                self.stderr = BinaryOutputWrapper(options.pop('stderr'))
+            options['no_color'] = True
+
+        super(BaseCommand, self).execute(*args, **options)
+
+    def add_algorithm(self, parser):
+        """Add the --algorithm option."""
+
+        parser.add_argument(
+            '--algorithm', metavar='{sha512,sha256,...}', default=ca_settings.CA_DIGEST_ALGORITHM,
+            help='Algorithm to use (default: %(default)s).')
+
+    def add_ca(self, parser, arg='--ca', help='Certificate authority to use (default: %s).',
+               allow_disabled=False, no_default=False):
+        if no_default is True:
+            default = None
+        else:
+            default = CertificateAuthority.objects.enabled().first()
+
+        help = help % {'default': default}
+        parser.add_argument('%s' % arg, metavar='SERIAL', help=help, default=default,
+                            allow_disabled=allow_disabled, action=CertificateAuthorityAction)
+
+    def add_format(self, parser, default=crypto.FILETYPE_PEM):
+        """Add the --format option."""
+
+        help_text = 'The format to use ("DER" is an alias for "ASN1"%s).'
+        if default == crypto.FILETYPE_PEM:
+            help_text %= ', default: PEM'
+        elif default == crypto.FILETYPE_ASN1:  # pragma: no cover
+            help_text %= ', default: ASN1'
+        elif default == crypto.FILETYPE_TEXT:  # pragma: no cover
+            help_text %= ', default: TEXT'
+        else:  # pragma: no cover
+            help_text %= ''
+
+        parser.add_argument('-f', '--format', metavar='{PEM,ASN1,DER,TEXT}', default=default,
+                            action=FormatAction, help=help_text)
+
+    def get_certificate(self, id):
+        try:
+            return self.certificate_queryset.get_by_serial_or_cn(id)
+        except Certificate.DoesNotExist:
+            raise CommandError('No valid certificate with CommonName/serial "%s" exists.' % id)
+        except Certificate.MultipleObjectsReturned:  # pragma: no cover - super unlikely
+            raise CommandError('Multiple valid certificates with CommonName "%s" found.' % id)
+
+
+class CertCommand(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'cert',
+            help='''Certificate by CommonName or serial. If you give a CommonName (which is not by
+                definition unique) there must be only one valid certificate with the given
+                CommonName.''')
+        super(CertCommand, self).add_arguments(parser)
+
+
+class CertificateAuthorityDetailMixin(object):
+    def add_ca_args(self, parser):
+        group = parser.add_argument_group(
+            'x509 extensions', 'Define various x509 extensions used when signing certificates.')
+        group.add_argument('--issuer-url', metavar='URL', action=URLAction,
+                           help='URL to the certificate of your CA (in DER format).')
+        group.add_argument(
+            '--issuer-alt-name', metavar='URL', action=URLAction,
+            help='URL to the homepage of your CA.'
+        )
+        group.add_argument(
+            '--crl-url', metavar='URL', action=MultipleURLAction, default=[],
+            help='URL to a certificate revokation list. Can be given multiple times.'
+        )
+        group.add_argument(
+            '--ocsp-url', metavar='URL', action=URLAction,
+            help='URL of an OCSP responder.'
+        )
---
+++ packages/django-ca/django_ca-1.0.1-py3-none-any.whl/django_ca/utils.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+"""Central functions to load CA key and cert as PKey/X509 objects."""
+
+import re
+import uuid
+
+from copy import deepcopy
+from datetime import datetime
+from datetime import timedelta
+from ipaddress import ip_address
+
+from django.core.validators import URLValidator
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.encoding import force_bytes
+from django.utils.encoding import force_text
+from django.utils.functional import Promise
+from django.utils.translation import ugettext_lazy as _
+
+from OpenSSL import crypto
+
+from django_ca import ca_settings
+
+# List of possible subject fields, in order
+SUBJECT_FIELDS = ['C', 'ST', 'L', 'O', 'OU', 'CN', 'emailAddress', ]
+
+# Description strings for various X509 extensions, taken from "man x509v3_config".
+EXTENDED_KEY_USAGE_DESC = _('Purposes for which the certificate public key can be used for.')
+KEY_USAGE_DESC = _('Permitted key usages.')
+SAN_OPTIONS_RE = '(email|URI|IP|DNS|RID|dirName|otherName):'
+_datetime_format = '%Y%m%d%H%M%SZ'
+
+
+class LazyEncoder(DjangoJSONEncoder):
+    """Encoder that also encodes strings translated with ugettext_lazy."""
+
+    def default(self, obj):
+        if isinstance(obj, Promise):
+            return force_text(obj)
+        return super(LazyEncoder, self).default(obj)
+
+
+def parse_date(date):
+    return datetime.strptime(date, _datetime_format)
+
+
+def format_date(date):
+    """Format date as ASN1 GENERALIZEDTIME, as required by various fields."""
+    return date.strftime(_datetime_format)
+
+
+def is_power2(num):
+    """Return True if num is a power of 2."""
+    return num != 0 and ((num & (num - 1)) == 0)
+
+
+def multiline_url_validator(value):
+    """Validate that a TextField contains one valid URL per line.
+
+    .. seealso:: https://docs.djangoproject.com/en/1.9/ref/validators/
+    """
+    validator = URLValidator()
+
+    for line in value.splitlines():
+        validator(line)
+
+
+def get_cert_subject(d):
+    """Returns an itemized dictionary in the correct order for a x509 subject."""
+    return sorted(d.items(), key=lambda e: SUBJECT_FIELDS.index(e[0]))
+
+
+def get_basic_cert(expires, now=None):
+    """Get a basic X509 cert object.
+
+    Parameters
+    ----------
+
+    expires : int
+        When, in number of days from now, this certificate will expire.
+    """
+    if expires < 0:
+        raise ValueError("Expires must not be negative.")
+
+    if now is None:  # pragma: no cover
+        now = datetime.utcnow()
+    now = now.replace(second=0, microsecond=0)
+
+    not_before = format_date(now)
+
+    # make expires to a datetime
+    expires = now + timedelta(days=expires + 1)
+    expires = expires.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    not_after = format_date(expires)
+
+    cert = crypto.X509()
+    cert.set_version(2) # V3 certificate
+    cert.set_serial_number(uuid.uuid4().int)
+    cert.set_notBefore(not_before.encode('utf-8'))
+    cert.set_notAfter(not_after.encode('utf-8'))
+    return cert
+
+
+def get_cert_profile_kwargs(name=None):
+    """Get kwargs suitable for get_cert X509 keyword arguments from the given profile."""
+
+    if name is None:
+        name = ca_settings.CA_DEFAULT_PROFILE
+
+    profile = deepcopy(ca_settings.CA_PROFILES[name])
+    kwargs = {
+        'cn_in_san': profile['cn_in_san'],
+        'subject': profile['subject'],
+    }
+    for arg in ['keyUsage', 'extendedKeyUsage']:
+        config = profile.get(arg)
+        if config is None or not config.get('value'):
+            continue
+
+        critical = config.get('critical', 'True')
+        if isinstance(config['value'], str):
+            kwargs[arg] = (critical, force_bytes(config['value']))
+        elif isinstance(config['value'], bytes):
+            kwargs[arg] = (critical, config['value'])
+        else:
+            kwargs[arg] = (critical, force_bytes(','.join(config['value'])))
+    return kwargs
+
+
+def get_subjectAltName(names, cn=None):
+    """Compute the value of the subjectAltName extension based on the given list of names.
+
+    The `cn` parameter, if provided, is prepended if not present in the list of names.
+
+    This method supports the `IP`, `email`, `URI` and `DNS` options automatically, if you need a
+    different option (or think the automatic parsing is wrong), give the full value verbatim (e.g.
+    `otherName:1.2.3.4;UTF8:some other identifier`.
+    """
+    values = []
+    names = sorted(set(names))
+
+    for name in names:
+        if not name:
+            continue
+        if isinstance(name, bytes):
+            name = name.decode('utf-8')
+
+        # Match any known literal values
+        if re.match(SAN_OPTIONS_RE, name):
+            values.append(name)
+            continue
+
+        try:
+            ip_address(name)
+            values.append('IP:%s' % name)
+            continue
+        except ValueError:
+            pass
+
+        if re.match('[a-z0-9]{2,}://', name):
+            values.append('URI:%s' % name)
+        elif '@' in name:
+            values.append('email:%s' % name)
+        else:
+            values.append('DNS:%s' % name)
+
+    if cn is not None:
+        value = 'DNS:%s' % cn
+        if value not in values:
+            values.insert(0, value)
+
+    return force_bytes(','.join(values))
