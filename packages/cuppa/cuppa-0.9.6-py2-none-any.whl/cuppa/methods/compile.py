ebe03b198d3c4000db01eb5bf3e72359c4ca9924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:02 +0000

    Add package cuppa/0.9.6/cuppa-0.9.6-py2-none-any.whl

---
+++ packages/cuppa/cuppa-0.9.6-py2-none-any.whl/cuppa/methods/compile.py
@@ -0,0 +1,36 @@
+
+#          Copyright Jamie Allsop 2013-2015
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   CompileMethod
+#-------------------------------------------------------------------------------
+
+import cuppa.progress
+
+from SCons.Script import Flatten
+
+class CompileMethod:
+
+    def __call__( self, env, source, **kwargs ):
+        sources = Flatten( [ source ] )
+        objects = []
+        if 'CPPPATH' in env:
+            env.AppendUnique( INCPATH = env['CPPPATH'] )
+
+        for source in sources:
+            objects.append(
+                env.Object(
+                    source = source,
+                    CPPPATH = env['SYSINCPATH'] + env['INCPATH'],
+                    **kwargs ) )
+
+        cuppa.progress.NotifyProgress.add( env, objects )
+
+        return objects
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "Compile", cls() )
