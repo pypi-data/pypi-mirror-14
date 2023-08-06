4b4203517c70f2993595f92bf28810a0ee990243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:48 +0000

    Add package Liujf_nester/1.4.0/Liujf_nester-1.4.0.tar.gz

---
+++ packages/Liujf_nester/Liujf_nester-1.4.0.tar.gz/Liujf_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'Liujf_nester',
+	version = '1.4.0',
+	py_modules = ['Liujf_nester'],
+	author = 'LiuJianfeng',
+	author_email = '314855457@qq.com',
+	url = 'www.baidu.com',
+	description = 'My first python coding',
+	)
