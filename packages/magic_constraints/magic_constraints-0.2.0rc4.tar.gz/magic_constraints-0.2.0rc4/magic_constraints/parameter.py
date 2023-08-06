6c15e1612caf7770014c9ccc0c3776a489abecee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:34 +0000

    Add package magic_constraints/0.2.0rc4/magic_constraints-0.2.0rc4.tar.gz

---
+++ packages/magic_constraints/magic_constraints-0.2.0rc4.tar.gz/magic_constraints-0.2.0rc4/magic_constraints/parameter.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+from builtins import *                  # noqa
+from future.builtins.disabled import *  # noqa
+
+from collections import namedtuple
+
+from magic_constraints.exception import MagicSyntaxError
+
+
+class Parameter(object):
+
+    def __init__(self, name, type_, **kwargs):
+        self.name = name
+
+        raise_on_nontype_object(type_)
+        self.type_ = type_
+
+        # key only arguemnt.
+        # 1. if nullable, None is accepted.
+        self.nullable = kwargs.get('nullable', False)
+        # 2. record default value.
+        if 'default' in kwargs:
+            self.with_default = True
+            self.default = kwargs['default']
+        else:
+            self.with_default = False
+            self.default = None
+        # 3. user-defined callback on input argument.
+        self.callback = kwargs.get(
+            'callback', lambda instance: True,
+        )
+
+        self._arguments_repr = self._generate_init_arguments_repr(
+            name, type_, **kwargs
+        )
+
+    def check_argument(self, instance):
+        if instance is None:
+            if not (self.nullable or issubclass(self.type_, type(None))):
+                return False
+            else:
+                return self.callback(None)
+        if not isinstance(instance, self.type_):
+            return False
+        return self.callback(instance)
+
+    def _generate_init_arguments_repr(self, name, type_, **kwargs):
+        # positional arguments.
+        prefix = "name={name}, type_={type_}".format(
+            name=conditional_repr(name),
+            type_=conditional_repr(type_),
+        )
+        # keyword-only arguments.
+        suffix = ', '.join(map(
+            lambda p: "{0}={1}".format(p[0], conditional_repr(p[1])),
+            sorted(kwargs.items(), key=lambda p: p[0]),
+        ))
+
+        # merge.
+        if suffix:
+            arguemnt_repr = "{0}, {1}".format(prefix, suffix)
+        else:
+            arguemnt_repr = prefix
+
+        return arguemnt_repr
+
+    def __repr__(self):
+        return repr_return(
+            'Parameter({})'.format(self._arguments_repr),
+        )
+
+
+def build_parameter_package(parameters):
+    raise_on_non_parameters(parameters)
+
+    name_hash = {}
+    start_of_defaults = -1
+
+    idx = 0
+    for parameter in parameters:
+
+        if parameter.name in name_hash:
+            raise MagicSyntaxError(
+                'redefinition of parameter',
+                name=parameter.name,
+            )
+
+        name_hash[parameter.name] = idx
+
+        if parameter.with_default:
+            if start_of_defaults < 0:
+                start_of_defaults = idx
+        else:
+            if start_of_defaults >= 0:
+                raise MagicSyntaxError(
+                    'Parameter without default should be placed '
+                    'in front of all parameters with default.',
+                    name=parameter.name,
+                )
+
+        idx += 1
+
+    ParameterPackage = namedtuple(
+        'ParameterPackage',
+        ['parameters', 'name_hash', 'start_of_defaults'],
+    )
+
+    return ParameterPackage(
+        parameters, name_hash, start_of_defaults,
+    )
+
+
+from magic_constraints.utils import (
+    raise_on_nontype_object, raise_on_non_parameters,
+    repr_return, conditional_repr,
+)  # noqa
---
+++ packages/magic_constraints/magic_constraints-0.2.0rc4.tar.gz/magic_constraints-0.2.0rc4/magic_constraints/utils.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+from builtins import *                  # noqa
+from future.builtins.disabled import *  # noqa
+
+import sys
+import types
+from abc import ABCMeta
+import collections as abc
+
+from funcsigs import signature
+from funcsigs import Parameter as SigParameter
+
+
+class CompoundArgument(object):
+    pass
+
+
+class AttributesBinder(object):
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __call__(self, attr, val):
+        setattr(self.obj, attr, val)
+
+
+def type_object(obj):
+    return hasattr(obj, '__bases__')
+
+
+def nontype_object(obj):
+    return not type_object(obj)
+
+
+def raise_on_non_function(function):
+    if not isinstance(function, types.FunctionType):
+        raise MagicTypeError(
+            'require FunctionType.',
+            function=function,
+        )
+
+
+def raise_on_non_parameters(parameters):
+    if not isinstance(parameters, abc.Iterable):
+        raise MagicTypeError(
+            'parameter should be Iterable.',
+            parameters=parameters,
+        )
+
+    for p in parameters:
+        if isinstance(p, Parameter):
+            continue
+        else:
+            raise MagicTypeError(
+                'require instance of Parameter.',
+                p=p,
+            )
+
+
+def raise_on_nontype_object(obj):
+    if nontype_object(obj):
+        raise MagicTypeError(
+            'require type object.',
+            obj=obj,
+        )
+
+
+def build_parameters_by_function_inspection(type_args, function, fi):
+    argument_sigs = signature(function)
+
+    if len(argument_sigs.parameters) - fi != len(type_args):
+        raise MagicSyntaxError(
+            'length of arguments not match.',
+            type_args=type_args,
+            function_signature=argument_sigs.parameters,
+        )
+
+    parameters = []
+    ti = 0
+    for name, sig_parameter in argument_sigs.parameters.items():
+        if fi > 0:
+            fi -= 1
+            continue
+
+        if sig_parameter.kind not in [
+            SigParameter.POSITIONAL_ONLY,
+            SigParameter.POSITIONAL_OR_KEYWORD
+        ]:
+            raise MagicSyntaxError(
+                'supports only POSITIONAL_ONLY '
+                'or POSITIONAL_OR_KEYWORD argument.',
+                name=name,
+                kine=SigParameter.kind,
+            )
+
+        default_value = sig_parameter.default
+        if default_value is sig_parameter.empty:
+            # no default.
+            parameters.append(
+                Parameter(name, type_args[ti]),
+            )
+        elif default_value is None:
+            parameters.append(
+                Parameter(name, type_args[ti], nullable=True, default=None),
+            )
+        else:
+            parameters.append(
+                Parameter(name, type_args[ti], default=default_value),
+            )
+
+        ti += 1
+
+    return build_parameter_package(parameters)
+
+
+def repr_return(text):
+    if sys.version_info.major == 2:  # pragma: no cover
+        text = text.encode('utf-8')
+    return text
+
+
+def conditional_repr(obj):
+    if isinstance(obj, ABCMeta) and hasattr(obj, 'main_cls'):
+        return repr(obj)
+    elif type_object(obj):
+        return obj.__name__
+    else:
+        return repr(obj)
+
+
+from magic_constraints.parameter import (
+    Parameter, build_parameter_package,
+)  # noqa
+from magic_constraints.exception import (
+    MagicSyntaxError, MagicTypeError,
+)  # noqa
