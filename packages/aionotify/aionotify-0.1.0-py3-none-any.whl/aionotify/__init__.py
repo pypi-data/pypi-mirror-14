e22f43261f107bf58e305be6da6e00bd955cef6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package aionotify/0.1.0/aionotify-0.1.0-py3-none-any.whl

---
+++ packages/aionotify/aionotify-0.1.0-py3-none-any.whl/aionotify/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2016 The aionotify project
+# This code is distributed under the two-clause BSD License.
+
+__version__ = '0.1.0'
+__author__ = 'Raphaël Barrois <raphael.barrois+aionotify@polytechnique.org>'
+
+
+from .enums import Flags
+from .base import Watcher
