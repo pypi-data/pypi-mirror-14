e0d50d131e111b9709d02de2b11bac4e0258726d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:16 +0000

    Add package nester_wyp/1.0.2/nester_wyp-1.0.2.zip

---
+++ packages/nester_wyp/nester_wyp-1.0.2.zip/nester_wyp-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+  name = 'nester_wyp',
+  version = '1.0.2',
+  py_modules = ['nester_wyp'],
+  author = 'patrick wang',
+  author_email = 'wyp4032@163.com',
+  Description = 'just a test module',
+  url = 'www.baidu.com',
+        )
