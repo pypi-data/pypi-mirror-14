116a6ff1c9dd685dd2501193ed27a0ed404b10c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:45 +0000

    Add package window/0.0.2/window-0.0.2.tar.gz

---
+++ packages/window/window-0.0.2.tar.gz/window-0.0.2/setup.py
@@ -0,0 +1,20 @@
+# _*_ coding: utf-8 _*_
+from setuptools import setup, Extension
+#from distutils.core import setup, Extension
+
+setup(name='window',
+      description='Easy ncurses applications',
+      author='Luke Brooks',
+      author_email='luke@psybernetics.org',
+      url="http://github.com/LukeB42/Window",
+      download_url="http://github.com/LukeB42/Window/tarball/0.1",
+      version='0.0.2',
+      py_modules=['window'],
+      long_description="""
+This module provides Window and Pane objects for rapidly creating ncurses programs without sacrificing quality.
+""",
+      keywords="ncurses tui",
+      license="MIT",
+      test_suite='test'
+      )
+
