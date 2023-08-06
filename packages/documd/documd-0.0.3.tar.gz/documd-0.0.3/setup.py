890bfc47082152cc9a73e04d2626f720b0830721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:45 +0000

    Add package documd/0.0.3/documd-0.0.3.tar.gz

---
+++ packages/documd/documd-0.0.3.tar.gz/documd-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='documd',
+    version='0.0.3',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    description='Simple markdown documentation from docstrings',
+    url='https://github.com/projectweekend/documd',
+    license='MIT',
+    packages=['documd'],
+    install_requires=[],
+)
