2b8f89648a6572fef140fb21b17068b9f6ae4540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package courseraprogramming/0.12.1/courseraprogramming-0.12.1.tar.gz

---
+++ packages/courseraprogramming/courseraprogramming-0.12.1.tar.gz/courseraprogramming-0.12.1/courseraprogramming/commands/__init__.py
@@ -0,0 +1,15 @@
+"Commands and their implementations for the courseraprogramming sdk."
+
+__all__ = [
+    "cat",
+    "config",
+    "grade",
+    "inspect",
+    "ls",
+    "publish",
+    "sanity",
+    "upload",
+    "version",
+]
+
+from . import *  # noqa
